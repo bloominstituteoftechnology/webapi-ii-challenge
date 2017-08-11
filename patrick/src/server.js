a447436266dba346195abd27457a8dd50de924fb
mixelpixel <mixelpix@mac.com>
2017-08-11 01:00:30 -0400

    missing title/contents reported but post not added ??? tests3/14

--- patrick/src/server.js
+++ patrick/src/server.js
@@ -6,6 +6,7 @@ const STATUS_USER_ERROR = 422;
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
 const posts = [];
+let id = 0;
 
 const server = express();
 // to enable parsing of json bodies for post requests
@@ -18,7 +19,23 @@ server.get('/posts', (req, res) => {
 });
 
 server.post('/posts', (req, res) => {
-  res.send('from HTTP POST /posts');
+  // res.send('RESPONSE from server: HTTP POST /posts');
+  const title = req.body.title;
+  const contents = req.body.contents;
+  if (!title) {
+    res.status(STATUS_USER_ERROR);
+    res.json({ error: 'Please add a TITLE to your post.' });
+    return;
+  }
+  if (!contents) {
+    res.status(STATUS_USER_ERROR);
+    res.json({ error: 'Please add CONTENTS to your post.' });
+    return;
+  }
+  const aPost = { id, title, contents };
+  posts.push(aPost);
+  res.json(posts);
+  id++;
 });
 
 module.exports = { posts, server };
