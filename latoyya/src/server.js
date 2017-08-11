8d9814a21302793e286596c9a749f205b2fa900d
Latoyya Smith <latoyyas@gmail.com>
2017-08-11 13:23:39 -0500

    updated post methood

--- latoyya/src/server.js
+++ latoyya/src/server.js
@@ -11,6 +11,36 @@ const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
 
+server.get('/', (req, res) => {
+  res.render('index.html');
+});
+
 // TODO: your code to handle requests
+server.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+server.post('/posts', (req, res) => {
+  const title = req.body.title;
+  const contents = req.body.contents;
+  
+  // Should provide both title and contents...error message if not
+  if (!title || !contents) {
+    res.status(STATUS_USER_ERROR);
+    res.json({ error: 'Provide a title and contents' });
+    return;
+  }
+  posts.push(posts);
+  res.send(posts);
+
+});
+
+server.put('/posts', (req, res) => {
+  res.send('Got a PUT request');
+});
+
+server.delete('/posts', (req, res) => {
+  res.send('Got a DELETE request');
+});
 
 module.exports = { posts, server };
