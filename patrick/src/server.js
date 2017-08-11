42e7e30c3768b57816053dfbfd4a4903f8ee0965
mixelpixel <mixelpix@mac.com>
2017-08-10 22:36:47 -0400

    adding id

--- patrick/src/server.js
+++ patrick/src/server.js
@@ -8,6 +8,7 @@ const STATUS_USER_ERROR = 422;
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
 const posts = [];
+let id = 0;
 
 const server = express();
 // to enable parsing of json bodies for post requests
@@ -87,13 +88,14 @@ server.post('/posts', (req, res) => {
 
   // differentiate var name of POST action from what is posted...
   // singular
-  const aPost = { title, contents };
+  const aPost = { id, title, contents };
   posts.push(aPost);
 
   // console.log('server.post('/posts',, ... ) YAY');
   // res.send('HTTP POST something?');
   // res.json({ posts });
   res.json(posts);
+  id++;
 });
 
 // UPDATE
