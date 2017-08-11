a767b2b883b7ae3203b815392f2f183819aa8092
mixelpixel <mixelpix@mac.com>
2017-08-11 12:19:40 -0400

    teh struggle is REAL

--- patrick/src/server.js
+++ patrick/src/server.js
@@ -5,37 +5,52 @@ const STATUS_USER_ERROR = 422;
 
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
-const posts = [];
+// const posts = [];
+const posts = []; // Karthik's hint re: using let posts = [] ???????
 let id = 0;
 
 const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
 
-// TODO: your code to handle requests
+// TODO: FILTERS
 server.get('/posts', (req, res) => {
-  // res.send('RESPONSE from server: HTTP GET /posts');
   res.json(posts); // <--- THIS ALONE PASSES FIRST TEST
 });
 
+// TODO: PASS "Request POST /posts adds a post:" TEST
 server.post('/posts', (req, res) => {
   // res.send('RESPONSE from server: HTTP POST /posts');
   const title = req.body.title;
-  const contents = req.body.contents;
   if (!title) {
     res.status(STATUS_USER_ERROR);
     res.json({ error: 'Please add a TITLE to your post.' });
     return;
   }
+  const contents = req.body.contents;
   if (!contents) {
     res.status(STATUS_USER_ERROR);
     res.json({ error: 'Please add CONTENTS to your post.' });
     return;
   }
-  const aPost = { id, title, contents };
-  posts.push(aPost);
-  res.json(posts);
+  const newPost = [id, title, contents];
+  // for (let i = 0; i < newPost.length; i++) {
+  //   posts.newPost[i];
+  // }
+  // newPost.forEach((item, index) => {
+  //   posts[newPost[index]] = item;
+  // });
+  posts.push(id, title, contents);
+  // posts.push(contents);
+  // posts.id.push(id);
+  // posts = Object.assign(posts, id, title, contents);
+  // res.json({ posts }); // <~~~ per KV intro video
+  res.json(posts);     // <~~~ per Tai
   id++;
 });
+
+// TODO: PUT
+
+// TODO: DELETE
 
 module.exports = { posts, server };
