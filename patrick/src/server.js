2953fdfc675905d275174cf3e4d6d832f3e6783c
mixelpixel <mixelpix@mac.com>
2017-08-10 23:47:48 -0400

    req.query

--- patrick/src/server.js
+++ patrick/src/server.js
@@ -16,9 +16,14 @@ server.use(bodyParser.json());
 
 // TODO: your code to handle requests
 
-// dummy pages
+/* dummy pages ************************************************************* */
 server.get('/hello', (req, res) => {
-  res.send('<h1>Hello!</h1>');
+  // http://localhost:3001/hello?name=Latoyya
+  const name = req.query.name;
+  if (!name) {
+    res.send('<h1>Hello, world!</h1>');
+  }
+  res.send(`<h1>Hello, ${name}!</h1>`);
 });
 server.get('/', (req, res) => {
   fs.readFile('index.html', 'utf8', (err, contents) => {
@@ -29,7 +34,7 @@ server.get('/', (req, res) => {
   });
 });
 
-// READ
+/* READ READ READ READ READ READ READ READ READ READ READ READ READ READ READ */
 // req = request
 // res = response
 // when the server receives
@@ -55,21 +60,28 @@ server.get('/posts', (req, res) => {
     const filtered = posts.filter((post) => {
       return post.title.indexOf(term) !== -1 || post.contents.indexOf(term) !== -1;
     });
-    res.json(filtered);
+    if (filtered.length > 0) {
+      res.json(filtered);
+      return;
+    }
+    res.status(STATUS_USER_ERROR);
+    res.json({ error: 'Please try again.' });
   }
 });
 
-// CREATE
+/* CREATE CREATE CREATE CREATE CREATE CREATE CREATE CREATE CREATE CREATE CREATE */
 // when server receives (i.e. when client sends)
 // an HTTP POST request
 // to POST a resource at the /posts location
 // invoke (req, res) CallBack
 server.post('/posts', (req, res) => {
-  /* add a post to posts array
-  {
-    title: "The post title",
-    contents: "The post contents"
-  }
+  /* add a post to
+  posts = [
+    {
+      title: "The post title",
+      contents: "The post contents"
+    }
+  ]
   */
   const title = req.body.title;
   const contents = req.body.contents;
@@ -98,13 +110,13 @@ server.post('/posts', (req, res) => {
   id++;
 });
 
-// UPDATE
+/* UPDATE UPDATE UPDATE UPDATE UPDATE UPDATE UPDATE UPDATE UPDATE UPDATE UPDATE */
 server.put('/posts', (req, res) => {
   // console.log('server.put('/posts',, ... ) YAY');
   res.send('HTTP PUT something?');
 });
 
-// DESTROY
+/* DESTROY DESTROY DESTROY DESTROY DESTROY DESTROY DESTROY DESTROY DESTROY */
 server.delete('/posts', (req, res) => {
   // console.log('server.delete('/posts',, ... ) YAY');
   res.send('HTTP DELETE something?');
