24a50a26f9d904eb53618af02fda027458da936d
mixelpixel <mixelpix@mac.com>
2017-08-11 21:43:58 -0400

    lecture 14/14!!!

--- patrick/src/server.js
+++ patrick/src/server.js
@@ -8,7 +8,7 @@ const STATUS_AWESOME = 200;
 // to change this to a let binding if you need to reassign it.
 // const posts = [];
 const posts = []; // Karthik's hint re: using let posts = [] ???????
-let id = 1;
+let id = 0;
 
 const server = express();
 // to enable parsing of json bodies for post requests
@@ -49,9 +49,9 @@ server.post('/posts', (req, res) => {
   }
 
   const newPost = { id, title, contents };
-  id++;
   posts.push(newPost);
   res.json(newPost);
+  id++;
 });
 
 // UPDATE
@@ -59,7 +59,6 @@ server.put('/posts', (req, res) => {
   const title = req.body.title;
   const contents = req.body.contents;
   const putId = req.body.id;
-  // const putPost = req.body;
   console.log('HELLO');
   console.log(req.body);
 
@@ -78,37 +77,44 @@ server.put('/posts', (req, res) => {
     res.json({ error: 'Please use an ID to identify the post.' });
     return;
   }
-  // const arty = posts.find((x) => {
-  //   return x.id === putId;
-  // });
-  // console.log(arty);
-  // console.log(req.body);
   console.log('Scooby snacks', posts);
-
   console.log('HERE I AM!');
   for (let i = 0; i < posts.length; i++) {
     console.log(`input ID: ${putId}`);
     console.log(`post array index id value: ${posts[i].id}`);
     if (putId === posts[i].id) {
-      console.log('inside the for loop if');
-      // badID = false;
-      // postsIndex = i;
+      console.log('inside the for loop IF');
       posts[i] = req.body;
-      // putPost = posts[i];
       res.status(STATUS_AWESOME);
       res.json(req.body);
       return;
     }
-    console.log('after the for loop');
-    res.status(STATUS_USER_ERROR);
-    res.json({ error: 'That id is not in the array.' });
-    return;
   }
+  console.log('after the for loop');
+  res.status(STATUS_USER_ERROR);
+  res.json({ error: 'That id is not in the array.' });
+  return;
 });
 
 // TODO: DELETE
 server.delete('/posts', (req, res) => {
-  res.send(posts);
+  const putId = req.body.id;
+  if (!putId) {
+    res.status(STATUS_USER_ERROR);
+    res.json({ error: 'Please use an ID to identify the post you wanna NUKE OUT OF EXISTENCE.' });
+    return;
+  }
+
+  for (let i = 0; i < posts.length; i++) {
+    if (putId === posts[i].id) {
+      // DELETE IT
+      posts.splice(i, 1);
+      res.status(200).json({ success: true }); return;
+    }
+  }
+  res.status(STATUS_USER_ERROR);
+  res.json({ error: "YOU F'ED UP BUDDY}" });
+  return;
 });
 
 module.exports = { posts, server };
