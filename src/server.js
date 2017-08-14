786ab2156a53345c18d5dc12fc03486ae590d5f2
wlharvey4 <pinecone062@gmail.com>
2017-08-13 23:41:58 -0700

    server.js completed

--- src/server.js
+++ src/server.js
@@ -3,27 +3,20 @@ const express = require('express');
 
 const STATUS_OK = 200;
 const STATUS_USER_ERROR = 422;
-const STRING = 'string';
 
 let nextID = 0;
-
 class Post {
   constructor(title, contents) {
     this.title = title;
     this.contents = contents;
     this.id = nextID++;
   }
-  toString() {
-    return `title: ${this.title}\tcontents: ${this.contents}`;
-  }
 }
 
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
 const posts = [];
-
 const server = express();
-
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
 
