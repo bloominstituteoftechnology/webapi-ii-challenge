a3a4bf1ef864b74bbc46202bd854d633fe68ba30
Noah Yamamoto <egrodo1@gmail.com>
2017-08-21 00:19:33 -0600

    Fixed syntax errors and passed all tests but one I disagree with.

--- src/server.js
+++ src/server.js
@@ -11,7 +11,6 @@ const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
 
-// TODO: your code to handle requests
 
 // GET method to handle requests to root.
 server.get('/', (req, res) => {
@@ -41,32 +40,33 @@ server.get('/posts', (req, res) => { // Display the posts.
     }
   });
 
-  if (results.length > 0)  { // If the search found a post.
+  if (results.length > 0) { // If the search found a post.
     res.json(results);
   } else {
     res.status(STATUS_USER_ERROR);
-    res.json({error: "Search term came up with no results."});
+    res.json({ error: 'Search term came up with no results.' });
   }
 });
 
 // POST method to make a new post.
 server.post('/posts', (req, res) => {
   if (!req.body.title || !req.body.contents) { // If any parameters are misisng, notify user.
-    let response = "You need to also send ";
+    let response = 'You need to also send ';
     if (!req.body.title && !req.body.contents) {
-      response += "both the title and the contents ";
+      response += 'both the title and the contents ';
     } else {
-      !req.body.title ? response += "the title " : ' ';
-      !req.body.contents ? response += "the contents " : ' ';
+      if (!req.body.title) response += 'the new title ';
+      if (!req.body.contents) response += 'the new contents ';
     }
-    response += "to build a post.";
+    response += 'to build a post.';
     res.status(STATUS_USER_ERROR);
-    res.json({error: response});
+    res.json({ error: response });
     return;
   }
 
-  const id = Math.random().toString().substr(2, 5); // Check for collision somehow.
-  const newPost = {id, title: req.body.title, contents: req.body.contents};
+  // TODO Check for collision somehow?
+  const id = parseInt(Math.random().toString().substr(2, 5), 10); // get a random number, convert to string so we can slice off all but 5 of it, then convert back to int.
+  const newPost = { id, title: req.body.title, contents: req.body.contents };
   posts.push(newPost);
   res.json(newPost);
 });
@@ -75,23 +75,23 @@ server.post('/posts', (req, res) => {
 server.put('/posts', (req, res) => {
   if (posts.length === 0) {
     res.status(STATUS_USER_ERROR);
-    res.json({error: "There are no posts in memory."});
+    res.json({ error: 'There are no posts in memory.' });
     return;
   }
 
   // IDEA What if they only want to update title/contents?
   if (!req.body.title || !req.body.contents || !req.body.id) { // If any parameters are misisng, notify user.
-    let response = "You need to send ";
+    let response = 'You need to send ';
     if (!req.body.title && !req.body.contents) {
-      response += "the id and new title/contents ";
+      response += 'the id and new title/contents ';
     } else {
-      !req.body.id ? response += "the id of the post " : '';
-      !req.body.title ? response += "the new title " : ' ';
-      !req.body.contents ? response += "the new contents " : ' ';
+      if (!req.body.id) response += 'the id of the post ';
+      if (!req.body.title) response += 'the new title ';
+      if (!req.body.contents) response += 'the new contents ';
     }
-    response += "to update a post.";
+    response += 'to update a post.';
     res.status(STATUS_USER_ERROR);
-    res.json({error: response});
+    res.json({ error: response });
     return;
   }
 
@@ -106,32 +106,32 @@ server.put('/posts', (req, res) => {
   }
 
   res.status(STATUS_USER_ERROR);
-  res.json({error: "That id wasn't found."});
+  res.json({ error: "That id wasn't found." });
 });
 
 server.delete('/posts', (req, res) => {
   if (posts.length === 0) {
     res.status(STATUS_USER_ERROR);
-    res.json({error: "There are no posts in memory."});
+    res.json({ error: 'There are no posts in memory.' });
     return;
   }
 
   if (!req.body.id) {
     res.status(STATUS_USER_ERROR);
-    res.json({error: "You need to send the post id."});
+    res.json({ error: 'You need to send the post id.' });
     return;
   }
 
   for (let i = 0; i < posts.length; i++) {
     if (posts[i].id === req.body.id) {
       posts.splice(i, 1);
-      res.json({success: true});
+      res.json({ success: true });
       return;
     }
   }
 
   res.status(STATUS_USER_ERROR);
-  res.json({error: "id didn't match any posts in memory."});
+  res.json({ error: "id didn't match any posts in memory." });
 });
 
 module.exports = { posts, server };
