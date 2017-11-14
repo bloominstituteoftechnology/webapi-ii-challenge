18469291d6048236ad5283c5b3b5495d8c184ac6
Kia Choi <sgchoi529@email.arizona.edu>
2017-11-14 12:23:38 -0700

    Kia's Commit - Completed from Q&A

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 
 const STATUS_USER_ERROR = 422;
+const STATUS_CREATED = 201;
 
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
@@ -26,12 +27,9 @@ const server = express();
 server.use(bodyParser.json());
 
 // TODO: your code to handle requests
-server.get('/posts', (req, res, next) => {
-    res.status(200).json(posts);
-});
 
 server.post('/posts', (req, res, next) => {
-   const { term } = req.query;
+   const term = req.query.term;
    if (term) {
        const termPosts = posts.filter((post) => {
             return post.title.indexOf(term) !== -1 ||
@@ -46,14 +44,69 @@ server.post('/posts', (req, res, next) => {
 server.post('/posts', (req, res) => {
     const { title, contents } = req.body;
     if (!title) {
-        res.status(STATUS_USER_ERROR).json( {error: 'Must provide a post title.'} );
+        res.status(STATUS_USER_ERROR)
+           .json( {error: 'Must provide a post title.'} );
         return;
     }
 
     if (!contents) {
-        res.status(STATUS_USER_ERROR).json( {error: 'Must provide a post content.'} );
+        res.status(STATUS_USER_ERROR)
+           .json({ error: 'Must provide a post content.' });
         return;
     }
+
+    const post = { id: nextID,
+                   title,
+                   contents
+                };
+    post.push(post);
+    res.status(STATUS_CREATED).json(post);
 });
+
+server.put('/posts', (req, res, next) => {
+    const { id, title, contents } = req.body;
+
+    if (!id) {
+        res.status(STATUS_USER_ERROR)
+           .json({ error: 'Must provide a post id.' });
+        return;
+    }
+
+    if (!title) {
+        res.status(STATUS_USER_ERROR)
+           .json({ error: 'Must provide a post title.' });
+        return;
+    }
+
+    if (!contents) {
+        res.status(STATUS_USER_ERROR)
+        .json({ error: 'Must provide a post content.' });
+     return;
+    }
+
+    const post = posts.find(p => p.id === id);
+    if (!post) {
+        res.status(STATUS_USER_ERROR)
+        .json({ error: 'Must provide a post id.' });
+     return;
+    }
+
+    post.title = title;
+    post.contents = contents;
+    res.json(post);
+});
+
+server.delete('/posts', (req, res, next) => {
+    const { id } = req.body;
+
+    if (!id) {
+        res.status(STATUS_USER_ERROR)
+        .json({ error: 'Must provide a post id.' });
+     return;
+    }
+
+    const post = posts.find(p => p.id === id);    
+})
+
 
 module.exports = { posts, server };
