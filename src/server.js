3760c022052a2bcf9cec14e1dbf834f89dc94e81
Perry Ahern <pahern@gmail.com>
2017-11-14 11:59:39 -0500

    Working GET and POST, started on PUT. No DELETE yet.

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-const bodyParser = require('body-parser');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const STATUS_USER_ERROR = 422;
 
@@ -11,6 +11,41 @@ const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
 
-// TODO: your code to handle requests
+server.get('/', function(req, res) {
+  res.status(200).send('<h2>Home Page</h2>');
+});
+
+server.get('/posts', function(req, res) {
+  const term = req.query.term;
+  let results = '';
+
+  if (term) {
+    results = posts.filter(phrase => phrase.title.includes(term) || phrase.contents.includes(term));
+  } else {
+    results = posts;
+  }
+  res.status(200).json(results);
+});
+
+server.post('/posts', function(req, res) {
+  const post = req.body;
+
+  if (post.title && post.contents) {
+    posts.push(post);
+    res.status(200).json(posts);
+  } else {
+    res.status(422).json(`User error`);
+  }
+});
+
+server.put('/posts', function(req, res) {
+  const update = req.body;
+
+  if (update.id && update.title && update.body) {
+    //update post
+  } else {
+    res.status(422).json(`User did not supply all fields: id, title, body`);
+  }
+});
 
 module.exports = { posts, server };
