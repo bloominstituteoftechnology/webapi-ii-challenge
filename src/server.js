f4a06cbef4ee0ab09b7bd2eee76af901d9da1ceb
Tyler Sanford <tylersanford0311@gmail.com>
2017-11-13 19:01:57 -0600

    Finished!

--- src/server.js
+++ src/server.js
@@ -11,6 +11,79 @@ const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
 
-// TODO: your code to handle requests
+const PORT = 4000;
+
+server.listen(PORT, (error) => {
+  if (error) return `<h2>Error! </h2> ${error}`;
+  return `I see you on port ${PORT}`;
+});
+
+const handleErrors = (res, error) => {
+  return res.status(STATUS_USER_ERROR).json(error);
+};
+
+server.get('/posts', (req, res) => {
+  const term = req.query.term;
+  let newPosts = [];
+  if (!term) res.json(posts);
+  newPosts = posts.filter((post) => {
+    return post.title.includes(term) || post.contents.includes(term);
+  });
+  res.json(newPosts);
+});
+
+server.post('/posts', (req, res) => {
+  const { title, contents } = req.body;
+  if (!title || !contents) {
+    return handleErrors(res, {
+      error: 'Your title or content was empty',
+    });
+  }
+  const post = {
+    title,
+    contents,
+    id: posts.length ? posts[posts.length - 1].id + 1 : 1,
+  };
+  posts.push(post);
+
+  res.status(201).json(post);
+});
+
+server.put('/posts', (req, res) => {
+  const { title, contents, id } = req.body;
+  if (!title || !contents || !id) {
+    return handleErrors(res, {
+      error: 'You must include a title, content, and and id to update',
+    });
+  }
+
+  const postToUpdate = posts.findIndex(post => id === post.id);
+
+  if (postToUpdate === -1) {
+    return handleErrors(res, {
+      error: 'No post found with that ID',
+    });
+  }
+
+  posts[postToUpdate] = { title, contents, id };
+  return res.json(posts[postToUpdate]);
+});
+
+server.delete('/posts', (req, res) => {
+  const id = req.body.id;
+  if (!id) {
+    return handleErrors(res, {
+      error: 'Must provide an ID',
+    });
+  }
+  const postToDelete = posts.findIndex(post => id === post.id);
+  if (postToDelete === -1) {
+    return handleErrors(res, {
+      error: 'Post not found, enter correct ID',
+    });
+  }
+  posts.splice(postToDelete, 1);
+  return res.json({ success: true });
+});
 
 module.exports = { posts, server };
