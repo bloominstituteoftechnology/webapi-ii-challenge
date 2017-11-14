1c079ddd5fa88e8a6028bb00d4ab1f9ed00e3362
Gary Dillard <thomas.dillard.studios@gmail.com>
2017-11-13 19:12:27 -0700

    cleaned up code: applied a proper unique id.

--- src/server.js
+++ src/server.js
@@ -3,10 +3,12 @@ const express = require('express');
 
 const STATUS_USER_ERROR = 422;
 const STATUS_OK = 200;
+const error = `Error status: ${STATUS_USER_ERROR}`;
 
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
 const posts = [];
+let lastId = 0;
 
 const server = express();
 // to enable parsing of json bodies for post requests
@@ -21,37 +23,53 @@ server.get('/posts', (req, res) => {
       return post.title.includes(term)
         || post.contents.includes(term);
     });
+
   res.status(STATUS_OK).send(retValue);
+});
+
+server.get('/posts/:id', (req, res) => {
+  const id = Number(req.params.id);
+
+  if (id > posts.length) {
+    res.status(STATUS_USER_ERROR).send({ error });
+  } else {
+    const postIndex = posts.findIndex(post => post.id === id);
+    res.status(STATUS_OK).send(posts[postIndex]);
+  }
 });
 
 server.post('/posts', (req, res) => {
   const { title, contents, } = req.body;
-  let post;
+
   if (!title || !contents) {
-    res.status(STATUS_USER_ERROR).send({ error: 'Error Message' });
+    res.status(STATUS_USER_ERROR).send({ error });
   } else {
-    post = { title, contents, id: posts.length + 1, };
+    const post = { title, contents, id: ++lastId, };
     posts.push(post);
+    res.status(STATUS_OK).send(post);
   }
-  res.status(STATUS_OK).send(post);
 });
 
 server.put('/posts', (req, res) => {
   const { id, title, contents, } = req.body;
-  if (!id || !title || !contents || id > posts.length) {
-    res.status(STATUS_USER_ERROR).send({ error: 'Error Message' });
+  const index = posts.findIndex(post => post.id === id);
+
+  if (!id || !title || !contents || index === -1) {
+    res.status(STATUS_USER_ERROR).send({ error });
   } else {
-    posts[id - 1] = { id, title, contents, };
-    res.status(STATUS_OK).send(posts[id - 1]);
+    posts[index] = { id, title, contents, };
+    res.status(STATUS_OK).send(posts[index]);
   }
 });
 
 server.delete('/posts', (req, res) => {
   const { id } = req.body;
-  if (!id || id > posts.length) {
-    res.status(STATUS_USER_ERROR).send({ error: 'Error Message' });
+  const index = posts.findIndex(post => post.id === id);
+
+  if (!id || index === -1) {
+    res.status(STATUS_USER_ERROR).send({ error });
   } else {
-    posts.splice(id - 1, 1);
+    posts.splice(index, 1);
     res.status(STATUS_OK).send({ success: true });
   }
 });
