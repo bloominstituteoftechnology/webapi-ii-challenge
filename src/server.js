fbd20df390139bfe045b9a527c5eb733695ac1ba
ubuntu <irishseanvaldivia@gmail.com>
2017-12-20 17:20:58 -0600

    Progress...

--- src/server.js
+++ src/server.js
@@ -5,14 +5,23 @@ const STATUS_USER_ERROR = 422;
 
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
-const posts = [];
+let posts = [
+  {
+    title: 'Keep on walking...',
+    contents: 'More mind-boggling stuff.',
+    id: 0
+  },
+  {
+    title: 'Just another',
+    contents: 'Even more stuff.',
+    id: 1
+  }
+];
 
 const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
 
 // TODO: your code to handle requests
-server.listen(3000, () => {
-  console.log("Let's get this show started!");
-});
+
 module.exports = { posts, server };
