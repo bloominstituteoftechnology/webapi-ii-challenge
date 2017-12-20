44d1f235ea209589cf7d28e9786b8800cc4491bb
fairlight-excalibur <lidgrt@gmail.com>
2017-12-20 16:40:58 -0600

    donezo

--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
 const { server } = require('./server.js');
 
+server.get("/", function( req, res ) {
+	console.log('page requested');
+	res.send('<h1>Hello cruel world</h1>');
+});
+
 server.listen(3000);
