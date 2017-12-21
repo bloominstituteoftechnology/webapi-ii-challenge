48f7ebedac0eae428f316266b48999237435442f
atiffany <abby.tiffany@gmail.com>
2017-12-20 19:39:53 -0500

    POST operational. Updated variable name to match README instructions

--- src/server.js
+++ src/server.js
@@ -6,8 +6,8 @@ const STATUS_USER_ERROR = 422;
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
 const posts = [
-    { title: 'First', post: 'Post1' },
-    { title: 'Second', post: 'Gouda\'s my favorite' }
+    { title: 'First', content: 'Post1' },
+    { title: 'Second', content: 'Gouda\'s my favorite' }
 ];
 const server = express();
 // to enable parsing of json bodies for post requests
@@ -19,15 +19,15 @@ server.get('/api/posts', (req, res) => {
   if (req.query.term) {
     const term = req.query.term;
     res.status(200).json(posts.filter((element) => {
-      return (element.title.includes(term) || element.post.includes(term));
+      return (element.title.includes(term) || element.content.includes(term));
     }));
   }
   res.status(200).json(posts);
 });
 
 server.post('/api/posts', (req, res) => {
-  if (typeof req.body.title === 'string' && typeof req.body.post === 'string') {
-    const newPost = { title: req.body.title, post: req.body.post };
+  if (typeof req.body.title === 'string' && typeof req.body.content === 'string') {
+    const newPost = { title: req.body.title, content: req.body.content };
     posts.push(newPost);
     res.status(200).json(posts);
   } else {
