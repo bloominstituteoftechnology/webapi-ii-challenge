d5076c361d9d76646f3b4a517ce4201337010320
JacquelynnRohrich <33401573+JacquelynnRohrich@users.noreply.github.com>
2017-12-21 12:07:37 -0600

    ugh

--- src/app.js
+++ src/app.js
@@ -1,3 +1,29 @@
+const { server } = require ('./server.js');
+const bodyParser = require ('body-parser');
+
+const port = 3000,
+const postsArray = [
+    {
+        title: "The post title", 
+        contents: "The post contents",
+    }
+];
+server.use(bodyParser.json());
+
+server.get('/', (req, res) => {
+    res.send(postsArray);
+});
+
+server.get('/posts', (req, res) => {
+    let term =req.query;
+    console.log(term);
+    let result = postArray.find((post) => {
+        let post = (req.body);
+        postsArray.push(post);
+        res.status(201).json(postsArray);
+    
+})
+
 const { server } = require('./server.js');
 
 server.listen(3000);
--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 const bodyParser = require('body-parser');
 const express = require('express');
 
@@ -12,5 +13,62 @@ const server = express();
 server.use(bodyParser.json());
 
 // TODO: your code to handle requests
+server.get('/posts', function (req, res) {
+    const searchTerm = req.query.term;
+    if (searchTerm) {
+        const filteredPosts = posts.filter(post => {
+            return post.title.includes(searchTerm) || post.contents.includes(searchTerm);
+        })
+        res.status(200).json(filteredPosts);
+    } else {
+        res.status(200).json(posts);
+    }
+    }
+});
 
+server.post('/posts', function (req, res) {
+    const { title, contents } = req.body;
+    if(title && contents) {
+        const id = getNextId();
+        const post = req.body; //{ title, contents }
+        post.id = id;
+        posts.push(post);
+        res.status(200).json(post);
+    } else {
+        res.status(STATUS_USER_ERROR).json ({ error: 'Please provide title and contents'})
+    }
+server.put('/posts', function(req, res) {
+    const { id, title, contents } = req.body;
+
+    if(id && title && contents) {
+        let post= posts.find(p => p.id === Number(id))
+        if (post) {
+            Object.assign(post, req.body );
+            res.status(200).jsopn(post);
+        }
+    } else {
+        res
+        .status(STATUS_USER_ERROR).json({ error: ' The post does not exist.'})
+        .json({ error: 'Please provide all the information'});
+    }
+});
+
+server.delete('/posts', functions(req, res) {
+    const { id } = req.body;
+    if (id) {
+        let post = posts.find(p => p === Number(id));
+        if (postIndex >= 0) {
+            posts.splice(postIndex, 1);
+            res.status(200).json({ success: true });
+        } else {
+            res.status(STATUS_USER_ERROR).json({ error: 'The post does not exist'});
+            } 
+        }else {
+            res
+            .status(STATUS_USER_ERROR)
+            .json ({ error: 'Please provide a valid Id.'})
+        }
+        }
+    }
+})
 module.exports = { posts, server };
