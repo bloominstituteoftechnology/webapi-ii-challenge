ef2755a7720f645c2cd225c9aac3faa4237c3132
atiffany <abby.tiffany@gmail.com>
2017-12-21 13:33:10 -0500

    passing all but two tests

--- src/server.js
+++ src/server.js
@@ -6,8 +6,8 @@ const STATUS_USER_ERROR = 422;
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
 const posts = [
-    { id: 0, title: 'First', content: 'Post1' },
-    { id: 1, title: 'Second', content: 'Gouda\'s my favorite' }
+    { id: 0, title: 'First', contents: 'Post1' },
+    { id: 1, title: 'Second', contents: 'Gouda\'s my favorite' }
 ];
 const server = express();
 // to enable parsing of json bodies for post requests
@@ -15,19 +15,20 @@ server.use(bodyParser.json());
 
 // TODO: your code to handle requests
 // works
-server.get('/api/posts', (req, res) => {
+server.get('/posts', (req, res) => {
   if (req.query.term) {
     const term = req.query.term;
     res.status(200).json(posts.filter((element) => {
-      return (element.title.includes(term) || element.content.includes(term));
+      return (element.title.includes(term) || element.contents.includes(term));
     }));
   }
   res.status(200).json(posts);
 });
 
-server.post('/api/posts', (req, res) => {
-  if (typeof req.body.title === 'string' && typeof req.body.content === 'string') {
-    const newPost = { id: [posts.length], title: req.body.title, content: req.body.content };
+server.post('/posts', (req, res) => {
+  const { title, contents } = req.body;
+  if (title && contents) {
+    const newPost = { title, contents, id: posts.length };
     posts.push(newPost);
     res.status(200).json(newPost);
   } else {
@@ -35,13 +36,13 @@ server.post('/api/posts', (req, res) => {
   }
 });
 
-server.put('/api/posts', (req, res) => {
+server.put('/posts', (req, res) => {
     // shorthand from lecture
-  const { id, title, content } = req.body;
-  if (title && content && id) {
-    const replacementPost = { id, title, content };
+  const { title, contents, id } = req.body;
+  if (title && contents && id) {
+    const replacementPost = { title, contents, id };
     // find if a post already has this id
-    let post = posts.find(p => p[id] === Number(id));
+    let post = posts.find(p => p.id === Number(id));
     if (post) {
       post = replacementPost;
       res.status(200).send(post);
@@ -53,7 +54,7 @@ server.put('/api/posts', (req, res) => {
   }
 });
 
-server.delete('/api/posts', (req, res) => {
+server.delete('/posts', (req, res) => {
   const index = req.body.id;
   const postIndex = posts.findIndex(p => p.index === Number(index));
   if (postIndex >= 0) {
