7e3af39894ac60648b9be2d6e98f818d9000f75d
atiffany <abby.tiffany@gmail.com>
2017-12-21 12:11:47 -0500

    updates to put

--- src/server.js
+++ src/server.js
@@ -6,8 +6,8 @@ const STATUS_USER_ERROR = 422;
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
 const posts = [
-    { title: 'First', content: 'Post1' },
-    { title: 'Second', content: 'Gouda\'s my favorite' }
+    { id: 0, title: 'First', content: 'Post1' },
+    { id: 1, title: 'Second', content: 'Gouda\'s my favorite' }
 ];
 const server = express();
 // to enable parsing of json bodies for post requests
@@ -27,7 +27,7 @@ server.get('/api/posts', (req, res) => {
 
 server.post('/api/posts', (req, res) => {
   if (typeof req.body.title === 'string' && typeof req.body.content === 'string') {
-    const newPost = { title: req.body.title, content: req.body.content };
+    const newPost = { id: [posts.length], title: req.body.title, content: req.body.content };
     posts.push(newPost);
     res.status(200).json(posts);
   } else {
@@ -36,7 +36,17 @@ server.post('/api/posts', (req, res) => {
 });
 
 server.put('/api/posts', (req, res) => {
-  posts[req.body.index] = req.body.post;
-  res.send(posts);
+  if (typeof req.body.title === 'string' && typeof req.body.content === 'string' && typeof req.body.id === 'number') {
+    const replacementPost = { id: req.body.id, title: req.body.title, content: req.body.content };
+    // find if a post already has this id
+    if (posts[req.body.id]) {
+      posts[req.body.id] = replacementPost;
+      res.send(posts);
+    } else {
+      res.status(400).json({ error: 'not a valid id' });
+    }
+  } else {
+    res.status(400).json({ error: 'please include all needed data' });
+  }
 });
 module.exports = { posts, server };
