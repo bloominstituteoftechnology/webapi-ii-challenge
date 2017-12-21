4e168dc0637a284f472bb8e9bc30e1217ce5e1cf
sarika <sarikaresume@gmail.com>
2017-12-20 17:32:51 -0800

    nodemon

--- src/server.js
+++ src/server.js
@@ -3,8 +3,20 @@ const express = require('express');
 
 const STATUS_USER_ERROR = 422;
 
+server.get('/greet-me', (req, res) => {
+  res.send('<h1>Hello!'</h1>);
+});
+
+server.listen(3000);
+
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
+
+
+title: "The post title",
+contents: "The post contents"
+}
+
 const posts = [];
 
 const server = express();
@@ -14,3 +26,7 @@ server.use(bodyParser.json());
 // TODO: your code to handle requests
 
 module.exports = { posts, server };
+
+
+
+GET /posts` POST /posts` PUT /posts`
