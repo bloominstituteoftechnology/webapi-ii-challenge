b788fefe00ebb95cebb1c9fe184fec00eeaaa3dd
sarika <sarikaresume@gmail.com>
2017-12-21 12:50:27 -0800

    index.html

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 console.log ('index is executed'); // CommonJS module syntax 
 // import express from 'express'; // ES2015 module syntax 
 
+const express = require('express');
 const server = express(); // this is a web server 
 
 server.get('/', function(req, res))
-  console.log('page requested');
-  res.send('Hello CS5');
-})
+  res.send(<h1>Hello CS5!</h1>)<p>This is a long paragraph</p>;
+});
 
-server.listen(8000); 
+const port = 8000; 
+server.listen(port, () => console.log('Server running on ${port}));
+
+// npm i -g nodemon 
+// yarn global add nodemon
+
+// to run the server type nodemon index.js
+// to stop the server when running use ctrl + c.
+// to restart the server if running with nodemon just type rs
+// http://localhost:8000/
