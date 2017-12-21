c72ad0c81802e244793976be0464bd8848812d54
sarika <sarikaresume@gmail.com>
2017-12-21 13:16:49 -0800

    about.html

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ console.log ('index is executed'); // CommonJS module syntax
 // import express from 'express'; // ES2015 module syntax 
 
 const express = require('express');
+const path = ''
 const server = express(); // this is a web server 
 
 // middleware 
@@ -13,11 +14,20 @@ server.use(function(req, res, next) {
 
 server.use(function(req, res, next) {
   console.log('middleware 2')
+  const loggedIn = true; 
+  if (!loggedIn) {
+      res.status(400).send('Not Logged In')
+  } else {
+      next(); 
+  }
 });
 
 server.get('/', function(req, res) { // req, res === the homies 
   res.send(<h1>Hello CS5!</h1>)<p>This is a long paragraph</p>);
 });
+
+const clientUrl = path.join(); 
+server.use('*', express.static())
 
 const port = 8000; 
 server.listen(port, () => console.log('Server running on ${port}));
