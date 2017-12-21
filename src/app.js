482254464ec786a5e27a332d65ce5f096bca6c81
atiffany <abby.tiffany@gmail.com>
2017-12-20 19:23:02 -0500

    GET function operational

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+
 const { server } = require('./server.js');
 
 server.listen(3000);
--- src/server.js
+++ src/server.js
@@ -5,12 +5,38 @@ const STATUS_USER_ERROR = 422;
 
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
-const posts = [];
-
+const posts = [
+    { title: 'First', post: 'Post1' },
+    { title: 'Second', post: 'Gouda\'s my favorite' }
+];
 const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
 
 // TODO: your code to handle requests
+// works
+server.get('/api/posts', (req, res) => {
+  if (req.query.term) {
+    const term = req.query.term;
+    res.status(200).json(posts.filter((element) => {
+      return (element.title.includes(term) || element.post.includes(term));
+    }));
+  }
+  res.status(200).json(posts);
+});
 
+server.post('/api/posts', (req, res) => {
+  if (typeof req.body.title === 'string' && typeof req.body.post === 'string') {
+    const newPost = { title: req.body.title, post: req.body.post };
+    posts.push(newPost);
+    res.status(200).json(posts);
+  } else {
+    res.status(400).json({ error: 'needs title and content' });
+  }
+});
+
+server.put('/api/posts', (req, res) => {
+  posts[req.body.index] = req.body.post;
+  res.send(posts);
+});
 module.exports = { posts, server };
