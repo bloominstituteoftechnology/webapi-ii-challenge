f611a5892abde0f02b7053a482022879014c243f
unknown <brcampbell001@outlook.com>
2018-02-14 11:48:12 -0600

    Starting on GET & POST

--- src/server.js
+++ src/server.js
@@ -13,4 +13,18 @@ server.use(bodyParser.json());
 
 // TODO: your code to handle requests
 
+// Get /posts
+//Query-string parameter term
+//filter the posts to those that have term in their title or contents or both
+//send down searched posts in a JSON response
+//Otherwise, send down the full array of posts as a JSON response
+
+// POST /posts
+//Require client: title || contents in req body
+// JSON error object - needs appropriate status code
+//If all correct, create new post object
+//Post needs unique, numeric id and add to post array
+//return newly created post object with assigned id to client in a JSON response
+
+
 module.exports = { posts, server };
