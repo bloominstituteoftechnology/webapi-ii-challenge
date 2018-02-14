c19eb521045a9335daed2ac6ad0b421ba77c20cd
unknown <brcampbell001@outlook.com>
2018-02-14 12:32:48 -0600

    GET AND POST FUNCTIONING

--- src/server.js
+++ src/server.js
@@ -2,22 +2,48 @@ const bodyParser = require('body-parser');
 const express = require('express');
 
 const STATUS_USER_ERROR = 422;
-
+const POST_ADDED = 200;
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
 const posts = [];
+let id = 1;
+
 
 const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
 
 // TODO: your code to handle requests
+server.get('/posts', (req, res) => {
+    const term = req.query.term;
+    if (term) {
+        const filtered = posts.filter((post) => {
+            return (
+                post.title.includes(term) || post.contents.includes(term)
+            );
+        });
+        res.status(200).json(filtered);
+    } else {
+        res.status(200).json(posts);
+    }
+})
 
-// Get /posts
-//Query-string parameter term
-//filter the posts to those that have term in their title or contents or both
-//send down searched posts in a JSON response
-//Otherwise, send down the full array of posts as a JSON response
+server.post('/posts', (req,res) => {
+    const title = req.body.title;
+    const contents = req.body.contents;
+
+    if(!title || !contents) {
+        res.status(STATUS_USER_ERROR);
+        res.json({ error: 'Must provide a title and content' });
+        return;
+    }
+    const addPost = {id, title, contents};
+    id++;
+    posts.push(addPost);
+    res.status(POST_ADDED);
+    res.json({ posts });
+    return;
+});
 
 // POST /posts
 //Require client: title || contents in req body
