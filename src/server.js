e8f1f2c274ff0e34155f8025ef99e6e54f260b9d
perryahern <pahern@gmail.com>
2018-02-14 13:33:40 -0500

    First commit, working post and get post includes id, title, contents get returns all or specific matching posts if term is provided

--- src/server.js
+++ src/server.js
@@ -1,16 +1,55 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 
+const STATUS_SUCCESS = 200;
+const STATUS_NO_CONTENT = 204;
 const STATUS_USER_ERROR = 422;
 
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
 const posts = [];
+let postId = 0;
 
 const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
 
-// TODO: your code to handle requests
+// POST operation
+server.post('/posts', (req, res) => {
+  if (!req.body.title || !req.body.contents) {
+    res.status(STATUS_USER_ERROR);
+    res.json({ error: 'Must provide both a title and contents' });
+  }
+  const newPost = { id: postId, title: req.body.title, contents: req.body.contents };
+  posts.push(newPost);
+  res.status(STATUS_SUCCESS);
+  res.json(newPost);
+});
+
+// GET operation:  GET all or just those containing specific term in title or contents
+server.get('/posts', (req, res) => {
+  if (req.query.term) {
+    // filter to find matching posts
+    const matchingPosts = posts.filter((post) => {
+      if (post.title.includes(req.query.term) || post.contents.includes(req.query.term)) return post;
+      return null;
+    });
+    if (matchingPosts.length === 0) {
+      // no matching posts were found
+      res.status(STATUS_NO_CONTENT);
+      res.json({ searchResults: `No posts found containing ${req.query.term}` });
+    } else {
+      // matching posts were found
+      res.status(STATUS_SUCCESS);
+      res.json({ searchResults: matchingPosts });
+    }
+  } else {
+    // no search term, return all
+    res.status(STATUS_SUCCESS);
+    res.json(posts);
+  }
+});
+
+server.listen(3000);
 
 module.exports = { posts, server };
