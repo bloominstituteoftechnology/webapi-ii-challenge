8ac95746452023d82c9627cf4e1b65b7c0588e7e
Troy Bradley <travlr2010@gmail.com>
2018-02-14 16:29:11 -0600

    get posts works

--- server.js
+++ server.js
@@ -11,11 +11,27 @@ const server = express();
 server.listen(PORT);
 server.use(bodyParser.json());
 
+let posts = [];
+let postId = 100;
+
+const writePosts = _ => {
+    fs.writeFileSync('posts.txt', JSON.stringify(posts), 'utf8');
+};
+
+const loadFile = _ => {
+    posts = JSON.parse(fs.readFileSync('posts.txt', 'utf8'));
+};
+
+loadFile();
+
+server.get('/posts', (req, res) => {
+    // fs.readFile('');
+    res.json(posts);
+});
+
 server.get('/', (req, res) => {
     res.send(
         '<p>Hello Troy!</p>'+
         '<p>The fun stuff is at /posts</p>'
     );
 });
-
-
