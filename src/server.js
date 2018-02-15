5bc9dd9fecc378037c752e18eb235981a9220f32
Anthony Ellis <aellisnola@gmail.com>
2018-02-14 18:19:11 -0600

    Get and Post up and running.

--- src/server.js
+++ src/server.js
@@ -1,16 +1,51 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 
+const STATUS_OK = 200;
 const STATUS_USER_ERROR = 422;
 
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
 const posts = [];
+let id = 1;
 
 const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
 
 // TODO: your code to handle requests
+
+server.get('/posts', (req, res) => {
+    const searchTerm = req.query.term;
+    if (searchTerm) {
+        const filtered = posts.filter((post) => {
+            return (
+                post.title.includes(seachTerm) || post.content.includes(term)
+            );
+        });
+        res.status(STATUS_OK).json(filtered);
+    } else {
+        res.status(STATUS_OK).json(posts);
+    }
+})
+
+
+server.post('/posts', (req, res) => {
+    const title = req.body.title;
+    const content = req.body.content;
+
+    if (!title || !content) {
+        res.status(STATUS_USER_ERROR);
+        res.json({ error: 'Please provide title and content.' });
+        return;
+    }
+
+    const addPost = { id, title, content };
+    id++;
+    posts.push(addPost);
+    res.status(STATUS_OK);
+    res.json({ posts });
+    return;
+});
 
 module.exports = { posts, server };
