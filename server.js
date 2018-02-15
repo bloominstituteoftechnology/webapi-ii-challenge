0c70569570b2f18e714970f8da0ac4a76c7dacc9
Anthony Ellis <aellisnola@gmail.com>
2018-02-15 15:51:11 -0600

    Server now working

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const server = express();
 const PORT = config.port;
 const GMAPS_KEY = config.gmaps.key;
 
+/* const url = `https://maps.googleapis.com/maps/api//place/textsearch/json?query=${req.body.search}&key=${GMAPS_KEY}` */
+
+
+
 server.listen(PORT, err => {
     if (err) {
         console.log(`Error starting server: ${err}`);
