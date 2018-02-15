9729b0111059672ef9ea232cf6dc89f202ab7a32
Anthony Ellis <aellisnola@gmail.com>
2018-02-15 10:05:33 -0600

    Delete works... attempted put but still trying to get it to actually work in Postman

--- src/server.js
+++ src/server.js
@@ -16,33 +16,77 @@ server.use(bodyParser.json());
 // TODO: your code to handle requests
 
 server.get('/posts', (req, res) => {
+
     const searchTerm = req.query.term;
     if (searchTerm) {
         const filtered = posts.filter((post) => {
             return (
-                post.title.includes(seachTerm) || post.content.includes(term)
+                post.title.includes(seachTerm) || post.contents.includes(term)
             );
         });
         res.status(STATUS_OK).json(filtered);
     } else {
         res.status(STATUS_OK).json(posts);
     }
-})
-
+});
 
 server.post('/posts', (req, res) => {
     const title = req.body.title;
-    const content = req.body.content;
+    const contents = req.body.contents;
 
-    if (!title || !content) {
+    if (!title || !contents) {
         res.status(STATUS_USER_ERROR);
-        res.json({ error: 'Please provide title and content.' });
+        res.json({ error: 'Please provide title and contents.' });
         return;
     }
 
-    const addPost = { id, title, content };
+    const addPost = { id, title, contents };
     id++;
     posts.push(addPost);
+    res.status(STATUS_OK);
+    res.json({ posts });
+    return;
+});
+
+server.put('/posts', (req, res) => {
+    const title = req.body.title;
+    const contents = req.body.contents;
+    const id = req.body.id;
+    console.log(title, contents, id);
+    if (!title || !contents || !id) {
+        res.status(STATUS_USER_ERROR);
+        res.json({ error: 'Please provide title, contents and ID' });
+        return;
+    }
+    let updateIndex = posts.findIndex((post) => post.id === id);
+    if (updateIndex < 0) {
+        res.status(STATUS_USER_ERROR);
+        res.json({ error: 'Please provide existing ID value' });
+        return;
+    }
+    let updated = { id, title, contents };
+    posts[updateIndex] = updated;
+    res.status(STATUS_OK);
+    res.json({ posts });
+    return;
+});
+
+server.delete('/posts', (req, res) => {
+    const id = req.body.id;
+
+    if (!id) {
+        res.status(STATUS_USER_ERROR);
+        res.JSON({ err: 'Please provide ID' });
+        return;
+    }
+    let updateIndex = posts.findIndex((post) => post.id === id);
+    console.log('Index Found at', updateIndex);
+    if (updateIndex < 0) {
+        res.status(STATUS_USER_ERROR)
+        res.json({ error: 'Please provide existing ID' });
+        return;
+    }
+    posts.splice(updateIndex, 1);
     res.status(STATUS_OK);
     res.json({ posts });
     return;
