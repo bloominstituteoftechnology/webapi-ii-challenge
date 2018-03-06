9b57f11be94d0ad44812411841ab72c96baa0459
rawalhome@gmail.com <rawalhome@gmail.com>
2018-03-06 12:45:38 -0600

    Dani Tacheny + Punit Rawal: GET /posts...done...the rest is WIP

--- src/server.js
+++ src/server.js
@@ -5,12 +5,37 @@ const STATUS_USER_ERROR = 422;
 
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
-const posts = [];
+const posts = [{title: "b",
+              contents: "b"},
+              {title: "ac",
+              contents: "b"},
+              {title: "b",
+              contents: "cd"},
+              {title: "e",
+              contents: "f"} ];
 
 const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
 
 // TODO: your code to handle requests
+server.get('/posts', (req, res) => {
+  const searchTerm = req.query.term;
+  if (searchTerm === undefined) {
+    res.send( { userPosts: posts } );
+  } else {
+    let newArr = [];
+    posts.forEach(obj => {
+      if (obj.title.includes(searchTerm) || obj.contents.includes(searchTerm)) {
+        newArr.push(obj);
+      }
+    });
+    if (newArr.length === 0) {
+      res.send( { userPosts: posts } );
+    } else {
+      res.send( { userPosts: newArr } );
+    }
+  }
+});
 
 module.exports = { posts, server };
