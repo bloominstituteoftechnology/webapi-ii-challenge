72efef50e149c9e90660797a25249eab83d5d32d
Ronnie Miksch <fakepreferences@gmail.com>
2018-03-06 10:23:09 -0800

    Completed get request with term searching regex

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 
 const STATUS_USER_ERROR = 422;
+const STATUS_SUCCESS = 200;
 
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
@@ -12,5 +13,21 @@ const server = express();
 server.use(bodyParser.json());
 
 // TODO: your code to handle requests
+server.get('/posts', (req, res) => {
+  if (req.query.term) {
+    let strictRegex = new RegExp(terms, 'i');
+
+    const searched = posts.filter(item => {
+      if (item.title.match(strictRegex) || item.contents.match(strictRegex)) {
+        return item;
+      }
+    });
+    res.status(STATUS_SUCCESS);
+    res.send(searched);
+  } else {
+    res.status(STATUS_SUCCESS);
+    res.send(posts);
+  }
+});
 
 module.exports = { posts, server };
