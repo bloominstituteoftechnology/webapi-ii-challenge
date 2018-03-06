3a7c6d81f1a73a78f9b70be38e03f58619380ffa
Cody Windeknecht <lhg614@mocs.utc.edu>
2018-03-06 16:37:22 -0500

    Delete started

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const STATUS_OKAY = 200;
 
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
-const posts = [
+let posts = [
   { id: 1, title: 'note1', contents: 'contents1' },
   { id: 2, title: 'note2', contents: 'contents2' },
   { id: 3, title: 'note3', contents: 'contents3' },
@@ -44,13 +44,25 @@ server.post('/posts', (req, res) => {
     res.json({ error: 'You Did Not Include a Title' });
   } else if (!req.body.contents) {
     res.status(STATUS_USER_ERROR);
-    res.json({ error: 'You Did Not Include a Title' });
+    res.json({ error: 'You Did Not Include a Content' });
   } else {
     const note = { id: nextID++, title: req.body.title, contents: req.body.contents };
     posts.push(note);
     res.status(STATUS_OKAY);
     res.json();
   }
+});
+
+server.delete('/posts', (req, res) => {
+  if (!req.body.id) {
+    res.status(STATUS_USER_ERROR);
+    res.json({ error: 'You Did Not Include an ID' });
+  }
+  const id = req.body.id;
+  const result = posts.filter(post => post[id] === req.body.id);
+  posts = result;
+  res.status(STATUS_OKAY);
+  res.json();
 });
 
 module.exports = { posts, server };
