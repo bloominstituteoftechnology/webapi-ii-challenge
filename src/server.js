4738402cc417004491df537fffae63bb6c61729d
Peter Gray <peter@petergraycreative.com>
2018-03-06 12:07:53 -0600

    Initial Commit
    Add in Succes Code and basic Get/Post/Put/Delete responses.

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 
 const STATUS_USER_ERROR = 422;
-
+const STATUS_USER_SUCCESS = 200;
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
 const posts = [];
@@ -12,5 +12,18 @@ const server = express();
 server.use(bodyParser.json());
 
 // TODO: your code to handle requests
+
+server.get('/posts', (req, res) => {
+
+});
+server.post('/posts', (req, res) => {
+
+});
+server.put('/', (req, res) => {
+
+});
+server.delete('/', (req, res) => {
+
+});
 
 module.exports = { posts, server };
