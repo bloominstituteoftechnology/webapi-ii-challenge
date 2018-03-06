e6c81e9c6a1472da0381c9fbb9e2fafa2dc57a41
Christopher Beards <beardscm@gmail.com>
2018-03-06 14:07:27 -0500

    Post is Functional

--- src/server.js
+++ src/server.js
@@ -38,5 +38,34 @@ server.get('/posts', (req, res) => {
   }
 });
 
+// - Ensure that the client provides both `title` and `contents` in the request
+//   body. If any of these don't exist, send an object of the form `{ error: "Error
+//   message" }` as a JSON response. Make sure to respond with an appropriate
+//   status code.
+
+// - If all fields are provided, create a new post object. Assign the post a
+//   unique, numeric `id` property that will act as its identifier, and add it to
+//   the posts array. Return the newly created post object, with its assigned `id`,
+//   to the client in a JSON response.
+
+server.post('/posts', (req, res) => {
+  const { title, contents } = req.body;
+  // console.log(req.body);
+  if (!title || !contents) {
+    res.status(STATUS_USER_ERROR); 
+    res.json({ error: "Missing Title or Contents" }); 
+  } else {
+
+    const post = {}
+    post.id = posts.length + 1;
+    post.title = title;
+    post.contents = contents;
+
+    posts.push(post);
+    res.json(post);
+  }
+
+});
+
 
 module.exports = { posts, server };
