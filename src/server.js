63227fcd63e9f352febd1eda3096aa741eff0ab8
Res Yeg <shaqk702@gmail.com>
2018-03-06 13:06:45 -0800

    get done

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const STATUS_USER_ERROR = 422;
 
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
-const posts = [];
+const posts = [{ title: "The post title", contents: "The post contents" }];
 // posts is an array of objects - { title: "The post title", contents: "The post contents" }
 
 const server = express();
@@ -17,8 +17,12 @@ server.get('/posts', (req, res) => {
 // If the client provides the query-string parameter term, filter the posts to those that have the term in their title or contents (or both), and send down those posts in a JSON response.
 // Otherwise, send down the full array of posts as a JSON response.
     const { term } = req.query;
-    if (term) {
-        res.json(term);
+    if (!term) {
+        res.json(posts);
+    } else {
+        for (let i=0; i<posts.length; i++) {
+            (Object.values(posts[i]).includes(term)) ? res.json(posts[i]): res.json(posts);
+        }
     }
 });
 
