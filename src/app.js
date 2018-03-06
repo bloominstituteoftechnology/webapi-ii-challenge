97d097d40f360de583186a7b75cd441434d9b23c
Cody Windeknecht <lhg614@mocs.utc.edu>
2018-03-06 15:53:17 -0500

    Post started
    Server.js --- Started work on server.post

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
 const { server } = require('./server.js');
 
-
 server.listen(3000, (err) => {
-    if (err) {
-        console.log(err);
-    } 
-    else {
+  if (err) {
+    console.log(err);
+  } else {
     console.log('server up and running...');
-    }});
+  }
+});
--- src/server.js
+++ src/server.js
@@ -2,14 +2,15 @@ const bodyParser = require('body-parser');
 const express = require('express');
 
 const STATUS_USER_ERROR = 422;
+const STATUS_OKAY = 200;
 
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
-let posts = [
-    { id:1 , title: 'note1', contents: 'contents1' },
-    { id:2 , title: 'note2', contents: 'contents2' },
-    { id:3 , title: 'note3', contents: 'contents3' },
-  ];
+const posts = [
+  { id: 1, title: 'note1', contents: 'contents1' },
+  { id: 2, title: 'note2', contents: 'contents2' },
+  { id: 3, title: 'note3', contents: 'contents3' },
+];
 
 const server = express();
 // to enable parsing of json bodies for post requests
@@ -18,20 +19,32 @@ server.use(bodyParser.json());
 // TODO: your code to handle requests
 
 server.get('/posts', (req, res) => {
-    if (req.query.term) {
-        let title = [];
-        console.log(req.query.term)
-        posts.forEach(((post, index) => {
-            if (posts[index].title.includes(req.query.term) || posts[index].contents.includes(req.query.term)) {
-                title.push(post);
-            };
-        }));
-        res.status(200);
-        res.json(title);
-    } else {
-        res.status(200);
-        res.json(posts)
-    }
+  if (req.query.term) {
+    const title = [];
+    posts.forEach((post, index) => {
+      if (
+        posts[index].title.includes(req.query.term) ||
+        posts[index].contents.includes(req.query.term)
+      ) {
+        title.push(post);
+      }
+    });
+    res.status(STATUS_OKAY);
+    res.json(title);
+  } else {
+    res.status(STATUS_OKAY);
+    res.json(posts);
+  }
+});
+
+server.post('/posts', (req, res) => {
+  if (!req.body.title) {
+    res.status(STATUS_USER_ERROR);
+    res.json({ error: 'You Did Not Include a Title' });
+  } else if (!req.body.contents) {
+    res.status(STATUS_USER_ERROR);
+    res.json({ error: 'You Did Not Include a Title' });
+  }
 });
 
 module.exports = { posts, server };
