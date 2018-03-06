6fa400537339c970922ddb9d2e5b9fa64576bb7d
Glenn-David Daniel <d2rd@outlook.com>
2018-03-06 11:27:57 -0800

    WIP server.js copied model code and setup HTTP command framework

--- src/app.js
+++ src/app.js
@@ -1,3 +1,15 @@
 const { server } = require('./server.js');
 
+/*
+THIS IS A SERVER SIDE APP SO CODE BELOW MAY GO IN server.js NOT HERE.
+    // const bodyParser = require('body-parser');
+    // const express = require('express');
+    // const fs = require('fs');
+
+    // const STATUS_USER_ERROR = 422;
+
+    // const server = express();
+    // // to enable parsing of json bodies for post requests
+    // server.use(bodyParser.json());
+*/
 server.listen(3000);
--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 
-const STATUS_USER_ERROR = 422;
+const STATUS_SUCCESS = 200;  //OK
+const STATUS_USER_ERROR = 422; //Unprocessable Entity
 
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
@@ -12,5 +13,26 @@ const server = express();
 server.use(bodyParser.json());
 
 // TODO: your code to handle requests
+server.post('/gues', (req, res) => {
+  if (!req.body.letter) {
+    res.status(STATUS_USER_ERROR);
+    res.json({error: "User must provide a letter"});
+  } else if (req.body.letter.length >1) {
+    res.status(STATUS_USER_ERROR);
+    res.json({error: "User must provide one letter only"});
+  } else if (typeof req.body.letter !== 'string') {
+    res.status(STATUS_USER_ERROR);
+    res.json({error: "Letter must be provided as a string"});
+  } else if (guessedLetters[req.body.letter]) {
+    res.status(STATUS_USER_ERROR);
+    res.json({error: 'The letter "${req.body.letter}" was already guessed'});
+  } else {
+    guessedLetters[req.body.letter] = true;
+    res.status(STATUS_SUCCESS);
+    res.send();
+  }
+});
+
+
 
 module.exports = { posts, server };
