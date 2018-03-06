af2fbbdaad97c2ca7e158f91aa985944985cb1a1
Res Yeg <shaqk702@gmail.com>
2018-03-06 12:54:01 -0800

    comments

--- src/server.js
+++ src/server.js
@@ -5,7 +5,8 @@ const STATUS_USER_ERROR = 422;
 
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
-const posts = [{title: "The post title",contents: "The post contents"}, {title: "Just some title",contents: "Just some contents"}];
+const posts = [];
+// posts is an array of objects - { title: "The post title", contents: "The post contents" }
 
 const server = express();
 // to enable parsing of json bodies for post requests
