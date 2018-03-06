3abcf5d524981a9f31a957fb89668a0835b2d829
Terrie Boeun Kim <TERR1E@Terries-MacBook-Pro.local>
2018-03-06 14:11:48 -0800

    status message update, minor fixes

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 
 const STATUS_USER_ERROR = 422;
+const STATUS_SUCCESS = 200;
 
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
@@ -14,13 +15,13 @@ server.use(bodyParser.json());
 // TODO: your code to handle requests
 
 let uniqueID = 0;
-let postArr = [{title: "test1", contents: "ahh blah"}, {title: "test5", contents: "blah blah"}, {title: "test10", contents: "test1 blah"}];
+// let postArr = [{title: "test1", contents: "ahh blah"}, {title: "test5", contents: "blah blah"}, {title: "test10", contents: "test1 blah"}];
 
 
 server.get('/posts', (req, res) => {
   const term = req.query.term
-  let indexArr = [];
-  let filteredPosts = postArr.filter((post) => {
+  // let indexArr = [];
+  const filteredPosts = posts.filter((post) => {
     if (post.title === term) {
       return true;
     } else {
@@ -36,10 +37,10 @@ server.get('/posts', (req, res) => {
   });
 
   if (filteredPosts.length === 0) {
-    res.status(200);
-    res.send(postArr);
+    res.status(STATUS_SUCCESS);
+    res.send(posts);
   } else {
-    res.status(200);
+    res.status(STATUS_SUCCESS);
     res.send(filteredPosts);
   }
 });
@@ -49,21 +50,20 @@ server.post('/posts', (req, res) => {
     title,
     contents
   } = req.body;
-  if (title !== undefined && contents !== undefined) {
+  if (title.length !== 0 && contents.length !== 0) {
     const newPostObj = {
-      title: title,
-      contents: contents,
-      id: uniqueID
+      title,
+      contents,
+      uniqueID
     };
-    postArr.push(newPostObj);
-    uniqueID++;
-    res.status(200);
+    posts.push(newPostObj);
+    ++uniqueID;
+    res.status(STATUS_SUCCESS);
     res.send('theyre defined');
   } else {
-    res.status(422);
+    res.status(STATUS_USER_ERROR);
     res.send('not defined');
   }
-
 });
 
 module.exports = { posts, server };
