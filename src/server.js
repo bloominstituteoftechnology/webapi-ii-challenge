bd2a3a452cce1b2b89fce727a758050325508353
algorithmsrock <algorithmsrock@gmail.com>
2018-03-06 13:05:13 -0800

    GET is working

--- src/server.js
+++ src/server.js
@@ -5,12 +5,39 @@ const STATUS_USER_ERROR = 422;
 
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
-const posts = [];
+const posts = [
+  {
+     title: "test",
+     content: "content",
+	}, 
+  
+  {
+     title: "test search",
+		 content: "content search",					
+  },
+];
 
 const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
 
 // TODO: your code to handle requests
+
+server.get("/posts/", (req, res) => {
+   res.status(200);
+	 res.send(posts);
+})
+
+server.get("/posts/search", (req, res) => {
+     let title = req.query.title;
+		 if (title) {
+		    filteredPosts = posts.filter((post) => {
+						return title === post.title;
+					})
+			}
+      res.status(200);
+			res.send(filteredPosts);
+		})		
+
 
 module.exports = { posts, server };
