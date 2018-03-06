c2dd2ee6ef084d60979872df47cd15ca55ed8d07
Nathan Daniel <natdf@icloud.com>
2018-03-06 15:19:50 -0600

    Commit 3 - added PUT

--- src/server.js
+++ src/server.js
@@ -7,19 +7,20 @@ const STATUS_USER_ERROR = 422;
 // to change this to a let binding if you need to reassign it.
 const posts = [
   {
+    id: 10,
     title: 'Title 1',
-    contents: 'dogs run'
+    contents: 'dogs run',
   },
   {
+    id: 11,
     title: 'Title 2',
     contents: 'cats meow',
-  }
+  },
 ];
 
 const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
-
 
 // TODO: your code to handle requests
 
@@ -27,7 +28,7 @@ server.get('/posts', (req, res) => {
   const term = req.query.term;
   const searchResults = [];
   if (term) {
-    posts.forEach((element) => {
+    posts.forEach(element => {
       if (element.title.includes(term) || element.content.includes(term)) {
         searchResults.push(element);
       } else {
@@ -55,6 +56,21 @@ server.post('/posts', (req, res) => {
     posts.push(newObj);
     res.json(posts);
   }
+});
+
+server.put('/posts', (req, res) => {
+  const title = req.body.title;
+  const contents = req.body.contents;
+  const id = req.body.id;
+  if (!title || !contents || !id) {
+    res.send({ error: 'Error message' });
+    res.status(422);
+  }
+  posts.forEach(element => {
+    if (element.id !== id) {
+      res.send({ error: 'Error message' });
+    }
+  });
 });
 
 module.exports = { posts, server };
