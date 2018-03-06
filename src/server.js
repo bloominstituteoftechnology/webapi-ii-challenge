a88f9cbcd883d6a5b2d82e1bc49810093c86e6c0
Daniel Lara <endtrippy@gmail.com>
2018-03-06 11:16:22 -0700

    Initial Commit

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 
+const STATUS_SUCCESS = 200;
 const STATUS_USER_ERROR = 422;
 
 // This array of posts persists in memory across requests. Feel free
@@ -8,9 +9,18 @@ const STATUS_USER_ERROR = 422;
 const posts = [];
 
 const server = express();
+server.use((req, res, next) => {
+  console.log(`You got a Request: ${req}`);
+  next();
+});
+
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
 
+server.get('/', (req, res) => {
+  res.status(STATUS_SUCCESS);
+  res.send({ Success: STATUS_SUCCESS });
+});
 // TODO: your code to handle requests
 
 module.exports = { posts, server };
