f75356ac40e41517f6a4d9b1074e975bdde42d0a
Michael Marshal <mikemarshalman@gmail.com>
2018-03-06 14:55:53 -0600

    put functional

--- src/server.js
+++ src/server.js
@@ -13,7 +13,9 @@ server.use(bodyParser.json());
 
 // TODO: your code to handle requests
 
+let uniqueID = 0;
 let postArr = [{title: "test1", contents: "blah blah"}, {title: "test5", contents: "blah blah"}, {title: "test10", contents: "test1 blah"}];
+
 
 server.get('/posts', (req, res) => {
   const term = req.query.term
@@ -26,6 +28,26 @@ server.get('/posts', (req, res) => {
     res.status(200);
     res.send(filteredPosts);
   }
+});
+
+server.post('/posts', (req, res) => {
+  const {
+    title,
+    contents
+  } = req.body;
+  if (title !== undefined && contents !== undefined) {
+    const newPostObj = {
+      title: title,
+      contents: contents
+    };
+    postArr.push(newPostObj);
+    res.status(200);
+    res.send('theyre defined');
+  } else {
+    res.status(422);
+    res.send('not defined');
+  }
+
 });
 
 module.exports = { posts, server };
