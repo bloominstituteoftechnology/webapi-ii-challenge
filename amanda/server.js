816e473c5bad3b3cc4d2a0d54ac8b2ebf2904870
Amanda Phillips <amanda@phillipsdev.org>
2018-03-06 16:53:01 -0600

    put working

--- amanda/server.js
+++ amanda/server.js
@@ -7,7 +7,7 @@ const STATUS_SUCCESS = 200;
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
 const posts = [];
-let postId = 0;
+let id = 0;
 
 const server = express();
 // to enable parsing of json bodies for post requests
@@ -32,8 +32,8 @@ server.post("/posts", (req, res) => {
   const { title, content } = req.body;
 
   if (title && content) {
-    postId++;
-    const post = { ...req.body, postId };
+    id++;
+    const post = { ...req.body, id };
 
     posts.push(post);
     res.status(STATUS_SUCCESS);
@@ -44,7 +44,25 @@ server.post("/posts", (req, res) => {
   }
 });
 
-server.put("/posts", (req, res) => {});
+server.put("/posts", (req, res) => {
+  const { id, title, content } = req.body;
+
+  if (title === undefined || content === undefined || id === undefined) {
+    res.status(STATUS_USER_ERROR);
+    res.json({ error: "You may have forgotten something." });
+  }
+  let post = posts.find(info => {
+    return info.id === id;
+  });
+  if (!post) {
+    res.status(STATUS_USER_ERROR);
+    res.json({ error: "No post with that id." });
+  }
+  post.title = title;
+  post.content = content;
+  res.status(STATUS_SUCCESS);
+  res.json(post);
+});
 
 server.delete("/posts", (req, res) => {});
 
