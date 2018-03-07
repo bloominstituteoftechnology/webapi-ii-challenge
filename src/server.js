c355bffe8d1defc6edcb80cbe374e90a3d30b997
Igor Yermak <igor.yermak@gmail.com>
2018-03-06 19:57:24 -0500

    POST request

--- src/server.js
+++ src/server.js
@@ -5,28 +5,49 @@ const STATUS_USER_ERROR = 422;
 const posts = [
   {
     title: "",
-  content: "",
-},
+    content: "",
+  },
 
 ];
 const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
 // TODO: your code to handle requests
-server.get("/posts", (req,res) => {
-if (req.query.term) {
-  let searchPosts = []; // initialize arr. for searched query
-   // if the post's title OR it's contents = query then push only those posts to the searchPosts array.
-  posts.forEach(post => {
-    if(post.title.includes(req.query) || post.contents.include(req.query)) 
-    searchPosts.push(post)
-  }) 
-  return res.send(searchPosts);
-//otherwise just send all posts
-} else res.send(posts);
+server.get("/posts", (req, res) => {
+  if (req.query.term) {
+    let searchPosts = []; // initialize arr. for searched query
+    // if the post's title OR it's contents = query then push only those posts to the searchPosts array.
+    posts.forEach(post => {
+      if (post.title.includes(req.query) || post.contents.include(req.query))
+        searchPosts.push(post)
+    })
+    return res.send(searchPosts);
+    //otherwise just send all posts
+  } else res.send(posts);
 });
 
-server.post 
+//POST request
+let counter = 1; // id counter
+let addPost = {}; // new post object arr.
+
+server.post('/posts', (req, res) => {
+  const { title, contents } = req.body; // both fields req.
+  if (!title && !contents) {
+    res.status(STATUS_USER_ERROR); //return err. if client does not provide both fields
+    res.json({ error: 'Error Message' });
+  } else { // otherwise increment ID, add a new post and return a OK status 
+    counter++;
+    const addPost = {
+      id: counter,
+      title: title,
+      contents: contents
+    };
+    posts.push(addPost);
+    res.status(200);
+    res.json(addPost);
+
+  }
+});
 
 
 
