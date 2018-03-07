660c738467253b79ebb9ba76bf95845abed18148
Glenn-David Daniel <d2rd@outlook.com>
2018-03-06 16:38:35 -0800

    Refactor with TA Wesley

--- src/server.js
+++ src/server.js
@@ -1,86 +1,86 @@
 const bodyParser = require('body-parser');
 const express = require('express');
-
 const STATUS_SUCCESS = 200;  //OK
 const STATUS_USER_ERROR = 422; //Unprocessable Entity
-
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
-// let posts = [];
-
+//const posts = [];
 const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
-
 // TODO: your code to handle requests
 let posts = [
   {
     id: 0,
     title: "First post title",
-    content: "First post content"
+    contents: "First post contents"
   },
   {
     id: 1,
     title: "Second post title",
-    content: "Second post content"
+    contents: "Second post contents"
   },
   {
     id: 2,
     title: "Third post title",
-    content: "Third post content"
+    contents: "Third post contents"
   }
 ]
-server.get('/posts/:term', (req, res) => {
+server.get("/users/:id/", (req, res) => {
   const {
+    id
+  } = req.params;
+  res.status(200);
+  res.send(posts[id])
+});
+server.get('/posts/:term/', (req, res) => {
+  const{
     term
-  } =req.params;
-  // console.log("Third",term);
-  //  '/posts/blue' in browser
-  let result=[];  // holds all hits.
-  if (term) { 
-    // some code here
+  } = req.params;
+console.log("TT",term)
+// /posts/blue
+  let result=[];
+  if (term) {
     for (let post in posts){
       if(post.title.toLowerCase().includes(term.toLowerCase())
-      || (post.content.toLowerCase().includes(term.toLowerCase())))
-      result.push(posts[post]);
+      || post.contents.toLowerCase().includes(term.toLowerCase())){
+        result.push(posts[post]);
+      }
+      
     }
+  
+    // some code here
   }  else {
     res.status(STATUS_SUCCESS);
-    res.send(posts); // sends everything if no match
+    res.send(posts);
   }
   if(result===[]){
     res.status(STATUS_SUCCESS);
-    res.send(posts); // sends everything if no match
+    res.send(posts);
+    
   }
 });
 /*
 server.post('/', (req, res) => {
   const {
-
   }
   if (!req.___._____) {
-
     // some code here
   }  else {
     res.status(STATUS_SUCCESS);
     res.send();
   }
-  if result=[]
 });
-
 server.put('/__', (req, res) => {
   if (!req.___._____) {
-
     // some code here
   }  else {
     res.status(STATUS_SUCCESS);
     res.send();
   }
 });
-
 server.delete('/__', (req, res) => {
   if (!req.___._____) {
-
     // some code here
   }  else {
     res.status(STATUS_SUCCESS);
