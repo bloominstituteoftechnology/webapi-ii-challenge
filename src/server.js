2765fa65832a24532764f69bab3ea4f17abf9d82
Arkoma <aaron@adburk.net>
2018-03-06 22:41:12 -0600

    add delete function

--- src/server.js
+++ src/server.js
@@ -4,42 +4,15 @@ const express = require('express');
 const STATUS_USER_ERROR = 422;
 const NOT_FOUND = 404;
 
-// This array of posts persists in memory across requests. Feel free
-// to change this to a let binding if you need to reassign it.
-const posts = [
-  // {
-  //   id: 1,
-  //   title: 'Going to the country',
-  //   contents: 'Going to eat me a lot of peaches'
-  // },
-  // {
-  //   id: 2,
-  //   title: '*Moving to the country',
-  //   contents: 'it\'s "moving" you idiot, not going'
-  // },
-  // {
-  //   id: 3,
-  //   title: 'I hate that song',
-  //   contents: 'It\'s so cheesy'
-  // },
-  /**
-   * {
-   * title: "The post title",
-   * contents: "The post contents"
-   * }
-   */
-];
+const posts = [];
 
 let idCounter = 0;
 
 const server = express();
-// to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
 
-// TODO: your code to handle requests
 server.get('/posts', (req, res) => {
   const { term } = req.query;
-  // console.log('term: ', term);
   if (term) {
     const searchArray = posts.filter((post) => {
       return (
@@ -47,7 +20,6 @@ server.get('/posts', (req, res) => {
         post.contents.toLowerCase().includes(term.toLowerCase())
       );
     });
-    // console.log(searchArray);
     if (searchArray.length < 1) {
       res.status(NOT_FOUND);
       res.json({ error: `${term} was not found` });
@@ -57,11 +29,6 @@ server.get('/posts', (req, res) => {
   } else {
     res.json(posts);
   }
-  /**
-   * - If the client provides the query-string parameter `term`, filter the posts to
-   those that have the `term` in their `title` or `contents` (or both), and
-   send down those posts in a JSON response.
-   */
 });
 
 server.post('/posts', (req, res) => {
@@ -80,32 +47,18 @@ server.post('/posts', (req, res) => {
     );
     res.json(posts);
   }
-  /**
-   * POST /posts
-   *
-   *  When the client makes a POST request to /posts
-   *
-   * - Ensure that the client provides both `title` and `contents` in the request
-   *   body. If any of these don't exist, send an object of the form `{ error- "Error
-   *   message" }` as a JSON response. Make sure to respond with an appropriate
-   *   status code
-   * - If all fields are provided, create a new post object. Assign the post a
-   *   unique, numeric `id` property that will act as its identifier, and add it to
-   *   the posts array. Return the newly created post object, with its assigned `id`,
-   *   to the client in a JSON response.
-   */
 }
 );
 
 server.put('/posts', (req, res) => {
   const { id, title, contents } = req.body;
-  const checkId = () => id === req.params.id;
-  // console.log(checkId());
-  const index = posts.findIndex(checkId);
-  // console.log(index);
-  // console.log(title);
-  // console.log(contents);
-  // console.log(id);
+  const index = () => {
+    for (let i = 0; i < posts.length; i++) {
+      if (posts[i].id === req.body.id) {
+        return i;
+      }
+    }
+  };
   if (!title || !contents || !id) {
     res.status(STATUS_USER_ERROR);
     res.json({ error: 'Posts must contain a title, an id, and contents' });
@@ -113,34 +66,30 @@ server.put('/posts', (req, res) => {
     res.status(STATUS_USER_ERROR);
     res.json({ error: 'You must update an existing post' });
   } else {
-    posts.splice(index, 1, { id, title, contents });
+    posts.splice(index(), 1, { id, title, contents });
     res.json(posts);
   }
 });
-/**
- * ### `PUT /posts`
- *  When the client makes a `PUT` request to `/posts`:
- *
- *  - Ensure that the client provides `id`, `title`, and `contents` in the request
- *    body. If any of these don't exist, send an object of the form `{ error: "Error
- *    message" }` as a JSON response. Make sure to respond with an appropriate
- *    status code.
- *
- *  - If the `id` doesn't correspond to a valid post, respond with an error in the
- *    same form as above.
- *
- *  - Modify the post with the given `id`, updating its `title` and `contents`.
- *    Respond with the newly updated post object in a JSON response.
- *
- */
 
 server.delete('/posts', (req, res) => {
   const { id, title, contents } = req.body;
-  const checkId = () => id === req.params.id;
-  const index = posts.findIndex(checkId);
-  console.log('index: ', index, 'id: ', id, 'req.params.id: ', req.params.id);
-  posts.splice(index, 1);
-  res.json(posts);
+  const index = () => {
+    for (let i = 0; i < posts.length; i++) {
+      if (posts[i].id === req.body.id) {
+        return i;
+      }
+    }
+  };
+  if (!id) {
+    res.status(STATUS_USER_ERROR)
+    res.json({ error: 'User must provide matching id to delete post' });
+  } else if ( id !== req.body.id) {
+    res.status(STATUS_USER_ERROR);
+    res.json({ error: 'You must delete an existing post' });
+  } else {
+    posts.splice(index(), 1);
+    res.json(posts);
+  }
 });
 
- module.exports = { posts, server };
+module.exports = { posts, server };
