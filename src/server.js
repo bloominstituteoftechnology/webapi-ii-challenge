0b08855c5f8edf05b0b2becfb2fb8d00b90e27f6
Charlie Sparks <csparks@antioch.edu>
2018-03-08 14:43:57 -0800

    ID functionality completely working. May come back and try to generate IDs that match the index of the object in the array if time allows

--- src/server.js
+++ src/server.js
@@ -4,11 +4,12 @@ const bodyParser = require('body-parser');
 const express = require('express');
 
 const STATUS_USER_ERROR = 422;
-const USER_ERROR_OBJECT = { error: "422: Unprocessable Entity"}
+const USER_ERROR_OBJECT = { error: "422: Unprocessable Entity" }
 
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
-const posts = [{ title: 'First post', content: "I like turtles" }, { title: 'Doge 1', content: "MacGruber" }];
+const posts = [{ id: 0, title: 'First post', content: "I like turtles" }, { id: 1, title: 'Doge 1', content: "MacGruber" }];
+let postCounter = 2;
 
 const server = express();
 // to enable parsing of json bodies for post requests
@@ -16,47 +17,50 @@ server.use(bodyParser.json());
 
 // TODO: your code to handle requests
 server.get("/", (req, res) => {
-    res.send("testeroo!");
+  res.send("testeroo!");
 })
 
 server.get("/posts", (req, res) => {
-    let term = req.query.term.toLowerCase();
+  let term = req.query.term;
 
-    if (!term) {
-        res.json(posts);
-        res.end();
-    }
+  if (!term) {
+    res.json(posts);
+    res.end();
+  }
 
-    posts.forEach(post => {
-        let title = post.title.toLowerCase().split(" ");
-        let content = post.content.toLowerCase().split(" ");
-        if (title.indexOf(term) > -1 || content.indexOf(term) > -1){
-            console.log("Result found!")
-            res.json(post);
-            res.end();
-        }; 
-    })
+  term = term.toLowerCase();
 
-    console.log("No result found");
-    res.status(STATUS_USER_ERROR);
-    res.send("No matching term was found!");
+  posts.forEach(post => {
+    let title = post.title.toLowerCase().split(" ");
+    let content = post.content.toLowerCase().split(" ");
+    if (title.indexOf(term) > -1 || content.indexOf(term) > -1) {
+      console.log("Result found!")
+      res.json(post);
+      res.end();
+    };
+  })
+
+  console.log("No result found");
+  res.status(STATUS_USER_ERROR);
+  res.send("No matching term was found!");
 })
 
 server.post("/posts", (req, res) => {
-    if (!req.body.title || !req.body.content){
-        res.status(STATUS_USER_ERROR);
-        res.json(USER_ERROR_OBJECT);
-        res.end();
-    }
+  if (!req.body.title || !req.body.content) {
+    res.status(STATUS_USER_ERROR);
+    res.json(USER_ERROR_OBJECT);
+    res.end();
+  }
 
-    //generate ID
-    const newPost = {
-        title: req.body.title,
-        content: req.body.content
-    }
+  const newPost = {
+    id: postCounter,
+    title: req.body.title,
+    content: req.body.content
+  }
 
-    //send ID
-    res.json(newPost);
+  posts.push(newPost);
+  postCounter++;
+  res.json(newPost);
 })
 
 
