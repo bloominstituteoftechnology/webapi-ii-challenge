d4b7009738668b9efd2f40376a68da52aade9fd2
Charlie Sparks <csparks@antioch.edu>
2018-03-08 12:00:28 -0800

    first get handler not working, submitting for help

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+/* eslint-disable */
+
 const { server } = require('./server.js');
 
 server.listen(3000);
+console.log("The server is listening on port 3000");
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 const bodyParser = require('body-parser');
 const express = require('express');
 
@@ -5,13 +7,37 @@ const STATUS_USER_ERROR = 422;
 
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
-const posts = [];
+const posts = [{ title: 'First post!' }, { doge: "MacGruber!" }];
 
 const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
 
 // TODO: your code to handle requests
+server.get("/", (req, res) => {
+    res.send("testeroo!");
+})
+
+server.get("/posts", (req, res) => {
+    const { term } = req.query.term;
+    if (!term){
+        res.json(posts);
+        console.log("No term inputted");
+    } else {
+       return posts.forEach(post => {
+            let keys = Object.keys(post);
+            let keySplit = keys[0].split(" ");
+            let values = Object.values(post);
+            let valSplit = values[0].split(" ");
+            if (keySplit.indexOf(term) > -1 || valSplit.indexOf(term) > -1){
+                res.json(post);
+            } else {
+                res.status(STATUS_USER_ERROR);
+                res.send("No such term found");
+            }
+        })
+    }
+})
+
 
 module.exports = { posts, server };
-// test
