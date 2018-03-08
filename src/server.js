b3e6104d70e51fc0a6dfe438ec895f0499e584fd
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-03-07 19:43:34 -0500

    Fixed missing ','; removed '/search' from GET search path

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-const bodyParser = require('body-parser');
-const express = require('express');
+const bodyParser = require("body-parser");
+const express = require("express");
 /* eslint-disable */
 
 const STATUS_USER_ERROR = 422;
@@ -7,9 +7,9 @@ const STATUS_USER_ERROR = 422;
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
 const posts = [
-	{ title: 'first title', contents: 'contents' },
-	{ title: 'second', contents: 'contents' },
-	{ title: 'third title', contents: 'contents' },
+  { title: "first title", contents: "contents" },
+  { title: "second", contents: "contents" },
+  { title: "third title", contents: "contents" }
 ];
 
 let idCounter = 2;
@@ -19,94 +19,93 @@ const server = express();
 server.use(bodyParser.json());
 
 // TODO: your code to handle requests
-server.get('/posts', (req, res) => {
-  res.status(200);
-  res.json(posts);
-});
+// server.get("/posts", (req, res) => {
+//   res.status(200);
+//   res.json(posts);
+// });
 
-server.get('/posts/search', (req, res) => {
-	const { term } = req.query;
-	console.log(term);
+server.get("/posts", (req, res) => {
+  const { term } = req.query;
   if (term) {
-    const filteredPosts = posts.filter((post) => {
+    const filteredPosts = posts.filter(post => {
       return post.title.includes(term) || post.contents.includes(term);
     });
-		res.status(200);
-		res.send(filteredPosts);
+    res.status(200);
+    res.send(filteredPosts);
   } else {
     res.status(200);
     res.json(posts);
   }
 });
 
-server.post('/posts', (req, res) => {
-	if (req.body.title && !req.body.contents) {
+server.post("/posts", (req, res) => {
+  if (req.body.title && !req.body.contents) {
     res.status(STATUS_USER_ERROR);
-    res.json({ error: 'You did not provide a title' })
+    res.json({ error: "You did not provide a title" });
   }
   if (!req.body.title && req.body.contents) {
     res.status(STATUS_USER_ERROR);
-    res.json({ error: 'You did not provide contents' })
+    res.json({ error: "You did not provide contents" });
   }
   if (!req.body.title && !req.body.contents) {
     res.status(STATUS_USER_ERROR);
-    res.json({ error: 'You did not provide both title and contents' });
+    res.json({ error: "You did not provide both title and contents" });
   }
   let { title, contents } = req.body;
   idCounter++;
   let newPost = {
     title: title,
     contents: contents,
-    id: idCounter,
-	}
-	posts.push(newPost);
-	res.status(200);
-	res.json(newPost);
-})
+    id: idCounter
+  };
+  posts.push(newPost);
+  res.status(200);
+  res.json(newPost);
+});
 
-server.put('/posts', (req, res) => {
-	let { title, contents, id } = req.body;
-	let updatedPost = {
+server.put("/posts", (req, res) => {
+  let { title, contents, id } = req.body;
+  let updatedPost = {
     title: title,
     contents: contents,
     id: id
-	}
-	let postsId = posts.map(post => post.id);
-	
-	if (!title || !contents || !id) {
+  };
+  let postsId = posts.map(post => post.id);
+
+  if (!title || !contents || !id) {
     res.status(STATUS_USER_ERROR);
-    res.json({ error: 'You did not include an ID, Title and Contents' });
-	}
-	if (!postsId.includes(id)) {
+    res.json({ error: "You did not include an ID, Title and Contents" });
+  }
+  if (!postsId.includes(id)) {
     res.status(STATUS_USER_ERROR);
-    res.json({ error: 'Invalid ID' });
-	}
-	if (title && contents && id) {
-    posts = posts.filter((post) => {
-			return post.id !== id;
-		})
+    res.json({ error: "Invalid ID" });
+  }
+  if (title && contents && id) {
+    posts = posts.filter(post => {
+      return post.id !== id;
+    });
     posts.push(updatedPost);
     res.status(200);
     res.json(updatedPost);
-	}
-})
+  }
+});
 
-server.delete('/posts', (req, res) => {
-	let { id } = req.body;
-	let postsId = posts.map(post => post.id);
-	if (id && postsId.includes(id)) {
+server.delete("/posts", (req, res) => {
+  let { id } = req.body;
+  let postsId = posts.map(post => post.id);
+  if (id && postsId.includes(id)) {
     posts = posts.filter(post => !(post.id === id));
     res.status(200);
     res.json({ success: true });
-	}
-	if (id && !(postsId.includes(id))) {
+  }
+  if (id && !postsId.includes(id)) {
     res.status(STATUS_USER_ERROR);
-    res.json({ error: 'No post with that ID exists!' });
-	}
-	if (!id || id === null) {
+    res.json({ error: "No post with that ID exists!" });
+  }
+  if (!id || id === null) {
     res.status(STATUS_USER_ERROR);
-    res.json({ error: 'You did not provide an id!' });
-	}
-})
+    res.json({ error: "You did not provide an id!" });
+  }
+});
 
 module.exports = { posts, server };
