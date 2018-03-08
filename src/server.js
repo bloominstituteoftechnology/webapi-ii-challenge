7a3b452a01ebd8f479e99c0eab6534065f0ddcff
Charlie Sparks <csparks@antioch.edu>
2018-03-08 12:14:09 -0800

    added whitespace for readability

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const STATUS_USER_ERROR = 422;
 
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
-const posts = [{ title: 'First post!' }, { doge: "MacGruber!" }];
+const posts = [{ title: 'First post!' }, { doge: 'MacGruber' }];
 
 const server = express();
 // to enable parsing of json bodies for post requests
@@ -19,19 +19,27 @@ server.get("/", (req, res) => {
 })
 
 server.get("/posts", (req, res) => {
-    const { term } = req.query.term;
+    console.log(req.query.term);
+    const { term } = req.query;
+
     if (!term){
         res.json(posts);
         console.log("No term inputted");
-    } else {
-       return posts.forEach(post => {
+
+    } else if (term) {
+        console.log("The term was inputted")
+
+        posts.forEach(post => {
             let keys = Object.keys(post);
             let keySplit = keys[0].split(" ");
             let values = Object.values(post);
             let valSplit = values[0].split(" ");
+
             if (keySplit.indexOf(term) > -1 || valSplit.indexOf(term) > -1){
                 res.json(post);
+
             } else {
+                console.log("Term not found");
                 res.status(STATUS_USER_ERROR);
                 res.send("No such term found");
             }
