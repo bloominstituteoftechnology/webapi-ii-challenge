44438fe02ef060ae7b37870cd0de82b987675149
arcadia-ego <arcadia-ego@github.com>
2018-04-02 13:46:22 -0700

    GET working

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 // import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
-
+const server = express();
 // add your server code starting here
+
+server.get('/', function(req, res) {
+    db
+    .find()
+    .then(users => {
+        res.json(users) 
+    })
+    .catch(error =>{
+        res.status(500).json({error: "The posts information could not be retrieved."})
+    })
+})
+
+const port = 5000;
+server.listen(port, () => console.log('Api running on port 5000'));
