d6860220953187b69d1cef9755ab141c1de7c87f
nourmhassan <nour-pop1@hotmail.com>
2018-04-02 13:50:04 -0700

    nour hassan server up and running

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
 // import your node modules
+const express = require('express');
+
 
 const db = require('./data/db.js');
 
+const server = express();
+
 // add your server code starting here
+server.get('/', (req, res)=>{
+    res.send({ api: 'Running...' })
+})
+
+server.get('/posts', (req, res)=> {
+    db
+        .find()
+        .then(users => {
+            res.json(users);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        })
+    })
+
+const port = 5000;
+server.listen(port, () => console.log('API Running on port 5000'))
