623a2195ac77a9ab5e1e80c920a80ea852db901e
DragonRydrz <DragonRydrz@me.com>
2018-04-02 18:45:10 -0400

    initial commit.  get all done, working on post

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 // import your node modules
+const express = require('express');
+const server = express();
+server.use(express.json());
+const port = 5000;
 
 const db = require('./data/db.js');
 
 // add your server code starting here
+
+server.get('/api/posts', (req, res) => {
+  db
+    .find()
+    .then(posts => res.json(posts))
+    .catch(error => res.status(500).json(error));
+});
+
+server.listen(port, () => {
+  console.log('Hit me with your best post!');
+});
