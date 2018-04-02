7964fa9e8438b3259e746c17dbfa3c67adb12af6
Kevin Jolley <30558808+Serrowxd@users.noreply.github.com>
2018-04-02 17:55:59 -0500

    Updates

--- server.js
+++ server.js
@@ -41,7 +41,10 @@ server.get('/api/posts/:id', (req, res) => { // GET ID Endpoint
       res.json(posts[0]);
     })
     .catch(error => {
-      res.status(500).json({ message: "The post with the specified ID does not exist." });
+      res.status(500).json({ error: "The post information could not be retrieved." });
+    })
+    .catch(error => {
+      res.status(404).json({ message: "The post with the specified ID does not exist." })
     });
 });
 
