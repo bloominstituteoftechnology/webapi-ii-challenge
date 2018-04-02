a372bf6f81ca2216077cb1aa4f271d58a883ceee
Omoniyi Alimi <omoniyialimi@Omoniyis-MacBook-Pro.local>
2018-04-02 18:45:35 -0400

    on GET

--- server.js
+++ server.js
@@ -1,5 +1,37 @@
-// import your node modules
+const express = require('express');
 
 const db = require('./data/db.js');
 
-// add your server code starting here
+const server = express();
+
+server.get('/', function(req, res){
+    res.send({api:'Running....'});
+});
+
+
+
+// server.get('/', function(req,res) => {
+//     db
+//     .find()
+//     .then(users => {
+//         res.json(users);
+//     })
+//     .catch(error => {
+//         res.status(500).json(error);
+//     });
+// });
+
+// server.get('/api/users/:id', (req,res) => {
+//     const { id } = req.params;
+//     db
+//     .findById(id)
+//     .then(users => {
+//         res.json(users[0]);
+//     })
+//     .cath(error => {
+//         res.status(500).json(error)
+//     });
+// });
+
+const port = 5000;
+server.listen(port, () => console.log('API Running on port 5000'));
