ac2bc88129271b8e619b3200143945426bb30b8d
Kevin Jolley <30558808+Serrowxd@users.noreply.github.com>
2018-04-02 15:46:55 -0500

    GET ID Endpoint

--- server.js
+++ server.js
@@ -10,17 +10,35 @@ server.get('/', (req, res) => {
   res.json({ api: 'Running...' });
 });
 
-server.post('/api/posts', (req, res) => { // POST Endpoint
-  db
-    .insert(posts)
-
-});
+// server.post('/api/posts', (req, res) => { // POST Endpoint
+//   db
+//     .insert(posts)
+//     .then(posts => {
+//       req.json(posts);
+//     })
+//     .catch(error => {
+//       res.status(500).json(error);
+//     });
+// });
 
 server.get('/api/posts', (req, res) => { // GET Endpoint
   db
     .find()
     .then(posts => {
       res.json(posts);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+server.get('/api/posts/:id', (req, res) => { // GET ID Endpoint
+  const { id } = req.params;
+
+  db
+    .findById(id)
+    .then(posts => {
+      res.json(posts[0]);
     })
     .catch(error => {
       res.status(500).json(error);
