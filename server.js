8f11412d8e19f17dbfa74220c8fe60917408c260
YFN Lucci <anderson56ak@gmail.com>
2018-04-02 15:43:45 -0500

    A.AndersonW.I.P.NEL

--- server.js
+++ server.js
@@ -1,5 +1,43 @@
-// import your node modules
+const express = require("express");
 
-const db = require('./data/db.js');
+const db = require("./data/db.js");
 
-// add your server code starting here
+const server = express();
+
+server.post("/api/posts", (req, res) => {
+  db
+    .insert(posts)
+    .then(posts => {
+      res.json(posts);
+    })
+    .catch(error => {
+      res.status(400).json(error);
+    });
+});
+
+server.get("/api/posts", (req, res) => {
+  db
+    .find()
+    .then(posts => {
+      res.json(posts);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+server.get("/api/posts/:id", (req, res) => {
+  const { id } = req.params;
+
+  db
+    .findById(id)
+    .then(posts => {
+      res.json(posts[0]);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+const port = 3000;
+server.listen(port, () => console.log("API Running on port 3000"));
