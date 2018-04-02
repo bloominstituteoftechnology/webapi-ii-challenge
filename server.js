ff8f599c1fb7bc5f2adab6e5a9f1b8a6c5db302a
Shaun Kelly <Shaun@redskelly.local>
2018-04-02 18:47:32 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,5 +1,58 @@
-// import your node modules
+const express = require('express');
 
 const db = require('./data/db.js');
 
-// add your server code starting here
+const server = express();
+
+// POST requests:
+
+server.post('/api/posts', (req, res) => {
+  const body = req.body ? req.body : {};
+
+  const { title, contents } = body;
+
+  if (!title || !contents) {
+    res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    return;
+  }
+
+  const newPost = {
+    title,
+    contents
+  }
+
+  db
+    .find().then().catch();
+})
+
+// GET requests:
+
+server.get('/api/posts', (req, res) => {
+  db
+    .find()
+    .then(posts => {
+      res.json(posts);
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'The posts information could not be retrieved.' });
+    });
+})
+
+server.get('/api/posts/:id', (req, res) => {
+  const { id } = req.params;
+  db
+    .findById(id)
+    .then(posts => {
+      res.json(posts[0]);
+    })
+    .catch(error => {
+      res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+    });
+})
+
+
+
+const port = 5000;
+server.listen(port, () => {
+  console.log('API Running on port 5000');
+})
