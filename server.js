e40921e144bf47e7344b20393f8fe77c433c41ab
Carlos Andrade <carlos.andrade@outlook.com>
2018-04-02 19:04:15 -0400

    completed No stretch goal

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const db = require('./data/db.js');
 
 const server = express();
 
+const bodyParser = require('body-parser'); server.use(bodyParser.json()); 
+
 // add your server code starting here
 server.get('/', (req, res)=>{
     res.send({ api: 'Running...' })
@@ -13,15 +15,15 @@ server.get('/', (req, res)=>{
 server.get('/api/posts', (req, res)=> {
     db
         .find()
-        .then(users => {
-            res.json(users);
+        .then(posts => {
+            res.json(posts);
         })
         .catch(error => {
             res.status(500).json(error);
         })
     })
 
-    server.get('/api/posts/:id', (req, res)=>{
+    server.get('/api/posts/:id', (req, res)=> {
         const{ id } = req.params;
         db
             .findById(id)
@@ -32,7 +34,43 @@ server.get('/api/posts', (req, res)=> {
                 res.status(500).json(error);
             })
     })
+
+    server.post('/api/posts', (req, res) => {
+              const  post = req.body;
+        db
+            .insert(post)
+            .then(post => {
+                res.json(posts)
+            })
+            .catch(error => {
+                res.status(500).json(error);
+            })
+    })
+
+    server.delete('/api/posts/:id', (req, res)=> {
+        const { id }= req.params;
+        db
+            .remove(id)
+            .then(posts=> {
+                res.json(posts)
+            })
+            .catch(error => {
+                res.status(500).json(error);
+            })
+    })
     
+    server.put('/api/posts/:id', (req, res) => {
+        const { id } = req.params;
+        const post = req.body;
+        db.update(id, post)
+          .then(posts => {
+            res.json(posts);
+          })
+          .catch(error => {
+            res.status(500).json(error);
+          });
+      });
+
 
 const port = 5000;
 server.listen(port, () => console.log('API Running on port 5000'))
