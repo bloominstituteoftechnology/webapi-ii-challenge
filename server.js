f65923f854f081ba33696474ff5673cde3f3516a
arcadia-ego <arcadia-ego@github.com>
2018-04-02 14:39:28 -0700

    POST working

--- server.js
+++ server.js
@@ -1,19 +1,53 @@
 // import your node modules
-const express = require('express');
-const db = require('./data/db.js');
+const express = require("express");
+const db = require("./data/db.js");
 const server = express();
+const bodyParser = require("body-parser");
 // add your server code starting here
 
-server.get('/', function(req, res) {
-    db
+server.use(bodyParser.json());
+
+server.get("/api/posts", (req, res) => {
+  db
     .find()
     .then(users => {
-        res.json(users) 
+      res.json(users);
     })
-    .catch(error =>{
-        res.status(500).json({error: "The posts information could not be retrieved."})
+    .catch(error => {
+      res
+        .status(500)
+        .json({ error: "The posts information could not be retrieved." });
+    });
+});
+
+server.get("/api/posts/:id", (req, res) => {
+  const { id } = req.params;
+  db
+    .findById(id)
+    .then(users => {
+      res.json(users[0]);
     })
-})
+    .catch(error => {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
+    });
+});
+
+server.post("/api/post", (req, res) => {
+    const { title, contents } = req.body;
+    const post = {title: title, contents: contents}
+  db
+    .insert(post)
+    .then(res.redirect("/api/posts"))
+    .catch(error => {
+      res
+        .status(400)
+        .json({
+          errorMessage: "Please provide title and contents for the post."
+        });
+    });
+});
 
 const port = 5000;
-server.listen(port, () => console.log('Api running on port 5000'));
+server.listen(port, () => console.log("Api running on port 5000"));
