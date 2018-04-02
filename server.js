8a26900f690e36d1dc98199394821f055ba3c338
KJ Sourcing <tmkogwork@gmail.com>
2018-04-02 15:46:52 -0700

    create through delete, working?

--- server.js
+++ server.js
@@ -1,5 +1,69 @@
-// import your node modules
+const express = require('express');
+
+const server = express();
 
 const db = require('./data/db.js');
 
-// add your server code starting here
+
+server.get('/', (req, res) => {
+    res.send({ api: 'Running...' })
+});
+
+
+server.get('/api/posts', (req, res) => {
+
+    db
+    .find()
+    .then(posts => {
+        res.json(posts);
+    })
+    .catch(error => {
+        res.status(500).json(error);
+    });
+});
+
+
+server.get('/api/posts/:id', (req, res) => {
+    const{ id } = req.params;
+
+    db
+    .findById(id)
+    .then(posts => {
+        res.json(posts[0]);
+    })
+    .catch(error => {
+        res.status(500).json(error);
+    });
+});
+
+
+server.delete('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+
+    db
+    .remove(id)
+    .then(posts => {
+        res.json(posts[0]);
+    })
+    .catch(error => {
+        res.status(500).json(error);
+    });
+});
+
+
+server.put('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+
+    db
+    .update(id)
+    .then(posts => {
+        res.json(posts[0]);
+    })
+    .catch(error => {
+        res.status(500).json(error);
+    });
+});
+
+
+const port = 5000;
+server.listen(port, () => console.log('API Running on port 5000'));
