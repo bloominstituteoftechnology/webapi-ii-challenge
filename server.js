82b78f97d57903bd8a406f5e673ef19d07936892
Johnathan Huggett <johnathanhuggett@Johnathans-MacBook-Pro.local>
2018-04-02 18:46:30 -0400

    Johnathan Huggett, node 1

--- server.js
+++ server.js
@@ -1,5 +1,48 @@
 // import your node modules
-
+const express = require('express');
+const bodyParser = require('body-parser')
 const db = require('./data/db.js');
 
+const server = express();
+server.use(bodyParser.json());
+
 // add your server code starting here
+
+server.get(`/api/posts`, (req, res) => {
+    db
+        .find()
+        .then(posts => {
+            res.json(posts)
+        })
+        .catch(err => {
+            res.status(500).json({ error: "The posts information could not be retrieved."});
+        });
+});
+
+server.post(`/api/posts`, (req, res) => {
+    const Body = req.body ? req.body : {}
+    const { title, contents } = Body;
+    // console.log(req.body);
+    if (!title || !contents) {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+        return;
+    }
+    const newPost = {
+        title,
+        contents,
+    }
+
+    db
+        .insert(newPost)
+        .then(id => {
+            const post = { ...newPost, id }
+            res.status(201).json(post)
+        })
+        .catch(err => {
+            res.status(500).json({ error: "The posts information could not be retrieved."});
+        });
+});
+
+const port = 5000;
+
+server.listen(port, () => console.log('API Running on port 5000'))
