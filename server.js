744a3cf0f5a971159d5ae0f74f517fb77086f0e6
Cristina Laryea <crispad06@gmail.com>
2018-04-02 16:51:58 -0400

    added express, displaying posts on browser

--- server.js
+++ server.js
@@ -1,5 +1,37 @@
-// import your node modules
+const express = require('express');
 
 const db = require('./data/db.js');
 
-// add your server code starting here
+const server = express();
+
+server.get('/', function(req, res) {
+    res.json({api: 'Running...'});
+});
+
+
+server.get('/api/posts', (req, res) => {
+    db
+        .find()
+        .then(posts => {
+            res.json(posts);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
+server.get('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    
+    db
+        .findById(id)
+        .then(posts => {
+            res.json(posts[0]);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
+const port = 5000;
+server.listen(port, () => console.log('API Running on port 5000'));
