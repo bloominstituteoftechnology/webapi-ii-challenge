1a87599fd36a09e0480deb98d3a2b12e233d1089
kait-schorr <kaitlynschorr@gmail.com>
2018-04-02 17:33:50 -0500

    Fixed error handling

--- server.js
+++ server.js
@@ -25,20 +25,16 @@ server.get("/api/posts/:id", (req, res) => {
   db
     .findById(req.params.id)
     .then(posts => {
-      if (post === []) {
-        res.status(404);
+      if (posts.length < 1) {
+        res.status(404).json({ error: "That post doesn't exist." });
       } else {
-        res.json(posts[0]);
+        res.status(200).json(posts);
       }
     })
     .catch(error => {
-      if (res.status === 404)
-        res.json({ message: "The post with the specified ID does not exist." });
-      else {
-        res
-          .status(500)
-          .json({ message: "The post information could not be retrieved." });
-      }
+      res
+        .status(500)
+        .json({ message: "The post information could not be retrieved." });
     });
 });
 
