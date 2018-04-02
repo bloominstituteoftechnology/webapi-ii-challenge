acf99efea285aafbe6ae62c047b8aa2b8fd7dfa3
kait-schorr <kaitlynschorr@gmail.com>
2018-04-02 16:21:07 -0500

    Finished delete and put

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 
 const server = express();
 server.use(bodyParser.json());
-server.use(bodyParser.urlencoded({ extended: false }));
+// server.use(bodyParser.urlencoded({ extended: false }));
 
 const db = require("./data/db.js");
 
@@ -15,7 +15,9 @@ server.get("/api/posts", (req, res) => {
       res.json(posts);
     })
     .catch(error => {
-      res.status(500).json(error);
+      res.status(500).json({
+        errorMessage: "The posts' information could not be retrieved."
+      });
     });
 });
 
@@ -23,10 +25,20 @@ server.get("/api/posts/:id", (req, res) => {
   db
     .findById(req.params.id)
     .then(posts => {
-      res.json(posts[0]);
+      if (post === []) {
+        res.status(404);
+      } else {
+        res.json(posts[0]);
+      }
     })
     .catch(error => {
-      res.status(500).json(error);
+      if (res.status === 404)
+        res.json({ message: "The post with the specified ID does not exist." });
+      else {
+        res
+          .status(500)
+          .json({ message: "The post information could not be retrieved." });
+      }
     });
 });
 
@@ -47,6 +59,37 @@ server.post("/api/posts/", (req, res) => {
           "There was an error while saving the post to the database."
       });
     });
+});
+
+server.delete("/api/posts/:id", (req, res) => {
+  db
+    .remove(req.params.id)
+    .then(posts => {
+      res.json(posts);
+    })
+    .catch(error => {
+      res.status(500).json({
+        errorMessage: "The post could not be removed."
+      });
+    });
+});
+
+server.put("/api/posts/:id", (req, res) => {
+  if (!req.body) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    });
+  }
+  db
+    .update(req.params.id, req.body)
+    .then(posts => {
+      res.status(200).json(req.body);
+    })
+    .catch(() =>
+      res
+        .status(500)
+        .json({ error: "The post information could not be modified." })
+    );
 });
 
 // add your server code starting here
