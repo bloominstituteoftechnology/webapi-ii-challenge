9e08592a4b2fd0daf026dc6e7af80445b01fd354
Cristina Laryea <crispad06@gmail.com>
2018-04-02 18:47:09 -0400

    post done

--- server.js
+++ server.js
@@ -1,37 +1,69 @@
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 const db = require('./data/db.js');
 
 const server = express();
 
-server.get('/', function(req, res) {
-    res.json({api: 'Running...'});
-});
+server.use(bodyParser.json());
 
 
-server.get('/api/posts', (req, res) => {
+server.post('/api/posts', (req, res) => {
+    const body = req.body ? req.body : {}
+   
+    const { title, contents } = body
+
+    if(!title || !contents) {
+        res.status(400).json({errorMessage: "Please provide title and contents for the post."})
+        return;
+    }
+
+    const newPost = {
+        title,
+        contents,
+    }
+
     db
-        .find()
-        .then(posts => {
-            res.json(posts);
+        .insert(newPost)
+        .then(id => {
+            const post = {...newPost, id}
+            res.status(201).json(post);
         })
         .catch(error => {
-            res.status(500).json(error);
+            res.status(500).json({error: "The post information could not be modified."});
         });
+    //res.json({ a: 1});
 });
+
+// server.get('/api/posts', (req, res) => {
+//     db
+//         .find()
+//         .then(posts => {
+//             res.json(posts);
+//         })
+//         .catch(error => {
+//             res.status(500).json({error: "The posts information could not be retrieved."});
+//         });
+// });
+
 
 server.get('/api/posts/:id', (req, res) => {
-    const { id } = req.params;
+    console.log('hello');
+    // const { id } = req.params;
     
-    db
-        .findById(id)
-        .then(posts => {
-            res.json(posts[0]);
-        })
-        .catch(error => {
-            res.status(500).json(error);
-        });
+    // db
+    //     .findById(id)
+    //     .then(posts => {
+    //         res.json(posts[0]);
+    //     })
+    //     .catch(error => {
+    //         res.status(500).json(error);
+    //     });
+    res.json({ a: 1});
 });
+
+
 
 const port = 5000;
 server.listen(port, () => console.log('API Running on port 5000'));
