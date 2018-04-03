0269b3493c65f33bfc1ecf067e7071ce7a9a6b75
KJ Sourcing <tmkogwork@gmail.com>
2018-04-02 22:26:59 -0700

    DONE, stretch goals now

--- server.js
+++ server.js
@@ -78,22 +78,19 @@ server.delete('/api/posts/:id', (req, res) => {
 
 server.put('/api/posts/:id', (req, res) => {
     const { id } = req.params;
+	const post = req.body;
+
+	if (!post) {
+      res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+	}
 
     db
-    .update(id)
+    .update(id, post)
     .then(posts => {
-        res.json(posts[0]);
+        res.status(200).json(posts[0]);
     })
     .catch(error => {
-        res.status(404).json({ message: "The post with the specified ID does not exist." });
-    });
- 	.insert()   
-    .catch(error => {
-      res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
-    });
- 	.update()   
-    .catch(error => {
-      res.status(400).json({ error: "The post information could not be modified." });
+      res.status(500).json({ error: "The post information could not be modified." });
     });
 });
 
