fdcef88172561cb2aa9d7214b3be15409977eb6c
Liz <liz@localhost.localdomain>
2018-04-03 16:08:33 -0400

    having problems with post man, switching

--- server.js
+++ server.js
@@ -37,19 +37,45 @@ server.get('/api/posts', (req, res) => {
   db
     .find()
     .then(posts => res.json(posts))
-    .catch(error => res.status(500).json(error));
+    .catch(error => res.status(500).json({ error: "The posts information could not be retrieved." }));
 });
 
 server.get("/api/posts/:id", (req, res) => {
   const { id } = req.params
   db
     .findById(id)
-    .then(user => { res.json(user[0]) })
+    .then(user => {
+      console.log(user)
+      if(user){
+        res.json(user[0])}
+      res.status(404).json({ message: "The post with the specified ID does not exist." })
+      })
     .catch(error => {
-      res.status(500).json(error);
+      res.status(500).json({ error: "The posts information could not be retrieved." });
     })
 });
 
+server.put('/api/posts/:id', (req, res) => {
+  const { id } = req.params;
+  const update = req.body;
+
+  db
+      .update(id, update)
+      .then(count => {
+          if (count > 0) {
+              db.findById(id).then(updatedPosts => {
+                  res.status(200).json(updatedPosts[0]);
+              });
+          } else {
+              res
+                  .status(404)
+                  .json({ message: 'The user with the specified ID does not exist.' });
+          }
+      })
+      .catch(error => {
+          res.status(500).json(error);
+      });
+});
 
 
 server.listen(port, () => {
