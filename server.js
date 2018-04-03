1fe5c2a60bbad83362fd84f663dc00b8fe8a16a7
Liz <liz@localhost.localdomain>
2018-04-03 15:48:13 -0400

    added more errer messages/validation

--- server.js
+++ server.js
@@ -6,20 +6,30 @@ const server = express();
 const morgan = require('morgan');
 const helmet = require('helmet');
 
+server.use(morgan('dev'));
+server.use(helmet());
+
 server.use(express.json());
 
 // add your server code starting here
 
 server.post('/api/posts', (req, res) => {
   const post = req.body;
-
+  if (!post.title || !post.contents) {
+    res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+  }
   db
     .insert(post)
     .then(newPost => {
-      res.json(newPost);
+      db
+        .findById(newPost.id)
+        .then(user => { res.json(user[0]) })
+        .catch(error => {
+          res.status(500).json(error);
+        })
     })
     .catch(error => {
-      res.status(500).json(error);
+      res.status(500).json({ error: "There was an error while saving the post to the database" });
     });
 });
 
@@ -31,13 +41,16 @@ server.get('/api/posts', (req, res) => {
 });
 
 server.get("/api/posts/:id", (req, res) => {
-  const { id } = req.params 
+  const { id } = req.params
   db
     .findById(id)
-    .then(user => {res.json(user[0])})
-    .catch(error => {res.status(500).json(error);
+    .then(user => { res.json(user[0]) })
+    .catch(error => {
+      res.status(500).json(error);
     })
 });
+
+
 
 server.listen(port, () => {
   console.log('Hit me with your best post!');
