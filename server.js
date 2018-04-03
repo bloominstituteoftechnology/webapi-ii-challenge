1063b5b7235ac1bc430b918b7670475ad0988e59
DragonRydrz <DragonRydrz@me.com>
2018-04-03 16:46:25 -0400

    project completed 100%

--- server.js
+++ server.js
@@ -75,6 +75,11 @@ server.put('/api/posts/:id', (req, res) => {
   const { id } = req.params;
   const update = req.body;
 
+  if (!update.title || !update.contents) {
+    res.status(400).json({
+      errorMessage: 'Please provide title and contents for the post.'
+    });
+  }
   db
     .update(id, update)
     .then(count => {
@@ -85,11 +90,42 @@ server.put('/api/posts/:id', (req, res) => {
       } else {
         res
           .status(404)
-          .json({ message: 'The user with the specified ID does not exist.' });
+          .json({ message: 'The post with the specified ID does not exist.' });
       }
     })
     .catch(error => {
-      res.status(500).json(error);
+      res
+        .status(500)
+        .json({ error: 'The post information could not be modified.' });
+    });
+});
+
+server.delete('/api/posts/:id', (req, res) => {
+  const { id } = req.params;
+
+  db
+    .findById(id)
+    .then(user => {
+      if (user[0]) {
+        const deletedUser = { ...user };
+        db
+          .remove(id)
+          .then(count => {
+            if (count > 0) {
+              res.status(200).json(deletedUser[0]);
+            }
+          })
+          .catch(error => {
+            res.status(500).json({ error: 'The post could not be removed' });
+          });
+      } else {
+        res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist.' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'The post could not be removed' });
     });
 });
 
