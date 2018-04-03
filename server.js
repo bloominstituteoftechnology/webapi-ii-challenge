c267480f18cfa4eeb2987ad6dda2308b2caf66b3
hrehman000@citymail.cuny.edu <hrehman000@citymail.cuny.edu>
2018-04-03 15:34:55 -0400

    delete working

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const db = require("./data/db.js");
+const morgan = require('morgan');
+const helmet = require('helmet');
 
 const server = express();
-server.use(bodyParser.urlencoded({ extended: true }));
-server.use(bodyParser.json());
+
+// add your server code starting here
+
+server.use(morgan('dev'));
+server.use(helmet());
+server.use(express.json());
 
 server.get("/", function(req, res) {
   res.send({ api: "Running...." });
 });
-// add your server code starting here
 
 server.get("/api/posts", function(req, res) {
   db
@@ -40,10 +44,7 @@ server.get("/api/posts/:id", function(req, res) {
 });
 
 server.post("/api/posts", function(req, res) {
-  const post = {
-    title: req.body.title,
-    contents: req.body.contents
-  };
+  const post = req.body;
   db
     .insert(post)
     .then(posts => {
@@ -54,6 +55,48 @@ server.post("/api/posts", function(req, res) {
         errorMessage: "Please provide title and contents for the post."
       });
     });
+});
+
+server.delete('/api/posts/:id', (req,res) => {
+  let post;
+  const { id } = req.params;
+  db
+  .findById(id)
+  .then(response => {
+    post = {...response[0]};
+    db
+    .remove(id)
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    })
+  })
+  .catch(error => {
+    res.status(500).json(error);
+  })
+});
+
+server.put('/api/posts/:id', (req, res) => {
+  const { id } = req.params;
+  const update = req.body;
+  db
+  .update(id, update)
+  .then(count => {
+  if (count > 0) {
+      db.findById(id).then(updatedUsers => {
+      res.status(200).json(updatedUsers[0]);
+      });
+  } else {
+      res
+      .status(404)
+      .json({ message: 'The post with the specified ID does not exist.' });
+  }
+  })
+  .catch(error => {
+  res.status(500).json(error);
+  });
 });
 
 const port = 5000;
