d6a0f494e9f72cf49d66e2673c9ccd988eb9c185
Frank Faustino <nawzyah@gmail.com>
2018-04-03 14:23:42 -0700

    Fixed delete. Now sends deleted post.

--- api/controllers.js
+++ api/controllers.js
@@ -13,7 +13,6 @@ module.exports = {
       const response = await db.insert({ title, contents })
       res.status(201).json({ title, contents })
     } catch (e) {
-      console.log(e)
       handleErr(res, 500, 'There was an error while saving the post to the database')
     }
   },
@@ -43,9 +42,10 @@ module.exports = {
     const { id } = req.params
     try {
       if (!id || isNaN(id)) return handleErr(res, 422, 'Please provide valid id')
+      const post = await db.findById(id)
+      if (post.length < 1) return handleErr(res, 404, `No post found with id ${id}`)
       const response = await db.remove(id)
-      if (!response) return handleErr(res, 404, `No post found with id ${id}`)
-      else res.status(200).json(`Success! ${id} was removed`)
+      res.status(200).json(post)
     } catch (e) {
       handleErr(res, 500, 'The post could not be removed')
     }
