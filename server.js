ee7bd10c63af878aacbd4d27a0326a64f1f49d24
Cristina Laryea <crispad06@gmail.com>
2018-04-03 17:29:10 -0400

    put works

--- server.js
+++ server.js
@@ -87,6 +87,36 @@ server.delete('/api/posts/:id', (req, res) => {
         })
 });
 
+server.put('/api/posts/:id', (req, res) => {
+    const { id } = req.params
+    const { title, contents } = req.body;
+    
+    if(title === undefined || contents === undefined) {
+        res.error(400).json({ errorMessage: "Please provide title and contents for the post." })
+    }
+    else{
+        const newPost = { 
+            title, 
+            contents
+        }
+    
+    db
+        .update(id, newPost)
+        .then(inserts => {
+            if(inserts === 0) {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+            }
+            else{
+                res.status(200).json({ message: "Updated ok." })
+            }
+        }
+        )
+        .catch(error => {
+            res.status(500).json({ error: "The post information could not be modified." })
+        })
+    }
+});
+
 
 
 const port = 5000;
