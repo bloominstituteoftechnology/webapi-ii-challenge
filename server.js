452eac3ded21e9722989011edf7896ff4047c2c1
KJ Sourcing <tmkogwork@gmail.com>
2018-04-03 17:31:08 -0700

    put update to deconstructured object to satisfy JSON

--- server.js
+++ server.js
@@ -82,25 +82,19 @@ server.delete('/api/posts/:id', (req, res) => {
 
 server.put('/api/posts/:id', (req, res) => {
     const { id } = req.params;
-	const { title, contents } = req.body;
-	const post = { title, contents };
+    const { title, contents } = req.body;
+    const post = { title, contents };
 
-	if (!title || !contents) {
-      res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
-	} else {
-	    db
-	    .update(id, post)
-	    .then(posts => {
-	    	if (posts.length < 1) {
-	    		res.status(404).json({ message: "The post with the specified ID does not exist." });
-	    	} else {
-	        	res.status(200).json(post, id);
-	    	}
-	    })
-	    .catch(error => {
-	      res.status(500).json({ error: "The post information could not be modified." });
-	    });
-	};
+    if (!title || !contents) return res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+    db
+        .update(id, post)
+        .then(posts => {
+            if (posts.length < 1) return res.status(404).json({ message: "The post with the specified ID does not exist." });
+            res.status(200).json({ title, contents, id });
+        })
+        .catch(error => {
+            res.status(500).json({ error: "The post information could not be modified." });
+        });
 });
 
 
