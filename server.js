28ddf09210d7a82ffa9a5a7374d02e50114cbd9a
Tiffany Robbins-Cabell <lillialskye@yahoo.com>
2018-04-04 00:52:14 -0400

    Tiffany Robbins

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // import your node modules
 const express =require ('express');
 const db = require('./data/db.js');
-const server = express();
+const server = express()
 const morgan = require('morgan');
 const helmet = require('helmet');
 
@@ -20,113 +20,89 @@ server.get('/api/posts', (req, res) => {
 db
 .find()
 .then (posts => {
-	res
-.status(500)
-.json({error: ' The post information could not be found'})
+	res.json(posts);
+})
+.catch(error => {
+res.status(500).json
+({error: ' The post information could not be retrieved'})
 })
 
-});
+})
+
 
 //find by Id
 server.get ('/api/posts/:id', (req, res) => {
 const { id } = req.params;
 db
 .findById(id)
-.then (posts => {
-	res.json (users [0]);
+.then (post => {
+	res.json (post [0]);
 })
 .catch(error => {
-	res.status(500).json (error);
-});
-});
+	res.status(404).json({
+		message: "The post with the specified ID does not exist." })
+})
+})
 
-// insert should be ok. 500 error
+
+
+// insert 
 server.post('/api/posts', function (req, res) {
 	const post =req.body;
 	db
-	insert(user) .then(response => {
+	insert(post)
+	.then(response => {
 		res.status(201).json(response)
 	})
 	.catch(error => {
 		res
 			.status(500)
 			.json({error: "There was an error while saving the post to the database"})
-	});
-	});
-
+	})
+	})
 /*
 
-// update isn't right d
-// crashes the server
-
+// update 
 server.put ('/api/posts/:id', (req, res)=> {
-const { id } req.params;
+const { id } = req.params;
 const update = req.body;
 db
 .update(id, update)
-.then ( count => {
- if(count > 0 ) {
-db
-.findById(id) .then(udpdated => {
-	res.status(200).json (updatedPost);
+.then(count => {
+ if (count > 0 ) {
+db.findById(id).then(udpdatePosts => {
+	res.status(200).json(updatePost[0]);
 })
 } else {
-	res.status(404).json ({'The post with the specified ID does not exist.'})
+	res.status(400).json ({'The post with the specified ID does not exist.'})
 }
 })
 .catch(error => {
-	res.status(500)
-	.json({ error: "The post information could not be retrieved." })
-});
-});
+	res.status(500).json(error);
+})
+})
 
-
-//delete  both deletes crash the server
-const { id } =req.params;
-if(!id) {
-	res.status(404).json ({
-	message: "The post with the specified ID does not exist." }
-	});
-}
-db
-.remove(id) .catch(error => {
-	res.status(500).json ({ error: "The post with the specified ID does not exist." });
-}
-db
-.remove(id)
-.then(() => {
-	res.json({message: 'Success'});
-
-
-});
-});
-
+*/
+//remove
 server.delete ('/api/posts/:id', (req, res) => {
 const { id } = req.params;
-let post
+let post;
+db
 .findById(id)
 .then(response => {
-	post = {...response[0];
+    post = {...response[0]};
 db
 .remove(id)
 .then (response => {
-	res.json(200).json(response);
+    res.status(500).json(response);
 })
-
+})
 .catch(error => {
-	res
-	.status(500)
-	.json({error:"The post could not be removed" })
+    res.status(500).json({
+    message: "The post could not be removed" })
 
-});
-};
-	
-*/
-
-
-
-
-
+})
+})
 
 const port= 5000;
 server.listen(port, () => 
