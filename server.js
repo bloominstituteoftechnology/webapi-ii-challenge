8a4be549e4c89085298fc67acb3e12f2964153b7
Megan Williamson <meganawilliamson@gmail.com>
2018-04-03 22:04:49 -0700

    complete, without stretch

--- server.js
+++ server.js
@@ -1,64 +1,120 @@
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
+
 const db = require('./data/db.js');
 
-const server = express();
-server.use(morgan('dev'));
-server.use(helmet());
-server.use(express.json());
+// middleware
+const server = express(); // create server
+server.use(morgan('dev')); // logger
+server.use(helmet()); // security
+server.use(express.json()); // parser
 
+// test connection
+server.get('/', (req, res) => {
+    res.json({ api: 'Running.....' });
+});
+
+// get
 server.get('/api/posts', (req, res) => {
     db
     .find()
-    .then(posts => {
-        res.json(posts)
+    .then(users => {
+        res.json(users);
     })
     .catch(error => {
         res.status(500).json({ error: "The posts information could not be retrieved." });
     });
 });
 
-server.get('/api/posts/:id'), (req, res) => {
-    console.log(req);
+// get using params
+server.get('/api/posts/:id', (req, res) => {
     const { id } = req.params;
-    res.json(200).json('Created');
-
-    // db
-    // .findById(id)
-    // .then(response => {
-    //     console.log('response', response);
-    // })
-    // .catch(error => {
-    //     res.status(500).json({ errorMessage: "The post information could not be retrieved." });
-    // })
-};
-
-server.post('/api/posts', (req, res) => {
-    const { title, contents } =req.body;
-    if (!title || !contents) {
-        return res.status(400).json({ errorMessage: 'Please provide title and contents for post'});
-    }
-    const newPost = {
-        title,
-        contents
-    }
-
     db
-    .insert(newPost)
-    .then(id => {
-        const post = {...newPost, id}
-        res.status(201).json({ created: "Created" });
+    .findById(id)
+    .then(post => {
+        if (post.length > 0) {
+            res.json(post[0]);
+        } else {
+        res.status(404).json({ message: "The post with the specified ID does not exist." });
+        }
     })
     .catch(error => {
-        res.status(500).json({ error: "There was an error while saving the post to the database" });
+        res.status(500).json({ error: "The post information could not be retrieved." });
+    });
+});
+
+// post
+server.post('/api/posts', (req, res) => {
+    const { title, contents } = req.body;
+    if (!title || !contents) {
+        return res.status(400).json({ errorMessage: 'Please provide title and contents for post'});
+    } else {
+        const post = req.body;
+        db
+        .insert(post)
+        .then(response => {
+            res.status(201).json({ post });
+        })
+        .catch(error => {
+            res.status(500).json({ error: "There was an error while saving the post to the database" });
+        });
+    }
+});
+
+// delete
+server.delete('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    let post;
+
+    db
+    .findById(id)
+    .then(post => {
+        if (post.length > 0) {
+            post = { ...post[0] };
+            db
+            .remove(id)
+            .then(response => {
+                res.status(200).json({ post });
+            })
+        } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist." });
+        }
+    })
+    .catch(error => {
+        res.status(500).json({ error: "The post could not be removed" });
+    });
+});
+
+// put
+server.put('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    const { title, contents } = req.body;
+    const update = req.body;
+    
+    db
+    .update(id, update)
+    .then(post => {
+        if (post > 0) {
+            if (!title || !contents) {
+                res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+            }
+            db
+            .findById(id)
+            .then(response => {
+                res.status(200).json(response);
+            });
+        } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist." });
+        }
+    })
+    .catch(error => {
+        res.status(500).json({ error: "The post information could not be modified." });
     });
 });
 
 
-
-// console.log('hello');
-const port = 5001;
+const port = 5000;
 server.listen(port, () => {
-    console.log('Server running on port 5001');
+    console.log('Server running on port 5000');
 })
