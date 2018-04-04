d10827e8793ce1513ada49de0335f966319b877b
Cristina Laryea <crispad06@gmail.com>
2018-04-03 21:14:59 -0400

    displaying posts on app

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 
-import logo from './logo.svg';
+import axios from 'axios';
 
 import './App.css';
-import axios from 'axios';
+import {
+  Container,
+  Col, 
+  Card,
+  CardTitle, 
+  CardBody,
+  CardText, 
+  Row
+}from 'reactstrap';
 
 class App extends Component {
   constructor(){
@@ -11,12 +19,38 @@ class App extends Component {
     this.state = {
       posts: []
     };
-  }
+  this.getPosts = this.getPosts.bind(this);
+}
+
+getPosts() {
+  axios.get('http://localhost:5000/api/posts')
+    .then(res => {
+      this.setState({ posts: res.data });
+    });
+}
+
+componentDidMount() {
+  this.getPosts();
+}
 
 
   render() {
-    return 
-      <div className="App" />;
+    return (
+      <Container className="my-5">
+        <Row className="App">
+          {this.state.posts.map(post => (
+            <Col sm={4} className="d-flex align-items-stretch">
+              <Card className="w-100 my-2">
+                <CardBody>
+                  <CardTitle>{post.title}</CardTitle>
+                  <CardText>{post.contents}</CardText>
+                </CardBody>
+              </Card>
+            </Col>   
+          ))}
+        </Row>
+      </Container>
+    );
   }
 }
 
