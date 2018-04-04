c87729edd9c5e9939fe4edd4382ddc528f9a21bb
KJ Sourcing <tmkogwork@gmail.com>
2018-04-03 17:25:18 -0700

    fixed delete and put

--- server.js
+++ server.js
@@ -64,17 +64,19 @@ server.get('/api/posts/:id', (req, res) => {
 
 server.delete('/api/posts/:id', (req, res) => {
     const { id } = req.params;
-
-    db
-    .remove(id)
-    .then(posts => {
-    	if (posts.length < 1) {
-    		res.status(404).json({ message: "The post with the specified ID does not exist." });
-    	}
+    db.findById(id).then(posts => {
+        if (posts.length < 1) {
+            return res.status(404).json({ message: "The post with the specified ID does not exist." });
+        }
+        let post = posts[0]
+        db.remove(id).then(() => {
+            res.status(200).json(post)
+        }).catch(error => {
+            res.status(500).json({ error: "The post information could not be removed" });
+        })
+    }).catch(error => {
+        res.status(500).json({ error: "The post information could not be found" });
     })
-    .catch(error => {
-        res.status(500).json({ error: "The post information could not be removed" });
-    });
 });
 
 
@@ -83,7 +85,7 @@ server.put('/api/posts/:id', (req, res) => {
 	const { title, contents } = req.body;
 	const post = { title, contents };
 
-	if (!post) {
+	if (!title || !contents) {
       res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
 	} else {
 	    db
