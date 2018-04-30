05f6e02f19023541a34b319c3e2037dc6641a11d
nickbrenn <nbren@tutanota.com>
2018-04-30 17:32:29 -0400

    Enabled adding a test post with a changing id that is based on the id of the last post in the current post array

--- server.js
+++ server.js
@@ -1,5 +1,51 @@
-// import your node modules
+const express = require("express");
+const db = require("./data/db.js");
+const server = express();
 
-const db = require('./data/db.js');
+server.get("/", (req, res) => {
+  res.send("Api running");
+});
 
-// add your server code starting here
+server.get("/api/posts", (req, res) => {
+  db
+    .find()
+    .then(posts => {
+      newId = posts[posts.length - 1].id + 1;
+      res.json(posts);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+server.get("/api/posts/:id", (req, res) => {
+  const id = req.params.id;
+  db
+    .findById(id)
+    .then(post => {
+      res.json(post);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+server.post("/api/posts", (req, res) => {
+  const testPost = {
+    id: db.find().then(posts => {
+      posts[posts.length - 1].id + 1;
+    }),
+    title: "TEST TITLE",
+    contents: "test contents"
+  };
+  db
+    .insert(testPost)
+    .then(post => {
+      res.json(post);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+server.listen(5000, () => console.log("\nwe doin it my broseph\n"));
