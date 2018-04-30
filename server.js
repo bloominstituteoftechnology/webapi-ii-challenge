f44bb8fddf22db1ac7e18bf833ed2e197e12034c
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-04-30 15:59:47 -0700

    Applied GET request for Posts & ID

--- server.js
+++ server.js
@@ -1,5 +1,42 @@
 // import your node modules
+const express = require('express');
 
 const db = require('./data/db.js');
 
+const server = express();
 // add your server code starting here
+
+server.get('/', (req, res) => {
+    res.send('This is 8K');
+  });
+
+//  GET Request to /api/posts:
+server.get('/api/posts', (req, res) => {
+  db
+    .find()
+    .then(posts => {
+      res.json(posts);
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'The posts information could not be retrived.' });
+    });
+});
+
+// GET Request to /api/posts/:id:
+server.get('/api/posts/:id', (req, res) => {
+  const id = req.params.id;
+  db
+    .findById(id)
+    .then(posts => {
+      if (posts.length === 0) {
+        res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+      } else {
+        res.json(posts[0]);
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'The post information could not be retrieved.' });
+    });
+});
+
+server.listen(8000, () => console.log('\n== API Running on port 8000 ==\n'));
