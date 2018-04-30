3bb5d4cdf104d5b2cbf04da52f71ece56935a083
totsuya <supertatortots@gmail.com>
2018-04-30 16:31:26 -0400

    added basic .get stuff

--- server.js
+++ server.js
@@ -3,3 +3,23 @@
 const db = require('./data/db.js');
 
 // add your server code starting here
+
+const express = require ('express');
+
+const server = express();
+
+server.get("/", (req, res) => {
+  res.send("API running");
+});
+
+server.get('/api/posts', (req, res)=> {
+  db
+  .find()
+  .then(posts => {
+    res.json(posts);
+  })
+  .catch(error => {
+    res.status(500).json({ error : error});
+    // error event thing here
+  });
+});
