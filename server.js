8680949f1aa7cbbdab3ac15c296f5630d03ca2db
2940cristian <2940cristian@gmail.com>
2018-04-30 14:29:35 -0700

    put working

--- server.js
+++ server.js
@@ -15,17 +15,34 @@ server.post("/api/posts", (req, res) => {
         title: "",
         contents: "",
     }).then(posts => {
-        console.log(post)
+        console.log('posted')
     }).catch(err => {
         res.statusCode === 400;
+        return res.json({
+            error: "The posts information could not be retrieved."
+        })
     })
 })
 
+server.put("/api/posts/:id", (req, res) => {
+    const id = req.params.id;
+    db.update(id, ({
+        title: "We changed",
+        contents: "did we though?"
+    })).then(post => {
+        console.log('success put');
+    }).catch(err => {
+        console.log(err)
+    })
+})
 server.get("/api/posts", (req, res) => {
     db.find().then(posts => {
         res.json(posts);
     }).catch(err => {
         res.statusCode = 500;
+        return res.json({
+            error: "There was an error while saving the post to the database"
+        })
     })
 })
 
@@ -43,7 +60,7 @@ server.delete("/api/posts/:id", (req, res) => {
     const id = req.params.id;
 
     db.remove(id).then(post => {
-        console.log(post)
+        console.log('deleted')
     }).catch(err => {
         res.statusCode = 500;
     })
