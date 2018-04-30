d1ef6177ec752797609f6ecc0edd0a00cc44ef3d
jeffrey <jeffreyneilflynn@gmail.com>
2018-04-30 15:39:51 -0400

    fetch post by id

--- server.js
+++ server.js
@@ -10,7 +10,18 @@ server.get('/', (req, res) => {
 server.get('/api/posts', (req, res) => {
   db.find()
     .then(posts => res.json(posts))
-    .catch(err => req.status(404).json({ error: err }) )
+    .catch(err => req.status(500).json({ error: "The posts information could not be retrieved." }) )
+})
+
+server.get('/api/posts/:id', (request, response) => {
+  const { id } = request.params;
+  db.findById(id)
+    .then(post => {
+      post.length === 0 ? 
+        response.status(404).json({ error: "The post with the specified ID does not exist." }) :
+        response.json(post[0])
+    })
+    .catch(error => response.status(500).json({ error: "The post information could not be retrieved." }))
 })
 
 server.listen(5000);
