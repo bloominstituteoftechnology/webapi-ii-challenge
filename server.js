18cba1a0f596205ba8899f3b48137bd270633185
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-04-30 15:37:43 -0400

    Added express, started wicreating an instance of the server

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 // import your node modules
+const express = require('express');
+
+// add your server code starting here
+
+const server = express();
 
 const db = require('./data/db.js');
 
-// add your server code starting here
+server.listen(3000);
