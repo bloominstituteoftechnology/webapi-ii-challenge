1e470205193a6f694383117e8835f6f09dc38d23
2940cristian <2940cristian@gmail.com>
2018-04-30 13:58:15 -0700

    Got delete and get working

--- server.js
+++ server.js
@@ -1,5 +1,51 @@
 // import your node modules
+const express = require("express")
 
 const db = require('./data/db.js');
 
 // add your server code starting here
+const server = express();
+
+server.get("/", (req, res) => {
+    res.send("It works, moving on")
+})
+
+server.post("/api/posts", (req, res) => {
+    db.insert(req.params).then(post => {
+        console.log(post)
+    }).catch(err => {
+        console.log(err)
+    })
+})
+
+
+server.get("/api/posts", (req, res) => {
+    db.find().then(posts => {
+        res.json(posts);
+    }).catch(err => {
+        res.statusCode = 500;
+    })
+})
+
+server.get("/api/posts/:id", (req, res) => {
+    const id = req.params.id;
+
+    db.findById(id).then(user => {
+        res.json(user)
+    }).catch(err => {
+        res.statusCode = 500;
+    })
+})
+
+server.delete("/api/posts/:id", (req, res) => {
+    const id = req.params.id;
+
+    db.remove(id).then(post => {
+        console.log(post)
+    }).catch(err => {
+        res.statusCode = 500;
+    })
+})
+
+
+server.listen(5000, () => console.log("listening on port 5000"));
