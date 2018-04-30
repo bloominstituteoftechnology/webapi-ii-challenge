0f21b023d0a00671e4a5d90845f4f30eceb94690
Harrison Brock <harrison@harrisonbrock.com>
2018-04-30 16:27:50 -0400

    Added: Get All Post

--- server.js
+++ server.js
@@ -2,4 +2,15 @@ const express = require('express');
 const db = require('./data/db');
 
 const server = express();
+
 // add your server code starting here
+server.get('/api/posts', (req, res) => {
+    db.find().then(posts => {
+        res.json(posts)
+    }).catch(error => {
+        console.log(error);
+    })
+
+});
+
+server.listen(5000,() => console.log(`API Running on port 5000`));
