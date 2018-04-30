afc7de6f391e2d6d25b4cfe921602fbf14dd43a1
jeffrey <jeffreyneilflynn@gmail.com>
2018-04-30 15:28:37 -0400

    posts rendering on screen

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-// import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
+const server = express();
 
-// add your server code starting here
+// route handler
+server.get('/', (req, res) => {
+  res.send("your api is running");
+})
+
+server.get('/api/posts', (req, res) => {
+  db.find()
+    .then(posts => res.json(posts))
+    .catch(err => req.status(404).json({ error: err }) )
+})
+
+server.listen(5000);
