e5f9177003abeaedd6d6cdc5b751f452bf811fdc
Justin Rigler <justinmrigler@gmail.com>
2018-04-30 19:24:57 -0400

    pulled in dependencies, laid out basic server structure
    I used yarn to install all of my dependencies

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
+const express = require('express');
 // import your node modules
 
 const db = require('./data/db.js');
 
 // add your server code starting here
+const server = express();
+
+// start db
+server.get('/', (req, res) => {
+  res.send('API running');
+});
+
+server.listen(5000, () => console.log('\n== API Running on port 5000 ==\n'));
