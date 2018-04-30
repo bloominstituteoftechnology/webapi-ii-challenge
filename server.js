15acbff58858195c95bc1b557e49d3f5db822175
Rachelle De Jesus Pestanas <rachelle.pestanas@gmail.com>
2018-04-30 17:41:44 -0500

    initial commit: dependencies installed / API Running

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
 // import your node modules
+const express = require('express');
 
 const db = require('./data/db.js');
 
 // add your server code starting here
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send('API running');
+});
+
+server.get('/api/posts', (req, res) => {
+//get the posts
+ db
+  .find()
+ .then(posts => {
+   res.json(posts);
+ })
+ .catch(err => {
+   res.status(500).json({ error: err });
+// do something with the error
+ });
+});
