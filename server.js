e49f9ff375c99d6fa212a9635173bee8cbd03df8
sadrak105 <sadrak10@gmail.com>
2018-04-30 15:23:49 -0400

    listening on port

--- server.js
+++ server.js
@@ -6,3 +6,5 @@ const db = require('./data/db.js');
 const server = express();
 
 // add your server code starting here
+
+server.listen(8000, () => console.log('\n== API Running on port 8000 ==\n'));
