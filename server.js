c6b41dddf7316fb4c6f2000271eb795aedd253f9
Jared Cuffe <jcuffe@gmail.com>
2018-04-30 16:01:40 -0700

    Modified to match this api

--- server.js
+++ server.js
@@ -5,15 +5,15 @@ var db = require('./data/db')
 app = express()
 app.use(bodyParser.json())
 
-app.get('/api/users/', (req, res) => {
+app.get('/api/posts/', (req, res) => {
   db.find()
     .then(response => res.status(200).send(response))
-    .catch(error => res.status(500).send({ error: "The users information could not be retrieved" }))
+    .catch(error => res.status(500).send({ error: "The posts information could not be retrieved" }))
 })
 
-app.post('/api/users/', (req, res) => {
-  var user = req.body
-  if (user && user.name && user.bio) {
+app.post('/api/posts/', (req, res) => {
+  var post = req.body
+  if (post && post.title && post.content) {
     db.insert(user)
       .then(response => {
         res.status(201).send(response)
@@ -24,24 +24,24 @@ app.post('/api/users/', (req, res) => {
   } else {
     res
       .status(400)
-      .send({ errorMessage: 'Please provide name and bio for the user.' })
+      .send({ errorMessage: 'Please provide title and content for the post.' })
   }  
 })
 
-app.get('/api/users/:id', (req, res) => {
+app.get('/api/posts/:id', (req, res) => {
   var id = req.params.id
   db.findById(id)
     .then(response => {
       if (response.length === 0) {
-        res.status(404).send({ message: "The user with the specified ID does not exist" })
+        res.status(404).send({ message: "The post with the specified ID does not exist" })
       } else {
         res.status(200).send(response)
       }
     })
-    .catch(error => res.status(500).send({ error: "The user information could not be retrieved" }))
+    .catch(error => res.status(500).send({ error: "The post information could not be retrieved" }))
 })
 
-app.delete('/api/users/:id', (req, res) => {
+app.delete('/api/posts/:id', (req, res) => {
   var id = req.params.id
   db.remove(id)
     .then(response => {
@@ -51,26 +51,27 @@ app.delete('/api/users/:id', (req, res) => {
         res.status(200).send({ message: `User with id ${id} deleted` })
       }
     })
-    .catch(error => res.status(500).send({ error: "The user could not be removed" }))
+    .catch(error => res.status(500).send({ error: "The post could not be removed" }))
 })
 
-app.put('/api/users/:id', (req, res) => {
+app.put('/api/posts/:id', (req, res) => {
   var id = req.params.id
-  var user = req.body
-  if (user && (user.name || user.bio)) {
-    db.update(id, user)
+  var post = req.body
+  if (post && (post.title || post.contents)) {
+    db.update(id, post)
       .then(response => {
         if (response == 0) {
-          res.status(404).send({ message: "The user with the specified ID does not exist" })
+          res.status(404).send({ message: "The post with the specified ID does not exist" })
         } else {
           db.findById(id)
             .then(response => {
               res.status(200).send(response)
             })
+            .catch(error => res.status(500).send( { error: "There was an error updating the post" }))
         }
       })
   } else {
-    res.status(400).send({ error: "Please provide name and bio for the user" })
+    res.status(400).send({ error: "Please provide title or content for the post" })
   }
 })
 
