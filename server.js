f3dffb7b58d427fb46b71c965a625e386dcf2d43
Justin Hong <jhong2009@gmail.com>
2018-04-30 13:27:59 -0700

    in server.js: set up route handler for GET request at /api/posts

--- server.js
+++ server.js
@@ -7,3 +7,23 @@ const db = require("./data/db");
 server.get("/", (req, res) => {
 	res.send("API running");
 });
+
+// GET api/posts
+server.get("/api/posts", (req, res) => {
+	// make a request for list of posts
+	// resolve by returning posts or throw an error
+	db
+		.find()
+		.then(posts => {
+			res.json(posts);
+		})
+		.catch(err => {
+			res
+				.status(500)
+				.json({ error: "The posts information could not be retrieved." });
+		});
+});
+
+server.listen(5000, () => {
+	console.log("\n== API Running on port 5000 ==\n");
+});
