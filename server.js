c42e5fc588dbab74606092a62b7d95dfaebd24b4
Nedu Robert <nedumrobert@gmail.com>
2018-04-30 21:08:07 +0100

    wrote an endpoint that returns an array of all post objects in the db

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 // import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
+const server = express();
 
 // add your server code starting here
+server.get('/api/posts', (req, res) => {
+    db
+    .find()
+    .then(posts => {
+        res.json(posts);
+    })
+    .catch(err => {
+        res.status(500).json({ error: "The posts information could not be retrieved."})
+    })
+});
+
+server.listen(5000, () => console.log('\n== API Running on port 5000 ==\n'));
