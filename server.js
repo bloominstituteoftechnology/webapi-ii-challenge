ae3167ccaaa2d7e52c7776c9328b6165e1cb0f2b
unknown <bsa6.23.94@gmail.com>
2018-04-30 15:47:57 -0400

    set up complete

--- server.js
+++ server.js
@@ -1,5 +1,41 @@
-// import your node modules
+const express = require("express");
 
 const db = require('./data/db.js');
 
+const server = express();
+
 // add your server code starting here
+
+server.get('/api/posts', (req, res) => {
+    db
+    .find()
+    .then(posts => {
+        res.json(posts);
+    })
+    .catch(err => {
+        res.status(500).json({ error: 'The posts information could not be retrieved.'})
+    });
+});
+
+
+server.get('/api/posts/:id', (req, res) => {
+    const id = req.params.id;
+    
+    db
+    .findById(id)
+    .then(posts => {
+        if (posts.length === 0) {
+            res.status(404).json({ message: "The post with the specified ID does not exist."})
+        } else {
+            res.json(post[0]);
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ error: 'The posts information could not be retrieved.'})
+    });
+});
+
+
+server.listen(5000, () => {
+    console.log('server listening on port 5000');
+  });
