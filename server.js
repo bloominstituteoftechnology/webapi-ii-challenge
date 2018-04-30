459c31721e25651df78c1390d67621bcbef463b5
mgchmiel <michaelchmielniak@yahoo.com>
2018-04-30 15:22:17 -0400

    added put

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 
 // add your server code starting here
-const port = 3000;
+const port = 5000;
 
 const server =  express();
 server.use(bodyParser.json())
@@ -20,9 +20,38 @@ server.get('/', (req, res) => {
 server.get('/api/posts', (req, res) => {
     db.find()
     .then( users => res.json(users))
-    .catch(error => res.status(500).json({ error: "Info not retrieved"}));
+    .catch(error => res.status(500).json({ error: "Info Not Retrieved"}));
 })
 
 server.get('api/posts/:id', function(req, res) {
     const { id } = req.params;
+    db.findById(id)
+    .then(posts => {
+        if (posts[0]) res.json(posts[0])
+    else res.status(404).json({ message: "Specific ID Doesn't Exist"})
+    })
+    .catch(error => res.status(500).json({ error:"Info Not Retrieved"}));
 })
+
+server.put('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    const { title, content } = req.body;
+    const updatePost = { title, content };
+    if (!title || !content) {
+        res.status(400).json({
+            errorMessage: "Enter Title and Content"
+        });
+        return;
+    }
+    db.findById(id)
+    .then(posts => {
+        if (posts[0]) {
+db.update(id, {title, content})
+.then(id => res.status(200).json({ title, content }))
+.catch(error => res.status(500).json({ error: "Can't Be Modified"}));
+        }
+        else res.status(404).json({ message: "Specific ID Doesn't Exist"})
+    })
+    .catch(error => res.status(500).json({ error: "Info Not Retrieved"}));
+})
+
