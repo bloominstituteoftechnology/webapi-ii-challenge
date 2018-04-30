c98cf923aa3b27468ac8d8134e592a3411461b09
Devon Bacon <devon.bacon@gmail.com>
2018-04-30 12:36:03 -0700

    Set up first get request for express server

--- server.js
+++ server.js
@@ -3,3 +3,9 @@ const express = require('express');
 const db = require('./data/db.js');
 
 // add your server code starting here
+const server = express();
+
+server.get('/api/posts', (req, res) => {
+
+});
+
