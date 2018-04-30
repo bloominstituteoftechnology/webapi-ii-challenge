4ac49c7b5c35f0ea854bcff460669ff5632cc65d
mgchmiel <michaelchmielniak@yahoo.com>
2018-04-30 15:30:36 -0400

    added delete and post

--- server.js
+++ server.js
@@ -55,3 +55,37 @@ db.update(id, {title, content})
     .catch(error => res.status(500).json({ error: "Info Not Retrieved"}));
 })
 
+server.post('api/posts', function(req, res) {
+    const { title, content } = req.body;
+    if (!title || !content) {
+        res.status(400).json({ errorMessage: "Enter Title and Content"})
+    }
+    console.log(post)
+    db
+    .insert(post)
+    .then(posts => {
+        res.status(201).json(posts);
+    })
+    .catch(error => {
+        res.status(500).json({error: "Can Insert In DB"})
+    })
+})
+
+server.delete('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    db.findById(id)
+    .then(posts => {
+        if (posts[0]) {
+            const removePost = posts[0]
+            db.remove(id)
+            .then(complete => {
+                res.status(200).json(removePost)
+            })
+            .catch(error => res.status(500).json({ error: "Can Not Be Removed"}));
+        }
+        else res.status(404).json({ message: "Specific ID Doesn't Exist"})
+    })
+    .catch(errror => res.status(500).json({ error: "Info Not Retrieved"}));
+})
+
+server.listen(port, () => console.log('API On Port 5000'));
