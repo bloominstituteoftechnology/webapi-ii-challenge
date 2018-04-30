cfd31534de253ffd6bfed1eb6f5a9029523b76e9
unknown <bsa6.23.94@gmail.com>
2018-04-30 17:28:34 -0400

    finished get and post

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
-
+const bodyParser = require('body-parser');
 const db = require('./data/db.js');
 
 const server = express();
+server.use(bodyParser.json());
 
 // add your server code starting here
+server.get('/', (req, res) => {
+    res.send('Api running');
+  });
 
 server.get('/api/posts', (req, res) => {
     db
@@ -27,11 +31,29 @@ server.get('/api/posts/:id', (req, res) => {
         if (posts.length === 0) {
             res.status(404).json({ message: "The post with the specified ID does not exist."})
         } else {
-            res.json(post[0]);
+            res.json(posts[0]);
         }
     })
     .catch(err => {
         res.status(500).json({ error: 'The posts information could not be retrieved.'})
+    });
+});
+
+
+server.post('/api/posts', (req, res) => {
+const {title, contents} = req.body;
+const postNew = { title, contents}
+    console.log(req);
+    if (title.length === 0 || contents.length === 0) {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    } else
+    db
+    .insert(postNew)
+    .then(post => {
+        res.status(201).json(post);
+})
+    .catch(err => {
+        res.status(500).json({ error: "There was an error while saving the post to the database" })
     });
 });
 
