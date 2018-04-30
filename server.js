eff5769fb8ad99346e16a384eada58c502862be4
Kyle Kauzlarich <kauzlarich.kyle@gmail.com>
2018-04-30 15:15:36 -0700

    7

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 // import your node modules
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const db = require('./data/db.js');
 
 // add your server code starting here
 const server = express();
+server.use(bodyParser.json());
 
 server.get('/', (req, res) => {
     res.send('Apr running');
 });
 
 //post
-server.post('/api/posts', function(req, res) {
+server.post('/api/posts', (req, res) => {
     const { title, contents } = req.body
     const post = {title: title, contents: contents};
-    db.insert(post).then().catch(err => {
+    db.insert(post).then(
+        res.status(200).json(post)
+    ).catch(err => {
         res.status(500).json({ error: "There was an error while saving the post to the database" });
     })
 
@@ -48,7 +51,14 @@ server.delete('/api/posts/:id', (req, res) => {
 });
 //put
 server.put('/api/posts/:id', (req, res) => {
-
+    const id = req.params.id;
+    const { title, contents } = req.body;
+    const post = {title: title, contents: contents};
+    db.update(id, post).then(
+        res.status(200).json(post)
+    ).catch(err => {
+        res.status(500).json({ error: "There was an error while saving the post to the database" });
+    })
 });
 
 server.listen(666, () => console.log('\n==API Running on port 666\n'));
