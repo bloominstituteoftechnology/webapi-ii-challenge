ea8c65653a2dc15314da5191c122039bcdb9cb6b
Yasin Shuman <email@yasin.io>
2018-04-30 12:42:23 -0700

    added express, began code on server.js

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 // import your node modules
+const express = require("express");
 
-const db = require('./data/db.js');
+const server = express();
+const db = require("./data/db.js");
 
 // add your server code starting here
+server.get("/", (req, res) => {
+  res.send("Api Running");
+});
+
+server.listen(5000, () => console.log("\n== API running on port 5000 ==\n"));
