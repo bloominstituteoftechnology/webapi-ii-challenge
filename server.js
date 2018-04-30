0f479fe48f5b39634b5c2eade4fb218b6198ed7d
Henry K <mykim1891@hotmail.com>
2018-04-30 16:43:52 -0700

    DELETE is working; working on PUT later

--- server.js
+++ server.js
@@ -14,10 +14,12 @@ server.use(express.json());
 
 // add your server code starting here
 
+// server GET, return 'API is running'
 server.get("/", (req, res) => {
   res.send("API is running");
 });
 
+// server GET, find "posts", then return it.
 server.get("/api/posts", (req, res) => {
   db
     .find()
@@ -29,15 +31,16 @@ server.get("/api/posts", (req, res) => {
     });
 });
 
+// server GET, find by ID first, then return "post"
 server.get("/api/posts/:id", (req, res) => {
   const id = req.params.id;
   db
     .findById(id)
-    .then(posts => {
-      if (posts.length === 0) {
+    .then(post => {
+      if (post.length === 0) {
         res.status(404).json({ message: "Post not found. Try again." });
       } else {
-        res.json(posts[0]);
+        res.json(post[0]);
       }
     })
     .catch(err => {
@@ -45,6 +48,7 @@ server.get("/api/posts/:id", (req, res) => {
     });
 });
 
+// server POST, Insert post, then give response, which is id of the post
 server.post("/api/posts", (req, res) => {
   const post = req.body;
 
@@ -58,3 +62,24 @@ server.post("/api/posts", (req, res) => {
     });
 });
 server.listen(5000, () => console.log("\n== API Running on port 5000 ==\n"));
+
+// server DELETE, find post by id, then all details of the post (content + title) is defined as 'post'.
+// delete the 'post' then return the title and content of post deleted.
+server.delete("/api/posts/:id", (req, res) => {
+  const id = req.params.id;
+
+  db
+  .findById(id)
+  .then(response => {
+    post = { ...response[0] };
+    db
+      .remove(id)
+      .then(response => {
+        res.status(201).json(post);
+      })
+      .catch(error => {
+        res.status(500).json({ error: "Nothing to delete" });
+      });
+  });
+  db;
+});
