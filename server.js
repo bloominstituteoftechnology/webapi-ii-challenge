ad86c5e0ce6907552134dcc4a384ed57323f872b
sadrak105 <sadrak10@gmail.com>
2018-04-30 16:02:31 -0400

    get posts

--- server.js
+++ server.js
@@ -6,5 +6,13 @@ const db = require('./data/db.js');
 const server = express();
 
 // add your server code starting here
+//GET
+server.get('/api/posts', (req, res) => {
+    db.find().then(posts => {
+        res.status(200).json(posts);
+    }).catch(err => {
+        res.status(500).json({error:"The posts information could not be retrieved."});
+    });
+})
 
 server.listen(8000, () => console.log('\n== API Running on port 8000 ==\n'));
