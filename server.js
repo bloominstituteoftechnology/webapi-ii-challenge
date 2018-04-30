3a6bd2de719b8695d0a1ee19f3355fb2a1594290
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-04-30 16:29:16 -0400

    Created GET request, shows array and individual post. Set up status code

--- server.js
+++ server.js
@@ -7,4 +7,32 @@ const server = express();
 
 const db = require('./data/db.js');
 
+server.get('/', (req, res) => {
+    res.send('API running');    
+})
+
+server.get('/api/posts', (req, res) => {
+    db
+    .find()
+    .then(posts => {
+        res.json(posts);
+    })
+    .catch(err => {
+        res.status(500).json({ error: "The posts information could not be retrieved." })
+    });
+});
+
+server.get('/api/posts/:id', (req, res) => {
+    const id = req.params.id;
+
+    db
+    .findById(id)
+    .then(post => {
+        res.json(post);
+    })
+    .catch(err => {
+        res.status(404).json({ message: "The post with the specified ID does not exist." })
+    })
+})
+
 server.listen(3000);
