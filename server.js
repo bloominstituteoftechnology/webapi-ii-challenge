c877e5749991e3cc14e159dc27e85f3bc4ab6b0c
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-04-30 16:00:31 -0700

    Sectioned code out & implemented GET & POST functions

--- server.js
+++ server.js
@@ -1,46 +1,71 @@
 // import your node modules
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const db = require('./data/db.js');
 
 // add your server code starting here
 const server = express();
+server.use(bodyParser.json());
 
 server.get('/', (req, res) => {
     res.send('API running');
 });
 
+//PART 1 - GET request to /api/posts
 server.get('/api/posts', (req, res) => {
-//get the posts
- db
- .find()
- .then(posts => {
-   res.json(posts);
- })
- .catch(err => {
-   res.status(500).json({ error: err });
-// do something with the error
- });
+    db
+    .find()
+    .then(posts => {
+        res.json(posts);
+    })
+    // do something with the error
+    .catch(err => {
+    res.status(500).json({ error: 'The posts information could not be retrieved.' });
+    });
 });
 
-// /api/posts/123
+//--------------------------------------------------------------------------------
+
+//PART 2 - GET request to /api/posts/:id
 server.get('/api/posts/:id', (req, res) => {
 // grab the id from URL parameters
-const id = req.params.id;
+    const id = req.params.id;
 
-db
- .findById(id)
- .then(posts => {
-    if (posts.length === 0) {
-     res.status(404).json({ message: 'post not found' });
-   } else {
-     res.json(posts[0]);
-   }
- })
- .catch(err => {
-// do something with the error
-   res.status(500).json({ error: err });
- });
+    db
+    .findById(id)
+    .then(posts => {
+        if (posts.length === 0) {
+        res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+    } else {
+        res.json(posts[0]);
+    }
+})
+//If there's an error in retrieving the post from the database
+    .catch(err => {
+    res.status(500).json({ error: 'The posts information could not be retrieved.' });
+    });
 });
 
+//--------------------------------------------------------------------------------
+
+//PART 3 - POST request to /api/posts
+server.post('/api.posts', (req, res) => {
+    const{title, contents} = req.body;
+    const postNew = {title, contents}
+        console.log(req);
+        if (title.length === 0 || contents.length === 0) {
+            res.status(400).json({ errorMessage: 'Please provide title and contents for the post.' })
+        } else
+        db
+        .insert(postNew)
+        .then(post => {
+            res.status(201).json(post);
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'There was an error while saving the post to the database.'})
+        });
+});
+
+
+//---------------------------------------------------------------------------------
 server.listen(5000, () => console.log('\n== API Running on port 5000 ==\n'));
