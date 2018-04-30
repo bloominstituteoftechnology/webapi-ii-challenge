1f40a02de7f6d46ff36d3a9127099164fec0fc95
David Alexander Morales <damorales89@gmail.com>
2018-04-30 17:57:02 -0400

    starting server

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 // import your node modules
+const express = require('express');
 
 const db = require('./data/db.js');
 
+const server = express();
+
 // add your server code starting here
+server.get('/', (req, res) => {
+    res.send('API IS LIT');
+  });
+  
+server.listen(8000, () => console.log('\n== API Running on port 5000 ==\n'));
