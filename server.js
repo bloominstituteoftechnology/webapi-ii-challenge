f440972b9680396cffed222cacdef7841c72ac3b
SyCoDe <SyCoDe@gmail.com>
2018-04-30 17:34:13 -0600

    POST, GET, PUT complete

--- server.js
+++ server.js
@@ -1,17 +1,72 @@
 // import your node modules
 const express = require('express');
+const db = require('./data/db.js');
+const bodyParser = require("body-parser");
 
 const server = express();
 
-const db = require('./data/db.js');
 
 // add your server code starting here
-server.get('/', (req, res) => {
-  res.send('API running');
+server.use(bodyParser.json());
+
+server.get("/", (req, res) => {
+  res.send("Api running");
 });
 
-server.get('/api/users', (req, res) => {
-  // req.send('API running');
+server.get("/api/posts", (req, res) => {
+  //get the posts
+  db
+    .find()
+    .then(posts => {
+      res.json(posts);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The posts information could not be retrieved." });
+      // do something with the error
+    });
+});
+
+// /api/posts/123
+server.get("/api/posts/:id", (req, res) => {
+  // grab the id from URL parameters
+  const id = req.params.id;
+
+  db
+    .findById(id)
+    .then(posts => {
+      if (posts.length === 0) {
+        res.status(404).json({ message: "post not found" });
+      } else {
+        res.json(posts[0]);
+      }
+    })
+    .catch(err => {
+      // do something with the error
+      res.status(500).json({ error: err });
+    });
+});
+
+server.post("/api/posts", (req, res) => {
+  const post = req.body;
+
+  if (post.title === "" || post.contents === "") {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    });
+  } else {
+    db
+      .insert(post)
+      .then(response => {
+        res.status(201).json({ post });
+      })
+      .throw(() => {
+        res.status(500).send({
+          error: "There was an error while saving the post to the database"
+        });
+      });
+  }
 });
 
 let port = 8999;
