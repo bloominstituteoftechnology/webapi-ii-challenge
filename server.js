b66f0fe3113b36db0aad274af85f6838147ca2a9
sadrak105 <sadrak10@gmail.com>
2018-04-30 17:59:12 -0400

    get by id

--- server.js
+++ server.js
@@ -9,10 +9,24 @@ const server = express();
 //GET
 server.get('/api/posts', (req, res) => {
     db.find().then(posts => {
-        res.status(200).json(posts);
+        res.json(posts);
     }).catch(err => {
         res.status(500).json({error:"The posts information could not be retrieved."});
     });
 })
+
+server.get('/api/posts/:id', (req,res) => {
+    const id = req.params.id
+
+    db.findById(id).then(posts => {
+        if(posts.length === 0){
+            res.status(404).json({ message: "The post with the specified ID does not exist." });
+        } else {
+            res.json(posts[0]);
+        }
+    }) .catch(err => {
+        res.status(500).json({error: "The post information could not be retrieved."})
+    })
+});
 
 server.listen(8000, () => console.log('\n== API Running on port 8000 ==\n'));
