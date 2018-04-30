c35bcaaff2ed10d461a788e830db54808b877328
SyCoDe <SyCoDe@gmail.com>
2018-04-30 17:04:51 -0600

    installed express, created server

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 // import your node modules
+const express = require('express');
+
+const server = express();
 
 const db = require('./data/db.js');
 
 // add your server code starting here
+server.get('/', (req, res) => {
+  res.send('API running');
+});
+
+server.get('/api/users', (req, res) => {
+  // req.send('API running');
+});
+
+let port = 8999;
+server.listen(port, ()=> console.log('\n== API Running on port ' + port + ' =='));
