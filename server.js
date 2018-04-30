8588e65fa0711b4e3432a2f7004497852bc5c57d
David Alexander Morales <damorales89@gmail.com>
2018-04-30 19:42:07 -0400

    get complete

--- server.js
+++ server.js
@@ -7,7 +7,40 @@ const server = express();
 
 // add your server code starting here
 server.get('/', (req, res) => {
-    res.send('API IS LIT');
+    res.send('API IS LITTY CITY!');
   });
+
+server.get('/api/posts', (req, res) => {
+    //get all the users
+    db
+        .find()
+        .then(users => {
+            res.json(users);
+        })  
+        .catch(err => {
+            res.status(500).json({ error: 'The posts information could not be retrieved.' });
+        });
+});
+
+// /api/users/id123
+
+server.get('/api/posts/:id', (req, res) => {
+    //grab the id from URL parameters
+    const id = req.params.id;
+
+    db
+        .findById(id)
+        .then(users => {
+            if (users.length === 0) {
+                res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+            }   else {
+                res.json(users[0]);
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'The post information could be not be retrieved' });
+        });
+})
+
   
 server.listen(8000, () => console.log('\n== API Running on port 5000 ==\n'));
