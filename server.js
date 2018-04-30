aba2ea6c0d573afd319c572d7a96663f7844e926
Robert Hogan <roberthogan@Roberts-MacBook-Pro.local>
2018-04-30 16:39:38 -0400

    Robert Hogan. Further progress on assignment.

--- server.js
+++ server.js
@@ -1,5 +1,45 @@
 // import your node modules
+const express = require('express');
+
+// add your server code starting here
+
+const server = express();
 
 const db = require('./data/db.js');
 
-// add your server code starting here
+server.get('/', (req, res) => {
+    res.send('API Running');
+});
+
+server.get('/api/posts', (req, res) => {
+    db
+    .find()
+    .then(posts => {
+        res.json(posts);
+    })
+    .catch(err => {
+        res.status(500).json({ error: "The posts information could not be retrieved" });
+    })
+
+})
+
+server.get('/api/posts/:id', (req, res) => {
+    const id = req.params.id;
+
+    db 
+    .findById(id)
+    .then(post => {
+        if(post.length === 0) {
+            res.status(404).json({ message: "Not Found" })
+        } else {
+        res.json(post);
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ message: "The post with the specified ID does not exist." });
+    })
+})
+
+
+
+server.listen(3000);
