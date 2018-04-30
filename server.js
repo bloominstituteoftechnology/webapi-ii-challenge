78be66a79f29706c273281bc60bb002c26ae059e
Yasin Shuman <email@yasin.io>
2018-04-30 12:59:28 -0700

    added get route for /api/posts

--- server.js
+++ server.js
@@ -5,8 +5,17 @@ const server = express();
 const db = require("./data/db.js");
 
 // add your server code starting here
-server.get("/", (req, res) => {
-  res.send("Api Running");
+server.get("/api/posts", (req, res) => {
+  db
+    .find()
+    .then(posts => {
+      res.json(posts);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The posts information could not be retrieved." });
+    });
 });
 
 server.listen(5000, () => console.log("\n== API running on port 5000 ==\n"));
