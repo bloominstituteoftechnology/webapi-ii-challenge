b835e0a158538496f26bf7d3d04b3638a101f68c
Henry K <mykim1891@hotmail.com>
2018-04-30 12:48:03 -0700

    Basic set up for server.listen

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 // import your node modules
+const express = require("express");
 
-const db = require('./data/db.js');
+const db = require("./data/db.js");
+
+const server = express();
 
 // add your server code starting here
+
+server.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+// server.post("/", (req, res) => {});
+
+server.listen(5000, () => console.log("\n== API Running on port 5000 ==\n"));
