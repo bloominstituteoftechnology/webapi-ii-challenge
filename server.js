0079dfb74c0734e3d5754ac1c3d7f1e88a931148
Paul Fang <paulfang@pauls-mbp.home>
2018-04-30 14:53:13 -0700

    working on POST

--- server.js
+++ server.js
@@ -5,6 +5,20 @@ const db = require('./data/db.js');
 const server = express();
 server.listen(5000, () => console.log('\n== API Running on port 5000 ==\n'));
 
+server.post('/api/users', (req, res) => {
+    const ob = req.body;
+    
+    db
+        .insert(ob)
+        .then(response => {
+            if (typeof req.body.title !== 'undefined' && typeof req.body.contents !== 'undefined') {
+                res.status(400).json({ message: 'Please provide title and contents for the post.'})
+            }
+         else {
+            res.status(201).json({ messege: 'Post Successful.' })
+        }})
+        .catch(err => {
+            res.status(500).json({ message: 'here was an error while saving the post to the database'})
+        })
 
-// add your server code starting here
-
+})
