c0ae095e937ee129372305dc01134ddc53502f54
Nedu Robert <nedumrobert@gmail.com>
2018-04-30 21:14:30 +0100

    wrote endpoint to return a post object with a specified id

--- server.js
+++ server.js
@@ -11,8 +11,25 @@ server.get('/api/posts', (req, res) => {
         res.json(posts);
     })
     .catch(err => {
-        res.status(500).json({ error: "The posts information could not be retrieved."})
+        res.status(500).json({ error: 'The posts information could not be retrieved.'})
     })
 });
+
+server.get('/api/posts/:id', (req, res) => {
+    const id = req.params.id;
+
+    db
+    .findById(id)
+    .then(posts => {
+      if (posts.length < 1) {
+        return res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+      } else {
+        return res.json(posts[0]);
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'The post information could not be retrieved.' });
+    });
+})
 
 server.listen(5000, () => console.log('\n== API Running on port 5000 ==\n'));
