c72105986ff4f944fbd3ae4de54c73faef3a0187
Mark Wong <fmarkwong@gmail.com>
2018-04-30 15:58:56 -0700

    get and post

--- server.js
+++ server.js
@@ -1,5 +1,31 @@
 // import your node modules
 
+const express = require('express');
+
+const server = express();
+
+server.listen('5000', () => console.log('server listening on port 5000'));
 const db = require('./data/db.js');
 
 // add your server code starting here
+
+server.post('/api/posts', (req, res) => {
+  if (!req.query.title || !req.query.contents) {
+    res.status(400).json({ errorMessage: "Please doit provide title and contents for the post." });
+  } else {
+    db
+      .insert({ title: req.query.title, contents: req.query.contents })
+      .then((response) => {
+        let newPost = db.findById(response.id) 
+        debugger
+        return res.status(201).json(newPost);
+      }); 
+  }
+});
+
+server.get('/api/posts', (req, res) => {
+  db
+    .find()
+    .then(posts => res.status(200).json(posts))
+    .catch(err => res.status(500).json({ error: err }));
+});
