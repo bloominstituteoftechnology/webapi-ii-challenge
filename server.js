b48718bbec7f284b46fc10ef1717fd526c611e60
Yasin Shuman <email@yasin.io>
2018-04-30 14:28:40 -0700

    added post route

--- server.js
+++ server.js
@@ -2,8 +2,10 @@
 const express = require("express");
 
 const server = express();
+const bodyParser = require("body-parser");
 const db = require("./data/db.js");
 
+server.use(bodyParser.json());
 // add your server code starting here
 server.get("/api/posts", (req, res) => {
   db
@@ -36,6 +38,25 @@ server.get("/api/posts/:id", (req, res) => {
       res
         .status(500)
         .json({ error: "The post information could not be retrieved." });
+    });
+});
+
+server.post("/api/posts", (req, res) => {
+  const newPost = req.body;
+  if (!newPost.title || !newPost.contents) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    });
+  }
+  db
+    .insert(newPost)
+    .then(post => {
+      res.status(201).json(post);
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "There was an error while saving the post to the database"
+      });
     });
 });
 
