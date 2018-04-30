35d8dbc0dc4e69441b60e02f1e8f3e319ed95aab
Harrison Brock <harrison@harrisonbrock.com>
2018-04-30 17:38:54 -0400

    Added get post by id

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const db = require('./data/db');
 const server = express();
 
 // add your server code starting here
+
+// get all post
 server.get('/api/posts', (req, res) => {
     db.find().then(posts => {
         res.json(posts)
@@ -11,6 +13,23 @@ server.get('/api/posts', (req, res) => {
         console.log(error);
     })
 
+});
+
+server.get('/api/posts/:id', (req, res) => {
+
+    const { id } = req.params;
+    db.findById(id)
+        .then(posts => {
+            if (posts.length) {
+                res.json(posts[0]);
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." });
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ error: "The post information could not be retrieved." });
+        });
 });
 
 server.listen(5000,() => console.log(`API Running on port 5000`));
