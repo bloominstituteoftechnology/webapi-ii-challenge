19385ff201fc3604c5f13ad5c3bcf49338cd3fee
BUDDHAPLEX <ericandrade@gmail.com>
2018-04-30 15:06:33 -0400

    partial set up of server.js file

--- server.js
+++ server.js
@@ -1,5 +1,44 @@
-// import your node modules
+const express = require('express');
 
-const db = require('./data/db.js');
+const db = require('./data/db');
 
-// add your server code starting here
+const server = express();
+
+server.get('/', (req, res) => {
+  res.send('Api running');
+})
+
+// get users
+server.get('/api/users', (req, res) => {
+  // get the users
+  db.find().then(users => {
+    res.json(users);
+  }).catch(err => { 
+    res.status(500).json({ error: err });
+  });
+});
+
+// /api/users/3 (by ID)
+server.get('/api/users/:id', (req, res) => {
+  // grab id from url params
+ const id = req.params.id;
+
+db
+  .findById(id)
+  .then(users => {
+    if (users.length === 0) {
+      res.status(404).json({ message: 'user not found' });
+    } else {
+    res.json(users[0]);
+    }
+  })
+  .catch(err => { 
+    res.status(500).json({ error: err });
+    // do something with err 
+  });
+});
+
+server.listen(5000, () => console.log('\n== API Running on port 5000 ==\n'));
+
+
+
