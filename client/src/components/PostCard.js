f026db2977d99770deb9194be8d100a98adb2210
Devon Bacon <devon.bacon@gmail.com>
2018-05-01 14:07:26 -0700

    Added style to PostCard.js

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -3,10 +3,10 @@ import '../styles/PostCard.css';
 
 const PostCard = props => {
   return (
-    <div className='PostCard'>
-      <p>{props.post.id}</p>
-      <p>{props.post.title}</p>
-      <p>{props.post.contents}</p>
+    <div className="PostCard">
+      <p className="PostCard_id">{props.post.id}</p>
+      <p className="PostCard_title">{props.post.title}</p>
+      <p className="PostCard_contents">{props.post.contents}</p>
     </div>
   )
 }
