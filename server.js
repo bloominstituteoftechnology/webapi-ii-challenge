aee2e99dd1e53276862bdf529150bac65e19d716
Chris <cjmarsh725@gmail.com>
2018-05-01 14:47:23 -0700

    Removed bodyParser

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 // import your node modules
 const express = require('express');
-const bodyParser = require('body-parser');
 const db = require('./data/db.js');
 
 // add your server code starting here
 const server = express();
-server.use(bodyParser.json());
+server.use(express.json());
 
 server.post('/api/posts', (req, res) => {
   if (!('title' in req.body) || !('contents' in req.body)) {
