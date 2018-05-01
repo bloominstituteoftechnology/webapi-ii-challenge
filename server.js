08e412faf2fef3763a8657d4adc1c237654e620b
EricGomez101 <eric_gomez_1@hotmail.com>
2018-05-01 15:53:47 -0700

    completed minimum requirements

--- server.js
+++ server.js
@@ -7,13 +7,55 @@ server.use(bodyParser.json());
 // add your server code starting here
 server.post('/api/posts', (req, res) => {
   const {title, contents} = req.body;
-  console.log(title, contents)
-  console.log(req.body);
+  const post = {title, contents};
+
   if (title === "" || title === undefined || contents === undefined || contents === "") {
     res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
   }
-  res.json({title, contents});
+
+  db.insert(post)
+  .then((response) => {
+    db.findById(response.id)
+      .then(newPost => {
+        res.status(201).json(newPost);
+      })
+  })
+  .catch(err => {
+    res.status(500).json({err:'server error'})
+  })
+});
+
+server.delete('/api/posts/:id', (req, res) => {
+  const {id} = req.params;
+
+  db.findById(id)
+    .then(data => {
+      const post = {...data[0]};
+      db.remove(id)
+        .then(() => {
+          res.status(200).json(post);
+        })
+    })
+    .catch(err => {
+      res.status(500).json({error: "The post could not be removed"})
+    })
+});
+
+server.put('/api/posts/:id', (req, res) => {
+  const {id} = req.params;
+  const update = req.body;
+
+  db.update(id, update)
+    .then(() => {
+      db.findById(id)
+        .then(post => {
+          res.status(200).json(post[0]);
+        })
+    }).catch(err => {
+      res.status(404).send({error: err});
+    })
 })
+
 server.get('/api/posts', (req, res) => {
   db.find()
   .then(posts => {
@@ -21,7 +63,8 @@ server.get('/api/posts', (req, res) => {
   }).catch(err => {
     res.json({error: err});
   })
-})
+});
+
 server.get('/api/posts/:id', (req, res) => {
   const {id} = req.params;
   db.findById(id)
@@ -30,5 +73,6 @@ server.get('/api/posts/:id', (req, res) => {
     }).catch(err => {
       res.json({error: err});
     })
-})
+});
+
 server.listen(5000, () => console.log('server listening on port 5000'))
