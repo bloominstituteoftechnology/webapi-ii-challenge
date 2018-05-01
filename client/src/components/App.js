45f83ecb23fdeef5dccb7d5b5f4a05c6e003e4ea
Devon Bacon <devon.bacon@gmail.com>
2018-05-01 12:25:28 -0700

    Modified react boilerplate

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import logo from '../logo.svg';
+import '../styles/App.css';
 
 class App extends Component {
   render() {
