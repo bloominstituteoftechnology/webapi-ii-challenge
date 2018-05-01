0ff238831ada10bda36fd3ca25c5fac533bca52e
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-05-01 15:29:11 -0700

    Installed helmet & switched from bodyPaser to Express; DELETE/PUT now working

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 // import your node modules
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
+const helmet = require('helmet'); //extra security for FREE
 const db = require('./data/db.js');
 
-// add your server code starting here
 const server = express();
-server.use(bodyParser.json());
 
+//add middleware
+server.use(helmet());
+server.use(express.json());
+// server.use(bodyParser.json());
+
+// add your server code starting here
+//route handlers
 server.get('/', (req, res) => {
     res.send('API running');
 });
+
 
 //PART 1 - GET request to /api/posts
 server.get('/api/posts', (req, res) => {
@@ -23,6 +30,7 @@ server.get('/api/posts', (req, res) => {
     res.status(500).json({ error: 'The posts information could not be retrieved.' });
     });
 });
+
 
 // - GET request to /api/posts/:id
 server.get('/api/posts/:id', (req, res) => {
@@ -46,7 +54,7 @@ server.get('/api/posts/:id', (req, res) => {
 
 //--------------------------------------------------------------------------------
 
-//PART 2 - POST request to /api/posts
+//PART 2 - POST request to /api.posts
 server.post('/api.posts', (req, res) => {
     const{title, contents} = req.body;
     const postNew = {title, contents}
@@ -86,7 +94,25 @@ server.delete('/api/posts/:id', (req, res) => {
 //---------------------------------------------------------------------------------
 
 //PART 4 - PUT request to /api/posts/:id
-
+server.put('/api/posts/:id', (req, res) => {
+    const id = req.params.id;
+    const { title, contents } = req.body;
+    if (!db.findById(id)) { 
+        res.status(404).json({ message: 'The post with the specified ID does not exist.' })
+    }
+    if (req.body.length === 0) {
+        res.status(400).json({ errorMessage: 'Please provide title and contents for the post.' })
+    } else
+    db.update(id, req.body)
+    .then(improve => {
+        res.status(200).json(improve);
+    })
+    //If there's an error in retrieving the post from the database
+    .catch(err => {
+        res.status(500).json({ error: 'The post information could not be modified.' })
+    });
+});
 
 //---------------------------------------------------------------------------------
+
 server.listen(5000, () => console.log('\n== API Running on port 5000 ==\n'));
