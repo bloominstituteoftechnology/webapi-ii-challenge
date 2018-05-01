c69cddf9b786a1763da01aafa3194cac4227cb9c
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-05-01 16:11:16 -0600

    Completed all endpoint specifications

--- server.js
+++ server.js
@@ -1,44 +1,107 @@
 // import your node modules
-
 const express = require('express');
-
+const helmet = require('helmet');
 const db = require('./data/db.js');
 
 // add your server code starting here
 
 const server = express();
 
+server.use(helmet());
+server.use(express.json());
+
+server.post('/api/posts', (req, res) => {
+    const userPost = req.body;
+    console.log('user post', userPost);
+
+    db
+        .insert(userPost)
+        .then(response => {
+            res.status(201).json(response);
+        })
+        .catch(err => {
+            if (err.error === 19) {
+                res.status(400).json({ errorMessage: 'Please provide title and contents for the post' });
+            } else {
+                res.status(500).json({ errorMessage: 'There was an error while saving the post to the database' });
+            }
+        });
+});
+
+server.delete('/api/posts/:id', function (req, res) {
+    const { id } = req.query;
+    let user;
+
+    db
+        .findById(id)
+        .then(foundPost => {
+            user = { ...foundPost[0] };
+        })
+
+    db
+        .remove(id)
+        .then(response => {
+            res.status(204).json(response);
+        })
+
+        .catch(err => {
+            res.status(500).json({ error: err });
+        });
+});
+
+server.put('/api/posts/:id', function (req, res) {
+    const { id } = req.params;
+    const update = req.body;
+
+    db
+        .update(id, update)
+        .then(count => {
+            if (count > 0) {
+                res.status(200).json(users[0]);
+            } else {
+                res.status(404).json({ message: 'The post with the specified ID does not exist' });
+            }
+        })
+        .catch(err => {
+            if (err.error === 19) {
+                res.status(400).json({ errorMessage: 'Please provide title and contents for the post' });
+            } else {
+                res.status(500).json({ error: 'The post information could not be modified' });
+            }
+        });
+});
+
 server.get('/', (req, res) => {
-    res.send('Api running');
+    res.send('Api Running');
 });
 
 server.get('/api/posts', (req, res) => {
 
     db
-    .find()
-    .then(posts => {
-        res.json(posts);
-    })
-    .catch(err => {
-        res.status(500).json({ error: err });
-    });
+        .find()
+        .then(posts => {
+            res.json(posts);
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'The post information could not be retrieved' });
+        });
 });
 
 server.get('/api/posts/:id', (req, res) => {
     const id = req.params.id;
 
     db
-    .findById(id)
-    .then(post => {
-        if (users.length === 0) {
-            res.status(404).json({ message: 'post not found' });
-        } else {
-            res.json(posts[0]);
-        }
-    })
-    .catch(err => {
-        res.status(500).json({ error: err });
-    });
+        .findById(id)
+        .then(post => {
+            if (users.length === 0) {
+                res.status(404).json({ message: 'The post with the specified ID does not exist' });
+            } else {
+                res.json(posts[0]);
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'The post information could not be retrieved' });
+        });
 
 });
 
