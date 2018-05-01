d077044b3e07092c98e00c2fea879b8b0c2e7d12
Rachelle De Jesus Pestanas <rachelle.pestanas@gmail.com>
2018-05-01 16:12:37 -0500

    implemented GET function

--- server.js
+++ server.js
@@ -1,24 +1,48 @@
 // import your node modules
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const db = require('./data/db.js');
 
 // add your server code starting here
 const server = express();
+server.use(bodyParser.json());
 
 server.get('/', (req, res) => {
     res.send('API running');
 });
 
+// GET request to /api/posts
 server.get('/api/posts', (req, res) => {
-//get the posts
  db
   .find()
  .then(posts => {
    res.json(posts);
  })
+
+ // do something with the error
  .catch(err => {
    res.status(500).json({ error: err });
-// do something with the error
  });
 });
+
+// GET request to /api/posts/:id
+server.get('/api/posts/:id', (req, res) => {
+  // grab the id from URL params
+  const id = req.params.id;
+
+  db
+  .findbyID(id)
+  .then(posts => {
+    if (posts.length === 0) {
+      res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+    } else {
+      res.json(posts[0]);
+    }
+  })
+})
+
+// If retrieval error from database
+.catch(err => {
+  res.status(500).json({ error: 'The posts information could not be retrieved.' });
+})
+
