a8561fdf6c69b0673f3a365934ec991116a2c1d4
Henry K <mykim1891@hotmail.com>
2018-04-30 17:32:51 -0700

    Fixed bug on second ver. of PUT

--- server.js
+++ server.js
@@ -65,25 +65,27 @@ server.post("/api/posts", (req, res) => {
 /* TWO ways to use delete */
 
 // server DELETE, find post by id, then all details of the post (content + title) is defined as 'post'.
-// delete the 'post' then return the title and content of post deleted.
+// delete the 'post' then return the title, content, time, and id of the post deleted.
 
 server.delete("/api/posts/:id", (req, res) => {
   const id = req.params.id;
 
   db
-  .findById(id)
-  .then(response => {
-    post = { ...response[0] };
-    db
-      .remove(id)
-      .then(response => {
-        res.status(201).json(post);
-      })
-      .catch(error => {
-        res.status(500).json({ error: "Nothing to delete" });
-      });
-  });
-  db;
+    .findById(id)
+    .then(response => {
+      post = { ...response[0] };
+      db
+        .remove(id)
+        .then(response => {
+          res.status(200).json(post);
+        })
+        .catch(error => {
+          res.status(500).json({ error: "Nothing to delete" });
+        });
+    })
+    .catch(error => {
+      res.status(500).json({ error: "Cannot update the post" });
+    });
 });
 
 // server DELETE, find post by id, delete post.
@@ -102,4 +104,48 @@ server.delete("/api/posts/:id", (req, res) => {
 //     });
 // });
 
-// server.listen(5000, () => console.log("\n== API Running on port 5000 ==\n"));
+/* ALSO TWO ways to use update! */
+
+// server PUT, find post by id, update post by items specified.
+// Postman e.g.: {title: "chnaged title"} will passed along to change title of post.
+// Give boolean respons of 1 or 0 for successful edit.
+
+// server.put("/api/posts/:id", (req, res) => {
+//   const id = req.params.id;
+//   const updatedPost = req.body;
+
+//   db
+//     .update(id, updatedPost)
+//     .then(response => {
+//       res.status(200).json(response);
+//     })
+//     .catch(err => {
+//       res.status(500).json({ error: "Cannot Update" });
+//     });
+// });
+
+// server PUT, find post by id, then all details of the post (content + title) is defined as 'post'.
+// update the 'post' then return the title, content, time, and id of the post updated.
+
+server.put("/api/posts/:id", (req, res) => {
+  const id = req.params.id;
+  const updatedPost = req.body;
+
+  db
+    .update(id, updatedPost)
+    .then(response => {
+      db
+        .findById(id)
+        .then(response => {
+          res.status(200).json(response[0]);
+        })
+        .catch(error => {
+          res.status(500).json({ error: "Cannot update the post" });
+        });
+    })
+    .catch(error => {
+      res.status(500).json({ error: "Cannot update the post" });
+    });
+});
+
+server.listen(5000, () => console.log("\n== API Running on port 5000 ==\n"));
