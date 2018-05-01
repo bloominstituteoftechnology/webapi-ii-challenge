269c51178fb865c503f6164cfc01a017e83f4d2c
Justin Hong <jhong2009@gmail.com>
2018-05-01 14:41:40 -0700

    in App.js: fixed merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from "react";
 import axios from "axios";
 // material components
@@ -51,31 +50,3 @@ class App extends Component {
 }
 
 export default App;
-||||||| merged common ancestors
-=======
-import React, { Component } from "react";
-
-// material components
-import RaisedButton from "material-ui/RaisedButton";
-import logo from "./logo.svg";
-import "./App.css";
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <RaisedButton label="Primary" primary={true} />
-      </div>
-    );
-  }
-}
-
-export default App;
->>>>>>> 17f03c4443eb1e2b6936f1e24ad28b3247c8954b
