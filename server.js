91e92dc332bb44ad4bb8727b5c8ec8588d81187f
markstez05 <36493764+markstez05@users.noreply.github.com>
2018-05-01 18:09:21 -0500

    got put working

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // import your node modules
+
 const express = require('express')
 const server = express()
 const bodyParser = require('body-parser');
@@ -6,24 +7,6 @@ const db = require('./data/db.js');
 server.use(express.json());
 
 // add your server code starting here
-
-
-server.post('/api/posts', (req,res) => {
-    const postData = req.body;
-    console.log('post data', postData)
-    db
-    .insert(postData)
-    .then(response => {
-    res.status(201).json(response)
-    })
-    .catch(err =>{
-        if (err.errno === 19){
-            res.status(400).json({ errorMessage: 'Please provide title and contents for the post.' })
-        } else {
-        res.status(500).json({ error: "There was an error while saving the post to the database" })
-        }
-    })
-})
 
 server.get('/api/posts', (req, res) => {
     db
@@ -72,17 +55,44 @@ server.delete('/api/posts/:id', (req, res) => {
     })
 })
 
-// server.put('/api/posts/:id', (req, res) => {
-//     const { id } = req.params;
-//     const update = req.body;
-//     db
-//     .update(id, update)
-//     .then(posts => {
-//         if (posts.length == 0)
-//         res.status(404).json({ error: 'The post with the specified ID does not exist.'})
-//         if (!title || !contents)
-//     })
+server.put('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    const update = req.body;
+    db
+    .update(id, update)
+    .then(posts => {
+        if (posts.length == 0) {
+        db.findById(id).then(posts => {
+            res.status(200).json(posts[0]);
+        })
+       } else {
+            res.status(404).json({ error: 'The post with the specified ID does not exist.'})
+        }
+        })
+       .catch (err => {
+           if (err.errno === 19) {
+           res.status(400).json({ errorMessage: 'Please provide title and contents for the post.' })
+           } else {
+               res.status(500).json({ error: 'The post information could not be modified.'})
+           }
+       })
+})
 
-// })
+server.post('/api/posts', (req,res) => {
+    const postData = req.body;
+    console.log('post data', postData)
+    db
+    .insert(postData)
+    .then(response => {
+    res.status(201).json(response)
+    })
+    .catch(err =>{
+        if (err.errno === 19){
+            res.status(400).json({ errorMessage: 'Please provide title and contents for the post.' })
+        } else {
+        res.status(500).json({ error: "There was an error while saving the post to the database" })
+        }
+    })
+})
 
 server.listen(5000, () => console.log('\n== API RUNNING ON PORT 5000==\n'));
