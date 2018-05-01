d7af1344ec2bc6e3ae0cf191d7b4459bfdb5ee3d
Justin Rigler <justinmrigler@gmail.com>
2018-05-01 18:58:14 -0400

    postById fixed, can create and delete ports

--- server.js
+++ server.js
@@ -1,14 +1,84 @@
 const express = require('express');
-// import your node modules
+const db = require('./data/db');
 
-const db = require('./data/db.js');
-
-// add your server code starting here
+let bodyParser = require('body-parser');
 const server = express();
 
-// start db
-server.get('/', (req, res) => {
-  res.send('API running');
+// add your server code starting here
+
+// get all post
+server.get('/api/posts', (req, res) => {
+  db
+    .find()
+    .then(posts => {
+      res.json(posts);
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
 
-server.listen(5000, () => console.log('\n== API Running on port 5000 ==\n'));
+server.get('/api/posts/:id', (req, res) => {
+  const { id } = req.params;
+  db
+    .findById(id)
+    .then(posts => {
+      if (posts.length) {
+        res.json(posts[0]);
+      } else {
+        res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist.' });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res
+        .status(500)
+        .json({ error: 'The post information could not be retrieved.' });
+    });
+});
+
+server.post('/api/posts', (req, res) => {
+  const title = req.body.title;
+  const contents = req.body.contents;
+
+  if (!title || !contents) {
+    res.status(400).json({
+      errorMessage: 'Please provide title and contents for the post.'
+    });
+    return;
+  }
+
+  db
+    .insert(req.body)
+    .then(post => {
+      console.log(post);
+      res.status(201).json(req.body);
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: 'There was an error while saving the post to the database'
+      });
+    });
+});
+
+server.delete('/api/posts/:id', (req, res) => {
+  let id = req.params.id;
+  db
+    .remove(id)
+    .then(response => {
+      if (response == 0) {
+        res
+          .status(404)
+          .send({ message: 'The post with the specified ID does not exist.' });
+      } else {
+        res.status(200).send({ message: `User with id ${id} deleted` });
+      }
+    })
+    .catch(error =>
+      res.status(500).send({ error: 'The post could not be removed' })
+    );
+});
+
+server.listen(5000, () => console.log(`API Running on port 5000`));
