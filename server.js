5c90de9a2e2b622ab9f74470d12860d8b19944a8
2940cristian <2940cristian@gmail.com>
2018-05-01 12:47:27 -0700

    Error messages for GET methods and fixed status codes

--- server.js
+++ server.js
@@ -57,9 +57,8 @@ server.get("/api/posts", (req, res) => {
     db.find().then(posts => {
         res.json(posts);
     }).catch(err => {
-        res.statusCode = 500;
-        return res.json({
-            error: "There was an error while saving the post to the database"
+        res.status(500).json({
+            error: "The posts information could not be retrieved."
         })
     })
 })
@@ -67,10 +66,20 @@ server.get("/api/posts", (req, res) => {
 server.get("/api/posts/:id", (req, res) => {
     const id = req.params.id;
 
+
+
     db.findById(id).then(user => {
-        res.json(user)
+        if(user.length>0) {
+            res.json(user)
+        } else {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist." 
+            })
+        }
     }).catch(err => {
-        res.statusCode = 500;
+        res.status(500).json({
+            error: "The post information could not be retrieved."
+        })
     })
 })
 
