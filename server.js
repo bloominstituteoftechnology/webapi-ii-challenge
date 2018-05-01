bb64693ba8b9330337611b160831ee1a653a61f1
cgoez <goez2001@gmail.com>
2018-05-01 09:55:52 -0700

    Setup server get

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
+import { WSAEUSERS } from "constants";
+
 // import your node modules
 
-const db = require('./data/db.js');
+const db = require("./data/db.js");
+const express = require("express");
+const server = express();
 
 // add your server code starting here
+
+server.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+// Get posts
+server.get("/api/posts", (req, res) => {
+  db
+    .find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(error => {
+      res.status(500).json({ error: "Can't get posts!" });
+    });
+});
