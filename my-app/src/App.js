2f8a37f7f4b841f34fb4f29604c67bf73d1d7446
2940cristian <2940cristian@gmail.com>
2018-05-01 15:39:22 -0700

    Added styling for posts

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -36,11 +36,38 @@ class App extends Component {
     axios.delete(`http://localhost:5000/api/posts/${id}`)
     .then(response => {
       alert(`Post ${id} was successfully deleted`)
+      this.fetchData()
     })
     .catch(err => {
       console.log("Error while deleting")
     })
   }
+
+
+  addPost = event => {
+    event.preventDefault();
+    const newPost = {
+      title: this.state.title,
+      contents: this.state.contents
+    }
+
+    axios.post(("http://localhost:5000/api/posts"), newPost)
+      .then(postSend => {
+        this.fetchData();
+      })
+      .catch(err => {
+        console.log("Error sending post")
+      })
+
+      this.setState({
+        title: '',
+        contents: ''
+      })
+  }
+
+  handleInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
 
 
@@ -50,20 +77,32 @@ class App extends Component {
       {this.state.mounted === false ? (
         <div>Fetching....</div>
       ) : (
-        <ul>
-          {this.state.posts.map(post => {
-            return(
-              <li key={post.id} className="card">
-                <h2>{post.title}</h2>
-                <p>- {post.contents}</p>
-                <button onClick={() => {
-                  this.delete(post.id)
-                }}>Delete</button>
-              </li>
-            )
-          })}
-        </ul>
+        <div>
+          <div className="banner">
+            Lambda Posts
+          </div>
+          <ul>
+            {this.state.posts.map(post => {
+              return(
+                <li key={post.id} className="card">
+                  <h2>{post.title}</h2>
+                  <p>- {post.contents}</p>
+                  <button onClick={() => {
+                    this.delete(post.id)
+                  }}>Delete</button>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
       )}
+      <div className="form">
+        <form onSubmit={this.addPost}>
+              <input onChange={this.handleInputChange} name="title" placeholder="Title" type="text"/>
+              <input onChange={this.handleInputChange} name="contents" placeholder="Content" type="text"/>
+              <button type="submit">Submit</button>
+          </form>
+      </div>
       </div>
     );
   }
