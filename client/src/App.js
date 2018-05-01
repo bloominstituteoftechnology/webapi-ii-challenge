c9e4b6b8ec84d16075b7cccc4af36f94b8285ccd
Natalie Schennum <natalieschennum@gmail.com>
2018-05-01 13:38:20 -0700

    Styled Much More

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,36 @@ import './App.css';
 import axios from 'axios';
 import styled from 'styled-components';
 
-const StyledDiv = styled.div``;
-const StyledH3 = styled.h3``;
+const StyledDiv = styled.div`
+width: 600px; 
+height: 100px; 
+border: 2px solid black;
+padding: 5px; 
+margin: 0 auto; 
+margin-bottom: 10px;  
+background: lightyellow; 
+box-shadow: 3px 5px; 
+&:hover{
+  background: lightblue; 
+  box-shadow: none; 
+}
+`;
+const Blockquote = styled.blockquote`
+font-style: italic;  
+`;
+const HeaderH1 = styled.h1`
+border: 2px solid black; 
+margin-top: 10px; 
+margin-bottom: 20px; 
+box-shadow: 5px 10px; 
+background: lightyellow; 
+&:hover{
+  background: lightblue; 
+}
+
+display: inline-block; 
+padding: 15px; 
+`; 
 
 class App extends Component {
   constructor(props) {
@@ -33,10 +61,11 @@ class App extends Component {
     return (
       <div className="App">
         <div className="app-container">
+        <HeaderH1>Node Posts</HeaderH1>
           {this.state.posts.map(post => (
             <StyledDiv>
-              <StyledH3>{post.title} </StyledH3>
-              <p>{post.contents} </p>
+              <Blockquote>{post.title} </Blockquote>
+              <p>~{post.contents} </p>
             </StyledDiv>
           ))}
         </div>
