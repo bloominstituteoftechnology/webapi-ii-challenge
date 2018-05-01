ee05bd00bc97f0c542baf20683c62ef10803674f
SyCoDe <SyCoDe@gmail.com>
2018-05-01 16:48:20 -0600

    added delete, working on stretch

--- server.js
+++ server.js
@@ -1,20 +1,45 @@
 // import your node modules
-const express = require('express');
-const db = require('./data/db.js');
-const bodyParser = require("body-parser");
+const express = require("express");
+// const helmet = require('helmet');
+const db = require("./data/db.js");
+const fs = require("fs"); //
+// const bodyParser = require("body-parser");   //this is an external npm module to parse, unnecessary since express has built in parser
 
 const server = express();
 
+// middleware
+server.use(express.json()); //built in Express parser 
+// server.use(helmet()); //security module
+// server.use(bodyParser.json());   //this is an external npm module to parse
+
+// route handlers   (Note routing is just middleware)
+server.post("/api/users", (req, res) => {
+  const userInfo = req.body;
+  console.log("user Info", userInfo);
+
+  db
+  .insert(userInfo)
+  .then(response => {
+    res.status(201).json(response);
+  })
+  .catch(err => {
+    res.status(500).json({error: err});
+  });
+});
+
+
+// http://foo.com?search=bar&sort=asc
+// req.query === { search: 'bar', sort: 'asc'}
 
 // add your server code starting here
-server.use(bodyParser.json());
-
+//testing server setup
 server.get("/", (req, res) => {
   res.send("Api running");
 });
 
+//get the posts
 server.get("/api/posts", (req, res) => {
-  //get the posts
+  // fs.readFile('file', 'filetype you want it to be read as'); //use if getting
   db
     .find()
     .then(posts => {
@@ -24,7 +49,6 @@ server.get("/api/posts", (req, res) => {
       res
         .status(500)
         .json({ error: "The posts information could not be retrieved." });
-      // do something with the error
     });
 });
 
@@ -43,7 +67,6 @@ server.get("/api/posts/:id", (req, res) => {
       }
     })
     .catch(err => {
-      // do something with the error
       res.status(500).json({ error: err });
     });
 });
@@ -69,5 +92,45 @@ server.post("/api/posts", (req, res) => {
   }
 });
 
+server.delete('/api/users', function(req, res) {
+  const { id } = req.query;
+  let user;
+  db
+    .findById(id)
+    .then(foundUser => {
+      user = { ...foundUser[0] };
+
+      db.remove(id).then(response => {
+        res.status(200).json(user);
+      });
+    })
+    .catch(err => {
+      res.status(500).json({ erro: err });
+    });
+});
+
+server.put('/api/users/:id', function(req, res) {
+  const { id } = req.params;
+  const update = req.body;
+
+  db
+    .update(id, update)
+    .then(count => {
+      if (count > 0) {
+        db.findById(id).then(users => {
+          res.status(200).json(users[0]);
+        });
+      } else {
+        res.status(404).json({ msg: 'user not found' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+
 let port = 8999;
-server.listen(port, ()=> console.log('\n== API Running on port ' + port + ' =='));
+server.listen(port, () =>
+  console.log("\n== API Running on port " + port + " ==")
+);
