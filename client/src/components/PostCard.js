676ad18122a327d979e26fbbbe66ef6bd84daf55
Devon Bacon <devon.bacon@gmail.com>
2018-05-01 13:06:06 -0700

    Cleaned up PostCard.js and made sure proper data was displayed

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -2,12 +2,11 @@ import React from 'react';
 import '../styles/PostCard.css';
 
 const PostCard = props => {
-  console.log(props);
   return (
     <div className='PostCard'>
       <p>{props.post.id}</p>
       <p>{props.post.title}</p>
-      <p>{props.post.content}</p>
+      <p>{props.post.contents}</p>
     </div>
   )
 }
