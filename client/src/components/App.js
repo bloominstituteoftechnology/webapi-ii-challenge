e61e54c5d8493e030d8a3bab92e33fba0bc14e1a
Devon Bacon <devon.bacon@gmail.com>
2018-05-01 12:54:54 -0700

    Pulled data from server and passed down to PostsList component in App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+
+import PostsList from './PostsList';
 import logo from '../logo.svg';
 import '../styles/App.css';
 
 class App extends Component {
+  state = { posts: [] };
+
+  componentDidMount() {
+    this.getPosts();
+  }
+
+  getPosts() {
+    axios.get('http://localhost:5000/api/posts')
+      .then(res => {
+        this.setState({ posts: res.data });
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
   render() {
     return (
       <div className="App">
@@ -14,6 +32,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <PostsList posts={this.state.posts}/>
       </div>
     );
   }
