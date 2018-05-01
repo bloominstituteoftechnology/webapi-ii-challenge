52fe3eef45c79f27c2deaa5c6381d9eab1dcb8c6
faulkenfett11 <36997363+faulkenfett11@users.noreply.github.com>
2018-05-01 12:49:07 -0700

    api done: post, get, delete, and put all functional

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
 const express = require('express');
+const helmet = require('helmet');
 
 const db = require('./data/db.js');
 
 const server = express();
 
+server.use(express.json());
+server.use(helmet());
 
 server.get('/', (req, res) => {
     res.send('API running');
 });
 
 server.post('/api/posts', (req, res) => {
-    let title = req.body;
-    let contents = req.body;
+    const { title, contents } = req.body;
     const id = req.params.id;
     const post = { title, contents, id };
     db
         .insert(post)
         .then(post => {
-            res.json(post);
-            res.status(201);
+            res.status(201).json(post);
         })
         .catch(err => { 
             res.status(500).json({ error: err });
@@ -72,5 +73,25 @@ server.delete('/api/posts/:id', (req, res) => {
             res.status(500).json({ error: "The post could not be removed." });
         });
 })
+
+server.put('/api/posts/:id', (req, res) => {
+    const id = req.params.id;
+    const { title, contents } = req.body;
+        db
+            .update( id, req.body)
+            .then(post => {
+                if (post.length === 0) {
+                    res.status(404).json({ message: 'The post with the specified ID does not exist.' })
+                } else {
+                    if ( !title || !contents ) {
+                        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+                    } else {
+                    res.status(200).json(post);
+            }}})
+        .catch(err => { 
+            res.status(500).json({ error: "The post information could not be modified." });
+        });
+})
+
 
 server.listen(5000, () => console.log('\n== API running on port 5000 ==\n'));
