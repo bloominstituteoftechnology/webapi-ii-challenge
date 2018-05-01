dc5fac39bd103a323c545c8ab80531268d3ab163
Devon Bacon <devon.bacon@gmail.com>
2018-05-01 12:56:57 -0700

    Used props passed to PoastCard to display data for each post

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import '../styles/PostCard.css';
 
 const PostCard = props => {
+  console.log(props);
   return (
-    <div>
-
+    <div className='PostCard'>
+      <p>{props.post.id}</p>
+      <p>{props.post.title}</p>
+      <p>{props.post.content}</p>
     </div>
   )
 }
