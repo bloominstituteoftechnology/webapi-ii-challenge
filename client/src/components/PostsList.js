f3825bc519682f95288e4a3fde1a4e2ac66490a1
Devon Bacon <devon.bacon@gmail.com>
2018-05-01 12:56:13 -0700

    Mapped through posts data from App.js and made a PostCard from each

--- client/src/components/PostsList.js
+++ client/src/components/PostsList.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import PostCard from './PostCard';
+import '../styles/PostsList.css';
+
+
 const PostsList = props => {
   return (
-    <div>
-      
+    <div className='PostsList'>
+      {props.posts.map(post => (
+        <PostCard post={post} />
+      ))}
     </div>
   )
 }
