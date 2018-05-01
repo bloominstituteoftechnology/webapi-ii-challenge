80bc44fd7912a447848f2cc5f130bc9ad6fa2f78
Devon Bacon <devon.bacon@gmail.com>
2018-05-01 14:51:49 -0700

    Finished basic styles for PostCards and added delete button

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -4,9 +4,15 @@ import '../styles/PostCard.css';
 const PostCard = props => {
   return (
     <div className="PostCard">
-      <p className="PostCard_id">{props.post.id}</p>
-      <p className="PostCard_title">{props.post.title}</p>
-      <p className="PostCard_contents">{props.post.contents}</p>
+      <div className="PostCard_header">
+        <p className="PostCard_id">{props.post.id}</p>
+        <p className="PostCard_delete"
+          onClick={() => props.delete(props.post.id)}>x</p>
+      </div>
+      <div className="PostCard_body">
+        <p className="PostCard_title">{props.post.title}</p>
+        <p className="PostCard_contents">{props.post.contents}</p>
+      </div>
     </div>
   )
 }
