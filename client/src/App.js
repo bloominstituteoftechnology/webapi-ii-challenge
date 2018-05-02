292830ba843482189d3cb589a70e4d93a24ede0f
Nedu Robert <nedumrobert@gmail.com>
2018-05-02 15:27:26 +0100

    added App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,84 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
+import axios from 'axios';
+
 import './App.css';
+import Post from './components/Post/Post';
+import Posts from './components/Posts/Posts';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+      title: '',
+      contents: ''
+    };
+  }
+
+  componentDidMount() {
+    this.getPosts();
+  }
+
+  getPosts = () => {
+    axios
+      .get('http://localhost:5000/api/posts')
+      .then(res => {
+        console.log(res);
+        this.setState({ posts: res.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  handleNewPost = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  addPost = e => {
+    const post = {
+      title: this.state.title,
+      contents: this.state.contents,
+    };
+    axios
+      .post('http://localhost:5000/api/posts', post)
+      .then(savedPost => {
+        console.log(savedPost);
+        this.setState({ title: '', contents: '' });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Route
+          exact
+          path="/posts"
+          render={props => (
+            <Posts
+              {...props}
+              posts={this.state.posts}
+              handleNewPost={this.handleNewPost}
+              addPost={this.addPost}
+              state={this.state}
+            />
+          )}
+        />
+        <Route
+          exact
+          path="/posts/:id"
+          render={props => (
+            <Post
+              {...props}
+              posts={this.state.posts}
+              getPosts={this.getPosts}
+            />
+          )}
+        />
       </div>
     );
   }
