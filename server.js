61125a1c6eb743e82d59c892f1f6df7ae3a8efbc
Sean Valdivia <irishseanvaldivia@gmail.com>
2018-05-17 14:18:42 -0500

    Starting on the server routes.

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ const db = require('./data/db');
 const server = express();
 
 server.listen(5000, () => {
-    console.log('===SERVER RUNNING ON PORT 5000');
+    console.log('===SERVER RUNNING ON PORT 5000===');
+})
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+// okay now, just like in the lecture video, let's make our server routes:
+
+server.get('/', () => {
+    console.log('<h2>GET REQUEST RECEIVED</h2>');
 })
 
