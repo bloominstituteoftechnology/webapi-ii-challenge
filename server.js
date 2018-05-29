61bb95e9c7d2109b25b0579a15f2949da0827dcf
maxwashington <mnwashington18@gmail.com>
2018-05-29 16:14:38 -0500

    delete post added

--- server.js
+++ server.js
@@ -21,7 +21,8 @@ server.get("/api/posts", (req, res) => {
     });
 });
 
-server.get("/api/post/:id", (req, res) => {
+server.get("/api/posts/:id", (req, res) => {
+  const { id } = req.param;
   db
     .findById(id)
     .than(post => {
@@ -35,6 +36,42 @@ server.get("/api/post/:id", (req, res) => {
       res
         .status(500)
         .json({ message: "The post with that specified ID does not exist." });
+    });
+});
+
+server.post("/api/posts", (req, res) => {
+  const { title, contents } = req.body;
+  if (title && contents) {
+    const obj = db
+      .insert({ title, contents })
+      .then(response => {
+        res.status(201).json({ id: response.id, title, contents });
+      })
+      .catch(error => {
+        res.status(500).json({ message: error });
+      });
+  } else {
+    res.status(400).json({
+      message: "Insert a title and contents for the post"
+    });
+  }
+});
+
+server.delete("/api/posts/:id", (req, res) => {
+  const { id } = req.params;
+  db
+    .remove(id)
+    .then(response => {
+      if (response != 0) {
+        res.status(200).json({ response: "Post Deleted" });
+      } else {
+        res.status(404).json({
+          errorMessage: "The post does not exist."
+        });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: "The post could not be deleted" });
     });
 });
 
