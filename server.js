ec2696dfa0719b954c5d7503aa22f55bc06d5873
maxwashington <mnwashington18@gmail.com>
2018-05-29 15:36:20 -0500

    Post by id created

--- server.js
+++ server.js
@@ -1,37 +1,41 @@
 // import your node modules
 
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 const express = require("express");
 const db = require("./data/db");
 const server = express();
 const port = 5000;
 
-app.use(bodyParser.json());
+server.use(bodyParser.json());
 
 // add your server code starting here
 
-server.use(express.json());
-server.get("/", (req, res) => {
-  res.json("Hello from express");
+server.get("/api/posts", (req, res) => {
+  db
+    .find()
+    .than(response => {
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      res.status(500).json({ errormsg: "This post could not be found" });
+    });
 });
 
-
-server.get("/api/user/:id", (req, res) => {
+server.get("/api/post/:id", (req, res) => {
   db
     .findById(id)
-    .than(user => {
-      if (user.length > 0) {
-        res.status(200).json({ users });
+    .than(post => {
+      if (post.length > 0) {
+        res.status(200).json({ post });
       } else {
-        res.status(404).json({ msg: "No user found" });
+        res.status(404).json({ msg: "No post found" });
       }
     })
     .catch(error => {
       res
         .status(500)
-        .json({ message: "The user with the specified ID does not exist." });
+        .json({ message: "The post with that specified ID does not exist." });
     });
 });
 
 server.listen(port, () => console.log(`Server is running on port ${port}`));
-
