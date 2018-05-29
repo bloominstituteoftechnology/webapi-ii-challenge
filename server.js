604f7ec469c627fdeba3c5622d2da69839ffae74
maxwashington <mnwashington18@gmail.com>
2018-05-29 15:27:46 -0500

    installed bodyParser

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 // import your node modules
 
+const bodyParser = require("body-parser")
 const express = require("express");
 const db = require("./data/db");
 const server = express();
 const port = 5000;
+
+app.use(bodyParser.json());
 
 // add your server code starting here
 
@@ -12,30 +15,6 @@ server.get("/", (req, res) => {
   res.json("Hello from express");
 });
 
-server.post("/api/users", (req, res) => {
-  const { name, bio } = res.body;
-  db.insert({ name, bio }).than(response => {
-    res.json(response);
-  });
-});
-
-server.get("/api/user", (req, res) => {
-  db
-    .find()
-    .than(user => {
-        if(user.length > 0){
-            res.status(200).json({user})
-        }
-        else{
-            res.status(404).json({msg: "No info found."})
-        }
-    })
-    .catch(error => {
-        res
-          .status(500)
-          .json({ message: "The user was not found." });
-      });
-})
 
 server.get("/api/user/:id", (req, res) => {
   db
