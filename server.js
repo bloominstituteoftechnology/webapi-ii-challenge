04cc3c66416124d8495cf2f18b043d7349f5ccbd
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-05-29 17:12:25 -0400

    Finished GET request

--- server.js
+++ server.js
@@ -11,7 +11,14 @@ server.post('/api/posts', (req, res) => {
 });
 
 server.get('/api/posts', (req, res) => {
-
+  db
+    .find()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(error => {
+      res.status().json({ error: 'The posts information could not be retrieved.' })
+    });
 });
 
 server.get('/api/posts/:id', (req, res) => {
