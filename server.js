afa21460c145f71729b4b580bae078698d981088
mister-corn <code@ronaldlibago.com>
2018-05-29 14:37:02 -0700

    CRUD Implemented. Bug Testing. Making Page to Present Data.

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const server = express();
 const db = require('./data/db.js');
 // Middleware Dependencies
 const helmet = require('helmet');
-
+const cors = require('cors');
 // add your server code starting here
 
 // Middleware
+server.use(cors());
 server.use(helmet());
 server.use(express.json());
 
@@ -56,6 +57,60 @@ server.get('/api/posts/:id', (req, res) => {
     })
 })
 
+// DELETE
+server.delete('/api/posts/:id', (req, res) => {
+  const { id } = req.params;
+
+  db.findById(id)
+    .then(post => {
+      if (post.length === 0) {
+        res.status(404).json({ message: 'The post with the specified ID does not exist.'});
+      } else {
+        db.remove(id)
+          .then(count => {
+          if (count === 1) {
+            res.json(post);
+          } else if ( count === 0) {
+            res.status(500).json({ message: "The post could not be removed" });
+          } else {
+            res.status(500).json({ message: "Serious Database Error. Contact administrator."});
+          }
+        })
+        .catch(err => res.status(500).json({ error: "The post could not be removed" }));
+      }
+    })
+})
+
+// PUT
+server.put('/api/posts/:id', (req, res) => {
+  // Variables
+  const { id } = req.params;
+  const postInfo = req.body;
+  console.log("'/api/users/:id' PUT postInfo:",postInfo,"id:",id);
+  // Operation
+  db.update(id, postInfo)
+    .then(count => {
+      console.log("'/api/users/:id' PUT count",count);
+      if (count === 1) {
+        db.findById(id)
+          .then(post => res.json(post))
+          .catch(err => res.status(500).json({ error: "Post update successful, but could not retrieve record." }));
+        } else if ( count === 0) {
+          res.status(404).json({ message: "The post with the specified ID does not exist." });
+        } else {
+          res.status(500).json({ message: "Serious Database Error. Contact administrator."});
+        }
+    })
+    .catch(err => {
+      console.log("'/api/users/:id' PUT error:",err);
+      if (err.errno === 19) {
+        res.status(400).json({ errorMessage: "Please provide BOTH title and content for the post." });
+      } else {
+        res.status(500).json({ error: "The post information could not be modified." });
+      }
+  });
+})
+
 // Server Start!
-const port = 5001;
+const port = 5000;
 server.listen(port, () => console.log(`\n=== Server Listening on Port ${port} ===`));
