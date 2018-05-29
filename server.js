2b0bc4b1befd0cbeb9489347b835f202e07f3690
NandoTheessen <nando.theessen@gmail.com>
2018-05-29 22:19:39 +0200

    Added dependencies for express & cors, setup server

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 // import your node modules
-
-const db = require('./data/db.js');
+const express = require('express')
+const server = express()
+const db = require('./data/db')
+const cors = require('cors')
 
 // add your server code starting here
+server.use(cors())
+server.use(express.json())
+
