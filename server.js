faf30d91136f2c0d0f43894f39b7cf0ce7a7d659
JSNavey <jsnavey@gmail.com>
2018-05-29 17:53:20 -0400

    initial commit - installed dependencies

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 // import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
 
 // add your server code starting here
+const port = 5000;
+const server = express();
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send('Test Test :)');
+});
+
+server.listen(port, () => console.log(`Server is running on port ${port}`));
