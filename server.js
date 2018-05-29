e662cb2afef63bc82bb2b81c7858db111722aa66
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-05-29 17:01:16 -0400

    Block out endpoints

--- server.js
+++ server.js
@@ -6,4 +6,24 @@ const port = 5000;
 
 server.use(express.json());
 
+server.post('/api/posts', (req, res) => {
+
+});
+
+server.get('/api/posts', (res, req) => {
+
+});
+
+server.get('/api/posts/:id', (res, req) => {
+
+});
+
+server.delete('/api/posts/:id', (res, req) => {
+
+});
+
+server.put('/api/posts/:id', (res, req) => {
+  
+});
+
 server.listen(port, () => console.log(`Server running on port ${port}`));
