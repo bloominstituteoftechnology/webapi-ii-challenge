ceef3522799f302f14af8dc5521a38a68576b598
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-05-29 17:16:58 -0400

    Finished GET request for a specified id

--- server.js
+++ server.js
@@ -22,7 +22,20 @@ server.get('/api/posts', (req, res) => {
 });
 
 server.get('/api/posts/:id', (req, res) => {
+  const { id } = req.params;
 
+  db
+    .findById(id)
+    .then(post => {
+      if(post.length === 0){
+        res.status(404).json({ error: 'The post with the specified ID does not exist.' });
+      } else {
+        res.status(200).json(post);
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'The post information could not be retrieved.' });
+    });
 });
 
 server.delete('/api/posts/:id', (req, res) => {
