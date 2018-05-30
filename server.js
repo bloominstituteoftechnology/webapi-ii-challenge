7678a87f417695a2aff564161bc2483f16f830d1
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-05-30 02:24:00 -0500

    implemented put request

--- server.js
+++ server.js
@@ -74,6 +74,30 @@ server.delete('/api/posts/:id', (req, res) => {
         })
 })
 
+// PUT REQUEST
+server.put('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    const { title, contents } = req.body;
+
+    db
+        .update(id, {title, contents})
+        .then(response => {
+            // console.log(response)
+            if (response === 0) {
+                res.status(404).json({ message: "The post with the specified ID does not exist." });
+            }
+
+            if (!title || !contents) {
+                res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+            }
+            
+            res.status(200).json(response)
+        })
+        .catch(error => {
+            res.status(500).json({ error: "The post information could not be modified." })
+        })
+})
+
 
 
 
