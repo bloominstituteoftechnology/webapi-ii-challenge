a92472a7459883ee5d13c92668e056fbf4c36df9
Cassandra Lamendola <cass.lamendola@gmail.com>
2018-05-30 14:10:26 -0700

    connect api with react app to show list of posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5555/api/posts')
+      .then(res => {
+        console.log(res.data)
+        this.setState({posts: res.data.users});
+        console.log(this.state)
+      })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Lambda Blog</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Posts:
         </p>
+        {this.state.posts.map(post => {
+          return(
+            <div className="postCard" key={post.id}>
+              <h2 className="postTitle">{post.title}</h2>
+              <p>{post.contents}</p>
+            </div>
+          )
+        })}
       </div>
     );
   }
