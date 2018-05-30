6f066cafe36f6810f1c759752b2279d02f3d8e63
Zach Campbell <beazlebubbles@gmail.com>
2018-05-30 13:39:20 -0600

    Installed dependencies Basic server setup

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
-// import your node modules
-
+const express = require('express');
+const cors = require('cors');
 const db = require('./data/db.js');
 
-// add your server code starting here
+const port = 5555;
+const server = express();
+server.use(express.json());
+server.use(cors({ origin: 'http://localhost:3000' }));
+
+
+
+server.listen(port, () => console.log(`Server running on port ${port}`));
