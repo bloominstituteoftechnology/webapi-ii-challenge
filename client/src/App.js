1f0b51f8b11af265fd9a490cea797886c4b7bfe8
kelly <kelfro@fastmail.com>
2018-05-30 15:15:14 -0700

    React app shows list of posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
-  render() {
+	constructor(props) {
+		super(props);
+		this.state= {
+			posts:[]
+		};
+	}
+
+componentDidMount() {
+	this.gatherPosts();
+}
+
+gatherPosts = () => {
+	axios.get('http://localhost:5555/api/posts')
+		.then(response => {
+			console.log (response)
+			this.setState({ posts: response.data.posts });
+		})
+		.catch(error => console.log(error));
+}
+	
+render() {
     return (
       <div className="App">
         <header className="App-header">
@@ -12,7 +33,16 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+	</p>
+	<div>
+		{this.state.posts.map(post => {
+			return (
+				<ul>
+					<li>{post.title}: {post.contents}</li>
+				</ul>
+			);
+		})}
+	</div>    
       </div>
     );
   }
