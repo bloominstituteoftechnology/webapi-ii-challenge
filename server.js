01d50eefe0d82a17b10bd78247375bd6d214f4b0
Brandon Hopper <BrandonHopper0@gmail.com>
2018-05-29 20:08:46 -0400

    -added express to dependencies -imported express and declared port -server listening on 5001

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 // import your node modules
-
-const db = require('./data/db.js');
+// importing express
+const express = require('express');
+const db      = require('./data/db.js');
 
 // add your server code starting here
+const port   = 5001;
+const server = express();
+server.use(express.json());
+
+
+// calling serving to listen to traffic 
+server.listen(port, () => console.log(`Server running on port ${port}`));
