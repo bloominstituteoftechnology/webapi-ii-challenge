4ee18a60f4692b28baf80f318e9dd5440ef2a80e
Andrew Kolumbic <andrewkolumbic@Andrews-Mac-mini.local>
2018-05-30 12:26:55 -0700

    initial commit

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 // import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
+const cors = require('cors');
 
+const port = 5555;
+const server = express();
+server.use(express.json());
+server.use(cors());
 // add your server code starting here
+
+server.listen(port, () => console.log(`Server running on ${port}`));
