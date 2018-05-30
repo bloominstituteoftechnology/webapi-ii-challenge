b735da469b59b5405fe683a99cfca0cb9390cc44
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-05-30 01:48:47 -0500

    implemented get request

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const server = express();
 server.use(express.json());
 
 // server code here
+
+//POST REQUEST
 server.post('/api/posts', (req, res) => {
     const { title, contents } = req.body
     if (!title || !contents) {
@@ -20,7 +22,19 @@ server.post('/api/posts', (req, res) => {
         .catch(error => {
             res.status(500).json({ error: "There was an error while saving the post to the database" });
         })
-})
+});
+
+// GET REQUEST
+server.get('/api/posts', (req, res) => {
+    db
+        .find()
+        .then(posts => {
+            res.status(201).json(posts);
+        })
+        .catch(error => {
+            res.status(500).json({ error: "The posts information could not be retrieved." });
+        })
+});
 
 
 
