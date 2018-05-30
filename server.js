5ff5bf89ea8f9c9ff0c965d82d141f0b91c4b9fa
jesuarva <jean.ariza@jesuarva.com>
2018-05-30 23:50:56 +0200

    ADD POST
    DONE:
    1. Create a midle wear to deal with the validation os the data to POST
    2. If the request body is missing the title or contents property:
    	a. cancel the request.
    	b. respond with HTTP status code 400 (Bad Request).
    	c. return the following JSON response: { errorMessage: "Please provide title and contents for the post." }.

--- server.js
+++ server.js
@@ -59,4 +59,29 @@ server.get(`${url}/:id`, (req, res) => {
     });
 });
 
+// Midlewear for POST endpoint
+function contentValid (req, res, next) {
+  const { title, contents } = req.body;
+  if (!title || !contents) {
+    res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+  } else {
+    next();
+  }
+}
+server.post(url, contentValid, (req, res) => {
+  const { title, contents } = req.body;
+  console.log("next",title, contents);
+
+
+  db
+    .insert({ title, contents })
+    .then(response => {
+      console.log("response", response);
+    })
+    .catch(e => {
+      console.log("error", e);
+      res.status(500).json( { error: "There was an error while saving the post to the database" });
+    });
+});
+
 server.listen(port, () => console.log("Server running on port %s ", port));
