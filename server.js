82cf13541a1b73e8f65982f56d54d2373946e35b
Jacob Henry <jacobiswinning@gmail.com>
2018-05-30 14:40:09 -0600

    added put, get, delete working on error messages

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const db = require('./data/db.js')
 const server = express()
+// const nodemon = require('nodemon')
 
 server.use(express.json())
 
@@ -10,6 +11,14 @@ const cors = require('cors')
 
 const port = 5000
 // add your server code starting here
+
+// methods
+const errorMessage = (status, message, res) => {
+  res.status(status).json({
+    errorMessage: message
+  })
+  return;
+}
 
 server.get('/', (req, res) => {
   res.send('server is running...')
@@ -42,9 +51,75 @@ server.get('/api/posts', (req, res) => {
       });
     })
     .catch(error => {
-      res.json({
-        error
-      });
+      errorMessage(500, 'The post could not be found!', res)
+    });
+});
+
+server.delete('/api/posts/:id', (req, res) => {
+  const {
+    id
+  } = req.params;
+  let post;
+
+  db
+    .findById(id)
+    .then(response => {
+      post = { ...response[0]
+      };
+
+      db.remove(id)
+        .then(response => {
+          res.status(200).json(post);
+        })
+        .catch(error => {
+          res.status(500).json(error);
+        });
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+})
+
+server.get('/api/posts/:id', (req, res) => {
+  const {
+    id
+  } = req.params;
+
+  db
+    .findById(id)
+    .then(posts => {
+      res.json(posts[0]);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+server.put('/api/posts/:id', (req, res) => {
+
+  const {
+    id
+  } = req.params;
+  const update = req.body;
+
+  db
+    .update(id, update)
+    .then(count => {
+      if (count > 0) {
+        db.findById(id).then(updatedPosts => {
+          res.status(200).json(updatedPosts[0]);
+        });
+
+      } else {
+        res
+          .status(404)
+          .json({
+            message: 'The post with the specified ID does not exist.'
+          });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
     });
 });
 
