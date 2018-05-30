0a8746e897736553bd11ced8e076afcde48158a6
kammagik <kammagik@yahoo.com>
2018-05-30 16:29:29 -0700

    added dependencies

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
 
 // add your server code starting here
 const port = 5000;
+const server = express();
