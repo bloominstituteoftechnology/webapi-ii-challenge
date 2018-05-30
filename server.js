d5983dff48cf203d36c5a764cd792e7afabd5920
bbenefield89 <bsquared18@gmail.com>
2018-05-30 17:27:33 -0400

    feat(server.js): GET/DELETE/PUT:`/api/posts/:id`

--- server.js
+++ server.js
@@ -36,4 +36,51 @@ app.post('/api/posts', (req, res) => {
     .catch(err => error(res, 500, "There was an error while saving the post to the database"));
 });
 
+/*************************
+** ROUTE: `/posts/api/:id` **
+*************************/
+// get
+app.get('/api/posts/:id', (req, res) => {
+  const { id } = req.params;
+  db.findById(id)
+    .then(data => {
+      if (data.length === 0) {
+        return error(res, 404, "The post with the specified ID does not exist.");
+      }
+      res.json(data);
+    })
+    .catch(err => error(res, 500, "The post information could not be retrieved."));
+});
+
+// delete
+app.delete('/api/posts/:id', (req, res) => {
+  const { id } = req.params;
+  db.remove(id)
+    .then(data => {
+      if (data.length === 0) {
+        return error(res, 404, "The post with the specified ID does not exist.");
+      }
+      res.json(data);
+    })
+    .catch(err => error(res, 500, "The post could not be removed"));
+});
+
+// put
+app.put('/api/posts/:id', (req, res) => {
+  const { id } = req.params;
+  const { title, contents } = req.body;
+  const post = { title, contents };
+  db.update(id, post)
+    .then(data => {
+      if (data === 0) {
+        return error(res, 404, "The post with the specified ID does not exist.");
+      }
+      if (title === undefined || contents === undefined) {
+        return error(res, 400, "Please provide title and contents for the post.");
+      }
+      res.json(data);
+    })
+    .catch(err => error(res, 500, "The post information could not be modified."));
+});
+
 app.listen(port, () => console.log(`Server is listening on PORT: ${ port }`));
