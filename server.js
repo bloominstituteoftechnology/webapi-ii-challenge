a5610a3accbce5382a12bdb5044dd4729e5f2cdf
udykas <udykas@gmail.com>
2018-05-30 16:42:21 -0400

    can successfully add a new post and can view individual posts by id. Errors work too

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ const server = express();
 server.use(express.json());
 server.use(cors({ origin: 'http://localhost:3000' }));
 
+const sendUserError = (status, message, res) => {
+    res.status(status).json({ errorMessage: message });
+    return;
+}
+
 // add your server code starting here
 
 server.get('/', (req, res) => {
@@ -21,8 +26,39 @@ server.get('/api/posts', (req, res) => {
         res.json({ posts })
     })
     .catch(err => {
-        res.status(500).json({ error: "The posts information could not be retrieved." })
+        sendUserError(500, "The posts information could not be retrieved.", res)
     })
+})
+
+server.post('/api/posts', (req, res) => {
+    const { title, contents } = req.body;
+    if(!title || !contents){
+        sendUserError(400, "Please provide title and contents for the post.", res);
+    }
+    db
+        .insert({ title, contents })
+        .then(response => {
+            res.json(response);
+        })
+        .catch(err => {
+            sendUserError(500, "There was an error while saving the post to the database", res);
+        })
+})
+
+server.get('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    db
+        .findById(id)
+        .then(post => {
+            if(post.length === 0) {
+                sendUserError(404, "The post with the specified ID does not exist.", res);
+                return;
+            }
+            res.json(post);
+        })
+        .catch(err => {
+            sendUserError(500, "The post information could not be retrieved.", res);
+        })
 })
 
 server.listen(port, () => console.log(`Server is running on port ${port}`));
