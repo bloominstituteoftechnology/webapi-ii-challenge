52bff2480905786b72d270935137c51a5bce79e1
maribelcuales <maribelcuales@gmail.com>
2018-05-31 06:38:58 +0800

    added Get method by /posts and /posts/id

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import { read } from 'fs';
+// import { read } from 'fs';
 
 // import your node modules
 const express = require('express'); 
@@ -50,6 +50,35 @@ server.post('/api/posts', (req, res) => {
             return; 
     });
 });
+
+server.get('/api/posts', (req, res) => {
+    db 
+        .find()
+        .then(posts => {
+            res.json({ posts });
+        })
+        .catch(error => {
+            sendUserError(500, "The posts information could not be retrieved.", res);
+            return;
+        });
+});
+
+server.get('/api/posts/:id', (req, res) => {
+    const { id } = req.params; 
+    db
+        .findById(id)
+        .then(post => {
+            if (post.length === 0) {
+                sendUserError(404, "The post with the specified ID does not exist.", res);
+                return; 
+            }
+            res.json(post);
+        })
+        .catch(error => {
+            sendUserError(500, "The post information could not be retrieved.", res);
+        });
+});
+
 
 server.listen(port, () => console.log(`Server running on port ${port}`));
 
