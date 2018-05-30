e204b216ca9a5655da3775103ad715aa23f98499
Niloc92 <prestonresult@github.com>
2018-05-30 18:28:11 -0500

    add

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
-// import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
 
-// add your server code starting here
+const port = 5555;
+const server = express();
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send('Hello Niloc');
+})
