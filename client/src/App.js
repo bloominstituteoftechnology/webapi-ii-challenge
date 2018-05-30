cb5417976dcf3fcd537936a296dea85b23a24dcd
Cassandra Lamendola <cass.lamendola@gmail.com>
2018-05-30 14:31:53 -0700

    non-functional styling
    - create update and delete buttons (non-functional)
    - add quotation marks to titles
    - add '-' before contents

--- client/src/App.js
+++ client/src/App.js
@@ -27,14 +27,24 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Lambda Quotes</h1>
         </header>
-        <p className="App-intro">
+        <h2 className="App-intro">
           Posts:
-        </p>
+        </h2>
         {this.state.posts.map(post => {
           return(
             <div className="postCard" key={post.id}>
-              <h2 className="postTitle">{post.title}</h2>
-              <p>{post.contents}</p>
+              <h2 className="postTitle">{`"${post.title}"`}</h2>
+              <p>{`- ${post.contents}`}</p>
+              <div className="buttons">
+                <button 
+                  className="btn">
+                  Update
+                </button>
+                <button 
+                  className="btn delete">
+                  Delete
+                </button>
+              </div>
             </div>
           )
         })}
