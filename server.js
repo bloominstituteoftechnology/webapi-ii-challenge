b1aba9dc4910024db74633cae607205b7b808498
bbenefield89 <bsquared18@gmail.com>
2018-05-30 15:20:43 -0400

    feat(server.js): add GET:`/api/posts`

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ const port = 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('Hello from Express'));
+/********************
+** ROUTE: `/posts` **
+********************/
+app.get('/api/posts', (req, res) => {
+  db.find()
+    .then(data => res.json(data))
+    .catch(err => res.status(500).json({ error: "The posts information could not be retrieved." }));
+});
 
 app.listen(port, () => console.log(`Server is listening on PORT: ${ port }`));
