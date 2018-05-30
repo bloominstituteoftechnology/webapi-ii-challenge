837c357ccb2497b9ebb2511478b82eaac12dfbe7
Jacob Henry <jacobiswinning@gmail.com>
2018-05-30 14:01:25 -0600

    Jake Henry added post and get posts

--- server.js
+++ server.js
@@ -1,5 +1,51 @@
 // import your node modules
+const express = require('express')
+const db = require('./data/db.js')
+const server = express()
 
-const db = require('./data/db.js');
+server.use(express.json())
 
+// middleware
+const cors = require('cors')
+
+const port = 5000
 // add your server code starting here
+
+server.get('/', (req, res) => {
+  res.send('server is running...')
+})
+
+server.post('/api/posts', (req, res) => {
+  const {
+    title,
+    contents
+  } = req.body;
+  db
+    .insert({
+      title,
+      contents
+    })
+    .then(response => {
+      res.json(response)
+    })
+    .catch(error => {
+      res.json(error)
+    })
+})
+
+server.get('/api/posts', (req, res) => {
+  db
+    .find()
+    .then(posts => {
+      res.json({
+        posts
+      });
+    })
+    .catch(error => {
+      res.json({
+        error
+      });
+    });
+});
+
+server.listen(5000, () => console.log(`server is running on port ${port}`))
