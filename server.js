12bb11a711dde7cc1e2630b9b08715ca40eea261
jesuarva <jean.ariza@jesuarva.com>
2018-05-30 22:44:13 +0200

    ADD GET endpoint
    DONE:
    If the request body is missing the title or contents property:
    1. cancel the request.
    2. respond with HTTP status code 400 (Bad Request).
    3. return the following JSON response: { errorMessage: "Please provide title and contents for the post."}.

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 const express = require("express");
 const cors = require("cors");
+const db = require("./data/db.js");
 
-const db = require('./data/db.js');
-
-const port = 6666;
 const server = express();
+const port = 6666;
+const endpoint = "/api/posts";
 
 server.use(express.json());
 server.use(cors());
 
+server.get(endpoint, (req, res) => {
+  db
+    .find()
+    .then(response => {
+      console.log("response", response);
+      
+      /**
+       * SOLLELY FOR TESTING PURPOSE: Uncomment the line below to test a fail in the server fetching the data.
+       * */
+      // return new Promise.reject();
+      
+      res.json(response);
+    })
+    .catch(e => {
+      console.log("error", e);
+      res.status(500).json({ error: "The posts information could not be retrieved." });
+    });
+});
+
+server.listen(port, () => console.log("Server running on port %s ", port));
