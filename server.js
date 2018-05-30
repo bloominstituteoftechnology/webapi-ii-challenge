1b09145ff35fafb82aa0ac3ce7a4527b33a0a01d
bbenefield89 <bsquared18@gmail.com>
2018-05-30 15:13:43 -0400

    server is up and running

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
-// import your node modules
+const express = require('express');
+const cors = require('cors');
 
+const app = express();
 const db = require('./data/db.js');
+const port = 5000;
 
-// add your server code starting here
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('Hello from Express'));
+
+app.listen(port, () => console.log(`Server is listening on PORT: ${ port }`));
