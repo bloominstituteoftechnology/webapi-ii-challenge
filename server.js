c7582378a29aea1561ab18832c44462ca9c06847
bbenefield89 <bsquared18@gmail.com>
2018-05-30 15:59:39 -0400

    feat(server.js): add POST:`/api/posts`

--- server.js
+++ server.js
@@ -1,20 +1,39 @@
 const express = require('express');
-const cors = require('cors');
+const cors    = require('cors');
 
-const app = express();
-const db = require('./data/db.js');
+const app  = express();
+const db   = require('./data/db.js');
 const port = 5000;
 
 app.use(cors());
 app.use(express.json());
 
+const error = (res, statusCode, errMessage) => {
+  res.status(statusCode).json({ error: errMessage });
+}
+
 /********************
 ** ROUTE: `/posts` **
 ********************/
+// get
 app.get('/api/posts', (req, res) => {
   db.find()
     .then(data => res.json(data))
-    .catch(err => res.status(500).json({ error: "The posts information could not be retrieved." }));
+    .catch(err => error(res, 500, "The posts information could not be retrieved."));
+});
+
+// post
+app.post('/api/posts', (req, res) => {
+  const { title, contents } = req.body;
+  const post = { title, contents };
+  db.insert(post)
+    .then(data => {
+      if (!title || !contents) {
+        return error(400, "Please provide title and contents for the post.");
+      }
+      res.status(201).json(data);
+    })
+    .catch(err => error(res, 500, "There was an error while saving the post to the database"));
 });
 
 app.listen(port, () => console.log(`Server is listening on PORT: ${ port }`));
