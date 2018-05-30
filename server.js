459420d14067b6e8410a3aead3ef9e60f5935dff
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-05-30 01:21:00 -0500

    initialized project installed dependencies including express imported express server listening on port 5000

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 // import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
+const port = 5000;
+
+const server = express();
+server.use(express.json());
 
 // add your server code starting here
+
+
+
+server.listen(port, () => console.log(`Magic Happening on port ${port}`))
