0d63b13ac30629c47dfc0135249b93b967f3520a
Andrew Kolumbic <andrewkolumbic@Andrews-Mac-mini.local>
2018-05-30 13:57:54 -0700

    put finished

--- server.js
+++ server.js
@@ -65,4 +65,27 @@ server.delete('/api/posts/:id', (req, res) => {
     })
 })
 
+
+
+
+
+server.put('/api/posts/:id', (req, res) => {
+    const { title, contents } = req.body;
+    if (title && contents) {
+        db.update(req.params.id, {title, contents})
+        .then(response => {
+            if (response === 1) {
+                res.status(200).json({ id: response.id, title, contents})
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ error: "The post information could not be modified." })
+        })
+    } else {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    }
+})
+
 server.listen(port, () => console.log(`Server running on ${port}`));
