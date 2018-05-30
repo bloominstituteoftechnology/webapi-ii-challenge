280ecc51354844e3da15da7cf0fc7965b92b8d7f
kelly <kelfro@fastmail.com>
2018-05-30 15:33:04 -0700

    did some table styling

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import { Table } from 'reactstrap';
 
 class App extends Component {
 	constructor(props) {
@@ -31,16 +32,19 @@ render() {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-	</p>
 	<div>
 		{this.state.posts.map(post => {
 			return (
-				<ul>
-					<li>{post.title}: {post.contents}</li>
-				</ul>
+				<Table striped>
+					<tbody>
+						<tr>
+							<td>{post.title}</td>
+							<td>{post.contents}</td>
+						</tr>
+					</tbody>
+				</Table>
 			);
+	
 		})}
 	</div>    
       </div>
