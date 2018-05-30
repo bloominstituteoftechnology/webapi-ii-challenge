1130e746e0a870a511b97044892d2463a449935f
jesuarva <jean.ariza@jesuarva.com>
2018-05-30 23:23:18 +0200

    ADD GET:id
    DONE:
    
    When the client makes a GET request to /api/posts/🆔
    1.If the post with the specified id is not found:
    	a. return HTTP status code 404 (Not Found).
    	b. return the following JSON object: { message: "The post with the specified ID does not exist." }.
    
    2.If there's an error in retrieving the post from the database:
    	a. cancel the request.
    	b. respond with HTTP status code 500.
    	c. return the following JSON object: { error: "The post information could not be retrieved." }.

--- server.js
+++ server.js
@@ -4,27 +4,58 @@ const db = require("./data/db.js");
 
 const server = express();
 const port = 6666;
-const endpoint = "/api/posts";
+const url = "/api/posts";
 
 server.use(express.json());
 server.use(cors());
 
-server.get(endpoint, (req, res) => {
+server.get(url, (req, res) => {
   db
     .find()
     .then(response => {
       console.log("response", response);
-      
+
       /**
-       * SOLLELY FOR TESTING PURPOSE: Uncomment the line below to test a fail in the server fetching the data.
+       * SOLLELY FOR TESTING PURPOSE: Uncomment the line below to test a fail in the server fetching the data with 'db.find()' method.
        * */
       // return new Promise.reject();
-      
+
       res.json(response);
     })
     .catch(e => {
       console.log("error", e);
-      res.status(500).json({ error: "The posts information could not be retrieved." });
+      res
+        .status(500)
+        .json({ error: "The posts information could not be retrieved." });
+    });
+});
+
+server.get(`${url}/:id`, (req, res) => {
+  const { id } = req.params;
+  db
+    .findById(id)
+    .then(response => {
+      // console.log("response", response, id);
+      // If ID is not found
+      response.length === 0 &&
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      // If ID is found --> Get user and return it whitin the response
+      return db.find().then(response => {
+        /**
+         * SOLLELY FOR TESTING PURPOSE: Uncomment the line below to test a fail in the server fetching the data with 'db.find()' method.
+         * */
+        // return new Promise.reject();
+
+        // Send the user info
+        res.json(response.filter(user => user.id === Number(id))[0]);
+      });
+    })
+    .catch(e => {
+      res
+        .status(500)
+        .json({ error: "The post information could not be retrieved." });
     });
 });
 
