de297e928bc55d9ed504a85f9a7241fc48550465
Fausto Wladimir Fraga <wladimir917@hotmail.com>
2018-05-30 16:27:54 -0400

    Server.js initial setup   Import node module express   Define listener port   Define server   Create a server listener

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 // import your node modules
-
-
+const express = require('express');
 const db = require('./data/db.js');
 
 // add your server code starting here
+
+const port = 5000;
+const server = express();
+
+server.listen(port, () => console.log(`Server running on port ${port}`));
