2fb87009c42ae984194585c5d896fed01a238b7e
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-05-30 01:35:45 -0500

    implemented post request

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// import your node modules
 const express = require('express');
 const db = require('./data/db.js');
 const port = 5000;
@@ -6,7 +5,23 @@ const port = 5000;
 const server = express();
 server.use(express.json());
 
-// add your server code starting here
+// server code here
+server.post('/api/posts', (req, res) => {
+    const { title, contents } = req.body
+    if (!title || !contents) {
+        res.status(400).json({errorMessage: "Please provide title and contents for the post."})
+    }
+
+    db
+        .insert({ title, contents })
+        .then(response => {
+            res.status(201).json(response)
+        })
+        .catch(error => {
+            res.status(500).json({ error: "There was an error while saving the post to the database" });
+        })
+})
+
 
 
 
