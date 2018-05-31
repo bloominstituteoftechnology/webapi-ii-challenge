050bd6db4b1350a14cc3e464178d88d2c2619231
jaklundun <govi.escudero@gmail.com>
2018-05-30 19:33:44 -0700

    Wrote GET Posts endpoint query.

--- server.js
+++ server.js
@@ -1,5 +1,29 @@
 // import your node modules.
+const express = require('express');
+const cors = require('cors');
+const db = require('./data/db');
+const port = 5001;
 
-const db = require('./data/db.js');
+const server = express();
+server.use(express.json());
+
+
+server.get('/api/posts', (req, res) => {
+    // 1st arg: route where a resource can be interacted with
+    // 2nd arg: callback to deal with sending responses, and handling incoming
+    db
+        .find()
+        .then(posts => {
+            res.json({ posts });
+        })
+        .catch(error => {
+            res.status(500).json({ errorMessage: error});
+         
+        })
+});
+
+
 
 // add your server code starting here
+server.listen(port, () => console.log('Server is running on port ${port}'));
+
