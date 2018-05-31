8842f71f6b96455b7046aff3c537c7cdb74f49ca
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-05-31 02:07:48 -0500

    enabled cors in server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const db = require('./data/db.js');
 const port = 5000;
 
 const server = express();
 server.use(express.json());
+server.use(cors({origin: 'http://localhost:3000'}));
 
 // server code here
 
