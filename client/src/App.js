97018dbf1e70137eade873b062083e923a2a2cbe
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-05-31 02:09:09 -0500

    imported axios into client

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import logo from './logo.svg';
 import './App.css';
+
 
 class App extends Component {
   render() {
@@ -10,9 +13,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      
       </div>
     );
   }
