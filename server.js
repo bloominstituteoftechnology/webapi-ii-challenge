b7ac661a552785a80a315fa0141290477fdc5b52
kammagik <kammagik@yahoo.com>
2018-05-31 09:50:15 -0700

    completed copying from mini

--- server.js
+++
@@ -1,7 +0,0 @@
-// import your node modules
-const express = require('express');
-const db = require('./data/db.js');
-
-// add your server code starting here
-const port = 5000;
-const server = express();
