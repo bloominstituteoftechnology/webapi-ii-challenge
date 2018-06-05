2c3df0b8e88484a2b60f661bb3497b0196622503
Rachelle De Jesus Pestanas <rachelle.pestanas@gmail.com>
2018-06-05 14:54:38 -0500

    installed yarn & dependencies, API now running

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 // import your node modules
-
+const express = require('express')
+const bodyParser = require('body-parser');
 const db = require('./data/db.js');
+const cors = require('cors');
 
 // add your server code starting here
+const server = express();
+server.use(bodyParser.json());
+
+server.get('/', (req, res) => {
+    res.send('API running');
+})
+
+server.listen(5000, () => console.log('\n== API Running on port 5000 ==\n'));
