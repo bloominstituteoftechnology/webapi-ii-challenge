b2ff903d3fb2909371c1cb71112935c052d2db1f
JSNavey <jsnavey@gmail.com>
2018-06-05 15:39:36 -0400

    installed CORS and updated it in server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 // import your node modules
 const express = require('express');
 const db = require('./data/db.js');
+const cors = require('cors');
 
 const port = 5000;
 const server = express();
 server.use(express.json());
+server.use(cors());
 
 // add your server code starting here
 server.get('/', (req, res) => {
