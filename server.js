94c1e929af95cda4303909d66d9dae40b883c8db
Rachelle De Jesus Pestanas <rachelle.pestanas@gmail.com>
2018-06-05 17:41:37 -0500

    implemented POST: /api/posts

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const bodyParser = require('body-parser');
 const db = require('./data/db.js');
 const cors = require('cors');
 
-// add your server code starting here
+// middleware
 const server = express();
 server.use(bodyParser.json());
 
+// add your server code starting here
 server.get('/', (req, res) => {
     res.send('API running');
 });
@@ -44,5 +45,23 @@ server.get('/api/posts/:id', (req, res) => {
         res.status(500).json({ error: 'The posts information could not be retrieved.' });
     });
 })
-    
+
+// POST request to /api/posts
+server.post('/api/posts', (req, res) => {
+    const{title, contents} = req.body;
+    const postNew = {title, contents}
+    /*if (title.length === 0 || contents.length === 0) {
+        res.status(400).json({ errorMessage: 'Please provide title and contents for the post.' })
+    } else*/
+    db
+        .insert(postNew)
+        .then(post => {
+            res.status(201).json(post);
+    })
+    // If retrieval error from database
+    .catch(err => {
+        res.status(500).json({ error: 'There was an error while saving the post to the database.' })
+    });
+});
+
 server.listen(5000, () => console.log('\n== API Running on port 5000 ==\n'));
