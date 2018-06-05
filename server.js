a12af04cf0e46fd9a1732a76ed86189cc8fbd7ea
Usman Javed <ujaved931@gmail.com>
2018-06-06 01:58:40 +0500

    Finished working on base project finished put method going ahead with the stretch

--- server.js
+++ server.js
@@ -94,5 +94,38 @@ server.delete('/api/posts/:id', (req, res) => {
         })
 });
 
+server.put(`/api/users/:id`, (req, res) => {
+    const { id } = req.params;
+    const { title, contents } = req.body;
+    if (!title || !contents) {
+        sendUserError(400, "Please provide title and contents for the post.", res);
+        return;
+    }
+    db
+        .update(id, { title, contents })
+        .then(response => {
+            if (response === 0) {
+                userError(404, "The post with the specified ID does not exist.", res);
+                return;
+            }
+            db
+                .findById(id)
+                .then(posts => {
+                    if (posts.length === 0) {
+                        userError(404, "The post with the specified ID does not exist.", res);
+                        return;
+                    }
+                    res.json(posts);
+                })
+                .catch(error => {
+                    userError(500, "The post information could not be modified.", res);
+                });
+        })
+        .catch(error => {
+            userError(500, "The post information could not be modified.", res);
+            return;
+        });
+});
+
 
 server.listen(port, () => console.log(`Server running on port ${port}`));
