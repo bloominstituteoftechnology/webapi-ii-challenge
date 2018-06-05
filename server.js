6d5026676ce4a568db045bb9d3c2c4b198202863
Usman Javed <ujaved931@gmail.com>
2018-06-06 00:37:44 +0500

    Node-express-lab project 1/3 done Forked and Cloned Implemented express and cors Implemented get endpoints for both posts and posts:id Working on other endpoints

--- server.js
+++ server.js
@@ -1,5 +1,51 @@
 // import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
+const cors = require('cors');
+
+
 
 // add your server code starting here
+const port = 5050;
+const server = express();
+server.use(express.json());
+server.use(cors());
+
+const userError = (status, message, res) => {
+    res.status(status).json({ errorMessage: message })
+}
+
+server.get('/', (req, res) => {
+    res.send('node-express-lab')
+});
+
+server.get('/api/posts',(req, res) => {
+    db
+        .find()
+        .then(posts => {
+            res.json({ posts })
+        })
+        .catch(error => {
+            userError(500, 'The posts information could not be retrieved.', res);
+        });
+});
+
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    db
+        .findById(id)
+        .then(posts => {
+            if (post.length === 0) {
+                userError(404, "The post with the specified ID does not exist.", res);
+                return;
+            }
+            res.send(posts);
+        })
+
+        .catch(error => {
+            userError(500, "The post information could not be retrieved.", res);
+        });
+});
+
+
+server.listen(port, () => console.log(`Server running on port ${port}`));
