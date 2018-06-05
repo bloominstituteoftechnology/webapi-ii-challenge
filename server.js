bd682f6608ddaca27b4cbc3a5be1dc82870395e5
Usman Javed <ujaved931@gmail.com>
2018-06-06 01:34:59 +0500

    Finished delete method having issues with implmenting my own delete method commented out my version and worked around it with a different syntax working on put method now

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ server.get('/', (req, res) => {
     res.send('node-express-lab')
 });
 
-server.get('/api/posts',(req, res) => {
+server.get('/api/posts', (req, res) => {
     db
         .find()
         .then(posts => {
@@ -30,7 +30,7 @@ server.get('/api/posts',(req, res) => {
         });
 });
 
-server.get('/api/users/:id', (req, res) => {
+server.get('/api/posts/:id', (req, res) => {
     const { id } = req.params;
     db
         .findById(id)
@@ -45,6 +45,53 @@ server.get('/api/users/:id', (req, res) => {
         .catch(error => {
             userError(500, "The post information could not be retrieved.", res);
         });
+});
+
+server.post('/api/posts', (req, res) => {
+    const { title, contents } = req.body;
+
+    if (!title || !contents) {
+        userError(400, 'Please provide title and contents for the post.', res)
+        return;
+    }
+    db
+        .insert({ title, contents })
+        .then(response => {
+            res.status(201).send(response);
+        })
+        .catch(error => {
+            userError(500, "There was an error while saving the post to the database", res)
+        })
+})
+
+// server.delete('/api/posts/:id', (req, res) => {
+//     db    
+//         .remove(req.params.id)
+//         .then(response => {
+//             if (response === 0) { 
+//                 userError(404, "The post with the specified ID does not exist.", res);
+//             } else { 
+//                 res.status(200).json(response) 
+//                 }
+//         })
+//         .catch(error => {
+//             userError(500, "the post could not be removed", res)
+//         });
+// Not sure why this isn't working and giving me error. Lost a good 20 mins figuring it out.
+
+server.delete('/api/posts/:id', (req, res) => {
+    db
+        .remove(req.params.id)
+        .then(response => {
+            if (response === 0) {
+                res.status(404).json({ message: "The post with the specified ID does not exist." });
+            } else {
+                res.status(200).json(response);
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ error: "The post could not be removed" });
+        })
 });
 
 
