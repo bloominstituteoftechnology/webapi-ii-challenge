536392cf0e0a4e76df255348c06a1dba9f8c2282
Tramane Hall <tramanedhall@gmail.com>
2018-06-05 18:58:41 -0400

    The Birth of a Server

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 // import your node modules
 
 const db = require('./data/db.js');
+const express = require('express');
+const cors = require('cors');
 
+express().listen(5865);
+console.log('Port 5865 is ALIIIIIIVVVVEEEEEE');
 // add your server code starting here
+/*
+Battleplan:
+1. Import express
+2. Import cors: Cross Origin Resource Sharing
+3. Implement GETS, POST, DELETE, PUT
+4. create-react-app:
+    1. Display list of posts
+    2. Enable ability to add post
+    3. Delete button that removes posts.
+
+*/
