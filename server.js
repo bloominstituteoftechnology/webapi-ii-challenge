f96f2663f41bf7b983e0464fe09052699d0278bc
Alec <afroenygam@gmail.com>
2018-07-23 18:59:25 -0400

    GET all functionality updated

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ server.get('/api/posts', (req, res) => {
   db.find().then(response => {
     res.status(200).json(response);
   }).catch(err => {
-    res.status(500).json(errorMessage: err)
+    res.status(500).json({errorMessage: "The posts could not be retrieved"});
   })
 });
 
