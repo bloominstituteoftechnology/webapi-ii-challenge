e4bd1cc9f176afb035cb5bf100816f6da032351e
Vu Cao <cdhvu@yahoo.com>
2018-07-23 14:25:58 -0700

    Create endpoint for GET

--- server.js
+++ server.js
@@ -3,3 +3,14 @@
 const db = require('./data/db.js');
 
 // add your server code starting here
+const express = require('express');
+
+const server = express();
+
+server.get('/api/posts', (req, res) => {
+    return db.find()
+    .then(response => res.status(200).json(response))
+    .catch(error => res.status(500).json({ error: "The posts information could not be retrieved." }))
+})
+
+server.listen(8000, () => console.log('API running on port 8000'));
