95d8e326be6f86557f71eb83320ec055bc617a20
DirupT <iamcooled@gmail.com>
2018-07-23 16:14:55 -0500

    added get user by id to server

--- server.js
+++ server.js
@@ -23,4 +23,14 @@ server.get('/api/posts', (req, res) => {
         .catch(err => res.status(500).json({ error: "The posts information could not be retrieved." }));
 })
 
+server.get('/api/posts/:id', (req, res) => {
+    db
+        .findById(req.params.id)
+        .then(user => {
+            if (user.length === 0) return res.status(404).json({ message: "The post with the specified ID does not exist." });
+            res.status(200).json(user);
+        })
+        .catch(err => res.status(500).json({ error: "The post information could not be retrieved." }));
+})
+
 server.listen(8000, () => console.log('API is running...'));
