06faf266154ac18a17fae6fd5200400a5e6eab60
cmok4290 <cmok4290@gmail.com>
2018-07-23 18:55:29 -0400

    added get req

--- server.js
+++ server.js
@@ -12,4 +12,29 @@ server.get('/', (req, res) => {
     res.status(200).send('Server running...');
 });
 
-server.listen(3000, () => console.log('Server running...'));
+server.get('/api/posts', (req, res) => {
+    db.find()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(error => {
+            res.status(500).json({error: 'The posts information could not be retrieved.'});
+        });
+});
+
+server.get('/api/posts/:id', (req, res) => {
+    const {id} = req.params;
+    db.findById(id)
+        .then(user => {
+            if(user[0]) {
+                res.status(200).json(user);
+            } else {
+                res.status(404).json({error: 'The post with the specified ID does not exist.'});
+            }
+        })
+        .catch(error => {
+            res.status(500).json({error: 'The post information could not be retrieved.'});
+        });
+});
+
+server.listen(8000, () => console.log('Server running...'));
