e3bd61f7f72c67b16ab85adbdb5f7953c2801674
DirupT <iamcooled@gmail.com>
2018-07-23 16:12:10 -0500

    added post to server

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ server.use(express.json());
 
 // add your server code starting here
 
+server.post('/api/posts', (req, res) => {
+    const { title, contents } = req.body;
+    if (!title || !contents) return res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+    db
+        .insert({ title, contents })
+        .then(users => res.status(201).json(users))
+        .catch(err => res.status(400).json({ error: "There was an error while saving the post to the database" }));
+})
+
 server.get('/api/posts', (req, res) => {
     db
         .find()
