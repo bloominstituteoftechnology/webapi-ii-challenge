1f59047a9912081497ccc515fa4d4e589879f5f6
CLN <cln@Sags-MacBook-Pro.local>
2018-07-23 18:20:57 -0400

    First commit,.

--- server.js
+++ server.js
@@ -1,5 +1,29 @@
 // import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
 
+
 // add your server code starting here
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send('Hello World');
+  });
+
+  server.get('/users', (req, res) => {
+    const users = [
+      {
+        id: 1,
+        name: 'User1',
+      },
+      {
+        id: 2,
+        name: 'User2',
+      },
+    ];
+  
+    
+  
+    res.status(200).json(users);
+  });
+  server.listen(9000, () => console.log('API running on port 9000'));
