aab420cf0e224cc376e4841d5fc305ed67fecb72
Alec <afroenygam@gmail.com>
2018-07-23 18:52:19 -0400

    project initialized, and GET partially updated

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 // import your node modules
 
 const db = require('./data/db.js');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
+const server = express();
+
+server.use(express.json());
+server.use(bodyParser.json());
 // add your server code starting here
+server.get('/api/posts', (req, res) => {
+  db.find().then(response => {
+    res.status(200).json(response);
+  }).catch(err => {
+    res.status(500).json(errorMessage: err)
+  })
+});
+
+
+server.listen(8000, () => console.log('App is listening...'));
