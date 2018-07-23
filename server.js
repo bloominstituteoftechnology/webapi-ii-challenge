9831819b21cbf640e477a46dd3f8dbb26b529097
cmok4290 <cmok4290@gmail.com>
2018-07-23 18:13:11 -0400

    init server

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 // import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
+const cors = require('cors');
 
 // add your server code starting here
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.status(200).send('Server running...');
+});
+
+server.listen(3000, () => console.log('Server running...'));
