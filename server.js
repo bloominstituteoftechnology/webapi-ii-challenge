928b7a4b69d02bcc183cd2b8b9261ebe89bc2e27
Blake Fletcher <blakefletcher@Blakes-MacBook-Air.local>
2018-07-23 15:38:17 -0400

    Added express, hello world at port 3000

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 // import your node modules
+const express = require('express');
+const server = express();
 
 const db = require('./data/db.js');
 
 // add your server code starting here
+
+server.get('/', (req, res) => {
+    res.send('Hello World');
+  });
+
+  server.listen(3000, () => console.log('API running...'));
