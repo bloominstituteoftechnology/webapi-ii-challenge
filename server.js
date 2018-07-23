ebbfb039a3a5ad1623fe27dfea8f1add4568f95c
Vu Cao <cdhvu@yahoo.com>
2018-07-23 14:47:30 -0700

    Created endpoint for user Id

--- server.js
+++ server.js
@@ -13,4 +13,19 @@ server.get('/api/posts', (req, res) => {
     .catch(error => res.status(500).json({ error: "The posts information could not be retrieved." }))
 })
 
+server.get('/api/posts/:id', (req, res) => {
+    const user = db.findById(req.params.id);
+
+    return user
+    .then(response => 
+        {
+        if (response.length!==0) {
+            res.status(200).json(response)
+        } else {
+            res.status(404).send({ message: "The post with the specified ID does not exist." })
+        }}
+    ) 
+    .catch(error => res.status(500).json({ error: "The posts information could not be retrieved." }))
+})
+
 server.listen(8000, () => console.log('API running on port 8000'));
