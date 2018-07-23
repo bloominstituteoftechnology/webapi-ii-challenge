f7862e09563643efc27df4d7ad7a94876ff035c4
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2018-07-23 12:45:46 -0700

    adds express, updates upath, adds get all posts endpoint

--- server.js
+++ server.js
@@ -1,5 +1,30 @@
-// import your node modules
+const express = require('express');
+const bodyParser = require('body-parser');
+const data = require('./data/db');
 
-const db = require('./data/db.js');
+const server = express();
+// turn on the body parser, using JSON data
+server.use(bodyParser.json());
+const PORT = 8000;
 
-// add your server code starting here
+// res.status(200).json(hobbits);
+
+server.get('/api/posts', (req, res) => {
+  data
+    .find()
+    .then(response => res.json(response))
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: 'The posts information could not be retrieved.' }),
+    );
+});
+
+server.post('/api/users', (req, res) => {
+  const user = req.body.user;
+  if (!user) {
+    res.status(422).json({ error: 'must supply a user' });
+  }
+});
+
+server.listen(PORT, console.log(`Server listening on port ${PORT}`));
