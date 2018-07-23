4b977320635104a9cbf00f58c740f363cabef56f
DirupT <iamcooled@gmail.com>
2018-07-23 16:06:42 -0500

    added get users to server

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 // import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
 
+const server = express();
+server.use(express.json());
+
 // add your server code starting here
+
+server.get('/api/posts', (req, res) => {
+    db
+        .find()
+        .then(users => res.status(200).json(users))
+        .catch(err => res.status(500).json({ error: "The posts information could not be retrieved." }));
+})
+
+server.listen(8000, () => console.log('API is running...'));
