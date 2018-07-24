5b3351d0b99937badf4801bef4a19ed01da77a7c
Simon <Simon_Freelance@hotmail.com>
2018-07-24 17:07:13 -0400

    Post request has been implemented

--- server.js
+++ server.js
@@ -9,6 +9,23 @@ const server = express();
 
 server.use(express.json());
 
+server.post('/api/posts', async (req, res) => {
+    try {
+        const { title, contents } = req.body;
+        if(title == undefined || contents == undefined) {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post"});
+        res.end();
+        return;
+        }
+        const post = await db.insert({ title, contents });
+        res.status('201').json(post);
+    } 
+    catch (err) {
+        res.status(500).json({ error: 'There was an error while saving the post to the database'});
+        res.end();
+    }
+})
+
 server.get('/api/posts', async (req, res) => {
     try {
         const users = await db.find();
