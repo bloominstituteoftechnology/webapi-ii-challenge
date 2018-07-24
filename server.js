d51be3394487491a8869503fb83deaa5bae9746c
Vu Cao <cdhvu@yahoo.com>
2018-07-23 19:06:18 -0700

    Create endpoint for POST

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ const db = require('./data/db.js');
 // add your server code starting here
 const express = require('express');
 
+//bodyParser middleware
+const bodyParser = require('body-parser');
 const server = express();
+
+//use middleware
+server.use(bodyParser.json());
+
 
 server.get('/api/posts', (req, res) => {
     return db.find()
@@ -28,13 +34,24 @@ server.get('/api/posts/:id', (req, res) => {
     .catch(error => res.status(500).json({ error: "The posts information could not be retrieved." }))
 })
 
-server.post('/api/posts/', (req, res) => {
-    if (!('contents' in req.query) || !('title' in req.query)) {
+server.post('/api/posts', (req, res) => {
+    if (!('contents' in req.body) || !('title' in req.body)) {
         return res.status(400).send({ errorMessage: "Please provide title and contents for the post." })
     }
-    return db.insert(req.query)
-    .then(response => res.status(201).json(req.query))
+    return db.insert(req.body)
+    .then(response => res.status(201).json(req.body))
     .catch(error => res.status(500).send({ error: "There was an error while saving the post to the database" }))
+})
+
+server.delete('/api/posts/:id', (req, res) => {
+    let deleted;
+    db.findById(req.params.id)
+    .then(response => deleted = response)
+
+    const request = db.remove(req.params.id)
+    return request
+    .then(response => res.status(200).json(deleted))
+    .catch(error => res.status(500).send(error))
 })
 
 server.listen(8000, () => console.log('API running on port 8000'));
