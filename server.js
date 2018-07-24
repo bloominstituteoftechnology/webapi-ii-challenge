39e41bff1cf28fcb7d2ce303921f36fd603f7b4c
Allen Hai <allenhai03@gmail.com>
2018-07-24 13:20:21 -0500

    add get handler for posts

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
 // import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
+
+const server = express();
+server.use(express.json());
+
+server.get('/api/posts', async (req, res) => {
+
+  try {
+  
+    // resolves to an array
+    const posts = await db.find()
+    res.status(200).json(posts)
+
+  } catch(e) {
+
+    res.status(500).json(e)
+
+  }
+   
+})
+
+server.listen(5000, () => console.log('💵'))
 
 // add your server code starting here
