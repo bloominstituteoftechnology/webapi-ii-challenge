a43e03063a755355236617c7bfdd533fe8a54050
Blake Fletcher <blakefletcher@Blakes-MacBook-Air.local>
2018-07-24 10:43:34 -0400

    This put be updated, basic functionality achieved

--- server.js
+++ server.js
@@ -70,4 +70,26 @@ server.delete('/api/posts/:id', (req, res) => {
     })
 })
 
+server.put('/api/posts/:id', (req, res) => {
+  const {id} = req.params
+  const update = req.body
+  db
+      .update(id, update)
+      .then(count => {
+          if ( count > 0 ) {
+              db
+                  .findById(id)
+                  .then(posts => {
+                      res.status(200).json(posts[0])
+                  })
+          } else {
+             res.status(404).json({message: "The post with the specified ID does not exist."}) 
+          }
+      })
+      .catch(err => {
+          res.status(500).json({message: "The post information could not be modified."})
+      })
+
+})
+
   server.listen(port, () => console.log(`Server is listening to port ${port}`)); // Our server will only send a response to GET requests made to the / route on port 3000
