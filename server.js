3d7bed4d7092e287273e8163f8dad9f93102034d
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-07-24 12:57:14 -0700

    delete request

--- server.js
+++ server.js
@@ -45,3 +45,16 @@ server.post('/api/posts', (req, res) => {
         res.status(500).json({ error: "There was an error while saving the post to the database" })
     })
 })
+
+server.delete('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    db.remove(id).then(p => {
+        if(!p) {
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        }
+        res.status(200).json(p)
+    })
+    .catch(err => {
+        res.status(500).json({ error: "The post could not be removed" })
+    })
+})
