4fb3c2a0be973ca5626137b8bee87eb306e07216
Kevin Tena <kevten09@gmail.com>
2018-07-24 16:38:26 -0500

    Added post function to create post

--- server.js
+++ server.js
@@ -5,12 +5,32 @@ const db = require('./data/db.js');
 // add your server code starting here
 const server = express();
 
-server.get('/api/users', (req, res) =>{
-    db.find().then(users => {
-        res.status(200).json(users);
+const bodyParser = require('body-parser');
+
+server.use(bodyParser.json());
+
+server.post('/api/posts', (req, res) => {
+    let post = req.body;
+    if (!('contents' in post) || !('title' in post)) {
+        res.status(400).send({ errorMessage: "Please provide title and contents for the post." })
+    }
+
+    db
+            .insert(post)
+            .then(response => {
+        res.status(201).json(post);
+        })
+        .catch(err => {
+        res.status(500).json({error: "There was an error while saving the post to the database"})
+        })
+ });
+
+server.get('/api/posts', (req, res) => {
+    db.find().then(posts => {
+        res.status(200).json(posts);
     })
     .catch(err => {
-        res.status(500).json({error: 'There was an error in retrieving the information'});
+        res.status(500).json({error: "The posts information could not be retrieved."});
     })
 });
 
