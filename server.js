2d70a6e44166645049203605ee7690881448af1b
Sukhada Gholba <sukhadagholba@gmail.com>
2018-07-24 12:58:15 -0700

    configured server file to respond to GET request with a specified id

--- server.js
+++ server.js
@@ -23,6 +23,23 @@ server.get('/api/posts', (req, res) => {
 
 });
 
+server.get('/api/posts/:id', (req, res) => {
+	const id = req.params.id;
+
+        const request = db.findById(id);
+
+        request.then(response => {
+        res.status(200).json(response);
+        })
+
+        .catch(err => {
+        res.status(404).json({error: "The user with the specified ID does not exist."});
+        })
+
+});
+
+
+
 
 
 server.listen(9000, () => console.log('API running on port 9000'));
