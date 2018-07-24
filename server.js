9a1cdacbca5d8d6092fb065c9a9cf811ee85ecae
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-07-24 17:20:18 -0500

    Ahmed Belhadj - Node Express Lab
    POST	/api/posts

--- server.js
+++ server.js
@@ -12,9 +12,25 @@ server.post("/api/posts", (req, res) => {
   db.insert();
 });
 
+server.post("/api/posts", async (req, res) => {
+  if (req.body.title == null || req.body.contents == null) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    });
+  }
+  try {
+    const post = await db.insert({ ...req.body });
+    res.status(201).json(post);
+  } catch (err) {
+    res.status(500).json({
+      error: "There was an error while saving the post to the database"
+    });
+  }
+});
+
 server.get("/api/posts", async (req, res) => {
   try {
-    const posts = await db.fond();
+    const posts = await db.find();
     res.status(200).json(posts);
   } catch (err) {
     res
