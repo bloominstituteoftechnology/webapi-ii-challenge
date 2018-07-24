a1d8c7288c18f9e5893f04c51c3a5d6e98b0ff89
emaquino44 <prexy44@yahoo.com>
2018-07-24 00:03:24 -0700

    created get endpoints

--- server.js
+++ server.js
@@ -6,6 +6,26 @@ const server = express();
 //middleware
 server.use(express.json());
 
+//route handlers
+server.get('/', (req, res) => {
+
+  res.send('Hello World');
+});
+
+server.get('/api/posts', (req, res) => {
+	db.find()
+	.then(posts => {
+		res.json(posts);
+	})
+	.catch(err => {
+		res.status(500).json({error: "Please provide title and contents for the post."});
+	});
+});
+
+
+
+
+
 
 // add your server code starting here
 server.listen(3000, () => console.log('API is running'));
