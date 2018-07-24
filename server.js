00c42847dc347662ed4693d7e90165be26e790b6
CLN <cln@Sags-MacBook-Pro.local>
2018-07-24 19:12:34 -0400

    Added a few error messages,.

--- server.js
+++ server.js
@@ -12,14 +12,23 @@ server.get('/', (req, res) => {
   });
 
   server.get('/api/posts', (req, res) => {
+    //res.end
+    // try{
+    //   const users = await db.find();
+    //   res.status(200).json(users);
+    // }catch(err){res.status(500)
+        //.json({error:"we have error"})
+
+    // }
       db.find()
       .then(posts=>{
         res.status(200).json(posts);
       })
       .catch(error =>{
         res.status(500)
-        .json({error:"we have error"})
+        .json({error:"The posts information could not be retrieved."})
       })
+      
     
 
   });
@@ -28,14 +37,20 @@ server.get('/', (req, res) => {
      .then(posts=>{
        res.status(200).json(posts)
      })
-
+     .catch(error =>{
+      res.status(500)
+      .json({error:"The post with the specified ID does not exist."})
+    })
     
 });
 server.post('/api/posts', (req, res) => {
   // const post = {id:nextId++, name: "sam"};
   // posts.push(post);
   // res.status(200).json(posts);
-
+  db.insert(req.params.title)
+  .then(posts=>{
+    res.status(200).json(posts)
+})
 });
 
 server.delete('/api/posts/:id', (req, res) => {
@@ -46,6 +61,15 @@ db.remove(req.params.id)
        res.status(200).json(posts)
 })
 });
+
+server.put('/api/posts/:id', (req, res) => {
+  // const {id} = req.params;
+  // posts = posts.filter(p => p.id != id)
+  db.update(req.params.id, req.body)
+       .then(posts=>{
+         res.status(200).json(posts)
+  })
+  });
 
 
   server.listen(9000, () => console.log('API running on port 9000'));
