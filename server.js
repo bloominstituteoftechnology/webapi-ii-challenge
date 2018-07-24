66ce260c14f095b340845e692fa727bce43ffe7a
Mariana Cerda <raymond_grc@yahoo.com>
2018-07-24 14:42:48 -0500

    Add search by post ID

--- server.js
+++ server.js
@@ -14,5 +14,21 @@ server.get("/api/posts", (req, res) => {
   })
 });
 
+server.get("/api/posts/:id", (req, res) => {
+  let post = req.params;
+  db.findById(post.id)
+  .then(post => {
+    if (post == ""){
+      res.status(404).send({ message: "The post with the specified ID does not exist." })
+    }
+    else {
+      res.send({ post });
+    }
+  })
+  .catch(error => {
+    res.status(404).send({ error: "The post information could not be retrieved." })
+  })
+})
+
 
 server.listen(8000, () => console.log('API running on port 8000'));
