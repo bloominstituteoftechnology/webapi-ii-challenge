19a02edbdf94c3d239806c1c90a956708c4e4247
Wdifulvio523 <w.difulvio523@gmail.com>
2018-07-24 16:01:05 -0400

    completed functionality to find by id and get post to render, as well as to post a new post and add it to the server

--- server.js
+++ server.js
@@ -15,10 +15,53 @@ server.get('/api/posts', (requestAnimationFrame, res) => {
         res.status(200).json(posts);
     })
     .catch(err => {
-        res.status(500).json({error:"Sorry, we can't find posts"});
+        res.status(500).json({ error: "The posts information could not be retrieved."});
     })
 });
 
+
+//findById
+server.get('/api/posts/:id', (req, res) => {
+    db
+    .findById(req.params.id)
+    .then(post => {
+        if (post.length === 0) {
+            res
+            .status(404)
+            .json({ message: "The post with the specified ID does not exist." });
+        }
+        res
+        .status(200)
+        .json(post);
+    })
+    .catch(error => {
+        res
+        .status(500)
+        .json({error: "The post information could not be retrieved."})
+    })
+});
+
+
+
+//Post
+
+server.post('/api/posts', (req, res) => {
+    const {title, contents} = req.body;
+    if (!title || !contents) {
+        res.status(400).json({errorMessage: "Please provide title and contents for the post."})
+        return;
+    }
+    db
+        .insert({
+            title, contents
+        })
+        .then(response => {
+            res.status(201).json(response);
+        })
+        .catch(error => {
+            res.status(500).json({error: "There was an error while saving the post to the database" })
+        })
+});
 
 
 
