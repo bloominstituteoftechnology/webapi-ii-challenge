46a13c9846bb2eae8b6069ed9b3f14bfc5228e89
Simon <Simon_Freelance@hotmail.com>
2018-07-24 17:06:33 -0400

    Post request has been implemented

--- server.js
+++ server.js
@@ -9,16 +9,48 @@ const server = express();
 
 server.use(express.json());
 
+server.post('/api/posts', async (req, res) => {
+    try {
+        const { title, contents } = req.body;
+        if(title == undefined || contents == undefined) {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post"});
+        res.end();
+        return;
+        }
+        const post = await db.insert({ title, contents });
+        res.status('201').json(post);
+    } 
+    catch (err) {
+        res.status(500).json({ error: 'There was an error while saving the post to the database'});
+        res.end();
+    }
+})
+
 server.get('/api/posts', async (req, res) => {
     try {
-        const users = await db.find();
-        res.status(200).json(users);
+        const posts = await db.find();
+        res.status(200).json(posts);
     }
     catch (err)  {
         res.status(500).json({ error: 'The posts information could not be retreived'});
         res.end();
     }
 })
+
+server.get('/api/posts/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const post = await db.findById(id);
+        if(post.length === 0) {
+            res.status(404).json({ message: 'The post with the specified ID does not exist'});
+            res.end();
+            return;
+        }
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(500).json({ error: 'The post information could not be retreived'});
+    }
+});
 
 
 server.listen(8001);
