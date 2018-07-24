fbd2a4b9641e55882c112555bc80cf479dc9b428
Alec <afroenygam@gmail.com>
2018-07-24 15:58:32 -0400

    GET methods updated with async/await

--- server.js
+++ server.js
@@ -3,39 +3,38 @@
 const db = require('./data/db.js');
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 
 const server = express();
 
 const sendUserError = (status, errorMessage, res) => {
   res.status(status).json({errorMessage: errorMessage});
-  return;
 }
 
 server.use(express.json());
-server.use(bodyParser.json());
+//server.use(bodyParser.json());
 server.use(cors());
 // add your server code starting here
-server.get('/api/posts', (req, res) => {
-  db.find().then(response => {
-    res.status(200).json(response);
-  }).catch(err => {
-    res.status(500).json({errorMessage: "The posts could not be retrieved"});
-  })
-});
+server.get('/api/posts', async (req, res) => {
+  try {
+      const posts = await db.find();
+      res.status(200).json(posts);
+  } catch(err) {
+    res.status(500).json({errorMessage: "The posts could not be retrieved", err: err});
+  }
+}); //Here I have used async/await syntax just to try it out.
 
-server.get('/api/posts/:id', (req, res) => {
-  db.findById(req.params.id).then(response => {
-    if (response.length > 0) {
-    res.status(200).json(response)
+server.get('/api/posts/:id', async (req, res) => {
+try {
+const post = await db.fondById(req.params.id);
+  if (post.length > 0) {
+    res.status(200).json(post);
   } else {
     sendUserError(404, "The post with the specified ID does not exist.", res);
-    return;
   }
-}).catch(err => {
-  sendUserError(500, err, res);
-  return;
-})
+} catch(err) {
+  sendUserError(500, `Server Error: ${err}`, res);
+}
 });
 
 server.post('/api/posts/', (req, res) => {
