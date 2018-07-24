9a2481639558bf1079c50dfdfc64aef0f83d21ad
Jasper <joshtietz11@gmail.com>
2018-07-24 16:15:32 -0500

    completed

--- server.js
+++ server.js
@@ -6,55 +6,85 @@ const db = require('./data/db.js');
 
 const server = express();
 
+server.use(express.json())
+
 server.get('/api/posts', (req, res) => {
   db.find()
   .then(response => {
-    res.json(response);
+    res.status(200).json(response);
   })
   .catch(error => {
+    res.status(404).send('error fetching data...')
     console.log(error.message)
   })
 })
 
 server.get('/api/posts/:id', (req, res) => {
-  db.findById(req)
+  db.findById(req.params.id)
   .then(response => {
-    res.json(response);
+    res.status(200).json(response);
   })
   .catch(error => {
+    res.status(404).send('error fetching individual data...')
     console.log(error.message)
   })
 })
 
 server.post('/api/posts', (req, res) => {
-  db.insert(req)
+  let { title, contents } = req.body;
+  let post = {
+    title,
+    contents
+  }
+  if (post.title === undefined || post.contents === undefined){
+      res.status(404).send('Posts need Title and Contents fields...')
+    } else {
+      db.insert(post)
+    .then(response => {
+      res.status(200).json(response)
+    })
+    .catch(error => {
+      res.status(404).send('error adding post...')
+      console.log(error.message)
+    })
+  }
+})
+
+server.delete('/api/posts/:id', (req, res) => {
+  const id = Number(req.params.id);
+  db.remove(id)
   .then(response => {
-    res.json(response)
+    res.status(200).json(response);
   })
   .catch(error => {
     console.log(error.message)
+    res.status(404).send("the post could not be removed")
   })
 })
 
 server.put('/api/posts/:id', (req, res) => {
-  db.update(req)
-  .then(response => {
-    res.json(response);
-  })
-  .catch(error => {
-    console.log(error.message)
-  })
+  let id = req.params.id;
+  let { title, contents } = req.body;
+  let changes = {
+    title,
+    contents
+  }
+  if (changes.title === undefined || changes.contents === undefined){
+      res.status(404).send('Posts need Title and Contents fields...')
+    } else {
+    db.update(id, changes)
+    .then(response => {
+      db.findById(req.params.id).then(response2 => {
+        res.status(200).json(response2)
+      })
+    })
+    .catch(error => {
+      console.log(error.message)
+      res.status(500).send('Unable to update information...')
+    })
+  }
 })
 
-server.delete('/api/posts', (req, res) => {
-  db.remove(req)
-  .then(response => {
-    res.json(response);
-  })
-  .catch(error => {
-    console.log(error.message)
-  })
-})
 
 
 server.listen(8000, () => console.log('API Running...'));
