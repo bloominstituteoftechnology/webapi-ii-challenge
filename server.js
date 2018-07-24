f06bba2f3fa8c719a4f4069870b1bb715a317bb4
Simon <Simon_Freelance@hotmail.com>
2018-07-24 17:07:27 -0400

    Refactored variables for get requests

--- server.js
+++ server.js
@@ -28,14 +28,29 @@ server.post('/api/posts', async (req, res) => {
 
 server.get('/api/posts', async (req, res) => {
     try {
-        const users = await db.find();
-        res.status(200).json(users);
+        const posts = await db.find();
+        res.status(200).json(posts);
     }
     catch (err)  {
         res.status(500).json({ error: 'The posts information could not be retreived'});
         res.end();
     }
 })
+
+server.get('/api/posts/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const post = await db.findById(id);
+        if(post.length === 0) {
+            res.status(404).json({ message: 'The post with the specified ID does not exist'});
+            res.end();
+            return;
+        }
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(500).json({ error: 'The post information could not be retreived'});
+    }
+});
 
 
 server.listen(8001);
