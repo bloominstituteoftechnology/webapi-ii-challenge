b87d3c25a7268f15d55b11aec122d168151f094b
Ari Mercado <ari@Apples-Mac-3.local>
2018-07-24 14:32:25 -0700

    created put endpoint

--- server.js
+++ server.js
@@ -61,6 +61,7 @@ server.post('/posts', (req, res) => {
     })
 })
 
+// delete post
 server.delete('/posts/:id', (req, res) => {
   const { id } = req.params;
   db
@@ -77,6 +78,37 @@ server.delete('/posts/:id', (req, res) => {
       res.status(500).json({
         error: "The post could not be removed"
       })
+    })
+})
+
+// ediit post
+server.put('/posts/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, bio } = req.body;
+  if (!title || !contents) {
+    return res.status(400).json({errorMessage: "Please provide title and contents for the post."})
+  }
+  db
+    .update(id, {title, contents})
+    .then(response => {
+      if (response == 0) {
+        return res.status(404).json({errorMessage: "Please provide title and contents for the post."})
+      }
+      db
+        .findById(id)
+        .then(user => {
+          if (user.length === 0) {
+            return res.status(404).json({message: "The post with the specified ID does not exist."})
+          } else {
+            return res.status(200).json(user);
+          }
+        })
+        .catch(error => {
+          return res.status(500).json({ errorMessage: 'Error looking up user'})
+        })
+      })
+    .catch(() => {
+      return res.status(500).json({ error: "The post information could not be modified."})
     })
 })
 
