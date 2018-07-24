274bf72d17fbd4f3ad49babf097eecfb72045cf0
Tyson Gushiken <tysonism@gmail.com>
2018-07-24 14:05:24 -0700

    Create DELETE endpoint

--- server.js
+++ server.js
@@ -53,4 +53,18 @@ server.post('/api/posts', async (req, res) => {
   }
 });
 
+server.delete('/api/posts/:id', async (req, res) => {
+  try {
+    const post = await db.findById(req.params.id);
+    if (post.length === 0) {
+      res.status(404).send({ error: 'The post with the specified ID does not exist.' })
+    } else {
+      await db.remove(req.params.id)
+      res.status(200).json(post);
+    }
+  } catch (err) {
+    res.status(500).send({ error: 'The post could not be removed.' });
+  }
+});
+
 server.listen(8000, () => console.log('API running on port 8000'));
