ac74199aecf3cf3b87739c20b66d054e6df22dae
Tyson Gushiken <tysonism@gmail.com>
2018-07-24 13:43:09 -0700

    Create GET by id endpoint

--- server.js
+++ server.js
@@ -16,12 +16,25 @@ server.get('/', (req, res) => {
   res.send('Node Express Lab');
 });
 
-server.get('/api/users', async (req, res) => {
+server.get('/api/posts', async (req, res) => {
   try {
     const posts = await db.find();
     res.status(200).json(posts);
   } catch (err) {
     res.status(500).send({ error: 'The posts information could not be retrieved.' })
+  }
+});
+
+server.get('/api/posts/:id', async (req, res) => {
+  try {
+    const post = await db.findById(req.params.id);
+    if (post.length === 0) {
+      res.status(404).send({ error: 'The post with the specified ID does not exist.'});
+    } else {
+      res.status(200).json(post);
+    }
+  } catch (err) {
+    res.status(500).send({ error: 'The post information could not be retrieved.' });
   }
 });
 
