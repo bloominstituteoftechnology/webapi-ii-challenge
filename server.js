a1ffc58eb88577cdb781c27506f57705c0ae8b24
Allen Hai <allenhai03@gmail.com>
2018-07-24 13:40:48 -0500

    add post handler for posts

--- server.js
+++ server.js
@@ -21,6 +21,29 @@ server.get('/api/posts', async (req, res) => {
    
 })
 
+server.post('/api/posts', async (req, res) => {
+
+  if(!req.body || !req.body.title || !req.body.contents) {
+    res.status(400).json({ error: "Please provide title and contents for the post" })
+  }
+
+  try {
+
+    // we are destructuring because db.insert returns an object with an id field
+    const { title, contents } = req.body
+    const { id } = await db.insert({ title, contents })
+    const savedPost = await db.findById(id)
+    
+    res.status(201).json(savedPost)
+
+  } catch(e) {
+
+    res.status(500).json({ error: "There was an error while saving the post to the database", e })
+
+  }
+
+})
+
 server.listen(5000, () => console.log('💵'))
 
 // add your server code starting here
