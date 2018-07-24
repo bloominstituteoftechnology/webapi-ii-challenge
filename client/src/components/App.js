965eadfc07d3501e9e5a0f17d23c2c5aca3db49f
cmok4290 <cmok4290@gmail.com>
2018-07-24 17:33:15 -0400

    rendered posts

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,18 +1,45 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import './App.css';
+import axios from 'axios';
+
+const URL = 'http://localhost:8000/api/posts';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get(URL)
+      .then(({data}) => {
+        // console.log(data);
+        this.setState({data});
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Node-Express-Lab</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {this.state.data.map(post => {
+          return (
+            <div key={post.id}>
+              <p>{`${post.title} - ${post.contents}`}</p>
+            </div>
+          );
+        })}
       </div>
     );
   }
