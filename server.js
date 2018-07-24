94f0daa1ebdb28491b881df6a4620ff8bd239d99
Simon <Simon_Freelance@hotmail.com>
2018-07-24 16:34:23 -0400

    Implemented get request to get all posts

--- server.js
+++ server.js
@@ -3,3 +3,22 @@
 const db = require('./data/db.js');
 
 // add your server code starting here
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/api/posts', async (req, res) => {
+    try {
+        const users = await db.find();
+        res.status(200).json(users);
+    }
+    catch (err)  {
+        res.status(500).json({ error: 'The posts information could not be retreived'});
+        res.end();
+    }
+})
+
+
+server.listen(8001);
