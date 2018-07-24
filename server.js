6746d9bf10589d3889ee49cabb7c014fc976da08
Wdifulvio523 <w.difulvio523@gmail.com>
2018-07-24 15:43:01 -0400

    William DiFulvio [Node-Express-Lab]
    Initial PR. Set up server and got it running. Added GET request to find posts on /api/posts. successfully retrieved posts

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
 // import your node modules
-
+const express = require("express")
 const db = require('./data/db.js');
 
 // add your server code starting here
+
+const server = express();
+server.use(express.json());
+
+//find posts
+server.get('/api/posts', (requestAnimationFrame, res) => {
+    db
+    .find()
+    .then(posts => {
+        res.status(200).json(posts);
+    })
+    .catch(err => {
+        res.status(500).json({error:"Sorry, we can't find posts"});
+    })
+});
+
+
+
+
+server.listen(8000, () => console.log("API running on port 8000"));
