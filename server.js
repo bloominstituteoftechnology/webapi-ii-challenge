cc50d7216bb754e1345fd2fee3a838f61cd0170a
Nicky Chen <nicky.chen0@gmail.com>
2018-07-24 14:10:19 -0700

    added get & post

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const db = require("./data/db.js");
 const express = require("express");
 
 const server = express();
+server.use(express.json()); // Body parser.
 
 // Get Request
 server.get("/api/posts", (req, res) => {
@@ -16,6 +17,23 @@ server.get("/api/posts", (req, res) => {
         .status(500)
         .json({ error: "The posts information could not be retrieved" });
     });
+});
+
+// Post Request
+server.post("/api/posts", (req, res) => {
+  const { title, contents } = req.body;
+  if (!title || !contents) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide title and contents for the post" });
+  }
+  db.insert({ title, contents })
+    .then(posts => res.status(201).json({ title, contents }))
+    .catch(err =>
+      res.status(500).json({
+        error: "There was an error while saving the post to the database"
+      })
+    );
 });
 
 server.listen(8000, () => console.log("API running on port 8000"));
