4c7097ef5065c5dfbbe662fe0093782d86ff4d48
Kevin Tena <kevten09@gmail.com>
2018-07-24 15:16:47 -0500

    Added get function to get db users

--- server.js
+++ server.js
@@ -4,3 +4,15 @@ const db = require('./data/db.js');
 
 // add your server code starting here
 const server = express();
+
+server.get('/api/users', (req, res) =>{
+    db.find().then(users => {
+        res.status(200).json(users);
+    })
+    .catch(err => {
+        res.status(500).json({error: 'There was an error in retrieving the information'});
+    })
+});
+
+
+server.listen(8000, () => console.log('API running on port 8000'));
