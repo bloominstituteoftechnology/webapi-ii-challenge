6b198784537ef0a6547c622c11d4f65c1176d5aa
Kevin <brackkevin@gmail.com>
2018-07-24 15:53:36 -0500

    get post get_id delete and put routes created

--- server.js
+++ server.js
@@ -1,5 +1,61 @@
-// import your node modules
+const express = require("express");
 
-const db = require('./data/db.js');
+const server = express();
+const db = require("./data/db.js");
 
-// add your server code starting here
+server.get("/api/posts", (req, res) => {
+  db.find()
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "sorry we failed you", err: err });
+    });
+});
+
+server.post("/api/posts", (req, res) => {
+  const post = { ...req.body };
+  db.insert(post)
+    .then(num_added => {
+      res.status(200).json(num_added);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "sorry we failed you", err: err });
+    });
+});
+
+server.get("/api/posts/:id", (req, res) => {
+  const { id } = req.params;
+  db.findById(id)
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "sorry we failed you", err: err });
+    });
+});
+
+server.delete("/api/posts/:id", (req, res) => {
+  const { id } = req.params;
+  db.remove(id)
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "sorry we failed you", err: err });
+    });
+});
+
+server.put("/api/posts/:id", (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  db.update(id, body)
+    .then(document => {
+      res.status(200).json(document);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "sorry we failed you", err: err });
+    });
+});
+
+server.listen(8000, () => console.log("API RUNNING ON PORT 8000..."));
