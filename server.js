ff317838880baff5d37927138d4ef02d7589da45
Randy Calderon <randycweb@gmail.com>
2018-07-24 14:43:18 -0700

    Finished get request to api/posts/:id

--- server.js
+++ server.js
@@ -10,13 +10,30 @@ server.get('/api/posts', async (req, res) => {
     try {
         const posts = await db.find()
         res.status(200).json(posts)
-    } catch {
+    } catch (err) {
         res.status(500).json({error: "The posts information could not be retrieved."})
     }
 })
 
 server.get('/api/posts/:id', async (req, res) => {
-    
+    try {
+        const id = req.params.id
+        const getId = await db.findById(id)
+        res.status(200).json(getId)
+        if (getId == undefined) {
+        res.status(404).json({message: "The post with the specified ID does not exist." })
+        }
+    } catch (err) {
+        res.status(500).json({error: "The post information could not be retrieved. "})
+    }
+})
+
+server.post('/api/posts', async (req, res) => {
+    try {
+
+    } catch (err) {
+        
+    }
 })
 
 server.listen(8000);
