62287dc8536009dcff53ec9165ee1828b359e07e
emaquino44 <prexy44@yahoo.com>
2018-07-24 14:19:45 -0700

    styled

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
@@ -23,12 +23,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Lambda Blog</h1>
-        </header>
+
         <p className="App-intro">
-          Posts:
+          LOTR Quotes:
         </p>
         {this.state.posts.map(post => {
           return(
