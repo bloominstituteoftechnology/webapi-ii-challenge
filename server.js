8b4fb1049712f6422c55758b8c0e497f2d4bea1b
Gabriel Duquette <gabrielduquette@gmail.com>
2018-07-23 23:57:24 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-// import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const server = express();
 
-// add your server code starting here
+server.use(bodyParser.json());
+server.use(cors());
