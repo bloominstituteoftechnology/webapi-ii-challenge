aa2c2d4363f1735f42b804af2909c5771b44ebdb
Nicky Chen <nicky.chen0@gmail.com>
2018-07-24 13:33:37 -0700

    initial commit

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 // import your node modules
 
-const db = require('./data/db.js');
+const db = require("./data/db.js");
 
 // add your server code starting here
+const express = require("express");
+
+const server = express();
+
+// Get Request
+server.get("/api/posts", (req, res) => {
+  db.find()
+    .then(posts => res.status(200).json(posts))
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The posts information could not be retrieved" });
+    });
+});
+
+server.listen(8000, () => console.log("API running on port 8000"));
