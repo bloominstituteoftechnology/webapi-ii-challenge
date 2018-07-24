5c2be8636b8611b2c64a83a736c18ff52dedc0c9
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-07-24 11:58:10 -0700

    initial commit

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 // import your node modules
-
+const express = require('express');
+const helmet = require ('helmet');
+const server = express();
+const port = 8000;
 const db = require('./data/db.js');
 
+server.use(helmet());
+server.use(express.json());
+
 // add your server code starting here
+
+server.listen(port, () => console.log(`Server is listening on port ${port}`))
