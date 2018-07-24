034c3095e6d32941c970d66cfd033639f03f7930
emaquino44 <prexy44@yahoo.com>
2018-07-23 23:43:59 -0700

    set up server

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 // import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
+const server = express();
+
+//middleware
+server.use(express.json());
+
 
 // add your server code starting here
+server.listen(3000, () => console.log('API is running'));
