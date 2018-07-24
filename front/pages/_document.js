cb41a0e9cc5c3b3d8805a78ebb2daccbb933ce9a
Allen Hai <allenhai03@gmail.com>
2018-07-24 15:02:55 -0500

    add basic global styles

---
+++ front/pages/_document.js
@@ -0,0 +1,29 @@
+import Document, { Head, Main, NextScript } from 'next/document'
+
+export default class extends Document {
+  render() {
+    return (
+      <html>
+        <Head>
+          <style>{`
+            * {
+              box-sizing: border-box;
+            }
+            
+            body {
+              padding: 0;
+              margin: 0;
+              background: #6d6464;
+              color: #fff;
+              font-family: SFMono-Regular, Menlo, Monaco, Consolas, Liberation Mono, Courier New, monospace;
+            }
+          `}</style>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
