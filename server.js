7a60b30e37c10323da3c5e40c294aa0e09c05b0c
JJ Ashcraft <jj@jjashcraft.com>
2018-07-24 15:44:32 -0400

    initial commit, GET functionality added

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 // import your node modules
 
-const db = require('./data/db.js');
+//required imports
 
-// add your server code starting here
+const express = require('express');
+const db = require('./data/db');
+
+const server = express();
+server.use(express.json());
+
+//GET POSTS
+server.get('/', async (req, res)=> {
+try {
+    const posts = await db.find()
+    res.status(200).json(posts);
+} catch (err) {
+    res.status(500).send('error', err);
+}
+
+});
+
+server.listen(8000, () => console.log('API running on port 8000'));
