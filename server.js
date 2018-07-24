01be872ae9fe66bed855667588b29cdd11c59b18
Mariana Cerda <raymond_grc@yahoo.com>
2018-07-24 14:18:47 -0500

    Init project, add get posts

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 // import your node modules
-
+const express = require("express");
+const server = express();
 const db = require('./data/db.js');
 
 // add your server code starting here
+server.get("/api/posts", (req, res) => {
+  db.find()
+  .then(posts => {
+            res.json({ posts });
+        })
+  .catch(error => {
+    res.status(500).send({ error: "The posts information could not be retrieved." })
+  })
+});
+
+
+server.listen(8000, () => console.log('API running on port 8000'));
