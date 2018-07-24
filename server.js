5ade4b0613db9f4b46b394f983825ecf4d76ec13
Erin Meredith <ekmeredith@att.net>
2018-07-24 16:35:10 -0400

    all CRUD functions working

--- server.js
+++ server.js
@@ -36,50 +36,62 @@ server.get('/api/posts/:id', (req, res) => {
         sendServerError('error: The post information could note be retrieved.');
     });
 });
-/*server.post('./api/posts', (req, res) => {
+server.post('/api/posts', (req, res) => {
     const { title, contents } = req.body;
-    const newPost = {title, contents}
+    const newPost = req.body;
+
     if (!title || !contents) {
-        res.status(400).send('error: Please provide title and contents for the post.');
+        res.status(400).send({error: 'Please provide title and contents for the post.'});
     }
-  /*  if (err) {
+    db.insert(newPost)
+    .then(response => {
+        res.status(200).json(response)
+    })
+    .catch(err => {
         sendServerError('error: There was an error while saving the post to the database.');
-    }
-    res.status(201);
-    posts.push(newPost);
-    res.send(posts);
+    });
 });
 
-server.delete('/api/posts/:id', (req, res) => {
-    res.send()
-    if (!id) {
-        res.status(404);
-        res.send('error: The post with the specified ID does not exist.');
-    }
-   /* if (err) {
-        sendServerError('error: The post could not be removed.');
-    }
-    
-})
-
 server.put('/api/posts/:id', (req, res) => {
+    const id = req.params.id;
     const { title, contents } = req.body;
     const updatedPost = { title, contents }
+
     if(!id) {
         res.status(404);
         res.send('error: The post with the specified ID does not exist.');
     }
     if (!title || !contents) {
         res.status(400);
-        res.send('error: Please provide title and contents for the post.');
+        res.send({error: 'Please provide title and contents for the post.'});
     }
-   /* if (err) {
-        sendServerError('error: The post information could not be modified.');
-    }
-    res.status(201);
 
-    res.send(updatedPost)
-})*/
+    db.update(id, updatedPost)
+    .then(response => {
+        res.status(200).json(response)
+    })
+    .catch(err => {
+        sendServerError('error: The post information could not be modified.');
+    });
+});
+
+server.delete('/api/posts/:id', (req, res) => {
+    const id = req.params.id;
+
+    if (!id) {
+        res.status(404);
+        res.send('error: The post with the specified ID does not exist.');
+    }
+
+    db.remove(id)
+    .then(
+        res.status(200).send('The post has been deleted')
+    )
+    .catch(err => {
+        sendServerError('error: The post could not be removed.');
+    });
+});
+
 
 
 server.listen(8000, () => console.log('API running'))
