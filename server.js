0bbb79ffb6623d1777fc0042904b40a3cd819d52
Vu Cao <cdhvu@yahoo.com>
2018-07-24 14:50:25 -0700

    Refactored code using async & await

--- server.js
+++ server.js
@@ -1,74 +1,85 @@
 // import your node modules
 
 const db = require('./data/db.js');
-
-// add your server code starting here
+const helmet = require('helmet');
 const express = require('express');
 
-//bodyParser middleware
-const bodyParser = require('body-parser');
+// add your server code starting here
 const server = express();
 
 //use middleware
-server.use(bodyParser.json());
+server.use(express.json());
+server.use(helmet());
 
-
-server.get('/api/posts', (req, res) => {
-    return db.find()
-    .then(response => res.status(200).json(response))
-    .catch(error => res.status(500).json({ error: "The posts information could not be retrieved." }))
+//Create endpoint for GET
+server.get('/api/posts', async (req, res) => {
+    try {
+        const response = await db.find();
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).json({ error: "The posts information could not be retrieved." })
+    }
 })
 
-server.get('/api/posts/:id', (req, res) => {
-    const user = db.findById(req.params.id);
-
-    return user
-    .then(response => 
-        {
-        if (response.length!==0) {
+//Create endpoint for GET by ID
+server.get('/api/posts/:id', async (req, res) => {
+    try {
+        const response = await db.findById(req.params.id);
+        if (response.length === 0) {
+            res.status(404).send({ message: "The post with the specified ID does not exist." });
+        } else {
             res.status(200).json(response)
-        } else {
-            res.status(404).send({ message: "The post with the specified ID does not exist." })
-        }}
-    ) 
-    .catch(error => res.status(500).json({ error: "The posts information could not be retrieved." }))
-})
-
-server.post('/api/posts', (req, res) => {
-    if (!('contents' in req.body) || !('title' in req.body)) {
-        return res.status(400).send({ errorMessage: "Please provide title and contents for the post." })
-    }
-    return db.insert(req.body)
-    .then(response => res.status(201).json(req.body))
-    .catch(error => res.status(500).send({ error: "There was an error while saving the post to the database" }))
-})
-
-server.delete('/api/posts/:id', (req, res) => {
-    let deleted;
-    db.findById(req.params.id)
-    .then(response => deleted = response)
-
-    const request = db.remove(req.params.id)
-    return request
-    .then(response => res.status(200).json(deleted))
-    .catch(error => res.status(500).send(error))
-})
-
-server.put('/api/posts/:id', (req, res) => {
-    if (!('contents' in req.body) || !('title' in req.body)) {
-        return res.status(400).send({ errorMessage: "Please provide title and contents for the post." })
-    }
-    const request = db.update(req.params.id, req.body);
-    return request
-    .then(response => {
-        if (response === 0) {
-            res.status(404).send({ message: "The post with the specified ID does not exist." })
-        } else {
-            db.find()
-            .then(re =>  res.status(200).json(re));
         }
-    })
-    .catch(error => res.status(500).send({ error: "The post information could not be modified." }))
+    } catch (error) {
+        res.status(500).send({ error: "The post information could not be retrieved." });
+    }
+})
+
+//Create endpoint for POST
+server.post('/api/posts',async (req, res) => {
+    if (!('contents' in req.body && 'title' in req.body)) {
+        return res.status(400).send({ errorMessage: "Please provide title and contents for the post." })
+    }
+    
+    try {
+        await db.insert(req.body);
+        res.status(201).json(req.body);
+    } catch(error) {
+        res.status(500).send({ error: "There was an error while saving the post to the database" })
+    }
+})
+
+//Create endpoint for DELETE
+server.delete('/api/posts/:id', async (req, res) => {
+    try {
+        const deleted = await db.findById(req.params.id);
+        if (deleted.length === 0) {
+            return res.status(404).send({ message: "The post with the specified ID does not exist." });
+        }
+
+        await db.remove(req.params.id);
+        res.status(200).json(deleted[0])
+    } catch (error) {
+        res.status(500).send({ error: "The post could not be removed" });
+    }
+})
+
+//Create endpoint for PUT
+server.put('/api/posts/:id',async (req, res) => {
+    if (!('contents' in req.body && 'title' in req.body)) {
+        return res.status(400).send({ errorMessage: "Please provide title and contents for the post." })
+    }
+    try {
+        const response = await db.update(req.params.id, req.body);
+        if (response===0) {
+            res.status(404).send({ message: "The post with the specified ID does not exist." });
+        } else {
+            const newPosts = await db.find();
+            res.status(200).json(newPosts);
+        }
+    } catch(erro){
+        res.status(500).send({ error: "The post information could not be modified." })
+    }
 })
 
 server.listen(8000, () => console.log('API running on port 8000'));
