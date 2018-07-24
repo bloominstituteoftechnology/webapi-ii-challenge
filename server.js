0bcf2bb09c5a2d2ca7c71f982aa9f92ea28ffd75
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-07-24 18:25:31 -0500

    Ahmed Belhadj - Node Express Lab
    GET	/api/posts/:id

--- server.js
+++ server.js
@@ -8,34 +8,48 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 
-server.post("/api/posts", (req, res) => {
-  db.insert();
-});
-
 server.post("/api/posts", async (req, res) => {
-  if (req.body.title == null || req.body.contents == null) {
-    res.status(400).json({
+  if (!req.body.title || !req.body.contents) {
+    return res.status(400).json({
       errorMessage: "Please provide title and contents for the post."
     });
-  }
-  try {
-    const post = await db.insert({ ...req.body });
-    res.status(201).json(post);
-  } catch (err) {
-    res.status(500).json({
-      error: "There was an error while saving the post to the database"
-    });
+  } else {
+    try {
+      const post = await db.insert({ ...req.body });
+      return res.status(201).json(post);
+    } catch (err) {
+      return res.status(500).json({
+        error: "There was an error while saving the post to the database"
+      });
+    }
   }
 });
 
 server.get("/api/posts", async (req, res) => {
   try {
     const posts = await db.find();
-    res.status(200).json(posts);
+    return res.status(200).json(posts);
   } catch (err) {
-    res
+    return res
       .status(500)
       .json({ error: "The posts information could not be retrieved." });
+  }
+});
+
+server.get("/api/posts/:id", async (req, res) => {
+  if (!db.findById(req.params.id)) {
+    return res.status(404).json({
+      message: "The post with the specified ID does not exist."
+    });
+  } else {
+    try {
+      const post = await db.findById(req.params.id);
+      return res.status(200).json(post);
+    } catch (err) {
+      return res
+        .status(500)
+        .json({ error: "The posts information could not be retrieved." });
+    }
   }
 });
 
