cb36950b6be3a1b66472d219f192d447a693313b
Gabriel Duquette <gabrielduquette@gmail.com>
2018-07-25 00:10:33 -0400

    fix try statements

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const server = express();
 
+const notRetrieved = "The information could not be retrieved.";
+const notFound = "The post with the specified ID does not exist.";
+const incompleteForm = "Please provide title and contents for the post.";
+const saveError = "There was an error while saving the post to the database";
+const editError = "The post information could not be modified.";
+
 server.use(bodyParser.json());
 server.use(cors());
 
@@ -13,13 +19,18 @@ server.get('/', (req, res) => {
 
 server.post('/api/posts', async (req, res) => {
   if (!req.body.title || !req.body.contents) {
-    return res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+    return res.status(400).json({ errorMessage: incompleteForm });
   }
   try {
-    const post = await db.insert(req.body);
-    res.status(201).json(post);
+    const { id } = await db.insert(req.body);
+    try {
+      const post = await db.findById(id);
+      res.status(201).json(post);
+    } catch (error) {
+      return res.status(404).json({ error: notFound });
+    }
   } catch (error) {
-    res.status(500).json({ error: "There was an error while saving the post to the database" });
+    res.status(500).json({ error: saveError });
   }
 });
 
@@ -28,29 +39,27 @@ server.get('/api/posts', async (req, res) => {
     const posts = await db.find();
     res.status(200).json(posts);
   } catch (error) {
-    res.status(500).json({ error: "The posts information could not be retrieved." });
+    res.status(500).json({ error: notRetrieved });
   }
 });
 
 server.get('/api/posts/:id', async (req, res) => {
   try {
     const post = await db.findById(req.params.id);
-    console.log(post);
     if (post.length === 0) {
-      return res.status(404).json({ message: "The post with the specified ID does not exist." });
+      return res.status(404).json({ message: notFound });
     }
     res.status(200).json(post);
   } catch (error) {
-    res.status(500).json({ error: "The post information could not be retrieved." })
+    res.status(500).json({ error: notRetrieved })
   }
 });
 
 server.delete('/api/posts/:id', async (req, res) => {
   try {
     const post = await db.remove(req.params.id);
-    console.log(post);
     if (post === 0) {
-      return res.status(404).json({ message: "The post with the specified ID does not exist." });
+      return res.status(404).json({ message: notFound });
     }
     res.status(200).json(post);
   } catch (error) {
@@ -60,17 +69,22 @@ server.delete('/api/posts/:id', async (req, res) => {
 
 server.put('/api/posts/:id', async (req, res) => {
   if (!req.body.title || !req.body.contents) {
-    return res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+    return res.status(400).json({ errorMessage: incompleteForm });
   }
   try {
-    const post = await db.update(req.params.id, req.body);
-    console.log(post);
-    if (post === 0) {
-      return res.status(404).json({ message: "The post with the specified ID does not exist." });
+    await db.update(req.params.id, req.body);
+    try {
+      const post = await db.findById(req.params.id);
+      if (post.length === 0) {
+        return res.status(404).json({ message: notFound });
+      } else {
+        return res.status(200).json(post);
+      }
+    } catch (error) {
+      return res.status(500).json({ error: notRetrieved });
     }
-    res.status(200).json(post);
   } catch (error) {
-    res.status(500).json({ error: "The post information could not be modified." });
+    return res.status(500).json({ error: editError });
   }
 });
 
