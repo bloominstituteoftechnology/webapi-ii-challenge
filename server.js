7edb0123885d670011583dbd4b2dd8d2268ce999
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2018-07-24 17:53:27 -0700

    convers to async/await

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ server.use(helmet());
 
 const PORT = 8001;
 
+const sendUserError = (status, errorMessage, res) => {
+  res.status(status).json({ errorMessage: errorMessage });
+};
+
 // Creates a post using the information sent inside the request body
 server.post('/api/posts', (req, res) => {
   const { title, contents } = req.body;
@@ -47,38 +51,36 @@ server.post('/api/posts', (req, res) => {
 });
 
 // Returns an array of all the post objects contained in the database
-server.get('/api/posts', (req, res) => {
-  data
-    .find()
-    .then(response => res.json(response))
-    .catch(err =>
-      res
-        .status(500)
-        .json({ error: 'The posts information could not be retrieved.' }),
-    );
+server.get('/api/posts', async (req, res) => {
+  try {
+    const response = await data.find();
+    return res.status(200).json(response);
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ error: 'The posts information could not be retrieved.' });
+  }
 });
 
 // Returns the post object with the specified id
-server.get('/api/posts/:id', (req, res) => {
+server.get('/api/posts/:id', async (req, res) => {
   // if id isn't supplied, it's just a get all "/api/posts"
   const id = req.params.id;
 
-  data
-    .findById(id)
-    .then(response => {
-      if (response.length !== 0) {
-        res.status(200).json(response);
-      } else {
-        res
-          .status(404)
-          .json({ message: 'The post with the specified ID does not exist.' });
-      }
-    })
-    .catch(() =>
-      res
-        .status(500)
-        .json({ error: 'The post information could not be retrieved.' }),
-    );
+  try {
+    const response = await data.findById(id);
+    if (response.length !== 0) {
+      return res.status(200).json(response);
+    } else {
+      return res
+        .status(404)
+        .json({ message: 'The post with the specified ID does not exist.' });
+    }
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ error: 'The post information could not be retrieved.' });
+  }
 });
 
 // delete a post - no ID given
@@ -87,31 +89,27 @@ server.delete('/api/posts/', (req, res) => {
 });
 
 // Removes the post with the specified id and returns the deleted post
-server.delete('/api/posts/:id', (req, res) => {
+server.delete('/api/posts/:id', async (req, res) => {
   const id = req.params.id;
 
-  // Removes the post with the specified id and returns the deleted post
-  data
-    .findById(id)
-    .then(findResponse => {
-      if (findResponse.length === 0) {
-        res
-          .status(404)
-          .json({ message: 'The post with the specified ID does not exist.' });
-        return;
-      }
-      data
-        .remove(id)
-        .then(() => res.status(200).json(findResponse))
-        .catch(() =>
-          res.status(500).json({ error: 'The post could not be removed' }),
-        );
-    })
-    .catch(() =>
-      res
-        .status(500)
-        .json({ error: 'The post information could not be retrieved.' }),
-    );
+  try {
+    const findResponse = await data.findById(id);
+    if (findResponse.length === 0) {
+      return res
+        .status(404)
+        .json({ message: 'The post with the specified ID does not exist.' });
+    }
+    try {
+      const delResponse = await data.remove(id);
+      return res.status(200).json(findResponse);
+    } catch (err) {
+      return res.status(500).json({ error: 'The post could not be removed' });
+    }
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ error: 'The post information could not be retrieved.' });
+  }
 });
 
 // edit a post - no ID given
@@ -121,7 +119,7 @@ server.put('/api/posts/', (req, res) => {
 
 // Updates the post with the specified id using data from the request body.
 // Returns the modified document, NOT the original
-server.put('/api/posts/:id', (req, res) => {
+server.put('/api/posts/:id', async (req, res) => {
   const id = req.params.id;
   const { title, contents } = req.body;
 
@@ -135,31 +133,27 @@ server.put('/api/posts/:id', (req, res) => {
   // post object
   const post = { title, contents };
 
-  data
-    .update(id, post)
-    .then(() => {
-      data
-        .findById(id)
-        .then(response => {
-          if (response.length === 0) {
-            res.status(404).json({
-              message: 'The post with the specified ID does not exist.',
-            });
-          } else {
-            res.status(200).json(response);
-          }
-        })
-        .catch(() => {
-          res.status(500).json({
-            error: 'The post information could not be retrieved.',
-          });
+  try {
+    await data.update(id, post);
+    try {
+      const response = await data.findById(id);
+      if (response.length === 0) {
+        return res.status(404).json({
+          message: 'The post with the specified ID does not exist.',
         });
-    })
-    .catch(() => {
-      res
-        .status(500)
-        .json({ error: 'The post information could not be modified.' });
-    });
+      } else {
+        return res.status(200).json(response);
+      }
+    } catch (err) {
+      return res.status(500).json({
+        error: 'The post information could not be retrieved.',
+      });
+    }
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ error: 'The post information could not be modified.' });
+  }
 });
 
 server.listen(PORT, console.log(`Server listening on port ${PORT}`));
