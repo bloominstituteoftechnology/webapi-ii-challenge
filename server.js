755a1a8a883fcec87045b388e6cb88ebb8403bbe
-tristan- <tristan.linehan@gmail.com>
2018-07-26 15:19:17 -0700

    PUT update completed.  MVP Met

--- server.js
+++ server.js
@@ -55,6 +55,16 @@ server.get('/api/posts', async(req, res) => {
         res.status(500).json({message: 'The posts information could not be retrieved.', error: error})
     }
 })
+// //SORT BY url querry
+// //localhost:8333/api/posts?sort_by=name
+// server.get('/api/posts', (req, res) => {
+//     const sortField = req.query.sort_by || 'id'; //!== sort_by
+//     //cannot find posts
+//     const response = posts.sort((a, b) => {
+//         return a[sortField] < b[sortField] ? -1 : 1;
+//     })
+//     res.status(200).json(response);
+// })
 
 //GET POST BY ID
 server.get('/api/posts/:id', (req, res) => {
@@ -112,6 +122,46 @@ server.delete('/api/posts/:id', (req, res) => {
             return;
         })
 })
+// //PUT UPDATE
+server.put('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    const { title, contents } = req.body;
+
+    if(!title || !contents) {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+        res.end();
+    }
+    db
+        .update(id, { title, contents })
+        .then(response => {
+            if (response === 0) {
+                res.status(404).json({ message: "The post with the specified ID does not exist." });
+                res.end();
+            }
+            db
+                .findById(id)
+                .then(post => {
+                    if (post.length === 0) {
+                        res.status(404).json({ message: "(NESTED) The post with the specified ID does not exist." });
+                        res.end();
+                    }
+                    res.json(post);
+                })
+                .catch(error => {
+                    res
+                        .status(500)
+                        .send({ message: "(NESTED) The post information could not be modified.", error: error.message });
+                        res.end();
+                })
+        })
+        .catch(error => {
+            res
+                .status(500)
+                .send({ message: "The post information could not be modified.", error: error.message });
+                res.end();
+        })
+})
+
 
 server.listen(port, () => console.log(`Server is running on port ${port}`));
 
