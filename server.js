8623518852743fe5189e609c0ed953ff836cdf0e
Sumayyah <sumayyah@zubairs-air.attlocal.net>
2018-07-31 15:28:22 -0700

    get and post working 🎉

--- server.js
+++ server.js
@@ -5,3 +5,44 @@ const db = require('./data/db.js');
 
 server.use(express.json());
 // add your server code starting here
+
+server.get('/api/posts', (req, res) => {
+  db.find()
+    .then((posts) => {
+      res.json(posts);
+    })
+    .catch(() => {
+      res.status(500).json({ error: 'The posts information could not be retrieved.' });
+    });
+});
+
+server.get('/api/posts/:id', (req, res) => {
+  const { id } = req.params;
+  db.findById(id)
+    .then((response) => {
+      if (response === 0) {
+        res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+      } else {
+        res.json(response); // send response if it exists
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ error: 'The post information could not be retrieved.' });
+    });
+});
+
+server.post('/api/posts', (req, res) => {
+  const post = req.body;
+  if (!post.title || !post.contents) {
+    res.status(400).json({ errorMessage: 'Please provide title and contents for the post.' });
+  }
+  db.insert(post)
+    .then((post) => {
+      res.status(201).json(post);
+    })
+    .catch(() => {
+      res.status(500).json({ error: 'There was an error while saving the post to the database' });
+    });
+});
+
+server.listen(5000, () => console.log('API running on port 5000'));
