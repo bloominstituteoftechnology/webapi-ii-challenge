121e630ddd7c2ddc835e4cfb2dcbb2a51286ea31
Katie Gorbell <katiegorbell@gmail.com>
2018-07-31 16:54:43 -0500

    added get request for db data

--- server.js
+++ server.js
@@ -4,7 +4,19 @@ const server = express();
 
 const db = require('./data/db.js');
 
+server.use(express.json());
+
 // add your server code starting here
 
 
-server.listen( , () => console.log('API running...'))
+server.listen(8000, () => console.log('API running...'))
+
+server.get('/posts', (req, res) => {
+    db.find()
+        .then(response => {
+            res.status(200).json(response)
+        })
+        .catch(() => {
+            res.status(500).json({ error: 'The posts information could not be retrieved' })
+        })
+})
