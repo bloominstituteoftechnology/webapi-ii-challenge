1803851db1520e19e465b562db4007dd5b663a72
Katie Gorbell <katiegorbell@gmail.com>
2018-07-31 17:51:54 -0500

    added delete request

--- server.js
+++ server.js
@@ -47,5 +47,19 @@ server.post('/posts', (req, res) => {
         })
 })
 
+server.delete('/posts/:id', (req, res) => {
+    const {id} = req.params;
+    db.remove(id)
+        .then(response => {
+            if (response == 0) {
+                res.status(404).json({ message: 'The post with the specified ID does not exist' })
+            }
+            res.status(200).json(response)
+        })
+        .catch(() => {
+            res.status(500).json({ error: 'The post could not be removed' })
+        })
+})
+
 
 server.listen(8000, () => console.log('API running...'))
