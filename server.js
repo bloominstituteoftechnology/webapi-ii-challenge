37e402b9e95be5186b18a7da0ba20aa6d80571b0
unknown <may.alex43@gmail.com>
2018-07-31 17:48:19 -0400

    having late start, setting up now

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 // import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
+const server= express();
 
 // add your server code starting here
+server.use(express.json());
+server.use(helmet());
