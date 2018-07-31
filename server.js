d921c65941844ae2e9968ead7420c003a412545d
Sumayyah <sumayyah@zubairs-air.attlocal.net>
2018-07-31 12:51:43 -0700

    PR

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // import your node modules
-
+const express = require('express');
+const server = express();
 const db = require('./data/db.js');
 
+server.use(express.json());
 // add your server code starting here
