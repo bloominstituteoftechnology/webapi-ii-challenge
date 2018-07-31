1dd11030b5b3aa57b370dd80b95135064fdec6d2
Daniel Chamorro <dan@dcitllc.com>
2018-07-31 15:55:16 -0400

    dchamorro - Added GET Request, Tested with PM

--- server.js
+++ server.js
@@ -49,4 +49,22 @@ server.post("/api/posts", (req, res) => {
     );
 });
 
+//* GET Request
+// TODO: When the client makes a GET request to /api/posts:
+
+// TODO: If there's an error in retrieving the posts from the database:
+// TODO: cancel the request.
+// TODO: respond with HTTP status code 500.
+// TODO: return the following JSON object: { error: "The posts information could not be retrieved." }.
+
+server.get("/api/posts", (req, res) => {
+  db.find()
+    .then(posts => res.status(200).json(posts))
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: "The posts information could not be retrieved." })
+    );
+});
+
 server.listen(8000, () => console.log("API running on port 8000"));
