38d431ad1ec65d49626ee8bd11c674a35b7322d2
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-07-31 11:53:03 -0600

    initial commit

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 // import your node modules
-
+const express = require('express');
+const helmet = require('helmet');
 const db = require('./data/db.js');
 
 // add your server code starting here
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
