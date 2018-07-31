8f4658080392ceca8fb10e1e7cbfc94fb0be3e28
Katie Gorbell <katiegorbell@gmail.com>
2018-07-31 13:59:44 -0500

    initial push

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 // import your node modules
+const express = require('express');
+const server = express();
 
 const db = require('./data/db.js');
 
 // add your server code starting here
+
+
+server.listen( , () => console.log('API running...'))
