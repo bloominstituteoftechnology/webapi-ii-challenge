30058e46d5fc9b3ed3833f1c5aeafa22aef97969
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-07-31 15:32:31 -0600

    get, post, put, delete

--- server.js
+++ server.js
@@ -9,3 +9,100 @@ const server = express();
 
 server.use(helmet());
 server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send('Api Running');
+});
+
+server.get('/api/posts', (req, res) => {
+  
+    db
+        .find()
+        .then(posts => {
+            res.json(posts);
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'The post information could not be retrieved' });
+        });
+});
+
+server.get('/api/posts/:id', (req, res) => {
+    const id = req.params.id;
+
+    db
+        .findById(id)
+        .then(post => {
+            if (users.length === 0) {
+                res.status(404).json({ message: 'The post with the specified ID does not exist' });
+            } else {
+                res.json(posts[0]);
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'The post information could not be retrieved' });
+        });
+
+});
+
+server.post('/api/posts', (req, res) => {
+    const userPost = req.body;
+    console.log('user post', userPost);
+
+    db
+        .insert(userPost)
+        .then(response => {
+            res.status(201).json(response);
+        })
+        .catch(err => {
+            if (err.error === 19) {
+                res.status(400).json({ errorMessage: 'Please provide title and contents for the post' });
+            } else {
+                res.status(500).json({ errorMessage: 'There was an error while saving the post to the database' });
+            }
+        });
+});
+
+server.put('/api/posts/:id', function (req, res) {
+    const { id } = req.params;
+    const update = req.body;
+
+    db
+        .update(id, update)
+        .then(count => {
+            if (count > 0) {
+                res.status(200).json(users[0]);
+            } else {
+                res.status(404).json({ message: 'The post with the specified ID does not exist' });
+            }
+        })
+        .catch(err => {
+            if (err.error === 19) {
+                res.status(400).json({ errorMessage: 'Please provide title and contents for the post' });
+            } else {
+                res.status(500).json({ error: 'The post information could not be modified' });
+            }
+        });
+});
+
+server.delete('/api/posts/:id', function (req, res) {
+    const { id } = req.query;
+    let user;
+
+    db
+        .findById(id)
+        .then(foundPost => {
+            user = { ...foundPost[0] };
+        })
+
+    db
+        .remove(id)
+        .then(response => {
+            res.status(204).json(response);
+        })
+
+        .catch(err => {
+            res.status(500).json({ error: err });
+        });
+});
+
+server.listen(5000, () => console.log('\n== API Running on port 5000 ==\n'));
