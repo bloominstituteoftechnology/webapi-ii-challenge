da124ff7bd73304dc80a278ac71024c6b0e094b5
sean chavez <seanchavez@seanchavezsMBP.home>
2018-07-31 19:45:39 -0700

    Create React  app and render  posts

---
+++ client/src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+      title: '',
+      contents: ''
+    }
+  }
+
+  componentDidMount(){
+    axios
+      .get('http://localhost:8000/api/posts')
+      .then(response => {
+        console.log(response);
+        this.setState({ posts: response.data })
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        {
+          this.state.posts.map(post => {
+            return (
+            <div key={post.id}>
+              <div>{post.title}</div>
+              <div>{post.contents}</div>
+            </div>
+            )
+          })
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const server = express();
 server.use(express.json());
+const cors = require('cors');
+server.use(cors());
 
 const db = require('./data/db.js');
 
