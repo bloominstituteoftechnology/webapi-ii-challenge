9be4bbd5a3820cdcb1bb7f51b78c1201b26152e2
M.Real <mreal126@protonmail.com>
2018-08-02 11:24:15 -0400

    Computer struggling to handle postman in addition to other stuff. Hoping to be able to test code soon.

--- server.js
+++ server.js
@@ -2,48 +2,81 @@ const express = require('express');
 const helmet = require('helmet');
 const db = require('./data/db');
 
-const port = 5555;
+const port = 8000;
 const server = express();
 server.use(helmet());
 server.use(express.json());
 
-let nextId = 3;
 
 server.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-server.get('/api/posts', (req, res) => {
-    db.find()
-        .then(posts => {
-            res.status(200).json(posts);
-    })
-        .catch(err => res.status(500).json({ message: "Error"}));
-});
-
-server.post('/api/posts', (req, res) => {
-    const content = { id: nextId++, ...req.body } // edit for post
-    posts.push(title);
-    res.status(200).json(posts);
-});
-
-server.put('/api/posts/:id', (req, res) => {
-    const id = req.params.id;
-    const changes = req.body;
-    const format = req.query.format || 's';
-
-    // update user
-    res.status(200).json(posts);
+server.get('/api/posts', async (req, res) => {
+    try {const response = await db.find();
+        res.status(200).json(response);
+    } 
+    catch (error) {
+        res.status(500).json({ error: "Post not retrieved" })
+    }
 })
 
-server.delete('/api/posts/:id', (req, res) => {
-    const { id } = req.params;
-    posts = posts.filter(u => u.id != id);
-    res.status(200).json(posts);
+server.get('/api/posts/:id', async (req, res) => {
+    try {
+        const response = await db.findById(req.params.id);
+        if (response.length === 0) {
+            res.status(404).send({ message: "Post does not exist" });
+        } 
+        else {
+            res.status(200).json(response)
+        }
+}   catch (error) {
+        res.status(500).send({ error: "Post not retrieved" });
+    }
 })
 
-server.get('/api/posts', (req, res) => {
-    const sortField = req.query.sortby || 'id';
-    
-    res.status(200).json(posts);
+server.post('/api/posts',async (req, res) => {
+    if (!('contents' in req.body && 'title' in req.body)) {
+        return res.status(400).send({ errorMessage: "Missing title and content" })
+    }
+    try {
+        await db.insert(req.body);
+        res.status(201).json(req.body);
+    } 
+    catch(error) {
+        res.status(500).send({ error: "Failure to save" })
+    }
 })
+
+server.delete('/api/posts/:id', async (req, res) => {
+    try {
+        const deleted = await db.findById(req.params.id);
+        if (deleted.length === 0) {
+            return res.status(404).send({ message: "Post does not exist" });
+        }
+        await db.remove(req.params.id);
+        res.status(200).json(deleted[0])
+    } 
+    catch (error) {
+        res.status(500).send({ error: "Failure to remove post" });
+    }
+})
+
+server.put('/api/posts/:id',async (req, res) => {
+    if (!('contents' in req.body && 'title' in req.body)) {
+        return res.status(400).send({ errorMessage: "Please provide title and contents for the post." })
+    }
+    try {
+        const response = await db.update(req.params.id, req.body);
+        if (response===0) {
+            res.status(404).send({ message: "Post does not exist" });
+        } else {
+            const newPosts = await db.find();
+            res.status(200).json(newPosts);
+        }
+}   catch(error){
+        res.status(500).send({ error: "Failure to modify" })
+    }
+})
+
+server.listen(8000, () => console.log('API running on port 8000')); 
