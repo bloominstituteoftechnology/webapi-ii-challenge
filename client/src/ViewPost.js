dfcea7e3a23c82f78f4735b25498006f61b6a34a
Sumayyah <sumayyah@zubairs-air.attlocal.net>
2018-08-06 00:23:12 -0700

    Css colors added 💖

--- client/src/ViewPost.js
+++ client/src/ViewPost.js
@@ -11,12 +11,25 @@ class ViewPost extends Component {
 
   render() {
     return (
-      <div>
-        <div>{this.props.location.state.title}</div>
-        <div>{this.props.location.state.contents}</div>
-        <Link to="/posts" onClick={() => this.props.deletePost(this.props.location.state.id)}>
-          Delete
-        </Link>
+      <div className="post-container">
+        <div className="post">
+          <div className="title">{this.props.location.state.title}</div>
+          <br />
+          <div className="text">{this.props.location.state.contents}</div>
+          <br />
+          <div className="links">
+            <Link
+              className="link"
+              to="/posts"
+              onClick={() => this.props.deletePost(this.props.location.state.id)}
+            >
+              Delete
+            </Link>
+            <Link className="link" to="/posts">
+              Back
+            </Link>
+          </div>
+        </div>
       </div>
     );
   }
