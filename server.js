2883d4c8abdd3e375b6fee6f2aa7b7c2d99011e3
Charles Cortinas <crcortinas@gmail.com>
2018-08-27 18:45:32 -0500

    GET
    Can use GET methods to get all items or find item by id number

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
+const express = require('express');
+const server = express();
 // import your node modules
 
 const db = require('./data/db.js');
 
 // add your server code starting here
-const express = require('express');
-
-const server = express();
-
-server.get('/', (req,res) => {
-  res.send('help');
-})
 
 server.get('/api/posts', (req, res) => {
-  const posts = db.find();
-  console.log(posts);
+  db.find()
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+});
+
+server.get('/api/posts/:id', (req, res) => {
+  db.findById(req.params.id)
+    .then((post) => {
+      res.status(200).json(post);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
 });
 
 server.listen(8000, () => {
