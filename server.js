4b6b24a3ccbac8ce179a413ef0d82f3ce81fd493
codejoncode <jonathanjholloway@gmail.com>
2018-08-27 16:36:54 -0500

    Posts are now on the page

--- server.js
+++ server.js
@@ -10,7 +10,13 @@ server.get('/', (req,res) => {
 })
 
 server.get('/posts', (req,res) =>{
-    db.find().then().catch()
+    db.find().then(posts => {
+        res.status(200).json(posts);
+    }).catch( error => {
+        console.log.error('error', error);
+
+        res.status(500).json({message: 'Error getting the data'})
+    })
 })
 // server.get('/users', (req, res) => {
 //     db.find().then( users => {
