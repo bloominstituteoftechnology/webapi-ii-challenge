05a7bd23ce84f2a0b44ad212bf59160c18ec2dae
Jurgen Kela <jurgenkela@Jurgens-MacBook-Pro.local>
2018-08-27 17:44:59 -0400

    POST method working

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-// import your node modules
 const express = require('express');
-
 const db = require('./data/db.js');
-
-// add your server code starting here
-
 const server = express();
+
+server.get('/api/posts', (req, res) => {
+    db.find()
+        .then(posts => {
+            res.status(200).json(posts)
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'Could not get your info '})
+        })
+})
+
+
+server.listen(3000, () => console.log('This is working'));
