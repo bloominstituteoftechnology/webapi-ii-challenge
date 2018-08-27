a1f39b88522dad15b4b87458ff3c922b7a8754d0
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2018-08-27 14:00:14 -0700

    Implemented GET by id

--- server.js
+++ server.js
@@ -10,16 +10,33 @@ server.get('/', (req, res) => {
     res.send('Hello Fernando');
 });
 
-server.get('/db', (req, res) => {
+server.get('/api/posts', (req, res) => {
     db.find()
-        .then(posts => {
-            res.status(200).json(posts);
-        })
-        .catch(err => {
-            console.error('error', err);
+    .then(posts => {
+        res.status(200).json(posts);
+    })
+    .catch(err => {
+        console.error('error', err);
 
-            res.status(500).json({ message: 'Error setting the data' });
-        });
+        res.status(500).json({ error: 'The posts information could not be retrieved.' });
+    });
 })
+
+server.get('/api/posts/:id', (req, res) => {
+    db.findById(req.params.id)
+    .then(post => {
+        if( post.length < 1) {
+            res.status(404).json({ error: 'The post with the specified ID does not exist.' })
+        }
+        else {
+            res.status(200).json(post);
+        }  
+    })
+    .catch(err => {
+        console.error('error', err);
+
+        res.status(500).json({ error: 'The post information could not be retrieved.' });
+    });
+});
 
 server.listen(9000, () => console.log('\n== API on port 9k ==\n'));
