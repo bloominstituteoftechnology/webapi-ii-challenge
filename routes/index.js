52a02b3081626c62df9bed93c11136e2cc4707dd
kamalnrf <kamalnrf@hotmail.com>
2018-08-28 02:03:56 +0530

    Add getPost by id

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const router = express.Router()
 const posts_controller = require('../controllers/post_controller')
 
 router.get('/', posts_controller.getPosts)
+router.get('/:id', posts_controller.getPostByID)
 router.post('/', posts_controller.addPost)
 router.put('/:id', posts_controller.updatePost)
 router.delete('/:id', posts_controller.deletePost)
