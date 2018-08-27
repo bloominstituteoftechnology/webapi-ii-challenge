63d51a3e7c90c02b6a8e8320f4f599bc1965a473
Charles Cortinas <crcortinas@gmail.com>
2018-08-27 18:13:49 -0500

    Initial Commit
    Initial Commit

--- server.js
+++ server.js
@@ -3,3 +3,19 @@
 const db = require('./data/db.js');
 
 // add your server code starting here
+const express = require('express');
+
+const server = express();
+
+server.get('/', (req,res) => {
+  res.send('help');
+})
+
+server.get('/api/posts', (req, res) => {
+  const posts = db.find();
+  console.log(posts);
+});
+
+server.listen(8000, () => {
+  console.log('Listening on port 8000...');
+})
