b08ceefe43bd66eb2f01a7eeed0ab64b17c12c35
William <kwontaewoongah@gmail.com>
2018-08-27 15:18:00 -0700

    implemented single post get

--- server.js
+++ server.js
@@ -11,6 +11,14 @@ server.use(express.json());
 server.get('/posts',(req,res)=>
     db.find().then(posts=>res.status(200).json(posts)).catch(err=>res.status(500).json({error: "The posts information could not be retrieved."}))
 )
-
+server.get('/posts/:id',(req,res)=>
+    db.findById(req.params.id)
+    .then(post=>{
+        if (post.length===0){
+        return res.status(404).json({message: "The post with the specified ID does not exist."})
+        } else {
+        return res.status(200).json(post)}})
+    .catch(err=>res.status(500).json({error: "The post information could not be retrieved." }))
+)
 
 server.listen(9000,()=>console.log('Engines firing server starting new horizons venturing.'))
