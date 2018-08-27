fef47a10eb0a2e15755cb69bdba91a8b497029a0
Sean <sean47900@gmail.com>
2018-08-27 16:08:41 -0500

    trying to get post working

--- server.js
+++ server.js
@@ -4,17 +4,16 @@ const server = express();
 const db = require('./data/db.js');
 
 // add your server code starting here
-server.get("/", (req, res) => {
-    res.send('Hello');
-})
- server.get("/posts", (req, res) => {
+
+server.get("/api/posts", (req, res) => {
     db.find()
         .then(posts => {
             res.status(200).json(posts)
         })
         .catch(err => {
             console.error('error', err)
-            res.status(500).json({message: 'Failed to retrieve data'})
+            res.status(500).json({errorMessage: 'Failed to retrieve data'})
         })
 })
- server.listen(9000, () => console.log(`server`))
+
+ server.listen(9000, () => console.log(`\n=== API on port 9000 ===\n`))
