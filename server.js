f0efc6cc7b9c010a3c9bfcfcd3a0180ae964230e
Carey <careycbaldwin@gmail.com>
2018-08-27 16:27:41 -0400

    included a get request to retrieve data from db.js

--- server.js
+++ server.js
@@ -12,4 +12,16 @@ server.get('/', (req, res) => {
     res.send('Server Initiated');
 });
 
+//Configuring Routing with specific endpoint
+server.get('/posts', (req,res) => {
+    db.find()
+    .then(posts => {
+        res.status(200).json(posts);
+    })
+    .catch(err => {
+        console.error('error',err);
+        res.status(500).json({message: 'Error getting data'})
+    });
+});
+
 server.listen(9000, () => console.log('\n==API on port 9k==\n'));
