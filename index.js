ae2752f8f9093dd8889f2a992cfda54f6c7de100
szincone <sawyerzincone@gmail.com>
2018-08-27 13:51:15 -0600

    finished first GET request that returns an array of all post objects in the db

--- index.js
+++ index.js
@@ -5,14 +5,22 @@ const server = express();
 
 server.use(express.json());
 
-server.get("/", (req, res) => {
-  res.send("Testing Server");
-});
-
-// server.get("/api/posts", (req, res) => {
-//   db.find()
-//     .then()
-//     .catch();
+// server.get("/", (req, res) => {
+//   res.send("Testing Server");
 // });
+
+server.get("/api/posts", (req, res) => {
+  db.find()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      console.error("error", err);
+
+      res
+        .status(500)
+        .json({ error: "The posts information could not be retrieved." });
+    });
+});
 
 server.listen(8000, () => console.log("\n== API on port 8k === \n"));
