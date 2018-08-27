b842c7789b832017b3fc530290d9931c65d6d973
szincone <sawyerzincone@gmail.com>
2018-08-27 15:17:36 -0600

    updatedd getid to include if for outofbounds

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ server.get("/api/posts", (req, res) => {
     })
     .catch(err => {
       console.error("error", err);
-
       res
         .status(500)
         .json({ error: "The posts information could not be retrieved." });
@@ -24,33 +23,21 @@ server.get("/api/posts", (req, res) => {
 });
 
 server.get("/api/posts/:id", (req, res) => {
-  // res
-  //   .send(req.params.id)
-  //   .then()
-  //   .catch();
-  // db.find(req.params.id)
-
-  // db.find()
-  //   .then(post => {
-  //     res.status(200).json(posts);
-  //   })
-  //   .catch(err => {
-  //     console.log("error", err);
-
-  //     res
-  //       .status(404)
-  //       .json({ message: "The post with the specified ID does not exist." });
-  //   });
   let id = req.params.id;
   db.findById(id)
     .then(post => {
+      if (post.length === 0) {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
       res.status(200).json(post);
     })
     .catch(err => {
       console.log("error", err);
       res
-        .status(404)
-        .json({ message: "The post with the specified ID does not exist." });
+        .status(500)
+        .json({ error: "The post information could not be retrieved." });
     });
 });
 
