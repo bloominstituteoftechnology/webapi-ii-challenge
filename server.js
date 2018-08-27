3102db743919868c59094fbbb39d1d2c0a0eb51b
Charlie Winslow <cwinslow22@yahoo.com>
2018-08-27 16:43:03 -0500

    updated functionality for .get /api/posts/:id

--- server.js
+++ server.js
@@ -20,9 +20,23 @@ server.get('/api/posts', (req, res) => {
     .catch(err => {
         console.error('error', err);
 
-        res.status(500).json({ message: 'Error getting the data' });
+        res.status(500).json({ error: 'The posts information could not be retrieved.' });
     })
 })
 
+server.get('/api/posts/:id', (req, res) => {
+    db.findById(req.params.id)
+    .then(post => {
+        if (post.length === 0) {
+            res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+            return;
+        }
+        res.status(200).json(post);
+    })
+    .catch(err => {
+        console.error('error', err);
+        rest.status(500).json({ error: 'The post information could not be retrieved.'})
+    })
+});
 
 server.listen(5000, () => console.log('/n== API on port 5k==/n') );
