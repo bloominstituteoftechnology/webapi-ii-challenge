678ea459de9f1f2d6dee4db0efdf0b987f4e7196
David Pok <david.pok@gmail.com>
2018-08-27 15:06:50 -0700

    adds get requests

--- server.js
+++ server.js
@@ -1,5 +1,37 @@
 // import your node modules
+const express = require("express");
 
-const db = require('./data/db.js');
+const db = require("./data/db.js");
 
 // add your server code starting here
+const server = express();
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send("i suck at coding");
+});
+
+server.get("/api/posts", (req, res) => {
+  db.find()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      console.log("error", err);
+      res.status(500).json({ message: "you suck" });
+    });
+});
+
+server.get("/api/posts/:id", (req, res) => {
+  db.findById(req.params.id)
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(err => {
+      console.log("error", err);
+      res.status(500).json({ message: "yousuck" });
+    });
+});
+
+server.listen(4000, () => console.log("/n= API on port 4000 =/n"));
