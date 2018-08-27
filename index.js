7cb412946a35d8dc4269351d6047b08aa3922f48
Carey <careycbaldwin@gmail.com>
2018-08-27 16:11:13 -0400

    installed dependencies and started up server

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-// import your node modules
 const express = require('express');
 const db = require('./data/db.js')
 const server = express();
-
-// add your server code starting here
 
 server.use(express.json());
 
