db9b1e6409150dab614b1f8ab6744783c6d36d74
kamalnrf <kamalnrf@hotmail.com>
2018-08-28 02:02:29 +0530

    Add Delete API

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,6 @@ const posts_controller = require('../controllers/post_controller')
 router.get('/', posts_controller.getPosts)
 router.post('/', posts_controller.addPost)
 router.put('/:id', posts_controller.updatePost)
+router.delete('/:id', posts_controller.deletePost)
 
 module.exports = router 
