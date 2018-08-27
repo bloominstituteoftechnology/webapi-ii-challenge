5dd37cfffe2d1761fb9375167ef941e5bda86afe
Charlie Winslow <cwinslow22@yahoo.com>
2018-08-27 15:45:35 -0500

    added functionality for /api/posts GET request

--- server.js
+++ server.js
@@ -12,5 +12,17 @@ server.get('/', (req, res) => {
     res.send('Welcome to your training day user');
 } );
 
+server.get('/api/posts', (req, res) => {
+    db.find()
+    .then(posts => {
+        res.status(200).json(posts);
+    })
+    .catch(err => {
+        console.error('error', err);
+
+        res.status(500).json({ message: 'Error getting the data' });
+    })
+})
+
 
 server.listen(5000, () => console.log('/n== API on port 5k==/n') );
