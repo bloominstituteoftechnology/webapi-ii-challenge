776c6bc2cd51f6cabcad4a9f27c908a5fa195f3c
szincone <sawyerzincone@gmail.com>
2018-08-27 16:44:21 -0600

    post working

--- index.js
+++ index.js
@@ -45,53 +45,47 @@ server.get("/api/posts/:id", (req, res) => {
 
 // START POST
 
+let nextId = 10;
 let posts = [
   {
-    id: 10,
-    title: "The post title",
-    contents: "The post contents"
-  },
-  {
-    id: 11,
+    id: nextId,
     title: "The post title",
     contents: "The post contents"
   }
 ];
-let nextId = 12;
 
 server.post("/api/posts", (req, res) => {
+  const post = req.body;
+  post.id = nextId++;
+  post.title = `Title number ${post.id}`;
+  post.contents = `Contents number ${post.id}`;
+
+  ///////////////////////////
+  // this kind of work below
   // const post = req.body;
   // post.id = nextId++;
 
   // posts.push(post);
   // res.status(201).json(posts);
+  // stop kind of works
+  //////////////////////////////
 
-  // const newPost = req.body;
-  // newPost.id = nextId++;
-  // console.log("NEWPOST", newPost.title);
-  // posts.push(newPost);
-  // db.insert(posts)
-  //   .then(post => {
-  //     console.log("POSTNEW", newPost.title);
-  //     // if (post.title === 0) {
-  //     //   res
-  //     //     .status(404)
-  //     //     .json({ message: "The post with the specified ID does not exist." });
-  //     // }
-  //     res.status(200).json(post);
-  //   })
-  //   .catch(err => {
-  //     console.log("error", err);
-  //     res
-  //       .status(500)
-  //       .json({ error: "The post information could not be retrieved." });
-  //   });
-
-  const post = req.body;
-  post.id = nextId++;
-
-  posts.push(post);
-  res.status(201).json(posts);
+  db.insert(post)
+    .then(post => {
+      console.log("POSTNEW", newPost.title);
+      // if (post.title === 0) {
+      //   res
+      //     .status(404)
+      //     .json({ message: "The post with the specified ID does not exist." });
+      // }
+      res.status(200).json(post);
+    })
+    .catch(err => {
+      console.log("error", err);
+      res
+        .status(500)
+        .json({ error: "The post information could not be retrieved." });
+    });
 });
 
 server.listen(8000, () => console.log("\n== API on port 8k === \n"));
