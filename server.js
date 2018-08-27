b2e7108bd077a389907a6fad88a3eaf4c6cfce4b
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-08-27 16:05:12 -0500

    On my last Get
    Nothing is showing up for my GET id page.

--- server.js
+++ server.js
@@ -6,3 +6,31 @@ const server = express();
 server.use(express.json()); 
 
 // add your server code starting here
+server.get('/', (req, res)=>{
+    res.send("Hello World");
+});
+
+server.get('/posts', (req, res)=>{
+    db.find()
+    .then( posts => {
+        res.status(200).json(posts);
+    })
+    .catch(err =>{
+        console.log('error', err); 
+
+        res.status(500).json({ message:'Error Getting Data' })
+    })
+}); 
+server.get('/posts/:id', (req, res)=>{
+    const id = req.params
+    db
+    .findById(id)
+    .then( posts =>{
+        res.status(200).json(posts);
+    }).catch(err=>{
+        console.log('error', err);
+        res.status(404).json({message: "The posts with the specified ID does not exist."})
+    })
+})
+
+server.listen(8000, ()=> console.log(`/n== API on port 8k ==/n`)); 
