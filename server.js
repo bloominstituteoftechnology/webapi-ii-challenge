141a51fb5e935176a25522d588d29dd0d1cf569e
codejoncode <jonathanjholloway@gmail.com>
2018-08-27 16:22:39 -0500

    The server is running but no posts yet

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
 // import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req,res) => {
+    res.send("Hello FSW12 Express Node.js backend")
+})
+
+server.get('/posts', (req,res) =>{
+    db.find().then().catch()
+})
+// server.get('/users', (req, res) => {
+//     db.find().then( users => {
+//         res.status(200).json(users);
+//     }).catch(err => {
+//         console.log.error('error', err);
+
+//         res.status(500).json({message: 'Error getting the data'})
+//     });
+// })
+
+
+server.listen(9000, () => console.log('\n==API on port 9000 ==\n'))
 
 // add your server code starting here
