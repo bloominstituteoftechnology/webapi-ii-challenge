d8bab55ea3f8213cb45ea17d4af161d490e226e2
Charlie Winslow <cwinslow22@yahoo.com>
2018-08-27 15:37:06 -0500

    setup a server.get for /

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 // import your node modules
 
 const db = require('./data/db.js');
+const express = require('express');
 
 // add your server code starting here
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send('Welcome to your training day user');
+} );
+
+
+server.listen(5000, () => console.log('/n== API on port 5k==/n') );
