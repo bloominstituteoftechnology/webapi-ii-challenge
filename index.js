55b657551444ab80d3312d0f0b5aa3870811f4ee
szincone <sawyerzincone@gmail.com>
2018-08-27 17:16:45 -0600

    working post

--- index.js
+++ index.js
@@ -46,42 +46,39 @@ server.get("/api/posts/:id", (req, res) => {
 // START POST
 
 let nextId = 10;
-let posts = [
-  {
-    id: nextId,
-    title: "The post title",
-    contents: "The post contents"
-  }
-];
+// let posts = [
+//   {
+//     id: nextId,
+//     title: "The post title",
+//     contents: "The post contents"
+//   }
+// ];
 
 server.post("/api/posts", (req, res) => {
-  const post = req.body;
-  post.id = nextId++;
-  post.title = `Title number ${post.id}`;
-  post.contents = `Contents number ${post.id}`;
-
   ///////////////////////////
-  // this kind of work below
+  // the only way I can get it to work with postman
   // const post = req.body;
   // post.id = nextId++;
-
   // posts.push(post);
   // res.status(201).json(posts);
   // stop kind of works
   //////////////////////////////
+  const post = req.body;
+  // post.id = nextId++;
+  post.id = nextId++;
+  post.title = `Title number ${post.id}`;
+  post.contents = `Contents number ${post.id}`;
+  if (!req.body.title || !req.body.contents) {
+    return res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    });
+  }
 
   db.insert(post)
     .then(post => {
-      console.log("POSTNEW", newPost.title);
-      // if (post.title === 0) {
-      //   res
-      //     .status(404)
-      //     .json({ message: "The post with the specified ID does not exist." });
-      // }
       res.status(200).json(post);
     })
     .catch(err => {
-      console.log("error", err);
       res
         .status(500)
         .json({ error: "The post information could not be retrieved." });
