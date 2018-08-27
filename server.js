ab922d86b93a9eca7a16d48a78ef925ca2e8445f
dasgma <dasgrigoma@gmail.com>
2018-08-27 15:47:15 -0700

    Updated a few things.

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 // import your node modules
 const express = require('express');
 const db = require('./data/db.js');
-const server = express();
+
 // add your server code starting here
+
+const server = express();
 server.use(express.json());
+
+
 
 server.get('/', (req, res) => {
     res.send('We are in the root!');
@@ -18,7 +22,29 @@ server.get('/api/posts', (req, res) => {
             console.error('error', error);
 
             res.status(500).json({
-                message: 'Error retrieving the data'
+                message: 'The posts information could not be retrieved!'
+            })
+        })
+})
+
+
+server.get('/api/posts/:id', (req, res) => {
+    const {id} = req.params;
+    db.findById(req.params.id)
+        .then(posts => {
+            if(posts.length > 0) {
+                res.json({posts})
+            } else {
+                res.status(404);
+                res.json({
+                    message: 'The post with the specified ID does not exist!'
+                })
+            }
+        })
+        .catch(error => {
+            res.status(500);
+            res.json({
+                error: 'The post information could not be retrieved!'
             })
         })
 })
@@ -27,7 +53,7 @@ server.post('api/posts', (req, res) => {
     if(!req.body.title || !req.body.contents) {
         res.status(400);
         res.json({
-            message: 'Please provide title and contents for the post'
+            message: 'Please provide title and contents for the post!'
         });
     } else {
         const {title, contents} = req.body;
@@ -42,7 +68,7 @@ server.post('api/posts', (req, res) => {
             .catch(error => {
                 res.status(500);
                 res.json({
-                    error: 'There was an error saving the post to the database!'
+                    error: 'There was an error while saving the post to the database!'
                 })
             })
     }
@@ -71,33 +97,33 @@ server.delete('/api/posts/:id', (req, res) => {
 })
 
 server.put('/api/posts/:id', (req, res) => {
-    if (req.body.title && req.body.contents) {
-        res.status(200);
-        res.json({user});
-    } else if (!req.body.title || !req.body.contents) {
+    const {title, contents} = req.body;
+    const id = req.params.id;
+    if (!title || !contents) {
         res.status(400);
         res.json({
-            message: 'Please provide title and the contents for the user!'
-        })
+            message: 'Please provide title and contents for the post!'
+        });
     } else {
-        const {id} = req.params;
-        const {title, content} = req.body;
-        db.update (id, {title, contents})
+        db.update(id, {title, contents})
             .then(success => {
-                if (posts.length > 0) {
-                    res.json({success});
+                if(success) {
+                    res.status(200);
+                    db.findById(id)
+                        .then(posts => {
+                            res.json({posts});
+                        });
                 } else {
                     res.status(404);
                     res.json({
                         message: 'The post with the specified ID does not exist!'
                     });
                 }
-            }
-    )
+            })
     .catch(error => {
         res.status(500);
         res.json({
-            error: 'The post could not be found'
+            error: 'The post information could not be modified!'
         });
     })
     }
