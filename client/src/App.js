83e658250efa5c2e914e71553365f559c265dfd0
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-08-27 13:20:23 -0700

    implement client side code

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Posts from './Posts';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Posts />
+      </div>
+    );
+  }
+}
+
+export default App;
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const db = require('./data/db.js');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('hello');
