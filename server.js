5a18e453c3332c2ba4432b0a02d39c96ac30f857
sca4cs <shannon.atkinson.ca@gmail.com>
2018-08-27 16:51:28 -0400

    get requests working

--- server.js
+++ server.js
@@ -1,6 +1,35 @@
 // import your node modules
+const express = require('express');
 
 const db = require('./data/db.js');
 
+const server = express();
+
 // add your server code starting here
-//test comment
+server.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+server.get('/api/posts', (req, res) => {
+    db.find()
+    .then(posts => {
+        res.status(200).json(posts);
+    })
+    .catch(err => {
+        console.error(err);
+        res.status(500).json({ error: "The posts information could not be retrieved." });
+    });
+})
+
+server.get('/api/posts/:id', (req, res) => {
+    db.findById(req.params.id)
+    .then(post => {
+        res.status(200).json(post);
+    })
+    .catch(err => {
+        console.error(err);
+        res.status(500).json({ error: "The post information could not be retrieved." });
+    });
+})
+
+server.listen(8000, () => console.log('API running on port 8000'));
