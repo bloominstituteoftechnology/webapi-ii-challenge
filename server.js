b0b81b69d05ee0161ea5c515afc8ba7376a35880
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2018-08-27 13:09:02 -0700

    Displaying server data

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
-// import your node modules
+const express = require('express');
 
 const db = require('./data/db.js');
 
-// add your server code starting here
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send('Hello Fernando');
+});
+
+server.get('/db', (req, res) => {
+    db.find()
+        .then(posts => {
+            res.status(200).json(posts);
+        })
+        .catch(err => {
+            console.error('error', err);
+
+            res.status(500).json({ message: 'Error setting the data' });
+        });
+})
+
+server.listen(9000, () => console.log('\n== API on port 9k ==\n'));
