29f3f2afb400bb6708459c155cfb876699353a2c
MichaelBoegner <cmikeb@gmail.com>
2018-08-27 15:14:02 -0400

    Charles Boegner
    Pull init

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 // import your node modules
+const express = require('express');
 
 const db = require('./data/db.js');
 
 // add your server code starting here
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send('hello world');
+});
+
+server.listen(9000, () => console.log('listen test'));
