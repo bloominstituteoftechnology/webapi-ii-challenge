fd48b1bef1d8d9f60277e2d32256690feae3770c
kyle <kauzlarich.kyle@gmail.com>
2018-08-27 13:39:45 -0700

    Added express, changes to server.js set the api to run on port 9500, added a prety blank route for '/' get returns "hello", added a route to 'api/posts' to get all, added a route to 'api/posts/:id' to get note by id

--- server.js
+++ server.js
@@ -1,5 +1,30 @@
-// import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
 
-// add your server code starting here
+const server = express();
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.status(200).json("hello")
+})
+
+server.get('/api/posts', (req, res) => {
+    db.find().then(results => {
+        res.status(200).json(results);
+    }).catch(err => {
+        res.status(500).json(err);
+    });
+});
+
+server.get(`/api/posts/:id`, (req, res) => {
+    const { id } = req.params;
+    db.findById(id).then(note => {
+        res.status(200).json(note);
+    }).catch(err => {
+        res.status(500).json(err);
+    });
+});
+
+server.listen(9500, () => {
+    console.log('\n==> server running on port 9500 <==\n')
+})
