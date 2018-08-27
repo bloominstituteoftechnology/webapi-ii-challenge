0e121e7620e5c85d136a6b1e0ad00141711da971
Grant Reighard <grant.reighard@gmail.com>
2018-08-27 16:26:17 -0400

    Got both GET endpoints working.

--- server.js
+++ server.js
@@ -16,7 +16,23 @@ server.get('/api/posts', (req, res) => {
         })
         .catch(err => {
             req.abort();
-            res.status(500).json({message: 'The posts information could not be retrieved.'})
+            res.status(500).json({error: 'The posts information could not be retrieved.'})
+        })
+})
+
+server.get('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    db.findById(id)
+        .then(post => {
+            if (post.length > 0) {
+                res.status(200).json(post)
+            } else {
+                res.status(404).json({message: 'The post with the specified ID does not exist.'})
+            }
+        })
+        .catch(err => {
+            req.abort();
+            res.status(500).json({error: 'The post information could not be retrieved.'})
         })
 })
 
