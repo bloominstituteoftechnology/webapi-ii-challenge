8726adb5576a5a36b1445c7163ebd22bc344a360
francistse23 <wtse.ucsd@gmail.com>
2018-08-27 11:43:04 -0700

    Francis Tse - Node Express Lab
    Data displaying

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 // import your node modules
-
+const express = require('express');
+const server = express();
 const db = require('./data/db.js');
 
 // add your server code starting here
+server.get("/", (req, res) => {
+    res.send('Node Express Lab');
+})
+
+server.get("/posts", (req, res) => {
+    db.find()
+        .then(posts => {
+            res.status(200).json(posts)
+        })
+        .catch(err => {
+            console.error('error', err)
+            res.status(500).json({message: 'Failed to retrieve data'})
+        })
+})
+
+server.listen(8000, () => console.log(`it's working`))
