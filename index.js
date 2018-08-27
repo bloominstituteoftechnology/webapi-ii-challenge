779a556663df1a575bbbb82222444a737cf2fc58
szincone <sawyerzincone@gmail.com>
2018-08-27 14:46:57 -0600

    individual get request by id working

--- index.js
+++ index.js
@@ -23,4 +23,35 @@ server.get("/api/posts", (req, res) => {
     });
 });
 
+server.get("/api/posts/:id", (req, res) => {
+  // res
+  //   .send(req.params.id)
+  //   .then()
+  //   .catch();
+  // db.find(req.params.id)
+
+  // db.find()
+  //   .then(post => {
+  //     res.status(200).json(posts);
+  //   })
+  //   .catch(err => {
+  //     console.log("error", err);
+
+  //     res
+  //       .status(404)
+  //       .json({ message: "The post with the specified ID does not exist." });
+  //   });
+  let id = req.params.id;
+  db.findById(id)
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(err => {
+      console.log("error", err);
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
+    });
+});
+
 server.listen(8000, () => console.log("\n== API on port 8k === \n"));
