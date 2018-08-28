c6c4b8f29227a5dbc235161035eb152847b4a1d3
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-08-28 16:29:46 -0500

    completed functionality of PUT, modified GETs and DELETE

--- server.js
+++ server.js
@@ -28,33 +28,66 @@ server.get('/posts', (req, res) => {
 server.get('/posts/:id', (req, res) => {
     db.findById(req.params.id)
         .then(post => {
-            res.status(200).json(post);
+            console.log(post);
+            if (post.length > 0) {
+                res.status(200).json(post);
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." });
+            }
         })
         .catch(err => {
             console.log('error', err);
 
-            res.status(404).json({ message: "The post with the specified ID does not exist." })
+            res.status(500).json({ error: "The post information could not be retrieved." })
         })
 })
 
 server.delete('/posts/:id', (req, res) => {
     db.remove(req.params.id)
-        // .then(
-        //     console.log(post);
-        //     res.status(200).json({
-        //         url: `/posts/${req.params.id}`,
-        //         operation: `DELETE for post with id ${req.params.id}`
-        //     })
-        // )
-        .then(post => {
-            console.log(post)
-            res.status(200).json(post);
+        .then(count => {
+            console.log(count);
+            if (count) {
+                res.status(204).end();
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." });
+            }
         })
         .catch(err => {
             console.log('error', err);
 
-            res.status(404).json({ message: "The post with the specified ID does not exist." })
+            res.status(500).json({ error: "The post could not be removed" })
         })
 })
+
+server.put('/posts/:id', (req, res) => {
+    db.update(req.params.id, req.body)
+        .then(count => {
+            if (count) {
+                db.findById(req.params.id)
+                    .then(post => {
+                        console.log(post);
+                        if (post.length > 0) {
+                            res.status(200).json(post);
+                        } else {
+                            res.status(404).json({ message: "The post with the specified ID does not exist." });
+                        }
+                    })
+                    .catch(err => {
+                        console.log('error', err);
+
+                        res.status(500).json({ error: "The post information could not be retrieved." })
+                    })
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." });
+            }
+
+        })
+        .catch(err => {
+            console.log('error', err);
+
+            res.status(500).json({ error: "The post information could not be modified." });
+        })
+})
+
 
 server.listen(8000, () => console.log('\n== API on port 8k==\n'))
