000bb1e239c709e197680c65af27a8951e89a516
szincone <sawyerzincone@gmail.com>
2018-08-28 14:22:33 -0600

    updated post request using mini labs

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ server.post("/api/posts", async (req, res) => {
   } else {
     try {
       const response = await db.insert(post);
-      res.status(201).json({ post });
+      res.status(201).json(response);
     } catch {
       res.status(500).json({
         error: "There was an error while saving the post to the database"
@@ -67,19 +67,20 @@ server.post("/api/posts", async (req, res) => {
 server.delete("/api/posts/:id", (req, res) => {
   const { id } = req.params;
 
-  db.remove(id)
-    .then(count => {
-      if (count) {
-        res.status(200).json(count);
-      } else {
-        res.status(404).json({
-          message: "The post with the specified ID does not exist."
-        });
-      }
-    })
-    .catch(err => {
-      res.status(500).json({ error: "The post could not be removed" });
-    });
+  // Working Promise Version Below
+  // db.remove(id)
+  //   .then(count => {
+  //     if (count) {
+  //       res.status(200).json(count);
+  //     } else {
+  //       res.status(404).json({
+  //         message: "The post with the specified ID does not exist."
+  //       });
+  //     }
+  //   })
+  //   .catch(err => {
+  //     res.status(500).json({ error: "The post could not be removed" });
+  //   });
 });
 // END DELETE
 
