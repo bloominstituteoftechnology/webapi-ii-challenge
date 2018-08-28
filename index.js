c5d43e76b4efccad6e70133289f25f9707976f2b
szincone <sawyerzincone@gmail.com>
2018-08-28 17:43:31 -0600

    updated cors to use middleware libary instead of manully adding headers

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const express = require("express");
-
+const cors = require("cors");
 const db = require("./data/db.js");
 const server = express();
 
 server.use(express.json());
+server.use(cors());
 
 // CORS
-server.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+// server.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   next();
+// });
 // end CORS
 
 // GET REQUEST
