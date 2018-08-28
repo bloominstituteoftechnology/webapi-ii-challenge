30d624ac11f6e8fd2d4e199dfa0e53f761ba4126
kyle <kauzlarich.kyle@gmail.com>
2018-08-28 12:47:47 -0700

    added routes added a delete to '/api/posts/:id' that deletes the post, added a put to '/api/posts/:id' that will update the note at the givin id

--- server.js
+++ server.js
@@ -35,6 +35,26 @@ server.get(`/api/posts/:id`, (req, res) => {
     });
 });
 
+server.delete('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    db.remove(id).then (() => {
+        res.status(204).json("Deleted");
+    }).catch(err => {
+        res.status(500).json(err);
+    });
+});
+
+server.put('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    const { title, contents } = req.body;
+    const newPost = { title: title, contents: contents};
+    db.update(id, newPost).then( () => {
+        res.status(200).json(newPost);
+    }).catch(err => {
+        res.status(500).json(err);
+    });
+});
+
 server.listen(9500, () => {
     console.log('\n==> server running on port 9500 <==\n')
 })
