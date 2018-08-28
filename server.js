5926029365eb2b1a5014306db335faaff70cefeb
codejoncode <jonathanjholloway@gmail.com>
2018-08-28 17:50:33 -0500

    able to get item by id

--- server.js
+++ server.js
@@ -21,6 +21,21 @@ server.get("/api/posts", (req, res) => {
     });
 });
 
+server.get("/api/posts/:id", (req,res) => {
+  const {id} = req.params
+  db.findById(id)
+    .then(post => {
+      if(post){
+        res.status(200).json(post)
+      } else {
+        res.status(404).json({message: `The post with specified id ${id} does not exist`})
+      }
+    })
+    .catch(error => {
+      res.status(500).json({error: "The post information could not be retrieved"})
+    })
+})
+
 server.post("/api/posts", async (req, res) => {
   const post = req.body;
   if (post.title && post.contents) {
