e86c4b13c99ce76f3399e0d502741306e762a51f
Lauren <llwor94@gmail.com>
2018-08-28 14:21:41 -0700

    added close button to edit

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,10 +1,11 @@
 import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import { deletePost, editPost } from "../actions";
-import trash from '../assets/delete.png';
-import edit from '../assets/edit.png';
+import trash from "../assets/delete.png";
+import edit from "../assets/edit.png";
+import close from "../assets/close.png";
 import styled from "styled-components";
-import Form from './Form';
+import Form from "./Form";
 
 const Button = styled.button`
   border: none;
@@ -13,24 +14,24 @@ const Button = styled.button`
   margin: 0 2px;
   img {
     height: 18px;
-  };
-
-  &:hover{
-    background: #03DAC5;
   }
-`
+
+  &:hover {
+    background: #03dac5;
+  }
+`;
 const ButtonWrapper = styled.div`
   margin: 5px 0;
   display: flex;
   justify-content: flex-end;
-`
+`;
 const Header = styled.h3`
   padding: 5px;
-`
+`;
 
 const Contents = styled.p`
   padding: 5px;
-`
+`;
 
 class Post extends Component {
   state = {
@@ -40,38 +41,48 @@ class Post extends Component {
   };
 
   componentDidMount() {
-    this.setState({title: this.props.post.title, contents: this.props.post.contents})
+    this.setState({
+      title: this.props.post.title,
+      contents: this.props.post.contents,
+    });
   }
 
   onSubmit = e => {
     e.preventDefault();
-    this.props.editPost(this.props.post.id, ({title: this.state.title, contents: this.state.contents}));
-    this.setState({editable: false})
-  }
+    this.props.editPost(this.props.post.id, {
+      title: this.state.title,
+      contents: this.state.contents,
+    });
+    this.setState({ editable: false });
+  };
 
   render() {
     return (
       <Fragment>
         {this.state.editable ? (
-          <Form
-            title={this.state.title}
-            contents={this.state.contents}
-            onChange={e => this.setState({[e.target.name]: e.target.value})} 
-            handleSubmit={this.onSubmit} 
-            />
+          <Fragment>
+            <Form
+              title={this.state.title}
+              contents={this.state.contents}
+              onChange={e => this.setState({ [e.target.name]: e.target.value })}
+              handleSubmit={this.onSubmit}
+            />{" "}
+            <Button style={{float: 'right'}} onClick={() => this.setState({ editable: false })}>
+              <img src={close} />{" "}
+            </Button>
+          </Fragment>
         ) : (
           <Fragment>
             <ButtonWrapper>
-            <Button onClick={() => this.setState({ editable: true })}>
-              <img src={edit} />
-            </Button>
-            <Button onClick={() => this.props.deletePost(this.props.post.id)}>
-            <img src={trash} /> 
-            </Button>
+              <Button onClick={() => this.setState({ editable: true })}>
+                <img src={edit} />
+              </Button>
+              <Button onClick={() => this.props.deletePost(this.props.post.id)}>
+                <img src={trash} />
+              </Button>
             </ButtonWrapper>
             <Header>{this.props.post.title}</Header>
             <Contents>{this.props.post.contents}</Contents>
-            
           </Fragment>
         )}
       </Fragment>
@@ -83,4 +94,7 @@ const mapStateToProps = state => ({
   posts: state.postsReducer.posts,
 });
 
-export default connect(mapStateToProps, {deletePost, editPost})(Post);
+export default connect(
+  mapStateToProps,
+  { deletePost, editPost }
+)(Post);
