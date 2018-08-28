b0c837146ee1a4db57bbe0ae45ea7a0358be116d
jordan-massingill <jordan_massingill@hotmail.com>
2018-08-28 15:46:35 -0500

    Functionality of all endpoints achieved

--- server.js
+++ server.js
@@ -7,40 +7,69 @@ const db = require('./data/db.js');
 
 // add your server code starting here
 
-const newPost = {
-  title: "How can yeh have any pudding...",
-  contents: "If yeh won't eat yer meat!"
-};
-
 server.use(express.json());
 
-server.get('/api/posts', (req, res) => {
+server.get('/posts', (req, res) => {
   db.find().then(posts => {
     res.status(200).json(posts)
   }).catch(err => {
     console.error('error', err);
 
-    res.status(500).json({ error: "The posts information could not be retrieved."})
+    res.status(500).json({ message: 'The posts information could not be retrieved.'})
   })
 });
 
-server.get('/api/posts/:id', (req, res) => {
+server.get('/posts/:id', (req, res) => {
   db.findById(parseInt(req.params.id)).then(post => {
     console.log(post);
     if (post.length === 0) {
-      res.status(404).json({  message: "The post with the specified ID does not exist." });
+      res.status(404).json({  message: 'The post with the specified ID does not exist.' });
     }
     else {
       res.status(200).json(post)
   }}).catch(err => {
     console.error('error', err);
 
-    res.status(500).json({ error: "The post information could not be retrieved."})
+    res.status(500).json({ error: 'The post information could not be retrieved.'})
   })
 });
 
-server.post('/api/posts', (req, res) => {
-  db.insert(newPost).then().catch()
+server.post('/posts', (req, res) => {
+  console.log(req.body);
+  const newPost = req.body;
+  if (newPost.title && newPost.contents) {
+    db.insert(newPost).then(id => {
+      console.log(id);
+      res.status(201).json({ message: 'Post created successfully!'})
+    }).catch(err => {
+      res.status(500).json({ message: 'There was an error adding your post'})
+    });
+  }
+  else {
+    res.status(422).json({ message: 'A post needs both a title as well as post contents to be saved'});
+  }
+})
+
+server.delete('/posts/:id', (req, res) => {
+  db.remove(req.params.id).then(count => {
+    console.log(count);
+    res.status(204).end();
+  }).catch(err => {
+    res.status(500).json(err);
+  })
+})
+
+server.put('/posts/:id', (req, res) => {
+  if (req.body.title && req.body.contents) {
+    db.update(req.params.id, req.body).then(count => {
+      res.status(201).json({ message: 'Your post was updated successfully'})
+    }).catch(err => {
+      res.status(500).json(err)
+    })
+  }
+  else {
+    res.status(422).json({ message: 'A post must have a title and post contents to save'})
+  }
 })
 
 server.listen(3000, ()=>console.log('Hello!'))
