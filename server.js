89add2cf8d0f1f288a7f77088298cc64d45e0fc0
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-08-28 14:06:24 -0600

    Added the post method

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require('express');
 // add your server code starting here
 
 const server = express(); 
+server.use(express.json())
 
 server.get('/api/posts', (req,res) => {
     db.find().then(posts => {
@@ -18,10 +19,41 @@ server.get('/api/posts', (req,res) => {
 server.get('/api/posts/:id',(req, res) => {
     const id = req.params.id;
     db.findById(id).then(post=> {
-        res.status(200).json(post)
+        if(post.length > 0){
+            res.status(200).json(post)
+        }
+        else {
+            res.status(404).json({ message: "The post with the specified ID does not exist."})
+        }
+      
     }).catch( err => {
         res.status(500).json({error: "The post information could not be retrieved." })
     })
+}); 
+
+server.post('/api/posts', (req, res) => {
+    const data = req.body
+    if(data.title && data.contents){
+        db.insert(data).then(response =>
+            db.findById(response.id).then(post => {
+                res.status(201).json(post)
+            })
+        ).catch(err => {
+            res.status(500).json({error: "There was an error while saving the post to the database"})
+        })
+    }else {
+        res.status(400).json( {errorMessage: "Please provide title and contents for the post."})
+    }
 })
+
+// server.post('/api/posts', (req, res) => {
+//         const newPost = req.body
+//         db.insert(newPost).then(postId => {
+//             res.status(201).json(postId)
+//         }).catch(err => {
+//             res.status(500).json({ error: "There was an error while saving the post to the database"})
+//         })
+    
+// })
 
 server.listen('3000'); 
