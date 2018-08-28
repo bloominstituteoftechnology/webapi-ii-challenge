885a0a9f827ad6efcfe0ec559c4a4dd56684c20f
Sean <sean47900@gmail.com>
2018-08-28 17:00:41 -0500

    created react app

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import axios from 'axios'; 
+
+ const url =   "http://localhost:9000/posts"
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default App;
