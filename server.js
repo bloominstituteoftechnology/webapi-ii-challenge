d3389eb689b91b9017db4a16d47a7febc22ae870
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-08-28 17:46:11 -0500

    fixed DELETE and added POST

--- server.js
+++ server.js
@@ -42,21 +42,41 @@ server.get('/posts/:id', (req, res) => {
         })
 })
 
+server.post('/posts', (req, res) => {
+    const newPost = req.body;
+
+    db.insert(newPost).then(post => {
+        res.status(201).json(post);
+    })
+        .catch(err => {
+            console.log('error', err);
+            res.status(500).json({ error: "There was an error while saving the post to the database" })
+        })
+})
+
 server.delete('/posts/:id', (req, res) => {
-    db.remove(req.params.id)
-        .then(count => {
-            console.log(count);
-            if (count) {
-                res.status(204).end();
-            } else {
-                res.status(404).json({ message: "The post with the specified ID does not exist." });
-            }
+    db.findById(req.params.id)
+        .then(post => {
+            db.remove(post[0].id)
+                .then(count => {
+                    console.log(count);
+                    if (count) {
+                        res.status(200).json(post)
+                    } else {
+                        res.status(404).json({ message: "The post with the specified ID does not exist." });
+                    }
+                })
+                .catch(err => {
+                    console.log('error', err);
+
+                    res.status(500).json({ error: "The post could not be removed" })
+                })
         })
         .catch(err => {
             console.log('error', err);
-
-            res.status(500).json({ error: "The post could not be removed" })
         })
+
+
 })
 
 server.put('/posts/:id', (req, res) => {
@@ -83,14 +103,12 @@ server.put('/posts/:id', (req, res) => {
                 } else {
                     res.status(404).json({ message: "The post with the specified ID does not exist." });
                 }
-
             })
             .catch(err => {
                 console.log('error', err);
 
                 res.status(500).json({ error: "The post information could not be modified." });
             })
-
     } else {
         res.status(400).json({ error: "Please provide title and contents for the post." });
     }
