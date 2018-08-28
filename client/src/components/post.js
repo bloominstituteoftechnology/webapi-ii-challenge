e530f8dd0694f3b038ca82c4d31b4309ecfa9b16
William <kwontaewoongah@gmail.com>
2018-08-28 13:43:19 -0700

    implemented delete client side

--- client/src/components/post.js
+++ client/src/components/post.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
-const Post=(props)=><div className='card'><p>{props.data.title}</p><p>{props.data.contents}</p></div>
+const Post=(props)=><div className='card'><p>{props.data.title}</p><p>{props.data.contents}</p>
+<i class="fas fa-trash-alt" onClick={()=>props.delete(props.data.id)}></i></div>
 
 export default Post;
--- client/src/components/posts.js
+++ client/src/components/posts.js
@@ -12,10 +12,13 @@ class Posts extends React.Component{
     componentDidMount(){
         axios.get('http://localhost:9000/posts').then(res=>this.setState({posts:res.data})).catch(err=>console.log(err));
     }
+    delete(id) {
+        axios.delete(`http://localhost:9000/posts/${id}`).then(res=>window.location.reload()).catch(err=>console.log(err));
+    }
     render() {
         return (
         <div>
-            {this.state.posts.length>0?this.state.posts.map((e,i)=><Post data={e} key={i}/>):null}
+            {this.state.posts.length>0?this.state.posts.map((e,i)=><Post data={e} key={i} delete={this.delete}/>):null}
         </div>
         )
     }
