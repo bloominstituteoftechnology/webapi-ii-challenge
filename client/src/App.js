700f2d522f18d125c271589e0a237557dbfa321a
William <kwontaewoongah@gmail.com>
2018-08-28 13:36:22 -0700

    implemented post client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Posts from './components/posts.js';
+import NewPostHandler from './components/newposthandler.js';
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <Posts></Posts>
+        <NewPostHandler/>
       </div>
     );
   }
---
+++ client/src/components/newposthandler.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+
+const Input=styled.input`
+width:100%
+max-width: 400px
+margin: 0 auto;
+`
+class NewPostHandler extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            title:'',
+            contents:''
+        }
+    }
+    handleInputChange=(e)=>{
+        this.setState({[e.target.name]:e.target.value});
+    }
+    submitPost=()=>{
+        const Post={
+            title:this.state.title,
+            contents:this.state.contents
+        }
+        axios.post('http://localhost:9000/posts',Post).then(res=>window.location.reload()).catch(err=>console.log(err));
+    }
+    render() {
+        return(
+            <div>
+                <Input type='text' name='title' placeholder='Enter post title' value={this.state.name} onChange={this.handleInputChange}/>
+                <Input type='text' name='contents' placeholder='Enter post contents' value={this.state.name} onChange={this.handleInputChange}/>
+                <button className='btn waves-effect waves-light' onClick={()=>this.submitPost()}>Submit New Post</button>
+            </div>
+        )
+    }
+}
+export default NewPostHandler;
