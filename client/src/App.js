b719c26f6b2d3fbe20c909d5fddaaffa73dc395f
Sean <sean47900@gmail.com>
2018-08-28 17:16:39 -0500

    working on app

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
 import axios from 'axios'; 
 
- const url =   "http://localhost:9000/posts"
+ const url = "http://localhost:9000/api/posts"
 
 class App extends Component {
+  constructor(){
+    super(); 
+    this.state={
+      posts: []
+    }
+  }
+  
+  componentDidMount(){
+     axios 
+    .get(url)
+    .then(response => {
+      this.setState({
+        posts: response.data
+      })
+    })
+    .catch(err => console.log(err))
+    
+  }
+
   render() {
     return (
       <div className="App">
-
+        {this.state.posts.map(user =>{
+          <div key={user.id}>
+          <p>{user.title}</p>
+          <p>{user.contents}</p>
+          </div>
+        })}
       </div>
     );
   }
