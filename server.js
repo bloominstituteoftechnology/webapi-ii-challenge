468efea61dd82b6f918c100992df83b33095640f
William <kwontaewoongah@gmail.com>
2018-08-27 17:35:29 -0700

    implemented delete functionality

--- server.js
+++ server.js
@@ -34,4 +34,15 @@ server.post('/posts',(req,res)=>{
     }
 }
 )
+server.delete('/posts/:id',(req,res)=>{
+    db.remove(req.params.id)
+    .then(count=>{
+        if (count>0) {
+            res.status(200).json({success:`Deleted ${count} item(s)`})
+        } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist." });
+        }
+    })
+    .catch(err=>res.status(500).json({error: "The post could not be removed" }))
+})
 server.listen(9000,()=>console.log('Engines firing server starting new horizons venturing.'))
