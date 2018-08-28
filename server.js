e49fdd799d2671983e05b264aa70b1fc9ac4bb48
adamcglee <adamcglee@gmail.com>
2018-08-28 13:59:31 -0700

    fixed issues to show 404 error on put

--- server.js
+++ server.js
@@ -46,8 +46,6 @@ server.get('/api/posts/:id', (req, res) => {
 server.post('/api/posts', (req, res) => {
     const {title, contents} = req.body;
     if(!title ||!contents) {
-        console.error('error', error);
-
         res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
     }
     db.insert({title, contents})
@@ -62,6 +60,45 @@ server.post('/api/posts', (req, res) => {
 
             res.status(500).json({ error: "There was an error while saving the post to the database" })
         });
+});
+
+server.delete('/api/posts/:id', (req, res) => {
+    const {id} = req.params;
+
+    db.remove(id)
+        .then(count => {
+            if(count) {
+                res.status(204).end()
+            }else {
+                res.status(404).json({ message: 'No post with this id was found' })
+            }
+            
+        })
+
+        .catch(err => res.status(500).json(err));
+});
+
+server.put('/api/posts/:id', (req, res) => {
+    const {title, contents} = req.body;
+    db.findById(req.params.id)
+        .then((id) => {
+            console.log(id)
+            if(id.length === 0) {
+                res.status(404).json({ message: 'The post with the specified ID does not exist.'})
+            } else {
+                if(!title ||!contents) {
+                    res.status(400).json({ errorMessage: "Please provide title and contents for the post." })    
+                } else{
+                   db.update(req.params.id, req.body)
+                    .then(posts => {
+                        res.status(200).json(posts)
+                    })
+                    .catch(err => res.status(500).json({ message: 'update failed'}))
+                    }; 
+                }
+            }           
+        )        
+        .catch(err => res.status(404).json({ message: 'The post with the specified ID does not exist.'}))
 });
  
 server.listen(9000, () => console.log('\n== API on port 9k ==\n'));
