3b7662e697b0fc2aa421a868ed1e8a1f0380a8b0
Kwaku Dapaah <kwa2the4@gmail.com>
2018-08-28 22:03:53 -0400

    appropirate stuff has been added

--- server.js
+++ server.js
@@ -1,5 +1,78 @@
+const express = require('express');
+const server = express();
+const bodyParser = require('body-parser');
+server.use(express.json());
+
+
+
 // import your node modules
 
 const db = require('./data/db.js');
 
+// routes
+
+server.get('/', (req, res) => {
+  res.send('hello welcome to this channel!')
+})
+
+server.get('/api/posts', (req, res) => {
+  db.find()
+  .then((posts) => {
+    res.status(200).json(posts)
+  })
+  .catch((error) => {
+    res.status(500).json({ error: "The posts information cannot be retrieved"});
+  })
+
+server.get('/api/posts/:id', async (req, res) => {
+  const { id } = req.params;
+  if(id) {
+    try {
+      const response = await db.findById(id);
+      res.status(200).json(response);
+    } catch(err){
+      res.status(500).json( { message: "The post information could not be retrieved" })
+    }
+  } else {
+    res.status(404).json({ error: "The post with the specified ID does not exist."})
+  }
+  
+})
+  
+  
+})
+
+server.post('/api/posts', async (req, res) => {
+  const post = req.body;
+  if(post.title && post.contents){
+    try{
+    const response = await db.insert(post);
+    res.status(201).json(response)
+  } catch(err){
+    res.status(501).json({ message: "cannot create post" });
+  }
+  } else {
+    res.status(400).json({ errorMessage: "Please provide title and contents for the post" })
+  }
+})
+
+server.delete('api/posts/:id', async (req, res) => {
+  const { id } = req.params;
+  if(id) {
+    try {
+      const response = await db.remove(id);
+      res.status(200).json(response);
+    } catch(err) {
+      res.status(500).json({ error: "The post could not be removed" });
+    }
+  } else {
+    res.status(404).json({ message: "The post with the specified ID does not exist" });
+  } 
+})
+
+
 // add your server code starting here
+const PORT = 5000;
+server.listen(PORT, () => {
+  console.log(`server is listening at port: ${PORT}`);
+})
