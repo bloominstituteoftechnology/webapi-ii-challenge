3fc9f026113e2e583f161f31205801324123316e
keegn <keegan.t.burkett@gmail.com>
2018-09-03 16:27:32 -0500

    get response is return array of post objects from the db

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
-// import your node modules
+const express = require('express'); // CommonJS
 
 const db = require('./data/db.js');
 
-// add your server code starting here
+const server = express();
+
+//configure middleware for the server
+server.use(express.json());
+
+//configure routing (routing is also a form of middleware)
+server.get('/', (req, res) => {
+    res.send('hello cs12');
+})
+
+server.get('/api/posts', (req, res) => {
+    db.find()
+    .then(posts => {
+        res.status(200).json(posts);
+    })
+    .catch(err => {
+        console.error('error', err);
+
+        res.status(500).json({ message: 'Error getting the data'})
+    })
+})
+
+//start the server
+server.listen(9000, () => console.log('\n== API on port 9K ==\n'))
