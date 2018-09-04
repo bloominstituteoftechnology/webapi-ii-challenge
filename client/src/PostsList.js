dc591397b8662e3374891f1325b59d35c5c4581a
Rachel DiCesare <racheldicesare@missrachelthedeveloper.com>
2018-09-04 19:51:17 -0400

    styled the posts completely done

--- client/src/PostsList.js
+++ client/src/PostsList.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './PostList.css'
  export default function PostsList({ post }){
    return(
-    <div>
-      <div>{post.title}</div>
-      <div>{post.contents}</div>
+    <div className="posts">
+      <div><h3>{post.title}</h3></div>
+      <div><h3>{post.contents}</h3></div>
     </div>
    )
 } 
