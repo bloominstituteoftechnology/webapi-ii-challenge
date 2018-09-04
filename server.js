c6688a65b6ea824e8b4f7d7c3f83d66113137f64
bri79319 <bri79319@gmail.com>
2018-09-03 22:22:18 -0400

    working on post method

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // import your node modules
-const express = require('express');
+const express = require("express");
 
-const db = require('./data/db.js');
+const db = require("./data/db.js");
 
 // add your server code starting here
 
@@ -9,8 +9,25 @@ const server = express();
 
 server.use(express.json());
 
-server.get('/', (req, res) => {
-    res.send('Hello FSW12');
+server.get("/", (req, res) => {
+  res.send("Hello FSW12");
 });
 
-server.listen(9000, () => console.log('\n== API on port 9k ==\n'));
+server.get("/users", (req, res) => {
+  db.find()
+    .then(users => {
+        res.status(200).json(users);
+    })
+    .catch(err => {
+      console.error("Error:", err);
+
+      res.status(500, json({ message: "Error getting the data" }));
+    });
+});
+
+server.post("/users", (req, res) => {
+    //http message = headers + body(data)
+    const user = req.body;
+})
+
+server.listen(9000, () => console.log("\n== API on port 9k ==\n"));
