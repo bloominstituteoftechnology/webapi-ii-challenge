102496aff246277d05259ace7093ae9082c3f078
bri79319 <bri79319@gmail.com>
2018-09-03 20:20:04 -0400

    setting up server with node and express js

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 // import your node modules
+const express = require('express');
 
 const db = require('./data/db.js');
 
 // add your server code starting here
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send('Hello FSW12');
+});
+
+server.listen(9000, () => console.log('\n== API on port 9k ==\n'));
