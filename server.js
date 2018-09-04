d13ccaa99d5b7d028bfa1bc9fcb131f87e5b74dc
AI2EJAY <JHammett1314@gmail.com>
2018-09-04 09:52:23 -0500

    Forked => Cloned => Yarn install/add express
    -the current code in server.js is from the lecture.

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 // import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
+const server = express();
+server.use(express.json());
+server.get('/', (req, res) => {
+    res.send('Hello FSW12');
+});
+server.get('/users', (req, res) => {
+    db.find()
+    .then(users => {
+        res.status(200).json(users);
+    })
+    .catch(err => {
+        console.error('error', err);
+        res.status(500).json({message: 'Error getting the data'})
+    })
+})
 
+server.listen(9000, () => console.log('API on port 9k'))
 // add your server code starting here
