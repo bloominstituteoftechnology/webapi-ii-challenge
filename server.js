61c5bb98e10ce1538f88731c7ae091fafb2b31c8
Arthur Bates <aabatesj@gmail.com>
2018-09-04 19:03:47 -0400

    initial commit
    still trying to get the hang of this

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 // import your node modules
 
-const db = require('./data/db.js');
-
 // add your server code starting here
+
+const server = express();
+
+server.get('/api/posts', (req, res) => {
+    db.find()
+        .then(posts => {
+            res.status(200).json(posts)
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'Information not found' })
+        })
+})
+
+server.listen(3000, () => console.log('Working'));
