dd6403d4d94d36f17807481c0e8e0a8ed4384d24
keegn <keegan.t.burkett@gmail.com>
2018-09-04 15:04:21 -0500

    Post is working

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ server.get('/', (req, res) => {
     res.send('hello cs12');
 })
 
+// GET complete
 server.get('/api/posts', (req, res) => {
     db.find()
     .then(posts => {
@@ -20,9 +21,33 @@ server.get('/api/posts', (req, res) => {
     .catch(err => {
         console.error('error', err);
 
-        res.status(500).json({ message: 'Error getting the data'})
+        res.status(500).json({  error: "The posts information could not be retrieved."})
     })
 })
+
+
+// POST complete
+server.post("/api/posts", async (req, res) => {
+    // http message = header + body(data)
+    const post = req.body; //this requires the express.json() middleware
+  
+    if (post.title && post.contents) {
+      try {
+        const response = await db.insert(post);
+        res.status(201).json({ message: "post created successfully" });
+        // 200-299 success (created in ^ case), 300-399: redirection, 400-499 client error, 500+ server error
+      } catch (ex) {
+        res.status(500).json({ error: "There was an error while saving the post to the database" });
+      }
+    } else {
+      res.status(400).json({ errorMessage: "Please provide title and contents for the post."  });
+    }
+    // equivilent to the above try and catch (not the if else thats extra example). below is promise (not async)
+    //   db.insert(user)
+    //     .then(response => response.status(201).json(response))
+    //     .catch(err => res.status(500).json(err));
+  });
+
 
 //start the server
 server.listen(9000, () => console.log('\n== API on port 9K ==\n'))
