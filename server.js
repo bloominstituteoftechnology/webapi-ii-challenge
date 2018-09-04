727ad897eea30be00e17f6d95dfbc0935809cadb
bri79319 <bri79319@gmail.com>
2018-09-04 15:05:27 -0400

    working on put endpoint

--- server.js
+++ server.js
@@ -11,22 +11,6 @@ const server = express();
 server.use(express.json()); //this teaches express how to parse JSON info from req.body
 
 //configure routing (routing is also a form of middleware)
-server.get("/", (req, res) => {
-  res.send("Hello FSW12");
-});
-
-server.get("/users", (req, res) => {
-  db.find()
-    .then(users => {
-      res.status(200).json(users);
-    })
-    .catch(err => {
-      console.error("Error:", err);
-
-      res.status(500, json({ message: "Error getting the data" }));
-    });
-});
-
 server.post("/users", async (req, res) => {
   //http message = headers + body(data)
   const user = req.body; //this requies the express.json() middleware
@@ -36,7 +20,7 @@ server.post("/users", async (req, res) => {
       const response = await db.insert(user);
       res.status(201).json({ message: "User created successfully" });
       //200-299: success, 300-399: redirection, 400-499: client error, 500+: server error
-    } catch (ex) {
+    } catch (err) {
       // handle error
       res.status(500).json({
         title: "Error adding the user",
@@ -53,6 +37,41 @@ server.post("/users", async (req, res) => {
   //   .then(response => response.status(201).json(response))
   //   .catch(err => res.status(500).json(err));
 });
+
+server.get("/", (req, res) => {
+  res.send("Hello FSW12");
+});
+
+server.get("/users", (req, res) => {
+  db.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      console.error("Error:", err);
+
+      res.status(500, json({ message: "Error getting the data" }));
+    });
+});
+
+server.delete("/users/:id", (req, res) => {
+  const { id } = req.params; //const id = req.params.id;
+
+  db.remove(id)
+    .then(count => {
+      console.log("Count: ", count);
+      if (count) {
+        res.status(204).end();
+      } else {
+        res.status(404).json({ message: "No user with this id was found" });
+      }
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.put("/users/:id", (req, res) => {
+  
+})
 
 //start the server
 server.listen(9000, () => console.log("\n== API on port 9k ==\n"));
