fdd5aedd055d26e26c784ca928a755fd7d0b9872
AI2EJAY <JHammett1314@gmail.com>
2018-09-04 17:00:43 -0500

    Updated GET works. still editing functions as I see fit

--- server.js
+++ server.js
@@ -2,14 +2,25 @@
 const express = require("express");
 const db = require("./data/db.js");
 const server = express();
+
 server.use(express.json());
+
+let nextId = 10;
+
+// SIMPLE GET
 server.get("/", (req, res) => {
   res.send("Hello FSW12");
 });
+
+// GET DATA
 server.get("/posts", (req, res) => {
   db.find()
     .then(posts => {
-      res.status(200).json(posts);
+      const sortField = req.query.sortby || "id";
+      const response = posts.sort(
+        (a, b) => (a[sortField] < b[sortField] ? -1 : 1)
+      );
+      res.status(200).json(response);
     })
     .catch(err => {
       console.error("error", err);
@@ -18,8 +29,11 @@ server.get("/posts", (req, res) => {
         .json({ error: "The posts information could not be retrieved." });
     });
 });
+
+//GET DATA FROM ID
 server.get("/posts/:id", (req, res) => {
-  db.findById(`/posts/${posts.id}`)
+  const id = req.params.id;
+  db.findById(`/posts/${id}`)
     .then(post => {
       res.status(200).json(post);
     })
@@ -31,15 +45,34 @@ server.get("/posts/:id", (req, res) => {
     });
 });
 
+//POST
 server.post("/posts", (req, res) => {
+  db.insert();
+  const post = req.body;
+  post.id = nextId++;
+  //Something here
   res.status(201).json({ url: "./posts", operation: "POST" });
 });
 
-server.delete("./posts/:id", (req, res) => {
-  db.remove(`/posts/${posts.id}`).then(post => {
+//PUT
+server.put("/posts", (req, res) => {
+  const post = posts.find(post => post.id == req.params.id);
+  db.update();
+  if (!post) {
+    res.status(404).json({ message: "Post does not exist" });
+  } else {
+    Object.assign(post, req.body);
+    res.status(200).json({ post });
+  }
+});
+
+//DELETE
+server.delete("/posts/:id", (req, res) => {
+  const id = req.params.id;
+  db.remove(`/posts/${id}`).then(post => {
     res
       .status(200)
-      .json(posts)
+      .json({ url: "/posts", operation: `DELETE for post with id ${id}` })
       .catch(err => {
         console.error("error", err);
         res
