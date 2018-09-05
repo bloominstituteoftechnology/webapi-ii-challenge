3d9113a39b452c90e4fd73e61411f1330503d979
cubsfan46350 <cubsfan.46350@msn.com>
2018-09-05 17:32:11 -0500

    completed building server and displaying posts

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 // import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
-
+const server = express ();
 // add your server code starting here
+server.get('/api/posts', (req, res) => {
+    db.find()
+        .then(posts => {
+            res.status(200).json(posts);
+        })
+        .catch(err => {
+            console.error('error', err);
+            res.status(500).json({ message: 'Cannot Get Data' });
+        });
+});
+server.listen(3000, () => console.log('Listening on Port 3000'));
