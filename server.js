ef5d64e4f2f60a99ff4719d6d3622d0cd8e13115
Sania Parekh <sparekh@Sanias-MacBook.local>
2018-09-05 10:58:13 -0500

    dependencies and imports, working on 'get' statement

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 // import your node modules
-
+const express = require('express'); 
 const db = require('./data/db.js');
 
 // add your server code starting here
+const server = express();
+server.use(express.json());
+
+
+server.get('/posts', (req, res) => {
+  res.send('Hello');
+})
+
+server.listen(7000, () => console.log('\n== API on port 7k ==\n'));
