835f9fb0a22614f504fdcacc951ac47ad581b23f
AlexanderCaves <ahcaves@gmail.com>
2018-09-05 10:11:17 -0700

    fixed--code moved to server.js

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 // import your node modules
+const express = require('express');
 
 const db = require('./data/db.js');
 
 // add your server code starting here
+const server = express();
+
+server.use(express.json());
+
+server.get('/posts', (req, res) => {
+    db.find()
+        .then(posts => {
+            res.status(200).json(posts);
+        })
+        .catch(err => {
+            console.error('error', err);
+
+            res.status(500).json({ message: 'Error getting the data' })
+        });
+})
+
+server.listen(8000, () => console.log('\n== API on port 8k ==\n'));
