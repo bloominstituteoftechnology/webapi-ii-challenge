171026a3a83272ce040f50ac84dfa04c1889c974
Fred <sohnf90@yahoo.com>
2018-09-05 00:49:18 -0700

    Comepleted get: returning array of post objects in the database

--- server.js
+++ server.js
@@ -1,5 +1,39 @@
 // import your node modules
+const express = require('express'); // import express from 'express'
 
 const db = require('./data/db.js');
 
 // add your server code starting here
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send('Home Page');
+})
+
+server.get('/api/posts', (req, res) => {
+    db.find()
+    .then( posts => {
+        res.status(200).json(posts);
+    })
+    .catch(err => {
+        console.log('error', err);
+
+        res.status(500).json({ error: 'The posts information could not be retrieved.'})
+    })
+})
+
+server.get('/api/posts/:id', (req, res) => {
+    db.find()
+    .then( posts => {
+        res.status(200).json(posts);
+    })
+    .catch(err => {
+        console.log('error', err);
+
+        res.status(500).json({ error: 'The post information could not be retrieved.'})
+    })
+})
+
+server.listen(9000, () => console.log('API running on port 9000'))
