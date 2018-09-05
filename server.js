980cdf95da4838b21061016453b62c3cc59de4c7
tulf <timzulf@gmail.com>
2018-09-04 20:22:47 -0400

    get post by id

--- server.js
+++ server.js
@@ -4,14 +4,41 @@ const server = express();
 const db = require('./data/db.js');
 
 // add your server code starting here
+
 server.get('/', (req, res) => {
   res.send("what's up ma brutha?")
 });
 
-server.get('/api/users', (req, res) => {
-  db.find().then((results) => {
-    res.status(200).json(results);
+//------Get request methods-------//
+
+server.get('/api/posts', (req, res) => {
+  db.find().then((posts) => {
+    res.status(200).json(posts);
+  })
+  .catch(err =>
+  {
+    res
+      .status(500)
+      .json({error: "The posts information could not be retrieved."})
   })
 });
+
+//ID:
+server.get('/api/posts/:id', (req, res) => {
+  const id = req.params.id
+  db.findById(id)
+  .then((post) => {
+    res.status(200).json(post);
+  })
+  .catch(err =>
+  {
+    res
+      .status(500)
+      .json({error: "The posts information could not be retrieved."})
+  })
+});
+
+
+
 
 server.listen(8000, () => console.log('API running on port 8000'))
