fe8d219e6b22308631de161eb0a330db1c5afd03
Vera Simon <ghjkl>
2018-10-01 13:04:30 -0700

    Node Express Lab - Day I
    Ran "yarn install", "yarn add express cors".
    Imported dependencies into index.js, got extremely barebone server running successfully.
    Fixed typos in README.md in regard to "server.js".

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 // import your node modules
-
+const express = require('express');
+const cors = require('cors');
 const db = require('./data/db.js');
 
 // add your server code starting here
+const server = express();
+server.use(cors());
+
+
+
+const port = 9001;
+server.listen(port, () => console.log(`~~~ API running on port ${port} ~~~`));
