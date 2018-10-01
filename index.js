9e9599dcd4c572760b4bd6353e2cfb60d28967da
Anthony Greb <agreb255@gmail.com>
2018-10-01 15:21:04 -0400

    MVP done, working on some stretch

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 // import your node modules
+const express = require("express");
+const cors = require("cors");
 
-const db = require('./data/db.js');
+const db = require("./data/db.js");
 
 // add your server code starting here
+const server = express();
+server.use(cors());
+
+server.get("/", (req, res) => {
+  res.send("<h1>Hello from Anthony's first server!!!</h1>");
+});
+
+server.get("/api/posts", (req, res) => {
+  db.find()
+    .then(posts => {
+      console.log("POSTS", posts);
+      res.json(posts);
+    })
+    .catch(error => res.send(error));
+});
+
+server.get("/api/posts/:id", (req, res) => {
+  db.findById(req.param.id)
+    .then(post => {
+      console.log("POSTS", post);
+      res.json(post);
+    })
+    .catch(error => res.send(error));
+});
+const port = 8000;
+server.listen(port, () => console.log(`API running on port ${port}`));
