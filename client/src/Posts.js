40b98bec914d75373dec505fcfca3b4a0dd0be83
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-10-01 15:40:28 -0400

    Connected created Posts client side to retrive data from server, imported into app

---
+++ client/src/Posts.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Posts extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            posts: [],
+            done: false     
+        };
+    }
+
+    componentDidMount() {
+        let url = "http://localhost:9000/api/posts";
+        axios.get(url)
+            .then(res => {
+                this.setState({posts: res.data, done: true})
+            })
+           .catch(err => {
+                console.log(err);
+            })
+    }
+
+    render() {
+        if (!this.state.done){
+                return (<div></div>)
+        }
+        return (
+                <div className="posts">
+                {this.state.posts.map(post => {
+                    return (
+                        <div key={post.id}>
+                            <h1>{post.title}</h1>
+                            <p>{post.contents}</p>
+                            <p>{post.create_at}</p>
+                        </div>
+                    )
+                })}
+               </div>
+        );
+    }
+}
+
+export default Posts;
