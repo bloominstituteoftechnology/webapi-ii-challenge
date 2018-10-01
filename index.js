1cf66ddcbebeabf11df9065c0485acbd6acff5ff
Andrew <ghobrial.andrew@gmail.com>
2018-10-01 16:09:57 -0400

    GET endpoint completed

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 // import your node modules
 
-const db = require('./data/db.js');
-
-// add your server code starting here
+const db = require("./data/db.js");
 
 // require the express npm module, needs to be added to the project using "yarn add" or "npm install"
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 
 // creates an express application using the express module
 const server = express();
+server.use(cors());
 
 // configures our server to execute a function for every GET request to "/"
 // the second argument passed to the .get() method is the "Route Handler Function"
 // the route handler function will run on every GET request to "/"
-server.get('/api/posts', (req, res) => {
-  // express will pass the request and response objects to this function
-  // the .send() on the response object can be used to send a response to the client
-  res.send('Hello World');
+
+server.get("/api/posts", (req, res) => {
+  db.find()
+		.then(posts => res.json(posts))
+    .catch(err => res.send(err));
+    console.log(`Here are the posts I've found: ${posts}`);
 });
 
-// once the server is fully configured we can have it "listen" for connections on a particular "port"
-// the callback function passed as the second argument will run once when the server starts
-server.listen(8000, () => console.log('API running on port 8000'));
+// Port listening code
+const port = 8000;
+server.listen(port, () =>
+  console.log(`Server is listening on Port ${port}`)
+);
