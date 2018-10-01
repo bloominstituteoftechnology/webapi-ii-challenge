d7a2875f0f55f04646137258e747a191c6cfa1f4
Brock Maraee Rohloff <00AgentB@gmail.com>
2018-10-01 14:16:53 -0600

    Partially implement POST, PUT, and DELETE endpoints. No validation.

--- index.js
+++ index.js
@@ -11,17 +11,39 @@ const port = 7777;
 server.listen(port, console.log(`=-=-= Server Active On Port ${port} =-=-=`));
 
 server.get('/', (request, response) => {
-    response.send(`<h1>Welcome to the LOTR Post Server!</h1>`)
-})
+    response.send(`<h1>Welcome to the LOTR Post Server!</h1>`);
+});
 
 server.get('/api/posts', (request, response) => {
     db.find()
     .then(posts => response.json(posts))
     .catch(error => response.send(error))
-})
+});
 
 server.get('/api/posts/:id', (request, response) => {
     db.findById(request.params.id)
     .then(post => response.json(post))
     .catch(error => response.send(error))
-})
+});
+
+server.post('/api/posts', (request, response) => {
+    db.insert(request.body)
+    .then(postId => response.json(postId))
+    .catch(error => response.send(error))
+});
+
+server.put('/api/posts/:id', (request, response) => {
+    db.update(request.params.id, request.body)
+    .then(updated => {
+        if(updated) {
+            response.send(updated)
+        }
+    })
+    .catch(error => response.send(error))
+});
+
+server.delete('/api/post/:id', (request, response) => {
+    db.remove(request.params.id)
+    .then()
+    .catch()
+});
