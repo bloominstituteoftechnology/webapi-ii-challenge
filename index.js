020fc35d817569d2eb75a31defd6ca9fcb88a8f1
decagondev <decagondevelopment@gmail.com>
2018-10-01 23:48:46 +0100

    (STRETCH) : added delete endpoint to the server

--- index.js
+++ index.js
@@ -111,7 +111,19 @@ server.get("/api/posts/:id", async (req, res) => {
     return the following JSON object: { error: "The post could not be removed" }.
 */
 
-server.delete("/api/posts/:id", async (req, res) => {});
+server.delete("/api/posts/:id", async (req, res) => {
+  try {
+    const post = await db.remove(req.params.id);
+    if (post === 0) {
+      return res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
+    }
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json({ error: "The post could not be removed" });
+  }
+});
 
 /*
   edit a single post based upon id TODO: fill in logic
