b8fe252a98d2ef6fb3cfbec3b1688bb247021b29
decagondev <decagondevelopment@gmail.com>
2018-10-01 21:01:44 +0100

    (MVP) : added skeleton for the 'get posts based upon id endpoint' : (USING async, await and try catch)

--- index.js
+++ index.js
@@ -73,7 +73,21 @@ server.get("/api/posts", async (req, res) => {
     return the following JSON object: { error: "The post information could not be retrieved." }.
 */
 
-server.get("/api/posts/:id", (req, res) => {});
+server.get("/api/posts/:id", async (req, res) => {
+  try {
+    const post = await db.findById(req.params.id);
+    if (post.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
+    }
+    res.status(200).json(post);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "The post information could not be retrieved." });
+  }
+});
 
 /*
   delete a single post besed upon id TODO: fill in logic
