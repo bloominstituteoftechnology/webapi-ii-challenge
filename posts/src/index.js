52b2dd73f4489808afd74f12ccc09d65cf32ccc5
Clint Kunz <clintkunz@gmail.com>
2018-10-01 14:50:34 -0600

    added redux and store to index file

--- posts/src/index.js
+++ posts/src/index.js
@@ -4,5 +4,17 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>
+    , document.getElementById('root'));
 registerServiceWorker();
