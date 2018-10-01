cd0bcd7b57296bd925c622df6ba0231e3e97aaf7
Keiran <keirankozlowski@gmail.com>
2018-10-01 17:06:47 -0400

    Set up GET posts and imported dependencies.

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 // import your node modules
 
 const db = require('./data/db.js');
+const express = require('express');
+const cors = require('cors');
 
 // add your server code starting here
+const server = express();
+server.use(cors());
+
+server.get('/api/posts', (request, response) => {
+    db.find()
+        .then(posts => response.json(posts))
+        .catch(error => response.send(error));
+        console.log(`Posts: ${posts}`)
+});
+
+const port = 8000;
+server.listen(port, () => 
+    console.log(`Server is listening to Port ${port}`)
+)
