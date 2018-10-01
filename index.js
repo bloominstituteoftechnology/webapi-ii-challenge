8fdfa2f8fca2476c3f2fec44ac2522cb57640773
Clint Kunz <clintkunz@gmail.com>
2018-10-01 14:33:44 -0600

    GET method functional

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 // import your node modules
-
+const express = require('express');
+const cors = require('cors');
 const db = require('./data/db.js');
 
 // add your server code starting here
+const server = express();
+
+server.use(cors());
+
+server.get('/', (req, res) => {
+    db.find()
+        .then(posts => {
+            console.log('server get', posts);
+            res.json(posts);
+        })
+        .catch(err => res.send(err))
+})
+
+const port = 8100; 
+
+server.listen(port, () => 
+    console.log(`\n=== API running on port ${port} ===`));
