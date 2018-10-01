b8847779be97deaf63159923184d4af782b406a1
David Layman <davidlayman85@gmail.com>
2018-10-01 16:55:51 -0400

    david layman node-express-lab

--- index.js
+++ index.js
@@ -3,3 +3,38 @@
 const db = require('./data/db.js');
 
 // add your server code starting here
+
+const express = require('express');
+
+const server = express();
+
+server.get('/',(req,res) => {
+res.send('use the endpoints "/posts", or "/posts/"number" to view either all posts, or a single post by id number')
+});
+
+server.get('/posts',(req,res)=>{
+db.find()
+	.then(data =>
+	res.status(200).json(data)
+	)
+	.catch(()=>{
+	res.status(500).json({text:'internal server error'})
+	})
+});
+
+server.get('/posts/:id',(req,res)=>{
+db.findById(req.params.id)
+	.then(data =>{
+	if(data.length){
+	res.status(200).json(data)
+	}
+	else{
+	res.status(404).json({message:'post not found'})
+	}
+	})
+	.catch(()=>{
+	res.status(500).json({text:'internal server error'})
+	})
+})
+
+server.listen(3333, () => console.log('server listening on port 3333'));
