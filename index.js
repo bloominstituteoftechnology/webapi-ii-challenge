97d164724cd0f19da81dc11204d381edb115dac5
Lndubose <lndubose13@gmail.com>
2018-10-01 13:08:20 -0700

    Finished get in server and I am making the react app now

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 // import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
 
 // add your server code starting here
+const server = express();
+
+const port = 5000;
+server.listen(port, () => console.log(`--- Server running on port ${port} ---\n`));
+
+server.get('/', (req, res) => {
+  res.send('<h1>Welcome to posts!</h1>')
+})
+
+server.get('/api/posts', (req, res) => {
+  db.find()
+    .then(response => res.json(response))
+    .catch(err => res.status(500).json({error: "The post information could not be retrieved."}))
+})
