9914cbb82fa3c1e9fdf877953b319c12404ac669
JeanetteFernandez <jeanfern555@gmail.com>
2018-10-01 15:37:46 -0500

    Completed GET Endpoint - collections of posts are showing.

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 // import your node modules
 
+const express = require('express');
+
 const db = require('./data/db.js');
 
 // add your server code starting here
+const server = express();
+
+server.get('/api/posts', (req, res) => {
+    db.find()
+      .then(posts => {
+          console.log('posts:', posts);
+          res.json(posts);
+      })
+      .catch(err => res.send(err));
+});
+
+
+
+
+const port = 8001;
+server.listen(port, () => 
+    console.log(`\n=== API running on port ${port} ===\n`)
+);
