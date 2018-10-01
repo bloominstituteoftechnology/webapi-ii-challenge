a8b2692c4e87bab493f65798167ef17b2eab1534
Clint Kunz <clintkunz@gmail.com>
2018-10-01 15:06:32 -0600

    reducer set up

--- posts/src/index.js
+++ posts/src/index.js
@@ -8,9 +8,9 @@ import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 
-import rootReducer from './reducers';
+import postsReducer from './reducers/index';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(postsReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
 <Provider store={store}>
---
+++ posts/src/reducers/index.js
@@ -0,0 +1,21 @@
+import { FETCHING_POSTS, POSTS_FETCHED, FETCH_ERROR } from '../actions';
+
+const initialState = {
+    fetchingPosts = false,
+    postsFetched = false,
+    posts = [],
+    error = null
+}
+
+export const postsReducer = (state = initialState, action) => {
+    switch(action.type) {
+        case FETCHING_POSTS:
+            return {...state, fetchingPosts: true};
+        case POSTS_FETCHED:
+            return {...state, fetchingPosts: false, posts: action.payload};
+        case FETCH_ERROR:
+            return {...state, fetchingPosts: false, error: action.payload};
+        default:
+            return state;
+    }
+}
