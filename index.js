b1e312a5dd346efaca89b64ffad68ebd5dc3a41d
Caleb Kirkwood <calebkirkwood@calebs-mbp.attlocal.net>
2018-10-01 16:16:34 -0400

    added get for /api/posts end point along with error handling

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 // import your node modules
+const express = require('express');
+const cors = require('cors');
+const port = 9000;
 
 const db = require('./data/db.js');
 
+const server = express();
+
 // add your server code starting here
+server.use(cors());
+
+server.get('/api/posts', (req, res) => {
+  db.find()
+    .then((posts) => {
+      console.log('** posts **', posts);
+      res.json(posts);
+    })
+    .catch((err) =>
+      res
+        .status(500)
+        .json({ error: 'The posts information could not be retrieved.' })
+    );
+});
+
+server.listen(port, (err) => {
+  if (err) console.log(err);
+  console.log(`server is listening on port ${port}`);
+});
