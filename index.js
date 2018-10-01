21f9cad3155d6260402a1dbedf555acc1f3460ad
decagondev <decagondevelopment@gmail.com>
2018-10-01 20:37:34 +0100

    (MVP) : added skeleton for the 'update post single post dependant upon id  endpoint'

--- index.js
+++ index.js
@@ -78,4 +78,30 @@ server.get("/api/posts/:id", (req, res) => {});
 
 server.delete("/api/posts/:id", (req, res) => {});
 
+/*
+  edit a single post based upon id TODO: fill in logic
+  --------------------------------
+
+  If the post with the specified id is not found:
+    return HTTP status code 404 (Not Found).
+    return the following JSON object: { message: "The post with the specified ID does not exist." }.
+
+  If the request body is missing the title or contents property:
+    cancel the request.
+    respond with HTTP status code 400 (Bad Request).
+    return the following JSON response: { errorMessage: "Please provide title and contents for the post." }.
+
+  If there's an error when updating the post:
+    cancel the request.
+    respond with HTTP status code 500.
+    return the following JSON object: { error: "The post information could not be modified." }.
+
+  If the post is found and the new information is valid:
+    update the post document in the database using the new information sent in the reques body.
+    return HTTP status code 200 (OK).
+    return the newly updated post.
+*/
+
+server.put("/api/posts/:id", (req, res) => {});
+
 server.listen(8000, () => console.log("API listenint on port 8000"));
