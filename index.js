48c810525c3b6202a25b76ad4619f5bd2f5b093a
Kevin Nguyen <kevinn858@gmail.com>
2018-10-01 12:54:38 -0700

    server is listening on port 8000

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 // import your node modules
 
 const db = require('./data/db.js');
+const express = require('express');
+const server = express();
+cors = require('cors');
 
 // add your server code starting here
+const port = 8000;
+server.listen(port, ()=>{
+    console.log(`API running on port ${port}`);
+})
