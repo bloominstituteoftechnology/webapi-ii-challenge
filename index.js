9f44b92df7ffd7e683c6be84a17b64faaa2782cb
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-10-01 15:03:35 -0400

    Huthman King: Node-Express-Labs Get

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 // import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
 
+
+
+
 // add your server code starting here
+
+const server = express(); // creates the server
+
+server.get('/', (req, res) => { // request route handler
+    res.send('Awaken');
+});
+
+server.get('/api/posts', (req, res) =>{
+    db.find()
+        .then(posts => {
+            console.log('\n** users **', posts);
+            res.json(posts);
+        })
+        .catch(err => {
+            console.log(err);
+            res.json({ error: "Uhh we can't seem to find your posts..."});
+        });
+});
+
+// watch for traffic in a particular computer port
+const port = 9000;
+server.listen (port, () => 
+console.log(`\n=== API running on port ${port} ===\n`)
+);
