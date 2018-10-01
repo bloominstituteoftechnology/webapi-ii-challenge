4b70bb6c7903d337b554da781ca9003c621298e7
Vera Simon <ghjkl>
2018-10-01 13:34:31 -0700

    Node Express Lab - Day I
    Finished GET All

--- index.js
+++ index.js
@@ -7,7 +7,17 @@ const db = require('./data/db.js');
 const server = express();
 server.use(cors());
 
+server.get('/', (req, res) => {
+    res.send('Nothing here...')
+});
 
+server.get('/api/posts', (req, res) => {
+    db.find()
+        .then((posts) => {
+            res.json(posts);
+        })
+        .catch((err) => res.send(err));
+});
 
 const port = 9001;
 server.listen(port, () => console.log(`~~~ API running on port ${port} ~~~`));
