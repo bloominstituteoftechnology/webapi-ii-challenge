125cc5edece6b8216fc5c7e16768956ccab21f78
Keiran <keirankozlowski@gmail.com>
2018-10-01 17:12:49 -0400

    Removed console.log that was causing an error.

--- index.js
+++ index.js
@@ -8,14 +8,13 @@ const cors = require('cors');
 const server = express();
 server.use(cors());
 
-server.get('/api/posts', (request, response) => {
-    db.find()
-        .then(posts => response.json(posts))
-        .catch(error => response.send(error));
-        console.log(`Posts: ${posts}`)
-});
-
 const port = 8000;
 server.listen(port, () => 
     console.log(`Server is listening to Port ${port}`)
 )
+
+server.get('/api/posts', (request, response) => {
+    db.find()
+        .then(posts => response.json(posts))
+        .catch(error => response.send(error));
+});
