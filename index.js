43ddc085eb4d1bade57cef984caf26752ee79fcf
decagondev <decagondevelopment@gmail.com>
2018-10-01 20:17:55 +0100

    (MVP) : imported cors, instantiated the server and set the server to listen on port 8000

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const db = require("./data/db.js");
 
 // import express
 const express = require("express");
+
+// import cors
+const cors = require("cors");
+
+// instantiate express server
+const server = express();
+
+server.listen(8000, () => console.log("API listenint on port 8000"));
