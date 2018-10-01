c00d896993071501f3dcfa219a3bed9364b51bf6
BrianARuff <thelonewolf198927@gmail.com>
2018-10-01 19:04:00 -0400

    added in redux for project with err handling and changed err number to 500 with special message

--- index.js
+++ index.js
@@ -17,19 +17,21 @@ server.use(cors());
 const port = 9000;
 
 // test page
-server.get("/", (req, res) => res.send(`
+server.get("/", (req, res) =>
+  res.send(`
 <h1>Weclome</h1>
 <h2>API End Points</h2>
 <ul>
-  <li><a href="http://localhost:9000/api/posts">/api/posts</a></li>
+  <li><a href=http://localhost:${port}/api/posts>/api/posts</a></li>
 </ul>
-`));
+`)
+);
 
 // GET listener
 server.get("/api/posts", (req, res) => {
   db.find()
     .then(posts => res.json(posts))
-    .catch(err => res.send(err));
+    .catch(err => res.status(500).send(`The posts information could not be retrieved.`));
 });
 
 // set up server to listen to w/e number port is
