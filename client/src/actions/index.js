dc2b09a3c6dae7b56955e73738d955e0bf36d95d
Michael Agard <mdagard@gmail.com>
2018-10-01 17:00:00 -0400

    add fetchFriend dependencies

---
+++ client/src/actions/index.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export const FETCHING_POSTS = 'FETCHING';
+export const FETCHED_POSTS = 'FETCHED';
+export const FETCH_POSTS_ERRORS = 'ERRORS';
+
+export const fetchPosts = () => {
+  return dispatch => {
+    dispatch({ type: FETCHING_POSTS });
+    axios
+      .get('http://localhost:3000/api/posts')
+
+      .then(response => {
+        console.log(response.data);
+        dispatch({ type: FETCHED_POSTS, payload: response.data });
+      })
+
+      .catch(error => dispatch({ type: FETCH_POSTS_ERRORS, error: error }));
+  };
+};
