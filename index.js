1d3ec1069f8dc7bed3d23d9d681adec9a5ef95e9
unknown <cornielleandres@gmail.com>
2018-10-01 14:57:40 -0400

    Set up server to accept basic GET request and create front end skeleton.

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 // import your node modules
-
+const express = require('express');
+const cors = require('cors');
 const db = require('./data/db.js');
 
 // add your server code starting here
+const server = express();
+server.use(cors());
+
+server.get('/', (req, res) => {
+	db.find()
+		.then(users => res.json(users))
+		.catch(err => console.log(err))
+});
+
+const port = 5000;
+server.listen(port, () => console.log(`\n=== Listening on port ${ port } ===`));
