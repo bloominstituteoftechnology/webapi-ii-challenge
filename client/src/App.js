e2430332a4fa6cedc5026593977730d49fc82b94
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-10-01 16:55:23 -0400

    added simple posts information display in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
   render() {
     return (
       <div className = 'App'>
-        <p>Test: </p>
+        <p>Test: {this.props.posts.map(post => {post})}</p>
       </div>
     );
   }
