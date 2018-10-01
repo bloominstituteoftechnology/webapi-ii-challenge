ebc4689555b0ba1e2bb580b244c23b5030876223
Kevin Sooter <kevin.m.sooter@gmail.com>
2018-10-01 15:35:08 -0500

    Created GET endpoint.  MVP done.

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
+// import your node modules
+const express = require('express');
+const cors = require('cors');
+
 // import database handler
 const db = require('./data/db.js');
 
-// import express
-const express = require('express');
-
-// import cors
-const cors = require('cors');
-
-// express server
+// add your server code starting here
 const server = express();
+
+server.use(cors());
+
+server.get('/api/posts', (req, res) => {
+  db.find()
+    .then(posts => {
+      console.log('\n** posts **', posts);
+      res.json(posts);
+    })
+    .catch(err => res.send(err));
+});
+
+const port = 8000;
+server.listen(port, () =>
+  console.log(`\n=== API running on port ${port} ===\n`)
+);
