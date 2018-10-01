7a26efd2dbbc5c45b7d9e28d7adaef0a9a378562
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2018-10-01 15:14:11 -0400

    - Added all needed dependencies - Created GET endpoint

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 // import your node modules
+const express = require("express");
+const cors = require("cors");
 
-const db = require('./data/db.js');
+const db = require("./data/db.js");
 
 // add your server code starting here
+const server = express();
+
+server.use(cors());
+
+server.get("/api/posts", (req, res) => {
+  db.find()
+    .then(posts => {
+      console.log("\n** posts **", posts);
+      res.json(posts);
+    })
+    .catch(err => res.send(err));
+});
+
+const port = 8000;
+server.listen(port, () =>
+  console.log(`\n=== API running on port ${port} ===\n`)
+);
