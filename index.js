640ff441f8fbc4796ec7e1eddbed898ec36795b1
Joannah Laluces <joannahlaluces@Joannahs-MacBook-Air.local>
2018-10-01 12:30:15 -0700

    Finished posts endpoint

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 // import your node modules
 
+const express = require('express');
 const db = require('./data/db.js');
+const cors = require('cors');
 
 // add your server code starting here
+
+const server = express();
+
+const port = 7500;
+
+server.listen(port, () =>
+  console.log(`Testing port ${port}`)
+);
+
+server.get('/', (req, res) => {
+  res.send('<h3>I am ROOT</h3>')
+});
+
+server.get('/api/posts', (req, res) => {
+  db.find()
+    .then(posts => {
+      res.json(posts)
+    })
+    .catch(err => {
+      res.send(`${err}: The posts information could not be retrieved.`)
+    })
+})
