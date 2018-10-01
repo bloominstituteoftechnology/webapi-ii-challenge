e1b1f63cb3f7a5dff0f2ee855697366112e9e3a0
unknown <cornielleandres@gmail.com>
2018-10-01 15:07:45 -0400

    Make successfull axios call and render data to front end.

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	state = {
+		users: [],
+	};
+
+	componentDidMount() {
+		const URL = 'http://localhost:5000/';
+		axios.get(`${ URL }`)
+			.then(res => {
+				console.log(res);
+				this.setState({
+					users: res.data
+				});
+			})
+			.catch(err => console.error(err))
+	}
+
+	render() {
+		return (
+			<div className = 'App'>
+				{ this.state.users.map((user, i) => <div key = { i }>{ user.title }</div>) }
+			</div>
+		);
+	}
 }
 
 export default App;
