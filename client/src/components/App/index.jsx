8110ab052397c36c49d9f54f91951f61442eb4e3
decagondev <decagondevelopment@gmail.com>
2018-10-01 21:13:20 +0100

    (DAY 1 STRETCH) : created client project and started making a skeleton for a post reading app

---
+++ client/src/components/App/index.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import axios from "axios";
+import "./index.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Node Express Labs</h1>
+        </header>
+        <p className="App-intro">posts will go here</p>
+      </div>
+    );
+  }
+}
+
+export default App;
