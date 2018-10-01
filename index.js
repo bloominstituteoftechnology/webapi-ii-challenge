ceaf5ae8d5b7a76b5982e224dd97a4c846b95979
m-j-hacker <mjhacker@gmail.com>
2018-10-01 15:59:24 -0500

    Initial commit

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 // import your node modules
+const express = require('express');
+const cors = require('cors');
 
 const db = require('./data/db.js');
 
+const server = express();
+
+
 // add your server code starting here
+server.use(cors());
+
+server.get('/', (req,res) => {
+    res.send('<h1>Michael Hacker - Node Express Lab Assignment</h1>');
+});
+
+server.get('/api/posts', (req, res) => {
+    db.find()
+    .then(posts => {
+        console.log('\n** posts **', posts);
+        res.status(200).json(posts);
+    })
+    .catch(err => res.send(err));
+})
+
+server.get('/api/posts/:id', (req, res) => {
+    db.findById(id)
+})
+
+const port = 8000;
+server.listen(port, () => console.log(`API running on port ${port}`));
