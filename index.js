8f482285afd53f4610a139b419974e7384353aa1
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-10-01 16:10:50 -0400

    added server/express imports and a test 'get' request

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 // import your node modules
+const express = require('express');
+const server = express();
 const knex = require('knex');
 const knexConfig = require('./knexfile.js');
 
 const db = require('./data/db.js');
 
 // add your server code starting here
+
+// quick text display to make sure everything loads properly
+
+server.get('/', (req, res) => {
+  res.send('Testing server.');
+});
