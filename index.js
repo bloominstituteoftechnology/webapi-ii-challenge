d463bc94e470d31dde4993dda45b55301ba115fe
m-j-hacker <mjhacker@gmail.com>
2018-10-01 16:30:39 -0500

    Got posts and posts/:id working properly on the server

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const server = express();
 // add your server code starting here
 server.use(cors());
 
+server.post('/api/posts')
+
 server.get('/', (req,res) => {
     res.send('<h1>Michael Hacker - Node Express Lab Assignment</h1>');
 });
@@ -20,11 +22,17 @@ server.get('/api/posts', (req, res) => {
         console.log('\n** posts **', posts);
         res.status(200).json(posts);
     })
-    .catch(err => res.send(err));
+    .catch(() => res.status(500).json({ error: "The posts information could not be retrieved."}));
 })
 
 server.get('/api/posts/:id', (req, res) => {
+    const id = req.params.id;
     db.findById(id)
+    .then(post => {
+        console.log('\n** POST **', post);
+        res.status(200).json(post);
+    })
+    
 })
 
 const port = 8000;
