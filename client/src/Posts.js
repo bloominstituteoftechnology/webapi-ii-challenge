3aaaa56f7470b6cfe9a372e2ca393c650fecf99b
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-10-01 16:17:48 -0400

    some styling

--- client/src/Posts.js
+++ client/src/Posts.js
@@ -29,10 +29,14 @@ class Posts extends Component {
                 <div className="posts">
                 {this.state.posts.map(post => {
                     return (
-                        <div key={post.id}>
-                            <h1>{post.title}</h1>
-                            <p>{post.contents}</p>
+                        <div className = 'forPosts' key={post.id}>
+                        <div className = 'title'>
+                            <h2>{post.title}</h2>
+                        </div>
+                        <div className = 'thePostss'>
+                            <p className = 'thePosts'>{post.contents}</p>
                             <p>{post.create_at}</p>
+                        </div>
                         </div>
                     )
                 })}
