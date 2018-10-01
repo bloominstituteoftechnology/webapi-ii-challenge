67c8cb0d65080f7abee05a78ddecb20b5ccc2f0e
Brock Maraee Rohloff <00AgentB@gmail.com>
2018-10-01 13:47:32 -0600

    Implement Initial Server and GET Method Endpoints.

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 // import your node modules
+const express =  require('express');
 
 const db = require('./data/db.js');
 
 // add your server code starting here
+
+const server = express();
+
+const port = 7777;
+server.listen(port, console.log(`=-=-= Server Active On Port ${port} =-=-=`));
+
+server.get('/', (request, response) => {
+    response.send(`<h1>Welcome to the LOTR Post Server!</h1>`)
+})
+
+server.get('/api/posts', (request, response) => {
+    db.find()
+    .then(posts => response.json(posts))
+    .catch(error => response.send(error))
+})
+
+server.get('/api/posts/:id', (request, response) => {
+    db.findById(request.params.id)
+    .then(post => response.json(post))
+    .catch(error => response.send(error))
+})
