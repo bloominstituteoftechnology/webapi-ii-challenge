49f5d25cadbd1b155f8a9cc483963dfa27d022a4
Arkadiusz Kusiak <akusiak23@gmail.com>
2018-10-01 16:26:28 -0500

    mvp

--- index.js
+++ index.js
@@ -3,3 +3,27 @@
 const db = require('./data/db.js');
 
 // add your server code starting here
+
+const express = require('express');
+const cors = require('cors');
+const server = express();
+
+server.use(cors());
+server.get('/', (req, res) => {
+    // request/route handler
+    res.send('test');
+});
+
+server.get('/api/posts', (req, res) => {
+    db.find()
+        .then(posts => {
+            console.log('\n** posts **', posts);
+            res.json(posts);
+        })
+        .catch(err => res.send(err));
+})
+
+const port = 8000;
+server.listen(port, () =>
+    console.log(`\n=== API running on port ${port} ===\n`)
+);
