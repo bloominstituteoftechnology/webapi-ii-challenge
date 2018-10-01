7b3378c0d7058fb5d5ba8a748d3425d019929f0c
Andrew <ghobrial.andrew@gmail.com>
2018-10-01 15:39:36 -0400

    added express, began writing GET request layout

--- index.js
+++ index.js
@@ -3,3 +3,22 @@
 const db = require('./data/db.js');
 
 // add your server code starting here
+
+// require the express npm module, needs to be added to the project using "yarn add" or "npm install"
+const express = require('express');
+
+// creates an express application using the express module
+const server = express();
+
+// configures our server to execute a function for every GET request to "/"
+// the second argument passed to the .get() method is the "Route Handler Function"
+// the route handler function will run on every GET request to "/"
+server.get('/api/posts', (req, res) => {
+  // express will pass the request and response objects to this function
+  // the .send() on the response object can be used to send a response to the client
+  res.send('Hello World');
+});
+
+// once the server is fully configured we can have it "listen" for connections on a particular "port"
+// the callback function passed as the second argument will run once when the server starts
+server.listen(8000, () => console.log('API running on port 8000'));
