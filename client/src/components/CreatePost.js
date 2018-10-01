9b22019f8ff9ab5e38f6ae46bc478bc1298fd0d9
unknown <cornielleandres@gmail.com>
2018-10-01 19:08:00 -0400

    Styled CreatePost component.

--- client/src/components/CreatePost.js
+++ client/src/components/CreatePost.js
@@ -1,10 +1,50 @@
 import React from 'react';
 import axios from 'axios';
 
+import styled from 'styled-components';
+
+const Form = styled.form`
+	display: flex;
+	flex-direction: column;
+	border: 1px solid black;
+	border-radius: 5px;
+	justify-content: center;
+	align-items: center;
+	width: 50%;
+	padding: 10px;
+	animation-name: fade-in-anim;
+	animation-duration: 2s;
+	animation-fill-mode: forwards;
+
+	input, textarea {
+		padding: 5px;
+		border-radius: 5px;
+		margin-bottom: 10px;
+		width: 50%;
+	}
+
+	textarea {
+		height: 100px;
+	}
+
+	button {
+		padding: 5px 10px;
+		border-radius: 5px;
+		background: white;
+
+		&:hover {
+			background: black;
+			color: white;
+			cursor: pointer;
+		}
+	}
+`;
+
 export default class CreatePost extends React.Component {
 	state = {
 		title: '',
 		contents: '',
+		errorMsg: '',
 	};
 
 	handleInputChange = e => {
@@ -15,9 +55,17 @@ export default class CreatePost extends React.Component {
 
 	handleSubmit = e => {
 		e.preventDefault();
+
+		if (!this.state.title || !this.state.contents) {
+			return this.setState({
+				...this.state,
+				errorMsg: 'All fields must be filled out.',
+			})
+		}
+
 		const newPost = {
-			[e.target[0].name]: e.target[0].value,
-			[e.target[1].name]: e.target[1].value,
+			title: this.state.title,
+			contents: this.state.contents,
 		};
 
 		const URL = 'http://localhost:5000';
@@ -29,25 +77,27 @@ export default class CreatePost extends React.Component {
 
 	render() {
 		return(
-			<form onSubmit = { this.handleSubmit }>
-				Title:
+			<Form onSubmit = { this.handleSubmit }>
+				Quote:
 				<input
-					placeholder = 'Enter title'
+					placeholder = 'Enter quote'
 					name = 'title'
 					value = { this.state.title }
 					onChange = { this.handleInputChange }
 				/>
 
 				Contents:
-				<input
+				<textarea
 					placeholder = 'Enter content'
 					name = 'contents'
 					value = { this.state.contents }
 					onChange = { this.handleInputChange }
 				/>
 
+				{ this.state.errorMsg && <p>{ this.state.errorMsg }</p> }
+
 				<button type = 'submit'>Submit</button>
-			</form>
+			</Form>
 		);
 	}
 };
