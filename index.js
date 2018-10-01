495b997bbfdff7e0499e3dd55487b84e9834489f
Michael Agard <mdagard@gmail.com>
2018-10-01 16:24:20 -0400

    add express cors, db, server, cors, endpoint /api/posts port 8000

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 // import your node modules
 
+const express = require('express'); // CommonJS modules > modules.exports = someCode;
+const cors = require('cors');
 const db = require('./data/db.js');
+const server = express(); // creates the server
 
 // add your server code starting here
+
+server.use(cors()); // this needed to connect from react
+
+server.get('/api/posts', (req, res) => {
+  db.find()
+    .then(users => {
+      console.log('\n** posts **, posts');
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+});
+
+const port = 8000;
+server.listen(port, () =>
+  console.log(`\n=== API running on port ${port} ===\n`)
+);
