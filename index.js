185b6734240a8b433d75bcb24f543f956e184f6c
Timothy Hoang <timh1203@gmail.com>
2018-10-01 16:44:53 -0400

    Add express and set up GET route handler

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-// import your node modules
-
+// MODULES
+const express = require('express');
 const db = require('./data/db.js');
 
-// add your server code starting here
+// SERVER SETUP
+const server = express();
+
+// API ENDPOINTS
+server.get('/api/posts', (req, res) => {
+  db.find()
+    .then(posts => {
+      console.log("posts", posts);
+      res.json(posts);
+    })
+    .catch(err => res.send(err));
+})
+
+// PORT LISTENER
+const port = 8000
+server.listen(port, () => console.log(`=== ${port} active ===`))
