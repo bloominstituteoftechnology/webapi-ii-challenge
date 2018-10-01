f4e8e17b779795948f13d60470e4ffc18d37575f
David Benavidez <benavidez_david13@yahoo.com>
2018-10-01 15:57:35 -0700

    finished mvp

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 // import your node modules
+const express = require('express');
+
 
 const db = require('./data/db.js');
 
+const server = express();
+
+
+const cors = require('cors');
+
+server.use(cors());
+ server.get('/', (req, res) => { //request/route handler
+    res.send('Hello FSW13');
+});
+ server.get('/api/posts', (req, res) =>{
+    db.find().then(posts => {
+        res.json(posts);
+    }).catch(err => res.send(err))
+});
+
 // add your server code starting here
+
+const port = 5000;
+server.listen(port, () => console.log(`API running on port ${port}`)); 
