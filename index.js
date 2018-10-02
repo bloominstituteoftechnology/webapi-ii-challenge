cd6b70f744d262e1c110dab7666e8da1ab5e353f
Timothy Hoang <timh1203@gmail.com>
2018-10-02 16:49:59 -0400

    Add post method for posts route

--- index.js
+++ index.js
@@ -6,17 +6,22 @@ const cors = require('cors');
 // SERVER SETUP
 const server = express();
 server.use(cors());
+server.use(express.json())
 
 // API ENDPOINTS
+server.post('/api/posts', (req, res) => {
+  const newPost = req.body;
+  db.insert(newPost)
+    .then(posts => res.json(posts))
+    .catch(err => res.status(400).json({ errorMessage: "Please provide title and contents for the post." }));
+})
+
 server.get('/api/posts', (req, res) => {
   db.find()
     .then(posts => {
-      console.log("posts", posts);
-      res.json(post);
+      res.json(posts);
     })
-    .catch(err => {
-      res.status(500).json({ error: "The posts information could not be retrieved." });
-    });
+    .catch(err => res.status(500).json({ error: "The posts information could not be retrieved." }));
 })
 
 // PORT LISTENER
