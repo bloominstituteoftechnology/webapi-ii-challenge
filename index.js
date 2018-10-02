fbb6ffe4902c9f55553cf5ca71252c2fbdb4c779
Michael Agard <mdagard@gmail.com>
2018-10-02 15:23:25 -0400

    added about api endpoint

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ server.get('/api/posts', (req, res) => {
     .catch(err => res.send(err));
 });
 
+server.get('/api/about', (req, res) => {
+  res.status(200).send('<h1>About This API</h1><p>This is a test message</p>');
+});
+
 const port = 8000;
 server.listen(port, () =>
   console.log(`\n=== API running on port ${port} ===\n`)
