0cfb00accdab41e91a188411e9352d4af5ede9f5
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2018-10-02 17:39:27 -0400

    Update endpoint is functional

--- index.js
+++ index.js
@@ -48,6 +48,19 @@ server.delete("/api/posts/:id", (req, res) => {
     .catch(err => console.error(err));
 });
 
+server.put("/api/posts/:id", (req, res) => {
+  const { id } = req.params;
+  const { title, contents } = req.body;
+  const newPost = { title, contents };
+  console.log(newPost);
+  db.update(id, newPost)
+    .then(post => {
+      console.log(post);
+      res.status(200).json(post);
+    })
+    .catch(err => console.error(err));
+});
+
 const port = 8000;
 server.listen(port, () =>
   console.log(`\n=== API running on port ${port} ===\n`)
