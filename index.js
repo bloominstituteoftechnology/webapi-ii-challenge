62e5a719993771ddebc1ef05d464e1940501e10d
Casey Baker <caseye.baker@gmail.com>
2018-10-02 15:22:38 -0700

    still working on conditionals

--- index.js
+++ index.js
@@ -22,16 +22,24 @@ server.get('/api/posts', (req, res) => {
   .then(posts => {
     console.log('\n** posts **', posts)
     res.json(posts);
-  }).catch(err => res.status(500).send({ error: "The posts information could not be retrieved." }))
+  }).catch(err => {
+    console.error(err)
+    res.status(500).send({ error: "The posts information could not be retrieved." })
+  })
 })
 
 server.get('/api/posts/:id', (req, res) => {
   const { id } = req.params;
   db.findById(id)
   .then(post => {
-    console.log('\n ** single post **', post)
+    if (!post) {
+      console.log(post);
+      res.status(404).json({ message: "The post with the specified ID does not exist." })
+    }
     res.json(post);
-  }).catch(err => res.send(err))
+  }).catch(err => {
+    res.status(500).send({ error: "The post information could not be retrieved."})
+  })
 })
 
 server.post('/api/posts', (req, res) => {
@@ -41,15 +49,18 @@ server.post('/api/posts', (req, res) => {
     .then(postId => {
       const { id } = postId;
       db.findById(id).then(post => {
-        if (!post) {
+        if (!post.title || !post.contents) {
           return res
-          .status(422)
-          .send({ Error: `Post does not exist by that id ${id}`});
+          .status(400)
+          .send({ Error: "Please provide title and contents for the post." });
         }
         res.status(201).json(post);
       });
     })
-    .catch(err => console.error(err));
+    .catch(err => {
+      console.error(err)
+    res.status(500).json({error: "There was an error while saving the post to the database."})
+  });
 })
 
 server.delete('/api/posts/:id', (req, res) => {
@@ -59,7 +70,10 @@ server.delete('/api/posts/:id', (req, res) => {
     .then(removedPost => {
       res.status(200).json(removedPost);
     })
-    .catch(err => console.error(err));
+    .catch(err => {
+       console.error(err)
+       res.status(500).json({ error: "The post could not be removed"})
+    });
 })
 
 server.put('/api/posts/:id', (req, res) => {
@@ -74,6 +88,9 @@ server.put('/api/posts/:id', (req, res) => {
           return res
           .status(404)
           .json({ message: "The post with the specified ID does not exist."})
+        }
+        else if (!updatedPost.title || !updatedPost.contents) {
+          res.status(400).json({ errorMessage: "Please provide title and contents for the post."})
         }
         res.status(200).json(updatedPost)
       })
