588355113c46e654770cb1649a4bc4c2316f4e44
Brock Maraee Rohloff <00AgentB@gmail.com>
2018-10-02 09:37:34 -0600

    finish 33451234624ds23

--- index.js
+++ index.js
@@ -17,23 +17,32 @@ server.get('/', (request, response) => {
 });
 
 server.get('/api/posts', (request, response) => {
+    const error500 = { message: "The posts information could not be retrieved" };
     db.find()
     .then(posts => response.json(posts))
-    .catch(error => response.send(error))
+    .catch(
+        error => {
+            response.status(500).send(JSON.stringify(error500))
+        })
 });
 
 server.get('/api/posts/:id', (request, response) => {
+    const error404 = { message: "The post with the specified ID does not exist." };
     db.findById(request.params.id)
-    .then(post => response.json(post))
+    .then(post => 
+        {   if (post.length > 0) {
+            response.json(post)}
+            else response.status(404).send(JSON.stringify(error404));
+        })
     .catch(error => response.send(error))
 });
 
-// server.post('/api/posts', (request, response) => {
-//     if(request.body.) {
-//     db.insert(request.body)
-//     .then(postId => response.json(postId))
-//     .catch(error => response.status(500).send(error))    
-// }});
+server.post('/api/posts', (request, response) => {
+    if(request.body.title && request.body.contents) {
+    db.insert({title: request.body.title, contents: request.body.contents})
+    .then(postId => response.json(postId))
+    .catch(error => response.status(500).send(error))    
+}});
 
 // server.put('/api/posts/:id', (request, response) => {
 //     db.update(request.params.id, request.body)
