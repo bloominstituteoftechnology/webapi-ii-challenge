cc2229be3f67811ec21fd7bd6229f48db8a1e7d5
duckets615 <33557104+duckets615@users.noreply.github.com>
2018-10-02 13:53:34 -0400

    day 1 mvp

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 // import your node modules
 
-const db = require('./data/db.js');
+
 
 // add your server code starting here
+// import your node modules
+const express = require('express');
+ const db = require('./data/db.js');
+ const server = express();
+ const cors = require('cors');
+ server.use(cors());
+ server.get('/', (req, res) => { //request/route handler
+    res.send('Hello Chris');
+});
+ server.get('/api/posts', (req, res) =>{
+    db.find().then(posts => {
+        res.json(posts);
+    }).catch(err => res.send(err))
+});
+ // add your server code starting here
+ const port = 8000;
+server.listen(port, () => console.log(`===API running on port ${port}===`));
+
+server.get('/api/contact', (req, res) => {
+  res
+    .status(200)
+    .send('<div><h1>Contact</h1><input placeholder="email" /></div>');
+});
