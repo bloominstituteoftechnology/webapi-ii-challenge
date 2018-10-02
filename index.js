5443541f8c23f9165e53169657969ccc838c4b6b
Cameron Ray <cameronray@Camerons-MacBook-Pro.local>
2018-10-02 15:52:51 -0700

    still working

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 // import your node modules
+const express = require('express');
 
 const db = require('./data/db.js');
 
 // add your server code starting here
 // adding a comment so I can commit changes for daily form
+
+const server = express();
+
+server.listen(9000, () => {console.log('API is running.')});
+
+server.get('/api/posts', (request, response) => {
+    db
+        .find()
+        .then((posts) => response.json(posts) )
+        .catch(err => response.send(err))
+})
