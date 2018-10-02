eef91f04f49be30987b5c49629ce652a1a71337c
Casey Baker <caseye.baker@gmail.com>
2018-10-02 14:31:11 -0700

    got basic post, delete, and put functionality up; working on conditionals and status codes

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const server = express();
 // add your server code starting here
 
 server.use(cors());
+server.use(express.json());
 
 server.get('/', (req, res) => {
   res.send('<h1>EEEEEEEYYYY~</h1>')
@@ -21,9 +22,8 @@ server.get('/api/posts', (req, res) => {
   .then(posts => {
     console.log('\n** posts **', posts)
     res.json(posts);
-  }).catch(err => res.send(err, `500`, `{ error: "The posts information could not be retrieved." }`))
+  }).catch(err => res.status(500).send({ error: "The posts information could not be retrieved." }))
 })
-
 
 server.get('/api/posts/:id', (req, res) => {
   const { id } = req.params;
@@ -32,6 +32,54 @@ server.get('/api/posts/:id', (req, res) => {
     console.log('\n ** single post **', post)
     res.json(post);
   }).catch(err => res.send(err))
+})
+
+server.post('/api/posts', (req, res) => {
+  const { title, contents } = req.body;
+  const newPost = { title, contents };
+  db.insert(newPost)
+    .then(postId => {
+      const { id } = postId;
+      db.findById(id).then(post => {
+        if (!post) {
+          return res
+          .status(422)
+          .send({ Error: `Post does not exist by that id ${id}`});
+        }
+        res.status(201).json(post);
+      });
+    })
+    .catch(err => console.error(err));
+})
+
+server.delete('/api/posts/:id', (req, res) => {
+  const { id } = req.params;
+  
+  db.remove(id)
+    .then(removedPost => {
+      res.status(200).json(removedPost);
+    })
+    .catch(err => console.error(err));
+})
+
+server.put('/api/posts/:id', (req, res) => {
+  const { title, contents } = req.body;
+  const updatedPost = { title, contents };
+  const { id } = req.params;
+  db.update(id, updatedPost)
+    .then(postId => {
+      const { id } = postId;
+      db.findById(id).then(post => {
+        if (!post) {
+          return res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist."})
+        }
+        res.status(200).json(updatedPost)
+      })
+     
+    })
+    .catch(err => console.error(err))
 })
 
 const port = 8000;
