db37ac5db9493dfeb99d33477d6be5955e29b2d3
Tristen Neu <tristenneu@Tristens-iMac.local>
2018-10-02 17:54:46 -0500

    Get Functionality

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
 // import your node modules
+const express = require('express');
+const cors = require('cors');
 
 const db = require('./data/db.js');
 
 // add your server code starting here
+const server = express();
+
+server.use(cors());
+server.use(express.json());
+
+server.get('/api/posts', (req, res) => {
+    db
+    .find()
+    .then(pts => {
+        res.json(pts);
+    })
+    .catch(err => {
+        res.status(500).send({ error: "The posts information could not be retrieved." })
+    });
+});
+
+server.get('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    db
+    .findById(id)
+    .then(pstidv => {
+        res.json(pstidv);
+    })
+    .catch(err => {
+        res.status(404).send({ message: "The post with the specified ID does not exist." })
+    })
+})
+
+const port = 5000;
+server.listen(port, () => console.log(`API running on port ${port}`));
