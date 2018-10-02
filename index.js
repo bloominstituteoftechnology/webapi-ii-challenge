c083ef58f182947a835c53ebe96289047c48345b
Caleb Kirkwood <calebkirkwood@calebs-mbp.attlocal.net>
2018-10-02 16:32:58 -0400

    completed post endpoint

--- index.js
+++ index.js
@@ -10,21 +10,25 @@ const server = express();
 // add your server code starting here
 server.use(cors());
 
+server.use(express.json());
+
 server.get('/api/posts', (req, res) => {
   db.find()
     .then((posts) => {
       console.log('** posts **', posts);
       res.json(posts);
     })
-    .catch((err) =>
+    .catch((err) => {
+      console.log(err);
       res
         .status(500)
-        .json({ error: 'The posts information could not be retrieved.' })
-    );
+        .json({ error: 'The posts information could not be retrieved.' });
+    });
 });
 
 server.get('/api/posts/:id', (req, res) => {
-  db.findById(req.params.id)
+  const id = req.params.id;
+  db.findById(id)
     .then((post) => {
       if (post) {
         res.status(200).json({ post });
@@ -34,11 +38,41 @@ server.get('/api/posts/:id', (req, res) => {
           .json({ message: 'The post with the specified ID does not exist.' });
       }
     })
-    .catch((error) => {
-      console.log(error);
+    .catch((err) => {
+      console.log(err);
       res
         .status(500)
         .json({ error: 'The post information could not be retrieved.' });
+    });
+});
+
+server.post('/api/posts', (req, res) => {
+  console.log(req.body);
+  const { title, contents } = req.body;
+  if (!title || !contents) {
+    return res.status(400).send({
+      errorMessage: 'Please provide title and contents for the post.',
+    });
+  }
+  const newPost = { title, contents };
+  db.insert(newPost)
+    .then((postId) => {
+      const { id } = postId;
+      db.findById(id).then((post) => {
+        console.log(post);
+        if (!post) {
+          return res.status(400).send({
+            errorMessage: 'Please provide title and contents for the post.',
+          });
+        }
+        res.status(201).json(post);
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: 'There was an error while saving the post to the database',
+      });
     });
 });
 
