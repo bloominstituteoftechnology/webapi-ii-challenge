24260785460b5e2085dac7ea89ad36ab1b08a1d5
Michael Agard <mdagard@gmail.com>
2018-10-02 15:45:45 -0400

    add route paths for nav and home

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import PostList from './components/PostList';
-
+import { Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import PostForm from './components/PostForm';
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <PostList />
+        <Route path="/" component={NavBar} />
+        <Route exact path="/" component={PostList} />
+        <Route path="/postform" component={PostForm} />
       </div>
     );
   }
