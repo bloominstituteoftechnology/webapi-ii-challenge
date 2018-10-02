fb3c34242a88d1e0ba556e3150d05d6ae9d5d923
m-j-hacker <mjhacker@gmail.com>
2018-10-02 15:18:12 -0500

    Got server post working properly

--- index.js
+++ index.js
@@ -10,7 +10,26 @@ const server = express();
 // add your server code starting here
 server.use(cors());
 
-server.post('/api/posts')
+server.use(express.json());
+
+server.post('/api/posts', (req, res) => {
+    console.log(req.body);
+    const { title, contents } = req.body;
+    const newPost = { title, contents };
+    db.insert(newPost)
+        .then(postId => {
+            const { id } = postId;
+            db.findById(id).then(post => {
+                if (!post) {
+                    return res.status(422).send({Error: `Post does not exist by that id ${id}`});
+                }
+                res.status(201).json(post);
+            })
+        })
+        .catch(err => {
+            res.send(err);
+        });
+});
 
 server.get('/', (req,res) => {
     res.send('<h1>Michael Hacker - Node Express Lab Assignment</h1>');
@@ -34,6 +53,10 @@ server.get('/api/posts/:id', (req, res) => {
     })
     
 })
+
+
+
+
 
 const port = 8000;
 server.listen(port, () => console.log(`API running on port ${port}`));
