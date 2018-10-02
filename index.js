03e0fbd82c9b3c6aea145b89836ea744c7267c9d
Kevin Sooter <kevin.m.sooter@gmail.com>
2018-10-02 15:55:07 -0500

    Day 2 completed.  Implemented Delete, Put, Post

--- index.js
+++ index.js
@@ -9,14 +9,103 @@ const db = require('./data/db.js');
 const server = express();
 
 server.use(cors());
+server.use(express.json());
+
+server.post('/api/posts', (req, res) => {
+  const { title, contents } = req.body;
+  const newPost = { title, contents };
+  db.insert(newPost)
+    .then(newPost => {
+      if (!newPost) {
+        return res.status(400).json({
+          errorMessage: 'Please provide title and contents for the post.'
+        });
+      }
+      res.status(201).json({ 'Post created': newPost });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: 'There was an error while saving the post to the database'
+      });
+    });
+});
 
 server.get('/api/posts', (req, res) => {
   db.find()
     .then(posts => {
-      console.log('\n** posts **', posts);
-      res.json(posts);
+      res.status(200).json(posts);
     })
-    .catch(err => res.send(err));
+    .catch(err => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ error: 'The posts information could not be retrieved.' });
+    });
+});
+
+server.get('/api/posts/:id', (req, res) => {
+  const { id } = req.params;
+  db.findById(id)
+    .then(posts => {
+      if (!posts) {
+        return res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist.' });
+      }
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ error: 'The post information could not be retrieved.' });
+    });
+});
+
+server.delete('/api/posts/:id', (req, res) => {
+  const { id } = req.params;
+  db.remove(id)
+    .then(removedPost => {
+      if (!id) {
+        return res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist.' });
+      }
+      res.status(200).json(removedPost);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: 'The post could not be removed' });
+    });
+});
+
+server.put('/api/posts/:id', (req, res) => {
+  const { id } = req.params;
+  const { title, contents } = req.body;
+  const updatedPost = { title, contents };
+  db.update(id, updatedPost)
+    .then(updatedPost => {
+      if (!id) {
+        return res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist.' });
+      }
+      if (!updatedPost) {
+        return res
+          .status(400)
+          .json({
+            errorMessage: 'Please provide title and contents for the post.'
+          });
+      }
+      res.status(200).json(updatedPost);
+    })
+    .catch(err => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ error: 'The post information could not be modified.' });
+    });
 });
 
 const port = 8000;
