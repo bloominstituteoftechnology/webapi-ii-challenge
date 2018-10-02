84cb851295bfd2742edcbae496a6d92eb98bbab9
Joseph Bradley <josephbradley@Josephs-MacBook-Air.local>
2018-10-02 06:09:23 -0700

    configuring GET

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 // import your node modules
 
+const express = require('express');
+
+const server = express();
+
 const db = require('./data/db.js');
 
 // add your server code starting here
+
+server.get('/', (req, res) => {
+    res.send('Is this working?')
+})
+
+const port = 8000
+server.listen(port, () => console.log(`\n=== API is running on port ${port} ===\n`));
