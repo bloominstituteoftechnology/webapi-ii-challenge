8a31e83e7f78e2993732dfe4b34ff4284cca666b
Joannah Laluces <joannahlaluces@Joannahs-MacBook-Air.local>
2018-10-02 11:54:33 -0700

    Finished add post functionality

--- index.js
+++ index.js
@@ -8,22 +8,43 @@ const cors = require('cors');
 
 const server = express();
 
+server.use(express.json());
+
 const port = 7500;
 
 server.listen(port, () =>
   console.log(`Testing port ${port}`)
 );
 
-server.get('/', (req, res) => {
+server.get('/', (req, res) => {  
   res.send('<h3>I am ROOT</h3>')
 });
 
-server.get('/api/posts', (req, res) => {
+server.get('/api/posts', (req, res) => {  
   db.find()
     .then(posts => {
       res.json(posts)
     })
     .catch(err => {
-      res.json({ message: "The post with the specified ID does not exist." })
+      res.status(500).json({ message: "The post with the specified ID does not exist." })
     })
-})
+});
+
+
+server.post('/api/posts', (req, res) => {
+  const { title, contents } = req.body;
+  const newPost = { title, contents };
+
+  db.insert(newPost)
+    .then(postId => {
+      const { id } = postId;
+      db.findById(id).then(post => {
+        if (!title || !contents) {
+          return res.status(400)
+            .send({ errorMessage: "Please provide title and contents for the post." });
+        }
+        res.status(201).json(post)
+      })
+    })
+    .catch(err => console.log(err));
+});
