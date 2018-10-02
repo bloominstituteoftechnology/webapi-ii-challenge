1374b7c8fa96fb114bc557f8a946e1064219393f
decagondev <decagondevelopment@gmail.com>
2018-10-02 20:48:29 +0100

    (STRETCH): add PostList component and Refactor App component + install react-modal and react-router-dom

--- client/src/components/App/index.jsx
+++ client/src/components/App/index.jsx
@@ -1,42 +1,138 @@
+// import React, { Component } from "react";
+// import axios from "axios";
+
+// import Post from "../Post";
+
+// import "./index.css";
+
+// class App extends Component {
+//   // state to hold posts
+//   state = {
+//     posts: []
+//   };
+
+//   // get all posts
+//   componentDidMount() {
+//     axios
+//       .get("http://localhost:8000/api/posts")
+//       .then(response => {
+//         console.log(response);
+//         this.setState({ posts: response.data });
+//       })
+//       .catch(error => console.log(error));
+//   }
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <h1 className="App-title">Node Express Labs</h1>
+//         </header>
+//         <div />
+//         <div className="main">
+//           {this.state.posts.map(post => {
+//             return <Post title={post.title} contents={post.contents} />;
+//           })}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
 import React, { Component } from "react";
+import "../components.css";
 import axios from "axios";
-
+import { Route, Switch, withRouter } from "react-router-dom";
+import Form from "../Form";
 import Post from "../Post";
-
-import "./index.css";
-
+import SideBar from "../SideBar";
+import PostList from "../PostList";
 class App extends Component {
-  // state to hold posts
   state = {
-    posts: []
+    posts: [],
+    title: "",
+    contents: ""
   };
 
-  // get all posts
+  // component did mount and refecth the Posts from the api
   componentDidMount() {
+    this.refetchPosts();
+  }
+
+  // refetch Posts
+  refetchPosts = () => {
     axios
-      .get("http://localhost:8000/api/posts")
+      .get(`http://localhost:8000/api/posts`)
       .then(response => {
-        console.log(response);
-        this.setState({ posts: response.data });
+        this.setState({ Posts: response.data });
       })
       .catch(error => console.log(error));
-  }
+  };
+
+  // handle input change
+  handleInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  // handle form submit
+  handleFormSubmit = e => {
+    e.preventDefault();
+
+    const newPost = {
+      title: this.state.title,
+      contents: this.state.contents
+    };
+
+    axios
+      .post(`http://localhost:8000/api/posts`, newPost)
+      .then(response => {
+        this.refetchPosts();
+        this.setState({
+          title: "",
+          contents: ""
+        });
+      })
+      .catch(error => console.log(error));
+
+    this.props.history.push("/");
+  };
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Node Express Labs</h1>
-        </header>
-        <div />
-        <div className="main">
-          {this.state.posts.map(post => {
-            return <Post title={post.title} contents={post.contents} />;
-          })}
-        </div>
+      <div className="container">
+        <Sidebar />
+
+        <Route
+          exact
+          path="/"
+          render={props => <PostList Posts={this.state.Posts} />}
+        />
+
+        <Switch>
+          <Route
+            path="/posts/add"
+            render={props => (
+              <Form
+                type={"new"}
+                title={this.state.title}
+                contents={this.state.contents}
+                handleFormSubmit={this.handleFormSubmit}
+                handleInputChange={this.handleInputChange}
+              />
+            )}
+          />
+
+          <Route
+            path="/posts/:id"
+            render={props => (
+              <Post {...props} refetchPosts={this.refetchPosts} />
+            )}
+          />
+        </Switch>
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
---
+++ client/src/components/PostList/index.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "../components.css";
+import "./index.css";
+import { Link } from "react-router-dom";
+
+const PostList = props => {
+  if (!props.posts) {
+    return <div>Posts are loading...</div>;
+  }
+  // if the posts do exist then we can slice them and reverse them so that newest post is shown first
+  const posts = props.posts.slice().reverse();
+
+  return (
+    <div className="main-container">
+      <h2>Your posts:</h2>
+      <div className="post-previews-container">
+        {posts.map(post => {
+          return (
+            <Link to={`/posts/${post._id}`} key={Math.random()}>
+              <div className="post-preview-container">
+                <h3>{post.title}</h3>
+                <p>{post.textBody}</p>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default PostList;
