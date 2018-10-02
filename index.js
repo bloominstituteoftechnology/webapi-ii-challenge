78e74055de43614d407772eb7fefae6c03136ecc
Benny Oseguera <oseguerabenny27@gmail.com>
2018-10-02 15:02:43 -0700

    Day 2 Node - Benny Oseguera

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const server = express();
 
 server.use(cors());
 
+server.use(express.json());
+
 // add your server code starting here
 
 // GET posts
@@ -20,15 +22,45 @@ server.get("/api/posts", (req, res) => {
     .catch(err => res.send(err));
 });
 
-// GET post
-// server.get(`/api/posts/${id}`, (req, res) => {
-//   db.find()
-//     .then(posts => {
-//       console.log(posts);
-//       res.json(posts);
-//     })
-//     .catch(err => res.send(err));
-// });
+// POST
+server.post("/api/posts", (req, res) => {
+  console.log(req.body);
+  const { title, contents } = req.body;
+  const newPost = { title, contents };
+  db.insert(newPost)
+    .then(id => {
+      db.findById(id).then(post => {
+        res.status(201).json(post);
+      });
+    })
+    .catch(err => {
+      console.error(err);
+    });
+});
+
+//DELETE
+server.delete("/api/posts/:id", (req, res) => {
+  const { id } = req.params;
+  db.remove(id)
+    .then(removedPost => {
+      console.log(removedPost);
+      res.status(200).json(removedPost);
+    })
+    .catch(err => console.error(err));
+});
+
+//PUT
+server.put("/api/posts/:id", (req, res) => {
+  const { id } = req.params;
+  const { title, contents } = req.body;
+  const newPost = { title, contents };
+  db.update(id, newPost)
+    .then(post => {
+      console.log(post);
+      res.status(200).json(post);
+    })
+    .catch(err => console.error(err));
+});
 
 const port = 8000;
 server.listen(port, () => console.log(`API running on port ${port}`));
