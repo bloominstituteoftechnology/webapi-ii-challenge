a0639a467ae6fb063c29e7d4f451d9b7ed374926
Michael Agard <mdagard@gmail.com>
2018-10-02 15:23:47 -0400

    add post details

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 
+const PostWrapper = styled.div`
+  max-width: 600px;
+  min-width: 500px;
+  border: 1px solid black;
+`;
 class Post extends Component {
   render() {
-    return <div className="Post">test</div>;
+    return (
+      <PostWrapper className="Post">
+        <h3>{this.props.post.title}</h3>
+        <p>{this.props.post.contents}</p>
+        {this.props.post.created_at}
+        {this.props.post.updated_at}
+      </PostWrapper>
+    );
   }
 }
 
