855e6d1f8aff9f228cc9f0c49e01c98f53d52c32
Brock Maraee Rohloff <00AgentB@gmail.com>
2018-10-02 17:00:53 -0600

    Add temp style.

--- express-lab/src/components/container/Posts.js
+++ express-lab/src/components/container/Posts.js
@@ -10,6 +10,7 @@ class Posts extends Component {
     render() {
         return (
             <section className="posts">
+            <h1 className="title">LOTR Posts</h1>
             { this.props.posts.map(post => <Post key={post.id} post={post}/>) }
             </section>
         )
--- express-lab/src/components/presentational/Post.js
+++ express-lab/src/components/presentational/Post.js
@@ -3,8 +3,9 @@ import React from 'react';
 const Post = props => {
     return (
         <div className="post">
+            <div className="name"><p>{props.post.contents.trim()}<span> Shared a Post</span></p></div>
             <div className="quote"><h3><em>“{props.post.title.trim().replace(/"/g,"'")}”</em></h3></div>
-            <div className="name"><p>- {props.post.contents.trim()}</p></div>
+            
         </div> 
     )
 }
