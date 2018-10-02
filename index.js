edac9e48e706beadc1012decf34386b42cb3d4e7
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-10-02 19:46:12 -0400

    added json objects that needed to be returned

--- index.js
+++ index.js
@@ -47,19 +47,26 @@ server.get('/api/posts', (req, res) =>{
 //get single post from the api
 server.get('/api/posts/:id', (req, res) =>{
     const { id } = req.params;
-    db.findById(id)
+    if(!id) {
+        res.status(404).json({message: "The post with the specified ID does not exisit"})
+    }
+     db.findById(id)
         .then(posts => {
             console.log('\n** posts **', posts);
+
             res.json(posts);
         })
         .catch(err => {
             console.log(err);
-            res.json({ error: "Uhh we can't seem to find your post..."});
+            res.json({ error: "The post information cannot be retrieved"});
         });
 });
 
 server.post('/api/posts', (req, res) =>{
     const { title, contents } = req.body;
+    if(!title || !contents) {
+        res.status(400).json({errorMessage: "Please provide title and contents for the post."})
+    }
     const newPost = { title, contents };
     db.insert(newPost)
     .then(postId => {
@@ -74,30 +81,44 @@ server.post('/api/posts', (req, res) =>{
             res.status(201).json(post);
         });
     })
-    .catch(err => console.error(err))
-})
+    .catch(err => {
+        console.error(err);
+        res.json({ error: "There was an error saving the post to the database" });
+    });
+});
 
 server.delete('/api/posts/:id', (req, res) => {
     console.log(req.params);
     const { id } = req.params;
+    if(!id) {
+        res.status(404).json({message: "The post with the specified ID does not exist"});
+    }
     db.remove(id)
     .then(removedPost =>{
         console.log(removedPost);
         res.status(200).json(removedPost);
     })
-    .catch(err => console.error(err));
+    .catch(() => res.status(500).json({error: "The post could not be removed."}));
 });
 
 server.put('/api/posts/:id', (req, res) =>{
     const { id } = req.params;
+    if(!id) {
+        res.status(404).json({message: "The post with the specified ID does not exist"});
+    }
     const { title, contents } = req.body;
+    if(!title || !contents) {
+        res.status(400).json({errorMessage: "Please provide title and contents for the post."})
+    }
     const newPost = { title, contents};
     db.update(id, newPost)
         .then(post => {
             console.log(post);
             res.status(200).json(post);
         })
-        .catch(err => console.error(err));
+        .catch(() => res.status(500).json({error: "The post information could not be modified."}));
+            
+
 });
 
 // watch for traffic in a particular computer port
