283bf5aa2f773b21c0a979a9c44c458c06832dfa
Brock Maraee Rohloff <00AgentB@gmail.com>
2018-10-02 15:27:14 -0600

    Implement Put and Delete endpoints.

--- index.js
+++ index.js
@@ -1,61 +1,135 @@
-// import your node modules
+// Node Modules
 const express =  require('express');
 const cors = require('cors');
-
 const db = require('./data/db.js');
 
-// add your server code starting here
+// Instatiate Server
 
 const server = express();
 server.use(cors());
+server.use(express.json());
 
-const port = 7777;
-server.listen(port, console.log(`=-=-= Server Active On Port ${port} =-=-=`));
 
+// ##### Error Messages #####
+const missingDataError = { errorMessage: "Please provide title and contents for the post." }
+const invalidTypeError = { errorMessage: "Post title and contents must be a string." }
+const postNotFound = { errorMessage: "The post with the specified ID does not exist." }
+const single500 = { errorMessage: "The post information could not be retrieved" }
+
+// ##### Root directory server READ #####
 server.get('/', (request, response) => {
-    response.send(`<h1>Welcome to the LOTR Post Server!</h1>`);
+    response.status(200).send(`<h1>Welcome to the LOTR Post Server!</h1>`);
 });
 
+// ##### READ All Posts #####
 server.get('/api/posts', (request, response) => {
-    const error500 = { message: "The posts information could not be retrieved" };
+
+    // Unique Error Messages
+    const error500 = { errorMessage: "The posts information could not be retrieved" };
+
+    // Database Promise Method
     db.find()
-    .then(posts => response.json(posts))
+    .then(posts => response.status(200).send(posts))
     .catch(
         error => {
             response.status(500).json(error500)
         })
 });
 
+// ##### READ Individual Post
 server.get('/api/posts/:id', (request, response) => {
-    const error404 = { message: "The post with the specified ID does not exist." };
+
     db.findById(request.params.id)
     .then(post => 
         {   if (post.length > 0) {
-            response.json(post)}
-            else response.status(404).json(error404);
+            response.status(200).send(post)}
+            else response.status(404).send(postNotFound);
         })
-    .catch(error => response.send(error))
+    .catch(error => response.status(500).send(single500))
 });
 
+// ##### CREATE New Post #####
 server.post('/api/posts', (request, response) => {
-    if(request.body.title && request.body.contents) {
-    db.insert({title: request.body.title, contents: request.body.contents})
-    .then(postId => response.json(postId))
-    .catch(error => response.status(500).send(error))    
-}});
 
-// server.put('/api/posts/:id', (request, response) => {
-//     db.update(request.params.id, request.body)
-//     .then(updated => {
-//         if(updated) {
-//             response.send(updated)
-//         }
-//     })
-//     .catch(error => response.send(error))
-// });
+    // Destructuring Request Body For Validation
+    const {title, contents} = request.body;
 
-// server.delete('/api/post/:id', (request, response) => {
-//     db.remove(request.params.id)
-//     .then()
-//     .catch()
-// });
+    // Unique Error Messages 
+    const errorSavingPost = { errorMessage: "There was an error while saving the post to the database." }
+    
+    // Error Handling Conditionals
+    if(!title && !contents) {
+        response.status(400).send(missingDataError);
+    }
+
+    if (!typeof title === typeof contents === 'string') {
+        response.status(400).send(invalidTypeError);
+    }
+
+    // Database Promise Method
+    db.insert({title, contents})
+    .then(postId => response.status(201).send(postId))
+    .catch(() => response.status(500).send(errorSavingPost))    
+});
+
+// ##### UPDATE Existing Post #####
+server.put('/api/posts/:id', (request, response) => {
+
+    // Destructuring Request Body For Validation
+    const {title, contents} = request.body;
+
+     // Unique Error Messages
+     const error500 = { errorMessage: "The post information could not be modified." };
+
+    // Error Handling Conditionals
+    if(!title && !contents) {
+        response.status(400).send(missingDataError);
+    }
+
+    if (!typeof title === typeof contents === 'string') {
+        response.status(400).send(invalidTypeError);
+    }
+
+    // Database Promise Methods
+    db.update(request.params.id, {title, contents})
+    .then(updated => {
+        if (updated) {
+            db.findById(request.params.id)
+            .then(post => 
+                {   if (post.length > 0) {
+                    response.status(200).send(post) 
+                } else { 
+                    response.status(404).send(postNotFound);
+                }})
+            .catch(() => response.status(500).send(single500))
+        } else { 
+            response.status(404).send(postNotFound);
+    }})
+    .catch(() => response.status(500).send(error500))
+});
+
+// ##### DELETE Existing Post #####
+server.delete('/api/posts/:id', (request, response) => {
+    let postDeleted = {};
+    // Unique Error Messages
+    const error500 = { errorMessage: "The post could not be removed" };
+
+    db.findById(request.params.id)
+    .then(post => 
+        {if (post.length > 0) {
+            db.remove(request.params.id)
+            .then( deleted => {
+                if(!deleted) {
+                    response.status(404).send("squiggly");
+                }
+                response.status(200).send(post) 
+            })
+            .catch(() => response.status(500).send(error500))
+        } else { 
+            response.status(404).send(postNotFound);
+        }})
+    .catch(() => response.status(500).send(single500))
+});
+
+const port = 7777;
+server.listen(port, console.log(`=-=-= Server Active On Port ${port} =-=-=`));
