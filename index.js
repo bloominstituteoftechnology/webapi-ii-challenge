cb1a601e1fd5f74e53bb196cc399d55510c47e87
Lndubose <lndubose13@gmail.com>
2018-10-02 16:57:32 -0700

    Now all the errors in PUT are working

--- index.js
+++ index.js
@@ -65,26 +65,23 @@ server.delete('/api/posts/:id', (req, res) => {
 
 server.put('/api/posts/:id', (req, res) => {
   const { id } = req.params;
+  console.log(id);
   const { title, contents } = req.body;
   const newUser = { title, contents };
-  if(title !== '' && contents !== '') {
+  if(title !== undefined && contents !== undefined) {
     db.update(id, newUser)
       .then(user => {
         db.findById(id) 
         .then(userId => {
           console.log(userId)
-          if(userId !== []) {
+          if(userId.length !== 0) {
             res.status(200).json(userId);
           } else {
             res.status(404).json({ message: "The post with the specified ID does not exist." })
           }
         })
-      .catch(err => res.status(404).json('error'))
-      if(user === 0) {
-        res.status(500).json({ error: "The post information could not be modified." })
-      }
-
-      })
+      .catch(err => res.status(500).json({ error: "The post information could not be modified." }))
+    })
   } else {
     res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
   }
