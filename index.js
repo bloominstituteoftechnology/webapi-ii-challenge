11bfaafe6c47975efb2a4d92b356839fcfdcb180
travis-russell <35874527+travis-russell@users.noreply.github.com>
2018-10-03 08:59:46 -0400

    .

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 // import your node modules
-
+const cors = require('cors');
 const db = require('./data/db.js');
+const express = require('express');
+const server = express();
 
+const port = 4140;
+server.listen(port, () => {
+    console.log(`Port #${port}`)
+})
+
+server.use(cors())
 // add your server code starting here
+server.get('/api/posts', (req, res) => {
+    console.log(res.data);
+})
