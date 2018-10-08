e44b37c7567f53974228453f4ceef8e2a92274fe
Brandon White <bwhite06@stac.edu>
2018-10-08 13:46:13 -0400

    Intial Commit
    -working on getting server running

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // import your node modules
-
+const express =require('express');
 const db = require('./data/db.js');
-
+const server = express()
+const port = 3005;
 // add your server code starting here
