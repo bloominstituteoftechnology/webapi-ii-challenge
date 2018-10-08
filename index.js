0983014f5d00095543d7551cbf8de8d11d1d7565
Brandon White <bwhite06@stac.edu>
2018-10-08 14:11:06 -0400

    "Get" Request Done
    -working on other requests

--- index.js
+++ index.js
@@ -3,4 +3,29 @@ const express =require('express');
 const db = require('./data/db.js');
 const server = express()
 const port = 3005;
+const cors = require('cors');
 // add your server code starting here
+server.use(cors());
+
+
+server.get('/api/posts', (req,res)=>{
+  db.find()
+  .then(posts=>{
+    console.log('success',posts);
+    res.json(posts)
+  })
+  .catch(err => res.send(err))
+})
+
+
+
+
+
+
+
+
+
+
+server.listen(port,()=>{
+  console.log(`\n=== Api Running On ${port} ===\n` )
+})
