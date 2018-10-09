79f0ca8c1fe478331cc591a56cf7827c23976d7a
verydecent <wnjhwng@gmail.com>
2018-10-08 22:30:35 -0700

    Created Express server, running Nodemon

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+// import express from 'express'; // ES2015 module > export default
+
+const express = require('express'); //  commonJS modulex > module,exports = somwCode
+
+const server = express();
+
+// watch for traffic in particular port
+
+// http://localhost:3000 > 3000 is the port, a place where we can test and run traffic apps
+ const port = 9000;
+ server.listen(port, () => console.log(` === A11PI running on port ${port} === `))
+
 // import your node modules
 
 const db = require('./data/db.js');
