fd48f6076c53f49cba6b29ceb21cd14b2807978c
IlanL75 <anatoly.leytman@gmail.com>
2018-10-09 12:42:15 -0700

    Adding GET by id and post

--- index.js
+++ index.js
@@ -6,21 +6,48 @@ const db = require('./data/db.js');
 
 // add your server code starting here
 const server = express();
-
+server.use(express.json());
 server.use(cors());
 
 server.get('/', (req, res) => {
     res.send('Good Morning :)')
 })
 
-server.get('/api/users', (req, res) => {
+server.get('/api/posts', (req, res) => {
     db.find()
-        .then(users => {
-            console.log('\n** users **', users);
-            res.json(users);
+        .then(posts => {
+            console.log('\n** posts **', posts);
+            res.status(200).json(posts);
         })
-        .catch(err => res.send(err));
+        .catch(err => res.status(500).json({ error: 'The posts information could not be retrieved.' }));
 });
+
+server.get('/api/posts/:id', (req, res) => {
+    db.findById(req.params.id)
+        .then(post => {
+        if (post.length === 0) {return res.status(404).json({message: "The post with the specified ID does not exist."});
+        } else
+        console.log(post);
+        res.status(200).json(post);
+        })
+        .catch(err => res.status(500).json({error: 'The post information could not be retrieved.'}));
+});
+
+server.post('/api/posts', (req, res) => {
+    const { title, contents } = req.body;
+    db.insert({ title, contents })
+      .then(postID => {
+        db.findById(postID).then(post => {
+          if (!post) {
+            return res.status(400).send({ errorMessage: `Please provide title and contents for the user.` });
+          }
+          res.status(201).json(post);
+        });
+    })
+      .catch(() => res.status(500).json({ error: 'There was an error while saving the post to the database.' }));
+});
+
+
 
 const port = 5000;
     server.listen(port, () => console.log(`\n*** Server listenning on port ${port} ***\n`)
