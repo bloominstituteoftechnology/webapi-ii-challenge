5e67f9841b96d73d51a5d8392737d8a6d3477247
Sasha <taylork85@gmail.com>
2018-10-08 22:44:09 -0500

    Corrected endpoint to api/posts & created React app

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 // import your node modules
 const express = require('express');
+const cors = require('cors');
 const db = require('./data/db.js');
 
 // add your server code starting here
 const server = express();
 const port = 7000;
 
+server.use(cors());
 server.listen(port, () => {console.log(`Server running on port ${port}`)});
 
-server.get('/api/users', (req, res) => {
+server.get('/api/posts', (req, res) => {
     db.find()
-    .then(users => {
-        console.log('users: ', users);
-        res.json(users);
+    .then(posts => {
+        console.log('posts: ', posts);
+        res.status(200).json(posts);
     })
     .catch(err => res.send(err));
 });
+
+server.get('/api/posts/:id', (req, res) => {
+    console.log(req);
+    db.findById(id);
+    //.then(posts )
+})
+
