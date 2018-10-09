2eb0cdf2381e0917a9a313bf7ebb5fef0e9bbca2
lyang9 <lyang9@buffalo.edu>
2018-10-09 18:33:26 -0400

    Imported PostForm and rendered NavLink and Route

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import axios from 'axios';
 import { Route, NavLink } from 'react-router-dom';
 import Posts from './Components/Posts';
+import PostForm from './Components/PostForm';
 
 class App extends Component {
   constructor(props) {
@@ -28,11 +29,18 @@ class App extends Component {
           <div>
             <NavLink exact to='/'>Posts</NavLink>
           </div>
+          <div>
+            <NavLink to='/post-form'>Add New Post</NavLink>
+          </div>
         </nav>
         <Route
           exact
           path='/'
           render={props => <Posts {...props} posts={this.state.posts} />}
+        />
+        <Route
+          path='/post-form'
+          render={props => <PostForm {...props} addPost={this.addPost} />}
         />
       </div>
     );
