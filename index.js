455fd69f797b42fb27817b298ab7858e9086728f
Sasha <taylork85@gmail.com>
2018-10-09 18:10:31 -0500

    Added post, put, and delete request

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const server = express();
 const port = 7000;
 
 server.use(cors());
+server.use(express.json());
+
 server.listen(port, () => {console.log(`Server running on port ${port}`)});
+
 
 server.get('/api/posts', (req, res) => {
     db.find()
@@ -23,6 +26,30 @@ server.get('/api/posts/:id', (req, res) => {
     const id = req.params.id;
     db.findById(id)
     .then(post => {res.status(200).json(post)
+    })
+    .catch(err => res.send(err));
+})
+
+server.post('/api/posts', (req, res) => {
+    const newPost = req.body;
+    db.insert(newPost)
+    res.status(201).json(newPost)
+    .catch(err => res.send(err));
+})
+
+server.delete('/api/posts/:id', (req, res) => {
+      const id = req.params.id;
+      db.remove(id)
+      res.json(id)
+      .catch(err => res.send(err));
+    })
+
+server.put('/api/posts/:id', (req, res) => {
+    const id = req.params.id;
+    const newPost = req.body;
+    db.update(id, newPost)
+    .then(post => {
+        res.status(200).json(post)
     })
     .catch(err => res.send(err));
 })
