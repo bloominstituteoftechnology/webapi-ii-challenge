278bf6bd65eaa802093a71c7f83680efa22afc3b
Ray Lomeli <raylom96@gmail.com>
2018-10-08 19:20:56 -0500

    imported node modules

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // import your node modules
-
-const db = require('./data/db.js');
+const express = require("express");
+const db = require("./data/db.js");
+const bodyParser = require;
+("body-parser");
+const cors = require("cors");
+const server = express();
 
 // add your server code starting here
