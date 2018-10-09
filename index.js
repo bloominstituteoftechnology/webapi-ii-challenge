71828b32b3a2a868c689c3b287606d65512bfa6c
verydecent <wnjhwng@gmail.com>
2018-10-09 10:30:16 -0700

    API database on local host

--- index.js
+++ index.js
@@ -2,16 +2,28 @@
 
 const express = require('express'); //  commonJS modulex > module,exports = somwCode
 
-const server = express();
+const db = require('./data/db.js');
+
+const server = express();   
+
+server.get('/', (req, res) => {
+    res.send('<h1>Wonjae Hwang</h1>')
+})
+
+server.get('/api/users', (req, res) => { // request handler, route handler, listener... names
+    db.find()
+        .then(users => {
+        console.log('\n** users **', users)
+        res.json(users);
+        })
+        .catch(err => res.send(err))
+})
 
 // watch for traffic in particular port
-
 // http://localhost:3000 > 3000 is the port, a place where we can test and run traffic apps
  const port = 9000;
- server.listen(port, () => console.log(` === A11PI running on port ${port} === `))
+ server.listen(port, () => console.log(` === API running on port ${port} === `))
 
 // import your node modules
-
-const db = require('./data/db.js');
 
 // add your server code starting here
