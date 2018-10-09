4a55cb63b02a7fb91ea5f6474bbd5b8a511e286d
Marious Davis <mlancelot88@gmail.com>
2018-10-09 08:41:24 -0500

    added server.post, moved port, & changed res.send to res.json

--- index.js
+++ index.js
@@ -6,24 +6,39 @@ const db = require("./data/db.js");
 
 // add your server code starting here
 const server = express();
-const port = 3333;
+
 server.use(cors());
-server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+
 server.get("/api/posts", (req, res) => {
   db.find()
     .then(posts => {
       console.log("posts: ", posts);
       res.json(posts);
     })
-    .catch(e => res.send(e));
+    .catch(e => res.json(e));
 });
+
 server.get("api/posts/:id", (req, res) => {
   const id = req.params.id;
   db.findById(id)
     .then(post => {
       res.json(post);
     })
-    .catch(e => res.send(e));
+    .catch(e => res.json(e));
+});
+
+server.post("/api/posts", (req, res) => {
+  const { title, contents } = req.body;
+  db.insert({ title, contents })
+    .then(response => {
+      res.json(response);
+    })
+    .catch(error => {
+      res.json(error);
+    });
+});
+
+const port = 3333;
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
