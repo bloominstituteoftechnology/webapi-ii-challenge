637712eef00f1561c93c244b57ab44c60208df3d
Jordan Marsaw <37964165+blokboy@users.noreply.github.com>
2018-10-09 02:10:30 -0500

    Imported and Set up Express

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 // import your node modules
+const express = require('express');
+const cors = require('cors');
 
 const db = require('./data/db.js');
 
+
 // add your server code starting here
+const server = express();
+const port = 3333;
+
+server.use(cors());
+server.listen(port, () => { console.log(`Server running on port ${port}`)});
+
+server.get('/api/posts', (req, res) => {
+  db.find()
+  .then(posts => {
+    console.log('posts: ', posts);
+    res.json(posts);
+  })
+  .catch(e => res.send(e));
+})
+
+server.get('api/posts/:id', (req, res) => {
+  const id = req.params.id;
+  db.findById(id)
+  .then(post => { res.json(post) })
+  .catch(e => res.send(e))
+  })
