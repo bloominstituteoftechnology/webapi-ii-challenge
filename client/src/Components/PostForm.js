363583c342c14a186e552f72478bc952b8a324eb
lyang9 <lyang9@buffalo.edu>
2018-10-09 18:20:08 -0400

    Created PostForm.js and called constructor

---
+++ client/src/Components/PostForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PostForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      contents: ''
+    };
+  }
+}
