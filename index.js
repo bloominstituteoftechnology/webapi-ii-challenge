3a36513dcb4993c93e90890bff61799f817ceacf
Brandon White <bwhite06@stac.edu>
2018-10-09 14:12:54 -0400

    ADDed put to api
    -api is fully functioning in postman
    -working on front end

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const server = express()
 const port = 3005;
 const cors = require('cors');
 // add your server code starting here
-server.use(cors());
+server.use(cors());// connect to react
+server.use(express.json());//use json data
 
-
+//Get requests
 server.get('/api/posts', (req,res)=>{
   db.find()
   .then(posts=>{
@@ -24,17 +25,46 @@ server.get('/api/posts', (req,res)=>{
      res.status(200).json(post);
    })
    .catch(err => res.send(err))
- })
+ });
+ //delete request
+server.delete('/api/posts/:id',(req,res)=> {
+  const {id} = req.params;
+  db.remove(id)
+  .then(removedPost=>{
+    res.json(removedPost);
+   console.log(removedPost)
+  })
+});
 
- 
+//Post request
+ server.post(`/api/posts`, (req,res) =>{
+   const {title, content} = req.body;
+   const newPost = {title, content}
+   console.log(newPost);
+   res.send('Success');
+   db.insert(newPost)
+   .then(post =>{
+     console.log("success", post);
 
+   })
+   .catch(err => res.send(err))
+ });
+ //Put request
+  server.put(`/api/posts/:id`,(req,res)=>{
+     const {title, content} = req.body;
+     const newPost = {title, content};
+     const {id} = req.params;
+     res.send('Success');
+     db.update(id,newPost)
+     .then(post => {
+       console.log('sucess',post)
+     })
+     .catch(err=>{
 
+     })
+  })
 
-
-
-
-
-
+//listening on 3005
 server.listen(port,()=>{
   console.log(`\n=== Api Running On ${port} ===\n` )
 })
