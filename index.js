89ff16031ede11798eecd6e417f571cd881a9c6b
MichelleOk <umbralcompendium@gmail.com>
2018-10-09 17:10:57 -0600

    get, getId, post, and delete, working, need to fix update

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
 const express = require('express');
 const db = require('./data/db.js');
 const server = express();
+server.use(express.json());
 const cors = require('cors');
 
 server.use(cors());
@@ -19,23 +20,26 @@ server.get('/', (req, res) => {
 
 // creates a post
 server.post('/api/posts', (req, res) => {
-    db.find().then(posts => {
         const { title, contents } = req.body;
-        const newPost = { title, content, id: postId };
-        if(!title || !contents) {
-            return res.status(400)}
-            res.json({ errorMessage: "Please provide title and contents for the post." });
-        if(title || content) {
-            return res.status(201)}
-            posts.push(newPost);
-            postId++;
-            res.json({posts});
-        if(!newPost) {
-            return res.status(500)}
-            res.json({  error: "There was an error while saving the post to the database"  });
+        const newPost = { title, contents };
+        db.insert(newPost)
+        .then(postId => {
+            const { id } = postId;
+            db.findById(id).then(post => {
+                console.log(post);
+                    res.status(201).send(post);
+            });
         })
-        .catch(err => res.send(err));
-    })
+        .catch(error => {
+            if(!title || !contents) {
+                return res.status(400).send({ errorMessage: "Please provide title and contents for the post." });
+            } else if (!post) {
+                return res.status(422).send({ Error: `There is no post with id ${postId}`});
+            } else {
+                 res.status(500).send({  error: "There was an error while saving the post to the database"  });
+            }
+        })
+    });
     
 
 // getting an array of all post objs 
@@ -53,52 +57,46 @@ server.post('/api/posts', (req, res) => {
 //getting post by id
 server.get('/api/posts/:id', (req, res) => {
     const { id } = req.params;
-    const{ title, contents } = req.body;
-    const findPostById = post => {
-        return post.id == id;
-    };
-    const foundPost = posts.find(findPostById);
-        if(!foundPost) {
-        return res.status(404), 
-        res.json({ message: "The post with the specified ID does not exist."})
-        } else {
-        if (title) foundPost.title = title;
-        if (contents) foundPost.contents = contents;
-            res.json(posts);
-        }})
+    db.findById(id).then(post => {
+        if(post.length === 0) {
+            return res.status(404).send({ message: "The post with the specified ID does not exist." });
+        }
+        res.status(200).json(post);
+    })
+    .catch(error => res.status(500).send({error: "The posts information could not be retrieved." }));
+});
         
 //delete post by id
 server.delete('/api/posts/:id', (req, res) => {
     const { id } = req.params;
-    const foundPost = posts.find(post => post.id == id);
-
-    if (foundPost) {
-        const PostRemove = { ...foundPost };
-        posts = pots.filter(post => post.id != id);
-        res.status(404).json(posts);
-    } else {
-        res.json({message: "The post with the specified ID does not exist."});
-    }
-    });
+   db.remove(id).then(deletedPost => {
+       if(!deletedPost) {
+           return res.status(404).send({ Error: "The post with the specified ID does not exist." });
+       } else {
+           res.status(200).json({ message: "You successfully deleted the post." });
+       }
+   })
+   .catch(error => res.status(500).send({ error: "The post failed to delete." }));
+});
 
 // //updates post by id
-server.put('/api/posts/:id', (req, res) => {
-    const { id } = req.params;
-    const { title, contents } = req.body;
-    const findPostById = post => {
-        return post.id == id;
-    };
-    const foundPost = posts.find(findPostById);
-    if (!foundPost) {
-        return( res.status(404), res.json({ message: "The post with the specified ID does not exist." }))
-    } if (!title || !contents) {
-        return (res.status(400), res.json({ errorMessage: "Please provide title and contents for the post." }))
-    } if (error) {
-        return (res.status(400), res.json({ error: "The post information could not be modified." }))
-    } else {
-        return (res.status(200), res.json(postId))
-    };
-});
+// server.put('/api/posts/:id', (req, res) => {
+//     const { id } = req.params;
+//     const { title, contents } = req.body;
+//     const findPostById = post => {
+//         return post.id == id;
+//     };
+//     const foundPost = posts.find(findPostById);
+//     if (!foundPost) {
+//         return( res.status(404), res.json({ message: "The post with the specified ID does not exist." }))
+//     } if (!title || !contents) {
+//         return (res.status(400), res.json({ errorMessage: "Please provide title and contents for the post." }))
+//     } if (error) {
+//         return (res.status(400), res.json({ error: "The post information could not be modified." }))
+//     } else {
+//         return (res.status(200), res.json(postId))
+//     };
+// });
 
 const port = 9000;
 server.listen(port, () => console.log(`API running tigerbalm on port ${port}`));
