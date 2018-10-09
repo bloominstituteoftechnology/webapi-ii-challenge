c13b85512ef00d2e18133a0030404ab454fc48ae
Ray Lomeli <raylom96@gmail.com>
2018-10-09 08:20:24 -0500

    created GET endpoint for all post objects in database

--- index.js
+++ index.js
@@ -41,4 +41,61 @@ server.get("/form", (req, res) => {
   );
 });
 
+// endpoints
+// will be using async and await with try catch blocks
+// ref async, await : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
+// ref try, catch: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/try...catch
+
+// When the client makes a POST request to /api/posts:
+
+// If the request body is missing the title or contents property:
+
+// cancel the request.
+// respond with HTTP status code 400 (Bad Request).
+// return the following JSON response: { errorMessage: "Please provide title and contents for the post." }.
+// If the information about the post is valid:
+
+// save the new post the the database.
+// return HTTP status code 201 (Created).
+// return the newly created post.
+// If there's an error while saving the post:
+
+// cancel the request.
+// respond with HTTP status code 500 (Server Error).
+// return the following JSON object: { error: "There was an error while saving the post to the database" }.
+server.post("/api/posts", async (req, res) => {
+  if (!req.body.title || !req.body.contents) {
+    return res.status(422).json({ errorMessage: formError });
+  }
+  try {
+    const { id } = await db.insert(req.body);
+    try {
+      const post = await db.findById(id);
+      res.status(201).json(post);
+    } catch (error) {
+      return res.status(404).json({ error: notExist });
+    }
+  } catch (error) {
+    res.status(500).json({ error: saveError });
+  }
+});
+
+/*
+  get all users
+  -------------
+  If there's an error in retrieving the users from the database:
+    cancel the request.
+    respond with HTTP status code 500.
+    return the following JSON object: { error: "The users information could not be retrieved." }. 
+*/
+
+server.get("/api/posts", async (req, res) => {
+  try {
+    const posts = await db.find();
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json({ error: getError });
+  }
+});
+
 server.listen(port, () => console.log(`API listenning on port ${port}`));
