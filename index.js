22dd65219def7171d6266102ca1eec7d2ed40786
Ray Lomeli <raylom96@gmail.com>
2018-10-09 07:44:45 -0500

    routed aboout page

--- index.js
+++ index.js
@@ -29,4 +29,9 @@ server.get("/", (req, res) => {
   res.send("<h1> Home Page </h1>");
 });
 
+//about page route
+server.get("/about", (req, res) => {
+  res.send("<h1>About Page </h1>");
+});
+
 server.listen(port, () => console.log(`API listenning on port ${port}`));
