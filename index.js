0c7140e670388dcfe2d391d06066e368e5ca239d
Emily McClanahan <emily.f.mcclanahan@gmail.com>
2018-10-09 16:13:55 -0500

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 // import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
 
+const server = express();
+
+server.get('/api/posts', (req, res) => {
+    db
+    .find()
+    .then(posts => {
+        res.json(posts);
+    })
+    .catch(err => res.send(err));
+})
+
 // add your server code starting here
+const port = 8000
+server.listen(port, () => console.log(`\n=== API running on port ${port} ===\n`));
