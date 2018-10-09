1da28977e7ea3c0dd8c6a86e86e78f6fb912f759
Sasha <taylork85@gmail.com>
2018-10-08 21:02:51 -0500

    Express imported & Server created

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
 
 // add your server code starting here
+const server = express();
+const port = 7000;
+
+server.listen(port, () => {console.log(`Server running on port ${port}`)});
