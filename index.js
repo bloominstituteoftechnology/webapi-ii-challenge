14abcccecc3ed9d9a1a538fc6988d1a9c0c65e65
Abdulfatah Ahmad <abdulfatah.a.ahmad@gmx.com>
2018-10-10 10:52:53 -0400

    Added Port & ServerListner
    #COMMENT
    
    Added port 6000
    Added API from the server to be run on port 6000

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ const express = require('express');
 
 // add your server code starting here
 
+//Express is the de-facto framework for building node.js servers and REST APIs
+
+
 const server = express(); // Creates a server
+
+const port = 6000;
+server.listen(port, () => console.log( `API is running on port ${port}`));
