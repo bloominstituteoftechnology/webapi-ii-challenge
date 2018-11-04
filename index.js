8ad2f6501fd89674df10a88b52f532b8f01bfeb0
Carlo Clamucha <carlo.clamucha@gmail.com>
2018-11-04 12:15:02 -0600

    finished day 1

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 // import your node modules
+const express = require('express');
+
+const server = express();
 
 const db = require('./data/db.js');
 
 // add your server code starting here
+
+server.get('/users', (req, res) =>
+  db
+    .find()
+    .then(users => res.status(200).json(users))
+    .catch(err => {
+      console.log('Error', err);
+      res.status(500).json({ message: 'Error' });
+    })
+);
+
+server.listen(9000, () =>
+  console.log('\n == server listening on port 9000 ==\n')
+);
