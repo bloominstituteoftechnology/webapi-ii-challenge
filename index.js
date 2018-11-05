963ac50e6b09e94abb7f2200c7ea2796dba913dc
Gabriel Samaniego <selfdev.gs@gmail.com>
2018-11-05 15:21:05 -0800

    get request

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 // import your node modules
-
-const db = require('./data/db.js');
-
+const express = require("express");
+const cors = require("cors");
+const db = require("./data/db.js");
+const server = express();
 // add your server code starting here
+server.use(cors());
+
+server.get("/api/posts", (req, res) => {
+  db.find()
+    .then(posts => {
+      res.json(posts);
+    })
+    .catch(err => res.send(err));
+});
+
+server.listen(4000, () => console.log("Server is listening on port 4000"));
