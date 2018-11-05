75980269c1ceddc7db97a3a3029a8d4273a66d50
Riley-Brown <riley@riley.gg>
2018-11-05 17:18:40 -0500

    Set up get request

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 // import your node modules
+const express = require("express");
 
-const db = require('./data/db.js');
+const db = require("./data/db.js");
 
 // add your server code starting here
+
+const server = express();
+server.get("/", (req, res) => {
+  res.json({ hello: "testing" });
+});
+
+server.get("/api/users", (req, res) => {
+  db.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "failed", error: err });
+    });
+});
+server.listen(9000, () => console.log("the server is alive"));
