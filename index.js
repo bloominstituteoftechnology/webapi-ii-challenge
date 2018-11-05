307cc4d427c495ccddd304e6f05b95ff042d74f5
DaftBeowulf <ajmboosh@gmail.com>
2018-11-05 15:14:19 -0600

    got GET by id wired up, but is not returning correct error message when trying to find invalid id

--- index.js
+++ index.js
@@ -20,4 +20,25 @@ server.get('/api/posts', (req, res) => {
     })
 })
 
-server.listen(5000, ()=>console.log('Server is listening on port 5000'));
+//GET request for specific post
+server.get('/api/posts/:id', (req, res) => {
+  //first define id based on url param
+    const {id} =req.params;
+
+    db.findById(id)
+.then(post => {
+    if (post) {
+     res.status(200)
+     .json(post);   
+    } else {
+        res.status(404)
+        .json({message: "The post with the specified ID does not exist."});
+    }
+})
+.catch(err=>{
+    res.status(500)
+    .json({message: "The post information could not be retrieved."})
+});
+});
+
+server.listen(8000, ()=>console.log('Server is listening on port 8000'));
