7d97db0a411fb6f8b726b72ade480e6da13ae0a9
BrandonVilaychith <blvilaychith@gmail.com>
2018-11-05 13:24:00 -0600

    /api/posts/:id endpoint finished

--- index.js
+++ index.js
@@ -9,12 +9,32 @@ const server = express();
 server.get('/api/posts', (req, res) => {
   db.find()
     .then(posts => {
-      res.json(posts);
+      res.status(200).json(posts);
     })
     .catch(error => {
       res
         .status(500)
         .json({ error: 'The posts information could not be retrieved.' });
+    });
+});
+
+server.get('/api/posts/:id', (req, res) => {
+  const id = req.params.id;
+
+  db.findById(id)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res
+          .status(404)
+          .json({ error: 'The post with the specified ID does not exist.' });
+      }
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ message: 'The post information could not be retrieved.' });
     });
 });
 
