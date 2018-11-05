5ca4f039089017afcf98a80f164d5c4cb27033c7
Steven O'Neill <oneillsteven957@gmail.com>
2018-11-05 16:31:08 -0600

    Day One MVP done
    Got the get request for a post with a specified id

--- index.js
+++ index.js
@@ -13,16 +13,32 @@ server.get('/', (req, res) => {
 
 server.get('/api/posts', (req, res) => {
     db.find()
-      .then(users => {
-        res.status(200).json(users);
+      .then(posts => {
+        res.status(200).json(posts);
       })
       .catch(err => {
         res
           .status(500)
-          .json({ error: "The posts information could not be retrieved.", error: err });
+          .json({ message: "The posts information could not be retrieved.", error: err });
       });
   });
   
+  server.get('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
   
+    db.findById(id)
+      .then(post => {
+        if (post && post.length) {
+          res.status(200).json(post);
+        } else {
+          res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+        }
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ message: "The post information could not be retrieved.", error: err });
+      });
+  });
 
 server.listen(9000, () => console.log('the server is active'));
