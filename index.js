8287fea2f3664f5417bc8d0e10d219452c35ec4a
Brad Mortensen <b.mort86@gmail.com>
2018-11-05 13:46:59 -0700

    building server

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 // import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
 
 // add your server code starting here
+const server = express();
+
+server.get('/api/posts', (req, res) => {
+  res.json('alive');
+});
+
+server.listen(9000, () => console.log('the server is alive!'));
