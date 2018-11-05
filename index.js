85c06636abecdda0a3c6ec899f7a4b9dddd76b5b
Lajawanti <lajawanti.dhake@gmail.com>
2018-11-05 16:19:31 -0500

    GET/api/posts Done

--- index.js
+++ index.js
@@ -4,5 +4,16 @@ const express = require('express');
 const server = express();
 
 // add your server code starting here
+//GET	/api/posts
+//returns an array of all the post objects contained in the database.
+server.get('/api/posts', (request, response) => {
+     db.find()
+       .then(posts => {
+           response.status(200).json(posts);
+        })
+       .catch(error => {
+            response.status(500).jason({error : 'The post could not be retrieved'})
+        })
+})
 
 server.listen(9000, () => console.log('server is live'));
