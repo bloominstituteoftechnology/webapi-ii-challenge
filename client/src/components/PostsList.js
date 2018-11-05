5cead1aeb730a0a06a3dc831c73153dfebf3de7f
Shawn Antonucci <shawnantonucci2018@gmail.com>
2018-11-05 14:41:28 -0800

    add titles

--- client/src/components/PostsList.js
+++ client/src/components/PostsList.js
@@ -4,9 +4,9 @@ import { PostsContainer, Posts, PostContent } from "../Styles";
 const PostsList = (props) => {
     return(
         <PostsContainer>
-            <Posts>{props.post.id}</Posts>
-            <Posts>{props.post.title}</Posts>
-            <PostContent>{props.post.contents}</PostContent>
+            <Posts>Id: {props.post.id}</Posts>
+            <Posts>Title: "{props.post.title}"</Posts>
+            <PostContent>Content: {props.post.contents}</PostContent>
         </PostsContainer>
     );
 }
