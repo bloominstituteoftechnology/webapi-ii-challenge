c0d0e0e01ab691f7b1f62497a6ec235fd03dc9d4
adamhinckley <adamhinckley@mac.com>
2018-11-05 13:37:24 -0700

    day one MVP done

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 // import your node modules
-
-const db = require('./data/db.js');
+const express = require("express");
+const server = express();
+const db = require("./data/db.js");
 
 // add your server code starting here
+server.get("/api/posts", (req, res) => {
+  db.find()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "The posts information could not be retrieved.", error: err });
+    });
+});
+server.get("/api/posts/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(req.params);
+
+  db.findById(id)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(err => {
+      res, status(500).json({ error: "The post information could not be retrieved.", error: err });
+    });
+});
+
+server.listen(8000, () => console.log("server is running"));
