3b0fb262aeab115ac631ef8f7b0db79136310742
ryanboris <ryan.boris@gmail.com>
2018-11-05 14:50:04 -0500

    Creates react/router/redux boilerplate

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './styles/App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <p>HI!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
