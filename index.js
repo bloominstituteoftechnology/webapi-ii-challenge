b0a497b4d1a626e7368d49ef557d6c2b5f586a14
Carlo Clamucha <carlo.clamucha@gmail.com>
2018-11-05 00:59:26 -0600

    added a post and put functionality

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
 const db = require('./data/db.js');
 
 // add your server code starting here
@@ -16,6 +18,38 @@ server.get('/users', (req, res) =>
       res.status(500).json({ message: 'Error' });
     })
 );
+
+server.post('/users', async (req, res) => {
+  const user = req.body;
+  if (user.title && user.contents) {
+    try {
+      const response = await db.insert(user);
+      res.status(201).json(response);
+    } catch {
+      res.status(500).json({ message: 'Error getting data' });
+    }
+  } else {
+    res.status(422).json({ message: 'A needs a title and content' });
+  }
+});
+
+server.put('/users/:id', async (req, res) => {
+  const { id } = req.params;
+  const user = req.body;
+
+  if (user.title && user.contents) {
+    try {
+      const response = await db.update(id, user);
+      res.status(200).json(response);
+    } catch {
+      res.status(404).json({
+        message: 'Needs a title and a content'
+      });
+    }
+  } else {
+    res.status(422).send('cannot be processed');
+  }
+});
 
 server.listen(9000, () =>
   console.log('\n == server listening on port 9000 ==\n')
