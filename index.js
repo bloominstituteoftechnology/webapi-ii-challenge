67f288c4aabc1d96eb4fcc8f1edeb5cbf0d20fe2
KHollobaugh <khollobaugh@gmail.com>
2018-11-05 16:31:22 -0500

    fixed bug where error message wouldn't display

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ server.get('/api/posts', (req, res) => {
         res.json(posts);
     })
     .catch(err => {
-        res.status(500).json( {message: "we failed, can't get posts", error: err });
+        res.status(500).json({ message: "The posts information could not be retrieved." });
     });
 });
 
@@ -29,17 +29,17 @@ server.get('/api/posts/:id', (req, res) => {
     const { id } = req.params;
 
     db.findById(id).then(post => {
-        if(post) {
+        if(post.id) {
            res.status(200).json(post); 
         } else {
-            res.status(404).json({ message: 'post not found' });
+            res.status(404).json({message: "The post with the specified ID does not exist."  });
         }
         
     })
     .catch(err => {
         res
         .status(500)
-        .json({ message: "we can't get the post", error: err })
+        .json({message: "The post information could not be retrieved." })
     });
 });
 
