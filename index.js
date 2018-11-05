8ecdab5deb833932caf5cf943ba877b86f43ba35
frankbenevento <frankbenevento123@gmail.com>
2018-11-05 12:58:14 -0800

    wired up the get by id

--- index.js
+++ index.js
@@ -6,8 +6,16 @@ const db = require('./data/db.js');
 
 
 server.get('/api/posts',(req,res)=>{
-    db.find().then((value)=>{ res.status(200).send(value)})
+    db.find().then((value)=>{ res.status(200).json(value)})
+             .catch(error => { res.status(500).json({ error: "The posts information could not be retrieved." })})   
 })
+
+server.get('/api/posts/:id',(req,res)=>{
+    db.findById(req.params.id)
+      .then((value)=>{ res.status(200).json(value)})
+      .catch(error => { res.status(404).json({ message: "The post with the specified ID does not exist." })})   
+})
+
 
 
 // add your server code starting here
