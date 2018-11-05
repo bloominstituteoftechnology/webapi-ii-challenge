4a7a1747c6552237d4ce23ba72d5c78a4e8d3f10
Brad Mortensen <b.mort86@gmail.com>
2018-11-05 13:58:43 -0700

    successfully set endpoints

--- index.js
+++ index.js
@@ -6,7 +6,33 @@ const db = require('./data/db.js');
 const server = express();
 
 server.get('/api/posts', (req, res) => {
-  res.json('alive');
+  db.find()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The posts information could not be retrieved.", error: err });
+    });
 });
 
-server.listen(9000, () => console.log('the server is alive!'));
+server.get('/api/posts/:id', (req, res) => {
+  const { id } = req.params;
+
+  db.findById(id)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The post information could not be retrieved.", error: err });
+    });
+});
+
+server.listen(9000, () => console.log('\nthe server is alive!\n'));
