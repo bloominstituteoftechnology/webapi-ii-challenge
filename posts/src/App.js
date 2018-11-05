a2345eac97ae850828efbd1c8f6ba8e4b78a9116
Lee Len Formento <leebatef@gmail.com>
2018-11-05 17:02:56 -0500

    Axios install. React test

--- posts/src/App.js
+++ posts/src/App.js
@@ -3,23 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: []
+    }
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1> test </h1>
       </div>
     );
   }
