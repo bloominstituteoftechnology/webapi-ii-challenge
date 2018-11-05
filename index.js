e62fad52b1b27995f38c9e048911d0e2cb041f02
sarahtennis <sarahtennis@Sarahs-Air.fios-router.home>
2018-11-05 15:29:37 -0500

    initial commit, GET request for /api/posts

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 // import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
+const server = express();
 
-// add your server code starting here
+// ***** GET request to /api/posts: *****
+// If there's an error in retrieving the posts from the database:
+// - cancel the request.
+// - respond with HTTP status code 500.
+// - return the following JSON object: { error: "The posts information could not be retrieved." }
+
+server.get('/api/posts', (req, res) => {
+    db.find()
+        .then(posts => {
+            res.json(posts);
+        })
+        .catch(err => {
+            res.status(500).json({ error: "The posts information could not be retrieved." })
+        })
+})
+
+server.listen(9000);
