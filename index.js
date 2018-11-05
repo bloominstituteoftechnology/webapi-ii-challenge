6bd435e3fe16d2fe927114ab08fb42742be359d1
ryanboris <ryan.boris@gmail.com>
2018-11-05 14:36:17 -0500

    Adds GET and GET/ID endpoints

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 // import your node modules
-
-const db = require('./data/db.js');
+const express = require('express')
+const db = require('./data/db.js')
 
 // add your server code starting here
+const server = express()
+
+
+server.get('/api/posts', (req, res) => {
+    db
+        .find()
+        .then(posts => res.status(200).json(posts))
+        .catch(err =>
+            console.log(err, res.status(500).json({ error: 'The posts information could not be retrieved.' }))
+        )
+})
+
+server.get('/api/posts/:id', (req, res) => {
+    db
+        .findById(req.params.id)
+        .then(
+            post =>
+                post.length === 0
+                    ? res.status(404).json({ message: 'The post could not be found' })
+                    : res.status(200).json(post)
+        )
+        .catch(e => console.log(e, res.status(500).json({ error: 'The post information could not be retrieved.' })))
+})
+
+server.listen(8000, () => console.log('Server is active on port 8000'))
