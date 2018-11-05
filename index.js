5da31f82b5ae9fca82000c8383df2aed2e1bb0b7
Yusuf Nafey <yusufnafey@yahoo.com>
2018-11-05 15:06:05 -0800

    get posts by ID complete

--- index.js
+++ index.js
@@ -7,4 +7,37 @@ const server = express();
 server.get('/', (req, res) => {
     res.send('<h2>hello test</h2>')
 });
+
+server.get('/api/posts', (req, res) => {
+    db.find()
+        .then(posts => {
+            res.status(200).json(posts)
+        })
+        .catch(error => {
+            res
+                .status(500)
+                .json({ message: "Sorry, failed to retrieve posts", error: error })
+        })
+})
+
+server.get('api/posts/:id', (req, res) => {
+    const { id } = req.params
+
+    db.findById(id)
+        .then(post => {
+            if (post) {
+                res.status(200).json(post)
+            } else {
+                res.status(404).json({ message: "Post not found" })
+            }
+        })
+        .catch(error => {
+            res
+                .status(500)
+                .json({ message: "Sorry, failed to retrieve post", error: error })
+        })
+})
+
+
+
 server.listen(8000, () => console.log('the server is alive!'));
