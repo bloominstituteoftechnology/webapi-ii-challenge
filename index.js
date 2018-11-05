c971e22fa39d348f62dad5dafb5c93e395ca4952
Edwin Torres <edwin.torres.jr2@gmail.com>
2018-11-05 17:34:28 -0500

    basic get endpoint

--- index.js
+++ index.js
@@ -4,11 +4,20 @@ const server = express();
 
 const db = require("./data/db.js");
 
+// add your server code starting here
 server.get("/", (req, res) => {
   res.send("Hello from Express");
 });
 
+server.get("/posts", (req, res) => {
+  db.find()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "unexpected failure, my bad" });
+    });
+});
 server.listen(5000, () =>
   console.log("Server running on http://localhost:5000")
 );
-// add your server code starting here
