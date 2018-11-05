20c1cbcaed26ed7ee4d43d0800deef0f3ef6a3f2
theryanclausen <theryanclausen@gmail.com>
2018-11-05 16:49:13 -0700

    client site yarned up and file structure layed out.

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div >
+        
+      </div>
+    );
+  }
+}
+
+export default App;
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // import your node modules
 const express = require('express')
 const db = require('./data/db.js');
+const cors = require('cors')
 const port = 3334
 const server = express()
 server.use(express.json())
+server.use(cors())
 // add your server code starting here
 
 const sendUserError = (status, errorMessage, res) => {
