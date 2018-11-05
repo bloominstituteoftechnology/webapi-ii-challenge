8a9cefa68272f09a1fbbcaacf7b0467fa5bc7853
Rahul Desai <rd825@georgetown.edu>
2018-11-05 15:02:41 -0500

    Completed GET functionality on server

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 // import your node modules
-
+const express = require('express');
+const server = express();
 const db = require('./data/db.js');
 
 // add your server code starting here
+server.get('/api/posts', (req,res) => {
+    db.find()
+    .then(posts => {
+        res.status(200).json(posts);
+    })
+    .catch(err => {
+        res.status(500).json({error: "The posts information could not be retrieved."})
+    })
+})
+
+server.get('/api/posts/:id', (req,res) => {
+    const id = req.params.id;
+    db.findById(id)
+        .then(post => {
+            if (post) {
+                res.status(200).json(post);
+            } 
+            res.status(404).json({message: "The post with the specified ID does not exist."});
+        })
+        .catch(err => {
+            res.status(500).json({error: "The post information could not be retrieved."})
+        })
+})
+
+server.listen(9000, () => console.log('live'));
