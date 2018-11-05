d83867715f94ecb1d57230b178ae65cb98447d3c
Chad Jemmett <chad.jemmett@gmail.com>
2018-11-05 13:28:52 -0700

    Chad Jemmett Node Express Lab

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 // import your node modules
 
+const express = require('express');
+const server = express();
 const db = require('./data/db.js');
 
 // add your server code starting here
+//
+
+
+
+server.get('/', (req, res)=> {
+  res.send('hello from express');
+})
+
+
+server.listen(5000, (res, req) => {
+  console.log('the server is listening on 5000')
+})
