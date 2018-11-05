21208c2ce207af448cafefc1b326e6d45ccd13dc
bummings <eabummings@gmail.com>
2018-11-05 15:39:56 -0500

    wired with express, ready to go

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // import your node modules
+const express = require("express");
+const server = express();
 
-const db = require('./data/db.js');
-
+const db = require("./data/db.js");
 // add your server code starting here
