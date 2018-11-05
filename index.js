e219fbbb6cc099a210c3ccd719c0583585ab6b55
Drewsup123c <drewsup123c@gmail.com>
2018-11-05 14:55:50 -0700

    got some of the get requests to work

--- index.js
+++ index.js
@@ -8,8 +8,25 @@ const express = require('express');
 const server = express();
 
 server.get('/api/posts',(req,res) => {
-    db.insert(req);
-    res.send("Post Has been Inserted");
+    db.insert(req.body)
+    db.find()
+        .then(posts => {
+            res.status(200).json(posts);
+        })
+        .catch(error => {
+            res.status(500).json({message : "I done messed up this request"})
+        })
 })
+
+server.get('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    db.findById(id)
+        .then(post => {
+            res.status(200).json(post);
+        })
+        .catch(err => {
+            res.status(500).json({message: `This doesn't exist or something dude : ${id}`});
+        });
+});
 
 server.listen(5000,() => console.log("server is running"));
