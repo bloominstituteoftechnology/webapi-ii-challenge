56b7013f471d22a9764df19487b4ac0eb294b60b
pedro <pedroluismontesinosrivera@gmail.com>
2018-11-05 16:22:39 -0500

    displaying posts

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 // import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
 
+const server = express();
+
 // add your server code starting here
+server.get('/api/posts', (req, res) => {
+    db.find()
+        .then(posts => {
+            res.status(200).json(posts);
+        })
+        .catch(error => {
+            res.status(500).json({message: 'cannot find post', 'error ':error});
+        })
+});
+
+
+server.listen(8000, () => console.log("Server running on port 8000"));
