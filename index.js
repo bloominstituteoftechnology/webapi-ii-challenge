f219331ba7ffd48170801bb1936c06fadbabbeee
pedro <pedroluismontesinosrivera@gmail.com>
2018-11-05 18:40:04 -0500

    I can now add a new post

--- index.js
+++ index.js
@@ -3,31 +3,46 @@ const express = require('express');
 const db = require('./data/db.js');
 
 const server = express();
+server.use(express.json());
 
 // add your server code starting here
+server.post('/api/posts', (req, res) => {
+    const post = req.body;
+
+    db.insert(post)
+        .then(post => {
+            if(!title || !contents){
+                res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+            }else{
+                posts.push(post);
+                res.status(201).json({ posts });
+            }
+        })
+});
+
 server.get('/api/posts', (req, res) => {
     db.find()
         .then(posts => {
             res.status(200).json(posts);
         })
         .catch(error => {
-            res.status(500).json({message: 'cannot find post', 'error ':error});
+            res.status(500).json({ error: 'The posts information could not be retrieved.', 'error': error });
         })
 });
 
 server.get('/api/posts/:id', (req, res) => {
-    db.findById(req.params.id)
+    const id = req.params.id;
+    db.findById(id)
         .then(post => {
             if(post){
                 res.status(200).json(post);
             }else{
-                res.status(404).json({message: 'cannot find post', 'error': error});
+                res.status(404).json({ message: 'The post with the specified ID does not exist.'});
             }
         })
         .catch(error => {
-            res.status(500).json({message: 'cannot get user at this moment', 'error': error});
+            res.status(500).json({ error: 'The post information could not be retrieved.' });
         });
 })
-
 
 server.listen(8000, () => console.log("Server running on port 8000"));
