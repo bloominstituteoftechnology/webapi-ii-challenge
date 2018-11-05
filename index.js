b89dd45544b7fdd40bcadf359e8d4f93f750a3db
frankbenevento <frankbenevento123@gmail.com>
2018-11-05 12:26:46 -0800

    got the basic get end point wired up

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 // import your node modules
-
+const express = require('express')
+const server = express();
+server.listen(5000, ()=> console.log('the server is up!'))
 const db = require('./data/db.js');
+
+
+server.get('/api/posts',(req,res)=>{
+    db.find().then((value)=>{ res.status(200).send(value)})
+})
+
 
 // add your server code starting here
