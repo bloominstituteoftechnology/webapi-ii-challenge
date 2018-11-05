beb747309d64924d9f2261cf8d32bcd29ecb7bfb
ryanboris <ryan.boris@gmail.com>
2018-11-05 14:37:35 -0500

    Adds ability to POST

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const db = require('./data/db.js')
 
 // add your server code starting here
 const server = express()
-
+server.use(express.json())
 
 server.get('/api/posts', (req, res) => {
     db
@@ -25,6 +25,18 @@ server.get('/api/posts/:id', (req, res) => {
                     : res.status(200).json(post)
         )
         .catch(e => console.log(e, res.status(500).json({ error: 'The post information could not be retrieved.' })))
+})
+
+server.post('/api/posts', (req, res) => {
+    if (!req.body.hasOwnProperty('title') || !req.body.hasOwnProperty('contents')) {
+        res.status(400).json({ errorMessage: 'Pleae provide title and contents for the post.' })
+    } else {
+        Ï
+        db
+            .insert(req.body)
+            .then(res.status(201).json(req.body))
+            .catch(res.status(500).json({ error: 'There was an error while saving the post to the database.' }))
+    }
 })
 
 server.listen(8000, () => console.log('Server is active on port 8000'))
