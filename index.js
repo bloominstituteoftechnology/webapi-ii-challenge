c6837fe6eb6b1125fafec69a1c8478064c4fece2
IAN CAMERON <iancameron@IANs-MacBook-Pro.local>
2018-11-05 16:50:23 -0700

    post

--- index.js
+++ index.js
@@ -18,11 +18,12 @@ server.get('/api/posts', (req, res) => {
 });
 
 server.get('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
     db .findById(id)
-        .then(posts => {
-            if(posts.id) {
+        .then(post => {
+            if(post) {
                 res.status(200)
-                res.json(posts)
+                res.json(post)
             } else {
                 res.status(404).json({ message: "The post with the specified ID does not exist."})
             }
@@ -31,3 +32,23 @@ server.get('/api/posts/:id', (req, res) => {
             res.status(500).json({ error: "The post information could not be retrieved."})
         })
 })
+
+server.post('/api/posts', (req, res) => {
+    const { title, contents } = req.body;
+    if(!title || !contents) {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post."})
+    } else {
+        const { post } = req.body;
+        db .insert(post)
+            .then(response => {
+                res.status(201)
+                res.json(posts)
+            })
+            .catch(error => {
+                res.status(500).json({ error: "There was an error while saving the post to the database" })
+            })
+           
+    }
+})
+
+server.listen(4000, console.log('server is running on port 4000'));
