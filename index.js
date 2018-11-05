1b3ace2a0a6afcc2aa088e2bdd181abdc97d41f8
KHollobaugh <khollobaugh@gmail.com>
2018-11-05 15:32:18 -0500

    changed get syntax

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
 const express = require('express');
 
-const greeter = require('./greeter.js');
+// const greeter = require('./greeter.js');
 const db = require('./data/db.js');
 
 const server = express();
@@ -12,38 +12,38 @@ server.get('/', (req, res) => {
     res.json('alive');
 });
 
-server.get('/greet', (req, res) => {
-    res.json({ hello: 'stranger' });
-});
+// server.get('/greet', (req, res) => {
+//     res.json({ hello: 'stranger' });
+// });
 
-server.get('/api/users', (req, res) => {
-    db.find().then(users => {
-        res.json(users);
+server.get('/api/posts', (req, res) => {
+    db.find().then(posts => {
+        res.json(posts);
     })
     .catch(err => {
-        res.status(500).json( {message: "we failed, can't get users", error: err });
+        res.status(500).json( {message: "we failed, can't get posts", error: err });
     });
 });
 
-server.get('/api/users/:id', (req, res) => {
+server.get('/api/posts/:id', (req, res) => {
     const { id } = req.params;
 
-    db.findById(id).then(user => {
-        if(user) {
-           res.status(200).json(user); 
+    db.findById(id).then(post => {
+        if(post) {
+           res.status(200).json(post); 
         } else {
-            res.status(404).json({ message: 'user not found' });
+            res.status(404).json({ message: 'post not found' });
         }
         
     })
     .catch(err => {
         res
         .status(500)
-        .json({ message: "we can't get the user", error: err })
+        .json({ message: "we can't get the post", error: err })
     });
 });
 
-server.get('/greet/:person', greeter);
+// server.get('/greet/:person', greeter);
 
 server.listen(9000, () => console.log('the server is alive!'));
 
