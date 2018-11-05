5f2eb6c3a87d47123e309f85167bbc7b5880e81e
Yusuf Nafey <yusufnafey@yahoo.com>
2018-11-05 14:47:59 -0800

    testing server, works

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // import your node modules
-
 const db = require('./data/db.js');
+const express = require('express');
+const server = express();
 
 // add your server code starting here
+server.get('/', (req, res) => {
+    res.send('<h2>hello test</h2>')
+});
+server.listen(8000, () => console.log('the server is alive!'));
