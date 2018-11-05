1ab80b32553a0419d778aaba7cf49c0a3681aa0c
sarahtennis <sarahtennis@Sarahs-Air.fios-router.home>
2018-11-05 16:46:56 -0500

    POST to /api/posts

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const db = require('./data/db.js');
+const bodyParser = require('body-parser');
 const server = express();
+
+server.use(bodyParser.json());
 
 // ***** GET request to /api/posts: *****
 // If there's an error in retrieving the posts from the database:
@@ -30,7 +33,7 @@ server.get('/api/posts', (req, res) => {
 server.get('/api/posts/:id', (req, res) => {
     db.findById(req.params.id)
         .then(post => {
-            if(post.length){
+            if (post.length) {
                 res.json(post);
             } else {
                 res.status(404).json({ message: "The post with the specified ID does not exist." });
@@ -39,6 +42,39 @@ server.get('/api/posts/:id', (req, res) => {
         .catch(err => {
             res.status(500).json({ error: "The post information could not be retrieved." });
         })
+})
+
+// ***** POST request to /api/posts *****
+// If the request body is missing the title or contents property:
+// - cancel the request.
+// - respond with HTTP status code 400 (Bad Request).
+// - return the following JSON response: { errorMessage: "Please provide title and contents for the post." }.
+// If the information about the post is valid:
+// - save the new post the the database.
+// - return HTTP status code 201 (Created).
+// - return the newly created post.
+// If there's an error while saving the post:
+// - cancel the request.
+// - respond with HTTP status code 500 (Server Error).
+// - return the following JSON object: { error: "There was an error while saving the post to the database" }.
+server.post('/api/posts', (req, res) => {
+    const { title, contents } = req.body;
+    const newPost = { title, contents };
+
+    if (newPost.title && newPost.contents) {
+        db.insert(newPost)
+            .then(post => {
+                return db.findById(Number(post.id));
+            })
+            .then(foundPost => {
+                res.status(201).json(foundPost[0]);
+            })
+            .catch(err => {
+                res.status(500).json({ error: "There was an error while saving the post to the database" });
+            })
+    } else {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+    }
 })
 
 server.listen(9000);
