53281e6928459597805b5ae8f2010fe5288ba85a
samsisle <samko9522@gmail.com>
2018-11-05 15:38:04 -0800

    Add DELETE request handler

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ server.use(cors());
 server.get('/api/posts', (_, res) => {
   db.find()
     .then(posts => res.status(200).json(posts))
-    .catch(() => res.status(500).json({ error: 'The posts information could not be retrieved.' }));
+    .catch(err => res.status(500).json({ error: 'The posts information could not be retrieved.' }));
 });
 
 server.get('/api/posts/:id', (req, res) => {
@@ -27,13 +27,33 @@ server.get('/api/posts/:id', (req, res) => {
           ? res.status(200).json(post)
           : res.status(404).json({ message: 'The post with the specified ID does not exist.' })
     )
-    .catch(() => {
+    .catch(err => {
       res.status(500).json({ error: 'The post information could not be retrieved.' });
     });
 });
 
 server.post('/api/posts', (req, res) => {
   const { title, contents } = req.body;
+  if (title && contents) {
+    db.insert(req.body)
+      .then(res => db.findById(res.id).then(post => res.status(201).json(post)))
+      .catch(err =>
+        res.status(500).json({ error: 'There was an error while saving the post to the database' })
+      );
+  } else {
+    res.status(400).json({ errorMessage: 'Please provide title and contents for the post.' });
+  }
+});
+
+server.delete('/api/posts/:id', (req, res) => {
+  db.remove(req.params.id)
+    .then(
+      post =>
+        post
+          ? res.status(200).send('You sucessfully deleted the post.')
+          : res.status(404).json({ message: 'The post with the specified ID does not exist.' })
+    )
+    .catch(err => res.status(500).json({ error: 'The post could not be removed' }));
 });
 
 server.listen(port, () => console.log(`Server running on port ${port}.`));
