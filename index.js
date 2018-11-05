1798cc1c0fdc711dcbc9c0cfbc455aa2c16e5b92
Hargobind Atwal <ethaipath@gmail.com>
2018-11-05 12:54:58 -0800

    init commit

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 // import your node modules
 
 const db = require('./data/db.js');
+const express = require('express');
+const PORT = 9000;
+
 
 // add your server code starting here
+
+const server = express();
+
+server.listen(PORT, () => console.log('Server is running on port: ' + PORT));
+
