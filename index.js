892c7496589c3ef0488607bc085f0890ca6dc998
Katie Cleary <katiecleary@airmaster.lan>
2018-11-05 16:41:16 -0500

    filled in get request code

--- index.js
+++ index.js
@@ -2,27 +2,42 @@
 
 const db = require('./data/db.js');
 
-
-
-// watch for connections on port 5000
-
-
 // add your server code starting here
+
 const express = require('express'); // import the express package
 const server = express(); // creates the server
 server.listen(5000, () =>
   console.log('Server running on http://localhost:5000')
-);
-server.get('/', (req, res) => {
-    res.send('Hello from Express');
-  });
+);// assigns a port
 
 server.get('/api/posts', (req, res) => {
-    res.send('Hello from Express');
+    db.find() //calling find method from db.js file 
+      .then(posts=> { 
+        res.status(200).json(posts);
+      }) // once you find them- communicated by 200 status code, display them
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: "The posts information could not be retrieved." });
+      });//if you 'catch' an error as defined by status 500 - let the client know
   });
 
-server.get('/api/id', (req, res) => {
-    res.send('Hello from Express');
+server.get('/api/posts/:id', (req, res) => {
+    const { id } = req.params; //pull the id off the request 
+    db.findById(id) //call findbyid method, passing in id from above
+      .then(post => { //then check for ...
+        if (post) { // status 200 - we found it!
+          res.status(200).json(post);
+        } else { // or oops - if we could retrieve it, we would but it's not here, status 404
+          res.status(404).json({ message: 
+            "The post with the specified ID does not exist." });
+        }
+      })
+      .catch(err => {
+        res //if data can't be retrieved ... 
+          .status(500)
+          .json({ error: "The post information could not be retrieved." });
+      });
   });
 /*
 
