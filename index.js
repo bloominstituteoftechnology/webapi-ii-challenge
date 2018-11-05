ec1dfd4599de9951587c03a7e79bd1c4bd9fd723
Riley-Brown <riley@riley.gg>
2018-11-05 17:51:34 -0500

    Added get request by id

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ server.get("/", (req, res) => {
   res.json({ hello: "testing" });
 });
 
+// get
 server.get("/api/users", (req, res) => {
   db.find()
     .then(users => {
@@ -19,4 +20,23 @@ server.get("/api/users", (req, res) => {
       res.status(500).json({ message: "failed", error: err });
     });
 });
+
+// get by id
+server.get("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  db.findById(id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({ message: "user not found" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "failed", error: err });
+    });
+});
+
+// server listen
 server.listen(9000, () => console.log("the server is alive"));
