2fc5d95cef2b4168f8852f452d14b2b8303976b3
koennecker <koenneckerb@gmail.com>
2018-11-05 14:01:43 -0700

    Added GET functionality. - Can fetch the array of posts or an individual post.

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-// import your node modules
+const express = require("express");
+const db = require("./data/db.js");
 
-const db = require('./data/db.js');
+const server = express();
+server.listen(9000, () => console.log("Listening on port 9000."));
 
-// add your server code starting here
+server.get("/api/posts", (req, res) => {
+  db.find()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Can't get posts", err: err });
+    });
+});
+
+server.get("/api/posts/:id", (req, res) => {
+  const { id } = req.params;
+
+  db.findById(id)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: "404 not found" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Can't get post", err: err });
+    });
+});
