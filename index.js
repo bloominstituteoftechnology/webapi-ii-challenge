6bc079b1a8f9735b50b712b3f3f866c1c8bca1cc
Steven O'Neill <oneillsteven957@gmail.com>
2018-11-05 16:02:27 -0600

    posts get request

--- index.js
+++ index.js
@@ -9,5 +9,20 @@ server.get('/', (req, res) => {
     res.json('active');
 });
 
-
 // add your server code starting here
+
+server.get('/api/posts', (req, res) => {
+    db.find()
+      .then(users => {
+        res.status(200).json(users);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: "The posts information could not be retrieved.", error: err });
+      });
+  });
+  
+  
+
+server.listen(9000, () => console.log('the server is active'));
