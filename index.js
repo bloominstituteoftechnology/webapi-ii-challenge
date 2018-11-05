0f6de0cbecf58824e3da2ead7b5989196957f5f3
ssmodish <smodish@hotmail.com>
2018-11-05 16:12:25 -0500

    find and findById, added need to be tested

--- index.js
+++ index.js
@@ -3,3 +3,40 @@
 const db = require('./data/db.js');
 
 // add your server code starting here
+const express = require('express');
+const server = express();
+
+server.get('/api/posts', (req, res) => {
+  db.find()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ message: 'find failed', error: err});
+    });
+});
+
+server.get('/api/posts/:id', (req, res) => {
+  db.findById(id)
+    .then(post => {
+      if(post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: 'post not found'})
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ message: 'findById failed', error: err});
+    });
+});
+
+
+
+
+
+
+server.listen(9000, () => console.log('Listening on 9000'));
