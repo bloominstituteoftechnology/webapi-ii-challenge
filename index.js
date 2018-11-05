db3cc4aabc97227517b7e49b3cb436b9a43f1152
Matthew Wright <matthewbwright@gmail.com>
2018-11-05 13:33:07 -0700

    completes the GET endpoints

--- index.js
+++ index.js
@@ -7,4 +7,41 @@ const db = require('./data/db.js');
 // add your server code starting here
 const server = express();
 
+server.get('/', (req, res) => {
+  res.json('Welcome to Hobbiton!');
+});
+
+server.get('/api/posts', (req, res) => {
+  db.find()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: 'The posts information could not be retrieved',
+        error: err
+      });
+    });
+});
+
+server.get('/api/posts/:id', (req, res) => {
+  const { id } = req.params;
+  db.findById(id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: 'The post information could not be retrieved',
+        error: err
+      });
+    });
+});
+
 server.listen(5800, () => console.log('this is the server'));
