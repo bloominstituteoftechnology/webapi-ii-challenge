1b8e5776c74c7863e52b47948f029793dbde9439
Brian G <brian.gregorius@gmail.com>
2018-11-05 15:26:23 -0500

    inital commit

--- index.js
+++ index.js
@@ -3,3 +3,14 @@
 const db = require('./data/db.js');
 
 // add your server code starting here
+
+const express = require('express');
+
+const server =express();
+
+server.get('/', (req, res) => {
+    res.json('hello');
+});
+
+server.listen(9003, () => console.log('server is working'));
+
