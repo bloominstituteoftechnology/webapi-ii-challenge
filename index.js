d1c8919625e1ff9e78eb85ed0bbdee6a8bba1670
pedro <pedroluismontesinosrivera@gmail.com>
2018-11-05 16:30:29 -0500

    retrieving post by id

--- index.js
+++ index.js
@@ -15,5 +15,19 @@ server.get('/api/posts', (req, res) => {
         })
 });
 
+server.get('/api/posts/:id', (req, res) => {
+    db.findById(req.params.id)
+        .then(post => {
+            if(post){
+                res.status(200).json(post);
+            }else{
+                res.status(404).json({message: 'cannot find post', 'error': error});
+            }
+        })
+        .catch(error => {
+            res.status(500).json({message: 'cannot get user at this moment', 'error': error});
+        });
+})
+
 
 server.listen(8000, () => console.log("Server running on port 8000"));
