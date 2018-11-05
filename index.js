2e56a2ef882b58bb7a58622849945886d1494eee
Steven O'Neill <oneillsteven957@gmail.com>
2018-11-05 15:53:21 -0600

    Imported node modules

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 // import your node modules
 
+const express = require('express');
+
 const db = require('./data/db.js');
+
+const server = express();
+server.get('/', (req, res) => {
+    res.json('active');
+});
+
 
 // add your server code starting here
