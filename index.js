d2eac85f70765b28fb21f4cfa07b3911e3e62adb
Yanrong Xu <xuyanrong@icloud.com>
2018-11-05 16:03:51 -0500

    creating the server

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
 
 // add your server code starting here
+const server = express();
+server.listen(9000, () => console.log('server is runner'));
