206b69e1e7fd47258e57e71d6efa371c7b405055
BrandonVilaychith <blvilaychith@gmail.com>
2018-11-05 16:13:58 -0600

    PUT finished

--- index.js
+++ index.js
@@ -77,4 +77,33 @@ server.delete('/api/posts/:id', (req, res) => {
     });
 });
 
+server.put('/api/posts/:id', (req, res) => {
+  const id = req.params.id;
+  const { title, contents } = req.body;
+  const updatedPost = { title, contents };
+
+  if (!title || !contents) {
+    return res
+      .status(400)
+      .json({
+        errorMessage: 'Please provide title and contents for the post.'
+      });
+  }
+
+  db.update(id, updatedPost)
+    .then(post => {
+      if (!post) {
+        return res.status(404).json({
+          message: 'The post with the specified ID does not exist.'
+        });
+      }
+      res.status(200).json(`Post with the ID of ${id} has been updated`);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ error: 'The post information could not be modified.' });
+    });
+});
+
 server.listen(8500, () => console.log('Server is running on port 8500...'));
