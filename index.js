cdbe85ec11d35858c5259a87bf21b08b7adcbe09
SavannahGreuel <smallidgesavannah@gmail.com>
2018-11-05 13:47:54 -0700

    initial commit, GET requests are complete

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 // import your node modules
-
+const express = require ('express')
 const db = require('./data/db.js');
+const server = express();
 
 // add your server code starting here
+server.get('/api/posts', (req, res) => {
+    db.find()
+    .then(posts => {
+        res.status(200).json(posts);
+    })
+    .catch(err => {
+        res.status(500).json({error: "the posts information could not be retrieved."})
+    })
+})
+
+server.get('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    db.findById(id)
+    .then (post => {
+        if (post) {
+            res.status(200).json(post);
+        }
+        res.status(404).json({message: "The post with the specified ID does not exist."});
+    })
+    .catch (err => {
+        res.status(500).json({error: "The post information could not be retrieved. "})
+    })
+})
+
+server.listen(8000, () => console.log( 'This server is alive and breathing!'))
