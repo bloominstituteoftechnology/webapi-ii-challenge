e53d286234ce83db28d1f8de7461e6573943d21d
rytwalker <rytwalker@gmail.com>
2018-11-05 15:59:09 -0500

    Added get requests

--- index.js
+++ index.js
@@ -5,4 +5,23 @@ const db = require('./data/db.js');
 // add your server code starting here
 const server = express();
 
+// Requests
+server.get('/api/posts', (req, res) => {
+  db.find()
+    .then(posts => res.status(200).json(posts))
+    .catch(err => res.status(500).json({ message: 'There was an error', err }));
+});
+
+server.get('/api/posts/:id', (req, res) => {
+  const { id } = req.params;
+  db.findById(id)
+    .then(
+      post =>
+        post
+          ? res.status(200).json(post)
+          : res.status(404).json({ message: 'That post does not exist' })
+    )
+    .catch(err => res.status(500).json({ message: 'There was an error', err }));
+});
+
 server.listen(4000, (req, res) => console.log('Server started on port 4000'));
