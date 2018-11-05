b100f85d075642d0abe3034ff5c4c0cf6615a664
bummings <eabummings@gmail.com>
2018-11-05 17:45:09 -0500

    MVP complete

--- index.js
+++ index.js
@@ -3,30 +3,32 @@ const express = require("express");
 const server = express();
 
 const db = require("./data/db.js");
+
 // add your server code starting here
 
 server.get("/", (req, res) => {
-  res.send("sup");
+  res.status(200).send("sup");
 });
 
 server.get("/api/posts", (req, res) => {
-  res.send("sup");
+  db.find()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Error 500: Posts not found." });
+    });
 });
 
 server.get("/api/posts/:id", (req, res) => {
-  const id = req.params;
-  res.send({ sup: id });
+  const { id } = req.params;
 
   db.findById(id)
-    .then(user => {
-      if (user) {
-        res.status(200).json(user);
-      } else {
-        res.status(404).json({ message: "nah" });
-      }
+    .then(post => {
+      res.status(200).json(post);
     })
     .catch(err => {
-      res.status(500).json({ message: "cannot get user: ", error: err });
+      res.status(500).json({ message: "Error 500: Post not found." });
     });
 });
 
