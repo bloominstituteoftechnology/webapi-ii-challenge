96117c2118e3a1bc6f4c99312b3b7c5cbc7ad1f1
Carlos Lantigua <clantigua954@gmail.com>
2018-11-05 16:18:44 -0500

    removed if else statement from get ALL posts method

--- index.js
+++ index.js
@@ -15,11 +15,7 @@ server.get('/api/posts', (req, res) => {
 	db
 		.find()
 		.then((posts) => {
-			if (posts) {
-				res.status(200).json(posts);
-			} else {
-				res.status(404).json({ message: 'Post not found' });
-			}
+			res.status(200).json(posts);
 		})
 		.catch((err) => res.status(500).json({ message: "Can't get post data!!" }));
 });
