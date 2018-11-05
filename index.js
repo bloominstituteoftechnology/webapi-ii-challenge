3bc6144ca6318d790eba985ad48aaaa95f15ff86
BrandonVilaychith <blvilaychith@gmail.com>
2018-11-05 13:18:00 -0600

    /api/posts endpoint finished

--- index.js
+++ index.js
@@ -6,4 +6,16 @@ const db = require('./data/db.js');
 // add your server code starting here
 const server = express();
 
+server.get('/api/posts', (req, res) => {
+  db.find()
+    .then(posts => {
+      res.json(posts);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ error: 'The posts information could not be retrieved.' });
+    });
+});
+
 server.listen(8000, () => console.log('Server is running on port 8000...'));
