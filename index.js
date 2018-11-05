83fa8c29e016e9af49950f2e12267b704f7e763d
Edwin Torres <edwin.torres.jr2@gmail.com>
2018-11-05 16:41:48 -0500

    dependancies added, server porting to URL

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 // import your node modules
+const express = require("express");
+const server = express();
 
-const db = require('./data/db.js');
+const db = require("./data/db.js");
 
+server.get("/", (req, res) => {
+  res.send("Hello from Express");
+});
+
+server.listen(5000, () =>
+  console.log("Server running on http://localhost:5000")
+);
 // add your server code starting here
