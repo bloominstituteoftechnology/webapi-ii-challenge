b0b6d6dbc65830c005a2141b44d188365989772b
IAN CAMERON <iancameron@IANs-MacBook-Pro.local>
2018-11-05 15:38:09 -0700

    get

--- index.js
+++ index.js
@@ -5,3 +5,29 @@ const db = require('./data/db.js');
 const express = require('express');
 
 // add your server code starting here
+const server = express();
+
+server.get('/api/posts', (req, res) => {
+    db .find()
+        .then(posts => {
+            res.json(posts)
+        })
+        .catch(error => {
+            res.status(500).json({ error: "The posts information could not be retrieved."})
+        })
+});
+
+server.get('/api/posts/:id', (req, res) => {
+    db .findById(id)
+        .then(posts => {
+            if(posts.id) {
+                res.status(200)
+                res.json(posts)
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist."})
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ error: "The post information could not be retrieved."})
+        })
+})
