16327f4d0f1ef41e0e5783c0f30566c21c15fd0d
Hargobind Atwal <ethaipath@gmail.com>
2018-11-05 13:17:28 -0800

    Got get request working

--- index.js
+++ index.js
@@ -9,5 +9,21 @@ const PORT = 9000;
 
 const server = express();
 
+server.get('/api/posts', (req, res) => {
+  db.find()
+    .then(posts => {
+      console.log(posts);
+      const formatedPosts = posts.map( (post) => ({ 
+        title: post.title, 
+        contents: post.contents 
+      }));
+      console.log(formatedPosts);
+      res.status(200).json(formatedPosts);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "The posts information could not be retrieved." });
+    })
+});
+
 server.listen(PORT, () => console.log('Server is running on port: ' + PORT));
 
