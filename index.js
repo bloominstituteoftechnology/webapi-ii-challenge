bbd3bb0c16214efeff5aea6311e71a54f22629a2
kyranmccann <kyran.mccann@gmail.com>
2018-11-05 12:06:25 -0800

    get posts and get by id endpoints

--- index.js
+++ index.js
@@ -1,5 +1,57 @@
 // import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
+
+const server = express();
+
+server.get('/api/posts', (req, res) => {
+  db.find()
+    .then(posts => {
+      res
+      .status(200)
+      .json(posts);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: 'The posts information could not be retrieved'});
+    })
+})
+
+server.get('/api/posts/:id', (req, res) => {
+  const { id } = req.params;
+
+  db.findById(id)
+    .then(post => {
+      if (post) {
+        res
+          .status(200)
+          .json(post);
+      } else {
+        res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist.' });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: 'The post information could not be retrieved' });
+    })
+});
+
+server.listen(9000, () => console.log('the server lives on port 9000'));
+
+// server.post('/api/posts' (req, res) => {
+//
+// })
+//
+// server.put('/api/posts/:id', (req, res) => {
+//
+// })
+//
+// server.delete('/api/posts/:id', (req, res) => {
+//
+// })
 
 // add your server code starting here
