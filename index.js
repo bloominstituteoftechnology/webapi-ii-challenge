79afaf6b21e4ccabe94ab95b94b6a44b4668db53
koennecker <koenneckerb@gmail.com>
2018-11-05 16:33:44 -0700

    Updated error messages to be in line with README.md

--- index.js
+++ index.js
@@ -7,13 +7,23 @@ server.use(cors());
 server.use(express.json());
 server.listen(9000, () => console.log("Listening on port 9000."));
 
+const sendUserError = (status, msg, res, err) => {
+  res.status(status).json({ error: msg, err: err });
+  return;
+};
+
 server.get("/api/posts", (req, res) => {
   db.find()
     .then(posts => {
       res.status(200).json(posts);
     })
     .catch(err => {
-      res.status(500).json({ message: "Can't get posts", err: err });
+      sendUserError(
+        500,
+        "The posts information could not be retrieved.",
+        res,
+        err
+      );
     });
 });
 
@@ -25,61 +35,98 @@ server.get("/api/posts/:id", (req, res) => {
       if (post) {
         res.status(200).json(post);
       } else {
-        res.status(404).json({ message: "404 not found" });
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
       }
     })
     .catch(err => {
-      res.status(500).json({ message: "Can't get post", err: err });
+      sendUserError(
+        500,
+        "The posts information could not be retrieved.",
+        res,
+        err
+      );
     });
 });
 
 server.post("/api/posts/", (req, res) => {
   const { title, contents } = req.body;
   const newPost = { title, contents };
+  if (!newPost) {
+    res
+      .status(400)
+      .json({ message: "Please provide title and contents for the post." });
+    return;
+  }
   db.insert(newPost)
     .then(postId => {
       const { id } = postId;
       db.findById(id).then(post => {
-        console.log(post);
-        if (!post) {
-          return res
-            .status(422)
-            .send({ Error: `Post does not exist by that id ${id}` });
-        }
         res.status(201).json(post);
       });
     })
-    .catch(err => console.error(err));
+    .catch(err => {
+      sendUserError(
+        500,
+        "There was an error while saving the post to the database",
+        res,
+        err
+      );
+    });
 });
 
 server.delete("/api/posts/:id", (req, res) => {
   const { id } = req.params;
-  db.remove(id)
-    .then(removedPost => {
-      res
-        .status(200)
-        .json(
-          removedPost
-            ? `Post ${id} successfully deleted.`
-            : `Error attempting to delete post ${id}.`
-        );
+  db.findById(id)
+    .then(post => {
+      if (post.length > 0) {
+        db.remove(id).then(removedPost => {
+          res
+            .status(200)
+            .json(removedPost ? `Post ${id} successfully deleted.` : null);
+        });
+      } else {
+        res
+          .status(404)
+          .send({ message: "The post with the specified ID does not exist." });
+      }
     })
-    .catch(err => console.error(err));
+    .catch(err => {
+      sendUserError(500, "The post could not be removed", res, err);
+    });
 });
 
 server.put("/api/posts/:id", (req, res) => {
   const { id } = req.params;
   const { title, contents } = req.body;
-  const postToUpdate = { title, contents };
-  db.update(id, postToUpdate)
-    .then(updatedPost => {
-      res
-        .status(200)
-        .json(
-          updatedPost
-            ? `Post ${id} successfully updated.`
-            : `Error attempting to update post ${id}.`
-        );
+  if (!title && !contents) {
+    res
+      .status(400)
+      .json({ message: "Please provide title and contents for the post." });
+    return;
+  }
+  db.findById(id)
+    .then(post => {
+      const postToUpdate = { title, contents };
+      if (post.length > 0) {
+        db.update(id, postToUpdate).then(updatedPost => {
+          res
+            .status(200)
+            .json(updatedPost ? `Post ${id} successfully updated.` : null);
+        });
+      } else {
+        res
+          .status(404)
+          .send({ message: "The post with the specified ID does not exist." });
+      }
     })
-    .catch(err => console.error(err));
+    .catch(err =>
+      sendUserError(
+        500,
+        "The post information could not be modified.",
+        res,
+        err
+      )
+    );
 });
