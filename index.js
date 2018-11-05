45e37f69a8cd8c0cb34d3d8ffacaa6513aabb1ca
Axolotl <samuelmachat@gmail.com>
2018-11-05 16:47:13 -0500

    get requests working

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 // import your node modules
 
 const db = require('./data/db.js')
-const expres = require('express')
+const express = require('express')
 
 // add your server code starting here
 
+const server = express()
+const port = 5000
+
+server.get('/', (req, res) => {
+  res.send('Hello from Express')
+})
+
+server.get('/api/posts', (req, res) => {
+  db.find()
+    .then(posts => res.json(posts))
+    .catch(err => res.status(500).send('Could not locate posts'))
+})
+
+server.get('/api/posts/:id', (req, res) => {
+  db.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err => res.status(500).send('Could not locate post'))
+})
+
+server.listen(port, () =>
+  console.log(`Server running on http://localhost:${port}`)
+)
