46b252c967f6de8f0c8db0c4a6762b35bf240300
samsisle <samko9522@gmail.com>
2018-11-05 14:07:06 -0800

    Update GET requests

--- server.js
+++ server.js
@@ -13,9 +13,27 @@ server.use(express.json());
 server.use(cors());
 
 /*--- request handlers ---*/
-server.get('/api/posts', async (_, res) => {
-  const posts = await db.find();
-  res.status(200).json(posts);
+server.get('/api/posts', (_, res) => {
+  db.find()
+    .then(posts => res.status(200).json(posts))
+    .catch(() => res.status(500).json({ error: 'The posts information could not be retrieved.' }));
+});
+
+server.get('/api/posts/:id', (req, res) => {
+  db.findById(req.params.id)
+    .then(
+      post =>
+        post.length > 0
+          ? res.status(200).json(post)
+          : res.status(404).json({ message: 'The post with the specified ID does not exist.' })
+    )
+    .catch(() => {
+      res.status(500).json({ error: 'The post information could not be retrieved.' });
+    });
+});
+
+server.post('/api/posts', (req, res) => {
+  const { title, contents } = req.body;
 });
 
 server.listen(port, () => console.log(`Server running on port ${port}.`));
