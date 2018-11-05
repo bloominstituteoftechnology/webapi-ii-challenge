358a41d8c5e407973e92203603cdfc32a7196b29
sarahtennis <sarahtennis@Sarahs-Air.fios-router.home>
2018-11-05 15:36:15 -0500

    GET request for /api/posts/:id

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// import your node modules
 const express = require('express');
 const db = require('./data/db.js');
 const server = express();
@@ -16,6 +15,29 @@ server.get('/api/posts', (req, res) => {
         })
         .catch(err => {
             res.status(500).json({ error: "The posts information could not be retrieved." })
+        })
+})
+
+// ***** GET request to /api/posts/:id *****
+// If the post with the specified id is not found:
+// - return HTTP status code 404 (Not Found).
+// - return the following JSON object: { message: "The post with the specified ID does not exist." }.
+// If there's an error in retrieving the post from the database:
+// - cancel the request.
+// - respond with HTTP status code 500.
+// - return the following JSON object: { error: "The post information could not be retrieved." }.
+
+server.get('/api/posts/:id', (req, res) => {
+    db.findById(req.params.id)
+        .then(post => {
+            if(post.length){
+                res.json(post);
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: "The post information could not be retrieved." });
         })
 })
 
