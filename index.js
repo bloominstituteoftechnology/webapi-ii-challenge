47eb6d0275dd69e4281fb029bc93636e90e7f453
Matthew Wright <matthewbwright@gmail.com>
2018-11-05 13:16:26 -0700

    sets up server on local host

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // import your node modules
 
+const express = require('express');
+
 const db = require('./data/db.js');
 
 // add your server code starting here
+const server = express();
+
+server.listen(5800, () => console.log('this is the server'));
