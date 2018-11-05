254ddfc07dc09f2b49c4ad794b5e40547a61a909
samsisle <samko9522@gmail.com>
2018-11-05 13:04:56 -0800

    Add GET request handler

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
-// import your node modules
+/*--- import your node modules ---*/
+const express = require('express');
+const cors = require('cors');
 
+/*--- file imports, constants ---*/
 const db = require('./data/db.js');
+const port = 5000;
 
-// add your server code starting here
+const server = express();
+
+/*--- server middleware ---*/
+server.use(express.json());
+server.use(cors());
+
+/*--- request handlers ---*/
+server.get('/api/posts', async (req, res) => {
+  const posts = await db.find();
+  res.status(200).json(posts);
+});
+
+server.listen(port, () => console.log(`Server running on port ${port}.`));
