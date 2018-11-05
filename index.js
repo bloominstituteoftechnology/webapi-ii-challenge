8206bf432d7c713b024572f598158b871a1d974e
rytwalker <rytwalker@gmail.com>
2018-11-05 15:48:59 -0500

    Inital commit server running

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
 
 // add your server code starting here
+const server = express();
+
+server.listen(4000, (req, res) => console.log('Server started on port 4000'));
