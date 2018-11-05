8a664d3a217c277ce7f4a7c96191f8ad65257f27
Chad Jemmett <chad.jemmett@gmail.com>
2018-11-05 14:01:45 -0700

    added client

--- index.js
+++ index.js
@@ -13,6 +13,32 @@ server.get('/', (req, res)=> {
   res.send('hello from express');
 })
 
+server.get('/api/posts', (req, res)=> {
+  db.find()
+    .then(posts => {
+    res.status(200).json(posts);
+    })
+    .catch(err => {
+    res
+      .status(500)
+      .json({message: 'We failed to get the users', error: err})
+    })
+})
+
+server.get('/api/posts/:id', (req, res)=> {
+  const {id} = req.params;
+  //db function
+  db.findById(id)
+    //then we use the then func pass in the anon function
+    .then(post => {
+    res.status(200).json(post);
+    })
+    .catch(err => {
+    res
+      .status(500)
+      .json({message: 'We failed to get the users', error: err})
+    })
+})
 
 server.listen(5000, (res, req) => {
   console.log('the server is listening on 5000')
