d453ab32369fbea7b3541cce3f31337439df960c
theryanclausen <theryanclausen@gmail.com>
2018-11-05 13:44:11 -0700

    server is expressed up and listening to a port

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 // import your node modules
-
+const express = require('express')
 const db = require('./data/db.js');
-
+const port = 3334
+const server = express()
+server.use(express.json())
 // add your server code starting here
+server.get('/', (req,res) => res.send('<h1>sup</h1>'))
+
+
+
+
+server.listen(port,()=> console.log(`I hear you ${port}`))
