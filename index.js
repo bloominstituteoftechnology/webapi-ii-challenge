fec7ddf1d3dffefcab2cb5b71c20473602098ec7
Lee Len Formento <leebatef@gmail.com>
2018-11-05 16:31:50 -0500

    Fixed my get error - thanks jasmine

--- index.js
+++ index.js
@@ -28,9 +28,12 @@ server.get('/api/posts/:id', (req, res) => {
     const { id } = req.params;
     db.findById(id)
     .then(user => {
+        if (user.length === 0) {
+            res.status(404).json({ error: "The post with the specified ID does not exist." });
+        } else {
         res.json(user);
-    })
-    .catch(err => res.status(404).json({ message: "The post with the specified ID does not exist." }));
+    }})
+    .catch(err => res.status(500).json({ error: "The post information could not be retrieved." }));
 })
 
 // POST
