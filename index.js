b35880970d427fe2ea33c0217c75a0c29c81a3c5
Drewsup123c <drewsup123c@gmail.com>
2018-11-05 14:11:45 -0700

    added express as a dependency and started work on get requests

--- index.js
+++ index.js
@@ -3,3 +3,13 @@
 const db = require('./data/db.js');
 
 // add your server code starting here
+
+const express = require('express');
+const server = express();
+
+server.get('/api/posts',(req,res) => {
+    db.insert(req);
+    res.send("Post Has been Inserted");
+})
+
+server.listen(5000,() => console.log("server is running"));
