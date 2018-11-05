48a20df84c5322afd0836472a4ba9ce4f0e54d35
unknown <quan.nguyen199712@gmail.com>
2018-11-05 18:23:24 -0500

    added a get request

--- index.js
+++ index.js
@@ -3,3 +3,18 @@
 const db = require('./data/db.js');
 
 // add your server code starting here
+const express = require('express');
+const server= express();
+
+server.get('/api/post',(req, res)=>{
+    db.find()
+        .then(posts =>{
+            res.status(200).json(posts);
+        })
+        .catch(err =>{
+            res
+                .status(500)
+                .json({message: 'Failed'})
+        })
+})
+server.listen(8000,()=>console.log('API Running on port 8000') )
