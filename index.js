c20d2e3b3acd3cc96e87874251eaad094324b862
Jordan Stoddard <jjsincorporated@gmail.com>
2018-11-05 13:50:02 -0800

    MVP Complete.

--- index.js
+++ index.js
@@ -3,3 +3,47 @@
 const db = require('./data/db.js');
 
 // add your server code starting here
+
+const express = require('express')
+const server = express()
+
+server.get('/', (req, res) => {
+ res.json({message: 'No content here, please see /api/posts to begin'})
+})
+
+server.get('/api/posts', (req, res) => {
+    db.find()
+    .then(posts => {
+        res.status(200).json(posts)
+    })
+    .catch(error => {
+        res.status(500).json({error: 'The posts information could not be received.'})
+    })
+})
+
+server.get('/api/posts/:id', (req, res) => {
+    const {id} = req.params;
+    db.findById(id)
+    .then(post => {
+        if (post && post.length) {
+            return res.status(200).json(post)
+        } else {
+           return res.status(404).json({message: 'The post with the specified ID does not exist'})
+        }
+
+        // console.log(URLid.id)
+    })
+    .catch(err => {
+        res.status(500).json({error: 'The post information could not be retrieved.'})
+    })
+})
+
+
+
+
+
+
+
+
+////
+server.listen(9000, () => console.log('Server listening at port 9000'))
