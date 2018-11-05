2f6045cacc4d0cf93e31c5bb2c66736d618ce732
theryanclausen <theryanclausen@gmail.com>
2018-11-05 14:20:10 -0700

    get all get by id

--- index.js
+++ index.js
@@ -5,9 +5,30 @@ const port = 3334
 const server = express()
 server.use(express.json())
 // add your server code starting here
+
+const sendUserError = (status, message, res) => {
+    res.status(status).json({ errorMessage: message });
+  };
+
 server.get('/', (req,res) => res.send('<h1>sup</h1>'))
 
-
+server.get("/api/posts", (req, res) => {
+    db.find().then(posts => {
+      res.json({ posts });
+    }).catch(err => sendUserError(400, 'No data here', res));
+  });
+  
+server.get("/api/posts/:id", (req, res) => {
+    const {id} = req.params
+    db.findById(id).then(post => {
+        if (post[0]){
+            res.json(post[0])
+        }
+        else{
+            sendUserError(400, `No post at ${id}`, res)
+        }
+    })
+})
 
 
 server.listen(port,()=> console.log(`I hear you ${port}`))
