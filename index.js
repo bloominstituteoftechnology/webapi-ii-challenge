4daabe897cf774c434674f2a55e32c851bba45ab
Elvis Ibarra <ielvisd@gmail.com>
2018-11-05 12:33:45 -0800

    GET works, Project Started

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 // import your node modules
-
 const db = require('./data/db.js');
 
+const express = require('express');
+const server = express();
 // add your server code starting here
+console.log('hello');
+server.listen(9000);
+
+server.get('/api/posts', (req, res) => {
+  db.find()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res.status(500).json({ message: " error: 'The posts information could not be retrieved'" });
+    });
+});
