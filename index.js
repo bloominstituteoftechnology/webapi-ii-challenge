a85f0df1ff3177ff8f243ff10f07df3ed30a37e5
Carlos Lantigua <clantigua954@gmail.com>
2018-11-05 16:13:08 -0500

    Carlos Lantigua

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ server.get('/', (req, res) => {
 	res.json('alive');
 });
 
+// get all posts
 server.get('/api/posts', (req, res) => {
 	db
 		.find()
@@ -22,5 +23,27 @@ server.get('/api/posts', (req, res) => {
 		})
 		.catch((err) => res.status(500).json({ message: "Can't get post data!!" }));
 });
+
+// get post by ID
+server.get('/api/posts/:id', (req, res) => {
+	const { id } = req.params;
+	db
+		.findById(id)
+		.then((post) => {
+			if (post) {
+				res.status(200).json(post);
+			} else {
+				res.status(404).json({ message: 'That post was not found' });
+			}
+		})
+		.catch((err) => res.status(500).json({ message: "Can't get that post data!!" }));
+});
+
+// add a post
+// server.put('/api/posts/:id', (req, res) => {
+// 	const { id } = req.params;
+
+// 	db.insert((title, content) => {});
+// });
 
 server.listen(9000, () => console.log('The server is live'));
