f2b597b4bc5ed1836fecc58f31203dd4a93a7652
Stublink <chrisrey95@gmail.com>
2018-11-05 14:43:59 -0500

    Added get all post endpoint

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const db = require('./data/db.js');
 // add your server code starting here
 const server = express();
 
-
+server.get('/api/posts', (req, res) => {
+    db.find().then(posts => {
+        res.status(200).json(posts)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "Failed to get users",
+            error: err
+        }) 
+    });
+});
 
 server.listen(9000, () => console.log('Server started'))
