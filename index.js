19d4c899c64ca366440008469ea0c24993445cad
Mindysueasaurus <mymindyyeager@gmail.com>
2018-11-06 11:03:57 -0600

    the road so far

--- index.js
+++ index.js
@@ -3,3 +3,16 @@
 const db = require('./data/db.js');
 
 // add your server code starting here
+const express = require('express');
+
+const server = express();
+
+server.get('api/posts', (req, res) => {
+  
+})
+
+server.get('/api/post/:id', (req, res) => {
+
+})
+
+server.listen(8000)
