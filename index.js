d9317e3640b94b3c2a26f7add5b0b27acb77d3ee
Elvis Ibarra <ielvisd@gmail.com>
2018-11-05 16:10:28 -0800

    DELETE & POST work

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ server.get('/api/posts', (req, res) => {
     .then(posts => {
       res.status(200).json(posts);
     })
-    .catch(err => {
-      res.status(500).json({ message: " error: 'The posts information could not be retrieved'", error: err });
+    .catch(error => {
+      res.status(500).json({ message: " error: 'The posts information could not be retrieved'", error: error });
     });
 });
 
@@ -39,16 +39,52 @@ server.post('/api/posts/', (req, res) => {
   // res.send('success');
   let newpost = req.body;
 
-  db.insert(newpost)
-    .then(newpost => {
+  db.find()
+    .then(posts => {
       if (!newpost) {
         res.status(404).json({ error: 'No new post was passed.' });
+      } else if (!newpost.title || !newpost.contents) {
+        res.status(400).json({ errorMessage: 'Please provide title and contents for the post.' });
       } else {
-        res.status(200).json(post);
+        db.insert(newpost);
+        console.log(newpost.title.length);
+        res
+          .status(201)
+          .json(newpost)
+          .catch(error => {
+            res
+              .status(500)
+              .json({ error: 'There was an error while saving the post to the database. The error is ', error });
+          });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'There was an error while saving the post to the database. The error is ', error });
+    });
+});
+
+server.delete('/api/posts/:id', (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+
+  db.findById(id)
+    .then(post => {
+      if (post.length === 0) {
+        res.status(404).json({ error: 'The post with the specified ID does not exist.' });
+      } else {
+        db.remove(id)
+          .then(postsdeleted => {
+            console.log('You have deleted: ', postsdeleted);
+
+            res.status(202).json({ message: 'Post removed' });
+          })
+          .catch(err => {
+            res.status(500).json({ error: 'The post could not be removed.' });
+          });
       }
     })
     .catch(err => {
-      res.status(500).json({ error: 'The post could not be added. Error is ', err });
+      res.status(500).json({ error: 'The post could not be removed.' });
     });
 });
 
