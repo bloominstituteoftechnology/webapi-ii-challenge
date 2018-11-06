c0145f8e1e6556a94a614256c28846f102b0a249
lola <lev@lolas-air.lan>
2018-11-05 19:46:04 -0500

    POST works

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 // import your node modules
 const express = require('express');
+const cors = require('cors');
 const db = require('./data/db.js');
 
-// add your server code starting here
 const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+// add your server code starting here
+
 
 server.get('/api/posts', (req, res) => {
     db.find()
         .then(posts => {
-            res.status(200).json(posts);
+            res.status(200).json(posts)
         })
         .catch(err => {
-            res
-                .status(500)
-                .json({ error: "The posts information could not be retrieved." })
+            res.status(500).json({ error: "The posts information could not be retrieved." })
         })
 })
 
@@ -26,9 +30,7 @@ server.get('/api/posts/:id', (req, res) => {
             res.status(404).json({ message: "The post with the specified ID does not exist." })
         })
         .catch(err => {
-            res
-                .status(500)
-                .json({ error: "The post information could not be retrieved." })
+            res.status(500).json({ error: "The post information could not be retrieved." })
         })
 })
 
@@ -41,10 +43,22 @@ server.delete('/api/posts/:id', (req, res) => {
             res.status(404).json({ message: "The post with the specified ID does not exist." })
         })
         .catch(err => {
-            res
-                .status(500)
-                .json({ error: "The post information could not be removed." })
+            res.status(500).json({ error: "The post information could not be removed." })
         })
 })
 
-server.listen(9000, () => console.log('Server up & running!'));
+server.post('/api/posts', (req, res) => {
+    if (req.body.title && req.body.contents) {
+        db.insert(req.body)
+        .then(addedNote => {
+            db.findById(addedNote.id).then(post => res.status(201).json(post))
+        })
+        .catch(err => {
+            res.status(500).json({ error: "There was an error while saving the post to the database" })
+        })
+    } else {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    }
+})
+
+server.listen(9000, () => console.log('Server up & running!'))
