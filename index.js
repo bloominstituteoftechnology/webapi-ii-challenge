355c68017dddc5b6bb0e378f3559d478584a7d5c
Axolotl <samuelmachat@gmail.com>
2018-11-06 18:10:21 -0500

    convert to try catch

--- index.js
+++ index.js
@@ -10,66 +10,103 @@ server.use(express.json())
 
 // configure routes
 
-// root
-server.get('/', (req, res) => {
-  res.send('Server is live')
-})
-
 // get all posts
-server.get('/api/posts', (req, res) => {
-  db.find()
-    .then(posts => res.status(200).json(posts))
-    .catch(err =>
-      res.status(500).json({ message: 'Could not locate posts', error: err })
-    )
+server.get('/api/posts', async (req, res) => {
+  try {
+    const posts = await db.find()
+    res.status(200).json(posts)
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: 'The posts information could not be retrieved.' })
+  }
 })
 
 // get specific post
-server.get('/api/posts/:id', (req, res) => {
-  db.findById(req.params.id)
-    .then(post => {
-      if (post) {
-        res.status(200).json(post)
-      } else {
-        res.status(404).json({ message: 'Could not locate post' })
-      }
-    })
-    .catch(err =>
-      res.status(500).json({ message: 'Could not locate post', error: err })
-    )
+server.get('/api/posts/:id', async (req, res) => {
+  try {
+    const post = await db.findById(req.params.id)
+    if (post) {
+      res.status(200).json(post)
+    } else {
+      res
+        .status(404)
+        .json({ message: 'The post with the specified ID does not exist.' })
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: 'The post information could not be retrieved.' })
+  }
 })
 
 // post to db
 server.post('/api/posts', async (req, res) => {
-  try {
-    const newPost = await db.insert(req.body)
-    const user = await db.findById(newPost.id)
-    res.status(201).json(user)
-  } catch (error) {
-    res.status(500).json({
-      error: 'There was an error while saving the post to the database'
-    })
+  const { title, contents } = req.body
+  if (!title || !contents) {
+    res
+      .status(400)
+      .json({ error: 'Please provide title and contents for the post.' })
+  } else {
+    try {
+      const newPost = await db.insert(req.body)
+      const post = await db.findById(newPost.id)
+      res.status(201).json(post)
+    } catch (error) {
+      res.status(500).json({
+        error: 'There was an error while saving the post to the database'
+      })
+    }
   }
 })
 
 // update in db
-server.put('/api/posts/:id', (req, res) => {
-  db.update(req.params.id, req.body)
-    .then(count => {
+server.put('/api/posts/:id', async (req, res) => {
+  if (!db.findById(req.params.id)) {
+    res
+      .status(404)
+      .json({ message: 'The post with the specified ID does not exist.' })
+  } else if (!req.body.title || !req.body.contents) {
+    res
+      .status(400)
+      .json({ error: 'Please provide title and contents for the post.' })
+  } else {
+    try {
+      const count = await db.update(req.params.id, req.body)
       if (count) {
-        res.status(200).json({ message: 'user updated' })
+        const post = await db.findById(req.params.id)
+        res.status(200).json(post)
       } else {
-        res.status(404).json({ message: 'user not found' })
+        throw new Error('Post not updated')
       }
-    })
-    .catch(err => res.status(500).json({ message: 'error updating db ' }))
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: 'The post information could not be modified.' })
+    }
+  }
 })
 
 // delete from db
-server.delete('/api/posts/:id', (req, res) => {
-  db.remove(req.params.id)
-    .then(count => res.status(200).json(count))
-    .catch(err => res.status(500).json({ message: 'error deleting post' }))
+server.delete('/api/posts/:id', async (req, res) => {
+  const post = await db.findById(req.params.id)
+
+  if (!post) {
+    res
+      .status(404)
+      .json({ message: 'The post with the specified ID does not exist.' })
+  } else {
+    try {
+      const count = await db.remove(req.params.id)
+      if (count) {
+        res.status(200).json(post)
+      } else {
+        throw new Error('Post not deleted')
+      }
+    } catch (error) {
+      res.status(500).json({ error: 'The post could not be removed' })
+    }
+  }
 })
 
 // set up listener
