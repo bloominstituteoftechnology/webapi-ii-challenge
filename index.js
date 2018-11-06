d0783957cc0feb1f42d51fc61ee5d98b53842cfd
Edward J <edjeong99@gmail.com>
2018-11-05 19:50:18 -0500

    implement get for /posts and /post/id

--- index.js
+++ index.js
@@ -3,3 +3,32 @@
 const db = require('./data/db.js');
 
 // add your server code starting here
+
+const express = require('express');
+const server = express();
+
+server.get('/api/posts', (req, res) => {
+
+    db.find()
+    .then(posts => res.json(posts))
+    .catch(err => {
+        res.status(500)
+        .json({message : "The posts information could not be retrieved.", 
+                error : err })
+    })
+    });
+
+    server.get('/api/posts/:id', (req, res) => {
+        const {id} = req.params;
+
+        db.findById(id)
+        .then(post => res.json(post))
+        .catch(err => {
+            res.status(500)
+            .json({message : "The post information could not be retrieved.", 
+                    error : err })
+        })
+        });
+
+
+server.listen(9000, console.log("\n Server ALIVE \n"));
