c49b2cf50ca02953ce498abb3bd7dc51ff31d193
DaftBeowulf <ajmboosh@gmail.com>
2018-11-06 14:19:40 -0600

    finished wiring up POST functionality, tested via Postman

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 // import your node modules
 const express=require('express');
+//our middleware that allows for cross-origin resource sharing (CORS)
 const cors = require('cors');
 const db = require('./data/db.js');
 
 // add your server code starting here
-//creat server
+//create server
 const server=express();
+
+//pass all of our requests through CORS
 server.use(cors());
-//GET request for /api/posts
+//and express' built-in middleware used to parse request 'body' and turn it into usable JSON
+server.use(express.json());
+
+//CRUD methods/verbs
+//GET request for /api/posts. 'next' is necessary param since we're using middleware
 server.get('/api/posts', (req, res, next) => {
     db.find()
     .then(posts =>{
@@ -27,6 +34,7 @@ server.get('/api/posts/:id', (req, res, next) => {
 
     db.findById(id)
 .then(post => {
+    //added post.length condition since post will still return but as an empty array--which is NOT falsey
     if (post && post.length) {
      res.status(200)
      .json(post);   
@@ -40,5 +48,26 @@ server.get('/api/posts/:id', (req, res, next) => {
     .json({message: "The post information could not be retrieved."})
 });
 });
+
+//POST
+server.post('/api/posts', async (req, res) =>{
+
+//deconstructing instead of req.body.{prop}
+const {title, contents} = req.body;
+
+//sends a failed 400 response if title or contents are missing
+if (!title || !contents) {
+res.status(400).json({errorMessage: "Please provide title and contents for the post."})
+} else {
+try {
+        const userData = req.body;
+        const userId = await db.insert(userData);
+        res.status(201).json(userId);
+    } catch (error) {
+        res.status(500).json({error: "There was an error while saving the post to the database"})
+    }
+}
+})
+
 
 server.listen(8000, ()=>console.log('Server is listening on port 8000'));
