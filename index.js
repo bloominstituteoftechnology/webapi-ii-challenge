22465474afd61e1b388b5f64200fbf96971d8b0e
AM_Myrick <amyrick.820@gmail.com>
2018-11-06 17:32:31 -0500

    updates post endpoint... again

--- index.js
+++ index.js
@@ -40,10 +40,13 @@ server.get('/api/posts/:id', (req, res) => {
 server.post("/api/posts", async (req, res) => {
     try {
         const postData = req.body;
-        await db.insert(postData);
-        postData.title === "" || postData.contents === "" ?
-            res.status(400).json({ errorMessage: "Please provide title and contents for the post." }):
-            res.status(201).json(postData);
+        if (postData.title === "" || postData.contents === "") {
+            res.status(400).json({ error: "Please provide title and contents for the post." })
+        }
+        else {
+            const post = await db.insert(postData);
+            res.status(201).json(post);
+        }
     } catch (error) {
         res.status(500).json({ error: "There was an error while saving the post to the database" })
     }
