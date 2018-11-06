b67d480214c4647fe188eb5e5cd8406909cb5185
Brian G <brian.gregorius@gmail.com>
2018-11-05 20:50:10 -0500

    added post functionality to server

--- index.js
+++ index.js
@@ -6,11 +6,22 @@ const db = require('./data/db.js');
 
 const express = require('express');
 
+
 const server = express();
+server.use(express.json())
 
 server.get('/', (req, res) => {
     res.json('hello');
 });
+
+postsId = 10;
+
+const sendErrorMsg = (msg, res) => {
+    res.status(500);
+    res.json({ Error: msg });
+    return;
+}
+
 
 // get all posts
 
@@ -50,21 +61,39 @@ server.delete('/api/posts/:id', (req, res) => {
 
     db.remove(id)
         .then(id => {
-            if (id.length >= 1) {
-                res.status(200).json(id);
+            if (id) {
+                res.status(200).json({ message: 'Post has been removed' });
             } else {
                 res.status(404).json({ message: 'the post with the specified ID does not exist' });
             }
         })
         .catch(err => {
-            res
-                .status(500)
-                .json({ message: '"The post could not be removed"' });
+            sendErrorMsg("The post could not be removed", res);
         });
 
 });
 
+// new post 
+
+server.post('/api/posts', (req, res) => {
+    const { title, contents } = req.body;
+    if (!title || !contents) {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+
+    }
+
+    db.insert({ title, contents })
+        .then(id => {
+            res.status(201).json(id);
+        })
+        .catch(err => {
+            sendErrorMsg({ error: 'There was an error while saving the post to the database' })
+        });
+});
+
+
 // server listening on port
+
 
 server.listen(9003, () => console.log('server is working'));
 
