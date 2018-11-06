94bfc8a5397014a519e63106e2deb83c23b78172
Daniel Ferrer <danfrrr13@gmail.com>
2018-11-06 15:37:14 -0500

    added post() and put() ability to the server and tested with postman.

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const db = require('./data/db.js');
 
 const server = express();
 
+// middleware
+
+server.use(express.json())
+
 server.get('/api/posts', (req, res) => {
     db.find()
         .then(posts => {
@@ -33,5 +37,32 @@ server.get('/api/posts/:id', (req,res) => {
             res.status(500).json({ error: "The post information could not be retrieved.", error: err });
         })
 });
+
+server.post('/api/posts', (req, res) => {
+    const postData = req.body;
+    db.insert(postData)
+        .then(postId => {
+            res.status(201).json(postId);
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'error creating post', err });
+        })
+});
+
+server.put('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+    db.update(id, changes)
+        .then(count => {
+            if (count) {
+                res.status(200).json(count);
+            } else {
+                res.status(404).json({ message: 'post not found' });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'error updating post', err });
+        })
+})
 
 server.listen(9000, () => console.log('The server is live!'));
