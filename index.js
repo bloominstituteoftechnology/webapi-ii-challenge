75ed92f2402c55746beaf337d6283ddf3f2124fc
Mindysueasaurus <mymindyyeager@gmail.com>
2018-11-06 11:18:11 -0600

    updated second get

--- index.js
+++ index.js
@@ -9,14 +9,26 @@ const server = express();
 
 server.get('api/posts', (req, res) => {
   db.find().then( posts => {
-    res.json(posts);
+    res.status(200).json(posts);
   }).catch( error => {
-    res.stats(500).json({error: "The posts information could not be retrieved."})
+    res.status(500).json({error: "The posts information could not be retrieved.", error: error})
   })
 })
 
 server.get('/api/post/:id', (req, res) => {
+  const { id } = req.params;
 
+  db.findById(id)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post)
+      } else {
+        res.status(404).json({ message: "The post with the specified ID does not exist."})
+      }
+    })
+    .catch( error => {
+      res.status(500).json({})
+    })
 })
 
 server.listen(8000)
