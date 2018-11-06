e7765eb9e375321400111dd18c52d55efc436f2f
Peter Pham <phamhere@gmail.com>
2018-11-06 14:56:17 -0700

    POST functionality

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const db = require("./data/db.js");
 // add your server code starting here
 const server = express();
 
+server.use(express.json());
 server.use(cors());
 
 server.get("/api/posts", (req, res) => {
@@ -39,5 +40,49 @@ server.get("/api/posts/:id", (req, res) => {
         .json({ error: "The post information could not be retrieved." });
     });
 });
+
+server.post("/api/posts", (req, res) => {
+  const { title, contents } = req.body;
+  if (title && contents) {
+    db.insert(req.body)
+      .then(postId => {
+        db.findById(postId.id).then(post => res.status(201).json(post));
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: "There was an error while saving the post to the database."
+        });
+      });
+  } else {
+    res
+      .status(400)
+      .json({ message: "Please provide title and contents for the post." });
+  }
+});
+
+server.put("/api/posts/:id", (req, res) => {
+  const { title, contents } = req.body;
+  if (title && contents) {
+    db.update(req.params.id, req.body)
+      .then(count => {
+        count
+          ? db.findById(req.params.id).then(post => res.send(200).json(post))
+          : res.status(404).json({
+              message: "The post with the specified ID does not exist."
+            });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: "The post information could not be modified." });
+      });
+  } else {
+    res
+      .status(400)
+      .json({ message: "Please provide title and contents for the post." });
+  }
+});
+
+server.delete("/api/posts/:id", (req, res) => {});
 
 server.listen(8000, () => console.log("API running on port 8000"));
