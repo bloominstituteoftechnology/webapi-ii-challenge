ada89c8e37f1b11063c979009bc119742873183a
TRIF3X <thewizardofcrypto@gmail.com>
2018-11-06 14:40:32 -0600

    delete in db now working

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 const express = require('express')
 const db = require('./data/db.js');
 const server = express();
-// add your server code starting here
 
+server.use(express.json());
+// add your server code starting here
 
 server.get('/api/posts', (req, res) => {
     db.find()
@@ -21,9 +22,9 @@ server.get('/api/posts/:id', (req, res) => {
     const { id } = req.params;
   
     db.findById(id)
-      .then(user => {
-        if (user) {
-          res.status(200).json(user);
+      .then(post => {
+        if (post) {
+          res.status(200).json(post);
         } else {
           res.status(404).json({ message: 'user not found' });
         }
@@ -35,5 +36,35 @@ server.get('/api/posts/:id', (req, res) => {
       });
   });
 
+server.post('/api/posts', (req, res) => {
+  console.log(req.body)
+  const post = req.body
+  db.insert(post)
+    .then(userId => {
+      if (!req.body.title || !req.body.contents === '') {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+        return;
+      } else {
+      res.status(201).json(userId)
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: "There was an error while saving the post to the database", err })
+    })
+})
+
+server.delete('/api/posts/:id', (req, res) => {
+  db.remove(req.params.id)
+    .then(userId => {
+      if(userId) {
+        res.status(200).json(req.body)
+      } else {
+        res.status(404).json({ message: "The post with the specified ID does not exist." })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: "The post could not be removed", err })
+    })
+})
 
 server.listen(9000)
