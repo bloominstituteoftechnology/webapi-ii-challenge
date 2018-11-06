8bae0a7db1ff67e0136453721d91824fe8857e14
Unknown <zachary.arney@gmail.com>
2018-11-06 17:41:11 -0600

    Create (post) new post, minor formatting

--- index.js
+++ index.js
@@ -19,15 +19,34 @@ server.get('/api/posts', (req, res) => {
 
 server.get('/api/posts/:id', (req, res) => {
   const { id } = req.params;
-  db.findById(id).then(post => {
+  db.findById(id).then((post) => {
     if (post.length) {
-      console.log("If true", post)
+      console.log('If true', post);
       res.status(200).json({ post: post });
     } else {
-      console.log("If flaes", post)
-      res.status(404).json({ message: "The post with the specified ID does not exist." })
+      console.log('If flaes', post);
+      res
+        .status(404)
+        .json({ message: 'The post with the specified ID does not exist.' });
     }
-  })
+  });
+});
+
+server.post('/api/posts', async (req, res) => {
+  console.log('BODY', req);
+  const postData = req.body;
+  try {
+    const postId = await db.insert(postData);
+    const post = await db.findById(postId.id);
+    res.status(201).json(post);
+  } catch (err) {
+    let message = 'error creating post';
+    if (err.errno === 19) {
+      message = 'Please provide title and contents for the post.';
+    }
+    res.status(400).json({ message, err });
+  }
+});
 })
 
 server.listen(9000, () => {
