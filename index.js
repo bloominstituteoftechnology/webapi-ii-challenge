9f39069e0b327187e522147951d53976e16724e8
Kat Fries <katherynejf@gmail.com>
2018-11-06 08:08:17 -0500

    minor changes

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 // 3. nodemon index.js (or current file, e.g., nodemon server.js)
 const express = require('express');
 
-const db = require('./data/db.js');
+const db = require('./data/db.js'); // CommonJS for Node.js
+// import db from './data/db.js'    // ES6 imports
 const server = express();
 
 
@@ -11,7 +12,7 @@ const server = express();
 // req is a requestHandler
 server.get('/', (req, res) => {
   // console.log(db.find());
-  res.send('<h2>Welcome to the Home Page (aka localhost:8000)</h2><p>To see all posts: localhost:8000/api/posts</p></h2><p>To see a specific post by id (e.g., id = 1): localhost:8000/api/posts/1</p>')
+  res.send('<h2>API running on localhost:8000</h2><p>To see all posts: localhost:8000/api/posts</p></h2><p>To see a specific post by id (e.g., id = 1): localhost:8000/api/posts/1</p>')
 })
 
 server.get('/api/posts', (req, res) => {
@@ -27,15 +28,16 @@ server.get('/api/posts', (req, res) => {
 })
 
 server.get('/api/posts/:id', (req, res) => {
-  const id = req.params.id;
+  const id = req.params.id;     // const { id } = req.params
   console.log('params:', req.params); // params: { id: '1' }
 
   db.findById(id)
-    .then(posts => {
-      console.log(posts) // logs id, title, contents, create_at, updated_at
-      !(posts.length) 
+    .then(post => {
+      console.log(post) // logs id, title, contents, create_at, updated_at
+      // !(post.length) // fixed issue of empty array
+      !(post.includes(post[0])) // alternative to !(post.length)
         ? res.status(404).json({ message: "The post with the specified ID does not exist."})
-        : res.status(200).json(posts);
+        : res.status(200).json(post[0]);
     })
     .catch(err => {
       res.status(500).json({ 
