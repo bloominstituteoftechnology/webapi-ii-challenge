30d8904da9f8472dfb017ce6b76574f13eca97c3
sean-one <basement365@gmail.com>
2018-11-06 12:17:09 -0800

    added delete call

--- index.js
+++ index.js
@@ -49,4 +49,20 @@ server.get('/api/posts/:id', (req, res) => {
         })
 })
 
+server.delete('/api/posts/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const count = await db.remove(id);
+        if(count === 0) {
+            res.status(404).json({ message: 'The post with the specified ID does not exist.'})
+        } else {
+            res.status(200).json(count)
+        }
+
+    } catch (error) {
+        res.status(500).json({ message: 'The post could not be removed'})
+    }
+
+})
+
 server.listen(port, () => console.log(`we are listening on port ${port}...`))
