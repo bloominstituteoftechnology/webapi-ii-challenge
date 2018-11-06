43a245cb461e39a877dd1cab3e00350236295f7e
TRIF3X <thewizardofcrypto@gmail.com>
2018-11-05 20:09:29 -0600

    serverGet complete

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
 // import your node modules
-
+const express = require('express')
 const db = require('./data/db.js');
-
+const server = express();
 // add your server code starting here
+
+
+server.get('/api/posts', (req, res) => {
+    db.find()
+        .then(users => {
+            res.status(200).json(users)
+        })
+        .catch(err => {
+            res
+              .status(500)
+              .json({ error: 'The posts information could not be retrieved' });
+          });
+})
+
+server.get('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+  
+    db.findById(id)
+      .then(user => {
+        if (user) {
+          res.status(200).json(user);
+        } else {
+          res.status(404).json({ message: 'user not found' });
+        }
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ message: "The post with the specified ID does not exist.", error: err });
+      });
+  });
+
+
+server.listen(9000)
