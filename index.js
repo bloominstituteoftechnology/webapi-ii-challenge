1549f176d08f12f78ae2ef9dc4c099ba8ff4fee5
Brandon Strellis <bstrellis@gmail.com>
2018-11-06 18:08:39 -0500

    Added post, put, delete functionality. Next: add conditional functionality to handle nonexistent posts.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const db = require('./data/db.js');
 // add your server code starting here
 
 const server = express();
+server.use(express.json());
 
 server.get('/api/posts', (req, res) => {
     db.find()
@@ -13,7 +14,7 @@ server.get('/api/posts', (req, res) => {
             res.status(200).json(posts);
         })
         .catch(err => {
-            res.status(500).json({ message: 'Sorry'})
+            res.status(500).json({ message: 'The posts information could not be retrieved.'})
         });
    
 });
@@ -22,7 +23,6 @@ server.get('/api/posts/:id', (req, res) => {
     const { id } = req.params;
     db.findById(id)
         .then(post => {
-            console.log('post',post);
             res.status(200).json(post);
         })
         .catch(err => {
@@ -30,19 +30,48 @@ server.get('/api/posts/:id', (req, res) => {
         });
 });
 
+server.post('/api/posts', async (req, res) => {
+    try {
+        const postData = req.body;
+        const postId = await db.insert(postData);
+        const post = await db.findById(postId.id);
+        res.status(201).json(post);
+    } catch(error) {
+        res.status(500).json({ message: error})
+    }
+
+});
+
+server.put('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    const changedPost = req.body;
+    db.update(id, changedPost)
+        .then(count => {
+            if (count) {
+                res.status(200).json({ message: `${count} posts updated`});
+            } else {
+                res.status(404).json({ message: 'post not found'});
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'error updating the post'});
+        });
+});
+
+server.delete('/api/posts/:id', (req, res) => {
+    db.remove(req.params.id)
+        .then(count => {
+            res.status(200).json(count);
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'error deleting post'});
+        });
+});
+
+
 server.listen(8000, () => console.log('API running on port 8000'));
 
 
 
 
-// server.get("/api/users", (req, res) => {
-//     db.find()
-//         .then(users => {
-//             res.status(200).json(users);
-//         })
-//         .catch(err => {
-//             res
-//                 .status(500)
-//                 .json({ message: "Sorry"})
-//         })
-// })
+
