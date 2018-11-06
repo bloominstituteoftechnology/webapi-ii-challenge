faa79474d8f4d87eeedb31cca161afebf0faf4b4
unknown <quan.nguyen199712@gmail.com>
2018-11-06 18:25:40 -0500

    added delete and get by id

--- index.js
+++ index.js
@@ -22,6 +22,22 @@ server.get('/api/posts',(req, res)=>{
 server.get('/', (req, res)=>{
     res.json('works')
 })
+server.get('api/posts/:id', (req, res)=>{
+    const {id}= req.params;
+    db.findById(id)
+        .then(post=>{
+            if(post){
+                res.status(200).json(post);
+            }else {
+                res.status(404).json({message: 'user not found'})
+            }
+        })
+        .cath(error=>{
+            res
+                .status(500)
+                .json({message: 'cant fetch post'})
+        })
+})
 server.post('/api/posts', async (req, res)=>{
     console.log('body', req.body);
     try {
@@ -32,9 +48,21 @@ server.post('/api/posts', async (req, res)=>{
     } catch (error) {
         let message='error creating the post';
         if(error.errno===19){
-            message= 'please provide both the name and the bio';
+            message= 'please provide both the title and the content';
         }
         res.status(400).json({message, error})
     }
+})
+server.delete('/api/posts/:id', (req, res)=>{
+    if(findById(req.params.id)===null){
+        res.status(404).json({message:'The post with the specific ID does not exist'})
+    }
+    db.remove(req.params.id)
+        .then(count=>{
+            res.status(200).json(count);
+        })
+        .catch(error =>{
+            res.status(500).json({message:'error deleting user'})
+        })
 })
 server.listen(8000,()=>console.log('API Running on port 8000') )
