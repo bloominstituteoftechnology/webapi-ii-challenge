c892ad1abfdba581c1608ab07288a11f81e93706
David247365 <ds051128@gmail.com>
2018-11-06 15:22:40 -0600

    completed post and made sure to cancel request if there is not tile or content in body of post

--- index.js
+++ index.js
@@ -3,10 +3,12 @@
 const express = require("express");
 const server = express();
 const cors = require("cors");
+const db = require("./data/db.js");
 
 server.use(cors());
 
-const db = require("./data/db.js");
+// middleware
+server.use(express.json()); // teaches express how to parse the JSON request body
 
 // add your server code starting here
 
@@ -48,4 +50,73 @@ server.get("/api/posts/:id", (req, res) => {
     });
 });
 
-server.listen(3002, () => console.log("Server Here"));
+// POST
+
+// server.post("/api/posts", async (req, res) => {
+//   console.log("body", req.body);
+//   try {
+//     const postData = req.body;
+//     const postId = await db.insert(postData);
+//     const post = await db.findById(postId.id);
+//     res.status(201).json(post);
+//     console.log(req.body);
+//   } catch (error) {
+//     if (req.body.title === "" && req.body.contents === "") {
+//       res.status(400).json({ message = "please provide info"});
+//     }else{
+//     res.status(500).json({ message: "error creating user", error });
+//   }}
+// });
+
+server.post("/api/posts", (req, res) => {
+  console.log("body", req.body);
+  const postData = req.body;
+  db.find(postData)
+    .then(postId => {
+      if (error.errno === 19) {
+        res
+          .status(400)
+          .json({ message: "Please provide title and contents for the post." });
+      } else {
+        res.status(201).json(postId);
+        return db.insert(postData);
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: "error creatin post" });
+    });
+});
+
+// Delete
+
+server.delete("/api/posts/:id", (req, res) => {
+  db.remove(req.params.id)
+    .then(count => {
+      res.status(200).json(count);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "error deleting post" });
+    });
+});
+
+// PUT
+
+server.put("/api/posts/:id", (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  db.update(id, changes)
+    .then(count => {
+      if (count) {
+        res.status(200).json({ message: `${count} post(s) updated` });
+      } else {
+        res.status(404).json({ message: "post not found" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "error with updating post", error: err });
+    });
+});
+
+server.put("api/users/:id", (req, res) => {});
+
+server.listen(3002, () => console.log("\n== Server Here ==\n"));
