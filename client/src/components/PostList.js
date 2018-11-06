6abe032c0271d88500903d869f28bc85ca542330
David247365 <ds051128@gmail.com>
2018-11-05 19:40:04 -0600

    just added in more styling

--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -4,7 +4,16 @@ const PostList = props => {
   return (
     <div>
       {props.post.map(p => {
-        return <h1>{p.title}</h1>;
+        return (
+          <div className="quote-card">
+            <h2>{p.title}</h2>
+            <p>{p.contents}</p>
+            <ul className="dates">
+              <li>Created at: {p.created_at}</li>
+              <li>Updated at: {p.updated_at}</li>
+            </ul>
+          </div>
+        );
       })}
     </div>
   );
