cd42dae6124197842637b231c2726f0881b97cad
txiong000 <tkx422@gmail.com>
2018-11-06 02:38:24 -0600

    Toua's node express lab, done with get request

---
+++ greeter.js
@@ -0,0 +1,6 @@
+const greeter = (req, res) => {
+    const person = req.params.person;
+    res.json({ hello: person})
+}
+
+module.exports = greeter;
--- index.js
+++ index.js
@@ -1,5 +1,38 @@
 // import your node modules
 
 const db = require('./data/db.js');
-
+const express = require('express')
 // add your server code starting here
+const server = express();
+const greeter = require('./greeter.js')
+
+server.get('/', (req, res) => {
+    res.json('alive')
+})
+
+server.get('/greet/:person', greeter)
+
+server.get('/api/posts', (req, res) => {
+    db.find().then(users => {
+        res.status(200).json(users);
+    }).catch(err => {
+        res.status(500).json({message: "we failed you, can't get the users"})
+    })
+})
+
+server.get('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    db.findById(id).then(user => {
+        console.log("user", user)
+        if (user.length > 0) {
+            res.status(200).json(user);
+        } else {
+            res.status(404).json({ message: 'user not found'});
+        }
+    }).catch(err => {
+        res.status(500).json({ message: "we failed you, can't get the user"})
+    })
+
+}) 
+
+server.listen(9000, ()=> console.log('the server is alive!'))
