9e40b401b6e3fe4c6400e61c7a551cd4c4f9f794
Shawn Antonucci <shawnantonucci2018@gmail.com>
2018-11-06 12:55:06 -0800

    added update

--- index.js
+++ index.js
@@ -49,32 +49,51 @@ server.post("/api/posts", (req, res) => {
       db.getById(postId.id)
         .then(post => {})
         .catch(error => {
-          res
-            .status(500)
-            .json({
-              error: "There was an error while saving the post to the database"
-            });
+          res.status(500).json({
+            error: "There was an error while saving the post to the database"
+          });
         });
     })
     .catch(error => {
-      res
-        .status(400)
-        .json({
-          errorMessage: "Please provide title and contents for the post."
-        });
+      res.status(400).json({
+        errorMessage: "Please provide title and contents for the post."
+      });
     });
 });
 
-server.delete('/api/posts/:id', (req, res) => {
-    db.remove(req.params.id).then(count => {
-        if (count) {
-            res.status(200).json(count);
-        } else  {
-            res.status(404).json({ message: "The post with the specified ID does not exist." })
-        }
-    }).catch(error => {
-        res.status(500).json({ error: "The post could not be removed" })
+server.delete("/api/posts/:id", (req, res) => {
+  db.remove(req.params.id)
+    .then(count => {
+      if (count) {
+        res.status(200).json(count);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error: "The post could not be removed" });
     });
-})
+});
+
+server.put("/api/posts/:id", (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  db.update(id, changes)
+    .then(count => {
+      if (count) {
+        res.status(200).json(count);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+      res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    })
+    .catch(error => {
+        res.status(500).json({ error: "The post information could not be modified." });
+    });
+});
 
 server.listen(8000, () => console.log("API running on port 8000"));
