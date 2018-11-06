3f1eed5eb98970054f5b74db1bdb9dfd5db2ff9e
SavannahGreuel <smallidgesavannah@gmail.com>
2018-11-06 14:29:34 -0700

    added post functionality

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const db = require('./data/db.js');
 const server = express();
 var cors = require ('cors')
 server.use(cors());
+server.use(express.json())
 
 
 // add your server code starting here
@@ -30,6 +31,17 @@ server.get('/api/posts/:id', (req, res) => {
     .catch (err => {
         res.status(500).json({error: "The post information could not be retrieved. "})
     })
+})
+
+//post new saying 
+server.post('/api/posts', async (req, res) => {
+    try {
+        const userBody = req.body;
+        const userId = await db.insert(userBody);
+        res.status(201).json(userId);
+    } catch (error) {
+        res.status(500).json({ message: 'error creating post:', error})
+    }
 })
 
 server.listen(8000, () => console.log( 'This server is alive and breathing!'))
