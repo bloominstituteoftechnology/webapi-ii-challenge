041bbaeb58a53192ee8563d9149c66983f47d75c
txiong000 <tkx422@gmail.com>
2018-11-06 14:05:11 -0600

    post working

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const express = require('express')
 const server = express();
 const greeter = require('./greeter.js')
 
+server.use(express.json()); // teach express how to parse the  JSON request body
+
 server.get('/', (req, res) => {
     res.json('alive')
 })
@@ -33,6 +35,43 @@ server.get('/api/posts/:id', (req, res) => {
         res.status(500).json({ message: "we failed you, can't get the user"})
     })
 
-}) 
+})
+
+server.post('/api/posts', async (req, res) => {
+    console.log('body:', req.body)
+    try {
+        const userData = req.body;
+        const userId = await db.insert(userData);
+        const user = await db.findById(userId.id)
+        res.status(201).json(user)
+    } catch (error) {
+        res.status(500).json({ message: 'error creating user', error})
+    }
+})
+
+server.delete('/api/posts/:id', (req, res) => {
+    db.remove(req.params.id).then(count => {
+        res.status(200).json(count);
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'error no user found', err})
+    })
+})
+
+server.put('api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+    db.update(id, changes)
+    .then(count => {
+        if(count) {
+            res.status(200).json({ message: `${count} user updated`});
+        } else {
+            res.status(404).json({ message: 'user not found'})
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'error updating the user'})
+    })
+})
 
 server.listen(9000, ()=> console.log('the server is alive!'))
