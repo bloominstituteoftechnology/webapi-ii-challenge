7d4f560800573918b334382c597253f210d4a4fe
Matthew Wright <matthewbwright@gmail.com>
2018-11-06 14:10:47 -0700

    adds POST, DELETE, and PUT methods

--- index.js
+++ index.js
@@ -45,4 +45,47 @@ server.get('/api/posts/:id', (req, res) => {
     });
 });
 
+server.post('/api/posts', async (req, res) => {
+  try {
+    const postData = req.body;
+    const postId = await db.insert(postData);
+    res.status(201).json(postId);
+  } catch (error) {
+    res.status(500).json({
+      message: 'There was an error while saving the post to the database'
+    });
+  }
+});
+
+server.delete('/api/posts/:id', (req, res) => {
+  db.remove(req.params.id)
+    .then(count => {
+      res.status(200).json(count);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'The post could not be removed' });
+    });
+});
+
+server.put('/api/posts/:id', (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  db.update(id, changes)
+    .then(count => {
+      if (count) {
+        res.status(200).json({ message: `${count} users updated` });
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID doesn't exist" });
+      }
+      res.status(200).json(count);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ message: 'The post information could not be modified' });
+    });
+});
+
 server.listen(5800, () => console.log('this is the server'));
