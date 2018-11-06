5a9c4ac8c9d812c7db5f3acbf4910ecbefbb8cf7
Yanrong Xu <xuyanrong@icloud.com>
2018-11-06 17:38:18 -0500

    got the put request work

--- index.js
+++ index.js
@@ -71,6 +71,25 @@ server.delete('/api/posts/:id', (req, res) => {
         })
 })
 
+server.put('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    const post = req.body;
+    db.update(id, post)
+        .then(count => {
+            if (post.title === '' || post.contents === '') {
+                res.status(400).json({errorMessage: "Please provide title and contents for the post."})
+            }
+            if (count) {
+                res.status(200).json(post);
+            }
+            else {
+                res.status(404).json({message: "The post with the specified ID does not exist."})
+            }
+        })
+        .catch(err => {
+            res.status(500).json({error: "The post information could not be modified."})
+        })
+})
 
 
 server.listen(9000, () => console.log('server is runner'));
