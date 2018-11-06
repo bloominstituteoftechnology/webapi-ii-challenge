161bf2b2e791562e25658553f3a20f635f767b1d
Katie Cleary <katiecleary@airmaster.lan>
2018-11-06 17:38:23 -0500

    wrote delete logic with .then and .catch

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ server.put('/api/posts', async (req, res) => {
            const errorMessage = "Please provide title and contents for the post"; 
          throw res.status(400).json({ errorMessage, error });
          } else if (!post.length) { // or oops - if we could retrieve it, we would but it's not here, status 404
-         res.status(404).json({ message: "The post with the specified ID does not exist." });
+         throw res.status(404).json({ message: "The post with the specified ID does not exist." });
        }
     } catch (error) {
         res.status(500).json({ error: "The post information could not be modified." });
@@ -47,20 +47,20 @@ server.put('/api/posts', async (req, res) => {
 
 //----- DELETE -----
 
-server.delete('/api/posts', (req, res) => {
-    res.status(204);
-});
-
-//----- DELETE -----
-
 server.delete('/api/posts/:id', (req, res) => {
-    const id = req.params.id;
-    // or we could destructure it like so: const { id } = req.params;
-    res.status(200).json({
-      url: `/api/posts/${id}`,
-      operation: `DELETE for post with id ${id}`,
-    });
-  });
+    db.remove(req.params.id)
+        .then(post => {
+        if (post && post.length) {
+         res.status(200).json(post);
+        } else { // or oops - if we could retrieve it, we would but it's not here, status 404
+        res.status(404).json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+        .catch(err => {
+         res.status(500).json({ error: "The post could not be removed" });
+        });
+      });
+      
 
 //----- GET -----
 
@@ -95,27 +95,15 @@ server.get('/api/posts/:id', (req, res) => {
       });
   });
 /*
-When the client makes a PUT request to /api/posts/:id:
+When the client makes a DELETE request to /api/posts/:id:
 
 If the post with the specified id is not found:
-THROW
+
 return HTTP status code 404 (Not Found).
 return the following JSON object: { message: "The post with the specified ID does not exist." }.
-If the request body is missing the title or contents property:
-THROW
-cancel the request.
-respond with HTTP status code 400 (Bad Request).
-return the following JSON response: { errorMessage: "Please provide title and contents for the post." }.
-If there's an error when updating the post:
-CATCH 
+If there's an error in removing the post from the database:
+
 cancel the request.
 respond with HTTP status code 500.
-return the following JSON object: { error: "The post information could not be modified." }.
-If the post is found and the new information is valid:
-
-TRY
-update the post document in the database using the new information sent in the request body.
-return HTTP status code 200 (OK).
-return the newly updated post.
-
+return the following JSON object: { error: "The post could not be removed" }.
 */
