6a9be2be7dc04d8aea8a3a655228a15214f8b39f
Kat Fries <katherynejf@gmail.com>
2018-11-06 16:51:50 -0500

    able to post posts

--- NOTES.md
+++ NOTES.md
@@ -1,3 +1,51 @@
+# Build a RESful API using Express and Node.js
+
+What is an API? An API is a server software that publishes a set of endpoints that 
+
+## HTTP
+### Methods: POST, GET, PUT DELETE
+    POST:   Create  
+    GET:    Read    .find() reads all items or .findById(id) reads 1 item 
+    PUT:    Update    
+    DELETE: Delete  
+
+### Status Codes
+    200s SUCCESS
+      200 OK
+      201 CREATED
+    300s REDIRCTS
+    400s USER/CLIENT ERRORS
+      400 BAD REQUEST
+      404 NOT FOUND
+    500s SERVER ERRORS
+      500 SERVER ERROR
+
+## Initialize project with npm init
+  Heavier Install // Lighter Yarn
+  npm init        // yarn init
+  npm init -y     // yarn init -y
+
+  Check package.json      // yarn
+  Check package.npm.json
+
+### Adding Dependencies: Nodemon, Live-Server
+
+  Check npmjs.com 
+  check last updated
+  check weekly downloads
+  check number of open issues
+
+  local vs global packages (development only)
+  Some dependencies are for development only (not during production), e.g., nodemon
+  yarn add nodemon --dev
+
+  yarn add live-server -D
+
+### More About package.json
+    "main" is main webpage, e.g., index.js
+    "scripts"
+    "devDependencies" lists what dependencies have been added (e.g., nodemon, live-server, etc.)
+
 1. What is Node.js?
 - It is a way to run JS outside of the browser.
 
--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const db = require('./data/db.js'); // CommonJS for Node.js
 // import db from './data/db.js'    // ES6 imports
 const server = express();
 
+server.use(express.json()); //teaches express how to parse the JSON request body   -- express automatically uses body-parser underneath
+// needed for posts (but not gets)
+
 
 // root of our site
 // req is a requestHandler
@@ -15,6 +18,7 @@ server.get('/', (req, res) => {
   res.send('<h2>API running on localhost:8000</h2><p>To see all posts: localhost:8000/api/posts</p></h2><p>To see a specific post by id (e.g., id = 1): localhost:8000/api/posts/1</p>')
 })
 
+// GET ALL POSTS
 server.get('/api/posts', (req, res) => {
   db.find()
     .then(posts => {
@@ -27,6 +31,7 @@ server.get('/api/posts', (req, res) => {
     })
 })
 
+// GET POSTS BY ID
 server.get('/api/posts/:id', (req, res) => {
   const id = req.params.id;     // const { id } = req.params
   console.log('params:', req.params); // params: { id: '1' }
@@ -34,8 +39,7 @@ server.get('/api/posts/:id', (req, res) => {
   db.findById(id)
     .then(post => {
       console.log(post) // logs id, title, contents, create_at, updated_at
-      // !(post.length) // fixed issue of empty array
-      !(post.includes(post[0])) // alternative to !(post.length)
+      !(post.length) // fixed issue of empty array
         ? res.status(404).json({ message: "The post with the specified ID does not exist."})
         : res.status(200).json(post[0]);
     })
@@ -45,6 +49,27 @@ server.get('/api/posts/:id', (req, res) => {
         error: err })
     })
 })
+
+// POST
+server.post('/api/posts', async (req, res) => {
+  console.log('req:', req)
+  // console.log('body:', req.body);
+  try {
+    const userData = req.body;
+    console.log(userData);
+    const userId = await db.insert(userData);
+    const user = await db.findById(userId.id)
+    res.status(201).json(user)
+  } catch (error) {
+    res.status(500).json({ message: 'error creating post' })
+  }
+})
+
+
+
+
+
+
 
 
 server.listen(8000, () => {console.log('API running on port 8000')})
