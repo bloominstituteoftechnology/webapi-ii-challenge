7f86a937cebb5c95cfdc3004a956eb468370f9d8
bummings <eabummings@gmail.com>
2018-11-06 21:51:14 -0500

    added throw for post with title or contents

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ server.use(express.json());
 
 const db = require("./data/db.js");
 
-// add your server code starting here
+// G E T
 
 server.get("/", (req, res) => {
-  res.status(200).send("sup");
+  res.status(200).send("<h1><em>sup</em></h1>");
 });
 
 server.get("/api/posts", (req, res) => {
@@ -33,52 +33,42 @@ server.get("/api/posts/:id", (req, res) => {
     });
 });
 
-// Post
-// server.post("/api/posts", (req, res) => {
-//   const { title, contents } = req.body;
-//   const newPost = { title, contents };
-//   db.insert(newPost)
-//     .then(postId => {
-//       const { id } = postId;
-//       if (!post) {
-//         return res.status(422).send({
-//           error: "There was an error while saving the post to the database"
-//         });
-//       }
-//       db.findById(id).then(post => {
-//         res.status(201).json(post);
-//       });
-//     })
-//     .catch(err => console.log(err));
-// });
-
+// P O S T
 server.post("/api/posts", async (req, res) => {
   try {
     const postData = req.body;
     const postId = await db.insert(postData);
+    console.log("post title:", postData.title);
+
+    if (!postData.title || !postData.contents) {
+      return res.status(400).json({
+        errorMessage: "Please provide title and contents for the post."
+      });
+    }
+
     res.status(201).json(postId);
   } catch (error) {
     res.status(500).json({ message: "error creating post" });
   }
 });
 
-// Update
+// P U T
 server.put("/api/posts/:id", (req, res) => {
   const userData = req.body;
   db.update(req.params.id, userData)
-    .then(count => {
-      res.status(200).json(count);
+    .then(post => {
+      res.status(200).json(post);
     })
     .catch(err => {
       res.status(500).json({ message: "error updating post" });
     });
 });
 
-// delete
+// D E L E T E
 server.delete("/api/posts/:id", (req, res) => {
   db.remove(req.params.id)
     .then(post => {
-      res.status(200).json(post);
+      res.status(204).json(post);
     })
     .catch(error => {
       res.status(500).json({ error: "The post could not be removed" });
