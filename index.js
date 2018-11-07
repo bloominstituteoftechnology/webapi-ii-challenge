18d7528e7d6fee4a5750b1fa776aef40e2cfc6df
Brian G <brian.gregorius@gmail.com>
2018-11-06 19:02:08 -0500

    minor formating

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ server.get('/api/posts', (req, res) => {
     db.find()
         .then(users => {
             res.json(users);
-        }).catch(err => {
+        })
+        .catch(err => {
             res.status(500).json({ message: 'The posts information could not be retrieved.' });
         });
 });
