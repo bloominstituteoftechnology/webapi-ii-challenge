510d9014e946f73afa3978f47e98ec7eaea9b488
Jordan Stoddard <jjsincorporated@gmail.com>
2018-11-06 23:02:58 -0800

    logic changes on error handlers.

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
 const Post = props => {
-    const {title, contents} = props.post
+    const {title, contents, id} = props.post
     return (
         <div className="LOTRCard">
-            <h1>LOTR Trivia Card</h1>
+            <h1>LOTR Trivia Card {id}</h1>
             <section>
                 <p>Challenge: {contents}</p>
                 <h2>Quote: {title}</h2>
