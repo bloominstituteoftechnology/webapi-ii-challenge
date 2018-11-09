5a2f277a5d9540fbabb145192d8c9c37a147bdb9
SabrinaGear <sabrinagear.ft@gmail.com>
2018-11-08 19:43:08 -0600

    debugging

---
+++ client/build/precache-manifest.27cafc7e66fc600768e733c616131abb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3ac3287506a5d65aeddfff1b6dd7364",
+    "url": "/static/media/words.f3ac3287.jpeg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1b571da0c79fd30195c6",
+    "url": "/static/js/main.1b571da0.chunk.js"
+  },
+  {
+    "revision": "e881f1f5d68f97035eed",
+    "url": "/static/js/1.e881f1f5.chunk.js"
+  },
+  {
+    "revision": "1b571da0c79fd30195c6",
+    "url": "/static/css/main.a4a499c6.chunk.css"
+  },
+  {
+    "revision": "b608e7d8e68a7ce789c871e381526256",
+    "url": "/index.html"
+  }
+];
