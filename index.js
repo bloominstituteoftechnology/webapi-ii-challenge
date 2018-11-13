890151a2a9d2e73ec7cef761a18fc17ac3b04174
Angelo Deleon <rudy209@comcast.net>
2018-11-13 14:37:20 -0800

    Working on setting up requests

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 // import your node modules
 
 const db = require('./data/db.js');
+const express = require("express");
+const server = express();
 
 // add your server code starting here
 
-const express = require("express");
-const server = express();
+server.get("/", (req, res) => {
+  res.status(200).send("sup");
+});
+
+ server.get("/api/posts", (req, res) => {
+  db.find()
+  .then(posts => {
+    res.status(200).json(posts);
+  })
+  .catch(err => {
+    res.status(500).json({ message: "Error 500: Posts not found."
+  });
+});
+
+
+ server.get("/api/posts/:id", (req, res) => {
+  const { id } = req.params;
+
+
+   db.findById(id)
+    
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Error 500: Post not found." 
+    });
+});
+ server.listen(9000, () => console.log("server be runnin"));
