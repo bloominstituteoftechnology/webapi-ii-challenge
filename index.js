1ae9689bf893d5f22a2b64c5f8201eed71bf57b5
Troy Amlee <troyamlee19@gmail.com>
2018-11-13 17:19:01 -0600

    [mvp day 1] get requests successful

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
 // import your node modules
+const express = require('express');
+const server = express();
 
 const db = require('./data/db.js');
 
+server.get('/api/posts', (req, res) => {
+    db.find()
+    .then(posts => {
+        res.status(200).json(posts)
+    })
+    .catch(err => {
+        res
+        .status(500).json({message: "The posts information could not be retrieved foo" })
+    })
+});
+
+server.get('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+
+    db.findById(id)
+    .then(post => {
+        if (!post) {
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        } else {res.status(200).json(post)}
+    })
+    .catch(err => {
+        res
+        .status(500).json({ error: "The post information could not be retrieved." })
+    })
+})
 // add your server code starting here
+
+server.listen(9000, () => console.log("Wussup Buttercup"));
