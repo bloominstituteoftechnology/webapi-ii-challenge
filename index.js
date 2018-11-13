8175cfa8025e7759a64d96e70e2d0d57cc8c92c0
Barbara Hunter <commandandcode@gmail.com>
2018-11-13 16:01:11 -0500

    express bug

--- index.js
+++ index.js
@@ -2,4 +2,10 @@
 
 const db = require('./data/db.js');
 
+const express = require('express');
+
 // add your server code starting here
+
+const server = express();
+
+server.listen(9000, () => console.log('hello world'));
