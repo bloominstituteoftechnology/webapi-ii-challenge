bcc67002ada9e802ad773c2a017b78c71a59306d
Lambda_School_Loaner_27 <lambda_school_loaner_27@Lambda-School-Loaner-27s-MacBook-Pro.local>
2018-11-13 18:03:24 -0600

    server is running

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 // import your node modules
+const express = require('express');
+const server = express();
 
 const db = require('./data/db.js');
 
 // add your server code starting here
+server.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+server.listen(7777, () => console.log('server is operational'));
