9e69a117d887fa0391c12f3de44f8a0a824eedfa
jaklundun <govi.escudero@gmail.com>
2018-11-14 15:19:56 -0800

    POST endpoint as well as response object and error handling middlewares now functional.

--- index.js
+++ index.js
@@ -7,6 +7,22 @@ const port = 8888;
 const server = express();
 server.use(express.json());
 
+
+//MIDDLEWARE (custom) for error handling
+const sendUserError = (status, message, res) => {
+    res.status(status).json({ errorMessage: message });
+    return;
+}
+
+//MIDDLEWARE (custom) for returning response object data
+const getbackPost = (id, res) => {
+    db.findById(id)
+    .then(user => {
+        res.json({ user })
+    })
+}
+
+//GET
 server.get('/api/posts', (req, res) => {
     db.find()
         .then(users => {
@@ -19,6 +35,7 @@ server.get('/api/posts', (req, res) => {
         })
 });
 
+//GET By ID
 server.get('/api/posts/:id', (req, res) => {
     const id = req.params.id;
     db.findById(id)
@@ -32,6 +49,26 @@ server.get('/api/posts/:id', (req, res) => {
         .catch(err => {
             console.log(err);
             sendUserError(500, "The post information could not be retrieved.", res);
+            return;
+        })
+});
+
+//POST
+
+server.post('/api/posts', (req, res) => {
+    const { title, contents } = req.body;
+    if (!title || !contents) {
+        sendUserError(400, 'Please provide title and content for the post.', res);
+        return;
+    }
+    db.insert({ title, contents })
+        .then(response => {
+            console.log(returnPost(response.id, res))
+            getbackPost(response.id, res);
+        })
+        .catch(err => {
+            console.log(err);
+            sendUserError(500, "There was an error while saving the post to the database.", res);
             return;
         })
 });
