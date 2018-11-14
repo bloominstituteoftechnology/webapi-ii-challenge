0edd1c358c544d9035915b9c87e7daa068e635e3
Lambda_School_Loaner_27 <lambda_school_loaner_27@Lambda-School-Loaner-27s-MacBook-Pro.local>
2018-11-13 21:35:35 -0600

    -get requests working -tried adding CORS middleware

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 // import your node modules
 const express = require('express');
+const cors = require('cors');
 const server = express();
+
+server.use(cors());
+server.use(express.json());
 
 const db = require('./data/db.js');
 
 // add your server code starting here
 server.get('/', (req, res) => {
-  res.send('Hello World');
+  res.json('Welcome to the server! Try adding: /api/posts');
 });
+
+server.get('/api/posts', (req, res) => {
+  db.find()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ message: "Sorry, we're having some trouble getting the posts..." })
+    })
+})
 
 server.listen(7777, () => console.log('server is operational'));
