fda150182d1c9e85c29034de5f7ceb5703bc530c
Lambda_School_Loaner_27 <lambda_school_loaner_27@Lambda-School-Loaner-27s-MacBook-Pro.local>
2018-11-13 22:00:02 -0600

    post function works

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const express = require('express');
 const cors = require('cors');
 const server = express();
 
-server.use(cors());
+server.use((request, response, next) => {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 server.use(express.json());
 
 const db = require('./data/db.js');
@@ -23,6 +27,17 @@ server.get('/api/posts', (req, res) => {
         .status(500)
         .json({ message: "Sorry, we're having some trouble getting the posts..." })
     })
-})
+});
+
+server.post('/api/posts', async (req, res) => {
+  console.log('body:', req.body);
+  try {
+    const postData = req.body;
+    const postId = await db.insert(postData);
+    res.status(201).json(postId);
+  } catch (error) {
+    res.status(500).json({ message: 'error creating post!!!', error });
+  }
+});
 
 server.listen(7777, () => console.log('server is operational'));
