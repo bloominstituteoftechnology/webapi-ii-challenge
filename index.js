0c47ea9198f0541789f15bc198b2b45e7adc7aa0
mikaelacurrier <mikaela.currier@gmail.com>
2018-11-13 19:07:25 -0500

    added update function and fixed some of the error codes

--- index.js
+++ index.js
@@ -7,17 +7,17 @@ const server = express();
 server.use(express.json());
 
 server.get('/api/posts', (req, res) => {
-    db.find().then(user => {
-        res.json(user)})
+    db.find().then(post => {
+        res.json(post)})
         .catch(err => {
             res.status(500).json({ message: "we failed"})
         });
 });
 server.get('/api/posts/:id', (req, res) => {
     const { id } = req.params;
-    db.findById(id).then(user => {
-        if(user) {
-            res.status(200).json(user)
+    db.findById(id).then(post => {
+        if(post) {
+            res.status(200).json(post)
         } else {
             res.status(404).json({ message: "No user found"})
         }
@@ -31,8 +31,33 @@ server.post('/api/posts', async (req, res) => {
         const user = await db.insert(userData);
         res.status(201).json(user);
     } catch (error) {
-        res.status(500).json({ message: "error creating the user"})
+        res.status(500).json({ error: "There was an error while saving the post to the database"})
     }
+});
+server.delete('/api/posts/:id', (req, res) => {
+    const {id} = req.params;
+    db.remove(id).then(count => {
+        if (count) {
+            res.status(200).json(count);
+        } else {
+            res.status(404).json({message: "The post with the specified ID does not exist." })
+        }
+    }).catch(err => {
+        res.status(500).json({error: "The post could not be removed."});
+    })
+});
+server.put('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    const postData = req.body;
+    db.update(id, postData).then(count => {
+        if(count) { 
+            res.status(200).json(count)}
+         else {
+            res.status(404).json({message: "The post with the specified ID does not exist."})
+        }
+    ).catch( err => {
+        res.status(500).json({ message: "Unable to update"});
+    })
 });
 
 server.listen(8000, () => 
