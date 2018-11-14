f8fb715d2bc5417d5a8238a5b271ab200bc09f87
jaklundun <govi.escudero@gmail.com>
2018-11-13 16:13:01 -0800

    Set up server and initial GET request.

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 // import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
-
 // add your server code starting here
+
+const port = 5000;
+const server = express();
+server.use(express.json());
+
+server.get('/api/posts', (req, res) => {
+    db.find()
+        .then(users => {
+            res.json({ users })
+        })
+
+});
+
+server.listen(port, () => console.log(`Server running on port ${port}`));
+
