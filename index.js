b6748ffc2ac482481f0c510a74e5aae339ce1a26
Phil Hart <phil@Phils-iMac.local>
2018-11-13 17:12:09 -0700

    MVP done

--- index.js
+++ index.js
@@ -54,8 +54,40 @@ server.post("/api/posts", async (req, res) => {
         res.status(201).json(post);
     } catch (error) {
         let message = "There was an error while saving the post to the database"
-        
-    }
-})
 
-server.listen(9000, () => console.log("\n== the server is alive! ==\n"));
+        if (error.errno === 19) {
+            message = "please provide both the title and the contents";
+          }
+          res.status(500).json({ message: message, error });
+        }
+    })
+
+    server.put("/api/posts/:id", (req, res) => {
+        const { id } = req.params;
+        const changes = req.body;
+        db.update(id, changes)
+          .then(count => {
+            if (count) {
+              res.status(200).json({ message: `${count} posts updated` });
+            } else {
+              res.status(404).json({ message: "post not found" });
+            }
+          })
+          .catch(err => {
+            res.status(500).json({ message: "error updating the post", err });
+          });
+      });
+      
+      server.delete("/api/posts/:id", (req, res) => {
+        console.log(req.params.id);
+        db.remove(req.params.id)
+          .then(count => {
+            res.status(200).json(count);
+          })
+          .catch(err => {
+            res.status(500).json({ message: "error deleting post", err });
+          });
+      });
+      
+      server.listen(8000, () => console.log("\n== the server is alive! ==\n"));
+      
