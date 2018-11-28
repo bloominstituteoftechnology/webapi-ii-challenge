fb96024f286631d5eff13dfc0e1a38666904d05f
Bruce Cabanayan <bcabanayan@Unknown.domain>
2018-11-27 21:58:34 -0700

    Imported express, created server and initiated listen function

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 // import your node modules
 
 const db = require('./data/db.js');
+const express = require('express');
 
 // add your server code starting here
+
+const PORT = 4001;
+const server = express();
+
+server.listen(PORT, err => {
+    console.log(`server is running`)
+});
