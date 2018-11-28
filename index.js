d6259cdafae858cdefbade00d2ac787134ecc592
kammagik <kammagik@yahoo.com>
2018-11-27 21:18:50 -0800

    not sure if get id is working

--- index.js
+++ index.js
@@ -10,15 +10,35 @@ server.get('/', (req, res) => {
 
 server.get('/api/posts', (req, res) => {
     db.find()
-    .then(posts => {
-        res.status(200).json(posts);
+    .then((posts) => {
+        res.json(posts);
     })
     .catch( err => {
         res
         .status(500)
-        .json({message : 'post could not be found'})
+        .json({message : 'post could not be retrieved'})
     })
 }) 
+
+server.get('api/posts/:id', (req, res) => {
+    const {id} = req.params;
+    db.findById(id)
+    .then( post => {
+        if(post){
+            res.json(post);
+        }
+        else {
+            res
+            .status(404)
+            .json({message: 'post does not exist'});
+        }
+    } )
+    .catch( err => {
+        res
+        .status(500)
+        .json({message : 'post could not be retrieved'})
+    })
+})
 
 server.listen(4000, () => {
     console.log('server is running');
