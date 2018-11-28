f0494d3fa8fb8aec012bdb93decfe5e8fa634fd4
Matthew Meitl <darksamus6@gmail.com>
2018-11-28 09:29:29 -0600

    finished GET /api/posts/:id

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // import your node modules
 const express = require('express');
 const server = express();
+//server.disable("etag");
 
 const db = require('./data/db.js');
 
@@ -8,22 +9,38 @@ const db = require('./data/db.js');
 server.get('/api/posts', (req, res) => {
     db.find()
       .then((posts)=>{
+          
         res.json(posts);
       })
       .catch(err => {
-          res.status(500).json({ error: "The posts information could not be retrieved." })
+          res
+            .status(500)
+            .json({ error: "The posts information could not be retrieved." });
       });
     
 });
 
 server.get('/api/posts/:id', (req, res) => {
-    db.find()
-      .then()
-      .catch();
+    const { id } = req.params;
+    db.findById(id)
+      .then((post)=>{
+          if(post.length){
+            res.json(post);
+          } else {
+            res
+            .status(404)
+            .json({ message: "The post with the specified ID does not exist." });
+          }
+      })
+      .catch(err => {
+          res
+            .status(500)
+            .json({ error: "The post information could not be retrieved." });
+      });
     
 });
 
-server.listen(5000, () => {
-    console.log('Server running on http://localhost:5000')
+server.listen(3000, () => {
+    console.log('Server running on http://localhost:3000')
 });
 
