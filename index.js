fb0bab1a776b236a9e46e0dc26d84f8b4ee6f46c
Willie Yturralde <willieino@hotmail.com>
2018-11-28 15:36:41 -0800

    modified js files

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
 // import your node modules
-
-const db = require('./data/db.js');
+const express = require('express');
+const db = require('./data/db');
+const server = express();
+const PORT = 4000;
 
 
 
 // add your server code starting here
+
+server.get('/api/posts', (req, res) => {
+    db.find()
+    .then((posts) => {
+        res.json(posts);
+    })
+    .catch(err => {
+        res
+        .status(500)
+        .json({error: "The posts information could not be retrieved."});
+    });
+});
+
+server.get('/api/posts/:id', (req, res) => {
+    const { id } = req.params
+    db.findById(id)
+    .then(post => {
+     if (post) {
+         res.json(post);
+     } else {
+         res
+         .status(404)
+         .json({ message: "The post with the specified ID does not exist."})
+     }
+    })
+    .catch(err => {
+        res
+        .status(500)
+        .json({error: "The post information could not be retrieved."});
+    });
+});
+
+
+server.listen(PORT, () => {
+    console.log(`server is running on port ${PORT} `);
+});
