6f7717f3d26981947d18b1feb48e63a5919c7d68
iepoch <staclem@live.com>
2018-11-27 21:14:23 -0700

    Intitial Commit, Install Yarn, add Express, created Server and Port

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // import your node modules
-
-const db = require('./data/db.js');
+const express = require("express");
+const db = require("./data/db.js");
 
 // add your server code starting here
+const server = express();
+const PORT = 5000;
