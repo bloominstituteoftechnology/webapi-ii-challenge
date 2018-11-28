54e9d6873c8620fd106d6d4eed4e014c7f297046
iepoch <staclem@live.com>
2018-11-27 21:26:43 -0700

    Created server get response for api/posts

--- index.js
+++ index.js
@@ -4,4 +4,21 @@ const db = require("./data/db.js");
 
 // add your server code starting here
 const server = express();
-const PORT = 5000;
+const PORT = 4000;
+
+server.get("/api/posts", (req, res) => {
+db.find()
+    .then(posts => {
+    res.json(posts);
+    })
+    .catch(err => {
+    res
+        .status(500)
+        .json({ error: "The posts information could not be retrieved."});
+    });
+});
+
+
+server.listen(PORT, () => {
+    console.log('server is up and running')
+})
