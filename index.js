d6b50e966b97c10d8ca3364ecfc5798e41592d71
hraya <hraya@lssu.edu>
2018-11-27 22:38:33 -0600

    Task 2 complete
    the id GET is complete

--- index.js
+++ index.js
@@ -6,15 +6,33 @@ const db = require('./data/db.js');
 const server = express();
 const PORT = 4000;
 // add your server code starting here
+
 server.get('/api/posts', (req, res) =>{
    db.find()
    .then((post)=>{
        res.json(post)
    })
    .catch(err =>{
-       status(500)
-       res.json({error: "The posts information could not be retrieved."})
+       res.status(500)
+            .json({error: "The posts information could not be retrieved."})
    })
+});
+
+server.get('/api/posts/:id', (req, res)=>{
+    const id = req.params.id;
+    db.findById(id)
+    .then(post =>{
+        if(post) {
+            res.json(post)
+        }else {
+            res.status(404)
+            .json({message: "The post with the specified ID does not exist."})
+        }
+    })
+    .catch(err => {
+        res.status(500)
+        .json({message: "The post with the specified ID does not exist."})
+    })
 })
 
 
