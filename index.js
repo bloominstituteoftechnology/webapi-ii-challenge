63bbf37397a73280860932f8beb56e1ea2e720a9
Bruce Cabanayan <bcabanayan@Unknown.domain>
2018-11-28 16:06:59 -0700

    Added get function to obtain array of post objects from database

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-// import your node modules
+// importing database and express
 
 const db = require('./data/db.js');
 const express = require('express');
 
-// add your server code starting here
+// designating server port and defining server using express
 
 const PORT = 4001;
 const server = express();
 
+// get array of posts
+
+server.get('/api/posts', (req, res) => {
+    db.find()
+        .then(posts => {
+            res
+                .json(posts);
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .json({message: 'failed to get posts'})
+        });
+});
+
 server.listen(PORT, err => {
-    console.log(`server is running`)
+    console.log(`server is still running for sure!`)
 });
