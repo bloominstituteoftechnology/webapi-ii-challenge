e4b6126d05e98364f7803e0a65fa477804ed3152
Jason Hedrick <jasonleehedrick@gmail.com>
2018-11-28 11:23:51 -0800

    Setup barebones functions for all CRUD requests.

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 // import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
 
+const server = express();
+const PORT = 3000;
+
 // add your server code starting here
+
+server.get('/api/posts', (req, res) => {
+    //find
+})
+
+server.get('/api/posts/:id', (req, res) => {
+    //findById
+})
+
+server.post('/api/posts', (req, res) => {
+    //insert
+})
+
+server.put('/api/posts/:id', (req, res) => {
+    //update
+})
+
+server.delete('/api/posts/:id', (req, res) => {
+    //remove
+})
+
+server.listen(PORT, () => {
+    console.log('Server is listening');
+})
