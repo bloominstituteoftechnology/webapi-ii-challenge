05755cc26b0c945d9f0a56d33e826d937024f37f
Kendra Williams <kmonique.williams@gmail.com>
2018-11-27 23:40:18 -0500

    get request api/posts/:id completed

--- index.js
+++ index.js
@@ -18,9 +18,24 @@ server.get("/api/posts", (req, res) => {
       })
       .catch(err => {
          res.status(500).json({ error: "The posts information could not be retrieved." })
+      });
+});
+
+server.get("/api/posts/:id", (req, res) => {
+   const {id} = req.params;
+   db.findById(id)
+      .then(post => {
+         if(post.length > 0){
+            res.json(post);
+         } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+         }
       })
+      .catch(err => {
+         res.status(404).json({ message: "The post with the specified ID does not exist." })
+      });
 });
 
 server.listen(PORT, () => {
    console.log(`Server is running on port ${PORT}`)
-})
+});
