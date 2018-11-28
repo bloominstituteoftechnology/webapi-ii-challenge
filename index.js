eb3b6190c72b576723c64c3878c81b109b3d71f4
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-11-27 23:30:32 -0500

    installed express built simple server

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 // import your node modules
+const express = require('express');
 
 const db = require('./data/db.js');
 
 // add your server code starting here
+const server = express();
+const PORT = 8080;
+
+server.listen(PORT, () => {
+  console.log(`server running on port: ${8080}`);
+});
