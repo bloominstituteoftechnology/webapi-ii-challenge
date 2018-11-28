cd57ea91aaebb89be81da8c1867e370d04ca0303
Emmanium <pradoe.ep@gmail.com>
2018-11-28 13:28:06 -0700

    Imported node modules added server code to make get requests

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 // import your node modules
+const express = require('express'); // imports express package
+const server = express(); // creates the server
 
 const db = require('./data/db.js');
 
 // add your server code starting here
+const hostname = '127.0.0.1'; // local computer ip address
+const port = 3000; // port to watch traffic
+
+server.get('/api/posts', (req, res) => {
+  db.find()
+    .then(posts => { res.json(posts) })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "There was an error while saving the post to the database" })
+    })
+})
+
+server.get('/api/posts/:id', (req, res) => {
+  res.json({ Hello: 'message' })
+})
+
+//start watching for connections
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`)
+})
