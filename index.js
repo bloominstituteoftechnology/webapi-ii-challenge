1bd00f5b3522c9ac1fcaeffff2766d37eb85cbf4
Matthew Meitl <darksamus6@gmail.com>
2018-11-28 08:28:25 -0600

    setup basic server and get requests

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 // import your node modules
+const express = require('express');
+const server = express();
 
 const db = require('./data/db.js');
 
 // add your server code starting here
+server.get('/api/posts', (req, res) => {
+    db.find()
+      .then()
+      .catch();
+    
+});
+
+server.get('/api/posts/:id', (req, res) => {
+    db.find()
+      .then()
+      .catch();
+    
+});
+
+server.listen(5000, () => {
+    console.log('Server running on http://localhost:5000')
+});
+
