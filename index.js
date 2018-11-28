f96c3c79f095f40653ed83b4d87e86be611907d1
hraya <hraya@lssu.edu>
2018-11-27 22:18:51 -0600

    Initial Commit
    began setting up index.js

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 // import your node modules
+const express = require('express');
 
 const db = require('./data/db.js');
 
+const server = express();
+const PORT = 4000;
 // add your server code starting here
+server.get('/api/post', (req, res) =>{
+   
+})
+
+
+
+
+server.listen(PORT, ()=>{
+    console.log(`Server works you did not break it, it's on port ${PORT}!`)
+})
