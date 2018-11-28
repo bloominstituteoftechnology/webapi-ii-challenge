b601c87cd5aa36c3ce7a5710199b44a6551328e9
Dewayne87 <dewaynelindsay87@gmail.com>
2018-11-28 04:01:29 -0800

    got get endpoints to work

--- index.js
+++ index.js
@@ -2,6 +2,41 @@
 const express = require('express');
 const server = express();
 const PORT = 4000;
-const db = require('./data/db.js');
+const db = require('./data/db');
 
 // add your server code starting here
+server.get('/api/posts', (req, res) => {
+    db.find()
+        .then(posts => {
+            res.json(posts);
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .json({ error: "The posts information could not be retrieved." })
+        })
+})
+
+server.get('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    db.findById(id)
+        .then(post => {
+            if (post) {
+                res.json(post);
+            } else {
+                res
+                    .status(404)
+                    .json({ message: "The post with the specified ID does not exist." })
+                console.log(res)
+            }
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .json({ error: "The post information could not be retrieved." })
+        })
+})
+
+server.listen(PORT, () => {
+    console.log('server is running')
+})
