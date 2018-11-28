a6713add8737782d1e132bd859bb000781a26d25
TexaCaliCoder <c.roth.lambda@gmail.com>
2018-11-27 22:23:13 -0600

    initial commit for PR, set up yarn install and dependancies as well as set up basics of my server.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 // import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
 
-// add your server code starting here
+const server = express();
+const PORT = 4040;
+
+server.get('/api/',(res, req))
+
+server.listen(PORT, ()=>{
+    console.log(`server is running on port: ${PORT}`);
+})
