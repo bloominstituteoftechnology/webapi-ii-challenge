04b8d12645292a0330060b1331344f8b47f05425
Megan Fontenot <lambda_school_loaner_28@Lambda-School-Loaner-28s-MacBook-Pro.local>
2018-11-28 01:10:18 -0600

    imported node modules and got server sending and listening.

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 // import your node modules
-
 const db = require('./data/db.js');
 
+const express = require('express');
+const server = express();
+const PORT = 3000;
+
 // add your server code starting here
+
+server.get('/', (req , res) => {
+    res.send('Shot in the dark..')
+})
+
+server.listen(PORT, () => {
+    console.log(`My server is running on port ${PORT}`);
+});
