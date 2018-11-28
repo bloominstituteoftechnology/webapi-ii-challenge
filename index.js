e1276afe65077d773b35c86a0e70fa2cb00f7a9b
Rob Salzberg <robsalzberg@gmail.com>
2018-11-28 10:46:12 -0500

    Server created and GET all posts

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 // import your node modules
-
-const db = require('./data/db.js');
+const express = require("express");
+const db = require("./data/db.js");
 
 // add your server code starting here
+const server = express();
+const PORT = 4000;
+
+//GET endpoints
+
+server.get("/api/posts", (req, res) => {
+  db.find()
+    .then(posts => {
+      res.json(posts);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The posts information could not be retrived." });
+    });
+});
+
+//start server listening
+
+server.listen(PORT, () => {
+  console.log("server is up and running");
+});
