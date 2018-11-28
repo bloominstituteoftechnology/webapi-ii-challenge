99248db71fb508853a708b4af0eddea98288b011
Jason Hedrick <jasonleehedrick@gmail.com>
2018-11-28 11:41:56 -0800

    Filled out get request functionality. Both for getting all the data and data by id.

--- index.js
+++ index.js
@@ -9,10 +9,30 @@ const PORT = 3000;
 
 server.get('/api/posts', (req, res) => {
     //find
+    db.find()
+    .then((posts) => {
+        res.json(posts);
+    })
+    .catch(() => {
+        res.status(500).json({message: 'Failed to get posts'});
+    })
 })
 
 server.get('/api/posts/:id', (req, res) => {
     //findById
+    const {id} = req.params;
+    db.findById(id)
+    .then((post) => {
+        if(post) {
+            res.json(post);
+        }
+        else {
+            res.status(404).json({message: 'Post does not exist'});
+        }
+    })
+    .catch(() => {
+        res.status(500).json({message: 'Failed to get post.'});
+    })
 })
 
 server.post('/api/posts', (req, res) => {
