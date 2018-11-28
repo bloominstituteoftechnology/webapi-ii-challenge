323943994922c9e5c8237c5577d9959b69c6a7f6
Rick Fleming <elfeming111@yahoo.com>
2018-11-27 21:09:35 -0700

    Created server and added get endpoint

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-// import your node modules
+const express = require('express');
 
 const db = require('./data/db.js');
 
-// add your server code starting here
+const server = express();
+
+server.get('/api/posts', (req,res)=>{
+    db.find()
+    .then(posts=>{
+        res.json(posts);
+    })
+    .catch(error=>{
+        res.status(500).json({error: 'The posts information could not be retrieved.'});
+    });
+});
+
+server.get('/api/posts/:id', (req, res)=>{
+
+});
+
+server.listen(5000, ()=>{
+    console.log('Staring server on port 5000');
+})
