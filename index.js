321ea8c188cca5c86830cc6a745ab7958bcef477
paulapivat <paul.apivat@gmail.com>
2018-11-28 12:37:02 +0700

    add dependencies, express, create two endpoints with GET

--- index.js
+++ index.js
@@ -1,5 +1,47 @@
 // import your node modules
 
+const express = require('express');
 const db = require('./data/db.js');
 
+const server = express();     //express is a function
+const PORT = 5000;
+
 // add your server code starting here
+
+server.get('/api/posts', (req, res) => {
+    db.find()
+    .then((posts) => {
+        res.json(posts);      //implicit status(200)
+    })
+    .catch(err => {
+        res 
+        .status(500)
+        .json({message: "failed to get posts list"})
+    });
+});
+
+server.get('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    db.findById(id)
+    .then((post) => {
+        if(post){
+            res.json(post);
+        } else {
+            res 
+            .status(404)
+            .json({message: "This Post does not exist"})
+        }
+    })
+    .catch(err => {
+        res 
+        .status(500)
+        .json({message: "Failed to get that specific post"});
+    })
+});
+
+
+// listening
+
+server.listen(PORT, () => {
+    console.log(`server on localhost:5000 is up and running on port ${PORT}`)
+})
