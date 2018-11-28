08c095918a453e99c40a297e27d39737b0819c1d
Jennifer King <jenbellpgh@gmail.com>
2018-11-28 02:07:10 -0500

    Started on the next part of the assignment, but need to wait for the next lecture!

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = 4000;
 
 const db = require('./data/db.js');
 
-// GET without arguments
+// GET	/api/posts	Returns an array of all the post objects contained in the database.
 server.get('/api/posts', (req, res) => {
   db.find()
     .then( posts => {
@@ -17,7 +17,7 @@ server.get('/api/posts', (req, res) => {
     })
 });
 
-// GET with id
+// GET	/api/posts/:id	Returns the post object with the specified id.
 server.get( '/api/posts/:id', (req, res) => {
   const {id} = req.params;
   db.findById(id)
@@ -37,6 +37,25 @@ server.get( '/api/posts/:id', (req, res) => {
       res.status(500).json({ error: "The post information could not be retrieved."});
     })
 });
+
+// POST	/api/posts	Creates a post using the information sent inside the request body.
+server.post( '/api/posts', (req, res) => {
+  console.log("req params:", req );
+  res.json({message: `${req.params.title}`});
+  //const {title, contents} = req.params;
+  // if( !req.params.title || !req.params.contents ){
+  //   res.status(400).json({ errorMessage: "Please provide title and contents for the post."});
+  // }
+  // else {
+  //   res.json({message: "Success"})
+  // }
+});
+
+
+// PUT	/api/posts/:id	Updates the post with the specified id using data from the request body. 
+//      Returns the modified document, NOT the original.
+
+// DELETE	/api/posts/:id	Removes the post with the specified id and returns the deleted post object. You may need to make additional calls to the database in order to satisfy this requirement.
 
 // Listener:
 server.listen( PORT, () => {
