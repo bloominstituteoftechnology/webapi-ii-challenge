3a7e0db1431d3eddcbb2d729d0985e6dcf098b83
TexaCaliCoder <c.roth.lambda@gmail.com>
2018-11-27 22:40:37 -0600

    accessing proper data from GET request

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 // import your node modules
 const express = require('express');
-const db = require('./data/db.js');
+const db = require('./data/db');
 
 const server = express();
 const PORT = 4040;
 
-server.get('/api/',(res, req))
+server.get('/api/posts',(req, res)=>{
+    db.find()
+        .then(posts =>{
+            res.json(posts)
+        })
+        .catch(err =>{
+            res.status(500)
+                .json({ error: "The posts information could not be retrieved" })
+        })
+})
+
+server.get('/api/posts/:id', (req, res)=>{
+    const { id } = req.params
+    db.findById(id)
+        .then((posts =>{
+            posts?res.json(posts):
+            res.status(404)
+            .json({ message: "The post with the specified ID does not exist." })
+        }))
+        .catch(err=>{
+            res.status(500)
+            .json({ error: "The posts information could not be retrieved" })
+        })
+})
 
 server.listen(PORT, ()=>{
     console.log(`server is running on port: ${PORT}`);
