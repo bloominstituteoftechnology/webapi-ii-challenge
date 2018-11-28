3baece5b90a3d625f496a6444452d7b7e2ddc2d8
Abdiel Fernandez <fernandezabdiel@gmail.com>
2018-11-27 23:38:00 -0500

    created the get endpoints for both the posts and the posts/id and also made the server listen on port 5000

--- index.js
+++ index.js
@@ -1,8 +1,46 @@
 // import your node modules
-const express = require(express);
+const express = require('express');
 
 const server = express();
 
 const db = require('./data/db.js');
 
+const PORT = 5000;
+
 // add your server code starting here
+
+//grabbing a list of all posts
+server.get('/api/posts', (req, res) => {
+  db.find()
+    .then( posts => {
+      res.json(posts)
+    })
+    .catch( err => {
+      res
+        .status(500)
+        .json({ error: "The posts information could not be retrieved."})
+    })
+})
+
+//grabbing an individual post by id
+server.get('/api/posts/:id', (req, res) => {
+  const { id } = req.params;
+  db.findById(id)
+    .then( post => {
+      if (post.length !== 0) {
+        res.json(post)
+      } else {
+        res
+          .status(404)
+          .json({message: "The post with the specified ID does not exist."});
+      }
+    })
+    .catch( err => {
+      res.json({message: 'unable to load posts'})
+    })
+})
+
+//listening
+server.listen(PORT, () => {
+  console.log(`my server is running on port ${PORT}`)
+})
