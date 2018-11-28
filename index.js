9343dc3ab5d5b83f3c5ebe18e6bc6968536c03ec
kammagik <kammagik@yahoo.com>
2018-11-27 20:28:25 -0800

    init and posts returned

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 // import your node modules
-
+const express = require('express');
+const server = express();
 const db = require('./data/db.js');
 
 // add your server code starting here
+server.get('/', (req, res) => {
+    res.json('HELLO!');
+});
+
+server.get('/api/posts', (req, res) => {
+    db.find()
+    .then(posts => {
+        res.status(200).json(posts);
+    })
+    .catch( err => {
+        res
+        .status(500)
+        .json({message : 'post could not be found'})
+    })
+}) 
+
+server.listen(4000, () => {
+    console.log('server is running');
+})
