23499bdef1971470f5e3edcccfc33848521683fb
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-11-27 23:48:12 -0500

    added GET for /api/posts and /api/posts/:id

--- index.js
+++ index.js
@@ -7,6 +7,39 @@ const db = require('./data/db.js');
 const server = express();
 const PORT = 8080;
 
+server.get('/api/posts', (req, res) => {
+  db
+    .find()
+    .then(posts => {
+      res.json(posts);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: 'The posts information could not be retrieved.' });
+    });
+});
+
+server.get('/api/posts/:id', (req, res) => {
+  const { id } = req.params;
+  db
+    .findById(id)
+    .then(post => {
+      if (post) {
+        res.json(post);
+      } else {
+        res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist.' });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: 'The post information could not be retrieved.' })
+    })
+});
+
 server.listen(PORT, () => {
   console.log(`server running on port: ${8080}`);
 });
