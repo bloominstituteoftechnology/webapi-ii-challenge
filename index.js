0b86ef8906e938fd4053d2f89a83b2d9207226e1
Mitchell Robles <mitchellr92@live.com>
2018-11-27 20:18:57 -0800

    added get requests

--- index.js
+++ index.js
@@ -10,11 +10,37 @@ const db = require('./data/db.js');
 
 server.get('/api/posts', (req, res) => {
 
-})
+    db.find()
+        .then((posts) => {
+            res.json(posts);
+        })
+        .catch(err => {
+            res.status(500)
+                .json({ message: 'Failed to get posts' })
+
+        })
+
+});
 
 server.get('/api/posts/:id', (req, res) => {
+
+    const { id } = req.params;
+
+    db.findById(id)
+        .then(post => {
+            if (post) {
+                res.json(post)
+            } else {
+                res.status(404)
+                    .json({ message: 'Failed to get post' })
+            }
+        })
+        //What is the purpose of this catch?
+        .catch(err => {
+            res.status(500)
+               .json({ message: 'Failed to get user' })
+        })
 
 })
 
 // Listen
-
