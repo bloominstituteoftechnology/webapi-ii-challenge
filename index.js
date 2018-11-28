091741d3db095ad4cbbdc70773e7843cf74342e5
Joe Mercado <jryel1970@gmail.com>
2018-11-27 22:55:34 -0500

    first commit for initial PR.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // import your node modules
 
+const express = require('express');
 const db = require('./data/db.js');
+
+const server = express();
+const PORT = 5000;
 
 // add your server code starting here
