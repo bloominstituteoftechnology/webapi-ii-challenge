ac455bc26d1f3db9ce65aa343fe7d3fb1c4ca27b
BrendonKootz <brendonkootz@gmail.com>
2018-11-28 14:39:29 -0600

    Posts are displaying

--- index.js
+++ index.js
@@ -1,5 +1,47 @@
-// import your node modules
+/* PRIMARY DEFINITIONS AND DEPENDANCIES */
 
-const db = require('./data/db.js');
+const DB = require('./data/db.js');
+const EXP = require('express');
+SERVER = EXP();
+console.log("Starting server... \t:^)\n");
+const PORT = 4000;
 
-// add your server code starting here
+/* MAIN SERVER SCRIPT */
+
+// Display Something
+SERVER.get('/',(req,res) => {
+	res.send("Hey there ;^)");
+});
+
+// Get questions
+SERVER.get('/api/posts',(req,res) => {
+	DB.find()
+		.then((posts) => {
+			console.log("Posts found:/t",posts);
+			res.json(posts);
+		})
+		.catch(err => {
+			res
+				.status(500)
+				.json({message: "Failed to locate 'Posts'"});
+		})
+});
+
+// Get question Id's
+SERVER.get('/api/posts:id',(req,res) => {
+	DB.find()
+		.then((posts) => {
+			console.log("Post ID:\t",posts)
+			res.json(posts)
+		})
+		.catch(err => {
+			res
+				.status(500)
+				.json({message: "Failed to find post ID"})
+		})
+});
+
+// Start Listenings
+SERVER.listen(PORT, () => {
+	console.log("Listening on port:\t",PORT);
+});
