69afb2251fca30ea896cb67417a64272a0a68d26
Mitchell Robles <mitchellr92@live.com>
2018-11-27 20:09:06 -0800

    added axpress and start setting up get requests

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 // import your node modules
 
+const express = require('express');
+
 const db = require('./data/db.js');
 
 // add your server code starting here
+
+// Ednpoints
+
+server.get('/api/posts', (req, res) => {
+
+})
+
+server.get('/api/posts/:id', (req, res) => {
+
+})
+
+// Listen
+
