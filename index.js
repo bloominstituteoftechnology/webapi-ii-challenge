52e7a03652afa7f0416f0be30cefdfc494b361e1
hraya <hraya@lssu.edu>
2018-11-27 23:53:29 -0600

    Completed Task 3
    Completed POST

--- index.js
+++ index.js
@@ -35,6 +35,22 @@ server.get('/api/posts/:id', (req, res)=>{
     })
 })
 
+server.post('/api/posts', (req, res) =>{
+    const {title , contents} = req.query
+    console.log(title, contents)
+    if(title == "" || contents == ""){
+        res.status(400).json({errorMessage: "Please provide title and contents for the post."})
+    } else {
+        db.insert({"title":title, "contents":contents})
+        .then(post =>{
+            res.status(201).json(post)
+        })
+        .catch(err =>{
+            res.status(500).json({ error: "There was an error while saving the post to the database"})
+        })
+    }
+})
+
 
 
 
