4a29b229a03c6e39ee7b404a0cc0054aa0555c80
Rick Fleming <elfeming111@yahoo.com>
2018-11-27 21:24:17 -0700

    Added get post with id endpoint

--- index.js
+++ index.js
@@ -15,7 +15,19 @@ server.get('/api/posts', (req,res)=>{
 });
 
 server.get('/api/posts/:id', (req, res)=>{
-
+    const {id} = req.params;
+    db.findById(id)
+    .then(post=>{
+        if(post.length){
+            res.json(post);
+        }
+        else{
+            res.status(404).json({message: 'The post with the specified ID does not exist.'});
+        }
+    })
+    .catch(error=>{
+        res.status(500).json({error: 'The post information could not be retrieved'});
+    });
 });
 
 server.listen(5000, ()=>{
