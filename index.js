f70a0fdc45ef3687c94d773fbf343f1f66f96bd3
TheGameFreak720 <luis.alvarez@commithub.com>
2018-11-27 23:00:29 -0500

    Got simple server running

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 // import your node modules
+const express = require('express');
 
 const db = require('./data/db.js');
 
 // add your server code starting here
+
+const server = express();
+
+const port = 3000;
+
+server.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
