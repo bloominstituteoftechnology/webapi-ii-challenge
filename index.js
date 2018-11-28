357343fec1d33c67518dabbb2337faa11df9b4ec
TheGameFreak720 <luis.alvarez@commithub.com>
2018-11-27 23:09:15 -0500

    constructed a get all posts request

--- index.js
+++ index.js
@@ -5,10 +5,19 @@ const db = require('./data/db.js');
 
 // add your server code starting here
 
-const server = express();
-
+const app = express();
 const port = 3000;
 
-server.listen(port, () => {
+app.get('/api/posts', function(req, res) {
+    db.find()
+        .then(posts => {
+            res.json(posts);
+        })
+        .catch(() => {
+            res.status(500).json({ error: "The posts information could not be retrieved." });
+        });
+});
+
+app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
