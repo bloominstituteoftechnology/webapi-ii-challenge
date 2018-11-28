cf8a94a2cb2513aeeb8bc6bbd3eb579c7a013c4b
Colin Dismuke <colin.dismuke@gmail.com>
2018-11-27 22:25:46 -0600

    Added GET request for all posts

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const app = express();
 const PORT = 3000;
 
 // Endpoints
+app.get("/api/posts", (req, res) => {
+  db.find()
+    .then(posts => {
+      res.json(posts);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Failed to get posts." });
+    });
+});
 
 // Listen
 app.listen(PORT, () => console.log(`Server started on port ${PORT}...`));
