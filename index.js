b4e4dfa696b95f363345e0af8769d831455dc252
Griffin <griffinshuth@gmail.com>
2018-11-27 22:51:55 -0700

    got my .post to work

--- index.js
+++ index.js
@@ -7,36 +7,52 @@ const PORT = 4000;
 
 server.get('/api/posts', (req, res) => {
     db.find()
-       .then(posts => {
-           res.status(200).json(posts)
-       })
-       .catch(err => {
-           res
-           .status(500)
-           .json({error: "The posts information could not be retrieved." });
-       });
-  });
+        .then(posts => {
+            res.status(200).json(posts)
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .json({ error: "The posts information could not be retrieved." });
+        });
+});
 
-  server.get('/api/posts/:id', (req, res) => {
-      const id = req.params.id;
+server.get('/api/posts/:id', (req, res) => {
+    const id = req.params.id;
 
-      db.findById(id)
+    db.findById(id)
         .then(post => {
             if (post) {
                 res.json(post);
             } else {
                 res
-                .status(404)
-                .json({message: "The post with the specified ID does not exist."});
+                    .status(404)
+                    .json({ message: "The post with the specified ID does not exist." });
             }
         })
         .catch(err => {
             res
                 .status(500)
-                .json({ error: "The post information could not be retrieved."  })
+                .json({ error: "The post information could not be retrieved." })
         });
-  });
+});
 
-  server.listen(PORT, () => {
-      console.log( `server is up and running on port ${PORT}`);
-  })
+server.post('/api/posts', (req, res) => {
+    const {title, contents} = req.query
+    console.log(title,contents)
+    db.insert({"title": title, "contents":contents})
+        .then(post => {
+            if (post) {
+                res.status(201).json(post)
+            } else {
+                res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+            }
+        })
+        .catch(err => {
+            console.log('200')
+            res.status(500).send({ error: "this didnt work"})
+        })
+})
+server.listen(PORT, () => {
+    console.log(`server is up and running on port ${PORT}`);
+})
