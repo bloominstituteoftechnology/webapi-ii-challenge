507ad66a98009c72cbb24b9220dd8ad329517637
Colin Dismuke <colin.dismuke@gmail.com>
2018-11-27 23:25:25 -0600

    Started building components to display posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import { Route, withRouter } from "react-router-dom";
+
+import Nav from "./components/Nav";
+import Form from "./components/Form";
+import Posts from "./components/Posts";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:3000/api/posts")
+      .then(res => {
+        this.setState({ posts: res.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Nav />
+        <Route
+          exact
+          path="/"
+          render={props => (
+            <Posts
+              {...props}
+              posts={this.state.posts}
+              delete={this.deletePost}
+            />
+          )}
+        />
+        <Route
+          path="/add-post"
+          render={props => (
+            <Form {...props} posts={this.state.posts} add={this.addPost} />
+          )}
+        />
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
