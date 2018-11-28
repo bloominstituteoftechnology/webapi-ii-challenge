2ca6e5361ae71439fcac17c5646df980ad3abe50
iepoch <staclem@live.com>
2018-11-27 22:13:32 -0700

    getting odd error message on the ID endpoint

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const db = require("./data/db.js");
 
 // add your server code starting here
 const server = express();
-const PORT = 4000;
+const PORT = 5555;
 
 server.get("/api/posts", (req, res) => {
 db.find()
@@ -14,11 +14,31 @@ db.find()
     .catch(err => {
     res
         .status(500)
-        .json({ error: "The posts information could not be retrieved."});
+        .json({ error: "The posts information could not be retrieved." });
     });
 });
 
+server.get("/api/posts/:id", (req, res) => {
+const { id } = req.params;
+
+db.findById(id)
+    .then(post => {
+        // res.json(post);
+    if (post) {
+        res.json(post);
+    } else {
+        res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
+    }
+    })
+    .catch(err => {
+    res
+        .status(500)
+        .json({ error: "The post information could not be retrieved." });
+    });
+});
 
 server.listen(PORT, () => {
-    console.log('server is up and running')
-})
+  console.log("server is up and running");
+});
