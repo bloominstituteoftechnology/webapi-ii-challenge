92d32b24d27f5b1547cb85a80baf07fdd2bedce1
Jennifer King <jenbellpgh@gmail.com>
2018-11-28 00:20:31 -0500

    Started up node, added listener & initial response

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 // import your node modules
+const express = require('express');
+
+const server = express();
+const PORT = 4000;
 
 const db = require('./data/db.js');
 
 // add your server code starting here
+server.get('/', (req, res) => {
+  res.send("Hello World!");
+})
+
+// Listener:
+server.listen( PORT, () => {
+  console.log( `Server started on port: ${PORT}`)
+});
