c666570d154f7d8ad1cad5537bb76e88458e7412
Griffin <griffinshuth@gmail.com>
2018-11-27 21:31:34 -0700

    got it done

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
 const express = require('express');
 
-
 const db = require('./data/db.js');
 
 const server = express();
 const PORT = 4000;
 
-// add your server code starting here
-
 server.get('/api/posts', (req, res) => {
-db.find()
-.then((posts) => {
-    res.json(posts);
-})
-.catch(err => {
-    res.status(500)
-    .json({errorMessage: "Please provide title and contents for the post."})
-})
+    db.find()
+       .then(posts => {
+           res.status(200).json(posts)
+       })
+       .catch(err => {
+           res
+           .status(500)
+           .json({error: "The posts information could not be retrieved." });
+       });
+  });
 
-})
-server.get('/api/posts/;id', (req, res) => {
-    const {id} = req.params;
-    db.findById(id)
-    .then(post => {
-        res.json(post)
-    })
-    .catch(err => {
-        res.status(500)
-    })
-})
+  server.get('/api/posts/:id', (req, res) => {
+      const id = req.params.id;
 
-server.listen(PORT, () => {
-    console.log(`server is up and running on port ${PORT}`);
-});
+      db.findById(id)
+        .then(post => {
+            if (post) {
+                res.json(post);
+            } else {
+                res
+                .status(404)
+                .json({message: "The post with the specified ID does not exist."});
+            }
+        })
+        .catch(err => {
+            res
+                .status(404)
+                .json({ error: "The post information could not be retrieved."  })
+        });
+  });
+
+  server.listen(PORT, () => {
+      console.log( `server is up and running on port ${PORT}`);
+  })
