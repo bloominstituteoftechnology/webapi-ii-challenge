620844099a984a369d8f7f1705b6885948028d06
Abdiel Fernandez <fernandezabdiel@gmail.com>
2018-11-28 23:37:48 -0500

    added styling to the components with styled-components

---
+++ client/src/components/quote/styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const MainDiv = styled.div `
+  height: auto;
+  width: 300px;
+`
+
+export const Content = styled.h2 `
+  text-decoration: underline;
+`
+
+export const Title = styled.p `
+  font-style: italic
+`
---
+++ client/src/components/quotes/styled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const MainDiv = styled.div `
+  display: flex
+  justify-content: center
+  flex-wrap: wrap
+`
