a1ad8b9be67a673a93e3ad539ffc8761b274de0a
Jesse Anderson <jesseanderson@Jesses-MacBook-Pro.local>
2018-11-28 20:54:45 -0600

    GET endpoints are working correctly

--- index.js
+++ index.js
@@ -1,5 +1,47 @@
 // import your node modules
+const express = require('express');
 
 const db = require('./data/db.js');
 
+const server = express();
+
+const port = 4000;
+
 // add your server code starting here
+
+server.get('/api/posts', (req, res) => {
+  db.find()
+    .then(posts => {
+      res.json(posts);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: 'The posts information could not be retrieved.' });
+    });
+});
+
+server.get('/api/posts/:id', (req, res) => {
+  const { id } = req.params;
+  db.findById(id)
+    .then(post => {
+      console.log('post ', post);
+      if (post[0]) {
+        res.status(200).json(post);
+      } else {
+        res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist.' });
+      }
+    })
+    .catch(err => {
+      console.log('err ', err);
+      res
+        .status(500)
+        .json({ error: 'The post information could not be retrieved.' });
+    });
+});
+
+server.listen(port, () => {
+  console.log(`server is up and running on port ${port}`);
+});
