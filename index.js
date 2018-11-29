b415916ec34283d90b20c25a391443c83f8f8ec9
BrandonMoll <brandonmoll85@gmail.com>
2018-11-28 20:27:59 -0600

    finished the GET and GET ID endpoints

--- index.js
+++ index.js
@@ -1,5 +1,44 @@
-// import your node modules
+const express = require('express');
 
-const db = require('./data/db.js');
+const db = require('./data/db');
+
+const server = express();
+
+const PORT = 4000;
 
 // add your server code starting here
+
+server.get('/api/posts', (req, res) => {
+    db.find()
+        .then(posts => {
+            res.json(posts);
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .json({message: 'failed to get posts'})
+        })
+});
+
+server.get('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    db.findById(id)
+        .then(post => {
+            if (post) {
+                res.json(post)
+            } else {
+                res
+                    .status(404)
+                    .json({message: 'post not found'})
+            }
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .json({message: 'failed to get post'})
+        })
+});
+
+server.listen(PORT, () => {
+    console.log('server is running')
+})
