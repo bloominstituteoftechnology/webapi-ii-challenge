86d17a3c01c09f68e1c05c5070f9f65d3c5a4652
Robin U Familara <robin.familara@cgius.com>
2018-11-28 22:27:16 -0600

    Installed dependencies -Get users

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 // import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
 
 // add your server code starting here
+const server = express();
+
+// middleware - put when doing react app
+
+// starting point
+server.get('/', (Req, res) => {
+    res.send('Test');
+})
+// request handlers.
+server.get('/api/users', (req, res) => {
+    db.find().then(users => {
+        console.log('\n** users **', users);
+        res.json(users);
+    })
+    .catch(err => res.send(err))
+})
+
+const port = 8000;
+server.listen(port, () => console.log(`API running on port ${port}`))
