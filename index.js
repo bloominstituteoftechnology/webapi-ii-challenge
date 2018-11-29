17afa27446b665ab68b11fb300e4da607847bdb3
Joe Mercado <jryel1970@gmail.com>
2018-11-28 20:56:24 -0500

    Updated index.js file. Testing server posts.

--- index.js
+++ index.js
@@ -2,8 +2,74 @@
 
 const express = require('express');
 const db = require('./data/db.js');
+const posts = require('./data/seeds/posts.js');
+const bodyParser = require('body-parser');
 
-const server = express();
-const PORT = 5000;
 
 // add your server code starting here
+const server = express();
+server.use(bodyParser.json());
+
+server.post('/api/posts', (req, res) => {
+	const { title, contents } = req.body;
+
+	if (!title || !contents) {
+		res.status(400).json({ errorMessage: 'Please provide title and contents for the post.'})
+	} 
+		if (title && contents === req.body) {
+			const newPost = {title, contents}
+			db.insert(newPost)
+			.then (newID => {
+				res.status(201).json(newPost)
+			})
+			.catch(error => {
+				res.status(500).json({errorMessage: 'There was an error while saving the post to the database.'});
+			});
+		}
+});
+
+server.get('/api/posts', (req, res) => {
+	db
+	  .find().then(posts => {
+	  	res.json(posts);
+	  })
+	  .catch(error => {
+	  	res.status(500).json({errorMessage: "The posts information could not be retrieved."});
+	  });
+});
+
+server.get('/api/posts/:id', (req, res) => {
+	const { id } = req.params;
+	db
+	  .findById(id)
+	  .then(posts => {
+	  	if(posts.length === 0) {
+	  		res.status(404).json({error: 'The post information could not be retrieved.'});
+	  	} else {
+	  		res.json(posts);
+	  	}
+	  })
+	  .catch(error => {
+	  	res.status(500).json({error: 'The post information could not be retrieved.'});
+	  })
+});
+
+server.delete('/api/posts/:id', (req, res) => {
+	const { id } = req.params;
+	db
+	  .remove(id)
+	  .then(deletions => {
+	  	if (deletions === 0) {
+	  		res.error(404).json({message: 'The post with the specified ID does not exist'})
+	  	} else {
+	  		res.status(200).json({message: `Post ${id} Deleted`})
+	  	}
+	  })
+	  .catch(error => {
+	  	res.status(500).json('The post could not be removed.')
+	  })
+})
+
+const PORT = 5000;
+
+server.listen(PORT, () => console.log('API Running on port 5000'));
