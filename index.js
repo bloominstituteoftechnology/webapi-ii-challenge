8cc00ecdaed991e59de292c81c642f74310b2c6e
cferrell2 <cferrell12182@gmail.com>
2018-11-28 23:06:13 -0500

    Create endpoints for server.get api's. Create endpoint that returns all posts.  Create endpoint that returns individual post specified by its id.

--- index.js
+++ index.js
@@ -8,7 +8,33 @@ const db = require('./data/db.js');
 
 // add your server code starting here
 
+//Return Array of All posted objects
+server.get('/api/posts', (req, res) =>{
+    db.find()
+        .then((posts) =>{
+            res.json(posts)
+        })
+        .catch((err) =>{
+            res.json('Unable to retrieve the posts');
+        })
+});
 
+//Return post object with specified ID
+server.get('/api/posts/:id', (req, res) =>{
+    const id = req.params.id;
+
+    db.findById(id)
+        .then((post) =>{
+            if(post.length !== 0){
+                res.json(post)
+            }else{
+                res.json('The requested post does not exist')
+            }
+        })
+        .catch((err) =>{
+            res.json('Unable to retrieve the post')
+        })
+});
 
 server.listen(PORT, () =>{
     console.log(`Server is up and running on port ${PORT}`);
