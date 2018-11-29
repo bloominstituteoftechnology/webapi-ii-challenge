e19a76b45ffb043299ec86ed270632837efa8906
unknown <kyleteeter12@gmail.com>
2018-11-28 20:54:11 -0600

    Initial Commit

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // import your node modules
-
+const express = require('express')
 const db = require('./data/db.js');
 
+const PORT = 3300;
+
 // add your server code starting here
+server.listen(PORT, () => console.log(`Server is running on ${PORT}`))
