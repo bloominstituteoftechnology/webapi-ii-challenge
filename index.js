6034da6e1a82a9236adab75b05dcf9d323973f77
Marc de la Torre <mdelatorre1314@gmail.com>
2018-11-28 20:12:51 -0800

    wrote out server code for get

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 // import your node modules
 
+const express = require("express")
 const db = require('./data/db.js');
 
 // add your server code starting here
+const server = express();
+const PORT = 3333;
+server
+    .get("/api/posts", (req, res) => {
+        db
+            .find()
+            .then((posts) => {
+                res.json(posts);
+            })
+            .catch(err => {
+                res
+                    .status(500)
+                    .json({error: "The posts information could not be retrieved."})
+            })
+    })
