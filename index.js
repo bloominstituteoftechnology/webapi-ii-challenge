e739f2adc8a2d631eb79e9a803e88d5248d43351
jbrock1981 <jbrock1981@gmail.com>
2018-11-28 20:57:26 -0600

    Jonathan Brock

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 // import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
-
+const PORT = 4000;
 // add your server code starting here
+const server = express();
+
+server.get('/api/posts', (req, res) => {
+    db.find()
+        .then((posts) => {
+            res.json(posts);
+        })
+        .catch(err => {
+            res.status(500)
+            .json({ message: "The posts information could not be retrieved." })
+        })
+})
+
+server.listen(PORT);
