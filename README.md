382be6e745b0a23c594956f12487b15c6e450fde
Abigayle Franklin <abifanklin@gmail.com>
2018-11-28 22:20:35 -0600

    GET Posts endpoint working. Working on getting error message for individual post GET.

--- README.md
+++ README.md
@@ -117,7 +117,7 @@ When the client makes a `DELETE` request to `/api/posts/:id`:
 - If the _post_ with the specified `id` is not found:
 
   - return HTTP status code `404` (Not Found).
-  - return the following JSON object: `{ message: "The post with the specified ID does not exist." }`.
+  - return the following JSON object: ``.
 
 - If there's an error in removing the _post_ from the database:
   - cancel the request.
--- index.js
+++ index.js
@@ -1,5 +1,38 @@
-// import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
 
-// add your server code starting here
+const server = express();
+
+server.get('/api/posts', (req, res) => {
+    db.find()
+    .then(posts => {
+        res.json(posts);
+    })
+    .catch(err => {
+        res.status(500).json({ error: "The posts information could not be retrieved." })
+    })
+})
+
+server.get('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    db.findById(id)
+    .then(post => {
+        if (post !== []) {
+            res.json(post)
+        } else {
+            res.status(404)
+            .json({ message: "The post with the specified ID does not exist." })
+        }
+       
+    })
+    .catch(err => {
+        res.status(500).json({ error: "The post information could not be retrieved." })
+    })
+})
+
+
+const PORT = 5000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
+
