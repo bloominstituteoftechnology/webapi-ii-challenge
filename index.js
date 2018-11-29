1a0fffd68673e202632f92621976271e09cde28b
Robert Billington <chillingtonbillington@gmail.com>
2018-11-28 22:51:49 -0600

    Completed get all

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 // import your node modules
 
 const db = require('./data/db.js');
+const express = require('express');
+const server = express();
+const PORT = 4000;
 
+server.get('/api/posts', (req, res) =>{
+    db.find()
+        .then(post =>{
+            res.json(post)
+        })
+        .catch(err =>{
+            res.status(500).json({error: 'Could not get post data'})
+        })
+})
+server.get('api/posts/:id', (req, res) =>{
+   
+})
+server.listen(PORT, () =>{
+    console.log('The server is working!');
+})
 // add your server code starting here
