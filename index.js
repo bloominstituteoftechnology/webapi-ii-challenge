48b20a5324bcd4211c1bdf3b119af32b8f110788
Pascale Pierre <pascaleee@gmail.com>
2018-11-28 20:22:50 -0600

    got find and find by id working

--- index.js
+++ index.js
@@ -1,5 +1,47 @@
 // import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
 
 // add your server code starting here
+const server = express();
+const PORT = 4000;
+
+server.get('/', (req,res) => {
+  res.json({message: 'server is listening'})
+})
+
+server.get('/api/posts', (req, res) => {
+  db.find()
+  .then((posts) => {
+    res.json(posts);
+  })
+  .catch(err => {
+    res.status(500)
+    .json({message: 'failed to get posts'})
+  });
+})
+
+server.get('/api/posts/:id', (req, res) => {
+  const { id } = req.params;
+db.findById(id)
+.then((post) => {
+    if (post){
+        res.json(post); 
+    }
+   else {
+    res
+    .status(404)
+    .json({message: 'user is not availble'}
+    )}
+})
+.catch(err => {
+    res
+    .status(500)
+    .json({message: 'failed to get user'})
+});
+})
+
+
+server.listen(PORT, () =>{
+  console.log(`listening on ${PORT}`)
+});
