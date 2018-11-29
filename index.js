4242734101b437a57e4d3f076603169c1c8c4b2b
louislee7 <7.louislee@gmail.com>
2018-11-28 17:10:30 -0800

    listening on server

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+const express = require('express')
 // import your node modules
+
 
 const db = require('./data/db.js');
 
 // add your server code starting here
-//initial commit
+const server = express();
+const PORT = 4000;
+
+server.listen(PORT, () => {
+    console.log(`server is now up and running on ${PORT}`)
+})
