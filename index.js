e294ef5d126f5d1cd08f4109178547c54cf2cd65
rlmclaughlin <rlmclaughlin@yahoo.com>
2018-11-28 21:43:52 -0600

    finished the Put request tasks. MVP is finished

--- index.js
+++ index.js
@@ -80,6 +80,31 @@ server.get('/api/posts/:id', (req, res) => {
      })
  })
 
+ server.put('/api/posts/:id', (req, res) => {
+     const { id } = req.params;
+     const { title, contents } = req.body;
+     const newPost = { title, contents }; 
+     if(!id){
+         res
+         .status(404)
+         .json({message: "The post with the specified ID does not exist."})
+     } else if(!title || !contents){
+         res
+         status(400)
+         .json({errorMessage: "Please provide title and contents for the post."})
+     }
+     db.update(id, newPost)
+     .then(post => {
+         res
+         .status(200)
+         .json(post)
+     })
+     .catch(err => {
+         res
+         .status(500)
+         .json({error: "The post information could not be modified.", err})
+     })
+ })
 
 server.listen(PORT, () => {
     console.log(`The server is now running on port ${PORT}`)
