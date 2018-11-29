a78d22e39f3304e3ba0bad92548ede399448c52d
Bryce Evans <bryceevans@Bryces-MacBook-Pro.local>
2018-11-28 19:35:38 -0700

    Mostly finished... one problem I'm working on.

--- index.js
+++ index.js
@@ -1,5 +1,47 @@
 // import your node modules
 
+const express = require('express');
+
 const db = require('./data/db.js');
 
 // add your server code starting here
+
+const server = express();
+const PORT = 4000;
+
+server.get('/api/posts', (req, res) => {
+    db
+      .find()
+      .then((posts) => {
+        res.json(posts);
+      })
+      .catch(err => {
+        res
+        .status(500)
+        .json({message: "The posts information could not be retrieved."});
+      });
+});
+
+server.get('/api/posts/:id', (req, res) => {
+  const {id} = req.params;
+  db
+    .findById(id)
+      .then(post => {
+        if (post) {
+          res.json(post);
+        } else {
+          res
+            .status(404)
+            .json({message: "The post with the specififed ID does not exist."});
+        }
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({message: "The post informatin could not be retrieved."});
+      })
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+});
