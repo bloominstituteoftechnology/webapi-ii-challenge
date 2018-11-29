052864be847994105716f8e1b9562d9753cb5a3e
jbrock1981 <jbrock1981@gmail.com>
2018-11-29 13:25:37 -0600

    get and getById working

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // import your node modules
 const express = require('express');
 const db = require('./data/db.js');
-const PORT = 4000;
+
 // add your server code starting here
 const server = express();
+const PORT = 4000;
 
 server.get('/api/posts', (req, res) => {
     db.find()
@@ -16,4 +17,23 @@ server.get('/api/posts', (req, res) => {
         })
 })
 
-server.listen(PORT);
+server.get('/api/posts/:id', (req, res) => {
+    const {id} = req.params;
+    db.findById(id)
+        .then((post) => {
+            if(post) {
+                res.json(post);
+            } else {
+                res.status(404)
+                    .json({ message: "The post with the specified ID does not exist." })
+            }
+        })
+        .catch(err => {
+            res.status(500)
+                .json({ message: "The post information could not be retrieved."})
+        })
+})
+
+server.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+});
