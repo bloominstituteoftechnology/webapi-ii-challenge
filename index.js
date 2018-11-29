8dabd01f1266fa26660adeb8780a37bddb168d96
Alex Martin <atonymartin20@yahoo.com>
2018-11-28 21:02:52 -0600

    Project Started. Server running with all get endpoints.

--- index.js
+++ index.js
@@ -1,5 +1,47 @@
 // import your node modules
+const express = require('express');
 
 const db = require('./data/db.js');
 
+const server = express();
+const PORT = 4500;
+
 // add your server code starting here
+
+server.get('/api/posts', (request, response) => {
+    db.find()
+        .then((posts) => {
+            response.json(posts);
+        })
+
+        .catch(err => {
+            response
+                .status(500)
+                .json({ error: "The posts information could not be retrieved." });
+        });
+});
+
+server.get('/api/posts/:id', (request, response) => {
+    const { id } = request.params;
+    db.findById(id)
+        .then(post => {
+            if (post) {
+                response.json(post)
+            }
+            else {
+                response
+                    .status(404)
+                    .json({ message: "The post with the specified ID does not exist." });
+            }
+        })
+
+        .catch(err => {
+            response
+                .status(500)
+                .json({ error: "The post information could not be retrieved." });
+        })
+})
+
+server.listen(PORT, () => {
+    console.log(`Server is up and running on port ${PORT}`);
+});
