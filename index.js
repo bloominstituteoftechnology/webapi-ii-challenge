9702a871e38fac8fe73ce6fc617af348b8effd5f
Robin U Familara <robin.familara@cgius.com>
2018-11-28 22:34:54 -0600

    Get and 500 error -GET complete -Post completed

--- index.js
+++ index.js
@@ -1,24 +1,53 @@
 // import your node modules
 const express = require('express');
 const db = require('./data/db.js');
+const cors = require('cors');
 
 // add your server code starting here
 const server = express();
 
 // middleware - put when doing react app
+server.use(cors());
+server.use(express.json());
 
 // starting point
-server.get('/', (Req, res) => {
-    res.send('Test');
+server.get('/', (req, res) => {
+    res.send('Home');
 })
 // request handlers.
-server.get('/api/users', (req, res) => {
+// GET
+server.get('/api/posts', (req, res) => {
     db.find().then(users => {
         console.log('\n** users **', users);
-        res.json(users);
+        res.status(200).json(users);
     })
-    .catch(err => res.send(err))
+    .catch(err => res.status(500).json({ error: "The post information could not be retrieved."}));
 })
+
+server.get('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    db.findById(id)
+    .then(user => {
+        res.json(user);
+    })
+    .catch(err => res.status(404).json({ message: "The post with the specified ID does not exist." }));
+})
+
+// POST
+server.post('/api/posts', (req, res) => {
+    if (!req.body || !req.body.title || !req.body.contents) {
+        res.status(400).json({ error: "Please provide title and contents for the post." })
+    }
+    const { title, contents } = req.body;
+    const newPost = { title, contents };
+    db.insert(newPost)
+
+       .then(insertedPost => {
+           res.status(201) // created
+       })
+       .catch(err => (res.status(500).json({ error: 'There was an error while saving the post to the database.' })))
+    })
+
 
 const port = 8000;
 server.listen(port, () => console.log(`API running on port ${port}`))
