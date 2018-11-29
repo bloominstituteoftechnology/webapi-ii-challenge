4e672f0851acba8d1f4be172a0cd2d1b65e1f98b
Denise Escobar <descobar1490@gmail.com>
2018-11-28 22:10:11 -0600

    added my server and my node modules. I creaed the shells for both my get requests

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 // import your node modules
+const express =require('express');
+
 
 const db = require('./data/db.js');
 
 // add your server code starting here
+const server = express();
+const PORT =4000
+
+server.get('/api/posts', (req, res) =>{
+    
+});
+
+server.get(`/api/posts/:id`, (req, res) =>{
+
+});
+
+server.listen(PORT, () =>{
+    console.log(`server is up and running on port ${PORT}`);
+});
