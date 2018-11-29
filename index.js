bc0fb14bede37675ded037cbd462b958100ba2ae
cferrell2 <cferrell12182@gmail.com>
2018-11-28 22:31:27 -0500

    install dependencies and create server. Server listening on port 4000

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 // import your node modules
+const express = require('express');
+const server = express();
+server.use(express.json());
+const PORT = 4000;
 
 const db = require('./data/db.js');
 
 // add your server code starting here
+
+
+
+server.listen(PORT, () =>{
+    console.log(`Server is up and running on port ${PORT}`);
+});
