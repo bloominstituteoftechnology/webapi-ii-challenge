acce3b2b5032ae7bac48fb00c8317c4da3b5681d
Freekeyboy <jjhutton81@gmail.com>
2018-11-28 20:32:17 -0800

    Got the MVP for day one completed... I'm pretty sure.

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 // import your node modules
+const express = require('express');
+const server = express();
 
 const db = require('./data/db.js');
 
+ server.get('/api/posts', (req, res) => {
+    db.find()
+    .then(posts => {
+        res.status(200).json(posts)
+    })
+    .catch(err => {
+        res
+        .status(500).json({message: "This is probably your fault." })
+    })
+});
+
+ server.get('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+     db.findById(id)
+    .then(post => {
+        if (!post) {
+            res.status(404).json({ message: "TThis is not the post you are looking for." })
+        } else {res.status(200).json(post)}
+    })
+    .catch(err => {
+        res
+        .status(500).json({ error: "There's a cat in the server." })
+    })
+})
 // add your server code starting here
+ server.listen(9000, () => console.log("Hello! Is there anybody in there?")); 
