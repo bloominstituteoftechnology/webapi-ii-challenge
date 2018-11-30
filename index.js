366fe05fd4ac80e5b5c3f4c37ac6cf3a0952bc70
Bryce Evans <bryceevans@Bryces-MacBook-Pro.local>
2018-11-30 13:41:57 -0700

    Put and Delete accomplished.

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const cors = require('cors');
 
 const db = require('./data/db.js');
 
+
 // add your server code starting here
 
 const server = express();
 const PORT = 4000;
+const purser = express.json();
+
+server.use(purser);
 
 server.use(cors());
 
@@ -45,6 +49,57 @@ server.get('/api/posts/:id', (req, res) => {
       })
 });
 
+// Post request
+
+server.post('/api/posts', (req, res) => {
+  const posts = req.body;
+
+  if (posts.title && posts.contents) {
+    db
+      .insert(posts)
+      .then(idInfo => {
+        db.findById(idInfo.id).then(posts => {
+          res
+          .status(201)
+          .json(posts);
+        })
+      })
+      .catch(err => {
+        res
+        .status(500)
+        .json({ message: "There was an error while saving the post to the database" })
+      })
+  } else {
+    res
+    .status(400)
+    .json({ message: "Please provide title and contents for the post."})
+  }
+});
+
+// Delete Request
+
+server.delete('/api/posts/:id', (req, res) => {
+  const { id } = req.params;
+  db
+    .remove(id)
+    .then(count => {
+      if (count) {
+        res
+          .json({ message: "Successfully deleted."})
+      } else {
+        res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist."})
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ message: "The post could not be removed." })
+    })
+})
+
+// Always at the bottom!!!!!
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 });
