7a94d27c6ca67c47655f2a0401b04c84b8d3aea3
Rick Fleming <elfeming111@yahoo.com>
2018-11-29 21:56:15 -0700

    Added react frontend to project

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Node Express Lab</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
