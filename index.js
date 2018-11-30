acabeea8a8e16e425d31b6db7bf1d86eca458db4
Robin U Familara <robin.familara@cgius.com>
2018-11-29 21:27:49 -0600

    Refactored my solution

--- index.js
+++ index.js
@@ -1,66 +1,50 @@
 // import your node modules
 const express = require('express');
+
 const db = require('./data/db.js');
-const cors = require('cors');
+
+const server = express();
+const port = 9000;
 
 // add your server code starting here
-const server = express();
 
-// middleware - put when doing react app
-server.use(cors());
-server.use(express.json());
+// testing testing :D
+server.get('/', (_, res) => {
+  res.json({ message: 'Introduction to NodeJS and Express' });
+});
 
-// starting point
-server.get('/', (req, res) => {
-    res.send('Home');
-})
-// request handlers.
-// GET
-server.get('/api/posts', (req, res) => {
-    db.find().then(users => {
-        console.log('\n** users **', users);
-        res.status(200).json(users);
+server.get('/api/posts', (_, res) => {
+  db.find('posts')
+    .then(users => {
+      res.status(200).json(users);
     })
-    .catch(err => res.status(500).json({ error: "The post information could not be retrieved."}));
-})
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The posts information could not be retrieved." });
+    })
+});
 
 server.get('/api/posts/:id', (req, res) => {
-    const { id } = req.params;
-    db.findById(id)
-    .then(user => {
-        res.json(user);
+  id = req.params.id;
+
+  db.findById(id)
+    .then(post => {
+    if (post.length === 0) {
+        res
+          .status(404)
+          .json({ error: "The post with the specified ID does not exist." });
+    } else {
+      res.status(200).json(post);      
+      }
     })
-    .catch(err => res.status(404).json({ message: "The post with the specified ID does not exist." }));
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The post information could not be retrieved." });
+    })
+});
+
+server.listen(port, () => {
+  console.log(`server listening on port ${port}`);
 })
-
-// POST
-server.post('/api/posts', (req, res) => {
-    if (!req.body || !req.body.title || !req.body.contents) {
-        res.status(400).json({ error: "Please provide title and contents for the post." })
-    }
-    const { title, contents } = req.body;
-    const newPost = { title, contents };
-    db.insert(newPost)
-
-       .then(insertedPost => {
-           res.status(201) // created
-       })
-       .catch(err => (res.status(500).json({ error: 'There was an error while saving the post to the database.' })))
-    })
-
-// DELETE
-server.delete('/api/posts/:id', (req, res) => {
-    console.log(req.params);
-    const { id } = req.params;
-    db.remove(id)
-    .then(removedPost => {
-        console.log(removedPost);
-        if(!id) {
-            return res.status(404).json({ message: "The post with the specified ID does not exist." });
-        }
-        res.status(200).json({ message: `The post with id ${ id } was successfully deleted.`});;
-    })
-    .catch(err => res.status(500).json({ error: "The post could not be removed" }));
-})
-const port = 8000;
-server.listen(port, () => console.log(`API running on port ${port}`))
