c3db77c419bfa4ba3136ee1992c4a092887378b1
TheGameFreak720 <luis.alvarez@commithub.com>
2018-11-29 23:44:44 -0500

    started to do put request

--- index.js
+++ index.js
@@ -73,6 +73,38 @@ app.delete('/api/posts/:id', (req, res) => {
         });
 });
 
+app.put('/api/post/:id', (req, res) => {
+    const post = req.body;
+    const id = req.params.id;
+
+    if (post) {
+        db.findById(id)
+        .then(post => {
+            if (post[0]) {
+                db.insert(id, post)
+                    .then(count => {
+                        if (count) {
+                            db.findById(id)
+                                .then(post => {
+                                    res.json(post);
+                                })
+                        } else {
+                            res.status(500).json({ error: "The post information could not be modified." })
+                        }
+                 });
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." });
+            }
+        })
+        .catch(() => {
+            res.status(500).json({ message: "The post with the specified ID does not exist." });
+        });
+    } else {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    }
+    
+});
+
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
