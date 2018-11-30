7af4576f6ce6adc588f5a970a5c08f9c70b9122a
Jorlost19 <39849563+Jorlost19@users.noreply.github.com>
2018-11-30 00:09:45 -0600

    successfully implemented swagger for API documentation you can test the API at http://localhost:4000/api-docs/

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+swaggerUi = require('swagger-ui-express'),
+swaggerDocument = require('./swagger.json');
 
 const db = require('./data/db.js');
-
+const router = express.Router()
 const server = express();
+server.use(express.json());
 const PORT = 4000;
 
 server.get('/api/posts', (req, res) => {
@@ -25,39 +28,36 @@ server.get('/api/posts/:id', (req, res) => {
 })
 
 server.post('/api/posts/', (req, res) => {
-    const { title, contents } = req.query;
-    if(!title||!contents){res.status(400).json({ errorMessage: "Please provide title and contents for the post." })}
+    const post = req.body;
+    if(!post.title||!post.contents){res.status(400).json({ errorMessage: "Please provide title and contents for the post." })}
     else {
-    const post = {title, contents}
     db.insert(post)
-        .then(id => res.status(201).json({...post, ...id}))
+        .then(idInfo => db.findById(idInfo.id).then(post => res.status(201).json(post[0])))
         .catch(err => res.status(500).json({error: "There was an error while saving the post to the database"}))}
 })
 
 server.delete('/api/posts/:id', (req, res) => {
     const { id } = req.params;    
     const post = db.findById(id).then(post => post);
-    // if(!post.length){res.status(404).json({message: "The post with the specified ID does not exist."})}
-    
     db.remove(id)
-        .then(records => res.json(post))
+        .then(records => records ? res.json(post) : res.status(404).json({message: "The post with the specified ID does not exist."}))
         .catch(err => res.status(500).json({error: "The post could not be removed"}))
     }
 )
 
 server.put('/api/posts/:id', (req, res) => {
     const { id } = req.params;
-    const {title, contents} = req.query;
-    const post = db.findById(id).then(post => post);
-    // if(!post.length){res.status(404).json({message: "The post with the specified ID does not exist."})};
-    if(!title||!contents){res.status(400).json({errorMessage: "Please provide title and contents for the post."})}
+    const post = req.body;
+    if(!post.title||!post.contents){res.status(400).json({errorMessage: "Please provide title and contents for the post."})}
     else{
-        const updatedPost = {title, contents};
-        db.update(id, updatedPost)
-            .then(records => res.json({...updatedPost, id}))
+        db.update(id, post)
+            .then(records => records ? db.findById(id).then(post => res.json(post)) : res.status(404).json({message: "The post with the specified ID does not exist."}))
             .catch(err => res.status(500).json({error: "The post information could not be modified."}))
         }
     }
 )
+
+server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+server.use('/api/posts', router)
 
 server.listen(PORT, () => console.log(`Server is up and running on port ${PORT}`));
