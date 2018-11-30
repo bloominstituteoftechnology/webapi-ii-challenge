b47e5aa08f0d63f82c71cc26e6f7e11cb783a632
Robert Billington <chillingtonbillington@gmail.com>
2018-11-30 12:17:36 -0600

    Wrote get by id. Not working, and I'm confused as to why?

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
 // import your node modules
 
-const db = require('./data/db.js');
+
 const express = require('express');
 const server = express();
 const PORT = 4000;
+const db = require('./data/db');
 
 server.get('/api/posts', (req, res) =>{
     db.find()
-        .then(post =>{
-            res.json(post)
+        .then(posts =>{
+            //console.log('This be posts', post)
+            res.json(posts)
         })
         .catch(err =>{
-            res.status(500).json({error: 'Could not get post data'})
+            res.status(500).json({error: 'Could not get posts data'})
         })
 })
 server.get('api/posts/:id', (req, res) =>{
-   
+   const { id } = req.params;
+   db.findById(id)
+    .then(post =>{
+        if(post.length){
+            res.json(post)
+        }else{
+            res.status(500).json({error:'Could not get specified post'})
+        }
+    })
+    .catch(err =>{
+        res.status(500).json({error: `User with ID of ${id} does not exist`})
+    })
+  
 })
+
 server.listen(PORT, () =>{
     console.log('The server is working!');
 })
