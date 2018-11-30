481d01f70fb8d25c42bc9fb247ce32707cf559e0
Megan Fontenot <lambda_school_loaner_28@Lambda-School-Loaner-28s-MacBook-Pro.local>
2018-11-29 21:30:51 -0600

    id displaying

--- index.js
+++ index.js
@@ -25,3 +25,22 @@ server.get('/api/posts', (req, res) => {
         res.json(`Huh, can't find those posts`)
     })
 })
+
+server.get('/api/posts/:id', (req, res) => {
+    const {id} = req.params;
+    db.findById(id)
+    .then(post => {
+        if(post) {
+            res.json(post);
+
+        } else {
+
+            status(404)
+            res.json('Error 404: Unknown post')
+        }
+    })
+    .catch(err => {
+        res.status(500)
+        res.json('Error 500: Unknown post')
+    })
+})
