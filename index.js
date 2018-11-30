90968566f22eaf9b3de756143bea86455320717f
iepoch <staclem@live.com>
2018-11-30 13:35:23 -0700

    Finished MVP

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ db.find()
 });
 
 server.get("/api/posts/:id", (req, res) => {
-const { id } = req.params;
+    const { id } = req.params;
+
 
 db.findById(id)
     .then(post => {
@@ -43,36 +44,45 @@ db.findById(id)
 });
 
 server.post("/api/posts", (req, res) => {
-    const postsInfo = req.body;
+    const posts= req.body;
+    if (posts.title && posts.contents) {
+        db.insert(posts)
+            .then(response => {
 
-    db.insert(postsInfo)
-        .then(post => {
-                res.status(201)
-                .json(post)
+                db.findById(response.id).then(posts => {
+                    res.status(201)
+                    .json(posts)
+                })
 
-        })
-        .catch(err => {
-            if (err.errno === 19) {
-                res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
-
-            }
-            else {
-                res.status(500).json({ error: "There was an error while saving the post to the database" })
-            }
             })
+            .catch(err => {
+                res
+                    .status(500)
+                    .json({ error: "There was an error while saving the post to the database" })
+
+            })
+    } else {
+        res
+            .status(400)
+            .json({errorMessage: "Please provide title and contents for the post."})
+    }
 })
 
 
 server.delete("/api/posts/:id", (req, res) => {
     const { id } = req.params
+
     db.remove(id)
         .then(count => {
-            if (count) {
-                res.json({message: "Successfully Deleted"})
-            }
-            else {
+            count ?
+                db.find()
+                    .then(posts => {
+                        res.status(200)
+                            .json(posts)
+                })
+            :
                 res.status(404).json({message: "Invalid Id"})
-            }
+
         })
         .catch(err => {
             res
@@ -81,6 +91,36 @@ server.delete("/api/posts/:id", (req, res) => {
         })
 })
 
+
+server.put('/api/posts/:id', (req, res) => {
+    const posts = req.body
+    const { id } = req.params
+    if (posts.title && posts.contents) {
+        db.update(id, posts)
+            .then(count => {
+                count ?
+                    db.findById(id).then(posts => {
+                        res.json(posts)
+                    })
+                    :
+                    res
+                        .status(404)
+                        .json({ message: "The post with the specified ID does not exist." })
+            })
+            .catch(err => {
+                res
+                    .status(500)
+                    .json({ error: "The post information could not be modified."})
+            })
+
+    } else {
+        res
+        .status(400)
+            .json({ errorMessage: "Please provide title and contents for the post."})
+    }
+
+})
+
 server.listen(PORT, () => {
-  console.log("server is up and running");
+console.log("server is up and running");
 });
