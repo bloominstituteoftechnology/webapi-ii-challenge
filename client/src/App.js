81633a70eb9b3cdc99bcc675b464b0f5a002eacb
Jennifer King <jenbellpgh@gmail.com>
2018-11-30 00:46:18 -0500

    Boiler plate for stretch goals - frontend client

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import Quotes from './Quotes/Quotes';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Quotes />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/Quotes/Quotes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import axios from 'axios';
+
+const Quotes = (props) => {
+  return (
+    <div>Quotes Here</div>
+  );
+};
+
+export default Quotes;
