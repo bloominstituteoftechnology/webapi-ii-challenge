175bf038b7094e1b8f2c8163f4899c95f7a15b86
Ryntak94 <ryan.matthews.professional@gmail.com>
2018-11-30 21:41:03 -0500

    Add 'post' functionality

--- index.js
+++ index.js
@@ -5,38 +5,52 @@ const db = require('./data/db.js');
 
 // add your server code starting here
 const server = express();
-
+const parser = express.json();
 const PORT = "4000"
+
+server.use(parser);
 
 server.get('/api/posts', (req, res)  =>  {
     db.find()
-        .then((users)   =>  {
-            res.send(users);
+        .then((posts)   =>  {
+            res.send(posts);
         })
         .catch(err  =>  {
-            console.log("error:", err);
+            res.status(500).json({ message: "The posts information could not be retrieved." });
         });
 })
 
 server.get('/api/posts/:id', (req, res)  =>  {
     const { id } = req.params;
     db.findById(id)
-        .then((user)   =>  {
-            res.send(user);
+        .then((posts)   =>  {
+            if(posts.length === 1)   {
+                res.send(posts);
+            }   else{
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+            }
+
         })
         .catch(err  =>  {
-            console.log("error:", err);
+            res.status(500).json({ error: "The post information could not be retrieved." });
         });
 })
 
-// server.delete('/api/posts/:id', (req, res)    =>  {
-//     const { id } = req.params;
-//     db.remove(id)
-//         .then((response)    =>  {
-//             console.log(response)
-//         })
-//
-// })
+server.post('/api/posts', (req, res)    =>  {
+    const post = req.body;
+    if(post.title === "" || post.contents === "")   {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    }
+    db.insert(post)
+        .then((post)  =>  {
+            res.status(201).json(post)
+        })
+        .catch(err  =>  {
+            res
+            .status(500)
+            .json({ error: "There was an error while saving the post to the database" });
+        })
+})
 
 server.listen(PORT, () =>  {
     console.log("server started. Kind of..");
