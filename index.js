781d51063a75c62ee6dd82a71f36dcc34aeccd45
Michael Trew <michaeltrew@Michaels-iMac.attlocal.net>
2018-12-01 14:26:04 -0500

    post done

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const server = express();
 const db = require('./data/db.js');
 const PORT = 4001;
+const parser = express.json();
+server.use(parser);
 // add your server code starting here
 server.get('/api/posts', (request, response) => {
   db.find()
@@ -18,7 +20,7 @@ server.get('/api/posts/:id', (request, response) => {
   const { id } = request.params;
   db.findById(id)
     .then((post => {
-      if (post.length>0) {
+      if (post) {
         response.json(post);
       }
       else {
@@ -28,6 +30,23 @@ server.get('/api/posts/:id', (request, response) => {
     .catch(err => {
       response.status(500).json({ error: 'failed to get post' })
     });
+});
+
+server.post('/api/posts', (req, res) => {
+  const post = req.body;
+
+  if (post.title && post.contents) {
+    db.insert(post).then(idInfo => {
+      db.findById(idInfo.id).then(post => {
+        res.status(201).json(post);
+      });
+    }).catch(err => {
+      res.status(500).json({ message: "failed to insert post in database" });
+    });
+  } else {
+    res.status(400).json({ message: "missing title or contents" })
+
+  }
 });
 
 
