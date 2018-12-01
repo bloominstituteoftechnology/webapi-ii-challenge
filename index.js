ac98a0ff29f410fff41413f2c0b9a32240fa39a7
Kendra Williams <kmonique.williams@gmail.com>
2018-11-30 21:09:30 -0500

    post method created, but if statement always results to false

--- index.js
+++ index.js
@@ -3,10 +3,11 @@
 const db = require('./data/db.js');
 const express = require("express");
 const server = express();
+//add parser
+server.use(express.json());
 const PORT = 4000;
 
-// add your server code starting here
-
+//get request
 server.get("/api/posts", (req, res) => {
    db.find()
       .then(post => {
@@ -36,6 +37,26 @@ server.get("/api/posts/:id", (req, res) => {
          res.status(404)
             .json({ message: "The post with the specified ID does not exist." })
       });
+});
+
+//post request
+server.post("/api/posts", (req, res) => {
+   const post = req.body;
+   console.log(post);
+   if(post.title && post.body) {
+      db.insert(post)
+         .then(postId => {
+            db.findById(postId)
+               .then(post => {
+                  res.status(201).json(post)
+               })
+         })
+         .catch(err => {
+            res.status(500).json({ error: "There was an error while saving the post to the database" });
+         })
+   } else {
+      res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+   }
 });
 
 server.listen(PORT, () => {
