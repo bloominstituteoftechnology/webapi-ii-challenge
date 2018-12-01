4036b06328f59bd341111d23bd99d314e9753118
unknown <kyleteeter12@gmail.com>
2018-12-01 11:49:01 -0600

    Posting new post to API and returning it.

--- index.js
+++ index.js
@@ -27,10 +27,31 @@ server.get('/api/posts/:id', (req, res) => {
                 res.status(404)
                 .json({message: "The post with the specified ID does not exist." })
             }
+            res.json(post);
         })
         .catch(err =>
             res.status(500)
             .json({ error: "The post information could not be retrieved." }))
 })
+
+server.post('/api/posts'), (req, res) => {
+    const post = req.body;
+
+    if (post.title && post.contents){
+        db.insert(post).then(idReturn => {
+            db.findById(idReturn.id).then
+            (post => {
+                res.status(201).json(post);
+            });
+            }) .catch(err => {
+                res.status(500)
+                .json({error: "There was an error while saving the post to the database" })
+        });
+    }else {
+        res.status(400).json({errorMessage: "Please provide title and contents for the post." })
+    }  
+    }; 
+
+
 
 server.listen(PORT, () => console.log(`Server is running on ${PORT}`));
