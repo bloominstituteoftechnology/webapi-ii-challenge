2cd222b8094dfbb70d2e5c7f9da8b8a30ac3a3c3
Dewayne87 <dewaynelindsay87@gmail.com>
2018-12-01 10:01:32 -0800

    added update functionailty

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const server = express();
 const PORT = 4000;
 const db = require('./data/db');
 server.use(express.json())
-// add your server code starting here
 server.disable("etag");
+
+// add your server code starting here
 server.get('/api/posts', (req, res) => {
     db.find()
         .then(posts => {
@@ -43,18 +44,19 @@ server.post('/api/posts', (req, res) => {
     const post = req.body;
 
     if (post.title && post.contents) {
-        db.insert(post).then(idInfo => {
-            console.log(idInfo.id)
-            db.findById(idInfo.id).then(post => {
+        db.insert(post)
+            .then(idInfo => {
+                db.findById(idInfo.id)
+                    .then(post => {
+                        res
+                            .status(201)
+                            .json(post);
+                    });
+            }).catch(err => {
                 res
-                    .status(201)
-                    .json(post);
-            });
-        }).catch(err => {
-            res
-                .status(500)
-                .json({ error: "There was an error while saving the post to the database" })
-        })
+                    .status(500)
+                    .json({ error: "There was an error while saving the post to the database" })
+            })
     } else {
         res
             .status(400)
@@ -62,27 +64,55 @@ server.post('/api/posts', (req, res) => {
     }
 })
 
+server.put('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    const updatedPost = req.body;
+    db.update(id, updatedPost)
+        .then(count => {
+            if (updatedPost.title && updatedPost.contents && count) {
+                db.findById(id)
+                    .then(post => {
+                        res
+                            .status(200)
+                            .json(post);
+                    });
+            } else if (!count && updatedPost.title && updatedPost.contents) {
+                res
+                    .status(404)
+                    .json({ message: "The post with the specified ID does not exist." })
+            } else {
+                res
+                    .status(400)
+                    .json({ errorMessage: "Please provide title and contents for the post." })
+            }
+        }).catch(err => {
+            res
+                .status(500)
+                .json({ error: "The post information could not be modified." })
+        })
+})
+
 server.delete('/api/posts/:id', (req, res) => {
     const { id } = req.params;
     let foundPost;
     db.findById(id).then(post => { foundPost = post });
-    db.remove(id).then(count => {
-        if (count) {
+    db.remove(id)
+        .then(count => {
+            if (count) {
+                res
+                    .status(200)
+                    .json(foundPost);
+            } else {
+                res
+                    .status(404)
+                    .json({ message: "The post with the specified ID does not exist." })
+            }
+        }).catch(err => {
             res
-                .status(200)
-                .json(foundPost);
-        } else {
-            res
-                .status(404)
-                .json({ message: "The post with the specified ID does not exist." })
-        }
-    }).catch(err => {
-        res
-            .status(500)
-            .json({ error: "The post could not be removed" })
-    })
+                .status(500)
+                .json({ error: "The post could not be removed" })
+        })
 })
-
 
 server.listen(PORT, error => {
     if (error) console.log(error);
