40c0ef956b4b0fef44e6d448df02723757d3d417
Denise Escobar <descobar1490@gmail.com>
2018-12-01 13:25:45 -0600

    I created a post request that returns the post

--- index.js
+++ index.js
@@ -6,7 +6,30 @@ const db = require('./data/db.js');
 
 // add your server code starting here
 const server = express();
+const parser = express.json();
 const PORT =4000;
+
+server.use(parser);
+
+//POST /api/posts
+
+server.post('/api/posts', (req, res) =>{
+    const post = req.body
+    db.insert(post)
+    .then( idInfo =>{
+        db.findById(idInfo.id).then(user =>{
+        console.log("post from insert method",user)
+        res
+        .status(201)
+        .json(user);
+        })
+    })
+    .catch(err =>{
+        res
+        .status(500)
+        .json({ error: "There was an error while saving the post to the database" })
+    });
+});
 
 server.get('/api/posts', (req, res) =>{
     db.find()
@@ -22,10 +45,8 @@ server.get('/api/posts', (req, res) =>{
 
 server.get('/api/posts/:id', (req, res) =>{
     const { id } = req.params
-    //console.log(req.params)
     db.findById(id)
     .then(post =>{
-        console.log(post.length)
         if (post.length !== 0){
             res.json(post);
         } else {
@@ -35,12 +56,16 @@ server.get('/api/posts/:id', (req, res) =>{
         }
     })
     .catch(err =>{
-        console.log("not working")
         res
         .status(500)
         .json({ error: "The posts information could not be retrieved." })
     })
 });
+
+
+//DELETE /api/posts/:id 
+
+//PUT /api/posts/:id
 
 server.listen(PORT, () =>{
     console.log(`server is up and running on port ${PORT}`);
