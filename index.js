fa6b89eaed38d9f54d81ccc3479f63dffdaffa24
Pascale Pierre <pascaleee@gmail.com>
2018-12-01 17:26:07 -0600

    put update is now working

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const parser = express.json();
 const server = express();
 const PORT = 4000;
 
-server.use(parser)
+server.use(parser);
 
 server.get("/", (req, res) => {
   res.json({ message: "server is listening" });
@@ -43,25 +43,70 @@ server.get("/api/posts/:id", (req, res) => {
     });
 });
 
-server.post('/api/posts', (req, res) => {
+server.post("/api/posts", (req, res) => {
   const post = req.body;
-  if (post.title && post.contents){
-  db.insert(post).then(postId => {
-      db.findById(postId.id).then(post => {
-      res.status(201).json(post);
+  if (post.title && post.contents) {
+    db.insert(post)
+      .then(postId => {
+        db.findById(postId.id).then(post => {
+          res.status(201).json(post);
+        });
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: "There was an error while saving the post to the database"
+        });
+      });
+  } else {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
     });
-  }).catch(err => {
-      res
-        .status(500)
-        .json({message: "failed to insert post in db" });
-    });
-  }
-  else{
-    res.status(400).json({message: "missing title or content"});
   }
 });
 
+server.delete("/api/posts/:id", (req, res) => {
+  const { id } = req.params;
+  db.remove(id)
+    .then(count => {
+      if (count) {
+        res.json({ message: "works" });
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: "The post could not be removed" });
+    });
+});
 
+server.put("/api/posts/:id", (req, res) => {
+  const { id } = req.params;
+  const post = req.body;
+
+  if (post.title && post.contents) {
+    db.update(id, post)
+      .then(count => {
+        if (count) {
+          db.findById(id).then(user => {
+            res.json(user);
+          });
+        } else {
+          res.status(404).json({
+            message: "The post with the specified ID does not exist."
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ error: "The post could not be removed" });
+      });
+  } else {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    });
+  }
+});
 
 server.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
