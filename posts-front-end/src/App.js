60ce73ccdc004d7d31124f8460e4aa23e4aba2d8
Ryntak94 <ryan.matthews.professional@gmail.com>
2018-12-01 14:40:13 -0500

    Finished stretch goals

--- posts-front-end/src/App.js
+++ posts-front-end/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Posts from './components/Posts.js';
 import SinglePost from './components/SinglePost.js';
+import Form from './components/Form.js';
 import { Route, withRouter } from 'react-router-dom';
 import axios from 'axios';
 
@@ -32,8 +33,11 @@ class App extends Component {
     render() {
         return (
             <div className="App">
+                <Route exact path="/" render={(props) =>  <Form posts={this.state.posts} getPosts={this.getPosts} {...props}/>} />
                 <Route exact path="/" render={(props)   =>  <Posts posts={this.state.posts} getPosts={this.getPosts} {...props}/>} />
                 <Route exact path="/posts" render={(props)   =>  <Posts posts={this.state.posts} getPosts={this.getPosts} {...props}/>} />
+                <Route exact path="/posts" render={(props) =>  <Form posts={this.state.posts} getPosts={this.getPosts} {...props}/>} />
+                <Route path="/post/:id" render={(props) =>  <Form posts={this.state.posts} getPosts={this.getPosts} {...props}/>} />
                 <Route path="/post/:id" render={(props) =>  <SinglePost posts={this.state.posts} getPosts={this.getPosts} {...props}/>} />
             </div>
         );
--- posts-front-end/src/components/Form.js
+++ posts-front-end/src/components/Form.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Form extends Component    {
     constructor(props)  {
@@ -6,13 +7,47 @@ class Form extends Component    {
         this.state  =   {
             title: "",
             contents: "",
+            buttonContent: "Submit"
         }
     }
+
+
+    onChangeHandler =   ({target})  =>  {
+        this.setState((state)   =>  ({
+            [target.name]: target.value,
+        }))
+    }
+
+    onClickHandler  =   ()  =>  {
+        if(this.props.match.params.id)  {
+            axios.put(`http://localhost:4000/api/posts/${this.props.match.params.id}`, { title: this.state.title, contents: this.state.contents} )
+                .then((data)    =>  {
+                    this.props.getPosts()
+                    this.setState((state)   =>  ({
+                        title: "",
+                        contents: ""
+                    }))
+                })
+        }   else {
+            axios.post(`http://localhost:4000/api/posts`, { title: this.state.title, contents: this.state.contents })
+                .then((data)    =>  {
+                    this.props.getPosts()
+                    this.setState((state)   =>  ({
+                        title: "",
+                        contents: ""
+                    }))
+                })
+        }
+    }
+
     render()    {
         return(
             <div>
-                <input placeholder="title" value={this.state.title} />
-                <input placeholder="contents" value={this.state.contents} />
+                <input onChange={this.onChangeHandler} name="title" placeholder="title" value={this.state.title} />
+                <input onChange={this.onChangeHandler} name="contents" placeholder="contents" value={this.state.contents} />
+                <div onClick={this.onClickHandler}>
+                    {this.state.buttonContent}
+                </div>
             </div>
         )
     }
