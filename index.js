d8574fa41b86f08a9bce93e56e8b06e344d0a256
Javier Alavrez <javieralvarez82@live.com>
2018-12-01 10:31:37 -0800

    server post werfied working tested with valid data and non-valid data now working on server delete

--- index.js
+++ index.js
@@ -24,69 +24,102 @@ server.get(`/api/posts`, (req, res) => {
 server.get('/api/posts/:id', (req, res) => {
     const { id } = req.params;
     db.findById(id)
-        .then(( posts ) => {
-            if( posts.length > 0 ) {
-            res.status(200)
-            .send(posts)
+        .then((posts) => {
+            if (posts.length > 0) {
+                res.status(200)
+                    .send(posts)
             }
             else {
                 res
-                .status(404)
-                .json({errorMessage: "Post Id Not Found"})
-            }
-        })
-        .catch( err => {
-            res
-            .status(500)
-            .json({errorMessage: "Post Id Not Found"})
-        })
-})
-
-//POST
-server.post('/api/post', (req, res) => {
-    const post = req.body;
-    if (post.title && post.content) {
-        db.insert(post)
-            .then(idInfo => {
-                db.findById(idInfo.id)
-                    .then ( post => {
-                    res
-                    .status(201).json( post );
-                })
-                res.status(201).json( idInfo )
-                    .catch( err => {
-                        res
-                            .status(400)
-                            .json({
-                                errorMessage: "Please provide title and contents for the post."
-                            })
-                    })
-            })
-    }
-})
-
-//DELETE
-
-server.delete(`/api/users/:id`, (req, res) => {
-    const { id } = req.params;
-    db.remove(id)
-        .then(post => {
-            if (post) {
-                db.json({ message: "Delete success" })
-            }
-            else {
-                res.status(404)
-                    .json({ message: "Invalid Id" })
+                    .status(404)
+                    .json({ errorMessage: "Post Id Not Found" })
             }
         })
         .catch(err => {
             res
-                .status(400)
-                .json({
-                    errorMessage: "Delete Failed"
-                });
-        })
+                .status(500)
+                .json({ errorMessage: "Post Id Not Found" })
+        });
 })
+
+//POST
+// server.post(`/api/post`, (req, res) => {
+//     const post = req.body;
+//     console.log(post);
+//     if ( post.title && post.contents ) {
+//         db.insert( post )
+//             .then( idInfo => {
+//                 db.findById( idInfo.id )
+//                     .then( post => {
+//                         res
+//                         .status( 201 )
+//                         .json( post );
+//                     });
+//                 res.status(201).json(idInfo)
+//                     .catch( err => {
+//                         res
+//                         .status(400)
+//                         .json({ errorMessage: err });
+//                     });
+//             });
+//     }
+// });
+
+server.post('/api/posts', (req, res) => {
+    const post = req.body;
+    console.log(post);
+    if(post.title && post.contents) {
+        db.insert( post )
+        .then( idInfo => {
+            db.findById( idInfo.id )
+                .then( newPost => {
+                    res
+                     .status(201)
+                     .send( newPost);
+                }) // newPost end
+        }) // idInfo end   
+    }
+    else if(post.title || post.content === undefined){
+        res
+         .status(400)
+         .json({errorMessage: "Please provide title and contents for the post." })
+        }
+    else {
+        res
+         .status(500)
+         .json({ error: "There was an error while saving the post to the database" })
+    }
+
+  });
+
+
+
+
+
+
+
+//DELETE
+
+// server.delete(`/api/users/:id`, (req, res) => {
+//     const { id } = req.params;
+//     db.remove(id)
+//         .then(post => {
+//             if (post) {
+//                 db.json({ message: "Delete success" })
+//             }
+//             else {
+//                 res.status(404)
+//                     .json({ message: "Invalid Id" })
+//             }
+//         })
+//         .catch(err => {
+//             res
+//                 .status(400)
+//                 .json({
+//                     errorMessage: "Delete Failed"
+//                 });
+//         })
+// })
 
 //PUT
 
