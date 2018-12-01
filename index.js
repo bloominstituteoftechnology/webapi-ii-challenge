80103a6e667fbe1aae46de81dc8ecd3d082a4f56
Ryntak94 <ryan.matthews.professional@gmail.com>
2018-11-30 22:09:03 -0500

    Add put and delete functionality

--- index.js
+++ index.js
@@ -13,10 +13,13 @@ server.use(parser);
 server.get('/api/posts', (req, res)  =>  {
     db.find()
         .then((posts)   =>  {
-            res.send(posts);
+            res
+            .send(posts);
         })
         .catch(err  =>  {
-            res.status(500).json({ message: "The posts information could not be retrieved." });
+            res
+            .status(500)
+            .json({ message: "The posts information could not be retrieved." });
         });
 })
 
@@ -25,30 +28,88 @@ server.get('/api/posts/:id', (req, res)  =>  {
     db.findById(id)
         .then((posts)   =>  {
             if(posts.length === 1)   {
-                res.send(posts);
+                res
+                .send(posts);
             }   else{
-                res.status(404).json({ message: "The post with the specified ID does not exist." })
+                res
+                .status(404)
+                .json({ message: "The post with the specified ID does not exist." })
             }
 
         })
         .catch(err  =>  {
-            res.status(500).json({ error: "The post information could not be retrieved." });
+            res
+            .status(500)
+            .json({ error: "The post information could not be retrieved." });
         });
 })
 
 server.post('/api/posts', (req, res)    =>  {
     const post = req.body;
     if(post.title === "" || post.contents === "")   {
-        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+        res
+        .status(400)
+        .json({ errorMessage: "Please provide title and contents for the post." })
     }
     db.insert(post)
         .then((post)  =>  {
-            res.status(201).json(post)
+            res
+            .status(201)
+            .json(post)
         })
         .catch(err  =>  {
             res
             .status(500)
             .json({ error: "There was an error while saving the post to the database" });
+        })
+})
+
+server.delete('/api/posts/:id', (req, res)  =>  {
+    const { id } = req.params;
+    db.remove(id)
+        .then((count)    =>  {
+            if(count === 0) {
+                res
+                .status(404)
+                .json({ message: "The post with the specified ID does not exist." })
+            }   else {
+                res
+                .json({ message: "Success!"})
+            }
+        })
+        .catch(err  =>  {
+            res
+            .status(500)
+            .json({ error: "The post could not be removed" })
+        })
+});
+
+server.put('/api/posts/:id', (req, res) =>  {
+    const { id } = req.params;
+    const post = req.body;
+    if(post.title === "" || post.contents === "" || !post.contents || !post.title)   {
+        res
+        .status(400)
+        .json({ errorMessage: "Please provide title and contents for the post." })
+    }
+    db.update(id, post)
+        .then((count)    =>  {
+            if(count === 0) {
+                res
+                .status(404)
+                .json({ message: "The post with the specified ID does not exist." })
+            }   else {
+                db.findById(id)
+                    .then(post  =>  {
+                        res
+                        .json(post)
+                    })
+            }
+        })
+        .catch(err  =>  {
+            res
+            .status(500)
+            .json({ error: "The post information could not be modified." })
         })
 })
 
