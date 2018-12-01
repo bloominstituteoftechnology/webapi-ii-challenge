e7a66d8b5e74a91f8a69cb4e3125c45c5300568e
unknown <kyleteeter12@gmail.com>
2018-12-01 12:36:34 -0600

    Finished Update with Put method.

--- index.js
+++ index.js
@@ -52,6 +52,32 @@ server.post('/api/posts', (req, res) => {
     }  
     }); 
 
+    server.put('/api/posts/:id', (req, res) => {
+        const post = req.body;
+        const {id} = req.params;
+
+        if (post.title && post.contents){
+            db.update(id, post).then(count => {
+                if (count) {
+                    db.findById(id).then(post => {
+                        res.json(post);
+                    })
+                }else {
+                    res.status(404).json({message: "The post with the specified ID does not exist."})
+                }
+            })
+            .catch(err => {
+                res.status(500)
+                .json({error: "The post information could not be modified."})
+            })
+
+        }else {
+            res.status(400).json({errorMessage: "Please provide title and contents for the post."})
+        }
+    })
+
+    server.delete('/api/posts/:id', (req, res))
+
 
 
 server.listen(PORT, () => console.log(`Server is running on ${PORT}`));
