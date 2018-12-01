ee5b02c5841ea909abdea62d358737a0c1e6e3aa
Alex Martin <atonymartin20@yahoo.com>
2018-12-01 08:41:39 -0600

    Got post to work and ran create react app

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ const db = require('./data/db.js');
 const server = express();
 const PORT = 4500;
 
+server.use(express.json());
+
 // add your server code starting here
 
+//GETS
 server.get('/api/posts', (request, response) => {
     db.find()
         .then((posts) => {
@@ -42,6 +45,41 @@ server.get('/api/posts/:id', (request, response) => {
         })
 })
 
+//POST
+server.post('/api/posts', (req, res) => {
+    const post = req.body;
+    if (post.title && post.contents) {
+        db.insert(post).then(idInfo => {
+            db.findById(idInfo.id)
+                .then(post => {
+                    res.status(201).json(post);
+                });
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .json({ error: "There was an error while saving the post to the database" });
+        })
+    }
+
+    else {
+        res
+            .status(400)
+            .json({ errorMessage: "Please provide title and contents for the post." });
+    }
+});
+
+//DELETE
+server.delete('/api/posts/:id', (req, res) => {
+
+});
+
+//PUT
+server.put('/api/posts/:id', (req, res) => {
+
+});
+
+// listening
 server.listen(PORT, () => {
     console.log(`Server is up and running on port ${PORT}`);
 });
