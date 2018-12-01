68c4a0519d0abaab8cc1f9cebff5f02c0cbd8cc2
Dewayne87 <dewaynelindsay87@gmail.com>
2018-11-30 20:25:02 -0800

    changed server.listen function callback and finished delete functionaility

--- index.js
+++ index.js
@@ -43,7 +43,8 @@ server.post('/api/posts', (req, res) => {
     const post = req.body;
 
     if (post.title && post.contents) {
-        db.insert(post).then(idInfo => { console.log(idInfo.id)
+        db.insert(post).then(idInfo => {
+            console.log(idInfo.id)
             db.findById(idInfo.id).then(post => {
                 res
                     .status(201)
@@ -61,7 +62,29 @@ server.post('/api/posts', (req, res) => {
     }
 })
 
-
-server.listen(PORT, () => {
-    console.log('server is running')
+server.delete('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    let foundPost;
+    db.findById(id).then(post => { foundPost = post });
+    db.remove(id).then(count => {
+        if (count) {
+            res
+                .status(200)
+                .json(foundPost);
+        } else {
+            res
+                .status(404)
+                .json({ message: "The post with the specified ID does not exist." })
+        }
+    }).catch(err => {
+        res
+            .status(500)
+            .json({ error: "The post could not be removed" })
+    })
 })
+
+
+server.listen(PORT, error => {
+    if (error) console.log(error);
+    console.log(`server is listening on port ${PORT}`);
+});
