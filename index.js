22091d490a55f53983f32a8dfb249913de4c2847
BrandonMoll <brandonmoll85@gmail.com>
2018-12-01 12:22:47 -0600

    delete and put working

--- index.js
+++ index.js
@@ -54,7 +54,48 @@ server.post('/api/posts', (req, res) => {
     } else {
         res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
     }
+});
+
+server.delete('/api/posts/:id', (req, res) => {
+    const {id} = req.params;
+
+    db.remove(id)
+    .then(count => {
+        if(count) {
+            //return the post
+            res.json({message: "ID Deleted"})
+        } else {
+            res.status(404).json({message: "The post with the specified ID does not exist."})
+        }
+    })
+    .catch(err => {
+        res.status(500).json({error: "The post could not be removed"})
+    })
+});
+
+server.put('/api/posts/:id', (req, res) => {
+    const post = req.body;
+    const {id} = req.params;
+
+    if(post.title && post.contents) {
+        db.update(id, post)
+        .then(count => {
+            if(count){
+                db.findById(id).then(post => {
+                    res.json(post)
+                })
+            } else {
+                res.status(404).json({message: "The post with the specified ID does not exist."})
+            }
+        })
+        .catch(err => {
+            res.status(500).json({error: "The post information could not be modified."})
+        })
+    } else {
+        res.status(400).json({errorMessage: "Please provide title and contents for the post."})
+    }
 })
+
 
 server.listen(PORT, () => {
     console.log('server is running')
