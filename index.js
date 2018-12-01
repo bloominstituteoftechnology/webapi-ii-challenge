07f23f892d0a65f463cbd8d3f2be38a5a39d108c
Dewayne87 <dewaynelindsay87@gmail.com>
2018-11-30 18:56:15 -0800

    Finished post functionailty

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const express = require('express');
 const server = express();
 const PORT = 4000;
 const db = require('./data/db');
-
+server.use(express.json())
 // add your server code starting here
 server.disable("etag");
 server.get('/api/posts', (req, res) => {
     db.find()
         .then(posts => {
             res
-            .status(200)
-            .json(posts);
+                .status(200)
+                .json(posts);
         })
         .catch(err => {
             res
@@ -26,11 +26,11 @@ server.get('/api/posts/:id', (req, res) => {
         .then(post => {
             if (post.length) {
                 res.json(post);
-            }else{
+            } else {
                 res
-                .status(404)
-                .json({ message: "The post with the specified ID does not exist." })
-                
+                    .status(404)
+                    .json({ message: "The post with the specified ID does not exist." })
+
             }
         })
         .catch(err => {
@@ -39,6 +39,28 @@ server.get('/api/posts/:id', (req, res) => {
                 .json({ error: "The post information could not be retrieved." })
         })
 })
+server.post('/api/posts', (req, res) => {
+    const post = req.body;
+
+    if (post.title && post.contents) {
+        db.insert(post).then(idInfo => { console.log(idInfo.id)
+            db.findById(idInfo.id).then(post => {
+                res
+                    .status(201)
+                    .json(post);
+            });
+        }).catch(err => {
+            res
+                .status(500)
+                .json({ error: "There was an error while saving the post to the database" })
+        })
+    } else {
+        res
+            .status(400)
+            .json({ errorMessage: "Please provide title and contents for the post." })
+    }
+})
+
 
 server.listen(PORT, () => {
     console.log('server is running')
