9f84f12d49c97f68ba0734811a8a5b34fcfa1bb2
Bruce Cabanayan <bcabanayan@Unknown.domain>
2018-12-01 13:00:58 -0700

    Added put functionality to edit existing posts

--- index.js
+++ index.js
@@ -51,8 +51,8 @@ server.get('/api/posts/:id', (req, res) => {
             res
                 .status(500)
                 .json({message: 'there was an error in retrieving the post'})
-        })
-})
+        });
+});
 
 // create new post
 
@@ -79,7 +79,7 @@ server.post('/api/posts', (req, res) => {
             .status(400)
             .json({ message: 'missing title or contents'})
     }
-})
+});
 
 // delete post
 
@@ -101,7 +101,40 @@ server.delete('/api/posts/:id', (req, res) => {
                 .status(500)
                 .json({message: 'failed to delete post'})
         });
-})
+});
+
+// update existing post
+
+server.put('/api/posts/:id', (req, res) => {
+    const updatedPost = req.body;
+    const { id } = req.params;
+    if (updatedPost.title && updatedPost.contents) {
+        db.update(id, updatedPost)
+            .then(count => {
+                if (count) {
+                    db.findById(id)
+                        .then(user => {
+                            res.json(user)
+                        });
+                }
+                else {
+                    res
+                        .status(404)
+                        .json({ message: 'could not find post'});
+                }
+            })
+            .catch(err => {
+                res
+                    .status(500)
+                    .json({message: 'failed to delete post'});
+            });
+    }
+    else {
+        res
+            .status(400)
+            .json({ message: 'missing title or contents'});
+    }
+});
 
 // initiate listening
 
