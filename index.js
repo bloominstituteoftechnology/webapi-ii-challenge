5beba3560049d15abec18b1afb4cfeaaeffe1c09
vy3191 <venkyafhs@gmail.com>
2018-12-03 11:11:35 -0500

    Working post request

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 const express = require('express');
 const db = require('./data/db.js');
 
-const PORT = '5000';
+const PORT = '8000';
 
 // add your server code starting here
 const server = express();
-
+server.use(express.json())
 server.get('/api/posts', (req,res) => {
       db.find()
         .then(posts => {
@@ -23,8 +23,9 @@ server.get('/api/posts/:id', (req, res) => {
      const {id} = req.params;
      db.findById(id)
        .then( post => {
-           if(post.length) {
-               res.send(post);
+            console.log(post[0])
+           if(post.length>0) {
+               res.status(200).send(post);
            } else {
                res.status(404)
                   .json({errorMessage: "This doesn't seem to be functional"})
@@ -34,10 +35,26 @@ server.get('/api/posts/:id', (req, res) => {
               res.status(500)
                  .json({
                      errorMessage: 'Not working'
-                 })
-       })
-})
+                 });
+       });
+});
 
+server.post('/api/posts', (req,res) => {
+      const post = req.body;
+      if(post.title && post.contents) {
+           console.log("Before insert:", post)
+          db.insert(post)
+            .then(post => {
+                 console.log("post from the insert method", post);
+                 res.send(post);
+            })
+            .catch(err => {
+                res.status(500)
+                   .json({errorMessage: "Failed to add new post.."})
+            })
+      }
+
+})
 server.listen(PORT, () => {
      console.log(`localhost is runs on ${PORT}`);
 })
