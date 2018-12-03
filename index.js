efb07ec5753c34267b1e774657d099c930c3c24f
Matthew Meitl <darksamus6@gmail.com>
2018-12-03 17:58:01 -0600

    added put this time

--- index.js
+++ index.js
@@ -59,6 +59,32 @@ server.delete('/api/posts/:id', (req, res) => {
   })
 })
 
+server.put('/api/posts/:id', (req, res) => {
+  const { id } = req.params;
+  const { title, contents } = req.body;
+  const newPost = { title, contents }; 
+  if(!id){
+      res
+      .status(404)
+      .json({message: "The post with the specified ID does not exist."})
+  } else if(!title || !contents){
+      res
+      status(400)
+      .json({errorMessage: "Please provide title and contents for the post."})
+  }
+  db.update(id, newPost)
+  .then(post => {
+      res
+      .status(200)
+      .json(post)
+  })
+  .catch(err => {
+      res
+      .status(500)
+      .json({error: "The post information could not be modified.", err})
+  })
+})
+
 server.listen(3000, () => {
     console.log('Server running on http://localhost:3000')
 });
