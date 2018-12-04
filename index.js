7d7b5fe80614a1521582f446eec347ca835cf1ca
Andres Rivera <andres.rivera.toro@gmail.com>
2018-12-03 21:04:33 -0500

    Add PUT method and endpoint

--- index.js
+++ index.js
@@ -79,4 +79,32 @@ server.delete('/api/posts/:id', (req, res) => {
     });
 });
 
+server.put('/api/posts/:id/', (req, res) => {
+  const { id } = req.params;
+  const { title, contents } = req.body;
+  db.findById(id).then(post => {
+    if (post.length) {
+      if (title && contents) {
+        db.update(id, { title, contents }).then(count => {
+          if (count) {
+            res.status(200).json({ title, contents });
+          } else {
+            res
+              .status(500)
+              .json({ error: 'The post information could not be modified.' });
+          }
+        });
+      } else {
+        res.status(400).json({
+          errorMessage: 'Please provide title and contents for the post.',
+        });
+      }
+    } else {
+      res
+        .status(404)
+        .json({ message: 'The post with the specified ID does not exist.' });
+    }
+  });
+});
+
 server.listen(PORT, () => console.log(`API running on port ${PORT}`));
