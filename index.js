a994e92e1d1bb1cf97538c9299a55d3a990498fb
Levi Appenfelder <levi.appenfelder@gmail.com>
2018-12-03 19:00:00 -0700

    PUT implemented

--- index.js
+++ index.js
@@ -12,25 +12,24 @@ server.get('/api/posts', (req,res) => {
     .catch(err => {
         res
         .status(500)
-        .json({"error": "The posts information could not be retrieved."})})
+        .json({error: "The posts information could not be retrieved."})})
 })
 
 server.get('/api/posts/:id', (req,res) => {
     const {id} = req.params;
     db.findById(id)
     .then( post => {
-        console.log(post)
         if (post.length > 0) {res.json(post)}
 
         else { 
             res
             .status(404)
-            .json({"message":"The post with the specified ID does not exist."})}
+            .json({message:"The post with the specified ID does not exist."})}
     })
     .catch(err => {
         res
         .status(500)
-        .json({"message": "failed to get post"})
+        .json({message: "failed to get post"})
     })
 })
 
@@ -38,18 +37,84 @@ server.get('/api/posts/:id', (req,res) => {
 
 server.post('/api/posts', (req,res) => {
     const post = req.body;
-    console.log(post)
-    db.insert(post)
-    .then(post => {res.json(post)})
+    if (post.title && post.contents){
+        db.insert(post)
+    .then(info => {
+        db.findById(info.id).then(post => {
+            res
+            .status(201)
+            .json(post)})
+        })
+        
     .catch(err => {
         res
         .status(500)
-        .json({"message": "failed to get post"})
+        .json({message: "failed to get post"})
+    })
+    }
+
+    else {
+        res
+        .status(400)
+        .json({menssage: "missing title or bio"})
+    }
+    
+})
+
+
+server.delete('/api/posts/:id', (req,res) => {
+    const {id} = req.params;
+    db.remove(id)
+    .then(count => {
+        if(count) {
+            res.json({message: "post deleted"})
+        }
+        else {
+            res
+            .status(404)
+            .json({message: "The post with the specified ID does not exist."})
+        }
+    })
+    .catch(err => {
+        res
+        .status(500)
+        .json({error: "The post could not be removed"})
     })
 })
 
-server.put('/api/users/:id', (req,res) => {
-    res.update(id, req.params({}))
+
+server.put('/api/posts/:id', (req,res) => {
+    const post = req.body;
+    const {id} = req.params;
+    if (post.title && post.contents) {
+        db.update(id, post)
+        .then(count => {
+            if (count) {
+                db.findById().then( post => {
+                    res.json(post)}
+                )
+            }
+
+            else { res
+                .status(404)
+                .json({message:"The post with the specified ID does not exist."})}
+        })
+        .catch(
+            err => {
+                res
+                .status(500)
+                .json({error: "The post could not be removed"})
+            }
+        )
+
+    }
+
+    else {
+        res
+        .status(400)
+        .json({message: "missing title or bio"})
+    }
+
 })
 
 server.listen(NUM, () => console.log(`listening on port ${NUM}`))
