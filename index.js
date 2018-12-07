08e75f8681b6dcdd02dc499493a59e6e9da57e69
Elzy <belzy@rkdixon.com>
2018-12-07 15:44:10 -0600

    Brandon G. Elzy - added CRUD operations to API

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const server = express();
 const db = require('./data/db.js');
 const port = 3000;
 
+server.use(bodyParser.json());
 
-server.get('/api/posts', (req, res) => { db.find().then( posts => res.json(posts)) });
+// Get all posts.
+server.get('/api/posts', (req, res) => db.find().then( posts => res.json(posts)));
 
+// Get post by id.
+server.get('/api/posts/:id', (req, res) => db.findById(req.params.id).then( post => res.json(post)));
+
+// Insert a new post.
+server.post('/api/posts', (req, res) => db.insert(req.body).then( id => res.json(id)));
+
+// Update a post.
+server.put('/api/posts/:id', (req, res) => db.update(req.params.id, req.body).then( count => res.json(count)));
+
+// Delete a post.
+server.delete('/api/posts/:id', (req, res) => db.remove(req.params.id).then( count => res.json(count)));
 
 server.listen(port, () => {
   console.log(`Server listening on port ${port}...`);
