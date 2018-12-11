e3f43dbdfd87289d6b88afb8a07d38bc13033be2
jbierly1 <james.bierly@gmail.com>
2018-12-11 10:05:31 -0500

    added get methods

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
 // import your node modules
+const express = require('express');
+const server = express();
 
 const db = require('./data/db.js');
 
 // add your server code starting here
+
+server.get('/api/posts', (req, res) => {
+	db.find()
+		.then(posts => {
+			res.status(200).json(posts);
+		})
+		.catch(err => {
+			res
+				.status(500)
+				.json({ error: 'The posts information could not be retrieved.' });
+		});
+});
+
+server.get('/api/posts/:id', (req, res) => {
+	const { id } = req.params;
+	db.findById(id)
+		.then(post => {
+			if (post) {
+				res.json(post);
+			} else {
+				res
+					.status(404)
+					.json({ message: 'The post with the specified ID does not exist.' });
+			}
+			res.status(200).json(post);
+		})
+		.catch(err => {
+			res
+				.status(500)
+				.json({ error: 'The post information could not be retrieved.' });
+		});
+});
+
+server.listen(8000, () => console.log('API running on port 8000'));
