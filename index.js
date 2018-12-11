16182a547177a01eb1927e0371d767c51bb79422
cocoitali <cmburatto@gmail.com>
2018-12-10 20:59:31 -0700

    MVP: (added dependencies, created skeleton for server)

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // import your node modules
-
+const express =require('express');
+const PORT = 4000;
 const db = require('./data/db.js');
 
 // add your server code starting here
+const server = express();
+
+server.listen(PORT, () =>console.log(`API running on port ${PORT}`))
