225fcec44c34cb7e9a0f014cf2f5e4cf0b57bde1
cocoitali <cmburatto@gmail.com>
2018-12-10 21:15:05 -0700

    MVP : (wrote get enpoint for /api/posts, rendering)

--- index.js
+++ index.js
@@ -6,4 +6,14 @@ const db = require('./data/db.js');
 // add your server code starting here
 const server = express();
 
+server.get('/api/posts', (req, res) => {
+    db.find()
+    .then(posts => {
+        res.json(posts);
+    })
+    .catch(err => {
+        res.status(500).json({error: "The posts information could not be retrieved."})
+    })
+})
+
 server.listen(PORT, () =>console.log(`API running on port ${PORT}`))
