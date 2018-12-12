011b6b12828df0836cf1e6ea00f7e5012a8d4289
cocoitali <cmburatto@gmail.com>
2018-12-11 20:33:06 -0700

    FIX : (handled second error)

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const db = require("./data/db.js");
 const server = express();
 server.use(express.json());
 
-
 server.get("/api/posts", (req, res) => {
   db.find()
     .then(posts => {
@@ -38,18 +37,28 @@ server.get("/api/posts/:id", (req, res) => {
 });
 
 server.post("/api/posts", (req, res) => {
-    const post = req.body
-  db.insert(post)
-    .then(post => {
-      res.status(201).json(post);
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({
+  const post = req.body;
+  if (post.title && post.contents) {
+    db.insert(post)
+      .then(idInfo => {
+        db.findById(idInfo.id).then(post => {
+          res.status(201).json(post);
+        });
+      })
+      .catch(err => {
+        res.status(500).json({
           error: "There was an error while saving the post to the database"
         });
-    });
+      });
+  } else {
+    res
+      .status(400)
+      .json({
+        errorMessage: "Please provide title and contents for the post."
+      });
+  }
 });
+
+server.put("api/posts/:id", (req, res) => {});
 
 server.listen(PORT, () => console.log(`API running on port ${PORT}`));
