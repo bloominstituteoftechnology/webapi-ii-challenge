b25ed7dee9b8addedf9adc89aaf389c29b0648f9
Lorenzo Evans <lorenzo.evans94@gmail.com>
2018-12-13 23:04:27 -0500

    Requiring necessary dependencies.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 // import your node modules
 
+const express = require('express')
+
+const cors = require('cors')
+
+const server = express()
+
+server.use(server.json(), cors())
+
 const db = require('./data/db.js');
 
 // add your server code starting here
