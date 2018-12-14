bc25a4d5ce11e3a6df314c132e8ff4f9d63c70f2
Lorenzo Evans <lorenzo.evans94@gmail.com>
2018-12-13 23:13:28 -0500

    Implementing post API endpoint.

--- index.js
+++ index.js
@@ -11,3 +11,25 @@ server.use(server.json(), cors())
 const db = require('./data/db.js');
 
 // add your server code starting here
+
+server.post('/api/users/', (req, res) => {
+ const { title, contents } = req.body
+ db.insert(user = {title, contents})
+  if (title, contents){
+   res
+    .status(201)
+    .send(user)
+    .json(user)
+  }
+  else if (!(title, contents)) {
+   res
+    .status(400)
+    .json({errorMessage: "Please provide title and contents for the post."})
+  }
+})
+
+const PORT = 3999
+
+server.listen(POST, () => {
+ console.log(`Server is running live on ${PORT}`)
+} )
