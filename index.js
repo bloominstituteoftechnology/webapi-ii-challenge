c8665560512e45b5b90df6cfaff06ed481b96642
Jesse Theisen <theiseja@gmail.com>
2018-12-15 12:02:58 -0600

    MVP: Added both GET endpoints

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 // import your node modules
-
-const db = require('./data/db.js');
+const express = require("express");
+const PORT = 3000;
+const db = require("./data/db.js");
 
 // add your server code starting here
-const express = require('express');
 const server = express();
-const PORT = 3000;
+server.use(express.json());
 
+server.get("/api/posts", (req, res) => {
+  db.find()
+    .then(posts => {
+      res.json(posts);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The posts information could not be retrieved." });
+    });
+});
+
+server.get("/api/posts/:id", (req, res) => {
+    const { id } = req.params;
+    db.findById(id)
+        .then(post => {
+            if (post) {
+                res.json(post);
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." });
+            }
+        })
+        .catch(err => {
+            res.status(404)
+            .json({ message: "The post with the specified ID does not exist." })
+        });
+});
+
+// listen
+server.listen(PORT, () => console.log(`API running on port ${PORT}`));
