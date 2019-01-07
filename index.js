dfaf2951dabf9d924706300fa69f26ae0e6b083f
Chaya Otikor <c.otikor@gmail.com>
2019-01-07 14:35:01 -0700

    Completed both GET endpoints, working on others

--- index.js
+++ index.js
@@ -3,3 +3,39 @@
 const db = require('./data/db.js');
 
 // add your server code starting here
+const express = require('express');
+
+const server = express();
+
+server.get('/', (req, res) => {
+	db.find()
+		.then((posts) => {
+			res.status(200).json({ posts });
+		})
+		.catch((err) => {
+			res
+				.status(500)
+				.json({ error: 'The post information could not be retrieved.' });
+		});
+});
+
+server.get('/api/posts/:id', (req, res) => {
+	const id = req.params.id;
+	db.findById(id)
+		.then((post) => {
+			if (post) {
+				res.status(200).json({ post });
+			} else {
+				res
+					.status(404)
+					.json({ message: 'The post with the specified ID does not exist.' });
+			}
+		})
+		.catch((err) => {
+			res
+				.status(500)
+				.json({ error: 'The post information could not be retrieved.' });
+		});
+});
+
+server.listen(8000, () => console.log('Server running on port 8000'));
