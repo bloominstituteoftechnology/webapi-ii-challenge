0892511f1d48cbbc1424a35099e28507cc74a25f
Cwright0428 <cwright0428@yahoo.com>
2019-01-07 15:53:47 -0600

    servers up working on get request

--- index.js
+++ index.js
@@ -3,4 +3,12 @@ express = require('express');
 
 const db = require('./data/db.js');
 
+const server = express();
+
 // add your server code starting here
+
+server.listen(5000, console.log('server running'))
+
+server,get('/api/posts/', (req, res)  => {
+
+})
