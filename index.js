309f470206feff60a6ca142307389a942252b068
Aman <amsingh714@gmail.com>
2019-01-07 15:35:17 -0500

    now returning posts by id

--- index.js
+++ index.js
@@ -1,29 +1,44 @@
 // import your node modules
-const express = require('express');
+const express = require("express");
 
-const db = require('./data/db.js');
+const db = require("./data/db.js");
 
 const port = 5000;
 // add your server code starting here
 const server = express();
 
-server.get('/', (req, res) => {
-    res.send('<h1>Welcome to the League of Shadows</h1>')
+server.get("/", (req, res) => {
+  res.send("<h1>Welcome to the League of Shadows</h1>");
 });
 
-server.get('/api/posts', (req,res) => {
-    db.find()
+server.get("/api/posts", (req, res) => {
+  db.find()
     .then(posts => {
-        if (posts) {
-            res.status(200).json(posts)
-        } else {
-            res.status(500).json({ error: 'The posts information could not be retrieved.'})
-        }
+      res.status(200).json(posts);
     })
     .catch(err => {
-        res.status(500).json(err);
-    })
+      res.status(500).json({ error: 'The posts information could not be retrieved.' });
+    });
+});
 
-})
+server.get("/api/posts/:id", (req, res) => {
+  const id = req.params.id;
+
+  db.findById(id)
+    .then(post => {
+      if (post.length > 0) {
+        res.status(200).json(post);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The post information could not be modified." });
+    });
+});
 
 server.listen(port, () => console.log(`Port: ${port}`));
