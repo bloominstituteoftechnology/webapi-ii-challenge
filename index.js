f28112ab8f29aca5f92c9907f94f5ba0082038b0
steelworx <eileencuevas62@gmail.com>
2019-01-07 16:28:12 -0500

    server now includes GET endpoints

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
 // import your node modules
+const express = require('express');
 
 const db = require('./data/db.js');
 
 // add your server code starting here
+
+const server = express();
+
+server.get('/api/posts/', (req, res) => {
+        db
+            .find()
+            .then(posts => {
+                res.status(200).json(posts)
+            })
+            .catch(() => {
+                res.status(500).json({ 'error': `'The posts information could not be retrieved.'` })
+            });
+    })
+
+server.get('/api/posts/:id', (req, res) => {
+    const id = req.params.id;
+
+    db
+        .findById(id)
+        .then(post => {
+            {post.length > 0 ? 
+                res.status(200).json(post) : 
+                res.status(404).json({ error: 'The post information could not be retrieved.' })
+            }
+        })
+        .catch(() => {
+            res.status(500).json({ error: 'The posts information could not be retrieved.' })
+        });
+})
+
+server.listen(3000, () => console.log('Server Running...'))
