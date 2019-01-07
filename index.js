bb8c82609e5e0cc7bfd5bdece8c222e2d068ada2
Grace <gracekang.93@gmail.com>
2019-01-07 12:10:52 -0800

    imports express and db.js into index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 // import your node modules
 
+const express = require('express');
+
 const db = require('./data/db.js');
 
 // add your server code starting here
+
+const server = express();
+
+server.listen(5000, () => console.log('server running'));
