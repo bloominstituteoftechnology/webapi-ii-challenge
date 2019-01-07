2a6eb16b5d1f02effb3c12fcc9ca68b62783a919
Cwright0428 <cwright0428@yahoo.com>
2019-01-07 17:52:44 -0600

    adding logic for error msg display

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 // import your node modules
-express = require('express');
+const express = require("express");
 
-const db = require('./data/db.js');
+const db = require("./data/db.js");
 
 const server = express();
 
 // add your server code starting here
+/*server.get('/', (req, res) => {
+  res.send('<h1>Yo Wassup</h1>')
+})*/
 
-server.listen(5000, console.log('server running'))
+server.get("/api/posts", (req, res) => {
+  db.find()
+    .then(posts => {
+    res.status(200).json(posts)
 
-server,get('/api/posts/', (req, res)  => {
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'err'})
+    });
+});
 
-})
+//2nd get req using find by ID method
+
+server.get("/api/posts/:id", (req, res) => {
+  const id = req.params.id
+  db.findById(id)
+    .then(posts => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({ message: 'User not found' });
+    res.status(200).json(posts)
+
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'err'})
+    });
+});
+server.listen(5000, () => console.log("up and at em!"));
