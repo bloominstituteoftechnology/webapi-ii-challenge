e44351845a07c46058b60244520b1cbaabbecfcf
Mr. Metelus <Mdmetelus@gmail.com>
2019-01-07 13:19:52 -0500

    "partial server"

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // import your node modules
+const express = require('express')
 
 const db = require('./data/db.js');
 
 // add your server code starting here
+const server = express();
+const PORT = 4545;
