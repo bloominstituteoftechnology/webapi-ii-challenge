878aa0c24cdd1afde89b08247bfb224ba3dc6a65
Aman <amsingh714@gmail.com>
2019-01-07 16:40:02 -0500

    data is being accessed

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    posts: []
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/api/posts')
+    .then(response => {
+      console.log(response)
+      this.setState({ posts: response.data })
+    })
+    .catch(err => console.log(err));
+  }
+
+
   render() {
     return (
       <div className="App">
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // import your node modules
 const express = require("express");
-
+var cors = require('cors')
 const db = require("./data/db.js");
 
 const port = 5000;
 // add your server code starting here
 const server = express();
+server.use(cors())
+
 
 server.get("/", (req, res) => {
   res.send("<h1>Welcome to the League of Shadows</h1>");
