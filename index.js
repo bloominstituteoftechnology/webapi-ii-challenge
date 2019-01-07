b2ad070ba2b4dda4a9027f4adfc0e95a445f583c
Justin <jdlow2@gmail.com>
2019-01-07 15:32:12 -0500

    Sets up express server, implements get for /posts/

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 // import your node modules
 
 const db = require('./data/db.js');
+const express = require('express');
 
 // add your server code starting here
+//
+server = express();
+
+server.get('/api/posts', (req, res) => {
+  db.find()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+server.listen(5000);
