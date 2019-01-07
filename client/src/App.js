063d013abb363e4852d816030615933ee5c36643
FreitagDavid <freitagdavid@outlook.com>
2019-01-07 18:49:16 -0500

    client/src/App: Created and refactored

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Posts from './components/Posts';
 
 const App = () => {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
+            <Posts />
         </div>
     );
 };
