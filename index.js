90342ab7404c4c3fe47bacd5b01c2047af6a0ed6
ykrueng <yasirah.krueng@gmail.com>
2019-01-07 13:58:47 -0600

    setup server to run on porst 5000

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // import your node modules
-
-const db = require('./data/db.js');
+const express = require("express");
+const bodyParser = require("body-parser");
+const db = require("./data/db.js");
 
 // add your server code starting here
+const server = express();
+server.use(bodyParser.json());
+
+server.listen(5000, () => console.log("Server is running on port: 5000"));
