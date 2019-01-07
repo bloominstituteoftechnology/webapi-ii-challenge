b8f78fb97bbc45fea31b3d2141a3ecdb840cb45d
Addison-Stavlo <AddieStavlo@gmail.com>
2019-01-07 15:53:34 -0500

    get requests added to server

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 // import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
 
 // add your server code starting here
+const server = express();
+
+server.get('/api/posts', (req,res) => {
+    db.find().then(posts => {
+        res.status(200).json({posts})
+    }).catch(err => {
+        res.status(500).json({ error: "The posts information could not be retrieved." });
+    });
+})
+
+server.get('/api/posts/:postid', (req,res) => {
+    const id = req.params.postid;
+
+    db.findById(id)
+    .then(post => {
+        if(post.length){
+            res.status(200).json(post);
+        }
+        else{
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        }
+    })
+    .catch(err => res.status(500).json({ error: "The post information could not be retrieved." }))
+})
+
+server.listen(5000, () => console.log('server running'));
