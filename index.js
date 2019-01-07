278a081a079d20a97cc36d2beba3e6f298c2dfd1
ToMMaaY <thuynh629@gmail.com>
2019-01-07 14:00:53 -0800

    Hooked up the get posts and get post by id endpoints

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
 // import your node modules
+const express = require("express");
+const db = require("./data/db.js");
 
-const db = require('./data/db.js');
+const server = express();
 
 // add your server code starting here
+
+server.get("/", (req, res) => {
+  db.find()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The posts information could not be retrieved." });
+    });
+});
+
+server.get("/api/posts/:id", (req, res) => {
+  const id = req.params.id;
+
+  db.findById(id)
+    .then(user => {
+      if (user.length > 0) {
+        res.status(200).json(user);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The post information could not be retrieved." });
+    });
+});
+
+server.listen(5005);
