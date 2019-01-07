b759efe7eddbe2ecfd623c25f86724bd95bfd880
Dennis Orbison <orbison.dennis@gmail.com>
2019-01-07 12:56:07 -0800

    post method working with database

--- index.js
+++ index.js
@@ -6,30 +6,56 @@ const db = require('./data/db.js');
 // add your server code starting here
 const server = express();
 
+server.use(express.json());
+
 server.get('/', (req, res) => {
   res.send('hello developer, welcome to my world');
 });
 
 server.get('/api/posts', (req, res) => {
+
   db.find()
     .then(posts => {
       res.status(200).json(posts);
     })
     .catch(err => res.status(500).json({error: "The Posts information could not be retrieved."}))
+
 });
 
 server.get('/api/posts/:id', (req, res) => {
+
   const id = req.params.id;
   db.findById(id)
     .then(post => {
-      res.status(200).json(post);
+
+      if(post.length) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({message: "The post with the specified ID does not exist."});
+      }
+      
     })
-    .catch(err => res.status(404).json({message: "The post with the specified ID does not exist."}))
-  // res.send(`place holder for dynamic ${id} posts`);
+    .catch(err => res.status(500).json({ error: "The post information could not be retrieved." }))
 });
 
 server.post('/api/posts', (req, res) => {
-  res.send('place holder');
+  const {title, contents} = req.body;
+
+  if(title && contents) {
+    db.insert({title, contents})
+      .then(id => {
+        const post_id = id.id;
+
+        db.findById(post_id)
+          .then(post => {
+            res.status(201).json(post);
+          })
+          .catch(err => res.status(400).json({errorMessage: "failed to send"}))
+      })
+      .catch(err => res.status(500).json({error: "There was an error while saving the post to the database "}))
+  } else {
+    res.status(400).json({errorMessage:  "Please provide title and contents for the post." });
+  }
 });
 
 server.delete('/api/posts/:id', (req, res) => {
