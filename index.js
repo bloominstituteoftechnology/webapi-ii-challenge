a120d105c340900067ea425b2bae8d3833b79a13
Wesley Meeks <wesmeeks88@gmail.com>
2019-01-07 14:27:14 -0700

    got api/posts set up and api/posts/:id set up, it will show error message as well if post not found.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const db = require('./data/db.js');
 
 const server = express();
 // add your server code starting here
-server.get('/', (req, res) => {
+server.get('/api/posts', (req, res) => {
     db.find()
         .then(posts => {
             res.status(200).json(posts);
@@ -13,6 +13,17 @@ server.get('/', (req, res) => {
         .catch(err => res.json(err));
 })
 
-
+server.get('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    db.findById(id)
+        .then(post => {
+            if (post.length === 0) {
+                res.status(404).json({ message: 'No post found' })
+            } else {
+                res.status(200).json(post)
+            }
+        })
+        .catch(err => res.status(500).json(err));
+})
 
 server.listen(5000, () => console.log('server running'));
