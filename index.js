758f45aab55f2bfcc2e52a4f85f2b85eacb28b33
Holden B <holden.bucher.92@gmail.com>
2019-01-07 11:30:24 -0600

    get specific id works

--- index.js
+++ index.js
@@ -16,4 +16,16 @@ app.get("/api/posts", (req, res) => {
   );
 });
 
+app.get(`/api/posts/:id`, (req, res) => {
+  const id = req.params.id;
+  db.findById(id).then(
+    doc => {
+      res.send(doc);
+    },
+    err => {
+      res.status(404).send({ message: "The post with the specified ID does not exist." });
+    }
+  );
+});
+
 app.listen(3000, () => console.log("The server is up and listening on port 3000"));
