0b00dfe9121868a218bb040f55e4b5641f4a6c5f
Mr. Metelus <Mdmetelus@gmail.com>
2019-01-07 14:37:26 -0500

    "server listen added"

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ const db = require('./data/db.js');
 // add your server code starting here
 const server = express();
 const PORT = 4545;
+
+
+server.listen(PORT, () => {
+    console.log(`The server is runnning on port ${PORT}`);
+});
