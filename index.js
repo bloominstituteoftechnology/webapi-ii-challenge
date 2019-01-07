3de0d01ec71fc097da358e12ae0ed0c572d782c0
FreitagDavid <freitagdavid@outlook.com>
2019-01-07 18:47:18 -0500

    index: Accidentally confused my if statement

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // import your node modules
-
 const express = require('express');
-
 const db = require('./data/db.js');
+const cors = require('cors');
 
 const server = express();
+
+server.use(cors());
 
 server.get('/api/posts', (req, res) => {
     db.find().then(posts => {
@@ -18,9 +19,9 @@ server.get('/api/posts/:id', (req, res) => {
     db.findById(id)
         .then(post => {
             if (post === []) {
-                res.status(200).json(post);
-            } else {
                 res.status(404).json({ message: 'Post does not exist.' });
+            } else {
+                res.status(200).json(post);
             }
         })
         .catch(err => res.status(500).json(err));
