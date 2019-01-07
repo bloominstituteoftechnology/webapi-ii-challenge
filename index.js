f48d14440aec057004166376cdf1165b0bbd0076
Dennis Orbison <orbison.dennis@gmail.com>
2019-01-07 04:44:26 -0800

    a few get requests set

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ const server = express();
 
 server.get('/', (req, res) => {
   res.send('hello developer, welcome to my world');
-})
+});
 
-server.listen(8000, () => console.log('Server running, listening on port 8000'))
+server.get('/api/posts', (req, res) => {
+  res.send('place holder for posts');
+});
+
+server.get('/api/posts/:id', (req, res) => {
+  const id = req.params.id;
+  res.send(`place holder for dynamic ${id} posts`);
+});
+
+server.listen(8000, () => console.log('Server running, listening on port 8000'));
