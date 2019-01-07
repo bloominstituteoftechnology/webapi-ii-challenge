da92cbd467f68b81d7a1846210869ff4c4e72fd5
Dennis Orbison <orbison.dennis@gmail.com>
2019-01-07 04:36:22 -0800

    root get request operating correctly

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ const db = require('./data/db.js');
 // add your server code starting here
 const server = express();
 
+server.get('/', (req, res) => {
+  res.send('hello developer, welcome to my world');
+})
+
 server.listen(8000, () => console.log('Server running, listening on port 8000'))
