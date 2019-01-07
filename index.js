0b668b3a8268da4e81e030063791be4065365fbc
Civoremo <omerovic_n@hotmail.com>
2019-01-07 14:23:10 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 // import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
 
 // add your server code starting here
+
+const server = express();
+
+server.get('/', (req, res) => {
+    db.find()
+        .then(posts => {
+            res.json({posts});
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+
+server.listen(5000, () => console.log('server running'));
