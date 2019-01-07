e2f665dc0edd6d994ad05a11ee18c83edd5802aa
Aman <amsingh714@gmail.com>
2019-01-07 18:25:10 -0500

    single post is being rendered

--- client/src/Components/Post/Post.js
+++ client/src/Components/Post/Post.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import axios from "axios";
 
 import './Post.css';
 
 
-const Post = props => {
-    return <div></div>
+class Post extends React.Component {
+    state = {
+        post: null
+    }
+
+    componentDidMount() {
+        axios.get(`http://localhost:5000/api/posts/${this.props.match.params.id}`)
+        .then(response => {
+            console.log(response)
+            this.setState({
+                post: response.data
+            })
+        })
+        .catch(err => console.log(err));
+    }
+
+    render() {
+        if (!this.state.post) return <h2 id="loading">Loading...</h2>;
+        return (
+            <div className="post">
+                <h2>{this.state.post.title}</h2>
+                <p>{this.state.post.contents}</p>
+            </div>
+        )
+    }
 }
 
 export default Post;
