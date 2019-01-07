26e514b76c5b943f1d4b0529f941b20297c95a87
FreitagDavid <freitagdavid@outlook.com>
2019-01-07 16:32:14 -0500

    monitor: Shows status code and labeled output

--- monitor.js
+++ monitor.js
@@ -3,7 +3,7 @@ const util = require('util');
 
 setInterval(() => {
     util.print('\u001b[2J\u001b[0;0H');
-    http.get('http://localhost:5000/api/posts', resp => {
+    http.get('http://localhost:5000/api/posts/15', resp => {
         let data = '';
 
         // A chunk of data has been recieved.
@@ -13,9 +13,10 @@ setInterval(() => {
 
         // The whole response has been received. Print out the result.
         resp.on('end', () => {
-            console.log(JSON.stringify(resp.headers, null, 4));
+            console.log('Status code:', resp.status);
+            console.log('Header:', JSON.stringify(resp.headers, null, 4));
             data = JSON.parse(data);
-            console.log(JSON.stringify(data, null, 4));
+            console.log('Data:', JSON.stringify(data, null, 4));
         });
     }).on('error', err => {
         console.log('Error: ' + err.message);
