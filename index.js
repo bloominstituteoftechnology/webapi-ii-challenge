ee4865fa00cdcb72afe01ffd5e759d9b8f6e2be1
Grace <gracekang.93@gmail.com>
2019-01-07 12:12:12 -0800

    handles get requests to server

--- index.js
+++ index.js
@@ -8,4 +8,44 @@ const db = require('./data/db.js');
 
 const server = express();
 
+server.get('/api/posts', (req, res) => {
+    db.find()
+        .then(posts => {
+            res
+                .status(200)
+                .json({ posts })
+        })
+        .catch(error => {
+            res
+                .status(500)
+                .json({ 
+                    error: "The posts information could not be retrieved." 
+                })
+        });
+});
+
+server.get('/api/posts/:id', (req, res) => {
+    db.findById(req.params.id)
+        .then(post => {
+            if (post) {
+                res
+                    .status(200)
+                    .json({ post })
+            } else {
+                res
+                    .status(404)
+                    .json({ 
+                        message: "The post with the specified ID does not exist." 
+                    })
+            }
+        })
+        .catch(error => {
+            res
+                .status(500)
+                .json({
+                    error: "The post information could not be retrieved."
+                })
+        });
+});
+
 server.listen(5000, () => console.log('server running'));
