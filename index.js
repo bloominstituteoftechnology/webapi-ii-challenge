bde7e854290b5d6e55cb17862a5a723d32671d7f
chasefulks <chase.fulks@gmail.com>
2019-01-07 17:31:55 -0600

    working

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
 // import your node modules
+// import db from './data/db.js';
+const express = require("express");
 
-const db = require('./data/db.js');
+const db = require("./data/db.js");
 
 // add your server code starting here
+const server = express();
+
+server.get("/", (req, res) => {
+  db.find()
+    .then(posts => {
+      res.status(400).json(posts);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+server.get("/api/posts/:postsid", (req, res) => {
+  const id = req.params.postsid;
+
+  db.findById(id)
+    .then(posts => {
+      if (posts) {
+        res.status(201).json(posts);
+      } else {
+        res.status(400).json({ message: "The post information could not be retrieved." });
+      }
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.listen(5000, () => console.log("server running"));
