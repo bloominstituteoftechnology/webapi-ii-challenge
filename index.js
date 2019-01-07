18913c63ed8d55580a297d13e1eaf3e579198780
Wesley Meeks <wesmeeks88@gmail.com>
2019-01-07 13:27:00 -0700

    Initial Commit, added dependencies.

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 // import your node modules
+const express = require('express');
 
 const db = require('./data/db.js');
 
+const server = express();
 // add your server code starting here
+// server.get('/', (req, res) => {
+//     db.find().then
+// })
+
+
+
+
+server.listen(6000, () => console.log('server running'));
