5d262a41b90759ea016693ffea58de50fd663425
FreitagDavid <freitagdavid@outlook.com>
2019-01-07 16:42:45 -0500

    index: get on /api/posts and on /api/posts/:id working

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 // import your node modules
 
+const express = require('express');
+
 const db = require('./data/db.js');
+
+const server = express();
+
+server.get('/api/posts', (req, res) => {
+    db.find().then(posts => {
+        res.send({ posts });
+    });
+});
+
+server.get('/api/posts/:id', (req, res) => {
+    const id = req.params.id;
+
+    db.findById(id)
+        .then(post => {
+            if (post === []) {
+                res.status(200).json(post);
+            } else {
+                res.status(404).json({ message: 'Post does not exist.' });
+            }
+        })
+        .catch(err => res.status(500).json(err));
+});
+
+server.listen(5000, () => console.log('server running'));
 
 // add your server code starting here
