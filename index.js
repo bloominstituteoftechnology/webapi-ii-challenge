1b63084e081e1c617d1360c539411a94695e00f0
Justin <jdlow2@gmail.com>
2019-01-07 16:31:58 -0500

    Implements delete requests to /posts/:id
    Successfully deleted post

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ const express = require('express');
 // add your server code starting here
 //
 server = express();
+// need this to read request body
 server.use(express.json());
 
+// get all posts
 server.get('/api/posts', (req, res) => {
   db.find()
     .then(posts => {
@@ -20,12 +22,15 @@ server.get('/api/posts', (req, res) => {
     );
 });
 
+// get single post by id
 server.get('/api/posts/:id', (req, res) => {
   db.findById(req.params.id)
     .then(post => {
+      // if a post is found, send it back
       if (post.length) {
         res.status(200).json(post);
       } else {
+        // findById will return an empty array if there is no post found
         res
           .status(404)
           .json({error: 'The post with the specified ID does not exist'});
@@ -39,17 +44,20 @@ server.get('/api/posts/:id', (req, res) => {
 });
 
 server.post('/api/posts', (req, res) => {
+  // ensure post has a title and contents
   if (!req.body.title || !req.body.contents) {
     res
       .status(400)
       .json({errorMessage: 'Please provide title and contents for the post.'});
   } else {
+    // if post has correct properties, add it to the db
     db.insert(req.body)
       .then(newPost => {
         db.find(newPost.id)
           .then(post => res.status(200).json(post))
           .catch(err => res.status(500).json(err));
       })
+      // throw an error
       .catch(err =>
         res.status(500).json({
           error: 'There was an error while saving the post to the database',
@@ -58,4 +66,25 @@ server.post('/api/posts', (req, res) => {
   }
 });
 
+server.delete('/api/posts/:id', (req, res) => {
+  // find the post first
+  db.findById(req.params.id)
+    .then(p => {
+      //post is found, remove the post and send response
+      db.remove(req.params.id)
+        .then(res.status(200).json(p))
+        // send error message if post cannot be deleted
+        .catch(err =>
+          res.status(500).json({error: 'The post could not be removed.'}),
+        );
+    })
+    // send error message if post cannot be found
+    .catch(err =>
+      res
+        .status(500)
+        .json({errorMessage: 'The post with the specified ID does not exist'}),
+    );
+});
+
+// listen up, server
 server.listen(5000);
