4ab208cdbb1292ea43470996828fe9b80017790a
Holden B <holden.bucher.92@gmail.com>
2019-01-07 12:09:20 -0600

    handle different types of queries from get method

--- index.js
+++ index.js
@@ -6,24 +6,42 @@ const express = require("express");
 // add your server code starting here
 
 const app = express();
+app.use(express.json());
 
 app.get("/api/posts", (req, res) => {
   db.find().then(
     doc => {
       res.send(doc);
     },
-    err => res.status(500).send({ error: "The posts information could not be retrieved" })
+    err => res.status(500).json({ error: "The posts information could not be retrieved" })
   );
 });
 
 app.get(`/api/posts/:id`, (req, res) => {
   const id = req.params.id;
-  db.findById(id).then(
+
+  db.findById(id).then(doc => {
+    if (isNaN(id)) {
+      return res.status(500).json({ error: "The post information could not be retrieved." });
+    } else if (JSON.stringify(doc).length === 2) {
+      return res.status(404).json({ message: "The post with the specified ID does not exist." });
+    }
+    res.send(doc);
+  });
+});
+
+app.delete("/api/posts/:id", (req, res) => {
+  const id = req.params.id;
+
+  db.remove(id).then(
     doc => {
-      res.send(doc);
+      res.status(200).json({
+        url: `/hobbits/${id}`,
+        operation: `DELETE for hobbit with id ${id}`
+      });
     },
     err => {
-      res.status(404).send({ message: "The post with the specified ID does not exist." });
+      res.status(404).json({ message: "The post with the specified ID does not exist." });
     }
   );
 });
