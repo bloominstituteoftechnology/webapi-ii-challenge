76ab8e8d1e9215d6ef016028ffd1a54d0805a3be
Justin <jdlow2@gmail.com>
2019-01-07 16:13:41 -0500

    Implements post request to /api/posts
    Successfully added a new post.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const express = require('express');
 // add your server code starting here
 //
 server = express();
+server.use(express.json());
 
 server.get('/api/posts', (req, res) => {
   db.find()
@@ -18,8 +19,6 @@ server.get('/api/posts', (req, res) => {
         .json({message: 'The posts information could not be retrieved'}),
     );
 });
-
-server.listen(5000);
 
 server.get('/api/posts/:id', (req, res) => {
   db.findById(req.params.id)
@@ -38,3 +37,25 @@ server.get('/api/posts/:id', (req, res) => {
         .json({error: 'The post information could not be retrieved'}),
     );
 });
+
+server.post('/api/posts', (req, res) => {
+  if (!req.body.title || !req.body.contents) {
+    res
+      .status(400)
+      .json({errorMessage: 'Please provide title and contents for the post.'});
+  } else {
+    db.insert(req.body)
+      .then(newPost => {
+        db.find(newPost.id)
+          .then(post => res.status(200).json(post))
+          .catch(err => res.status(500).json(err));
+      })
+      .catch(err =>
+        res.status(500).json({
+          error: 'There was an error while saving the post to the database',
+        }),
+      );
+  }
+});
+
+server.listen(5000);
