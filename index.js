42c7f0fed789d37110ff00113a158be77c465b8a
Civoremo <omerovic_n@hotmail.com>
2019-01-07 14:50:07 -0500

    get posts and get post by id are working

--- index.js
+++ index.js
@@ -9,10 +9,25 @@ const server = express();
 server.get('/', (req, res) => {
     db.find()
         .then(posts => {
-            res.json({posts});
+            res.status(200).json({ posts });
         })
         .catch(err => {
-            res.json(err);
+            res.status(500).json({error: 'The posts information could not be retrieved.'});
+        });
+});
+
+server.get('/api/posts/:id', (req, res) => {
+    const id = req.params.id;
+    db.findById(id)
+        .then(post => {
+            if(post.length !== 0) {
+                res.status(200).json( post );
+            } else {
+                res.status(404).json({message: 'The post with the specified ID does not exist.'});
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err);
         });
 });
 
