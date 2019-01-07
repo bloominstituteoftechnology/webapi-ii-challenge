4bb0d267added0fe74363033c74796d74a44a60f
Jarrad Miller <jm020801@gmail.com>
2019-01-07 15:58:04 -0700

    commit

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 // import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
 
 // add your server code starting here
+const server = express();
+
+server.get('/', (req, res) => {
+    db.find()
+      .then(posts => {
+          res.json(posts);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+});
+
+server.get('/api/posts/:postid', (req, res) => {
+    const id = req.params.postid;
+
+    db.findById(id)
+      .then(post => {
+        if (post) {
+          res.status(200).json(post);
+        } else {
+          res.status(404).json({ message: "The post with the specified ID does not exist."});
+        }
+      })
+      .catch(err => res.status(500).json({error: "The post information could not be retrieved." }));
+  });
+
+server.listen(5000, () => console.log('Server is running')); 
