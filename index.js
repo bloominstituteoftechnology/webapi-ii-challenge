86303db0fc1163a6cb441e841c5750f475d9a882
Aman <amsingh714@gmail.com>
2019-01-08 15:43:27 -0500

    POST is working

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
 // import your node modules
 const express = require("express");
-var cors = require('cors')
+var cors = require("cors");
 const db = require("./data/db.js");
 
 const port = 5000;
 // add your server code starting here
 const server = express();
-server.use(cors())
-
+server.use(cors());
+server.use(express.json());
 
 server.get("/", (req, res) => {
   res.send("<h1>Welcome to the League of Shadows</h1>");
 });
 
+// GET - READ
 server.get("/api/posts", (req, res) => {
   db.find()
     .then(posts => {
       res.status(200).json(posts);
     })
     .catch(err => {
-      res.status(500).json({ error: 'The posts information could not be retrieved.' });
+      res
+        .status(500)
+        .json({ error: "The posts information could not be retrieved." });
     });
 });
 
+// GET - Read with specific ID
 server.get("/api/posts/:id", (req, res) => {
   const id = req.params.id;
 
@@ -41,6 +45,33 @@ server.get("/api/posts/:id", (req, res) => {
         .status(500)
         .json({ error: "The post information could not be modified." });
     });
+});
+
+// POST - CREATE
+server.post("/api/posts", (req, res) => {
+  const post = req.body;
+  console.log(req.body);
+
+  if (!post.title || !post.contents) {
+    res
+      .status(400)
+      .json({
+        errorMessage: "Please provide title and contents for the post."
+      });
+  } else {
+    db.insert(post)
+      .then(result => {
+        console.log(result);
+        res.status(201).json(result);
+      })
+      .catch(err =>
+        res
+          .status(500)
+          .json({
+            error: "There was an error while saving the post to the database"
+          })
+      );
+  }
 });
 
 server.listen(port, () => console.log(`Port: ${port}`));
