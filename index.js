3dec5d6236b207ddc301e8bcb99ac1090de18a96
Cwright0428 <cwright0428@yahoo.com>
2019-01-08 10:20:14 -0600

    added if else logic

--- index.js
+++ index.js
@@ -13,29 +13,25 @@ const server = express();
 server.get("/api/posts", (req, res) => {
   db.find()
     .then(posts => {
-    res.status(200).json(posts)
-
+      res.status(200).json(posts);
     })
     .catch(err => {
-      res.status(500).json({ message: 'err'})
+      res.status(500).json({ message: "err" });
     });
 });
 
 //2nd get req using find by ID method
 
 server.get("/api/posts/:id", (req, res) => {
-  const id = req.params.id
+  const id = req.params.id;
   db.findById(id)
     .then(posts => {
-      if (user) {
-        res.status(200).json(user);
-      } else {
-        res.status(404).json({ message: 'User not found' });
-    res.status(200).json(posts)
-
+      if (posts.length === 0) {
+        res.status(404).json({ message: "not found" });
+      } else res.status(200).json(posts);
     })
     .catch(err => {
-      res.status(500).json({ message: 'err'})
+      res.status(500).json({ message: "err" });
     });
 });
 server.listen(5000, () => console.log("up and at em!"));
