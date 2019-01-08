6610e9f481d6bd9557e9f8c283faf38b68d0094a
chasefulks <chase.fulks@gmail.com>
2019-01-08 17:33:39 -0600

    some of day 2

--- index.js
+++ index.js
@@ -1,34 +1,77 @@
-// import your node modules
-// import db from './data/db.js';
-const express = require("express");
+// implement your API here
+// import db from './data/db.js'; 
+const express = require('express');
 
-const db = require("./data/db.js");
+const db = require('./data/db.js'); 
 
-// add your server code starting here
 const server = express();
 
-server.get("/", (req, res) => {
+// wire up global middleware
+server.use(express.json()); 
+
+server.get('/api/posts', (req, res) => {
   db.find()
     .then(posts => {
-      res.status(400).json(posts);
+      res.status(200).json(posts);
     })
     .catch(err => {
       res.json(err);
     });
 });
 
-server.get("/api/posts/:postsid", (req, res) => {
-  const id = req.params.postsid;
+server.get('/api/posts/:id', (req, res) => {
+  const id = req.params.id;
 
   db.findById(id)
     .then(posts => {
       if (posts) {
-        res.status(201).json(posts);
+        res.status(200).json(posts);
       } else {
-        res.status(400).json({ message: "The post information could not be retrieved." });
+        res.status(404).json({ message: 'posts not found' });
       }
     })
     .catch(err => res.status(500).json(err));
 });
 
-server.listen(5000, () => console.log("server running"));
+server.post('/api/posts', (req, res) => {
+  const postInfo = req.body; 
+
+  db.insert(postInfo) 
+    .then(result => {
+      db.findById(result.id)
+        .then(post => {
+          res.status(201).json(post);
+        })
+        .catch(err =>
+          res.status(500).json({ message: 'the get by id failed', error: err })
+        );
+    })
+    .catch(err =>
+      res.status(500).json({ message: 'the post failed', error: err })
+    );
+});
+
+server.delete('/api/posts/:id', (req, res) => {
+  const id = req.params.id;
+
+  db.findById(id)
+    .then(post => {
+      if (post) {
+        db.remove(id).then(count => {
+          res.status(200).json(post);
+        });
+      } else {
+        res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist.' });
+      }
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.get('/posts/first/:first/last/:last', (req, res) => {
+  res.send({ hello: `${req.params.first} ${req.params.last}` });
+});
+
+server.listen(5000, () => console.log('server running'));
+
