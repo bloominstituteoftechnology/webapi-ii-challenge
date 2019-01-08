f776a032493585487e381d51456a6b5267280a9a
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-01-07 20:56:30 -0500

    Updated further error messages

--- index.js
+++ index.js
@@ -93,7 +93,13 @@ server.put('/api/posts/:id', async (req, res) => {
 server.delete('/api/posts/:id', (req, res) => {
   db.remove(req.params.id)
     .then(count => {
-      res.status(200).json(count);
+      if (count === 0) {
+        res.status(404).json({
+          message: 'The post with the specified ID does not exist.'
+        });
+      } else {
+        res.status(200).json(count);
+      }
     })
     .catch(err => {
       res.status(500).json({ message: 'The post could not be removed.' });
