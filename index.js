15f400acb991b84eb882bc7e90eb9e1482f97f2e
David Situ <davidsitu626@gmail.com>
2019-01-07 16:17:19 -0800

    get successful, get id still pending

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
 // import your node modules
+const express = require('express');
 
 const db = require('./data/db.js');
 
+const server = express();
+
 // add your server code starting here
+server.get('/', (req, res) => {
+  db.find()
+  .then(posts => {
+    if (posts){
+    res.status(200).json({ posts })
+  } else{
+    res.status(500).json({ error: "The posts information could not be retrieved." })
+  }
+  }).catch( err => {
+    res.send(err);
+  });
+});
+
+server.get('/api/posts/:id', (req, res) => {
+  const id = req.params.id;
+
+  db.findById(id)
+  .then(post => {
+    if (post) {
+      res.status(200).json( post );
+    } else{
+      res.status(404).json({ message: "The post with the specified ID does not exist." });
+    }
+  })
+  .catch( err => res.status(500).json(err))
+});
+
+
+server.listen(5000, () => console.log('server running!'));
