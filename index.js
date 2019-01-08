b5b41f1fdecd721ff8b559e722474ca13b707579
tryingtokeepup <kl.hawaii@gmail.com>
2019-01-07 16:36:43 -0800

    I keep messing this up. First commit, going to take code from lecture project and pop it into this.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // import your node modules
 
-const db = require('./data/db.js');
+const db = require("./data/db.js");
 
 // add your server code starting here
+
+//first commit, making the new branch for new system of pushing at lambda.
