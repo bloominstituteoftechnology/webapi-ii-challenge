33f72dd65e8edc92c723ec5b1ec112aeda4dd861
Aman <amsingh714@gmail.com>
2019-01-08 17:05:18 -0500

    DELETE is working

--- index.js
+++ index.js
@@ -74,4 +74,28 @@ server.post("/api/posts", (req, res) => {
   }
 });
 
+
+
+// DELETE
+server.delete("/api/posts/:id", (req, res) => {
+  const id = req.params.id;
+
+  db.findById(id)
+  .then(post => {
+    if (post) {
+      db.remove(id).then(count => {
+        res.status(200).json(post);
+      })
+    } else {
+      res.status(404).json({ message: "The post with the specified ID does not exist." })
+    }
+  })
+  .catch(err => res.status(500).json({ error: "The post could not be removed" }))
+})
+
+
+
+
+
+
 server.listen(port, () => console.log(`Port: ${port}`));
