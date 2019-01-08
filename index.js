90f7fc4326cade0bd325929d675aa211eeba836c
ToMMaaY <thuynh629@gmail.com>
2019-01-08 15:10:48 -0800

    Refactored code to use async await

--- index.js
+++ index.js
@@ -7,105 +7,104 @@ const cors = require("cors");
 server.use(cors());
 server.use(express.json());
 
-server.get("/", (req, res) => {
-  db.find()
-    .then(posts => {
-      res.status(200).json(posts);
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: "The posts information could not be retrieved." });
-    });
+server.get("/", async (req, res) => {
+  try {
+    const result = await db.find();
+
+    res.status(200).json(result);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "The posts information could not be retrieved." });
+  }
 });
 
-server.get("/api/posts/:id", (req, res) => {
+server.get("/api/posts/:id", async (req, res) => {
   const id = req.params.id;
 
-  db.findById(id)
-    .then(user => {
-      if (user.length > 0) {
-        res.status(200).json(user);
-      } else {
-        res
-          .status(404)
-          .json({ message: "The post with the specified ID does not exist." });
-      }
-    })
-    .catch(err => {
+  try {
+    const post = await db.findById(id);
+
+    if (post.length > 0) {
+      res.status(200).json(post);
+    } else {
       res
-        .status(500)
-        .json({ error: "The post information could not be retrieved." });
-    });
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
+    }
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "The post information could not be retrieved." });
+  }
 });
 
-server.post("/api/posts", (req, res) => {
-  const userInfo = req.body; // reads information from the body of the request
+server.post("/api/posts", async (req, res) => {
+  const userInfo = req.body;
 
-  db.insert(userInfo)
-    .then(response => {
-      if (!userInfo.title || !userInfo.contents) {
+  try {
+    const result = await db.insert(userInfo);
+
+    if (!userInfo.title || !userInfo.contents) {
+      res.status(400).json({
+        errorMessage: "Please provide title and contents for the post."
+      });
+    } else {
+      res.status(201).json(userInfo);
+    }
+  } catch (err) {
+    res.status(500).json({
+      error: "There was an error while saving the post to the database"
+    });
+  }
+});
+
+server.delete("/api/posts/:id", async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const post = await db.findById(id);
+
+    if (post.length) {
+      await db.remove(id);
+      res.status(200).json(post);
+    } else {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
+    }
+  } catch (err) {
+    res.status(500).json({ error: "The post could not be removed" });
+  }
+});
+
+server.put("/api/posts/:id", async (req, res) => {
+  const id = req.params.id;
+  const updatedObject = req.body;
+
+  try {
+    const post = await db.findById(id);
+
+    if (!post.length) {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
+    } else {
+      await db.update(id, updatedObject);
+
+      if (!updatedObject.title || !updatedObject.contents) {
         res.status(400).json({
           errorMessage: "Please provide title and contents for the post."
         });
       } else {
-        res.status(201).json(userInfo);
+        res.status(200).json(updatedObject);
       }
-    })
-    .catch(err => {
-      res.status(500).json({
-        error: "There was an error while saving the post to the database"
-      });
-    });
-});
-
-server.delete("/api/posts/:id", (req, res) => {
-  const id = req.params.id;
-
-  db.findById(id)
-    .then(user => {
-      if (user.length) {
-        db.remove(id).then(count => {
-          res.status(200).json(user);
-        });
-      } else {
-        res
-          .status(404)
-          .json({ message: "The post with the specified ID does not exist." });
-      }
-    })
-    .catch(err =>
-      res.status(500).json({ error: "The post could not be removed" })
-    );
-});
-
-server.put("/api/posts/:id", (req, res) => {
-  const id = req.params.id;
-  const updatedObject = req.body;
-
-  db.findById(id)
-    .then(post => {
-      if (!post.length) {
-        res
-          .status(404)
-          .json({ message: "The post with the specified ID does not exist." });
-      } else {
-        db.update(id, updatedObject).then(count => {
-          if (!updatedObject.title || !updatedObject.contents) {
-            res.status(400).json({
-              errorMessage: "Please provide title and contents for the post."
-            });
-          } else {
-            res.status(200).json(updatedObject);
-          }
-        });
-      }
-    })
-    .catch(err =>
-      res
-        .status(500)
-        .json({ error: "The post information could not be modified." })
-    );
+    }
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "The post information could not be modified." });
+  }
 });
 
 server.listen(5005);
