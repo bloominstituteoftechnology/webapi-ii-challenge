16047fe17d81a4edc2f4385f538fb5944ebfa072
Josh Z <jzieger10@gmail.com>
2019-01-08 18:03:03 -0500

    completed POST, PUT, DELETE endpoints. Started working on react app to list posts

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-// import your node modules
 
-const db = require('./data/db.js');
+const db = require("./data/db.js");
 
-// add your server code starting here
-// implement your API here
 const express = require("express");
 
 const server = express();
+
+server.use(express.json());
 
 server.get("/api/posts", (req, res) => {
 	db.find()
@@ -14,7 +13,9 @@ server.get("/api/posts", (req, res) => {
 			res.status(200).json({ posts });
 		})
 		.catch(() => {
-			res.status(500).json({ error: "The posts information could not be retrieved." });
+			res.status(500).json({
+				error: "The posts information could not be retrieved.",
+			});
 		});
 });
 
@@ -22,15 +23,99 @@ server.get("/api/posts/:id", (req, res) => {
 	const id = req.params.id;
 	db.findById(id)
 		.then(post => {
-            if(post.length < 1) {
-                res.status(404).json({ message: "The post with the specified ID does not exist." })
-            } else {
-                res.status(200).json(post);
-            }
+			if (post.length < 1) {
+				res.status(404).json({
+					message: "The post with the specified ID does not exist.",
+				});
+			} else {
+				res.status(200).json(post);
+			}
 		})
 		.catch(() => {
-			res.status(500).json({ error: "The post information could not be retrieved." });
+			res.status(500).json({
+				error: "The post information could not be retrieved.",
+			});
 		});
+});
+
+server.post("/api/posts", (req, res) => {
+	const post = req.body;
+	db.insert(post)
+		.then(result => {
+			if (post) {
+				db.findById(result.id)
+					.then(post => {
+						res.status(201).json(post);
+					})
+					.catch(err =>
+						res.status(500).json({
+							errorMessage: "The POST findById failed",
+							error: err,
+						})
+					);
+			} else {
+				res.status(400).json({
+					errorMessage:
+						"Please provide title and contents for the post.",
+				});
+			}
+		})
+		.catch(err =>
+			res.status(500).json({
+				error:
+					"There was an error while saving the post to the database",
+			})
+		);
+});
+
+server.put("/api/posts/:id", (req, res) => {
+	const id = req.params.id;
+	const postUpdates = req.body;
+	db.update(id, postUpdates)
+		.then(result => {
+			if (postUpdates && postUpdates.contents) {
+				db.findById(result.id)
+					.then(post => {
+						console.log(postUpdates);
+						res.status(201).json(postUpdates);
+					})
+					.catch(err =>
+						res.status(500).json({
+							errorMessage: "The PUT findById failed",
+							error: err,
+						})
+					);
+			} else {
+				res.status(400).json({
+					errorMessage:
+						"Please provide title and contents for the post.",
+				});
+			}
+		})
+		.catch(err =>
+			res.status(500).json({
+				error: "The post information could not be modified." ,
+			})
+		);
+});
+
+server.delete("/api/posts/:id", (req, res) => {
+	const id = req.params.id;
+	db.findById(id)
+		.then(post => {
+			if (post) {
+				db.remove(id).then(() => {
+					res.status(200).json(post);
+				});
+			} else {
+				res.status(404).json({
+					message: "The post with the specified ID does not exist.",
+				});
+			}
+		})
+		.catch(err =>
+			res.status(500).json({ error: "The post could not be removed" })
+		);
 });
 
 server.listen(5000, () => console.log("Server running"));
