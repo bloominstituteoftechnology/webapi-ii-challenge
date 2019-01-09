57574e99f6f52d66984c4d9c43a5b95e5d901024
AlexOstrander <alexanderostrander23@gmail.com>
2019-01-09 14:40:50 -0500

    Fixed if/else in my get request for api/posts/:id so that it will throw errors when requesting a ID with no post

--- index.js
+++ index.js
@@ -5,42 +5,46 @@ const db = require('./data/db.js');
 // add your server code starting here
 const server = express();
 const PORT = 4000
-
-
+server.use(express.json());
+//Grabs alll post in the database
 server.get('/api/posts', (req, res) => {
     db.find()
       .then(post => {
         res
             .json(post);
     })
+//Reports back in json data form for user when error occurs
       .catch(err => {
         res
             .status(500)
-            .json({ message: "Unable to retrieve posts"});
+            .json({ error: "The posts information could not be retrieved." });
     })
 });
 
-
+//Endpoints
+//Grabs posts by Id when the user request a specific post id
 server.get('/api/posts/:id', (req, res) => {
     const { id } = req.params;
     db.findById(id)
         .then(postId => {
-        if (postId) {
+        if (postId.length > 0) {
           res.json(postId);
         } else {
             res
                 .status(404)
-                .json({ message: "Post id invalid"});
+                .json({ message: "The post with the specified ID does not exist." });
         }
     })
+//Reports back in json data form for user when error occurs
         .catch(err => {
         res
             .status(500)
-            .json({ message: "Unable to get post Id"})
+            .json({ error: "The post information could not be retrieved." })
     })
 });
 
 
+//server listening service
 server.listen(PORT, () => {
     console.log(`Server is up on ${PORT}`)
 })
