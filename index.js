5be0564f6c2761677ad72ed5ae0b3e0a510e9192
AlexOstrander <alexanderostrander23@gmail.com>
2019-01-08 22:12:07 -0500

    Created Get endpoints for server

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
 // import your node modules
-
+const express = require('express');
 const db = require('./data/db.js');
 
 // add your server code starting here
+const server = express();
+const PORT = 4000
+
+
+server.get('/api/posts', (req, res) => {
+    db.find()
+      .then(post => {
+        res
+            .json(post);
+    })
+      .catch(err => {
+        res
+            .status(500)
+            .json({ message: "Unable to retrieve posts"});
+    })
+});
+
+
+server.get('/api/posts/:id', (req, res) => {
+    const { id } = req.params;
+    db.findById(id)
+        .then(postId => {
+        if (postId) {
+          res.json(postId);
+        } else {
+            res
+                .status(404)
+                .json({ message: "Post id invalid"});
+        }
+    })
+        .catch(err => {
+        res
+            .status(500)
+            .json({ message: "Unable to get post Id"})
+    })
+});
+
+
+server.listen(PORT, () => {
+    console.log(`Server is up on ${PORT}`)
+})
