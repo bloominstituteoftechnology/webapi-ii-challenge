2ecc5f0897a3c317367f948712472efc1471809c
Mr. Metelus <Mdmetelus@gmail.com>
2019-01-09 11:05:04 -0500

    "MVP complete"

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
 // import your node modules
 const express = require('express');
-
 const db = require('./data/db');
 
 // add your server code starting here
 const server = express();
 
 const PORT = 4545;
-const parser = express.json();
 
+//++++++++++++++++++++++++ 
+// middlewear  and addins
+//+++++++++++++++++++++++
+const parser = express.json();
 server.use(parser);
+
+
+// wire up middleware
+// server.use(express.json());
+
 
 
 server.get('/api/posts', (req, res) => {
     db.find()
-    .then(posts => {
-            console.log(`posts ${posts}`);
-            res.status(200).send(posts) 
+    .then( posts => {
+            console.log(`posts = ${posts}`);
+            res.status(200).send(posts); 
     })
     .catch(err => {
-        res.status(500).send(`<h1>Bad Request<h1>`);
-        //res.status(500).json({message: `failed to get users`});
+        // res.status(500).send(`<h1>Bad Request<h1>`);
+        res.status(500).json({message: `failed to get users`});
+        //res.(status(500).json(err);
     });
 });
     
@@ -44,15 +52,17 @@ server.get('/api/posts/:id', (req, res) => {
 //++++++++++++++++++++++++++++++++++++++++
 // Day 2 - put post delete stuff here
 //++++++++++++++++++++++++++++++++++++++++
+
+
 server.post('/api/posts', (req, res) => {
-    const black = req.body;
+    const post = req.body;
     console.log(req.body);
-    if (a && b) {
+    if (post.title && post.contents) {
         db.insert(post)
-            .then((postData)=> {
+            .then( postData => {
                 console.log('post from body', postData);
-                db.findById(postData.id).then( (post) => {
-                    res.status(201).json(post)
+                db.findById(postData.id).then( post => {
+                    res.status(201).json(post);
                 });
             })
             .catch( (err) => {
@@ -63,47 +73,66 @@ server.post('/api/posts', (req, res) => {
             });
 
     } else {
-        console.log("++++ERROR missing name or bio!! +++");
-        res.status(400).json({ message: "missing manditory data"});
+        console.log("++++ERROR missing title or contents!! +++");
+        res.status(400).json({ message: "missing manditory data!!"});
     }
 });
 
-server.delete('/api/posts/:id', (req, res) => {
-    // const { id } = req.params;
-    // or
-    const id = req.params.id;
-    console.log(id);
-    db.findById(id)
-    .then(user => {
-        if (user) {
 
-            db.remove(id)
-                .then(deleteNum => {
-                    console.log(deleteNum);
-                    if (count) {
-                        res.status(204).json(user);
-                    } else {
-                        res.status(404).json({ message: 'invalid id'});
-                    }
-                })
-                .catch(err => {
-                    res.status(500).json({ message: `Your failed to Delete user, please try again.` })
-                });
 
-        } else {
-            res.status(404)
-                    .json({ message: 'This user with the specfied ID does not exist.'})
-        }
-    })
-    .catch(err => {
-        res.status(500).json({ message: `Your failed to Identify a user, please try again.` })
-    });
-});
+
 
 //++++++++++++++++++++++++++++++++++++
 //+++++++  Put  ++++++++++++ regular Put
 //++++++++++++++++++++++++++++++++++++++
 
+server.put("/api/posts/:id", (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+  
+    db.findById(id)
+      .then(post => {
+        if (!post) {
+          res
+            .status(404)
+            .json({ message: "The post with the specified ID does not exist." });
+        }
+        if (!changes.title || !changes.contents) {
+          res.status(400).json({
+            errorMessage: "Please provide title and contents for the post."
+          });
+        }
+        db.update(id, changes).then(newPost => {
+          res.status(200).json(newPost);
+        });
+      })
+      .catch(err =>
+        res
+          .status(500)
+          .json({ error: "The post information could not be modified." })
+      );
+  });
+
+
+server.delete("/api/posts/:id", async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        const thisToGo = await db.findById(id);
+
+        if ( thisToGo === 0 || thisToGo === []) {
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        }
+
+        const thisPostGone = await db.remove(id);
+        if ( thisPostGone === 1 ) {
+            res.status(200).json(thisToGo);
+        }
+
+    } catch (error) {
+        res.status(500),json({ error: "The post could not be removed" })
+    }
+});
 
 
 
@@ -112,8 +141,9 @@ server.delete('/api/posts/:id', (req, res) => {
 //Listener
 //++++++++++++++++++++++++++++++++++++++
 
-server.listen(PORT, () => {
-    console.log(`The server is runnning on port ${PORT}`);
+server.listen( PORT, () => {
+    console.log(`The server is runnning on port ${ PORT }`);
+
 });
 
 
