0b93c88ff63f3f1b1a6b6b74d8f786f8480f5f4d
Cwright0428 <cwright0428@yahoo.com>
2019-01-08 20:02:19 -0600

    post and put functionality

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const server = express();
   res.send('<h1>Yo Wassup</h1>')
 })*/
 
+server.use(express.json());
+
 server.get("/api/posts", (req, res) => {
   db.find()
     .then(posts => {
@@ -33,5 +35,36 @@ server.get("/api/posts/:id", (req, res) => {
     .catch(err => {
       res.status(500).json({ message: "err" });
     });
+});
+
+server.post("/api/posts", (req, res) => {
+  const postInfo = req.body;
+  db.insert(postInfo).then(result => {
+    db.findById(result.id)
+      .then(posts => {
+        res.status(201).json(posts);
+      })
+      .catch(err =>
+        res.status(500).json({ message: "missing title", error: err })
+      );
+  });
+});
+
+server.put("/api/posts/:id", (req, res) => {
+  const id = req.params.id;
+  const post = req.body;
+  db.update(id, post).then(updated => {
+    if (!post.contents || !post.title) {
+      res
+        .status(400)
+        .json({ message: "Please provide title and contents for the post." });
+    } else if (!updated) {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
+    } else {
+      res.status(200).json(post);
+    }
+  });
 });
 server.listen(5000, () => console.log("up and at em!"));
