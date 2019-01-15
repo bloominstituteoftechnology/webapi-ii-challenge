f5eba83076bfa80c918d44bb659ff1383774d949
AlexOstrander <alexanderostrander23@gmail.com>
2019-01-14 21:59:13 -0500

    Troubleshooting git error

--- index.js
+++ index.js
@@ -89,6 +89,27 @@ server.put('/api/posts/:id', (req, res) => {
     const { id } = req.params;
     const post = req.body;
     
+    if (post.title && post.contents) {
+        db.update(id, post).then(count => {
+            if (count == true) {
+                db.findById(id).then(post => {
+                    res.json(post);
+                })
+            } else {
+                res
+                    .status(404)
+                    .json({ message: "The post with the specified ID does not exist!" })
+            }
+        }).catch(err => {
+            res
+                .status(500)
+                .json({ error: "The post information could not be modified." })
+        })
+    } else {
+        res
+            .status(400)
+            .json({ errorMessage: "Please provide title and contents for the post." })
+    }
 })
 
 
