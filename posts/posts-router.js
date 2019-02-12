201d9923115aaca4e88ee7fa1e05aa7d69601ce4
Scott Vojik <scott.vojik@gmail.com>
2019-02-12 14:19:57 -0800

    post functionality working

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -16,5 +16,34 @@ router.get('/', (req, res) => {
     })
 });
 
+router.get('/:id', (req, res) => {
+  db.findById(req.params.id)
+    .then(post => {
+      if (post) {
+        return res.status(200).json({ success: true, post })
+      } else {
+        res.status(404).json({ success: false, error: "The post with the specified ID does not exist."})
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ success: false, error: "The post information could not be retrieved."})
+    })
+});
+
+//POSTS
+
+router.post('/', async (req, res) => {
+  if (!req.body.title || !req.body.contents) {
+    return res.status(400).json({ success: false, error: "Please provide title and contents for the post."})
+  }
+  try {
+    const post = await db.insert(req.body);
+    res.status(201).json({ success: true, post})
+  } catch (error) {
+    res.status(500).json({ success: false, error: "There was an error while saving the post."})
+  }
+  
+});
+
 
 module.exports = router;
