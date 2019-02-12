51178dd1a5f8da865622b443cfdce6005e437ce0
Scott Vojik <scott.vojik@gmail.com>
2019-02-12 13:00:23 -0800

    GET is working

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -4,5 +4,17 @@ const db = require('../data/db');
 
 const router = express.Router();
 
+//GET
+
+router.get('/', (req, res) => {
+  db.find()
+    .then(posts => {
+      res.status(200).json({ success: true, posts });
+    })
+    .catch(error => {
+      res.status(500).json({ success: false, error: "The posts information could not be retrieved"})
+    })
+});
+
 
 module.exports = router;
