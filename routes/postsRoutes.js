4acdd06416cb7ca8a29ad612a4f84fb5ff113cc9
Noah <noah.stoeckel@gmail.com>
2019-02-28 20:50:08 -0500

    Adds Put Route To The Posts Router

--- routes/postsRoutes.js
+++ routes/postsRoutes.js
@@ -70,4 +70,29 @@ router.delete("/:id", (req, res) => {
     );
 });
 
+router.put("/:id", ({ params: { id }, body }, res) => {
+  //if the body has all the correct values
+  !body.title || !body.contents
+    ? res.status(400).json({
+        errorMessage: "Please provide title and contents for the post."
+      })
+    : db
+        .update(id, body)
+        .then(count => {
+          //if a post was updated
+          count > 0
+            ? db.findById(id).then(post => {
+                res.status(200).json(post);
+              })
+            : res.status(404).json({
+                message: "The post with the specified ID does not exist."
+              });
+        })
+        .catch(error =>
+          res
+            .status(500)
+            .json({ error: "The post information could not be modified." })
+        );
+});
+
 module.exports = router;
