317eb71a5b864b7d37f7f118523e46e42ba7aa78
Noah <noah.stoeckel@gmail.com>
2019-03-02 12:48:18 -0500

    Styles Post Component

--- blog-client/src/components/Post.js
+++ blog-client/src/components/Post.js
@@ -3,25 +3,58 @@ import "./Post.css";
 
 const Post = props => {
   const [post, setPost] = useState({});
+  const [isEditing, setIsEditing] = useState(false);
+  const [title, setTitle] = useState("");
+  const [contents, setContents] = useState("");
 
   useEffect(() => {
     fetch(`http://localhost:8000/api/posts/${props.postId}`)
       .then(response => response.json())
       .then(post => {
         setPost(post[0]);
+        setTitle(post[0].title);
+        setContents(post[0].contents);
       });
   }, []);
 
   return (
     <section className="post-container">
       <div className="title">
-        <h2>{post.title}</h2>
+        {isEditing ? (
+          <input
+            type="text"
+            name="title"
+            className="input-title"
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+          />
+        ) : (
+          <h2>{post.title}</h2>
+        )}
         <div className="buttons">
-          <button>Δ</button>
+          <button
+            onClick={e => {
+              e.preventDefault();
+              setIsEditing(!isEditing);
+            }}
+          >
+            Δ
+          </button>
           <button>X</button>
         </div>
       </div>
-      <p>{post.contents}</p>
+      {isEditing ? (
+        <textarea
+          name="contents"
+          className="textarea-contents"
+          cols="90"
+          rows="10"
+          value={contents}
+          onChange={e => setContents(e.target.value)}
+        />
+      ) : (
+        <p>{post.contents}</p>
+      )}
     </section>
   );
 };
