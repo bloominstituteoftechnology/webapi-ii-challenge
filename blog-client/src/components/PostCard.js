6be493cd6694b49cb36ef4bb317f56729eaebfa4
Noah <noah.stoeckel@gmail.com>
2019-03-01 22:23:51 -0500

    Adds Text Clipping To The Postcard Component

--- blog-client/src/components/PostCard.js
+++ blog-client/src/components/PostCard.js
@@ -2,16 +2,19 @@ import React from "react";
 import "./PostCard.css";
 
 const PostCard = ({ title, contents }) => {
+  function clipText(text, maxLength) {
+    return text.slice(0, maxLength).concat(" ...");
+  }
+
+  const maxBodyLen = 230;
+  const maxTitleLen = 25;
+
   return (
-    <section>
-      <h2>{title}</h2>
-      <p>{contents}</p>
+    <section className="post-card">
+      <h2>{clipText(title, maxTitleLen)}</h2>
+      <p>{clipText(contents, maxBodyLen)}</p>
     </section>
   );
 };
 
 export default PostCard;
-
-// {
-//
-// }
