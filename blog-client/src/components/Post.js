f36a56db11a47a3777082e96fa16d077c6d7ab16
Noah <noah.stoeckel@gmail.com>
2019-03-02 13:31:52 -0500

    Adds Update And Delete Methods To Post Component
    - fixes styling of buttons when title is large

--- blog-client/src/components/Post.js
+++ blog-client/src/components/Post.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./Post.css";
 
 const Post = props => {
+  console.log(props);
   const [post, setPost] = useState({});
   const [isEditing, setIsEditing] = useState(false);
   const [title, setTitle] = useState("");
@@ -14,8 +15,37 @@ const Post = props => {
         setPost(post[0]);
         setTitle(post[0].title);
         setContents(post[0].contents);
-      });
+      })
+      .catch(error => console.log(error));
   }, []);
+
+  const updatePost = postBody => {
+    fetch(`http://localhost:8000/api/posts/${props.postId}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(postBody)
+    })
+      .then(response => response.json())
+      .then(pst => {
+        setPost({ ...post, ...postBody });
+      });
+  };
+
+  const deletePost = id => {
+    fetch(`http://localhost:8000/api/posts/${props.postId}`, {
+      method: "DELETE"
+    })
+      .then(response => response.json())
+      .then(msg => {
+        console.log(msg);
+      });
+  };
+
+  const showPosts = () => {
+    props.setDisplayForm(false);
+    props.setDisplayPost(false);
+    props.setDisplayPosts(true);
+  };
 
   return (
     <section className="post-container">
@@ -32,15 +62,46 @@ const Post = props => {
           <h2>{post.title}</h2>
         )}
         <div className="buttons">
-          <button
-            onClick={e => {
-              e.preventDefault();
-              setIsEditing(!isEditing);
-            }}
-          >
-            Δ
-          </button>
-          <button>X</button>
+          {isEditing ? (
+            <button
+              onClick={() => {
+                updatePost({ title: title, contents: contents });
+                setIsEditing(false);
+              }}
+              className="btn-check"
+            >
+              &#10004;
+            </button>
+          ) : (
+            <button
+              onClick={e => {
+                e.preventDefault();
+                setIsEditing(true);
+              }}
+            >
+              Δ
+            </button>
+          )}
+          {isEditing ? (
+            <button
+              onClick={e => {
+                e.preventDefault();
+                setIsEditing(false);
+                setContents(post.contents);
+              }}
+            >
+              Ø
+            </button>
+          ) : (
+            <button
+              onClick={() => {
+                deletePost();
+                showPosts();
+              }}
+            >
+              X
+            </button>
+          )}
         </div>
       </div>
       {isEditing ? (
@@ -60,3 +121,5 @@ const Post = props => {
 };
 
 export default Post;
+
+//
