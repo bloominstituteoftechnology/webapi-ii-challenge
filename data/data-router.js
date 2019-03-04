134de80d2739bc7d408cef772b7d0cd7b0c86669
Kseniya Platonava <ksu2008@gmail.com>
2019-03-04 16:35:39 -0500

    Add Get by ID

--- data/data-router.js
+++ data/data-router.js
@@ -17,6 +17,24 @@ router.get("/", async (req, res) => {
 
 //GET ID
 
+router.get('/:id', async (req, res) => {
+  try {
+    const post = await Posts.findById(req.params.id);
+
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ message: "The post with the specified ID does not exist." });
+    }
+  } catch (error) {
+    // log error to database
+    console.log(error);
+    res.status(500).json({
+      message: 'The post information could not be retrieved.',
+    });
+  }
+});
+
 //DELETE
 
 //PUT
