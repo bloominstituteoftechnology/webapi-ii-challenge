11d95415180a6a666c4299afeeb134abac5c6bb8
Kseniya Platonava <ksu2008@gmail.com>
2019-03-04 16:53:15 -0500

    Working on Put and Post

--- data/data-router.js
+++ data/data-router.js
@@ -56,8 +56,47 @@ router.delete('/:id', async (req, res) => {
 
 //PUT
 
+
+router.put('/:id', async (req, res) => {
+
+  const { title, contents } = req.body
+
+  try {
+    const post = await Posts.update(req.params.id, { title, contents });
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ message: "Please provide title and contents for the post." });
+    }
+  } catch (error) {
+    // log error to database
+    console.log(error);
+    res.status(500).json({
+      message: "The post information could not be modified.",
+    });
+  }
+});
+
 //POST
 
+// router.post('/:id/messages', async (req, res) => {
+//   const newMessage = req.body;
+//   newMessage.hub_id = req.params.id;
+
+//   try {
+//     const hub = await Hubs.findById(req.params.id);
+
+//     if (hub) {
+//       const message = await Hubs.addMessage(newMessage);
+//       res.status(201).json(message);
+//     } else {
+//       res.status(400).json({err: 'invalid hub id'});
+//     }
+//   } catch (e) {
+//     res.status(500).json({err: e});
+//   }
+
+// });
 
 
 
