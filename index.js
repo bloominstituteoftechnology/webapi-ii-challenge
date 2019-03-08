15113416e9b538c299df59a5e4276d705e4bf0f7
Mckay <mckaywrigley@gmail.com>
2019-03-07 23:02:06 -0700

    added POST api/posts

--- index.js
+++ index.js
@@ -10,6 +10,23 @@ const server = express();
 // Middleware
 server.use(express.json());
 
+
+// CREATE
+server.post('/api/posts', (req, res) => {
+    const { title, contents, created_at, updated_at } = req.body;
+    if(!title || !contents) {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+    }
+    db.insert({ title, contents, created_at, updated_at })
+        .then(post => {
+            res.status(201).json(post)
+        })
+        .catch(err => {
+            res.status(500).json({ error: "There was an error while saving the post to the database." })
+        })
+});
+
+
 // Run Server
 server.listen(3000, () => {
     console.log('server running');
