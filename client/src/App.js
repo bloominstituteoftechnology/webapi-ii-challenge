426b2f615ed5e03cd440a7890f6e5a8d0350d583
Brian Illes <brianrilles@gmail.com>
2019-03-19 15:38:39 -0700

    user can delete posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,30 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-function Post({ post }) {
+function Post({ post, id, deletePost }) {
   return (
     <div className="post-wrapper">
       <h2>{post.title}</h2>
       <p>{post.contents}</p>
+      <span onClick={() => deletePost(id)}>delete post</span>
     </div>
   );
 }
 
 export default function App() {
   const [posts, setPosts] = useState([]);
+
+  const deletePost = id => {
+    axios
+    .delete(`http://localhost:9000/api/posts/${id}`)
+  
+    .catch(err => {
+      console.log(err)
+    })
+    // const newPosts = [...posts];
+    // newPosts.splice(id, 1);
+    // setPosts(newPosts);
+  }
 
   useEffect(() => {
     axios
@@ -28,8 +41,8 @@ export default function App() {
   return (
     <div className="app">
       <h1>Posts:</h1>
-      {posts.map((post, index) => (
-        <Post post={post} />
+      {posts.map((post) => (
+        <Post post={post} id={post.id} key={post.id} deletePost={deletePost} />
       ))}
     </div>
   );
