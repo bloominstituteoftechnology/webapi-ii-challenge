d88e483cdd1f1d9d512e9dd5eec3ba2b199155f8
Brian Illes <brianrilles@gmail.com>
2019-03-19 12:48:50 -0700

    GET /api/posts/:id

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -36,4 +36,23 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  db.findById(id)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: "The post information could not be retrieved."
+      });
+    });
+});
+
 module.exports = router;
