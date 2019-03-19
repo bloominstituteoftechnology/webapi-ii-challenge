c5946473d0e53f2f59c37ea56b7a8cec45eab804
Maksim V <maks112v@gmail.com>
2019-03-19 16:21:35 -0400

    finished

--- data/router.js
+++ data/router.js
@@ -16,36 +16,90 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   try {
-
+    const post = await Posts.findById(req.params.id);
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({
+        message: "The post with the specified ID does not exist."
+      })
+    }
   } catch (error) {
     console.log(error);
     res.status(500).json({
-      error: "The posts information could not be retrieved."
+      error: "The post information could not be retrieved."
     });
   }
 });
 
-router.post('/', (req, res) => {
-  console.log(req.body);
+router.post('/', async (req, res) => {
   const {
     title,
     contents
   } = req.body;
-  console.log(title, contents);
   if (title && contents) {
     try {
-      
+      const post = await Posts.insert({
+        title,
+        contents
+      });
+      res.status(201).json(post);
     } catch (error) {
       console.log(error);
-      res.status(500).json({ error: "There was an error while saving the post to the database" })
+      res.status(500).json({
+        error: "There was an error while saving the post to the database"
+      })
     }
   } else {
     res.status(400).json({
       errorMessage: "Please provide title and contents for the post."
     });
   }
+});
+
+router.delete('/:id', async (req, res) => {
+  try{
+    const post = await Posts.remove(req.params.id);
+    console.log(post);
+    if(post > 0){
+      res.status(200).json({message: "post deleted"})
+    }
+    else {
+      res.status(404).json({ message: "The post with the specified ID does not exist." });
+    }
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "The post could not be removed" });
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  const {
+    title,
+    contents
+  } = req.body;
+  if (title && contents) {
+    try {
+      const post = await Posts.update(req.params.id, {title, contents});
+      if (post > 0){
+        res.status(200).json(post);
+      }
+      else {
+        res.status(404).json({ message: "The post with the specified ID does not exist." });
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        error: "The post information could not be modified."
+      })
+    }
+  } else {
+    res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+  }
+
 });
 
 module.exports = router;
