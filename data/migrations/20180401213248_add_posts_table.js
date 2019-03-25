218d037fccd620682a5fcce0d8037472fee2b784
Luis Hernandez <luishrd@gmail.com>
2019-03-25 09:01:27 -0600

    updates db schema

--- data/migrations/20180401213248_add_posts_table.js
+++ data/migrations/20180401213248_add_posts_table.js
@@ -2,7 +2,7 @@ exports.up = function(knex) {
   return knex.schema.createTable('posts', function(posts) {
     posts.increments();
 
-    posts.text('title').notNullable();
+    posts.string('title', 1024).notNullable();
     posts.text('contents').notNullable();
 
     posts.timestamps(true, true);
