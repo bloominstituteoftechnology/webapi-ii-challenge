764f1398788b3c0761d8ebb629798290463ca855
Patrick <patrick.schwindt14@gmail.com>
2019-03-26 17:45:26 -0600

    Commit

--- data/hubs/posts-router.js
+++ data/hubs/posts-router.js
@@ -66,15 +66,15 @@ router.get('/', async (req, res) => {
     try {
       const count = await Hubs.remove(req.params.id);
       if (count > 0) {
-        res.status(200).json({ message: 'The hub has been nuked' });
+        res.status(200).json({ message: 'Yay' });
       } else {
-        res.status(404).json({ message: 'The hub could not be found' });
+        res.status(404).json({ message: 'The post with the specified ID does not exist.' });
       }
     } catch (error) {
       // log error to database
       console.log(error);
       res.status(500).json({
-        message: 'The post could not be removed',
+        message: 'The post information could not be retrieved.',
       });
     }
   });
