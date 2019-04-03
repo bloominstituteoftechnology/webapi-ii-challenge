dc231944c573f9f13c38905ba7d799241612fb47
Marc Thiongo <marcthuo@gmail.com>
2019-04-02 21:22:32 -0400

    complete with corrections

--- neon/neon-router.js
+++ neon/neon-router.js
@@ -6,27 +6,44 @@ const router = express.Router()
 
 router.post('/', async (req, res) => {
     try {
-       const dub = await Data.insert(req.body);
-       if (!title || !contents) {
-           res.status(400).json({ 
-               message: 'Please provide title and contents for the post.',
-           })
-           return;
-     }
-     res.json(dub);
-     res.status(201).json(dub);
-    }
-    catch (error) {
-        console.log(error);
-        res.status(500).json({
-            message: 'There was an error while saving the post to the database',
-        });
-    }
-});
+        // const postData = req.body;
+        const postId = await Data.insert(req.body);
+        // const post = await Data.findById(postId.id);
+        res.status(201).json(postId);
+      } catch (error) {
+        let message = "There was an error while saving the post to the database";
+    
+        if (error.errno === 19) {
+          message = "please provide both the title and the contents";
+        }
+        res.status(500).json({ message: message, error });
+      }
+})
+
+
+    // router.post('/', async (req, res) => {
+    //     try {
+//        const dub = await Data.insert(req.body);
+//        if (error.errno===19) { // Use when there is no title or content
+//            res.status(400).json({ 
+//                message: 'Please provide title and contents for the post.',
+//            });
+//            return;
+//      }
+     
+//      res.status(201).json(dub);
+//     }
+//     catch (error) {
+//         console.log(error);
+//         res.status(500).json({
+//             message: 'There was an error while saving the post to the database',
+//         });
+//     }
+// });
 
 router.get('/', async (req, res) => {
     try {
-        const dubs = await Data.find(req.body);
+        const dubs = await Data.find();
         res.status(200).json(dubs);
     } 
     catch (error) {
@@ -38,13 +55,16 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
+    const {id} = req.params;
     try {
-        const dub = await Data.findById(req.params.id);
-        if (!id) {
-            res.status(404).json({
-                message: `The post with the specified ID, ${id} does not exist.`,
-            })
-        } res.json(dub);
+        const dub = await Data.findById(id);
+        if (dub) {
+            res.status(200).json(dub)
+        } else {
+        res.status(404).json({
+            message: `The post with the specified ID, ${id} does not exist.`,
+        }) 
+    }
     } 
     catch (error) {
         console.log(error);
@@ -56,8 +76,10 @@ router.get('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
     try {
-        const dubs = await Data.remove(req.params.id);
-        if (!id) {
+        const dubs = await Data.remove(req.params.id).then(dubs => {
+            res.status(200).json(dubs)
+        });
+        {
             res.status(404).end({
                 message: `The post with the specified ID, ${id} does not exist.`,
             })
@@ -71,29 +93,27 @@ router.delete('/:id', async (req, res) => {
     }
 });
 
-
-router.put('./:id', async (req, res) => {
+router.put('/:id', async (req, res) => {
     try {
-        const dub = await Data.update(req.params.id, req.body);
-        if (!title || !contents) {
-            res.status(400).json({
-                message: 'Please provide title and contents for the post.',
-            })
-        } res.json(dub);
+        const dub = await Data.update(req.params.id, req.body).then(dub => {
+            res.status(200).json(dub);
+        });
+        {
         res.status(200).json(Data);
         if (!id) { 
             res.status(404).json({
                 message: `The post with the specified Id, ${id} does not exist.`,
             })
         } res.json(dub);
-    }
+    }}
     catch (error) {
         console.log(error);
-        res.status(500).json({
-            message: 'The post information could not be modified.',
-        });
-    } 
-});   
+        if (error.errno===19) {
+            let message = 'Please provide title and contents for the post.'
+        
+        res.status(500).json({message: 'The post information could not be modified.'});
+    }}
+});    
    
 
 module.exports = router;
