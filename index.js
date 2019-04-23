c4a1a83cd00bda4149262ca96b4fe255764a77f9
Chubbard022 <chubbard022@gmail.com>
2019-04-23 12:02:06 -0700

    getting message by id working, error not showing up when id is not defined

--- index.js
+++ index.js
@@ -17,6 +17,20 @@ server.get("/api/posts",(req,res)=>{
     })
 })
 //-------------------------------------
+server.get("/api/posts/:id",(req,res)=>{
+    const messageID = req.params.id
+    db.findById(messageID)
+    .then(message=>{
+        if(!messageID){
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        }else
+        res.status(200).json(message)
+    })
+    .catch(err=>{
+        res.status(500).json({ error: "The post information could not be retrieved." })
+    })
+})
+//-------------------------------------
 server.listen(4000, () => {
   console.log('\n*** Server Running on http://localhost:4000 ***\n');
 });
