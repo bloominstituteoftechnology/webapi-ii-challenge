621e1b98d91f4bfdd2dd403915a53e401592fdc2
Chubbard022 <chubbard022@gmail.com>
2019-04-23 12:29:53 -0700

    delete functionality working

--- index.js
+++ index.js
@@ -23,12 +23,39 @@ server.get("/api/posts/:id",(req,res)=>{
     .then(message=>{
         if(!messageID){
             res.status(404).json({ message: "The post with the specified ID does not exist." })
-        }else
+        }else{
         res.status(200).json(message)
-    })
+    }})
     .catch(err=>{
         res.status(500).json({ error: "The post information could not be retrieved." })
     })
+})
+//-------------------------------------
+server.post("/api/posts",(req,res)=>{
+    db.insert()
+    .then()
+    .catch()
+})
+//-------------------------------------
+server.delete("/api/posts/:id",(req,res)=>{
+    const messageID = req.params.id
+    db.remove(messageID)
+    .then(message=>{
+        if(!message){
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        }else{
+            res.status(204).json(message)
+        }
+    })
+    .catch(err=>{
+        res.status(500).json({ error: "The post could not be removed" })
+    })
+})
+//-------------------------------------
+server.put("/api/posts/:id",(req,res)=>{
+    db.update()
+    .then()
+    .catch()
 })
 //-------------------------------------
 server.listen(4000, () => {
