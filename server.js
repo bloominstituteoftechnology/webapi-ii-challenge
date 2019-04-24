25a641e38cb1adc0c5c95a91263b8288a8be69e1
jjgorton <jjgorton@gmail.com>
2019-04-23 23:12:12 -0400

    changes?

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const postsRouter = require('./posts/posts-router');
 
 const server = express();
 
+const cors = require('cors');
+
 server.use(express.json());
+
+server.use(cors());
 
 server.get('/', (req, res) => {
 	res.send(`
-	<h2>its working!</h2>
+	<h2>One Ring to Rule them All...</h2>
 	`);
 });
 
