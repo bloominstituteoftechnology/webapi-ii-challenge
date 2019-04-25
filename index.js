6e8b6497900fa115f4dbfe85f9b3e53890424f9d
Chubbard022 <chubbard022@gmail.com>
2019-04-25 11:11:22 -0700

    deploying to heroku

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const server = require("./server")
+const evnReader = require("dotenv")
+evnReader.config(); //reads the .env file and merges it into process.env
 
-server.listen(4000, () => {
+
+const server = require("./server")
+const port = process.env.PORT || 4000;
+
+server.listen(port, () => {
   console.log('\n*** Server Running on http://localhost:4000 ***\n');
 });
