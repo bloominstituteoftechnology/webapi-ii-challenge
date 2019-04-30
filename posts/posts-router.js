ad1e177696adb95b0b390d5e1520949ef2708dfe
cammac <camm@rocktechcomputer.com>
2019-04-30 13:51:28 -0700

    Added delete endpoint

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -42,4 +42,23 @@ router.post('/', async (req, res) => {
   }
 })
 
+router.delete('/:id', async (req, res) => {
+  try {
+    const count = await db.remove(req.params.id);
+    if (count > 0) {
+      res.status(200).json({
+        message: "The post has been removed"
+      })
+    } else {
+      res.status(404).json({
+        message: "The post with the specified ID does not exist."
+      })
+    }
+  } catch (error) {
+    res.status(500).json({
+      error: "The post could not be removed"
+    })
+  }
+})
+
 module.exports = router;
