523b6ac06d6dbda687f71dbb0d22f7bb8e543f57
cammac <camm@rocktechcomputer.com>
2019-04-30 13:27:02 -0700

    Added get by id and post endpoints

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -8,13 +8,38 @@ router.get('/', async (req, res) => {
     const posts = await db.find(req.query);
     res.status(200).json(posts)
   } catch (error) {
-    console.log(error);
     res.status(500).json({
-      message: 'Error retrieving the posts'
+      error: "The posts information could not be retrieved."
     })
   };
 });
 
+router.get('/:id', async (req, res) => {
+  try {
+    const post = await db.findById(req.params.id);
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({
+        message: "The post with the specified ID does not exist."
+      })
+    }
+  } catch (error) {
+    res.status(500).json({
+      error: "The post information could not be retrieved."
+    })
+  }
+})
 
+router.post('/', async (req, res) => {
+  try {
+    const post = await db.insert(req.body);
+    res.status(201).json(post);
+  } catch (error) {
+    res.status(500).json({
+      error: "There was an error while saving the post to the database"
+    })
+  }
+})
 
 module.exports = router;
