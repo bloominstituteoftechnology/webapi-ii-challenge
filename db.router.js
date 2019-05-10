93b9cd5a15c8f114b8082820bd8be2a0da380b42
Miguel Diaz <37884809+diaz4674@users.noreply.github.com>
2019-05-09 23:31:27 -0700

    working Delete function

--- db.router.js
+++ db.router.js
@@ -47,6 +47,19 @@ router.post('/', async (req, res) => {
     }
 })
 
+//Delete
+router.delete('/:id', async (req, res) => {
+    try {
+        const  remove = await db.remove(req.params.id);
+        if(remove > 0 ) {
+            res.status(200).json({message: 'The user has been destroyed'})
+        } else {
+            res.status(404).json({message: "The post with the specified ID does not exist"})
+        } 
+    } catch(error) {
+        res.status(500).json({message: "The post could not be removed"})
+    }
+})
 
 
 module.exports = router;
