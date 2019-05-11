6ea2ae4ebc2457c036932e1d3ec57c5c40d2ce0b
Ronny Alvarado <rsalvarado777@gmail.com>
2019-05-10 22:07:10 -0500

    Created the get endpoint to retrieve post specific by id - also had to use .first() on findbyId function to make it work though

--- router/posts-router.js
+++ router/posts-router.js
@@ -15,4 +15,22 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const posts = await db.findById(id);
+    if (posts) {
+      res.status(200).json(posts);
+    } else {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
+    }
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "The post information could not be retrieved" });
+  }
+});
+
 module.exports = router;
