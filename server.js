6e9ac49b3b2a8dc27e9e1fb1c894b3531584120d
cpscott1 <thacam18@gmail.com>
2019-05-16 21:47:56 -0500

    testing something

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const server = express();
 server.use(express.json());
 server.use('/api/posts', PostsRouter);
 
+server.get('/', (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`)
+});
+
 
 
 
