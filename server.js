7e5f50d6f389507a505ca1107621fa9e07f37bb2
cpscott1 <thacam18@gmail.com>
2019-05-16 23:54:17 -0500

    upteenth test

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PostsRouter = require('./posts/posts-router.js');
 const server = express();
 
 server.use(express.json());
-server.use('https://sprint-app.herokuapp.com/api/posts', PostsRouter);
+server.use('api/posts', PostsRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's figure this out together</h2>`)
