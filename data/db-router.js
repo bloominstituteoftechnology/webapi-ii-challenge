541793f953d8f43759bce48b9da2fd750099fcdd
shanreed <srtech5set@gmail.com>
2019-06-05 07:44:26 -0700

    mvp completed

--- data/db-router.js
+++ data/db-router.js
@@ -4,8 +4,42 @@ const Comments = require('./db');
 
 const router = express.Router();
 
-//GET
-router.get('/', async (req, res) => {
+
+//POST
+router.post('/', async (req, res) => {
+    try {
+        const {title, contents} = req.body
+        const post = await Posts.insert(req.body);
+      if (title && contents) {
+      res.status(201).json(post);
+      } else if (!title || !contents) {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+      }
+    } catch (error) {
+      // log error to database
+      console.log(error);
+      res.status(500).json({error: "There was an error while saving the post to the database"});
+    }
+  });
+//POST
+  
+router.post('/:id/comments', async (req, res) => {
+    try {
+        const comments = await Posts.insertComment({post_id:req.params.id, text:req.body.text});
+        if (comments) {
+            res.status(201).json(comments);
+          } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist." });
+          }
+        } catch (error) {
+          // log error to database
+          console.log(error);
+          res.status(500).json({error: "The post information could not be retrieved."});
+        }
+})
+
+  //GET
+  router.get('/', async (req, res) => {
     try {
       const posts = await Posts.find(req.query);
       res.status(200).json(posts);
@@ -47,39 +81,7 @@ router.get('/', async (req, res) => {
     }
   })
 
-//Post
-  router.post('/', async (req, res) => {
-    try {
-        const {title, contents} = req.body
-        const post = await Posts.insert(req.body);
-      if (title && contents) {
-      res.status(201).json(post);
-      } else if (!title || !contents) {
-        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
-      }
-    } catch (error) {
-      // log error to database
-      console.log(error);
-      res.status(500).json({error: "There was an error while saving the post to the database"});
-    }
-  });
-
-  router.post('/:id/comments', async (req, res) => {
-    try {
-        const {text} = req.body
-        const comment = await Comments.insertComment(req.body);
-      if (title && contents) {
-      res.status(201).json(comment);
-      } else if (!title || !contents) {
-        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
-      }
-    } catch (error) {
-      // log error to database
-      console.log(error);
-      res.status(500).json({error: "There was an error while saving the post to the database"});
-    }
-  });
-
+  //DELETE
   router.delete('/:id', async (req, res) => {
     try {
       const count = await Posts.remove(req.params.id);
@@ -94,7 +96,8 @@ router.get('/', async (req, res) => {
       res.status(500).json({error: "The post could not be removed" });
     }
   });
-
+  
+//PUT/UPDATE
   router.put('/:id', async (req, res) => {
     try {
         const {title, contents} = req.body
