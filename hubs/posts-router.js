2984b68341ce7a88e56ac2d74c92a5e5491324f2
MrGio7 <lashagiorgi1233@gmail.com>
2019-06-20 15:23:55 +0400

    added findById() router.get function

--- hubs/posts-router.js
+++ hubs/posts-router.js
@@ -1,14 +1,28 @@
 const router = require('express').Router();
 const Posts = require('../data/db');
 
-router.get('/', async (req, res) => {
-    const posts = await Posts.find(req.params)
+router.get('/',  (req, res) => {
+    const posts = Posts.find(req.params)
     .then(posts => {
         res.status(200).json(posts);
     })
     .catch(errorMessage => {
-        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+        res.status(500).json({ error: "The posts information could not be retrieved." })
     })
 });
+
+router.get('/:id', (req, res) => {
+    const post = Posts.findById(req.params.id)
+    .then(post => {
+        if (post) {
+        res.status(200).json(post)
+    } else {
+        res.status(404).json({ message: "The post with the specified ID does not exist." });
+    }
+    })
+    .catch(error => {
+        res.status(404).json({ message: "The post with the specified ID does not exist." })
+    })
+})
 
 module.exports = router;
