322d0d6c76182a80befb116a568e4e7428260838
alexisjcarr <acarr59@gmail.com>
2019-07-09 12:23:18 -0500

    ➕ Adds axios dependency to the client
     #1

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+import "./App.css";
+
+const App = () => {
+  const [posts, setPosts] = useState([]);
+  useEffect(()=> {
+
+  }, [posts])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World!</h1>
     </div>
   );
-}
+};
 
 export default App;
