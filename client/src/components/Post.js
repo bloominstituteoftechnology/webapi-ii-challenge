2c784135e84a4a632bb39b7d818af69bcb4ee4b5
alexisjcarr <acarr59@gmail.com>
2019-07-09 12:43:25 -0500

    ➕ Adds styled-components dependency and adds light styling.
     #1

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,11 +1,29 @@
 import React from "react";
+import styled from "styled-components";
+
+// const StyledNav = styled(NavLink)`
+//   margin: 10px;
+//   color: black;
+//   &:focus, &:hover, &:visited, &:link, &:active {
+//       text-decoration: none;
+// `;
+
+const Card = styled.div`
+  border-radius: 50px;
+  padding: 30px;
+  width: 500px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  text-align: center;
+  margin: 0 auto;
+  margin-bottom: 20px;
+`;
 
 const Post = props => {
   return (
-    <div>
+    <Card>
       <h1>{props.post.title}</h1>
       <p>{props.post.contents}</p>
-    </div>
+    </Card>
   );
 };
 
