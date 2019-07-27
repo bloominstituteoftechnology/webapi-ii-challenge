81b79c8a23c7cb407d759fdb7c06250221385c7f
Mariana Huh <mhuh0176@gmail.com>
2019-07-26 18:41:10 -0700

    GET/:id works

--- data/posts-router.js
+++ data/posts-router.js
@@ -12,4 +12,44 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.post('/', async (req, res) => {
+  const post = { ...req.body };
+  try {
+    const newPost = await Posts.insert(req.body);
+    if (newPost.title && newPost.contents) {
+      console.log(req.body, newPost);
+      res.status(201).json(post);
+    } else {
+      res.status(400).json({
+        success: false,
+        message: 'Please provide title and contents for the post.'
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: 'There was an error while saving the post to the database'
+    });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const post = await Posts.findById(req.params.id);
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res
+        .status(404)
+        .json({ message: 'The post with the specified ID does not exist.' });
+    }
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ message: 'The post information could not be retrieved.' });
+  }
+});
+
 module.exports = router;
