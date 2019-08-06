bdf2d73e6aa09bb9ec1d3557287fd7bfad31c1da
Richard Scott Prins Jr <richardsprins@gmail.com>
2019-08-06 14:49:41 -0700

    adding endponts for post routes

--- routes/post-router.js
+++ routes/post-router.js
@@ -18,7 +18,7 @@ router.get('/:id', (req, res) => {
   const postId = req.params.id;
   console.log('post ID', postId)
   const postData = req.body;
-  console.log('psot data', postData)
+  console.log('post data', postData)
 
   if(!postId){
     res.status(404).json({ message: "The post with the specified ID does not exist." })
@@ -33,6 +33,24 @@ router.get('/:id', (req, res) => {
   }
 })
 
+// POST new post 
+router.post('/', (req, res) => {
+  const postData = req.body;
+  console.log('post data POST', postData);
+
+  if(!postData.title || !postData.contents) {
+    res.status(400).json({ message: "Please provide title and contents for the post." })
+  } else {
+    Data.insert(postData)
+    .then(post => {
+      res.status(201).json(post)
+      console.log('post', post)
+    })
+    .catch(error => {
+      res.status(500).json({ message: "There was an error while saving the post to the database" })
+    })
+  }
+})
 
 
 module.exports = router
