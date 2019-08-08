81ec156e544f4bf30b61efe096a434ed72508523
Zaur <42130952+ZaurInvictus@users.noreply.github.com>
2019-08-08 16:59:21 -0500

    post call created

--- index.js
+++ index.js
@@ -7,8 +7,40 @@ server.use(express.json())
 
 
 server.post('/api/posts', (req, res) => {
-  res.send('Hello')
+  const { title, contents } = req.body
+  console.log(req.body)
+
+  if(!title || !contents) {
+    res.status(400)
+    .json({ error: "Please provide title and contents for the post."})
+  } else {
+    db.insert(req.body)
+    .then(newUser => {
+      res.status(201).json(newUser)
+    })
+    .catch(() => {
+      res.status(500).json({
+         error: "There was an error while saving the post to the database"
+      })
+    })
+  }
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 server.listen(4000, () => {
