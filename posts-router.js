cf1b1ac7bb29253ea7329f94fc0ee12cedba1d1b
cwern40 <chris.wernli@hotmail.com>
2019-08-10 10:32:51 -0600

    I created the posts-router.js file and imported it into the server.js file

---
+++ posts-router.js
@@ -0,0 +1,5 @@
+const express = require('express');
+
+const posts = require('./data/db');
+
+const router = express.Router();
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express') //importing in express
 
+const PostsRouter = require('./posts-router');
+
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/posts', PostsRouter);
 
 server.get('/', (req, res) => {
     res.send(`<h1>Test<h1>`)
