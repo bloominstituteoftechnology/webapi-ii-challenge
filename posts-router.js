b2f09a82428d44215d0b488579540682ac29e5b0
cwern40 <chris.wernli@hotmail.com>
2019-08-10 10:39:33 -0600

    I added a get request to get a list of the posts

--- posts-router.js
+++ posts-router.js
@@ -1,5 +1,20 @@
 const express = require('express');
 
-const posts = require('./data/db');
+const Posts = require('./data/db');
 
 const router = express.Router();
+// URI: /api/posts
+
+// Get request to get a list of the posts
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Posts.find()
+        res.status(200).json(posts);
+    } catch {
+        res.status(500).json({
+            message: 'The posts information could not be retrieved.'
+        })
+    }
+})
+
+module.exports = router;
