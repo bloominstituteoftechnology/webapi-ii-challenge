7cb4658021d6bd0a8305dca176e4790f038ba27d
cwern40 <chris.wernli@hotmail.com>
2019-08-10 11:11:32 -0600

    I added the get by ID functionality and the post functionality to add a post.

--- posts-router.js
+++ posts-router.js
@@ -17,4 +17,46 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await Posts.findById(req.params.id);
+        console.log(post);
+        if (post.length > 0) {
+            res.status(200).json(post)
+        } else {
+            res.status(404).json({
+                message: 'The post with the specified ID does not exist.'
+            });
+        }
+    } catch {
+        res.status(500).json({
+            message: 'The post information could not be retrieved.'
+        })
+    }
+})
+
+router.post('/', async (req, res) => {
+    if (!req.body.title || !req.body.contents) {
+        res.status(400).json({
+            errorMessage: 'Please provide a title and contents for the post.'
+        })
+    } else {
+        try {
+            const post = await Posts.insert(req.body);
+            try {
+                const find = await Posts.findById(post.id)
+                res.status(201).json(find)
+            } catch {
+                res.status(500).json({
+                    message: 'error in displaying newly create post.'
+                })
+            }
+        } catch {
+            res.status(500).json({
+                message: 'There was an error while saving the post to the database'
+            })
+        }
+    }
+})
+
 module.exports = router;
