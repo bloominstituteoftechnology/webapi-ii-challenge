1fa2a053906a8509b04bb39be51fe4e0552c9826
cwern40 <chris.wernli@hotmail.com>
2019-08-15 16:22:54 -0600

    Added the port variable to the index file and removed the package-lock.json

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const server = (require('./server'))
 
-server.listen(4000, () => {
-    console.log('Server running on 4000');
+const port = process.env.PORT || 4000
+
+server.listen(port, () => {
+    console.log(`Server running on ${port}`);
 });
