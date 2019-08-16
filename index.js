0a05a513e2640624af1132ff3da804fa842736d8
quan005 <ralphealfreeman@gmail.com>
2019-08-15 18:45:21 -0700

    Webapi IV Challenge

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+require('dotenv').config();
+
 const server = require('./server.js');
+
+const port = process.env.PORT || 4000;
 
 server.listen(4000, () => {
   console.log('\n*** Server Running on http://localhost:4000 ***\n');
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const helmet = require('helmet');
 
 const router = require('./router.js');
 
 const server = express();
 
+server.use(helmet());
 server.use(express.json());
 server.use('/api/posts', router);
 
