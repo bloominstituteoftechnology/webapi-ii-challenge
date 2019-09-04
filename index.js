4ad461a5406dba4396f79f0d4b41ccd9e96c9b3c
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-09-04 16:16:39 -0400

    all endpoints (minus /comments endpoints) working with all crud operations. confirmed using insomnia

---
+++ index.js
@@ -0,0 +1,111 @@
+// implement your API here
+
+const express = require('express');
+
+const db = require('./data/db.js');
+
+const port = 5000;
+
+const server = express();
+server.use(express.json());
+
+server.post('/api/posts', (req, res) => {
+  const userInfo = req.body;
+
+  db.insert(userInfo)
+    .then(result => {
+      res.json(result);
+      res.status(201);
+    })
+    .catch(error => {
+      res.rend(error);
+      res.status(500);
+    });
+});
+
+server.post('/api/posts/:id/comments', (req, res) => {
+  const userInfo = req.body;
+
+  db.insert(userInfo)
+    .then(result => {
+      res.json(result);
+      res.status(201);
+    })
+    .catch(error => {
+      res.rend(error);
+      res.status(500);
+    });
+});
+
+server.get('/api/posts', (req, res) => {
+  db.find()
+    .then(response => {
+      res.json(response);
+      res.status(200);
+    })
+    .then(error => {
+      res.rend(error);
+      res.status(500);
+    });
+});
+
+server.get('/api/posts/:id', (req, res) => {
+  const id = req.params.id;
+
+  db.findById(id)
+    .then(response => {
+      res.json(response);
+      res.status(201);
+    })
+    .catch(error => {
+      res.json(error);
+      res.status(500);
+    });
+});
+
+server.get('/api/posts/:id/comments', (req, res) => {
+  const id = req.params.id;
+
+  db.findById(id)
+    .then(response => {
+      res.json(response);
+      res.status(201);
+    })
+    .catch(error => {
+      res.json(error);
+      res.status(500);
+    });
+});
+
+server.delete('/api/posts/:id', (req, res) => {
+  const id = req.params.id;
+
+  db.remove(id)
+  .then(response => {
+    res.json(response);
+    res.status(201);
+  })
+  .catch(error => {
+    res.json(error);
+    res.status(500);
+  });
+});
+
+server.put('/api/posts/:id', (req, res) => {
+  const id = req.params.id;
+  const userInfo = req.body;
+
+  db.update(id, userInfo)
+    .then(response => {
+      res.json(response);
+      res.status(200);
+    })
+    .catch(error => {
+      res.json(error);
+      res.status(500);
+    });
+})
+
+server.listen(port, () => {
+  console.log(`server listening on port ${port}`);
+});
