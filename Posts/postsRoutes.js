1449b399946189e3b2b54e04c1dcce7305cdeea1
Tommy <tommykindle83@gmail.com>
2019-09-04 14:55:07 -0700

    Have create new Post set up and tested

--- Posts/postsRoutes.js
+++ Posts/postsRoutes.js
@@ -5,24 +5,50 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   db.find()
-    .then(posts => {
-      if (posts) {
-        req.status(200).json(posts);
-      } else {
-        res.status(500).json({ error: 'The posts information could not be retrieved.' })
-      }
-    })
-})
+    .then(posts => res.status(200).json(posts))
+    .catch(err => {
+      res.status(500).json({ error: "The Post information could not be retrieved" })
+    });
+});
 
 router.get('/:id', (req, res) => {
   const id = req.params.id;
   db.findById(id)
-    .then(posts => {
+    .then(([posts]) => {
       if (posts) {
-        res.status(200).json(posts);
+        res.status(200).json(posts)
       } else {
-        res.status(400).json({ error: 'The post with the specified ID does not exist.' })
+        res.status(404).json({ message: "The post with the specified ID does not exsist" })
       }
+    })
+});
+
+router.get('/:id/comments', (req, res) => {
+  const id = req.params.id;
+  db.findPostComments(id)
+    .then(comments => {
+      if (comments) {
+        res.status(200).json(comments)
+      } else {
+        res.status(404).json({ error: 'The comments information could not be retrieved.' })
+      }
+    })
+})
+
+router.post('/', (req, res) => {
+  const { title, contents } = req.body;
+  if (!title || !contents) {
+    return res.status(400).json({ error: "Please provide title and contents for the post" })
+  }
+  db.insert({ title, contents })
+    .then(({ id }) => {
+      db.findById(id)
+        .then(([post]) => {
+          res.status(201).json(post)
+        })
+    })
+    .catch(err => {
+      res.status(500).json({ error: "There was an error while saving the post to the database" })
     })
 })
 
