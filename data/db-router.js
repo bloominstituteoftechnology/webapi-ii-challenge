f8a382091d1d389ddd073350f05ccb3016426ffb
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-10-01 15:13:13 -0500

    still working on get

--- data/db-router.js
+++ data/db-router.js
@@ -5,17 +5,39 @@ const BPosts = require('./db.js');
 //Router() --> express! 
 const router = express.Router();
 
-//endpoints
-
+//ENDPOINTS
+//*************GET********************
 router.get('/', (req, res) => {
     BPosts.find(req.query)
-        .then(posts => {
-            res.status(200).json(posts);
+        .then(post => {
+            res.status(200).json(post);
         })
         .catch(error => {
-            res.status(500).json({ errorMessage: "Please provide title and contents for the post." });
+            res.status(500).json({ error: "The posts information could not be retrieved." });
         })
 })
+//*************GET (id) ********************
+
+router.get('/:id', (req, res) => {
+    BPosts.findById(req.params.id)
+        .then(postObj => {
+            if (postObj) {
+                res.status(200).json(postObj);
+            }
+
+            else{
+                res.status(404).json({ message: "The post with the specified ID does not exist." });
+            }
+            
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ error: "The post information could not be retrieved." });
+        })
+})
+//*************       ********************
+
+
 
 
 
