36ee9c60c9ba202973e3e54d3c565897a75d3862
Roger McConkie <roger.mcconkiejr@gmail.com>
2019-10-01 14:10:33 -0600

    a lot of endpoints working

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -29,6 +29,18 @@ const router = express.Router()
             res.status(500).json({ errorMessage: "The posts information could not be retrieved." });
         });
     });
+
+    router.get('/:id/comments', (req, res)=>{
+        const id = req.params.id;
+        postsModel
+        .findCommentById(id)
+        .then(posts => {
+            res.status(200).json(posts).json({ errorMessage: "The posts information could not be retrieved." });
+        })
+        .catch(error=> {
+            res.status(500)
+        })
+    })
     
     router.post('/', (req, res)=>{ 
         const postData = req.body;
@@ -36,7 +48,7 @@ const router = express.Router()
         console.log('post data', postData);
     
         if (!postData.title || !postData.contents) {
-            res.status(400).json({ errorMessage: "Please provide name and bio for the post." });
+            res.status(400).json({ errorMessage: "Please provide a title and contents for the post." });
         } else {
         postsModel
         .insert(postData)
@@ -47,6 +59,21 @@ const router = express.Router()
             res.status(500).json({ message: 'There was an error while saving the post to the database' });
         })
     }
+    })
+
+    router.post ('/:id/comments', (req, res)=>{
+
+        postsModel
+        .insertComment(req.body)
+        .then(posts => {
+            if (!posts) {
+                res.status(404).json({ message: "The post with the specified ID does not exist." });
+            } else if (!req.body.text) {
+                res.status(400).json({ message: "Please provide text for the comment."})
+            } else {res.status(201).json(posts)}
+        }).catch(error=>{
+            res.status(500).json({ message: 'There was an error while saving the post to the database' })
+        })
     })
     
     router.delete('/:id', (req, res)=>{
