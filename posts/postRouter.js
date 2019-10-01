3e2b87b2adddf0b0371249899143ceb90837898a
zachary-peasley <zpman95@hotmail.com>
2019-10-01 17:03:39 -0500

    Added put and deleted request

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -53,6 +53,85 @@ router.get('/:id', (req, res) => {
             res.status(500).json ({message: 'The post with the sqecified ID does not exist.'})
         }
     })
+
+
+  router.delete('/:id', (req, res) => {
+      const id = req.params.id;
+      DateBase.remove(id)
+      .then(deleted => {
+          console.log(deleted);
+          if (deleted) {
+              res.status(200).json(id);
+          } else {
+              res.status(404).json({message: "The post with the specified ID does not exist "})
+          }
+      })
+      .catch(error => {
+          console.log(error);
+          res.status(500).json({error: "there was an error deleteing the post"})
+      })
+  }) 
+
+  router.put('/:id', (req,res) => {
+      const id = req.params.id;
+      const title = req.body.title;
+      const contents = req.body.contents;
+      if( !title && !contents) {
+          return res.status(400).json({error: " Please porvide title and contents for the people"})
+      }
+      DateBase.update(id, {title, contents })
+      .then(update => {
+          console.log(updated);
+          if (updated) {
+              DateBase.findById(id)
+              .then(post => {
+                  console.log(post)
+                  if(post.length) {
+                      res.status(200).json(post)
+                  } else {
+                      res.status(404).json({message:"The post with the ID does not exists"})
+                  }
+              })
+              .catch(error => {
+                console.log(error);
+                res.status(500).json({error: "there was an error deleteing the post"})
+              })
+          }
+      })
+  })
+
+
+router.get('/:post_id/comments', (req,res ) => {
+    const {post_id} = req.params;
+    DateBase.findById(post_id)
+    .then(post => {
+        if(post.length) {
+            DateBase.findPostComments(post_id)
+            .then(comments => 
+                res.status(200).json(comments))
+        } else {
+            res.status(404).json({message: "The post with the ID does not exist"})
+        }
+    })
+    .catch (error => {
+        console.log(error);
+        res.status(500).json({error: "The comments information could not be retrieved"})
+    });
+    });
+
+    router.post('/:post_id/comments', (req, res) => {
+        const { post_id } = req.params;
+        const text = req.body.text;
+        DateBase.insertComment({text, post_id})
+        .then(comment => {
+            res.status(200).json(comment)
+        })
+        .catch (error => {
+            console.log(error);
+            res.status(500).json({error: "The comments information could not be retrieved"})
+    
+    })
+})
 })
 
 module.exports = router;
