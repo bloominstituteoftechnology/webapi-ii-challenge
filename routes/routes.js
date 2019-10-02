c4fffb7ceb00295d2ff194d42c963cd4fa817df5
Brad <Brad@BRADSPC>
2019-10-02 14:24:47 -0600

    Get route working

---
+++ routes/routes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const db = require('../data/db.js');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  db.find()
+    .then(allPosts => res.status(200).json(allPosts))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({error: "Couldn't find posts"})
+    })
+});
+
+router.post('/', (req, res) => {
+  const {postTitle, postContent} = req.body;
+
+  if(postTitle && postContent){
+    db.insert({postTitle, postContent})
+      .then()
+  }
+})
+
+
+module.exports = router;
