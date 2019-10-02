fb787e2f4ea1c348c3d7ef08485858fc3556b27e
ian-sanchez <silkyhairbear@gmail.com>
2019-10-02 10:57:05 -0700

    posting

--- postRouter.js
+++ postRouter.js
@@ -8,7 +8,43 @@ router.get('/', (req, res) => {
         .then(posts => res.status(200).json(posts))
         .catch(err => {
             console.log(err);
-            res.status(500).json()
+            res.status(500).json({ error: "The posts information could not be retrieved." })
+        })        
+})
+
+router.post('/', (req, res) => {
+    const { title, contents } = req.body;
+    if (!title || !contents) {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+    } else {
+        db.insert({title, contents})
+            .then(({id}) => {
+                db.findById(id)
+                    .then(([post]) => {
+                        res.status(201).json(post);
+                    })
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({ error: "There was an error while saving the post to the database" })
+            })
+    }
+})
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    db.findById(id)
+        .then(([post]) => {
+            console.log(post);
+            if (post) {
+                res.status(200).json(post);
+            } else {
+                res.status(404).json({
+                    err: err,
+                    message: "The post with the specified ID does not exist." 
+                })
+            }
+            
         })
 })
 
