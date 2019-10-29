a213ec5522a0d37f3521fbeca550e6b62dd6a5c0
Pere <pere.solaclaver@gmail.com>
2019-10-29 12:39:45 +0000

    POST post end point

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const posts = require("./data/db");
 
 const server = express();
 
@@ -8,12 +9,38 @@ server.use(express.json());
 
 // BEGINNING OF END POINTS
 
-server.get('/', (req, res) => {
-    res.status(200).json('hello from server!')
-})
+server.post("/api/posts", createPost);
 
 // END OF END POINTS
 
+// BEGINNING OF REQUEST HANDLERS
+
+function createPost(req, res) {
+  const { title, contents } = req.body;
+
+  if (!title || !contents) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide title and contents for the post." })
+      .end();
+  }
+  posts
+    .insert(req.body)
+    .then(data => {
+      res.status(201).json(data);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({
+          error: "There was an error while saving the post to the database"
+        })
+        .end();
+    });
+}
+
+// END OF REQUEST HANDLERS
+
 server.listen(process.env.PORT || 3000, () => {
-    console.log(`Listening on ${process.env.PORT || 3000}`)
-})
+  console.log(`Listening on ${process.env.PORT || 3000}`);
+});
