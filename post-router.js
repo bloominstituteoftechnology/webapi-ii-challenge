fb9ade4c53fb7bed0fc7409cfc548d71154ccd19
Angela Flowers <me@angela.flowers>
2019-10-29 19:02:03 -0400

    post requests for new posts/comments

--- post-router.js
+++ post-router.js
@@ -8,10 +8,10 @@ router.get('/', (req, res) => {
             res.status(200).json(posts);
         })
         .catch(err => {
-            console.log('\n=== ERROR ===\n', error);
-            res.status(500).json({ error: "The posts information could not be retrieved." })
+            console.log('\n=== ERROR ===\n', err);
+            res.status(500).json({ error: "The posts information could not be retrieved." });
         })
-})
+});
 
 router.get('/:id', (req, res) => {
     const postID = req.params.id;
@@ -19,16 +19,16 @@ router.get('/:id', (req, res) => {
     db.findById(postID)
         .then(post => {
             if (!post.length) {
-                res.status(404).json({ message: "The post with the specified ID does not exist." })
+                res.status(404).json({ message: "The post with the specified ID does not exist." });
             } else {
                 res.status(200).json(post);
             }
         })
         .catch(err => {
-            console.log('\n=== ERROR ===\n', error);
-            res.status(500).json({ error: "The post information could not be retrieved." })
+            console.log('\n=== ERROR ===\n', err);
+            res.status(500).json({ error: "The post information could not be retrieved." });
         });
-})
+});
 
 router.get('/:id/comments', (req, res) => {
     const postID = req.params.id;
@@ -41,16 +41,77 @@ router.get('/:id/comments', (req, res) => {
             db.findPostComments(postID)
             .then(comments => res.status(200).json(comments))
             .catch(err => {
-                console.log('\n=== ERROR ===\n', error);
+                console.log('\n=== ERROR ===\n', err);
                 res.status(500).json({ error: "The comments information could not be retrieved." })
             });
         }
     })
     .catch(err => {
-        console.log('\n=== ERROR ===\n', error);
+        console.log('\n=== ERROR ===\n', err);
         res.status(500).json({ error: "The comments information could not be retrieved." })
     });
     
+});
+
+router.post('/', (req, res) => {
+    const newPost = req.body;
+
+    if (!newPost.title || !newPost.contents) {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+    } else {
+        db.insert(newPost)
+            .then(confirmation => {
+                db.findById(confirmation.id)
+                    .then(newlyPosted => {
+                        res.status(201).json(newlyPosted);
+                    })
+                    .catch(err => {
+                        console.log('\n=== ERROR ===\n', err);
+                        res.status(500).json({ error: "There was an error while saving the post to the database" });
+                    });
+                })
+            .catch(err => {
+                console.log('\n=== ERROR ===\n', err);
+                res.status(500).json({ error: "There was an error while saving the post to the database" });
+            });
+    }
+});
+
+
+router.post('/:id/comments', (req, res) => {
+    const postID = req.params.id;
+    const newComment = req.body;
+
+    if (!newComment.text) {
+        res.status(400).json({ errorMessage: "Please provide text for the comment." })
+    } else {
+        db.findById(postID)
+        .then(post => {
+            if (!post.length) {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+            } else {
+                db.insertComment({ ...newComment, post_id: postID})
+                    .then(commentID => {
+                        db.findCommentById(commentID.id)
+                            .then(commented => {
+                                res.status(201).json(commented);
+                            })
+                            .catch(err => {
+                                console.log('\n=== ERROR ===\n', err);
+                                res.status(500).json({ error: "There was an error while saving the comment to the database" })
+                            });
+                    })
+                    .catch(err => {
+                        console.log('\n=== ERROR ===\n', err);
+                        res.status(500).json({ error: "There was an error while saving the comment to the database" })
+                    });        
+            }
+        })
+        .catch(err => {
+            console.log('\n=== ERROR ===\n', err);
+            res.status(500).json({ error: "There was an error while saving the comment to the database" })
+        });
+    }    
 });
 
 
