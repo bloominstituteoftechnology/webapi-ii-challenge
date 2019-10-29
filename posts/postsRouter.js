50cb3291a32d2789414835f46e8c5c545717e42a
Cody Brewer <brewer.cody.m@gmail.com>
2019-10-29 17:34:30 -0600

    PUT - /api/posts/:id working

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -17,8 +17,8 @@ router.post("/", (req, res) => {
             - return the newly created _post_.
         */
       insert(newPost)
-        .then(insertedUser => {
-          res.status(200).json(insertedUser);
+        .then(insertedpost => {
+          res.status(200).json(insertedpost);
         })
         /*- If there's an error while saving the _post_:
             - cancel the request.
@@ -87,11 +87,11 @@ router.get("/:id", (req, res) => {
     */
     .catch(err => {
       res.status(500).json({
-        errorMessage: `The user information could not be retrieved.`,
+        errorMessage: `The post information could not be retrieved.`,
         serverError: `${err}`
       });
     });
-}); 
+});
 // DELETE - /api/posts/:id - Removes the post with the specified id and returns the deleted post object. You may need to make additional calls to the database in order to satisfy this requirement.
 
 router.delete("/:id", (req, res) => {
@@ -115,12 +115,59 @@ router.delete("/:id", (req, res) => {
       - respond with HTTP status code `500`.
       - return the following JSON object: `{ error: "The post could not be removed" }`. */
     .catch(err => {
-      res
-        .status(500)
-        .json({
-          errorMessage: "The post could not be removed",
-          serverError: `${err}`
-        });
+      res.status(500).json({
+        errorMessage: "The post could not be removed",
+        serverError: `${err}`
+      });
     });
 });
 // PUT - /api/posts/:id - Updates the post with the specified id using data from the request body. Returns the modified document, NOT the original.
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { title, contents } = req.body;
+  const post = { title, contents };
+
+  title && contents
+    ? update(id, post)
+        .then(updatedPost => {
+          updatedPost
+            ? /*
+          - If the post is found and the new information is valid:
+            - update the post document in the database using the new information sent in the `request body`.
+            - return HTTP status code `200` (OK).
+            - return the newly updated _post_.
+          */
+              res.status(200).json(updatedPost)
+            : /*
+               - If the _post_ with the specified `id` is not found:
+                 - return HTTP status code `404` (Not Found).
+                 - return the following JSON object: `{ message: "The post with the specified ID does not exist." }`.
+            */
+              res
+                .status(404)
+                .json({
+                  errorMessage: `The post with the specified ID does not exist.`
+                });
+        })
+        /*
+      - If there's an error when updating the _post_:
+        - cancel the request.
+        - respond with HTTP status code `500`.
+        - return the following JSON object: `{ error: "The post information could not be modified." }`.
+      */
+        .catch(err =>
+          res.status(500).json({
+            errorMessage: `The post information could not be modified.`,
+            serverError: `${err}`
+          })
+        )
+    : /*
+      - If the request body is missing the `title` or `contents` property:
+        - cancel the request.
+        - respond with HTTP status code `400` (Bad Request).
+        - return the following JSON response: `{ errorMessage: "Please provide title and contents for the post." }`.
+      */
+      res
+        .status(400)
+        .json({ errorMessage: `Please provide title and contents for the post.` });
+});
