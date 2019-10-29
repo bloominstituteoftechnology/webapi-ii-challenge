1f3c943aa8c169fedd79fc676ea88dc89c8b0b02
Angela Flowers <me@angela.flowers>
2019-10-29 17:53:47 -0400

    all get requests working

--- post-router.js
+++ post-router.js
@@ -13,4 +13,45 @@ router.get('/', (req, res) => {
         })
 })
 
+router.get('/:id', (req, res) => {
+    const postID = req.params.id;
+    
+    db.findById(postID)
+        .then(post => {
+            if (!post.length) {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+            } else {
+                res.status(200).json(post);
+            }
+        })
+        .catch(err => {
+            console.log('\n=== ERROR ===\n', error);
+            res.status(500).json({ error: "The post information could not be retrieved." })
+        });
+})
+
+router.get('/:id/comments', (req, res) => {
+    const postID = req.params.id;
+
+    db.findById(postID)
+    .then(post => {
+        if (!post.length) {
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        } else {
+            db.findPostComments(postID)
+            .then(comments => res.status(200).json(comments))
+            .catch(err => {
+                console.log('\n=== ERROR ===\n', error);
+                res.status(500).json({ error: "The comments information could not be retrieved." })
+            });
+        }
+    })
+    .catch(err => {
+        console.log('\n=== ERROR ===\n', error);
+        res.status(500).json({ error: "The comments information could not be retrieved." })
+    });
+    
+});
+
+
 module.exports = router;
