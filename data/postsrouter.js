19a5fb8df58c010aaf51badc350f7e7d44530ef1
Jashele T <jashele@Jasheles-Air.domain>
2019-11-05 14:31:18 -0700

    tests working in postman - GET and also /api/posts showing up

--- data/postsrouter.js
+++ data/postsrouter.js
@@ -3,19 +3,46 @@ const router = express.Router();
 const Posts = require('./db.js');
 
 
-//Creates a post using the information sent inside the request body.
+
+// Returns an array of all the post objects contained in the 
+router.get('/', (req, res) => {
+  Posts.find(req.query)
+  .then(posts => {
+    res.status(200).json(posts);
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({
+      message: "Error retrieving the posts"
+    })
+  })
+}); // READ data
+
+
+// 	Returns the post object with the specified id.
 //code here.
+
+
+
+//Creates a post using the information sent inside the request body.
+router.post("/", (req, res) => {
+  Posts.add(req.body)
+  .then(data => {
+    res.status(201).json(data);
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({
+      message: "Error adding the data"
+    });
+  });
+
+});  // CREATE data
 
 
 // 	Creates a comment for the post with the specified id using information sent inside of the request body.
 //code here.
 
-// Returns an array of all the post objects contained in the 
-//code here.
-
-
-// 	Returns the post object with the specified id.
-//code here.
 
 
 // Returns an array of all the comment objects associated with the post with the specified id.
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const server = require('./server.js')
+const server = require('./server.js');
 
 server.listen(4000, () => {
   console.log('\n*** Server Running on http://localhost:4000 ***\n');
