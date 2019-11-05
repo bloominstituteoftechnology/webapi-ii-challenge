94d4708191e7183e3d8d61666ea2842062ce7547
Jashele T <jashele@Jasheles-Air.domain>
2019-11-05 15:08:26 -0700

    tested deletion of my post, postman test passed

--- data/postsrouter.js
+++ data/postsrouter.js
@@ -3,6 +3,39 @@ const router = express.Router();
 const Posts = require('./db.js');
 
 
+// Creates a post using the information sent inside the request body.
+router.post("/", (req, res) => {
+  Posts.insert(req.body)
+  .then(data => {
+    res.status(201).json(data);
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({
+      message: "Error adding the data"
+    });
+  });
+
+});  // CREATE data
+
+
+// Creates a comment for the post with the specified id using information sent inside of the request body.
+router.post('/:id/comments', async (req, res) => {
+    const commentInfo = {...req.body, post_id: 
+    req.params.id }
+
+    try {
+      const comment = await Posts.insertComment(commentInfo);
+      res.status(201).json(comment);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({
+        err
+      });
+    }
+});  // CREATE data
+
+
 
 // Returns an array of all the post objects contained in the 
 router.get('/', (req, res) => {
@@ -63,50 +96,32 @@ router.get('/:id/comments', async (req, res) => {
 
 
 
-// Creates a post using the information sent inside the request body.
-router.post("/", (req, res) => {
-  Posts.insert(req.body)
-  .then(data => {
-    res.status(201).json(data);
-  })
-  .catch(error => {
-    console.log(error);
-    res.status(500).json({
-      message: "Error adding the data"
-    });
-  });
-
-});  // CREATE data
-
-
-// Creates a comment for the post with the specified id using information sent inside of the request body.
-router.post('/:id/comments', async (req, res) => {
-    const commentInfo = {...req.body, post_id: 
-    req.params.id }
-
-    try {
-      const comment = await Posts.insertComment(commentInfo);
-      res.status(201).json(comment);
-    } catch (err) {
-      console.log(err);
-      res.status(500).json({
-        err
-      });
-    }
-});  // CREATE data
-
-
-
-// Returns an array of all the comment objects associated with the post with the specified id.
-//code here.
-
-
 // Removes the post with the specified id and returns the deleted post object. You may need to make additional calls to the database in order to satisfy this requirement.
-//code here.
+router.delete('/:id', (req, res) => {
+ Posts.remove(req.params.id)
+ .then(count => {
+   if (count > 0) {
+     res.status(200).json({
+       Message: "The post has been nuked"
+     });
+   } else {
+     res.status(404).json({
+       Message: "The post could not be found"
+     })
+   }
+ }) .catch(error => {
+   console.log(error);
+   res.status(400).json({
+     Message: "Error removing the post"
+   })
+ })
+}); // DESTORYING data
 
 
 // Updates the post with the specified id using data from the request body. Returns the modified document, NOT the original.
-//code here.
+router.put('/:id', (req, res) => {
+  res.status(200).json({url: '/hobbits', operation: 'PUT'});
+}) // UPDATE data
 
 
 
