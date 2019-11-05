135a7fc5b5bc54a4fcbb3c51ce32948f02f103a0
dmunter2 <49967133+dmunter2@users.noreply.github.com>
2019-11-04 20:37:46 -0800

    post, get working

---
+++ blogs/blog-router.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const Blogs = require('../data/db');
+
+const router = express.Router();
+
+
+
+
+router.get('/', ((req, res) => {
+    Blogs.find()
+        .then(posts => {
+            res.status(200).json(posts)
+        })
+        .catch(err => {
+            res.status(404).json({message: 'this isnt wokring'})
+        })
+}))
+
+
+router.post('/', ((req, res) => {
+    const {title, contents} = req.body;
+    if (!title || !contents){
+        res.status(400).json({
+            errorMessage: 'you must have the title and contents'
+        })
+    } else {
+      Blogs.insert(req.body)
+        .then(postId => {
+            return postId.id;
+            }) .then(id => {
+                Blogs.findById(id)
+            }) .then(post => {
+                res.status(201).json(post);
+            }) .catch(err => {
+                res.status(500).json({error: 'could not retreive info'})
+            })
+        .catch( err => {
+            res.status(500).json({error: 'not working'})
+        })
+    }
+}))
+
+
+
+router.put('/:id', (req,res) => {
+    const id = req.body.id
+    const posts = req.body
+
+    Blogs.update(id, posts)
+        .then(post => {
+            res.status(200).json(post)
+        })
+
+        .catch(err => {
+            res.status(404).json({error: "there is an error"})
+        })
+})
+
+
+
+
+
+// router.delete('/:id', (req, res) => {
+
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,8 @@
+const server = require('./server.js')
+
+
+server.listen(5000, () => {
+    console.log('--Server 4000 is working --')
+})
+
+
